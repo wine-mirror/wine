a4777098737bde23bbe2b5cec33724a0ab5da073
Juergen Schmied <juergen.schmied@metronet.de>
1998-11-06 10:12:29 +0000

    Fixed one wrong parameter.

--- include/interfaces.h
+++ include/interfaces.h
@@ -46,7 +46,7 @@ typedef struct {
 	STDMETHOD_(ULONG,AddRef) (THIS) PURE;
 	STDMETHOD_(ULONG,Release) (THIS) PURE;
 	STDMETHOD(CreateInstance) (THIS_ LPUNKNOWN pUnkOuter, REFIID riid, LPVOID FAR* ppvObject) PURE;
-    STDMETHOD(LockServer) (THIS) PURE;
+	STDMETHOD(LockServer) (THIS_ BOOL32) PURE;
 } *LPCLASSFACTORY_VTABLE,IClassFactory_VTable;
 
 struct IClassFactory {
