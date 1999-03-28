8768d7cc9968aab5ffae9bde9a9d3118011da4ee
Jess Haas <paranoid07@hotmail.com>
1999-03-28 09:17:11 +0000

    Fixed a warning.

--- msdos/ioports.c
+++ msdos/ioports.c
@@ -21,6 +21,7 @@
 #include "dosexe.h"
 #include "options.h"
 #include "debug.h"
+#include "miscemu.h"
 
 static WORD tmr_8253_countmax[3] = {0xffff, 0x12, 1}; /* [2] needs to be 1 ! */
 /* if byte_toggle is TRUE, then hi byte has already been written */
