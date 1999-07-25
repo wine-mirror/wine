f518b1939e568c902454ba27c7bb4545bcdd3067
Juergen Schmied <juergen.schmied@metronet.de>
1999-07-25 12:17:31 +0000

    New file. Contains functions not in the official headers but widely used from other dlls.

---
+++ include/wine/undocshell.h
@@ -0,0 +1,54 @@
+#ifndef __WINE_UNDOCSHELL_H
+#define __WINE_UNDOCSHELL_H
+
+#include "windef.h"
+#include "shell.h"
+#include "wine/obj_shellfolder.h"	/* strret */
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* defined(__cplusplus) */
+
+/****************************************************************************
+ *	IDList Functions
+ */
+LPITEMIDLIST WINAPI ILClone (LPCITEMIDLIST pidl);
+LPITEMIDLIST WINAPI ILCloneFirst(LPCITEMIDLIST pidl);
+LPITEMIDLIST WINAPI ILCombine(LPCITEMIDLIST iil1,LPCITEMIDLIST iil2);
+
+DWORD WINAPI ILGetSize(LPITEMIDLIST pidl);
+
+LPITEMIDLIST WINAPI ILGetNext(LPITEMIDLIST pidl);
+LPITEMIDLIST WINAPI ILFindLastID(LPITEMIDLIST pidl);
+BOOL WINAPI ILRemoveLastID(LPCITEMIDLIST pidl);
+LPITEMIDLIST WINAPI ILFindChild(LPCITEMIDLIST pidl1,LPCITEMIDLIST pidl2);
+
+BOOL WINAPI ILIsEqual(LPCITEMIDLIST pidl1, LPCITEMIDLIST pidl2);
+
+BOOL WINAPI ILGetDisplayName(LPCITEMIDLIST pidl,LPSTR path);
+
+DWORD WINAPI ILFree(LPITEMIDLIST pidl);
+
+LPITEMIDLIST WINAPI SHSimpleIDListFromPathA (LPSTR lpszPath);
+LPITEMIDLIST WINAPI SHSimpleIDListFromPathW (LPWSTR lpszPath);
+LPITEMIDLIST WINAPI SHSimpleIDListFromPathAW (LPVOID lpszPath);
+
+HRESULT WINAPI SHILCreateFromPathA (LPSTR path, LPITEMIDLIST * ppidl, DWORD attributes);
+HRESULT WINAPI SHILCreateFromPathW (LPWSTR path, LPITEMIDLIST * ppidl, DWORD attributes);
+HRESULT WINAPI SHILCreateFromPathAW (LPVOID path, LPITEMIDLIST * ppidl, DWORD attributes);
+
+LPITEMIDLIST WINAPI ILCreateFromPathA(LPSTR path);
+LPITEMIDLIST WINAPI ILCreateFromPathW(LPWSTR path);
+LPITEMIDLIST WINAPI ILCreateFromPathAW(LPVOID path);
+
+/*
+	string functions
+*/
+HRESULT WINAPI SHBindToParent(LPCITEMIDLIST pidl, REFIID riid, LPVOID *ppv, LPCITEMIDLIST *ppidlLast);
+
+HRESULT WINAPI StrRetToStrN (LPVOID dest, DWORD len, LPSTRRET src, LPITEMIDLIST pidl);
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /* defined(__cplusplus) */
+
+#endif /* __WINE_SHLOBJ_H */
