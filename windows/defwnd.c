268a36e872d7e95c0ea2265006042f817e86a27a
Dmitry Timoshkov <dmitry@sloboda.ru>
2000-04-23 19:57:21 +0000

    Removed the longstanding hack in response to WM_ACTIVATE message.

--- windows/defwnd.c
+++ windows/defwnd.c
@@ -294,10 +294,6 @@ static LRESULT DEFWND_DefWinProc( WND *wndPtr, UINT msg, WPARAM wParam,
 	/* The default action in Windows is to set the keyboard focus to
 	 * the window, if it's being activated and not minimized */
 	if (LOWORD(wParam) != WA_INACTIVE) {
-		/* I don't know who put this SetWindowPos here, it does not
-		 * seem very logical to have it here... (FIXME?) */
-		SetWindowPos(wndPtr->hwndSelf, HWND_TOP, 0, 0, 0, 0,
-			 SWP_NOMOVE | SWP_NOSIZE);
 		if (!(wndPtr->dwStyle & WS_MINIMIZE))
 			SetFocus(wndPtr->hwndSelf);
 	}
