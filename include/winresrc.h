2e41c3b1f7f3542435ffc5458bebcc1f0d74f16a
Hidenori Takeshima <hidenori@a2.ctktv.ne.jp>
2000-06-23 15:35:54 +0000

    Added include/winresrc.h for porting with winelib.

---
+++ include/winresrc.h
@@ -0,0 +1,12 @@
+#ifndef _WINRESRC_=0A=
+#define _WINRESRC_=0A=
+=0A=
+#include "windef.h"=0A=
+#include "winuser.h"=0A=
+#include "commctrl.h"=0A=
+#include "dde.h"=0A=
+#include "winnt.h"=0A=
+#include "dlgs.h"=0A=
+#include "winver.h"=0A=
+=0A=
+#endif /* _WINRESRC_ */=0A=
