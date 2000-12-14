f45f66beff3ee59c2773823b8930525b57912a35
Alexandre Julliard <julliard@winehq.org>
2000-12-14 19:30:01 +0000

    Commented out large stack init now that thread stacks are at least 1Mb.

--- scheduler/sysdeps.c
+++ scheduler/sysdeps.c
@@ -298,8 +298,10 @@ void SYSDEPS_SwitchToThreadStack( void (*func)(void) )
     teb->stack_top = (LPVOID) cur_stack;
     teb->stack_low = (LPVOID)(cur_stack - rl.rlim_cur);
 
+#if 0
     SYSDEPS_LargeStackTop = (LPVOID)(cur_stack - 2*page_size);
     SYSDEPS_LargeStackLow = (LPVOID)(cur_stack - rl.rlim_cur);
+#endif
 
     SYSDEPS_CallOnStack( stackTop, stackLow, 
                          (int (*)(void *))func, NULL );
