335b9e3be3f14395c7e53af4c59139fada71c396
Alexandre Julliard <julliard@winehq.org>
2000-12-19 19:38:48 +0000

    Include stdlib.h instead of wchar.h.

--- library/port.c
+++ library/port.c
@@ -14,6 +14,7 @@
 #include <assert.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -23,7 +24,6 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <wchar.h>
 #ifdef HAVE_SYS_MMAN_H
 #include <sys/mman.h>
 #endif
