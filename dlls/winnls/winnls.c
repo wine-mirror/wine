b455c75242d701f3937cad6ef345e1e1ce697fbe
Andreas Mohr <amohr@codeweavers.com>
2001-02-12 01:13:54 +0000

    A couple of stubs for Terminal Server Client 16 bit.

--- dlls/winnls/winnls.c
+++ dlls/winnls/winnls.c
@@ -1,1 +1,13 @@
-/* nothing here yet */
+#include "windef.h"
+#include "wine/winuser16.h"
+
+BOOL WINAPI WINNLSEnableIME16(HWND16 hWnd, BOOL fEnable)
+{
+    /* fake return of previous status. is this what this function should do ? */
+    return !fEnable;
+}
+
+BOOL WINAPI WINNLSGetEnableStatus16(HWND16 hWnd)
+{
+    return FALSE;
+}
