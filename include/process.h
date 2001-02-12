be98c7b1b34f9d4769bb80c926060594ef3718b7
Jon Griffiths <jon_p_griffiths@yahoo.com>
2001-02-12 03:38:46 +0000

    Use extern "C" for Winelib C++ apps.

--- include/process.h
+++ include/process.h
@@ -7,6 +7,10 @@
 #ifndef _INC_PROCESS
 #define _INC_PROCESS
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef void (*LPBEGINTHREAD)(LPVOID);
 typedef UINT WINAPI (*LPBEGINTHREADEX)(LPVOID);
 
@@ -14,5 +18,9 @@ ULONG _beginthread(LPBEGINTHREAD,UINT,LPVOID);
 void  _endthread(void);
 ULONG _beginthreadex(LPVOID,UINT,LPBEGINTHREADEX,LPVOID,UINT,LPUINT);
 void  _endthreadex(UINT);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  /* _INC_PROCESS */
