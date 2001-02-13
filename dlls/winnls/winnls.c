7ac3db7b7fd06093fa7ae172713d60bbab010312
Patrik Stridvall <ps@leissner.se>
2001-02-13 20:19:15 +0000

    Fixed some issues found by winapi_check.

--- dlls/winnls/winnls.c
+++ dlls/winnls/winnls.c
@@ -1,12 +1,18 @@
 #include "windef.h"
 #include "wine/winuser16.h"
 
+/***********************************************************************
+ *		WINNLSEnableIME (WINNLS.16)
+ */
 BOOL WINAPI WINNLSEnableIME16(HWND16 hWnd, BOOL fEnable)
 {
     /* fake return of previous status. is this what this function should do ? */
     return !fEnable;
 }
 
+/***********************************************************************
+ *		WINNLSGetEnableStatus (WINNLS.18)
+ */
 BOOL WINAPI WINNLSGetEnableStatus16(HWND16 hWnd)
 {
     return FALSE;
