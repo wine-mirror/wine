0ba5909038373f190892d044cdfff6923a60f830
Johannes E. Schindelin <Johannes.Schindelin@gmx.de>
2001-02-28 21:41:17 +0000

    Some minor changes to the German keyboard layout.

--- windows/x11drv/keyboard.c
+++ windows/x11drv/keyboard.c
@@ -155,11 +155,11 @@ static const char main_key_IS[MAIN_LEN][4] =
 /*** German keyboard layout (contributed by Ulrich Weigand) */
 static const char main_key_DE[MAIN_LEN][4] =
 {
- "^°","1!","2\"","3§","4$","5%","6&","7/{","8([","9)]","0=}","ß?\\","'",
- "qQ","wW","eE","rR","tT","zZ","uU","iI","oO","pP","üÜ","+*~",
+ "^°","1!","2\"²","3§³","4$","5%","6&","7/{","8([","9)]","0=}","ß?\\","'`",
+ "qQ@","wW","eE€","rR","tT","zZ","uU","iI","oO","pP","üÜ","+*~",
  "aA","sS","dD","fF","gG","hH","jJ","kK","lL","öÖ","äÄ","#´",
- "yY","xX","cC","vV","bB","nN","mM",",;",".:","-_",
- "<>"
+ "yY","xX","cC","vV","bB","nN","mMµ",",;",".:","-_",
+ "<>|"
 };
 
 /*** German keyboard layout without dead keys */
