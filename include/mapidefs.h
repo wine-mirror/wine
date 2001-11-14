d3f8e83495db7a6851c5513b87d84579ca10c58e
Jussi Jumppanen <jussij@zeusedit.com>
2001-11-14 21:23:17 +0000

    Added missing MAPI defines.

--- include/mapidefs.h
+++ include/mapidefs.h
@@ -28,4 +28,24 @@ typedef union tagCY
 } CY;
 #endif /* _tagCY_DEFINED */
 
+/* MAPI Recipient types */
+#ifndef MAPI_ORIG
+#define MAPI_ORIG        0          /* Message originator   */
+#define MAPI_TO          1          /* Primary recipient    */
+#define MAPI_CC          2          /* Copy recipient       */
+#define MAPI_BCC         3          /* Blind copy recipient */
+#define MAPI_P1          0x10000000 /* P1 resend recipient  */
+#define MAPI_SUBMITTED   0x80000000 /* Already processed    */
+#endif
+
+/* Bit definitions for abFlags[0] */
+#define MAPI_SHORTTERM   0x80
+#define MAPI_NOTRECIP    0x40
+#define MAPI_THISSESSION 0x20
+#define MAPI_NOW         0x10
+#define MAPI_NOTRESERVED 0x08
+
+/* Bit definitions for abFlags[1]  */
+#define MAPI_COMPOUND    0x80
+
 #endif /*__WINE_MAPIDEFS_H*/
