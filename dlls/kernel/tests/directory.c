80b72db33bb078718c5e1d40db1f543292104cc4
Francois Gouget <fgouget@codeweavers.com>
2002-04-01 21:00:26 +0000

    Windows headers must be included after wine/test.h.

--- dlls/kernel/tests/directory.c
+++ dlls/kernel/tests/directory.c
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include "winbase.h"
 #include "wine/test.h"
+#include "winbase.h"
 
 
 /* If you change something in these tests, please do the same
