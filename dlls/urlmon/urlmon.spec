b54389b3ed3f77d92b72633e13241a1f69299017
Hidenori Takeshima <hidenori@a2.ctktv.ne.jp>
2002-04-08 22:45:34 +0000

    Added some stubs.

--- dlls/urlmon/urlmon.spec
+++ dlls/urlmon/urlmon.spec
@@ -1,10 +1,12 @@
 name    urlmon
 type    win32
+init	COMIMPL_DllMain
 
 import	ole32.dll
+import	kernel32.dll
 import	ntdll.dll
 
-debug_channels (urlmon win32)
+debug_channels (comimpl urlmon)
 
 1 stub CDLGetLongPathNameA
 2 stub CDLGetLongPathNameW
@@ -26,60 +28,60 @@ debug_channels (urlmon win32)
 @ stub CopyStgMedium
 @ stub CreateAsyncBindCtx
 @ stdcall CreateAsyncBindCtxEx(ptr long ptr ptr ptr long) CreateAsyncBindCtxEx
-@ stub CreateFormatEnumerator
+@ stdcall CreateFormatEnumerator(long ptr ptr) CreateFormatEnumerator
 @ stdcall CreateURLMoniker(ptr str ptr) CreateURLMoniker
-@ stdcall DllCanUnloadNow() URLMON_DllCanUnloadNow
-@ stdcall DllGetClassObject(ptr ptr ptr) URLMON_DllGetClassObject
+@ stdcall DllCanUnloadNow() COMIMPL_DllCanUnloadNow
+@ stdcall DllGetClassObject(ptr ptr ptr) COMIMPL_DllGetClassObject
 @ stdcall DllInstall(long ptr) URLMON_DllInstall
 @ stdcall DllRegisterServer() URLMON_DllRegisterServer
 @ stdcall DllRegisterServerEx() URLMON_DllRegisterServerEx
 @ stdcall DllUnregisterServer() URLMON_DllUnregisterServer
 @ stdcall Extract(long long) Extract
 @ stub FaultInIEFeature
-@ stub FindMediaType
-@ stub FindMediaTypeClass
-@ stub FindMimeFromData
-@ stub GetClassFileOrMime
-@ stub GetClassURL
+@ stdcall FindMediaType(str ptr) FindMediaType
+@ stdcall FindMediaTypeClass(ptr str ptr long) FindMediaTypeClass
+@ stdcall FindMimeFromData(ptr wstr ptr long wstr long ptr long) FindMimeFromData
+@ stdcall GetClassFileOrMime(ptr wstr ptr long wstr long ptr) GetClassFileOrMime
+@ stdcall GetClassURL(wstr ptr) GetClassURL
 @ stub GetComponentIDFromCLSSPEC
 @ stub GetMarkOfTheWeb
-@ stub GetSoftwareUpdateInfo
-@ stub HlinkGoBack
-@ stub HlinkGoForward
-@ stub HlinkNavigateMoniker
-@ stub HlinkNavigateString
-@ stub HlinkSimpleNavigateToMoniker
-@ stub HlinkSimpleNavigateToString
-@ stub IsAsyncMoniker
-@ stub IsLoggingEnabledA
-@ stub IsLoggingEnabledW
-@ stub IsValidURL
-@ stub MkParseDisplayNameEx
+@ stdcall GetSoftwareUpdateInfo(wstr ptr) GetSoftwareUpdateInfo
+@ stdcall HlinkGoBack(ptr) HlinkGoBack
+@ stdcall HlinkGoForward(ptr) HlinkGoForward
+@ stdcall HlinkNavigateMoniker(ptr ptr) HlinkNavigateMoniker
+@ stdcall HlinkNavigateString(ptr wstr) HlinkNavigateString
+@ stdcall HlinkSimpleNavigateToMoniker(ptr wstr wstr ptr ptr ptr long long) HlinkSimpleNavigateToMoniker
+@ stdcall HlinkSimpleNavigateToString(wstr wstr wstr ptr ptr ptr long long) HlinkSimpleNavigateToString
+@ stdcall IsAsyncMoniker(ptr) IsAsyncMoniker
+@ stdcall IsLoggingEnabledA(str) IsLoggingEnabledA
+@ stdcall IsLoggingEnabledW(wstr) IsLoggingEnabledW
+@ stdcall IsValidURL(ptr wstr long) IsValidURL
+@ stdcall MkParseDisplayNameEx(ptr wstr ptr ptr) MkParseDisplayNameEx
 @ stdcall ObtainUserAgentString(long str ptr) ObtainUserAgentString
 @ stub PrivateCoInstall
 @ stdcall RegisterBindStatusCallback(ptr ptr ptr long) RegisterBindStatusCallback
-@ stub RegisterFormatEnumerator
-@ stub RegisterMediaTypeClass
-@ stub RegisterMediaTypes
-@ stub ReleaseBindInfo
+@ stdcall RegisterFormatEnumerator(ptr ptr long) RegisterFormatEnumerator
+@ stdcall RegisterMediaTypeClass(ptr long ptr ptr long) RegisterMediaTypeClass
+@ stdcall RegisterMediaTypes(long ptr ptr) RegisterMediaTypes
+@ stdcall ReleaseBindInfo(ptr) ReleaseBindInfo
 @ stdcall RevokeBindStatusCallback(ptr ptr) RevokeBindStatusCallback
-@ stub RevokeFormatEnumerator
-@ stub SetSoftwareUpdateAdvertisementState
+@ stdcall RevokeFormatEnumerator(ptr ptr) RevokeFormatEnumerator
+@ stdcall SetSoftwareUpdateAdvertisementState(wstr long long long) SetSoftwareUpdateAdvertisementState
 @ stub URLDownloadA
-@ stub URLDownloadToCacheFileA
-@ stub URLDownloadToCacheFileW
-@ stub URLDownloadToFileA
-@ stub URLDownloadToFileW
+@ stdcall URLDownloadToCacheFileA(ptr str ptr long long ptr) URLDownloadToCacheFileA
+@ stdcall URLDownloadToCacheFileW(ptr wstr ptr long long ptr) URLDownloadToCacheFileW
+@ stdcall URLDownloadToFileA(ptr str str long ptr) URLDownloadToFileA
+@ stdcall URLDownloadToFileW(ptr wstr wstr long ptr) URLDownloadToFileW
 @ stub URLDownloadW
-@ stub URLOpenBlockingStreamA
-@ stub URLOpenBlockingStreamW
-@ stub URLOpenPullStreamA
-@ stub URLOpenPullStreamW
-@ stub URLOpenStreamA
-@ stub URLOpenStreamW
+@ stdcall URLOpenBlockingStreamA(ptr str ptr long ptr) URLOpenBlockingStreamA
+@ stdcall URLOpenBlockingStreamW(ptr wstr ptr long ptr) URLOpenBlockingStreamW
+@ stdcall URLOpenPullStreamA(ptr str long ptr) URLOpenPullStreamA
+@ stdcall URLOpenPullStreamW(ptr wstr long ptr) URLOpenPullStreamW
+@ stdcall URLOpenStreamA(ptr str long ptr) URLOpenStreamA
+@ stdcall URLOpenStreamW(ptr wstr long ptr) URLOpenStreamW
 @ stub UrlMkBuildVersion
-@ stub UrlMkGetSessionOption
+@ stdcall UrlMkGetSessionOption(long ptr long ptr long) UrlMkGetSessionOption
 @ stdcall UrlMkSetSessionOption(long ptr long long) UrlMkSetSessionOption
-@ stub WriteHitLogging
+@ stdcall WriteHitLogging(ptr) WriteHitLogging
 @ stub ZonesReInit
 
--- dlls/urlmon/urlmon_main.c
+++ dlls/urlmon/urlmon_main.c
@@ -21,10 +21,22 @@
 #include "windef.h"
 #include "winerror.h"
 #include "wtypes.h"
+#include "ole2.h"
+#include "urlmon.h"
+
+#include "comimpl.h"
 
 #include "wine/debug.h"
 
-WINE_DEFAULT_DEBUG_CHANNEL(win32);
+WINE_DEFAULT_DEBUG_CHANNEL(urlmon);
+
+
+const COMIMPL_CLASSENTRY COMIMPL_ClassList[] =
+{
+  /* list of exported classes */
+  { NULL, NULL }
+};
+
 
 /***********************************************************************
  *		DllInstall (URLMON.@)
@@ -35,28 +47,6 @@ HRESULT WINAPI URLMON_DllInstall(BOOL bInstall, LPCWSTR cmdline)
 	debugstr_w(cmdline));
 
   return S_OK;
-}
-
-/***********************************************************************
- *		DllCanUnloadNow (URLMON.@)
- */
-HRESULT WINAPI URLMON_DllCanUnloadNow(void)
-{
-    FIXME("(void): stub\n");
-
-    return S_FALSE;
-}
-
-/***********************************************************************
- *		DllGetClassObject (URLMON.@)
- */
-HRESULT WINAPI URLMON_DllGetClassObject(REFCLSID rclsid, REFIID riid,
-					LPVOID *ppv)
-{
-    FIXME("(%p, %p, %p): stub\n", debugstr_guid(rclsid), 
-	  debugstr_guid(riid), ppv);
-
-    return CLASS_E_CLASSNOTAVAILABLE;
 }
 
 /***********************************************************************
@@ -118,7 +108,7 @@ HRESULT WINAPI CoInternetGetSession(DWORD dwSessionMode,
       ERR("dwReserved: %ld, must be zero\n", dwReserved);
     }
 
-    return S_OK;
+    return E_NOTIMPL;
 }
 
 
@@ -133,5 +123,260 @@ HRESULT WINAPI ObtainUserAgentString(DWORD dwOption, LPCSTR pcszUAOut, DWORD *cb
       ERR("dwOption: %ld, must be zero\n", dwOption);
     }
 
-    return S_OK;
+    return E_NOTIMPL;
 }
+
+/*****************************************************************************
+ * stubs
+ */
+
+typedef struct
+{
+	DWORD		dwStructSize;
+	LPSTR		lpszLoggedUrlName;
+	SYSTEMTIME	StartTime;
+	SYSTEMTIME	EndTime;
+	LPSTR		lpszExtendedInfo;
+} HIT_LOGGING_INFO;
+
+
+typedef struct
+{
+	ULONG	cbSize;
+	DWORD	dwFlags;
+	DWORD	dwAdState;
+	LPWSTR	szTitle;
+	LPWSTR	szAbstract;
+	LPWSTR	szHREF;
+	DWORD	dwInstalledVersionMS;
+	DWORD	dwInstalledVersionLS;
+	DWORD	dwUpdateVersionMS;
+	DWORD	dwUpdateVersionLS;
+	DWORD	dwAdvertisedVersionMS;
+	DWORD	dwAdvertisedVersionLS;
+	DWORD	dwReserved;
+} SOFTDISTINFO;
+
+
+HRESULT WINAPI CreateFormatEnumerator(UINT cFormatEtcs,FORMATETC* pFormatEtcs,IEnumFORMATETC** ppenum)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI FindMediaType(LPCSTR pszTypes,CLIPFORMAT* pcfTypes)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI FindMediaTypeClass(IBindCtx* pbc,LPCSTR pszType,CLSID* pclsid,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI FindMimeFromData(IBindCtx* pbc,LPCWSTR pwszURL,void* pbuf,DWORD cb,LPCWSTR pwszMimeProposed,DWORD dwMimeFlags,LPWSTR* ppwszMimeOut,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI GetClassFileOrMime(IBindCtx* pbc,LPCWSTR pwszFilename,void* pbuf,DWORD cb,LPCWSTR pwszMime,DWORD dwReserved,CLSID* pclsid)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI GetClassURL(LPCWSTR pwszURL,CLSID* pclsid)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI GetSoftwareUpdateInfo(LPCWSTR pwszDistUnit,SOFTDISTINFO* psdi)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI HlinkGoBack(IUnknown* punk)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI HlinkGoForward(IUnknown* punk)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI HlinkNavigateMoniker(IUnknown* punk,IMoniker* pmonTarget)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI HlinkNavigateString(IUnknown* punk,LPCWSTR szTarget)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI HlinkSimpleNavigateToMoniker(
+	IMoniker* pmonTarget,LPCWSTR pwszLocation,LPCWSTR pwszTargetFrame,
+	IUnknown* punk,IBindCtx* pbc,IBindStatusCallback* pbscb,
+	DWORD dwHLNF,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI HlinkSimpleNavigateToString(
+	LPCWSTR pwszTarget,LPCWSTR pwszLocation,LPCWSTR pwszTargetFrame,
+	IUnknown* punk,IBindCtx* pbc,IBindStatusCallback* pbscb,
+	DWORD dwHLNF,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI IsAsyncMoniker(IMoniker* pmon)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+BOOL WINAPI IsLoggingEnabledA(LPCSTR pszURL)
+{
+	FIXME("() stub\n");
+	return FALSE;
+}
+
+BOOL WINAPI IsLoggingEnabledW(LPCWSTR pwszURL)
+{
+	FIXME("() stub\n");
+	return FALSE;
+}
+
+HRESULT WINAPI IsValidURL(IBindCtx* pbc,LPCWSTR pwszURL,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI MkParseDisplayNameEx(IBindCtx* pbc,LPCWSTR pwszDisplayName,ULONG* pulCharEaten,IMoniker** ppmon)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI RegisterFormatEnumerator(IBindCtx* pbc,IEnumFORMATETC* penum,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI RegisterMediaTypeClass(IBindCtx* pbc,UINT cTypes,const LPCSTR* pszTypes,CLSID* pclsid,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI RegisterMediaTypes(UINT cTypes,const LPCSTR* pszTypes,CLIPFORMAT* pcfTypes)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+void WINAPI ReleaseBindInfo(BINDINFO* pbi)
+{
+	FIXME("() stub\n");
+}
+
+HRESULT WINAPI RevokeFormatEnumerator(IBindCtx* pbc,IEnumFORMATETC* penum)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI SetSoftwareUpdateAdvertisementState(LPCWSTR pwszDistUnit,DWORD dwAdvState,DWORD dwAdvVerMS,DWORD dwAdvVerLS)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLDownloadToCacheFileA(IUnknown* punk,LPCSTR pszURL,LPSTR pszNameBuf,DWORD dwNameBufLen,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLDownloadToCacheFileW(IUnknown* punk,LPCWSTR pwszURL,LPWSTR pwszNameBuf,DWORD dwNameBufLen,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLDownloadToFileA(IUnknown* punk,LPCSTR pszURL,LPCSTR pszFileName,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLDownloadToFileW(IUnknown* punk,LPCWSTR pwszURL,LPCWSTR pwszFileName,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLOpenBlockingStreamA(IUnknown* punk,LPCSTR pszURL,IStream** ppstream,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLOpenBlockingStreamW(IUnknown* punk,LPCWSTR pwszURL,IStream** ppstream,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLOpenPullStreamA(IUnknown* punk,LPCSTR pszURL,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLOpenPullStreamW(IUnknown* punk,LPCWSTR pwszURL,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLOpenStreamA(IUnknown* punk,LPCSTR pszURL,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI URLOpenStreamW(IUnknown* punk,LPCWSTR pwszURL,DWORD dwReserved,IBindStatusCallback* pbscb)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+HRESULT WINAPI UrlMkGetSessionOption(DWORD dwOpt,void* pvBuf,DWORD dwBufLen,DWORD* pdwLen,DWORD dwReserved)
+{
+	FIXME("() stub\n");
+	return E_NOTIMPL;
+}
+
+BOOL WINAPI WriteHitLogging(HIT_LOGGING_INFO* pli)
+{
+	FIXME("() stub\n");
+	return FALSE;
+}
+
+
