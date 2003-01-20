61e9670d31f61d3fdbb334491926acf10b9d36f5
Gerald Pfeifer <pfeifer@dbai.tuwien.ac.at>
2003-01-20 23:27:27 +0000

    #include <stdlib.h> instead of the non-standard <malloc.h>.

--- dlls/dsound/tests/dsound.c
+++ dlls/dsound/tests/dsound.c
@@ -18,8 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <malloc.h>
 #include <math.h>
+#include <stdlib.h>
 
 #include "wine/test.h"
 #include "dsound.h"
