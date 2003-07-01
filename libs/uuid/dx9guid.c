e460cfee52139e5e1e7c58d2f67f483357660427
Raphael Junqueira <fenix@club-internet.fr>
2003-07-01 00:56:33 +0000

    Added d3d9 guids.

---
+++ libs/uuid/dx9guid.c
@@ -0,0 +1,33 @@
+/*
+ * DirectX 9 GUID definitions
+ *
+ * Copyright 2000 Alexandre Julliard
+ * Copyright 2000 Francois Gouget
+ * Copyright 2003 Raphael Junqueira
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define COM_NO_WINDOWS_H
+#include "windef.h"
+#include "winbase.h"
+#include "wingdi.h"
+#include "winuser.h"
+
+#include "objbase.h"
+#include "olectl.h"
+#include "initguid.h"
+
+#include "d3d9.h"
