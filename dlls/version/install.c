b99ab2dd18724c409d8c5dae6b5f4dcf27184e84
Dimitrie O. Paun <dpaun@rogers.com>
2003-09-27 02:22:21 +0000

    Merge documentation/status/version into the version code.

--- dlls/version/install.c
+++ dlls/version/install.c
@@ -17,6 +17,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * TODO
+ *   o Check the installation functions.
  */
 
 #include <stdlib.h>
@@ -575,4 +578,3 @@ DWORD WINAPI VerInstallFileW(
     HeapFree( GetProcessHeap(), 0, wcurd );
     return ret;
 }
-
