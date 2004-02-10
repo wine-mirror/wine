bdb63eca780dfa19b24412bcbfcf424eb7c3c89c
Huw Davies <huw@codeweavers.com>
2004-02-10 20:08:24 +0000

    Add the F_NOTIFY defines if we're compiling with linux and they're not in the system header files.

--- server/change.c
+++ server/change.c
@@ -35,6 +35,19 @@
 #include "thread.h"
 #include "request.h"
 
+#ifdef linux
+#ifndef F_NOTIFY
+#define F_NOTIFY 1026
+#define DN_ACCESS       0x00000001      /* File accessed */
+#define DN_MODIFY       0x00000002      /* File modified */
+#define DN_CREATE       0x00000004      /* File created */
+#define DN_DELETE       0x00000008      /* File removed */
+#define DN_RENAME       0x00000010      /* File renamed */
+#define DN_ATTRIB       0x00000020      /* File changed attibutes */
+#define DN_MULTISHOT    0x80000000      /* Don't remove notifier */
+#endif
+#endif
+
 struct change
 {
     struct object  obj;      /* object header */
