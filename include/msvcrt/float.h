518acec60c2abb2b3a5d9151ff97d04ea6974493
Felix Nawothnig <felix.nawothnig@t-online.de>
2004-03-25 23:36:14 +0000

    Define (L)DBL_* and FLT_*.

--- include/msvcrt/float.h
+++ include/msvcrt/float.h
@@ -24,6 +24,51 @@
 extern "C" {
 #endif
 
+#define DBL_DIG        15
+#define DBL_EPSILON    2.2204460492503131e-016
+#define DBL_MANT_DIG   53
+#define DBL_MAX        1.7976931348623158e+308
+#define DBL_MAX_10_EXP 308
+#define DBL_MAX_EXP    1024
+#define DBL_MIN        2.2250738585072014e-308
+#define DBL_MIN_10_EXP (-307)
+#define DBL_MIN_EXP    (-1021)
+
+#define _DBL_RADIX  2
+#define _DBL_ROUNDS 1
+
+#define DBL_RADIX  _DBL_RADIX
+#define DBL_ROUNDS _DBL_ROUNDS
+
+#define FLT_DIG        6
+#define FLT_EPSILON    1.192092896e-07F
+#define FLT_MANT_DIG   24
+#define FLT_MAX        3.402823466e+38F
+#define FLT_MAX_10_EXP 38
+#define FLT_MAX_EXP    128
+#define FLT_MIN        1.175494351e-38F
+#define FLT_MIN_10_EXP (-37)
+#define FLT_MIN_EXP    (-125)
+
+#define FLT_RADIX  2
+#define FLT_ROUNDS 1
+
+#define LDBL_DIG        DBL_DIG
+#define LDBL_EPSILON    DBL_EPSILON
+#define LDBL_MANT_DIG   DBL_MANT_DIG
+#define LDBL_MAX        DBL_MAX
+#define LDBL_MAX_10_EXP DBL_MAX_10_EXP
+#define LDBL_MAX_EXP    DBL_MAX_EXP
+#define LDBL_MIN        DBL_MIN
+#define LDBL_MIN_10_EXP DBL_MIN_10_EXP
+#define LDBL_MIN_EXP    DBL_MIN_EXP
+
+#define _LDBL_RADIX  _DBL_RADIX
+#define _LDBL_ROUNDS _DBL_ROUNDS
+
+#define LDBL_RADIX  _LDBL_RADIX
+#define LDBL_ROUNDS _LDBL_ROUNDS
+
 /* _controlfp masks and bitflags - x86 only so far */
 #ifdef __i386__
 
