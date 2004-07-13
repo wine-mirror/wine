6812cbc82963f118ffeb8aaae7b91c632060bf80
Mike Hearn <mh@codeweavers.com>
2004-07-13 03:37:07 +0000

    Add a 16 bit pair DLL for TWAIN.

---
+++ dlls/twain/twain16_main.c
@@ -0,0 +1,43 @@
+/*
+ * TWAIN 16-bit functions
+ *
+ * Copyright (C) 2004 Mike Hearn for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "twain.h"
+#include "twain_i.h"
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(twain);
+
+/* Main entry point for the TWAIN library */
+TW_UINT16 WINAPI
+DSM_Entry16 (pTW_IDENTITY pOrigin,
+           pTW_IDENTITY pDest,
+           TW_UINT32    DG,
+           TW_UINT16    DAT,
+           TW_UINT16    MSG,
+           TW_MEMREF    pData)
+{
+    FIXME("stub\n");
+
+    return TWRC_FAILURE;
+}
