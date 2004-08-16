b05354d347df27886dab7b3086d963ed4a7b1462
Robert Reif <reif@earthlink.net>
2004-08-16 21:09:27 +0000

    Adds enough of ks.h, ksguid.h and ksmedia.h to start adding support for WAVEFORMATEXTENSIBLE.

---
+++ include/ksmedia.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2004 Robert Reif
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef _KS_
+#error must include ks.h before ksmedia.h
+#endif
+
+#ifndef _KSMEDIA_
+#define _KSMEDIA_
+
+DEFINE_GUID(KSDATAFORMAT_SUBTYPE_PCM, 0x00000001, 0x0000, 0x0010, 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71);
+
+#endif  /* _KSMEDIA_ */
