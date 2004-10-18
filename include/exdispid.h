f05c860d23303a614216165f52ace4beb7c4c1f2
Jacek Caban <jack@itma.pwr.wroc.pl>
2004-10-18 21:43:18 +0000

    - Added dispinterfaces: DWebBrowserEvents and DWebBrowserEvents2. - Added coclass: WebBrowser. - Added exdispid.h.

---
+++ include/exdispid.h
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2004 Jacek Caban
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef EXDISPID_H_
+#define EXDISPID_H_
+
+#define DISPID_BEFORENAVIGATE        100
+#define DISPID_NAVIGATECOMPLETE      101
+#define DISPID_STATUSTEXTCHANGE      102
+#define DISPID_QUIT                  103
+#define DISPID_DOWNLOADCOMPLETE      104
+#define DISPID_COMMANDSTATECHANGE    105
+#define DISPID_DOWNLOADBEGIN         106
+#define DISPID_NEWWINDOW             107
+#define DISPID_PROGRESSCHANGE        108
+#define DISPID_WINDOWMOVE            109
+#define DISPID_WINDOWRESIZE          110
+#define DISPID_WINDOWACTIVATE        111
+#define DISPID_PROPERTYCHANGE        112
+#define DISPID_TITLECHANGE           113
+#define DISPID_FRAMEBEFORENAVIGATE   200
+#define DISPID_FRAMENAVIGATECOMPLETE 201
+#define DISPID_FRAMENEWWINDOW        204
+#define DISPID_BEFORENAVIGATE2       250
+#define DISPID_NEWWINDOW2            251
+#define DISPID_NAVIGATECOMPLETE2     252
+#define DISPID_ONQUIT                253
+#define DISPID_ONVISIBLE             254
+#define DISPID_ONTOOLBAR             255
+#define DISPID_ONMENUBAR             256
+#define DISPID_ONSTATUSBAR           257
+#define DISPID_ONFULLSCREEN          258
+#define DISPID_DOCUMENTCOMPLETE      259
+#define DISPID_ONTHEATERMODE         260
+#define DISPID_ONADDRESSBAR          261
+
+#define DISPID_SELECTIONCHANGED 200
+
+#define DISPID_WINDOWREGISTERED 200
+#define DISPID_WINDOWREVOKED    201
+
+#define DISPID_RESETFIRSTBOOTMODE    1
+#define DISPID_RESETSAFEMODE         2
+#define DISPID_REFRESHOFFLINEDESKTOP 3
+#define DISPID_ADDFAVORITE           4
+#define DISPID_ADDCHANNEL            5
+#define DISPID_ADDDESKTOPCOMPONENT   6
+#define DISPID_ISSUBSCRIBED          7
+#define DISPID_SHELLUIHELPERLAST     7
+
+#endif /* EXDISPID_H_ */
