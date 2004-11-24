04e4bbc43baf312ab3288bb6f496e6256f238f8e
Alexandre Julliard <julliard@winehq.org>
2004-11-24 18:32:35 +0000

    Prevent use of MAKEPOINTS in Wine code.

--- include/wingdi.h
+++ include/wingdi.h
@@ -1756,7 +1756,11 @@ typedef struct
 } BITMAPFILEHEADER, *PBITMAPFILEHEADER, *LPBITMAPFILEHEADER;
 #include <poppack.h>
 
+#ifdef __WINESRC__
+#define MAKEPOINTS(l)  do_not_use_MAKEPOINTS_in_wine
+#else
 #define MAKEPOINTS(l)  (*((POINTS *)&(l)))
+#endif
 
 typedef struct
 {
