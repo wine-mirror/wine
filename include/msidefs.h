a477def6e16dc536c2bf1e38b4f3d9216424f81e
Mike McCormack <mike@codeweavers.com>
2005-02-08 12:56:00 +0000

    Add the msidbCustomAction and msidbFeatureAttributes enumerations.

--- include/msidefs.h
+++ include/msidefs.h
@@ -46,6 +46,49 @@ enum msidbTextStyleStyleBits
     msidbTextStyleStyleBitsStrike = 0x00000008,
 };
 
+enum msidbCustomActionType
+{
+    msidbCustomActionTypeDll = 0x00000001,
+    msidbCustomActionTypeExe = 0x00000002,
+    msidbCustomActionTypeTextData = 0x00000003,
+    msidbCustomActionTypeJScript = 0x00000005,
+    msidbCustomActionTypeVBScript = 0x00000006,
+    msidbCustomActionTypeInstall = 0x00000007,
+
+    msidbCustomActionTypeBinaryData = 0x00000000,
+    msidbCustomActionTypeSourceFile = 0x00000010,
+    msidbCustomActionTypeDirectory = 0x00000020,
+    msidbCustomActionTypeProperty = 0x00000030,
+
+    msidbCustomActionTypeContinue = 0x00000040,
+    msidbCustomActionTypeAsync = 0x00000080,
+
+    msidbCustomActionTypeFirstSequence = 0x00000100,
+    msidbCustomActionTypeOncePerProcess = 0x00000200,
+    msidbCustomActionTypeClientRepeat = 0x00000300,
+    msidbCustomActionTypeInScript = 0x00000400,
+
+    msidbCustomActionTypeRollback = 0x00000100,
+    msidbCustomActionTypeCommit = 0x00000200,
+
+    msidbCustomActionTypeNoImpersonate = 0x00000800,
+    msidbCustomActionTypeTSAware = 0x00004000,
+
+    msidbCustomActionType64BitScript = 0x00001000,
+    msidbCustomActionTypeHideTarget = 0x00002000
+};
+
+enum msidbFeatureAttributes
+{
+    msidbFeatureAttributesFavorLocal = 0x00000000,
+    msidbFeatureAttributesFavorSource = 0x00000001,
+    msidbFeatureAttributesFollowParent = 0x00000002,
+    msidbFeatureAttributesFavorAdvertise = 0x00000004,
+    msidbFeatureAttributesDisallowAdvertise = 0x00000008,
+    msidbFeatureAttributesUIDisallowAbsent = 0x00000010,
+    msidbFeatureAttributesNoUnsupportedAdvertise = 0x00000020
+};
+
 #ifdef __cplusplus
 }
 #endif
