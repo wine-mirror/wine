21a0c3b4fcf3f3dd0a8dcdb6cba3150c3ff69b1b
Jakob Eriksson <jakov@vmlinux.org>
2005-03-15 19:32:38 +0000

    CryptAcquireContextA() can return NTE_FAIL on Windows 98.

--- dlls/advapi32/tests/crypt.c
+++ dlls/advapi32/tests/crypt.c
@@ -138,7 +138,7 @@ static void test_acquire_context(void)
 	hProv = 0;
 	SetLastError(0xdeadbeef);
 	result = pCryptAcquireContextA(&hProv, szKeySet, szRsaBaseProv, PROV_RSA_FULL, 0);
-	ok(result && (GetLastError() == ERROR_ENVVAR_NOT_FOUND || GetLastError() == ERROR_SUCCESS  || GetLastError() == ERROR_RING2_STACK_IN_USE), "%d/%ld\n", result, GetLastError());
+	ok(result && (GetLastError() == ERROR_ENVVAR_NOT_FOUND || GetLastError() == ERROR_SUCCESS || GetLastError() == ERROR_RING2_STACK_IN_USE || GetLastError() == NTE_FAIL), "%d/%ld\n", result, GetLastError());
 
 	if (hProv) 
 		pCryptReleaseContext(hProv, 0);
@@ -147,7 +147,7 @@ static void test_acquire_context(void)
 	hProv = 0;
 	SetLastError(0xdeadbeef);
 	result = pCryptAcquireContextA(&hProv, szKeySet, "", PROV_RSA_FULL, 0);
-	ok(result && (GetLastError() == ERROR_ENVVAR_NOT_FOUND || GetLastError() == ERROR_SUCCESS  || GetLastError() == ERROR_RING2_STACK_IN_USE), "%d/%ld\n", result, GetLastError());
+	ok(result && (GetLastError() == ERROR_ENVVAR_NOT_FOUND || GetLastError() == ERROR_SUCCESS  || GetLastError() == ERROR_RING2_STACK_IN_USE || GetLastError() == NTE_FAIL), "%d/%ld\n", result, GetLastError());
 
 	if (hProv) 
 		pCryptReleaseContext(hProv, 0);
