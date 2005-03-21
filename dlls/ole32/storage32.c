963b63ecbd130a1db0c6d0462b770dcb601f1c50
Troy Rollo <wine@troy.rollo.name>
2005-03-21 10:30:01 +0000

    ix test for STGM_SHARE_EXCLUSIVE on nameless DocFiles.

--- dlls/ole32/storage32.c
+++ dlls/ole32/storage32.c
@@ -5486,7 +5486,7 @@ HRESULT WINAPI StgCreateDocfile(
     WCHAR tempPath[MAX_PATH];
     static const WCHAR prefix[] = { 'S', 'T', 'O', 0 };
 
-    if (STGM_SHARE_MODE(grfMode) == STGM_SHARE_EXCLUSIVE)
+    if (STGM_SHARE_MODE(grfMode) != STGM_SHARE_EXCLUSIVE)
       goto end;
 
     memset(tempPath, 0, sizeof(tempPath));
