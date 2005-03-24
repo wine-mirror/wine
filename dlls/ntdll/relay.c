3ef45c7805b6d56e0621c7406fa6d96d38444796
Rein Klazes <wijn@wanadoo.nl>
2005-03-24 15:08:37 +0000

    Support relay tracing of functions with 18 arguments.

--- dlls/ntdll/relay.c
+++ dlls/ntdll/relay.c
@@ -489,6 +489,13 @@ static LONGLONG call_cdecl_function( LONGLONG_CPROC func, int nb_args, const int
     case 16: ret = func(args[0],args[1],args[2],args[3],args[4],args[5],
                         args[6],args[7],args[8],args[9],args[10],args[11],
                         args[12],args[13],args[14],args[15]); break;
+    case 17: ret = func(args[0],args[1],args[2],args[3],args[4],args[5],
+                        args[6],args[7],args[8],args[9],args[10],args[11],
+                        args[12],args[13],args[14],args[15],args[16]); break;
+    case 18: ret = func(args[0],args[1],args[2],args[3],args[4],args[5],
+                        args[6],args[7],args[8],args[9],args[10],args[11],
+                        args[12],args[13],args[14],args[15],args[16],
+                        args[17]); break;
     default:
         ERR( "Unsupported nb of args %d\n", nb_args );
         assert(FALSE);
@@ -540,6 +547,13 @@ static LONGLONG call_stdcall_function( LONGLONG_FARPROC func, int nb_args, const
     case 16: ret = func(args[0],args[1],args[2],args[3],args[4],args[5],
                         args[6],args[7],args[8],args[9],args[10],args[11],
                         args[12],args[13],args[14],args[15]); break;
+    case 17: ret = func(args[0],args[1],args[2],args[3],args[4],args[5],
+                        args[6],args[7],args[8],args[9],args[10],args[11],
+                        args[12],args[13],args[14],args[15],args[16]); break;
+    case 18: ret = func(args[0],args[1],args[2],args[3],args[4],args[5],
+                        args[6],args[7],args[8],args[9],args[10],args[11],
+                        args[12],args[13],args[14],args[15],args[16],
+                        args[17]); break;
     default:
         ERR( "Unsupported nb of args %d\n", nb_args );
         assert(FALSE);
