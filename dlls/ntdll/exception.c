5037202554a47357adea0fcab7ca4ef8944b65be
Mike Hearn <mike@navi.cx>
2005-05-13 13:56:47 +0000

    Make unimplemented function message a bit prettier.

--- dlls/ntdll/exception.c
+++ dlls/ntdll/exception.c
@@ -235,11 +235,11 @@ void WINAPI __regs_RtlRaiseException( EXCEPTION_RECORD *rec, CONTEXT *context )
     if (rec->ExceptionCode == EXCEPTION_WINE_STUB)
     {
         if (HIWORD(rec->ExceptionInformation[1]))
-            FIXME( "call (from %p) to unimplemented function %s.%s\n",
+            MESSAGE( "wine: Call from %p to unimplemented function %s.%s, aborting\n",
                    rec->ExceptionAddress,
                    (char*)rec->ExceptionInformation[0], (char*)rec->ExceptionInformation[1] );
         else
-            FIXME( "call (from %p) to unimplemented function %s.%ld\n",
+            MESSAGE( "wine: Call from %p to unimplemented function %s.%ld, aborting\n",
                    rec->ExceptionAddress,
                    (char*)rec->ExceptionInformation[0], rec->ExceptionInformation[1] );
     }
