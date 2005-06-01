8ec66822784b4964b89b03769eb530b0d1462708
Mike McCormack <mike@codeweavers.com>
2005-06-01 19:49:29 +0000

    Define some functions and structures.

--- include/wintrust.h
+++ include/wintrust.h
@@ -100,6 +100,26 @@ typedef struct _WINTRUST_DATA
     DWORD  dwProvFlags;
 } WINTRUST_DATA, *PWINTRUST_DATA;
 
+typedef struct _CRYPT_TRUST_REG_ENTRY
+{
+    DWORD cbStruct;
+    WCHAR *pwszDLLName;
+    WCHAR *pwszFunctionName;
+} CRYPT_TRUST_REG_ENTRY, *PCRYPT_TRUST_REG_ENTRY;
+
+typedef struct _CRYPT_REGISTER_ACTIONID
+{
+    DWORD cbStruct;
+    CRYPT_TRUST_REG_ENTRY sInitProvider;
+    CRYPT_TRUST_REG_ENTRY sObjectProvider;
+    CRYPT_TRUST_REG_ENTRY sSignatureProvider;
+    CRYPT_TRUST_REG_ENTRY sCertificateProvider;
+    CRYPT_TRUST_REG_ENTRY sCertificatePolicyProvider;
+    CRYPT_TRUST_REG_ENTRY sFinalPolicyProvider;
+    CRYPT_TRUST_REG_ENTRY sTestPolicyProvider;
+    CRYPT_TRUST_REG_ENTRY sCleanupProvider;
+} CRYPT_REGISTER_ACTIONID, *PCRYPT_REGISTER_ACTIONID;
+
 #include <poppack.h>
 
 
@@ -107,6 +127,8 @@ typedef struct _WINTRUST_DATA
 extern "C" {
 #endif
 
+BOOL      WINAPI WintrustAddActionID(GUID*,DWORD,CRYPT_REGISTER_ACTIONID*);
+void      WINAPI WintrustGetRegPolicyFlags(DWORD*);
 LONG      WINAPI WinVerifyTrust(HWND,GUID*,WINTRUST_DATA*);
 
 #ifdef __cplusplus
