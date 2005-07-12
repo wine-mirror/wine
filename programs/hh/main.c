8831329dae3c55fd39ac890a0da26e7e8e9b0880
Jacek Caban <jack@itma.pwr.wroc.pl>
2005-07-12 20:41:52 +0000

    Added hh.exe.

---
+++ programs/hh/main.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2004 Jacek Caban
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <windows.h>
+
+typedef int WINAPI DOWINMAIN(HMODULE hMod, LPSTR cmdline);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR cmdline, int cmdshow)
+{
+
+    HMODULE hModule;
+    DOWINMAIN *doWinMain;
+
+    hModule = LoadLibrary("hhctrl.ocx");
+    doWinMain = GetProcAddress(hModule, "doWinMain");
+
+    return doWinMain(hInst, cmdline); 
+}
