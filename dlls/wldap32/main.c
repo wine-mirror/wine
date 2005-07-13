8d63dfb73ef36ba0b86dd76613766cc4ba08a976
Hans Leidekker <hans@it.vu.nl>
2005-07-13 11:56:15 +0000

    Beginnings of WLDAP32.

---
+++ dlls/wldap32/main.c
@@ -0,0 +1,44 @@
+/*
+ * WLDAP32 - LDAP support for Wine
+ *
+ * Copyright 2005 Hans Leidekker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "config.h"
+
+#include "wine/debug.h"
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(wldap32);
+
+BOOL WINAPI DllMain( HINSTANCE hinst, DWORD reason, LPVOID reserved )
+{
+    TRACE( "(%p, 0x%08lx, %p)\n", hinst, reason, reserved );
+
+    switch (reason)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls( hinst );
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
---
+++ dlls/wldap32/wldap32.spec
@@ -0,0 +1,5 @@
+@ stub LdapGetLastError
+@ stub LdapMapErrorToWin32
+@ stub ldap_open
+@ stub ldap_openA
+@ stub ldap_openW
