98ac2c1defc83cb9a63e105fb66ec5a27c8cefa8
Hans Leidekker <hans@it.vu.nl>
2005-07-18 18:06:42 +0000

    Implement ber_* functions.

---
+++ dlls/wldap32/ber.c
@@ -0,0 +1,92 @@
+/*
+ * WLDAP32 - LDAP support for Wine
+ *
+ * Copyright 2005 Hans Leidekker
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "config.h"
+
+#ifndef HAVE_LDAP
+
+#include "windef.h"
+#include "winldap.h"
+
+#define LBER_ERROR (~0UL)
+
+BerElement *ber_alloc_t( INT options )
+{
+    return NULL;
+}
+
+BERVAL *ber_bvdup( BERVAL *berval )
+{
+    return NULL;
+}
+
+void ber_bvecfree( PBERVAL *berval )
+{
+}
+
+void ber_bvfree( BERVAL *berval )
+{
+}
+
+ULONG ber_first_element( BerElement *berelement, ULONG *len, CHAR **opaque )
+{
+    return LBER_ERROR;
+}
+
+INT ber_flatten( BerElement *berelement, PBERVAL *berval )
+{
+    return LBER_ERROR;
+}
+
+void ber_free( BerElement *berelement, INT buf )
+{
+}
+
+BerElement *ber_init( BERVAL *berval )
+{
+    return NULL;
+}
+
+ULONG ber_next_element( BerElement *berelement, ULONG *len, CHAR *opaque )
+{
+    return LBER_ERROR;
+}
+
+ULONG ber_peek_tag( BerElement *berelement, ULONG *len )
+{
+    return LBER_ERROR;
+}
+
+INT ber_printf( BerElement *berelement, PCHAR fmt, ... )
+{
+    return LBER_ERROR;
+}
+
+ULONG ber_skip_tag( BerElement *berelement, ULONG *len )
+{
+    return LBER_ERROR;
+}
+
+INT ber_scanf( BerElement *berelement, PCHAR fmt, ... )
+{
+    return LBER_ERROR;
+}
+
+#endif /* HAVE_LDAP */
--- dlls/wldap32/wldap32.spec
+++ dlls/wldap32/wldap32.spec
@@ -1,5 +1,18 @@
 @ stub LdapGetLastError
 @ stub LdapMapErrorToWin32
+@ cdecl ber_alloc_t(long)
+@ cdecl ber_bvdup(ptr)
+@ cdecl ber_bvecfree(ptr)
+@ cdecl ber_bvfree(ptr)
+@ cdecl ber_first_element(ptr ptr ptr)
+@ cdecl ber_flatten(ptr ptr)
+@ cdecl ber_free(ptr long)
+@ cdecl ber_init(ptr)
+@ cdecl ber_next_element(ptr ptr ptr)
+@ cdecl ber_peek_tag(ptr ptr)
+@ varargs ber_printf(ptr str)
+@ varargs ber_scanf(ptr str)
+@ cdecl ber_skip_tag(ptr ptr)
 @ stub ldap_open
 @ stub ldap_openA
 @ stub ldap_openW
