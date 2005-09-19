43e73ebba2d4d9f18eac19d84aae0a2488a98d72
Marcus Meissner <marcus@jet.franken.de>
2005-09-19 14:29:41 +0000

    Support more arguments in _invoke().

--- dlls/oleaut32/typelib.c
+++ dlls/oleaut32/typelib.c
@@ -4503,6 +4503,15 @@ _invoke(FARPROC func,CALLCONV callconv, int nrargs, DWORD *args) {
 	case 11:
 		res = func(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10]);
 		break;
+	case 12:
+		res = func(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10],args[11]);
+		break;
+	case 13:
+		res = func(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10],args[11],args[12]);
+		break;
+	case 14:
+		res = func(args[0],args[1],args[2],args[3],args[4],args[5],args[6],args[7],args[8],args[9],args[10],args[11],args[12],args[13]);
+		break;
 	default:
 		FIXME("unsupported number of arguments %d in stdcall\n",nrargs);
 		res = -1;
