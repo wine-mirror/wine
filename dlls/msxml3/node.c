d1b21b5c5021bf9bdd81dc1342a624953cb5b3fc
Huw Davies <huw@codeweavers.com>
2005-11-08 12:46:43 +0000

    Fix cut-n-paste error in licence text.

--- dlls/msxml3/node.c
+++ dlls/msxml3/node.c
@@ -3,12 +3,12 @@
  *
  * Copyright 2005 Mike McCormack
  *
- * iface library is free software; you can redistribute it and/or
+ * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
- * iface library is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
