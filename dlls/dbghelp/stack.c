df7be14ce2ff962d5c63dae408a14e4f3ba2ce86
Eric Pouech <eric.pouech@wanadoo.fr>
2005-12-19 17:41:32 +0100

    dbghelp: Fixed missing parameters info in StackWalk64.

--- dlls/dbghelp/stack.c
+++ dlls/dbghelp/stack.c
@@ -518,6 +518,11 @@ BOOL WINAPI StackWalk64(DWORD MachineType, HANDLE hProcess, HANDLE hThread,
     addr_32to64(&frame32.AddrStack,  &frame64->AddrStack);
     addr_32to64(&frame32.AddrBStore, &frame64->AddrBStore);
     frame64->FuncTableEntry = frame32.FuncTableEntry; /* FIXME */
+    frame64->Params[0] = (ULONG)frame32.Params[0];
+    frame64->Params[1] = (ULONG)frame32.Params[1];
+    frame64->Params[2] = (ULONG)frame32.Params[2];
+    frame64->Params[3] = (ULONG)frame32.Params[3];
+    frame64->Params[4] = (ULONG)frame32.Params[4];
     frame64->Far = frame32.Far;
     frame64->Virtual = frame32.Virtual;
     frame64->Reserved[0] = (ULONG)frame32.Reserved[0];
