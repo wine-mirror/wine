6501d7b155138f03d6e6247d6ab8e9b24713e5b0
Robert Reif <reif@earthlink.net>
2006-01-03 13:15:55 +0100

    kernel: DelayLoadFailureHook crash fix.

--- dlls/kernel/module.c
+++ dlls/kernel/module.c
@@ -992,7 +992,10 @@ FARPROC WINAPI DelayLoadFailureHook( LPCSTR name, LPCSTR function )
 {
     ULONG_PTR args[2];
 
-    ERR( "failed to delay load %s.%s\n", name, function );
+    if ((ULONG_PTR)function >> 16)
+        ERR( "failed to delay load %s.%s\n", name, function );
+    else
+        ERR( "failed to delay load %s.%u\n", name, LOWORD(function) );
     args[0] = (ULONG_PTR)name;
     args[1] = (ULONG_PTR)function;
     RaiseException( EXCEPTION_WINE_STUB, EH_NONCONTINUABLE, 2, args );
