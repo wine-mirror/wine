6cb7ca4177725158984cfe8d998df1601047697c
Alexandre Julliard <julliard@winehq.org>
2006-02-07 16:08:19 +0100

    setupapi: Store the full name to the INF file in the inf_file structure (based on a patch by James Hawkins).

--- dlls/setupapi/dirid.c
+++ dlls/setupapi/dirid.c
@@ -165,7 +165,7 @@ static const WCHAR *get_csidl_dir( DWORD csidl )
 }
 
 /* retrieve the string corresponding to a dirid, or NULL if none */
-const WCHAR *DIRID_get_string( HINF hinf, int dirid )
+const WCHAR *DIRID_get_string( int dirid )
 {
     int i;
 
@@ -188,7 +188,6 @@ const WCHAR *DIRID_get_string( HINF hinf, int dirid )
     else
     {
         if (dirid > MAX_SYSTEM_DIRID) return get_unknown_dirid();
-        if (dirid == DIRID_SRCPATH) return PARSER_get_src_root( hinf );
         if (!system_dirids[dirid]) system_dirids[dirid] = create_system_dirid( dirid );
         return system_dirids[dirid];
     }
--- dlls/setupapi/setupapi_private.h
+++ dlls/setupapi/setupapi_private.h
@@ -32,12 +32,13 @@
 /* string substitutions */
 
 struct inf_file;
-extern const WCHAR *DIRID_get_string( HINF hinf, int dirid );
+extern const WCHAR *DIRID_get_string( int dirid );
 extern unsigned int PARSER_string_substA( struct inf_file *file, const WCHAR *text,
                                           char *buffer, unsigned int size );
 extern unsigned int PARSER_string_substW( struct inf_file *file, const WCHAR *text,
                                           WCHAR *buffer, unsigned int size );
-extern const WCHAR *PARSER_get_src_root( HINF hinf );
+extern const WCHAR *PARSER_get_inf_filename( HINF hinf );
+extern WCHAR *PARSER_get_src_root( HINF hinf );
 extern WCHAR *PARSER_get_dest_dir( INFCONTEXT *context );
 
 /* support for Ascii queue callback functions */
