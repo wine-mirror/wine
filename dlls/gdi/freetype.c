69a23a608eea59624b2f37ab424e0f42b3da5baf
Dmitry Timoshkov <dmitry@codeweavers.com>
2006-02-23 20:33:06 +0800

    gdi: Use "MS Sans Serif" as default sans serif font, not Arial.

--- dlls/gdi/freetype.c
+++ dlls/gdi/freetype.c
@@ -284,10 +284,10 @@ static struct list font_list = LIST_INIT(font_list);
 
 static const WCHAR defSerif[] = {'T','i','m','e','s',' ','N','e','w',' ',
 			   'R','o','m','a','n','\0'};
-static const WCHAR defSans[] = {'A','r','i','a','l','\0'};
+static const WCHAR defSans[] = {'M','S',' ','S','a','n','s',' ','S','e','r','i','f','\0'};
 static const WCHAR defFixed[] = {'C','o','u','r','i','e','r',' ','N','e','w','\0'};
 
-static const WCHAR defSystem[] = {'A','r','i','a','l','\0'};
+static const WCHAR defSystem[] = {'S','y','s','t','e','m','\0'};
 static const WCHAR SystemW[] = {'S','y','s','t','e','m','\0'};
 static const WCHAR MSSansSerifW[] = {'M','S',' ','S','a','n','s',' ',
 			       'S','e','r','i','f','\0'};
