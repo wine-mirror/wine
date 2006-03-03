c3a125179d98329c35c1027e603fd6c3fd4a8be6
Juan Lang <juan_lang@yahoo.com>
2006-03-03 10:48:52 -0800

    crypt32: Move serialization functions to serialize.c.

--- dlls/crypt32/crypt32_private.h
+++ dlls/crypt32/crypt32_private.h
@@ -19,6 +19,11 @@
 #ifndef __CRYPT32_PRIVATE_H__
 #define __CRYPT32_PRIVATE_H__
 
+/* The following aren't defined in wincrypt.h, as they're "reserved" */
+#define CERT_CERT_PROP_ID 32
+#define CERT_CRL_PROP_ID  33
+#define CERT_CTL_PROP_ID  34
+
 /* Returns a handle to the default crypto provider; loads it if necessary.
  * Returns NULL on failure.
  */
@@ -26,5 +31,14 @@ HCRYPTPROV CRYPT_GetDefaultProvider(void);
 
 void crypt_oid_init(HINSTANCE hinst);
 void crypt_oid_free(void);
+
+/* Helper function for store reading functions and
+ * CertAddSerializedElementToStore.  Returns a context of the appropriate type
+ * if it can, or NULL otherwise.  Doesn't validate any of the properties in
+ * the serialized context (for example, bad hashes are retained.)
+ * *pdwContentType is set to the type of the returned context.
+ */
+const void *CRYPT_ReadSerializedElement(const BYTE *pbElement,
+ DWORD cbElement, DWORD dwContextTypeFlags, DWORD *pdwContentType);
 
 #endif
