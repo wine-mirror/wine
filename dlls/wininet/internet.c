472ad3a450f044aa6c99086b36d0666e28dd9c59
Victor Pelt <victor.pelt@gmail.com>
2006-03-06 14:04:47 +0100

    wininet: Add stub for missing InternetSetOption.

--- dlls/wininet/internet.c
+++ dlls/wininet/internet.c
@@ -2455,6 +2455,9 @@ BOOL WINAPI InternetSetOptionW(HINTERNET hInternet, DWORD dwOption,
     case INTERNET_OPTION_CONNECT_RETRIES:
         FIXME("Option INTERNET_OPTION_CONNECT_RETRIES: STUB\n");
         break;
+    case INTERNET_OPTION_CONTEXT_VALUE:
+	 FIXME("Option INTERNET_OPTION_CONTEXT_VALUE; STUB\n");
+	 break;
     default:
         FIXME("Option %ld STUB\n",dwOption);
         INTERNET_SetLastError(ERROR_INVALID_PARAMETER);
