d30975d57619a7cca712bea4ea4d53cf6bad80a3
Dmitry Timoshkov <dmitry@codeweavers.com>
2006-04-27 20:52:12 +0900

    Add a stub implementation of mprapi.dll.

---
+++ dlls/mprapi/mprapi.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2006 Dmitry Timoshkov
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "mprapi.h"
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(mprapi);
+
+/*****************************************************
+ *      DllMain
+ */
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
+{
+    TRACE("(%p, %ld, %p)\n", hinst, reason, reserved);
+
+    switch(reason)
+    {
+    case DLL_WINE_PREATTACH:
+        return FALSE;  /* prefer native version */
+
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls( hinst );
+        break;
+    }
+    return TRUE;
+}
+
+BOOL APIENTRY MprAdminIsServiceRunning(LPWSTR server)
+{
+    FIXME("(%s): stub!\n", debugstr_w(server));
+
+    return FALSE;
+}
