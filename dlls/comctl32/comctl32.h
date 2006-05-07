ab71fada12b5d80ce40c95e95fc2cb734ba97c88
Saulius Krasuckas <saulius2@ar.fi.lt>
2006-05-08 00:42:31 +0300

    comctl32: Increase FILEVERSION up to 5.81.4704.1100.

--- dlls/comctl32/comctl32.h
+++ dlls/comctl32/comctl32.h
@@ -147,9 +147,9 @@ INT  Str_GetPtrWtoA (LPCWSTR lpSrc, LPSTR lpDest, INT nMaxLen);
 BOOL Str_SetPtrAtoW (LPWSTR *lppDest, LPCSTR lpSrc);
 BOOL Str_SetPtrWtoA (LPSTR *lppDest, LPCWSTR lpSrc);
 
-#define COMCTL32_VERSION_MINOR 80
-#define WINE_FILEVERSION 5, COMCTL32_VERSION_MINOR, 0, 0
-#define WINE_FILEVERSIONSTR "5.80"
+#define COMCTL32_VERSION_MINOR 81
+#define WINE_FILEVERSION 5, COMCTL32_VERSION_MINOR, 4704, 1100
+#define WINE_FILEVERSIONSTR "5.81"
 
 /* Our internal stack structure of the window procedures to subclass */
 typedef struct _SUBCLASSPROCS {
