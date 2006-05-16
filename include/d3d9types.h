f9fc4b86c0be0322d9f2674fc3f3a86ac8d6c65c
Vitaliy Margolen <wine-patch@kievinfo.com>
2006-05-16 07:30:15 -0600

    d3d: Add D3DDEVTYPE_NULLREF definition.

--- include/d3d9types.h
+++ include/d3d9types.h
@@ -740,6 +740,7 @@ typedef enum _D3DDEVTYPE {
     D3DDEVTYPE_HAL         = 1,
     D3DDEVTYPE_REF         = 2,
     D3DDEVTYPE_SW          = 3,
+    D3DDEVTYPE_NULLREF     = 4,
 
     D3DDEVTYPE_FORCE_DWORD = 0xffffffff
 } D3DDEVTYPE;
