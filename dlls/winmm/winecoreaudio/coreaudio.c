ea50196447f65ee927a5599ee3dc35ab373f957d
Alexandre Julliard <julliard@winehq.org>
2006-06-01 13:15:54 +0200

    winecoreaudio: Fixed the FSF address.

--- dlls/winmm/winecoreaudio/coreaudio.c
+++ dlls/winmm/winecoreaudio/coreaudio.c
@@ -15,7 +15,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
 
@@ -111,4 +111,3 @@ LRESULT CALLBACK CoreAudio_DriverProc(DWORD_PTR dwDevID, HDRVR hDriv, UINT wMsg,
 	return DefDriverProc(dwDevID, hDriv, wMsg, dwParam1, dwParam2);
     }
 }
-
