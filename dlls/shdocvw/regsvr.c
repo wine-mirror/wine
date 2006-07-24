591309875ade3c5cc77b98c84c24201316c00773
Mike McCormack <mike@codeweavers.com>
2006-07-24 12:02:20 +0900

    shdocvw: Register MruListLong.

--- dlls/shdocvw/regsvr.c
+++ dlls/shdocvw/regsvr.c
@@ -505,6 +505,8 @@ static LONG recursive_delete_keyW(HKEY base, WCHAR const *name)
 
 static const GUID CLSID_MicrosoftBrowserArchitecture =
 {0xa5e46e3a, 0x8849, 0x11d1, {0x9d, 0x8c, 0x00, 0xc0, 0x4f, 0xc9, 0x9d, 0x61}};
+static const GUID CLSID_MruLongList =
+{0x53bd6b4e, 0x3780, 0x4693, {0xaf, 0xc3, 0x71, 0x61, 0xc2, 0xf3, 0xee, 0x9c}};
 
 /***********************************************************************
  *		coclass list
@@ -585,6 +587,15 @@ static struct regsvr_coclass const coclass_list[] = {
     {
         &CLSID_MicrosoftBrowserArchitecture,
         "Microsoft Browser Architecture",
+        NULL,
+        "shdocvw.dll",
+        "Apartment",
+        NULL,
+        NULL
+    },
+    {
+        &CLSID_MruLongList,
+        "MruLongList",
         NULL,
         "shdocvw.dll",
         "Apartment",
