bb49763154a3f280a04be4a494e22ae68ee6709c
Konstantin Petrov <argentum@office.etersoft.ru>
2006-09-04 20:46:34 +0400

    svrapi: Added a stub svrapi.dll.

--- include/lmcons.h
+++ include/lmcons.h
@@ -29,32 +29,33 @@
 
 /* Lan manager API defines */
 
-#define UNLEN       256                 /* Maximum user name length */
+#define UNLEN       256
 #define LM20_UNLEN  20
-#define GNLEN       UNLEN               /* Maximum group name length */
+#define GNLEN       UNLEN
 #define LM20_GNLEN  LM20_UNLEN
-#define PWLEN       256                 /* Maximum password length */
+#define PWLEN       256
 #define LM20_PWLEN  14
-#define CNLEN       15                  /* Computer name length  */
+#define SHPWLEN     8
+#define CNLEN       15
 #define LM20_CNLEN  CNLEN
-#define DNLEN       CNLEN               /* Maximum domain name length */
+#define DNLEN       CNLEN
 #define LM20_DNLEN  DNLEN
-#define UNCLEN      (CNLEN+2)           /* UNC computer name length */
+#define UNCLEN      (CNLEN+2)
 #define LM20_UNCLEN (LM20_CNLEN+2)
-#define NNLEN       80                  /* max share name length */
-#define LM20_NNLEN  12                  /* max LM 2.0 share name length */
+#define NNLEN       80
+#define LM20_NNLEN  12
 #define RMLEN       (UNCLEN+1+NNLEN)
 #define LM20_RMLEN  (LM20_UNCLEN+1+LM20_NNLEN)
-#define SNLEN       80                  /* max service name length */
+#define SNLEN       80
 #define LM20_SNLEN  15
-#define STXTLEN     256                 /* service text length */
+#define STXTLEN     256
 #define LM20_STXTLEN 63
 #define PATHLEN     256
 #define LM20_PATHLEN 256
-#define DEVLEN      80                  /* max device name length */
+#define DEVLEN      80
 #define LM20_DEVLEN 8
-#define EVLEN       16                  /* max event name length */
-#define CLTYPE_LEN  12                  /* max client type length */
+#define EVLEN       16
+#define CLTYPE_LEN  12
 
 /* platform IDs */
 #define PLATFORM_ID_DOS 300
