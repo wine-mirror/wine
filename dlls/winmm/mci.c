a8f02b8bb7fc96e36c1b00dad2aeed2357629af0
Andrew Talbot <Andrew.Talbot@talbotville.com>
2006-11-22 16:14:45 +0000

    winmm: Cast-qual warnings fix.

--- dlls/winmm/mci.c
+++ dlls/winmm/mci.c
@@ -304,14 +304,14 @@ static int MCI_MapMsgAtoW(UINT msg, DWORD_PTR dwParam1, DWORD_PTR *dwParam2)
             if (dwParam1 & MCI_OPEN_TYPE)
             {
                 if (dwParam1 & MCI_OPEN_TYPE_ID)
-                    mci_openW->lpstrDeviceType = (LPWSTR)mci_openA->lpstrDeviceType;
+                    mci_openW->lpstrDeviceType = (LPCWSTR)mci_openA->lpstrDeviceType;
                 else
                     mci_openW->lpstrDeviceType = MCI_strdupAtoW(mci_openA->lpstrDeviceType);
             }
             if (dwParam1 & MCI_OPEN_ELEMENT)
             {
                 if (dwParam1 & MCI_OPEN_ELEMENT_ID)
-                    mci_openW->lpstrElementName = (LPWSTR)mci_openA->lpstrElementName;
+                    mci_openW->lpstrElementName = (LPCWSTR)mci_openA->lpstrElementName;
                 else
                     mci_openW->lpstrElementName = MCI_strdupAtoW(mci_openA->lpstrElementName);
             }
@@ -980,8 +980,8 @@ static	LPCWSTR		MCI_FindCommand(UINT uTbl, LPCWSTR verb)
  */
 static	DWORD		MCI_GetReturnType(LPCWSTR lpCmd)
 {
-    lpCmd = (LPCWSTR)((BYTE*)(lpCmd + strlenW(lpCmd) + 1) + sizeof(DWORD) + sizeof(WORD));
-    if (*lpCmd == '\0' && *(const WORD*)((BYTE*)(lpCmd + 1) + sizeof(DWORD)) == MCI_RETURN) {
+    lpCmd = (LPCWSTR)((const BYTE*)(lpCmd + strlenW(lpCmd) + 1) + sizeof(DWORD) + sizeof(WORD));
+    if (*lpCmd == '\0' && *(const WORD*)((const BYTE*)(lpCmd + 1) + sizeof(DWORD)) == MCI_RETURN) {
 	return *(const DWORD*)(lpCmd + 1);
     }
     return 0L;
