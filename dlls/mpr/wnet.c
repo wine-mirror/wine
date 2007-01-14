817265a74e46bc51b92e711f3b42ed60d826a01b
Paul Vriens <paul.vriens.wine@gmail.com>
2007-01-14 13:23:45 +0100

    mpr: Fix typo (Coverity).

--- dlls/mpr/wnet.c
+++ dlls/mpr/wnet.c
@@ -819,7 +819,7 @@ DWORD WINAPI WNetEnumResourceA( HANDLE hEnum, LPDWORD lpcCount,
         ret = WN_BAD_POINTER;
     else if (!lpcCount)
         ret = WN_BAD_POINTER;
-    if (!lpBuffer)
+    else if (!lpBuffer)
         ret = WN_BAD_POINTER;
     else if (!lpBufferSize)
         ret = WN_BAD_POINTER;
