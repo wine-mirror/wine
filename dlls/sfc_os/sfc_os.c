161897e187f5f301290b04156d3da1ea89680c71
Detlef Riekenberg <wine.dev@web.de>
2007-01-16 20:35:06 +0100

    sfc_os: Add stub for sfc_os.dll.

---
+++ dlls/sfc_os/sfc_os.c
@@ -0,0 +1,85 @@
+/*
+ * Implementation of the System File Checker (Windows File Protection)
+ *
+ * Copyright 2006 Detlef Riekenberg
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "winerror.h"
+#include "sfc.h"
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(sfc);
+
+/******************************************************************
+ *      DllMain
+ */
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    TRACE("(%p, %d, %p)\n",hinstDLL, fdwReason, lpvReserved);
+
+    switch(fdwReason)
+    {
+        case DLL_WINE_PREATTACH:
+            return FALSE;           /* prefer native version */
+
+        case DLL_PROCESS_ATTACH:
+            DisableThreadLibraryCalls( hinstDLL );
+            break;
+    }
+    return TRUE;
+}
+
+/******************************************************************
+ *              SfcIsFileProtected     [sfc_os.@]
+ *
+ * Check, if the given File is protected by the System
+ *
+ * PARAMS
+ *  RpcHandle    [I] This must be NULL
+ *  ProtFileName [I] Filename with Path to check
+ *
+ * RETURNS
+ *  Failure: FALSE with GetLastError() != ERROR_FILE_NOT_FOUND
+ *  Success: TRUE, when the File is Protected
+ *           FALSE with GetLastError() == ERROR_FILE_NOT_FOUND,
+ *           when the File is not Protected
+ *
+ *
+ * BUGS
+ *  We return always the Result for: "File is not Protected"
+ *
+ */
+BOOL WINAPI SfcIsFileProtected(HANDLE RpcHandle, LPCWSTR ProtFileName)
+{
+    static BOOL reported = FALSE;
+
+    if (reported) {
+        TRACE("(%p, %s) stub\n", RpcHandle, debugstr_w(ProtFileName));
+    }
+    else
+    {
+        FIXME("(%p, %s) stub\n", RpcHandle, debugstr_w(ProtFileName));
+        reported = TRUE;
+    }
+
+    SetLastError(ERROR_FILE_NOT_FOUND);
+    return FALSE;
+}
