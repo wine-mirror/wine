aa9dcb42019e0ebc0913d1a56b150684212099ea
Alexandre Julliard <julliard@winehq.org>
2007-01-22 23:18:45 +0100

    oleaut32/tests: Add tmarshal test.

---
+++ dlls/oleaut32/tests/tmarshal_dispids.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2005-2006 Robert Shearman for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#define DISPID_TM_NAME 1
+#define DISPID_TM_DOSOMETHING 2
+#define DISPID_TM_STATE 3
+#define DISPID_TM_MAP 4
+#define DISPID_TM_SETOLECOLOR 5
+#define DISPID_TM_GETOLECOLOR 6
+#define DISPID_TM_CLONE 7
+#define DISPID_TM_CLONEDISPATCH 8
+#define DISPID_TM_CLONECOCLASS 9
+#define DISPID_TM_ARRAY 10
+#define DISPID_TM_VARARRAYPTR 11
+#define DISPID_TM_VARIANT 12
+#define DISPID_TM_ERROR 13
+
+#define DISPID_NOA_BSTRRET 1
