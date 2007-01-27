1efcf3769203e4d14ffe144939fce61ae5e853b1
Andrew Talbot <Andrew.Talbot@talbotville.com>
2007-01-27 18:11:12 +0000

    setupapi: Declare some functions static.

--- dlls/setupapi/setupx_main.c
+++ dlls/setupapi/setupx_main.c
@@ -280,7 +280,7 @@ static LDD_LIST *pFirstLDD = NULL;
 
 static BOOL std_LDDs_done = FALSE;
 
-void SETUPX_CreateStandardLDDs(void)
+static void SETUPX_CreateStandardLDDs(void)
 {
     HKEY hKey = 0;
     WORD n;
@@ -350,7 +350,7 @@ void SETUPX_CreateStandardLDDs(void)
  * RETURN
  *   ERR_VCP_LDDINVALID if ldid < LDID_ASSIGN_START.
  */
-RETERR16 SETUPX_DelLdd(LOGDISKID16 ldid)
+static RETERR16 SETUPX_DelLdd(LOGDISKID16 ldid)
 {
     LDD_LIST *pCurr, *pPrev = NULL;
 
