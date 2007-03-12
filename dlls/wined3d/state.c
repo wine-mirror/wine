05d461aee99e861388a0f451b9e626fd5fa07495
H. Verbeet <hverbeet@gmail.com>
2007-03-12 23:22:06 +0100

    wined3d: Remove some redundant checks in the vertexdeclaration() state handler.

--- dlls/wined3d/state.c
+++ dlls/wined3d/state.c
@@ -2905,10 +2905,7 @@ static void vertexdeclaration(DWORD state, IWineD3DStateBlockImpl *stateblock, W
 
     handleStreams(stateblock, useVertexShaderFunction, context);
 
-    /* Do I have to use ? TRUE : FALSE ? Or can I rely on 15==15 being equal to TRUE(=1)? */
-    transformed = ((device->strided_streams.u.s.position.lpData != NULL ||
-                    device->strided_streams.u.s.position.VBO != 0) &&
-                    device->strided_streams.u.s.position_transformed) ? TRUE : FALSE;
+    transformed = device->strided_streams.u.s.position_transformed;
     if (transformed) useVertexShaderFunction = FALSE;
 
     if(transformed != context->last_was_rhw && !useVertexShaderFunction) {
