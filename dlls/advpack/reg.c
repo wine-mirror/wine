9b0686e4a4a51a25e743b5f286cef512d525817b
Joris Huizer <joris_huizer@yahoo.com>
2007-03-17 07:08:27 -0700

    advpack: sign-compare fix.

--- dlls/advpack/reg.c
+++ dlls/advpack/reg.c
@@ -221,7 +221,7 @@ static HRESULT write_predefined_strings(HMODULE hm, LPCWSTR ini_path)
  */
 HRESULT WINAPI RegInstallW(HMODULE hm, LPCWSTR pszSection, const STRTABLEW* pstTable)
 {
-    int i;
+    unsigned int i;
     CABINFOW cabinfo;
     WCHAR tmp_ini_path[MAX_PATH];
     HRESULT hr = E_FAIL;
