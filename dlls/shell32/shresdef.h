1f4c704678a2b688ea04e0f580dc7cc184e1d04a
Louis Lenders <xerox_xerox2000@yahoo.co.uk>
2007-03-22 09:16:54 +0000

    shell32: Define avi resource #163.

--- dlls/shell32/shresdef.h
+++ dlls/shell32/shresdef.h
@@ -150,5 +150,6 @@ FIXME: Need to add them, but for now just let them use the same: searching.avi
 #define IDR_AVI_FINDCOMPUTER       152
 #define IDR_AVI_FILEMOVE           160
 #define IDR_AVI_FILECOPY           161
+#define IDR_AVI_FILENUKE           163
 
 #endif
