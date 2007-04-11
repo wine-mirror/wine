0d88a38238088ed5a36bbe56ec5c9b62106fa8c0
Stefan Dösinger <stefan@codeweavers.com>
2007-04-11 14:02:38 +0200

    wined3d: Dirtify the shader and declaration states if the bound interface is destroyed.

--- dlls/wined3d/vertexdeclaration.c
+++ dlls/wined3d/vertexdeclaration.c
@@ -98,6 +98,11 @@ static ULONG WINAPI IWineD3DVertexDeclarationImpl_Release(IWineD3DVertexDeclarat
     TRACE("(%p) : Releasing from %d\n", This, This->ref);
     ref = InterlockedDecrement(&This->ref);
     if (ref == 0) {
+        if(iface == This->wineD3DDevice->stateBlock->vertexDecl) {
+            /* See comment in PixelShader::Release */
+            IWineD3DDeviceImpl_MarkStateDirty(This->wineD3DDevice, STATE_VDECL);
+        }
+
         HeapFree(GetProcessHeap(), 0, This->pDeclarationWine);
         HeapFree(GetProcessHeap(), 0, This);
     }
