69a88a9d4012bf9437758cb502562237413652b9
Alexandre Julliard <julliard@winehq.org>
2007-05-04 12:08:15 +0200

    Fix minor differences in the formatting of the license notices.

--- dlls/comctl32/tests/trackbar.c
+++ dlls/comctl32/tests/trackbar.c
@@ -11,7 +11,11 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
-*/
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
 
 #include <assert.h>
 #include <windows.h>
--- dlls/rsaenh/rsa.c
+++ dlls/rsaenh/rsa.c
@@ -6,7 +6,7 @@
  * Based on public domain code by Tom St Denis (tomstdenis@iahu.ca)
  *
  * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public 
+ * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
