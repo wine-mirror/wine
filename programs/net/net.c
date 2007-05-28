9aaaafa77459573115910a53da1e3f9dacba9f6c
Tim Schwartz <tim@sanityinternet.com>
2007-05-28 10:02:48 -0500

    net.exe: Add NET START command.

--- programs/net/net.c
+++ programs/net/net.c
@@ -18,15 +18,59 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
+
+#define NET_START 0001
+
+static BOOL net_service(int operation, char *service_name)
+{
+    SC_HANDLE SCManager, serviceHandle;
+    BOOL result = 0;
+    char service_display_name[4096];
+    DWORD buffer_size = sizeof(service_display_name);
+
+    SCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+    if(!SCManager)
+    {
+        printf("Couldn't get handle to SCManager.\n");
+        return FALSE;
+    }
+    serviceHandle = OpenService(SCManager, service_name, SC_MANAGER_ALL_ACCESS);
+    if(!serviceHandle)
+    {
+        printf("Couldn't get handle to service.\n");
+        CloseServiceHandle(SCManager);
+        return FALSE;
+    }
+
+
+    GetServiceDisplayName(SCManager, service_name, service_display_name, &buffer_size);
+    if (!service_display_name[0]) strcpy(service_display_name, service_name);
+
+    switch(operation)
+    {
+    case NET_START:
+        printf("The %s service is starting.\n", service_display_name);
+        result = StartService(serviceHandle, 0, NULL);
+
+        printf("The %s service ", service_display_name);
+        if(!result) printf("failed to start.\n");
+        else printf("was started successfully.\n");
+        break;
+    }
+
+    CloseServiceHandle(serviceHandle);
+    CloseServiceHandle(SCManager);
+    return result;
+}
 
 int main(int argc, char *argv[])
 {
-    int ret = 0;
 
     if (argc < 2)
     {
         printf("The syntax of this command is:\n\n");
-        printf("NET [ HELP ]\n");
+        printf("NET [ HELP | START ]\n");
         return 1;
     }
 
@@ -35,7 +79,23 @@ int main(int argc, char *argv[])
         printf("The syntax of this command is:\n\n");
         printf("NET HELP command\n    -or-\nNET command /HELP\n\n");
         printf("   Commands available are:\n");
-        printf("   NET HELP\n");
+        printf("   NET HELP	NET START\n");
     }
-    return ret;
+
+    if(!strcasecmp(argv[1], "start"))
+    {
+        if(argc < 3)
+        {
+            printf("Specify service name to start.\n");
+            return 1;
+        }
+
+        if(!net_service(NET_START, argv[2]))
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    return 0;
 }
