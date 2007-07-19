648f7b1d9ffa442433983eacb365e9544bff940d
Dmitry Timoshkov <dmitry@codeweavers.com>
2007-07-19 21:34:53 +0900

    winuser.h: Add RealChildWindowFromPoint declaration.

--- include/winuser.h
+++ include/winuser.h
@@ -4813,6 +4813,7 @@ UINT        WINAPI PrivateExtractIconExW(LPCWSTR,int,HICON*,HICON*,UINT);
 UINT        WINAPI PrivateExtractIconsA(LPCSTR,int,int,int,HICON*,UINT*,UINT,UINT);
 UINT        WINAPI PrivateExtractIconsW(LPCWSTR,int,int,int,HICON*,UINT*,UINT,UINT);
 BOOL        WINAPI PtInRect(const RECT*,POINT);
+HWND        WINAPI RealChildWindowFromPoint(HWND,POINT);
 UINT        WINAPI RealGetWindowClassA(HWND,LPSTR,UINT);
 UINT        WINAPI RealGetWindowClassW(HWND,LPWSTR,UINT);
 #define     RealGetWindowClass WINELIB_NAME_AW(RealGetWindowClass)
