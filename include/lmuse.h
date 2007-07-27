2908dab7840d045efb0e959cdc393f3a808fa6d2
Tim Schwartz <tim@sanityinternet.com>
2007-07-27 17:43:16 -0500

    lmuse.h: Add structures needed by NetUseAdd().

--- include/lmuse.h
+++ include/lmuse.h
@@ -1,7 +1,5 @@
 /*
- * Copyright 2002 Andriy Palamarchuk
- *
- * General lm header which includes other lm headers.
+ * Copyright (C) 2007 Tim Schwartz
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -17,21 +15,27 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
+#ifndef _LMUSE_H
+#define _LMUSE_H
 
-#ifndef __WINE_LM_H
-#define __WINE_LM_H
-
-#include <lmcons.h>
-#include <lmerr.h>
-#include <lmshare.h>
-#include <lmbrowsr.h>
-#include <lmaccess.h>
-#include <lmserver.h>
-#include <lmwksta.h>
-#include <lmapibuf.h>
-#include <lmstats.h>
-#include <lmjoin.h>
-#include <lmmsg.h>
-#include <lmuse.h>
-
+#ifdef __cplusplus
+extern "C" {
 #endif
+
+typedef struct _USE_INFO_1 {
+  LMSTR ui1_local, ui1_remote, ui1_password;
+  DWORD ui1_status, ui1_asg_type, ui1_refcount, ui1_usecount;
+} USE_INFO_1, *PUSE_INFO_1, *LPUSE_INFO_1;
+
+typedef struct _USE_INFO_2 {
+  LMSTR ui2_local, ui2_remote, ui2_password;
+  DWORD ui2_status, ui2_asg_type, ui2_refcount, ui2_usecount;
+  LPWSTR ui2_username;
+  LMSTR ui2_domainname;
+} USE_INFO_2, *PUSE_INFO_2, *LPUSE_INFO_2;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ndef _LMUSE_H */
