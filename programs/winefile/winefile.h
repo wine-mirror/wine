dc75f8a07a130f842550a3fc66f119810fdbced7
Michael Stefaniuc <mstefani@redhat.de>
2007-07-29 22:37:38 +0200

    winefile: Use SendMessageW instead of SNDMSG.

--- programs/winefile/winefile.h
+++ programs/winefile/winefile.h
@@ -104,7 +104,7 @@ enum IMAGE {
 #endif
 
 #define FRM_CALC_CLIENT     0xBF83
-#define Frame_CalcFrameClient(hwnd, prt) (SNDMSG(hwnd, FRM_CALC_CLIENT, 0, (LPARAM)(PRECT)prt))
+#define Frame_CalcFrameClient(hwnd, prt) (SendMessageW(hwnd, FRM_CALC_CLIENT, 0, (LPARAM)(PRECT)prt))
 
 typedef struct
 {
