8be5b58ba7ff060111b44c0b4c1562ad6a8d1b58
Alexandre Julliard <julliard@winehq.org>
2007-09-19 17:55:22 +0200

    winex11.drv: Update the wm hints when the window frame is changed.

--- dlls/winex11.drv/winpos.c
+++ dlls/winex11.drv/winpos.c
@@ -403,10 +403,13 @@ BOOL X11DRV_SetWindowPos( HWND hwnd, HWND insert_after, const RECT *rectWindow,
                     TRACE( "mapping non zero size or off-screen win %p\n", hwnd );
                     mapped = TRUE;
                 }
+
+                if (mapped || (swp_flags & SWP_FRAMECHANGED))
+                    X11DRV_set_wm_hints( display, data );
+
                 if (mapped)
                 {
                     X11DRV_sync_window_style( display, data );
-                    X11DRV_set_wm_hints( display, data );
                     wine_tsx11_lock();
                     XMapWindow( display, data->whole_window );
                     XFlush( display );
