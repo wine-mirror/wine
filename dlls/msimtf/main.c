0101e58d43d1eebec2c9dcbfa8071f2eb3db55ad
Jacek Caban <jacek@codeweavers.com>
2007-10-16 20:50:20 +0200

    msimtf: Added Dll[Un]RegisterServer implementation.

--- dlls/msimtf/main.c
+++ dlls/msimtf/main.c
@@ -19,13 +19,22 @@
 #include "config.h"
 
 #include <stdarg.h>
+#include <stdio.h>
 
 #include "windef.h"
 #include "winbase.h"
+#include "wingdi.h"
+#include "winuser.h"
+#include "winreg.h"
+#include "advpub.h"
+#include "ole2.h"
+#include "dimm.h"
 
 #include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(msimtf);
+
+static HINSTANCE msimtf_instance;
 
 /******************************************************************
  *              DllMain (msimtf.@)
@@ -37,6 +46,7 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
     case DLL_WINE_PREATTACH:
         return FALSE;  /* prefer native version */
     case DLL_PROCESS_ATTACH:
+        msimtf_instance = hInstDLL;
         DisableThreadLibraryCalls(hInstDLL);
         break;
     case DLL_PROCESS_DETACH:
@@ -63,13 +73,59 @@ HRESULT WINAPI DllCanUnloadNow(void)
     return S_FALSE;
 }
 
+#define INF_SET_CLSID(clsid)                  \
+    do                                        \
+    {                                         \
+        static CHAR name[] = "CLSID_" #clsid; \
+                                              \
+        pse[i].pszName = name;                \
+        clsids[i++] = &CLSID_ ## clsid;       \
+    } while (0)
+
+static HRESULT register_server(BOOL doregister)
+{
+    HRESULT hres;
+    HMODULE hAdvpack;
+    typeof(RegInstallA) *pRegInstall;
+    STRTABLEA strtable;
+    STRENTRYA pse[1];
+    static CLSID const *clsids[34];
+    int i = 0;
+
+    static const WCHAR wszAdvpack[] = {'a','d','v','p','a','c','k','.','d','l','l',0};
+
+    INF_SET_CLSID(CActiveIMM);
+
+    for(i = 0; i < sizeof(pse)/sizeof(pse[0]); i++) {
+        pse[i].pszValue = HeapAlloc(GetProcessHeap(), 0, 39);
+        sprintf(pse[i].pszValue, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+                clsids[i]->Data1, clsids[i]->Data2, clsids[i]->Data3, clsids[i]->Data4[0],
+                clsids[i]->Data4[1], clsids[i]->Data4[2], clsids[i]->Data4[3], clsids[i]->Data4[4],
+                clsids[i]->Data4[5], clsids[i]->Data4[6], clsids[i]->Data4[7]);
+    }
+
+    strtable.cEntries = sizeof(pse)/sizeof(pse[0]);
+    strtable.pse = pse;
+
+    hAdvpack = LoadLibraryW(wszAdvpack);
+    pRegInstall = (typeof(RegInstallA)*)GetProcAddress(hAdvpack, "RegInstall");
+
+    hres = pRegInstall(msimtf_instance, doregister ? "RegisterDll" : "UnregisterDll", &strtable);
+
+    for(i=0; i < sizeof(pse)/sizeof(pse[0]); i++)
+        HeapFree(GetProcessHeap(), 0, pse[i].pszValue);
+
+    return hres;
+}
+
+#undef INF_SET_CLSID
+
 /***********************************************************************
  *          DllRegisterServer (msimtf.@)
  */
 HRESULT WINAPI DllRegisterServer(void)
 {
-    FIXME("()\n");
-    return S_OK;
+    return register_server(TRUE);
 }
 
 /***********************************************************************
@@ -77,6 +133,5 @@ HRESULT WINAPI DllRegisterServer(void)
  */
 HRESULT WINAPI DllUnregisterServer(void)
 {
-    FIXME("()\n");
-    return S_OK;
+    return register_server(FALSE);
 }
