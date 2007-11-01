ff47878863586e63b20012ddab9b3d3d32682a35
Jacek Caban <jacek@codeweavers.com>
2007-11-01 18:50:49 +0100

    shdocvw: Move registration code to .inf file.

--- dlls/shdocvw/factory.c
+++ dlls/shdocvw/factory.c
@@ -19,8 +19,13 @@
  */
 
 #include <string.h>
-#include "wine/debug.h"
+#include <stdio.h>
+
 #include "shdocvw.h"
+#include "winreg.h"
+#include "advpub.h"
+
+#include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(shdocvw);
 
@@ -165,4 +170,107 @@ HRESULT register_class_object(BOOL do_reg)
     }
 
     return CoRevokeClassObject(cookie);
+}
+
+static const GUID CLSID_MicrosoftBrowserArchitecture =
+    {0xa5e46e3a, 0x8849, 0x11d1, {0x9d, 0x8c, 0x00, 0xc0, 0x4f, 0xc9, 0x9d, 0x61}};
+static const GUID CLSID_MruLongList =
+    {0x53bd6b4e, 0x3780, 0x4693, {0xaf, 0xc3, 0x71, 0x61, 0xc2, 0xf3, 0xee, 0x9c}};
+
+#define INF_SET_CLSID(clsid)                  \
+    do                                        \
+    {                                         \
+        static CHAR name[] = "CLSID_" #clsid; \
+                                              \
+        pse[i].pszName = name;                \
+        clsids[i++] = &CLSID_ ## clsid;       \
+    } while (0)
+
+static HRESULT register_server(BOOL doregister)
+{
+    HRESULT hres;
+    HMODULE hAdvpack;
+    typeof(RegInstallA) *pRegInstall;
+    STRTABLEA strtable;
+    STRENTRYA pse[13];
+    static CLSID const *clsids[13];
+    int i = 0;
+
+    static const WCHAR wszAdvpack[] = {'a','d','v','p','a','c','k','.','d','l','l',0};
+
+    INF_SET_CLSID(CUrlHistory);
+    INF_SET_CLSID(Internet);
+    INF_SET_CLSID(InternetExplorer);
+    INF_SET_CLSID(MicrosoftBrowserArchitecture);
+    INF_SET_CLSID(MruLongList);
+    INF_SET_CLSID(SearchAssistantOC);
+    INF_SET_CLSID(ShellNameSpace);
+    INF_SET_CLSID(ShellSearchAssistantOC);
+    INF_SET_CLSID(ShellShellNameSpace);
+    INF_SET_CLSID(ShellUIHelper);
+    INF_SET_CLSID(ShellWindows);
+    INF_SET_CLSID(WebBrowser);
+    INF_SET_CLSID(WebBrowser_V1);
+
+    for(i = 0; i < sizeof(pse)/sizeof(pse[0]); i++) {
+        pse[i].pszValue = HeapAlloc(GetProcessHeap(), 0, 39);
+        sprintf(pse[i].pszValue, "{%08X-%04X-%04X-%02X%02X-%02X%02X%02X%02X%02X%02X}",
+                clsids[i]->Data1, clsids[i]->Data2, clsids[i]->Data3, clsids[i]->Data4[0],
+                clsids[i]->Data4[1], clsids[i]->Data4[2], clsids[i]->Data4[3], clsids[i]->Data4[4],
+                clsids[i]->Data4[5], clsids[i]->Data4[6], clsids[i]->Data4[7]);
+    }
+
+    strtable.cEntries = sizeof(pse)/sizeof(pse[0]);
+    strtable.pse = pse;
+
+    hAdvpack = LoadLibraryW(wszAdvpack);
+    pRegInstall = (typeof(RegInstallA)*)GetProcAddress(hAdvpack, "RegInstall");
+
+    hres = pRegInstall(shdocvw_hinstance, doregister ? "RegisterDll" : "UnregisterDll", &strtable);
+
+    for(i=0; i < sizeof(pse)/sizeof(pse[0]); i++)
+        HeapFree(GetProcessHeap(), 0, pse[i].pszValue);
+
+    return hres;
+}
+
+#undef INF_SET_CLSID
+
+/***********************************************************************
+ *          DllRegisterServer (msimtf.@)
+ */
+HRESULT WINAPI DllRegisterServer(void)
+{
+    ITypeLib *typelib;
+    HRESULT hres;
+
+    static const WCHAR shdocvwW[] = {'s','h','d','o','c','v','w','.','d','l','l',0};
+
+    hres = register_server(TRUE);
+    if(FAILED(hres))
+        return hres;
+
+    hres = LoadTypeLibEx(shdocvwW, REGKIND_REGISTER, &typelib);
+    if(FAILED(hres)) {
+        ERR("Could not load typelib: %08x\n", hres);
+        return hres;
+    }
+
+    ITypeLib_Release(typelib);
+
+    return hres;
+}
+
+/***********************************************************************
+ *          DllUnregisterServer (msimtf.@)
+ */
+HRESULT WINAPI DllUnregisterServer(void)
+{
+    HRESULT hres;
+
+    hres = register_server(FALSE);
+    if(FAILED(hres))
+        return hres;
+
+    return UnRegisterTypeLib(&LIBID_SHDocVw, 1, 1, LOCALE_SYSTEM_DEFAULT, SYS_WIN32);
 }
