ba6b7e8ba525d82d73f91050f340648d605956f7
Huw Davies <huw@codeweavers.com>
2007-11-02 15:45:39 +0000

    inetcomm: Add a test for MimeOleCreateVirtualStream.

---
+++ dlls/inetcomm/tests/mimeole.c
@@ -0,0 +1,49 @@
+/*
+ * MimeOle tests
+ *
+ * Copyright 2007 Huw Davies
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#define COBJMACROS
+
+#include "windows.h"
+#include "ole2.h"
+#include "mimeole.h"
+
+#include <stdio.h>
+#include <assert.h>
+
+#include "wine/test.h"
+
+
+static void test_CreateVirtualStream(void)
+{
+    HRESULT hr;
+    IStream *pstm;
+
+    hr = MimeOleCreateVirtualStream(&pstm);
+    ok(hr == S_OK, "ret %08x\n", hr);
+
+    IStream_Release(pstm);
+}
+
+START_TEST(mimeole)
+{
+    OleInitialize(NULL);
+    test_CreateVirtualStream();
+    OleUninitialize();
+}
