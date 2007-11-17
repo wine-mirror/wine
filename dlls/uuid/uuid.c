7fdcc87def3b77e6e3b7f2cab96427054624e8e5
Jacek Caban <jacek@codeweavers.com>
2007-11-17 20:02:28 +0100

    include: Added isguids.h.

--- dlls/uuid/uuid.c
+++ dlls/uuid/uuid.c
@@ -69,6 +69,7 @@ DEFINE_GUID(GUID_NULL,0,0,0,0,0,0,0,0,0,0,0);
 #include "urlhist.h"
 #include "hlguids.h"
 #include "dimm.h"
+#include "isguids.h"
 
 /* FIXME: cguids declares GUIDs but does not define their values */
 
