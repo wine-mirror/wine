51d6a08d57931dc0cd122b655fe1fe400488e0aa
Lionel Debroux <lionel_debroux@yahoo.fr>
2007-11-25 09:56:22 +0100

    msvcrt: Test more offsets for _aligned_offset_realloc (especially offset > alignment).

--- dlls/msvcrt/tests/heap.c
+++ dlls/msvcrt/tests/heap.c
@@ -334,6 +334,84 @@ static void test_aligned(void)
     test_aligned_offset_realloc(256, 128, 32, 4);
     test_aligned_offset_realloc(256, 512, 64, 4);
     test_aligned_offset_realloc(256, 128, 64, 4);
+
+    test_aligned_offset_realloc(256, 512, 0, 8);
+    test_aligned_offset_realloc(256, 128, 0, 8);
+    test_aligned_offset_realloc(256, 512, 4, 8);
+    test_aligned_offset_realloc(256, 128, 4, 8);
+    test_aligned_offset_realloc(256, 512, 8, 8);
+    test_aligned_offset_realloc(256, 128, 8, 8);
+    test_aligned_offset_realloc(256, 512, 16, 8);
+    test_aligned_offset_realloc(256, 128, 16, 8);
+    test_aligned_offset_realloc(256, 512, 32, 8);
+    test_aligned_offset_realloc(256, 128, 32, 8);
+    test_aligned_offset_realloc(256, 512, 64, 8);
+    test_aligned_offset_realloc(256, 128, 64, 8);
+
+    test_aligned_offset_realloc(256, 512, 0, 16);
+    test_aligned_offset_realloc(256, 128, 0, 16);
+    test_aligned_offset_realloc(256, 512, 4, 16);
+    test_aligned_offset_realloc(256, 128, 4, 16);
+    test_aligned_offset_realloc(256, 512, 8, 16);
+    test_aligned_offset_realloc(256, 128, 8, 16);
+    test_aligned_offset_realloc(256, 512, 16, 16);
+    test_aligned_offset_realloc(256, 128, 16, 16);
+    test_aligned_offset_realloc(256, 512, 32, 16);
+    test_aligned_offset_realloc(256, 128, 32, 16);
+    test_aligned_offset_realloc(256, 512, 64, 16);
+    test_aligned_offset_realloc(256, 128, 64, 16);
+
+    test_aligned_offset_realloc(256, 512, 0, 32);
+    test_aligned_offset_realloc(256, 128, 0, 32);
+    test_aligned_offset_realloc(256, 512, 4, 32);
+    test_aligned_offset_realloc(256, 128, 4, 32);
+    test_aligned_offset_realloc(256, 512, 8, 32);
+    test_aligned_offset_realloc(256, 128, 8, 32);
+    test_aligned_offset_realloc(256, 512, 16, 32);
+    test_aligned_offset_realloc(256, 128, 16, 32);
+    test_aligned_offset_realloc(256, 512, 32, 32);
+    test_aligned_offset_realloc(256, 128, 32, 32);
+    test_aligned_offset_realloc(256, 512, 64, 32);
+    test_aligned_offset_realloc(256, 128, 64, 32);
+
+    test_aligned_offset_realloc(256, 512, 0, 64);
+    test_aligned_offset_realloc(256, 128, 0, 64);
+    test_aligned_offset_realloc(256, 512, 4, 64);
+    test_aligned_offset_realloc(256, 128, 4, 64);
+    test_aligned_offset_realloc(256, 512, 8, 64);
+    test_aligned_offset_realloc(256, 128, 8, 64);
+    test_aligned_offset_realloc(256, 512, 16, 64);
+    test_aligned_offset_realloc(256, 128, 16, 64);
+    test_aligned_offset_realloc(256, 512, 32, 64);
+    test_aligned_offset_realloc(256, 128, 32, 64);
+    test_aligned_offset_realloc(256, 512, 64, 64);
+    test_aligned_offset_realloc(256, 128, 64, 64);
+
+    test_aligned_offset_realloc(256, 512, 0, 96);
+    test_aligned_offset_realloc(256, 128, 0, 96);
+    test_aligned_offset_realloc(256, 512, 4, 96);
+    test_aligned_offset_realloc(256, 128, 4, 96);
+    test_aligned_offset_realloc(256, 512, 8, 96);
+    test_aligned_offset_realloc(256, 128, 8, 96);
+    test_aligned_offset_realloc(256, 512, 16, 96);
+    test_aligned_offset_realloc(256, 128, 16, 96);
+    test_aligned_offset_realloc(256, 512, 32, 96);
+    test_aligned_offset_realloc(256, 128, 32, 96);
+    test_aligned_offset_realloc(256, 512, 64, 96);
+    test_aligned_offset_realloc(256, 128, 64, 96);
+
+    test_aligned_offset_realloc(256, 512, 0, 112);
+    test_aligned_offset_realloc(256, 128, 0, 112);
+    test_aligned_offset_realloc(256, 512, 4, 112);
+    test_aligned_offset_realloc(256, 128, 4, 112);
+    test_aligned_offset_realloc(256, 512, 8, 112);
+    test_aligned_offset_realloc(256, 128, 8, 112);
+    test_aligned_offset_realloc(256, 512, 16, 112);
+    test_aligned_offset_realloc(256, 128, 16, 112);
+    test_aligned_offset_realloc(256, 512, 32, 112);
+    test_aligned_offset_realloc(256, 128, 32, 112);
+    test_aligned_offset_realloc(256, 512, 64, 112);
+    test_aligned_offset_realloc(256, 128, 64, 112);
 }
 
 START_TEST(heap)
