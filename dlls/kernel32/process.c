9c00a78da52dc14acdb391ae02ec10a63d3b85ab
Alexandre Julliard <julliard@winehq.org>
2008-01-14 15:49:56 +0100

    kernel32: Don't inherit stdin/stdout handles in wineboot.

--- dlls/kernel32/process.c
+++ dlls/kernel32/process.c
@@ -797,6 +797,11 @@ static HANDLE start_wineboot(void)
 
         memset( &si, 0, sizeof(si) );
         si.cb = sizeof(si);
+        si.dwFlags = STARTF_USESTDHANDLES;
+        si.hStdInput  = 0;
+        si.hStdOutput = 0;
+        si.hStdError  = GetStdHandle( STD_ERROR_HANDLE );
+
         GetSystemDirectoryW( cmdline, MAX_PATH );
         lstrcatW( cmdline, command_line );
         if (CreateProcessW( NULL, cmdline, NULL, NULL, FALSE, DETACHED_PROCESS, NULL, NULL, &si, &pi ))
