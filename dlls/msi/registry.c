007833f1302dff64dcdf1c58332b2f086eef5dd3
Christoph von Wittich <Christoph@ApiViewer.de>
2008-01-20 17:58:40 +0100

    msi: Fix process token leak.

--- dlls/msi/registry.c
+++ dlls/msi/registry.c
@@ -478,13 +478,17 @@ static UINT get_user_sid(LPWSTR *usersid)
         return ERROR_FUNCTION_FAILED;
 
     size = sizeof(buf);
-    if (!GetTokenInformation(token, TokenUser, (void *)buf, size, &size))
+    if (!GetTokenInformation(token, TokenUser, (void *)buf, size, &size)) {
+        CloseHandle(token);
         return ERROR_FUNCTION_FAILED;
+    }
 
     user = (PTOKEN_USER)buf;
-    if (!ConvertSidToStringSidW(user->User.Sid, usersid))
+    if (!ConvertSidToStringSidW(user->User.Sid, usersid)) {
+        CloseHandle(token);
         return ERROR_FUNCTION_FAILED;
-
+    }
+    CloseHandle(token);
     return ERROR_SUCCESS;
 }
 
