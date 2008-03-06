ae9411089ab5e1d6df7b5bfaf776d3957d1fcf5f
Francois Gouget <fgouget@codeweavers.com>
2008-03-06 13:27:39 +0100

    ddk: Add a stub ntddk.h header.

---
+++ include/ddk/ntddk.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2008 Francois Gouget for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef _NTDDK_
+#define _NTDDK_
+
+/* Note: We will probably have to duplicate everything ultimately :-( */
+#include <ddk/wdm.h>
+
+#include <excpt.h>
+/* FIXME: #include <ntdef.h> */
+#include <ntstatus.h>
+/* FIXME: #include <bugcodes.h> */
+/* FIXME: #include <ntiologc.h> */
+
+
+typedef enum _BUS_DATA_TYPE
+{
+    ConfigurationSpaceUndefined = -1,
+    Cmos,
+    EisaConfiguration,
+    Pos,
+    CbusConfiguration,
+    PCIConfiguration,
+    VMEConfiguration,
+    NuBusConfiguration,
+    PCMCIAConfiguration,
+    MPIConfiguration,
+    MPSAConfiguration,
+    PNPISAConfiguration,
+    MaximumBusDataType
+} BUS_DATA_TYPE, *PBUS_DATA_TYPE;
+
+#endif
