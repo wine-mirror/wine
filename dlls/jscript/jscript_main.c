fa23a2c4b68ce2dd5d506075099949d3e476d78d
Jacek Caban <jacek@codeweavers.com>
2008-03-09 00:07:57 +0100

    jscript: Added stub DLL.

---
+++ dlls/jscript/jscript_main.c
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2008 Jacek Caban for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "config.h"
+
+#include <stdarg.h>
+
+#define COBJMACROS
+
+#include "windef.h"
+#include "winbase.h"
+
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(jscript);
+
+/******************************************************************
+ *              DllMain (jscript.@)
+ */
+BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
+{
+    TRACE("(%p %d %p)\n", hInstDLL, fdwReason, lpv);
+
+    switch(fdwReason)
+    {
+    case DLL_WINE_PREATTACH:
+        return FALSE;  /* prefer native version */
+    case DLL_PROCESS_ATTACH:
+         DisableThreadLibraryCalls(hInstDLL);
+        break;
+    }
+
+    return TRUE;
+}
+
+/***********************************************************************
+ *		DllGetClassObject	(jscript.@)
+ */
+HRESULT WINAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv)
+{
+    FIXME("%s %s %p\n", debugstr_guid(rclsid), debugstr_guid(riid), ppv);
+    return CLASS_E_CLASSNOTAVAILABLE;
+}
+
+/***********************************************************************
+ *          DllCanUnloadNow (jscript.@)
+ */
+HRESULT WINAPI DllCanUnloadNow(void)
+{
+    FIXME("()\n");
+    return S_FALSE;
+}
+
+/***********************************************************************
+ *          DllRegisterServer (jscript.@)
+ */
+HRESULT WINAPI DllRegisterServer(void)
+{
+    FIXME("()\n");
+    return S_OK;
+}
+
+/***********************************************************************
+ *          DllUnregisterServer (jscript.@)
+ */
+HRESULT WINAPI DllUnregisterServer(void)
+{
+    FIXME("()\n");
+    return S_OK;
+}
