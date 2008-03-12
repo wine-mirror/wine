66c728dec1a37f10c6e039f0a63558c05590ab9d
Jacek Caban <jacek@codeweavers.com>
2008-03-12 18:41:11 +0100

    jscript: Added Dll[Un]RegisterServer implementation.

--- dlls/jscript/jscript_main.c
+++ dlls/jscript/jscript_main.c
@@ -19,15 +19,33 @@
 #include "config.h"
 
 #include <stdarg.h>
+#include <stdio.h>
 
 #define COBJMACROS
 
 #include "windef.h"
 #include "winbase.h"
+#include "winuser.h"
+#include "winreg.h"
+#include "ole2.h"
+#include "advpub.h"
+
+#include "initguid.h"
+#include "activscp.h"
+#include "activaut.h"
 
 #include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(jscript);
+
+static const CLSID CLSID_JScript =
+    {0xf414c260,0x6ac0,0x11cf,{0xb6,0xd1,0x00,0xaa,0x00,0xbb,0xbb,0x58}};
+static const CLSID CLSID_JScriptAuthor =
+    {0xf414c261,0x6ac0,0x11cf,{0xb6,0xd1,0x00,0xaa,0x00,0xbb,0xbb,0x58}};
+static const CLSID CLSID_JScriptEncode =
+    {0xf414c262,0x6ac0,0x11cf,{0xb6,0xd1,0x00,0xaa,0x00,0xbb,0xbb,0x58}};
+
+static HINSTANCE jscript_hinstance;
 
 /******************************************************************
  *              DllMain (jscript.@)
@@ -41,7 +59,8 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
     case DLL_WINE_PREATTACH:
         return FALSE;  /* prefer native version */
     case DLL_PROCESS_ATTACH:
-         DisableThreadLibraryCalls(hInstDLL);
+        DisableThreadLibraryCalls(hInstDLL);
+        jscript_hinstance = hInstDLL;
         break;
     }
 
@@ -67,12 +86,69 @@ HRESULT WINAPI DllCanUnloadNow(void)
 }
 
 /***********************************************************************
+ *              register_inf
+ */
+
+#define INF_SET_ID(id)             \
+    do                             \
+    {                              \
+        static CHAR name[] = #id;  \
+                                   \
+        pse[i].pszName = name;     \
+        clsids[i++] = &id;         \
+    } while (0)
+
+static HRESULT register_inf(BOOL doregister)
+{
+    HRESULT hres;
+    HMODULE hAdvpack;
+    HRESULT (WINAPI *pRegInstall)(HMODULE hm, LPCSTR pszSection, const STRTABLEA* pstTable);
+    STRTABLEA strtable;
+    STRENTRYA pse[7];
+    static CLSID const *clsids[7];
+    int i = 0;
+
+    static const WCHAR advpackW[] = {'a','d','v','p','a','c','k','.','d','l','l',0};
+
+    INF_SET_ID(CLSID_JScript);
+    INF_SET_ID(CLSID_JScriptAuthor);
+    INF_SET_ID(CLSID_JScriptEncode);
+    INF_SET_ID(CATID_ActiveScript);
+    INF_SET_ID(CATID_ActiveScriptParse);
+    INF_SET_ID(CATID_ActiveScriptEncode);
+    INF_SET_ID(CATID_ActiveScriptAuthor);
+
+    for(i = 0; i < sizeof(pse)/sizeof(pse[0]); i++) {
+        pse[i].pszValue = HeapAlloc(GetProcessHeap(), 0, 39);
+        sprintf(pse[i].pszValue, "{%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x}",
+                clsids[i]->Data1, clsids[i]->Data2, clsids[i]->Data3, clsids[i]->Data4[0],
+                clsids[i]->Data4[1], clsids[i]->Data4[2], clsids[i]->Data4[3], clsids[i]->Data4[4],
+                clsids[i]->Data4[5], clsids[i]->Data4[6], clsids[i]->Data4[7]);
+    }
+
+    strtable.cEntries = sizeof(pse)/sizeof(pse[0]);
+    strtable.pse = pse;
+
+    hAdvpack = LoadLibraryW(advpackW);
+    pRegInstall = (void *)GetProcAddress(hAdvpack, "RegInstall");
+
+    hres = pRegInstall(jscript_hinstance, doregister ? "RegisterDll" : "UnregisterDll", &strtable);
+
+    for(i=0; i < sizeof(pse)/sizeof(pse[0]); i++)
+        HeapFree(GetProcessHeap(), 0, pse[i].pszValue);
+
+    return hres;
+}
+
+#undef INF_SET_CLSID
+
+/***********************************************************************
  *          DllRegisterServer (jscript.@)
  */
 HRESULT WINAPI DllRegisterServer(void)
 {
-    FIXME("()\n");
-    return S_OK;
+    TRACE("()\n");
+    return register_inf(TRUE);
 }
 
 /***********************************************************************
@@ -80,6 +156,6 @@ HRESULT WINAPI DllRegisterServer(void)
  */
 HRESULT WINAPI DllUnregisterServer(void)
 {
-    FIXME("()\n");
-    return S_OK;
+    TRACE("()\n");
+    return register_inf(FALSE);
 }
