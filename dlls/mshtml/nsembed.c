5f6ce2de632196894d14871fcde0efd255bfd597
Jacek Caban <jacek@codeweavers.com>
2008-03-16 23:41:55 +0100

    mshtml: Break Wine Gecko 0.0.1 support.

--- dlls/mshtml/nsembed.c
+++ dlls/mshtml/nsembed.c
@@ -265,9 +265,7 @@ static BOOL load_wine_gecko(PRUnichar *gre_path)
     if(res != ERROR_SUCCESS)
         return FALSE;
 
-    ret = load_wine_gecko_v(gre_path, hkey, GECKO_VERSION, GECKO_VERSION_STRING)
-        || load_wine_gecko_v(gre_path, hkey, "0.0.1", "Wine Gecko 0.0.1\n")
-        || load_wine_gecko_v(gre_path, hkey, NULL, "Wine Gecko 0.0.1\n");
+    ret = load_wine_gecko_v(gre_path, hkey, GECKO_VERSION, GECKO_VERSION_STRING);
 
     RegCloseKey(hkey);
     return ret;
