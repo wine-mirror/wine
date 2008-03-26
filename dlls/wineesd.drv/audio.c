52f6018c73c9b8844a377bcf804d6034258f077f
Andrew Talbot <andrew.talbot@talbotville.com>
2008-03-26 21:56:30 +0000

    wineesd.drv: Assign to structs instead of using memcpy.

--- dlls/wineesd.drv/audio.c
+++ dlls/wineesd.drv/audio.c
@@ -1227,7 +1227,7 @@ static DWORD wodOpen(WORD wDevID, LPWAVEOPENDESC lpDesc, DWORD dwFlags)
 
     wwo->wFlags = HIWORD(dwFlags & CALLBACK_TYPEMASK);
 
-    memcpy(&wwo->waveDesc, lpDesc, sizeof(WAVEOPENDESC));
+    wwo->waveDesc = *lpDesc;
     copy_format(lpDesc->lpFormat, &wwo->waveFormat);
 
     if (wwo->waveFormat.Format.wBitsPerSample == 0) {
@@ -1881,8 +1881,8 @@ static DWORD widOpen(WORD wDevID, LPWAVEOPENDESC lpDesc, DWORD dwFlags)
     dwFlags &= ~WAVE_DIRECTSOUND;
 
     wwi->wFlags = HIWORD(dwFlags & CALLBACK_TYPEMASK);
- 
-    memcpy(&wwi->waveDesc, lpDesc, sizeof(WAVEOPENDESC));
+
+    wwi->waveDesc = *lpDesc;
     copy_format(lpDesc->lpFormat, &wwi->waveFormat);
 
     if (wwi->waveFormat.Format.wBitsPerSample == 0) {
