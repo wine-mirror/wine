3bc7657616bcbe16e07792b3fd1bebd4cdb07ba3
Eric Pouech <eric.pouech@orange.fr>
2008-04-30 21:29:15 +0200

    dbghelp: Revert commit ae1791b0b8d181e0d01f8db1cae4eba48edefbac and correctly fix what this patch was trying to do.

--- dlls/dbghelp/stabs.c
+++ dlls/dbghelp/stabs.c
@@ -792,10 +792,9 @@ static inline int stabs_pts_read_array(struct ParseTypedefData* ptd,
 static int stabs_pts_read_type_def(struct ParseTypedefData* ptd, const char* typename,
                                    struct symt** ret_dt)
 {
-    static struct symt null_dt = {SymTagNull};
     int			idx;
     long		sz = -1;
-    struct symt*	new_dt = &null_dt; /* newly created data type */
+    struct symt*	new_dt = NULL;     /* newly created data type */
     struct symt*	ref_dt;		   /* referenced data type (pointer...) */
     long		filenr1, subnr1, tmp;
 
@@ -808,7 +807,7 @@ static int stabs_pts_read_type_def(struct ParseTypedefData* ptd, const char* typ
     while (*ptd->ptr == '=')
     {
 	ptd->ptr++;
-	PTS_ABORTIF(ptd, new_dt->tag != SymTagNull);
+	PTS_ABORTIF(ptd, new_dt != NULL);
 
 	/* first handle attribute if any */
 	switch (*ptd->ptr)      
