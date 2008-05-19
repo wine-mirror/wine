wine-mirror/wine

commit 4046075462c00f4479f185d1c0514584ff851223

cabinet: Remove order-of-evaluation dependencies.

dlls/cabinet/fdi.c
@@ -1034,7 +1034,8 @@ struct Ziphuft **t, cab_LONG *m, fdi_decomp_state *decomp_state)
         w += l[h++];            /* add bits already decoded */
 
         /* compute minimum size table less than or equal to *m bits */
-        z = (z = g - w) > (cab_ULONG)*m ? *m : z;        /* upper limit */
+        if ((z = g - w) > (cab_ULONG)*m)    /* upper limit */
+          z = *m;
         if ((f = 1 << (j = k - w)) > a + 1)     /* try a k-w bit table */
         {                       /* too few codes for k-w bit table */
           f -= a + 1;           /* deduct codes from patterns left */
@@ -1177,7 +1178,9 @@ static cab_LONG fdi_Zipinflate_codes(const struct Ziphuft *tl, const struct Ziph
       ZIPDUMPBITS(e)
       do
       {
-        n -= (e = (e = ZIPWSIZE - ((d &= ZIPWSIZE-1) > w ? d : w)) > n ?n:e);
+        d = max(d & (ZIPWSIZE - 1), w);
+        e = min(ZIPWSIZE - d, n);
+        n -= e;
         do
         {
           CAB(outbuf)[w++] = CAB(outbuf)[d++];

commit 5ddd35f493f40ecd447fa8d0a4591e92a8e91306

cabinet: Fix the regression caused by 4046075462c00f4479f185d1c0514584ff851223.

dlls/cabinet/fdi.c
@@ -1178,8 +1178,9 @@ static cab_LONG fdi_Zipinflate_codes(const struct Ziphuft *tl, const struct Ziph
       ZIPDUMPBITS(e)
       do
       {
-        d = max(d & (ZIPWSIZE - 1), w);
-        e = min(ZIPWSIZE - d, n);
+        d &= ZIPWSIZE - 1;
+        e = ZIPWSIZE - max(d, w);
+        e = min(e, n);
         n -= e;
         do
         {