c41a9039690b3850b260c683a0650fdfa4d1f436
David Adam <david.adam.cnrs@gmail.com>
2008-06-09 12:55:54 +0200

    Revert "amstream: Fix the condition of existence of the IMediaStream interface."
    This reverts commit 653c7e7dcaf7ebee2cfc8a6e356779dc8974aac6.

--- dlls/amstream/mediastream.c
+++ dlls/amstream/mediastream.c
@@ -71,8 +71,7 @@ static HRESULT WINAPI IMediaStreamImpl_QueryInterface(IMediaStream* iface, REFII
     TRACE("(%p/%p)->(%s,%p)\n", iface, This, debugstr_guid(riid), ppvObject);
 
     if (IsEqualGUID(riid, &IID_IUnknown) ||
-        IsEqualGUID(riid, &IID_IAMMultiMediaStream) ||
-        IsEqualGUID(riid, &IID_IDirectDrawMediaStream))
+        IsEqualGUID(riid, &IID_IAMMultiMediaStream))
     {
       IClassFactory_AddRef(iface);
       *ppvObject = This;
