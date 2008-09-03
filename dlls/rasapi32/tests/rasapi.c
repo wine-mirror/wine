d82d5d665c1d21eda57e35b634f0e58c19f0dbba
James Hawkins <jhawkins@codeweavers.com>
2008-09-02 22:58:32 -0500

    rasapi32: Fix several failing tests in win98.

--- dlls/rasapi32/tests/rasapi.c
+++ dlls/rasapi32/tests/rasapi.c
@@ -77,28 +77,32 @@ static void test_rasenum(void)
     /* test first parameter */
     cb = bufsize;
     result = pRasEnumDevicesA(NULL, &cb, &cDevices);
-    ok(result == ERROR_BUFFER_TOO_SMALL,
+    ok(result == ERROR_BUFFER_TOO_SMALL ||
+    result == ERROR_INVALID_USER_BUFFER, /* win98 */
     "Expected ERROR_BUFFER_TOO_SMALL, got %08d\n", result);
 
     rasDevInfo[0].dwSize = 0;
     cb = bufsize;
     result = pRasEnumDevicesA(rasDevInfo, &cb, &cDevices);
     todo_wine
-    ok(result == ERROR_INVALID_SIZE,
+    ok(result == ERROR_INVALID_SIZE ||
+    result == ERROR_INVALID_USER_BUFFER, /* win98 */
     "Expected ERROR_INVALID_SIZE, got %08d\n", result);
 
     rasDevInfo[0].dwSize = sizeof(RASDEVINFOA) -1;
     cb = bufsize;
     result = pRasEnumDevicesA(rasDevInfo, &cb, &cDevices);
     todo_wine
-    ok(result == ERROR_INVALID_SIZE,
+    ok(result == ERROR_INVALID_SIZE ||
+    result == ERROR_INVALID_USER_BUFFER, /* win98 */
     "Expected ERROR_INVALID_SIZE, got %08d\n", result);
 
     rasDevInfo[0].dwSize = sizeof(RASDEVINFOA) +1;
     cb = bufsize;
     result = pRasEnumDevicesA(rasDevInfo, &cb, &cDevices);
     todo_wine
-    ok(result == ERROR_INVALID_SIZE,
+    ok(result == ERROR_INVALID_SIZE ||
+    result == ERROR_INVALID_USER_BUFFER, /* win98 */
     "Expected ERROR_INVALID_SIZE, got %08d\n", result);
 
     /* test second parameter */
@@ -139,14 +143,16 @@ static void test_rasenum(void)
     "Expected ERROR_INVALID_PARAMETER, got %08d\n", result);
 
     result = pRasEnumDevicesA(NULL, &cb, NULL);
-    ok(result == ERROR_INVALID_PARAMETER,
+    ok(result == ERROR_INVALID_PARAMETER ||
+    result == ERROR_INVALID_USER_BUFFER, /* win98 */
     "Expected ERROR_INVALID_PARAMETER, got %08d\n", result);
 
     cb = 0;
     rasDevInfo[0].dwSize = 0;
     result = pRasEnumDevicesA(rasDevInfo, &cb, &cDevices);
     todo_wine
-    ok(result == ERROR_INVALID_SIZE,
+    ok(result == ERROR_INVALID_SIZE ||
+    broken(result == ERROR_BUFFER_TOO_SMALL), /* win98 */
     "Expected ERROR_INVALID_SIZE, got %08d\n", result);
 
     HeapFree(GetProcessHeap(), 0, rasDevInfo);
