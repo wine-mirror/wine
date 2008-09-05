f815b8b9d5fe68d8d2a17c64e84b4a027a14beda
Jan de Mooij <jandemooij@gmail.com>
2008-09-05 11:17:53 +0200

    comdlg32/printdlg: Leave FromPages/ToPages empty if it contains max. WORD value.

--- dlls/comdlg32/printdlg.c
+++ dlls/comdlg32/printdlg.c
@@ -956,9 +956,12 @@ BOOL PRINTDLG_ChangePrinterA(HWND hDlg, char *name,
     lpdm = PrintStructures->lpDevMode;  /* use this as a shortcut */
 
     if(!(lppd->Flags & PD_PRINTSETUP)) {
-      /* Print range (All/Range/Selection) */
-        SetDlgItemInt(hDlg, edt1, lppd->nFromPage, FALSE);
-	SetDlgItemInt(hDlg, edt2, lppd->nToPage, FALSE);
+	/* Print range (All/Range/Selection) */
+	if(lppd->nFromPage != 0xffff)
+	    SetDlgItemInt(hDlg, edt1, lppd->nFromPage, FALSE);
+	if(lppd->nToPage != 0xffff)
+	    SetDlgItemInt(hDlg, edt2, lppd->nToPage, FALSE);
+
 	CheckRadioButton(hDlg, rad1, rad3, rad1);		/* default */
 	if (lppd->Flags & PD_NOSELECTION)
 	    EnableWindow(GetDlgItem(hDlg, rad2), FALSE);
@@ -1160,9 +1163,12 @@ static BOOL PRINTDLG_ChangePrinterW(HWND hDlg, WCHAR *name,
     lpdm = PrintStructures->lpDevMode;  /* use this as a shortcut */
 
     if(!(lppd->Flags & PD_PRINTSETUP)) {
-      /* Print range (All/Range/Selection) */
-        SetDlgItemInt(hDlg, edt1, lppd->nFromPage, FALSE);
-	SetDlgItemInt(hDlg, edt2, lppd->nToPage, FALSE);
+	/* Print range (All/Range/Selection) */
+	if(lppd->nFromPage != 0xffff)
+	    SetDlgItemInt(hDlg, edt1, lppd->nFromPage, FALSE);
+	if(lppd->nToPage != 0xffff)
+	    SetDlgItemInt(hDlg, edt2, lppd->nToPage, FALSE);
+
 	CheckRadioButton(hDlg, rad1, rad3, rad1);		/* default */
 	if (lppd->Flags & PD_NOSELECTION)
 	    EnableWindow(GetDlgItem(hDlg, rad2), FALSE);
