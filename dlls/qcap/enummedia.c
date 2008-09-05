a4dc5ba3cb44593b2b6b3307c7bb630a087b7a76
Andrew Talbot <andrew.talbot@talbotville.com>
2008-09-05 20:26:44 +0100

    qcap: Sign-compare warning fix.

--- dlls/qcap/enummedia.c
+++ dlls/qcap/enummedia.c
@@ -167,7 +167,7 @@ static ULONG WINAPI IEnumMediaTypesImpl_Release(IEnumMediaTypes * iface)
 
     if (!refCount)
     {
-        int i;
+        ULONG i;
         for (i = 0; i < This->enumMediaDetails.cMediaTypes; i++)
            if (This->enumMediaDetails.pMediaTypes[i].pbFormat)
               CoTaskMemFree(This->enumMediaDetails.pMediaTypes[i].pbFormat);
