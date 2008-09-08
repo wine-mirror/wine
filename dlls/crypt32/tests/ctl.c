8733ec83261cf017145676a2d72ca11f7feeb4f3
James Hawkins <jhawkins@codeweavers.com>
2008-09-08 00:50:23 -0500

    crypt32: Trace the returned value on failure.

--- dlls/crypt32/tests/ctl.c
+++ dlls/crypt32/tests/ctl.c
@@ -362,7 +362,7 @@ static void testAddCTLToStore(void)
     ok(!ret &&
      (GetLastError() == CRYPT_E_EXISTS ||
       GetLastError() == CRYPT_E_NOT_FOUND), /* win9x */
-     "expected CRYPT_E_EXISTS, got %08x\n", GetLastError());
+     "expected CRYPT_E_EXISTS, got %d %08x\n", ret, GetLastError());
     CertCloseStore(store, 0);
 
     store = CertOpenStore(CERT_STORE_PROV_MEMORY, 0, 0,
