5288c5b07c8f0116d298cb6cece49b26d10cbf6c
Jacek Caban <jacek@codeweavers.com>
2008-09-17 23:32:27 +0200

    jscript: Added '^=' expression implementation.

--- dlls/jscript/tests/lang.js
+++ dlls/jscript/tests/lang.js
@@ -297,6 +297,10 @@ tmp = 0xf0f0^0xff00;
 ok(tmp === 0x0ff0, "0xf0f0^0xff00 !== 0x0ff0");
 ok(getVT(tmp) === "VT_I4", "getVT(0xf0f0^0xff00) = " + getVT(tmp));
 
+tmp = 5;
+ok((tmp ^= 3) === 6, "tmp(5) ^= 3 !== 6");
+ok(getVT(tmp) === "VT_I4", "getVT(tmp ^= 3) = " + getVT(tmp));
+
 ok(1 < 3.4, "1 < 3.4 failed");
 ok(!(3.4 < 1), "3.4 < 1");
 ok("abc" < "abcd", "abc < abcd failed");
