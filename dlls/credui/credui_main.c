3a498da739c58918a5a689d865419dfc3f259a7b
Andrew Talbot <andrew.talbot@talbotville.com>
2008-09-22 19:38:42 +0100

    credui: Sign-compare warnings fix.

--- dlls/credui/credui_main.c
+++ dlls/credui/credui_main.c
@@ -441,7 +441,7 @@ static void CredDialogCommandOk(HWND hwndDlg, struct cred_dialog_params *params)
 
     if (!strchrW(user, '\\') && !strchrW(user, '@'))
     {
-        INT len_target = strlenW(params->pszTargetName);
+        ULONG len_target = strlenW(params->pszTargetName);
         memcpy(params->pszUsername, params->pszTargetName,
                min(len_target, params->ulUsernameMaxChars) * sizeof(WCHAR));
         if (len_target + 1 < params->ulUsernameMaxChars)
