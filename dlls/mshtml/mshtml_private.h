c584effc737d5cc058f2cec6b54d456ae0e83ed3
Jacek Caban <jacek@codeweavers.com>
2008-09-30 17:41:45 +0200

    mshtml: Move IHTMLElementCollection to separated file.

--- dlls/mshtml/mshtml_private.h
+++ dlls/mshtml/mshtml_private.h
@@ -584,6 +584,7 @@ void doc_insert_script(HTMLDocument*,nsIDOMHTMLScriptElement*);
 IDispatch *script_parse_event(HTMLDocument*,LPCWSTR);
 void set_script_mode(HTMLDocument*,SCRIPTMODE);
 
+IHTMLElementCollection *HTMLElementCollection_Create(IUnknown*,HTMLElement**,DWORD);
 IHTMLElementCollection *create_all_collection(HTMLDOMNode*);
 IHTMLElementCollection *create_collection_from_nodelist(HTMLDocument*,IUnknown*,nsIDOMNodeList*);
 
