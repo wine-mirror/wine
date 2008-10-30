003392b545580899b0cb605f4e8b3a7f9e97c33f
Kirill K. Smirnov <lich@math.spbu.ru>
2008-10-30 19:31:52 +0400

    gdi32: Fix copy/paste typo.

--- dlls/gdi32/metafile.c
+++ dlls/gdi32/metafile.c
@@ -913,7 +913,7 @@ BOOL WINAPI PlayMetaFileRecord( HDC hdc,  HANDLETABLE *ht, METARECORD *mr, UINT 
     case META_DIBSTRETCHBLT:
       {
         LPBITMAPINFO info = (LPBITMAPINFO) &(mr->rdParm[10]);
-        LPSTR bits = (LPSTR)info + bitmap_info_size( info, mr->rdParm[2] );
+        LPSTR bits = (LPSTR)info + bitmap_info_size( info, DIB_RGB_COLORS );
         StretchDIBits( hdc, (SHORT)mr->rdParm[9], (SHORT)mr->rdParm[8], (SHORT)mr->rdParm[7],
                        (SHORT)mr->rdParm[6], (SHORT)mr->rdParm[5], (SHORT)mr->rdParm[4],
                        (SHORT)mr->rdParm[3], (SHORT)mr->rdParm[2], bits, info,
