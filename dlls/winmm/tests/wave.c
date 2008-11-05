aa50cd316c58638c834a7bf68aa6eae67c5e9d93
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2008-11-05 22:03:45 +1100

    winmm: Increase tolerance range.

--- dlls/winmm/tests/wave.c
+++ dlls/winmm/tests/wave.c
@@ -789,13 +789,13 @@ static void wave_out_test_deviceOut(int device, double duration,
             }
         }
 
-        /* Check the sound duration was between -2% and +10% of the expected value */
+        /* Check the sound duration was between -10% and +25% of the expected value */
         end=GetTickCount();
         actual = (end - start) / 1000.0;
         if (winetest_debug > 1)
             trace("sound duration=%g ms\n",1000*actual);
-        ok((actual > (0.97 * (duration+paused))) &&
-           (actual < (1.1 * (duration+paused))),
+        ok((actual > (0.9 * (duration+paused))) &&
+           (actual < (1.25 * (duration+paused))),
            "The sound played for %g ms instead of %g ms\n",
            1000*actual,1000*(duration+paused));
         for (i = 0; i < headers; i++) {
