75cb8a7ad38844eb58a365f3f249b7efbe9a5c45
Jérôme Gardou <jerome.gardou@gmail.com>
2008-11-12 09:03:32 +0100

    d3dx8: Implement D3DXMatrixStack_Scale.

--- dlls/d3dx8/math.c
+++ dlls/d3dx8/math.c
@@ -818,9 +818,15 @@ static HRESULT WINAPI ID3DXMatrixStackImpl_RotateYawPitchRollLocal(ID3DXMatrixSt
 
 static HRESULT WINAPI ID3DXMatrixStackImpl_Scale(ID3DXMatrixStack *iface, FLOAT x, FLOAT y, FLOAT z)
 {
+    D3DXMATRIX temp;
     ID3DXMatrixStackImpl *This = (ID3DXMatrixStackImpl *)iface;
-    FIXME("(%p) : stub\n",This);
-    return E_NOTIMPL;
+
+    TRACE("iface %p\n", iface);
+
+    D3DXMatrixScaling(&temp, x, y, z);
+    D3DXMatrixMultiply(&This->stack[This->current], &This->stack[This->current], &temp);
+
+    return D3D_OK;
 }
 
 static HRESULT WINAPI ID3DXMatrixStackImpl_ScaleLocal(ID3DXMatrixStack *iface, FLOAT x, FLOAT y, FLOAT z)
