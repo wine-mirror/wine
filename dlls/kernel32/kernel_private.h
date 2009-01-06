9f6ff3d5af60374ffae9f47b69f568e2889168ad
Alexandre Julliard <julliard@winehq.org>
2009-01-06 12:49:38 +0100

    kernel32: Change the DEFINE_REGS_ENTRYPOINT macro to use a number of args instead of a size.
    Also assume that all register functions are stdcall.

--- dlls/kernel32/kernel_private.h
+++ dlls/kernel32/kernel_private.h
@@ -146,12 +146,12 @@ extern HANDLE get_BaseNamedObjects_handle(void);
 /* Register functions */
 
 #ifdef __i386__
-#define DEFINE_REGS_ENTRYPOINT( name, args, pop_args ) \
+#define DEFINE_REGS_ENTRYPOINT( name, args ) \
     __ASM_GLOBAL_FUNC( name, \
                        "pushl %eax\n\t" \
                        "call " __ASM_NAME("__wine_call_from_32_regs") "\n\t" \
                        ".long " __ASM_NAME("__regs_") #name "-.\n\t" \
-                       ".byte " #args "," #pop_args )
+                       ".byte 4*" #args ",4*" #args )
 #endif
 
 #endif
