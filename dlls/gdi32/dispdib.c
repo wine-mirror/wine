ca82e0f5b34b861e20b87316b8af8dc4663a2ca4
Francois Gouget <fgouget@free.fr>
2009-01-07 12:25:40 +0100

    gdi32: Remove some dead code.

--- dlls/gdi32/dispdib.c
+++ dlls/gdi32/dispdib.c
@@ -29,74 +29,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(ddraw);
 
-#if 0
-
-static int dispdib_multi = 0;
-
-static WORD DISPDIB_Begin(WORD wFlags)
-{
-    unsigned Xres,Yres,Depth;
-
-    switch(wFlags&DISPLAYDIB_MODE) {
-        case DISPLAYDIB_MODE_DEFAULT:
-            /* FIXME: is this supposed to autodetect? */
-        case DISPLAYDIB_MODE_320x200x8:
-            Xres=320; Yres=200; Depth=8; break;
-        case DISPLAYDIB_MODE_320x240x8:
-            Xres=320; Yres=240; Depth=8; break;
-        default:
-            return DISPLAYDIB_NOTSUPPORTED;
-    }
-    /* more or less dummy calls to Death/Resurrection, for completeness */
-    /* FIXME: what arguments should they get? */
-    Death16(0);
-    if (VGA_SetMode(Xres,Yres,Depth)) {
-         Resurrection16(0,0,0,0,0,0,0);
-         return DISPLAYDIB_NOTSUPPORTED;
-    }
-    return DISPLAYDIB_NOERROR;
-}
-
-static void DISPDIB_End(void)
-{
-    Resurrection16(0,0,0,0,0,0,0); /* FIXME: arguments */
-    VGA_Exit();
-}
-
-static void DISPDIB_Palette(LPBITMAPINFO lpbi)
-{
-    VGA_SetQuadPalette(lpbi->bmiColors,0,256);
-}
-
-static void DISPDIB_Show(LPBITMAPINFOHEADER lpbi,LPSTR lpBits,WORD uFlags)
-{
-    int Xofs,Yofs,Width=lpbi->biWidth,Height=lpbi->biHeight,Delta;
-    int Pitch = (Width + 3) & ~3;
-    unsigned int sPitch,sWidth,sHeight;
-    LPSTR surf = DOSMEM_MapDosToLinear(0xa0000);
-
-    if (VGA_GetMode(&sHeight,&sWidth,NULL)) return;
-    sPitch=320;
-
-    Delta=(Height<0)*2-1;
-    Height*=-Delta; Pitch*=Delta;
-
-    if (uFlags&DISPLAYDIB_NOCENTER) {
-        Xofs=0; Yofs=0;
-    } else {
-        Xofs=(sWidth-Width)/2;
-        Yofs=(sHeight-Height)/2;
-    }
-    surf += (Yofs*sPitch)+Xofs;
-    if (Pitch<0) lpBits-=Pitch*(Height-1);
-    for (; Height; Height--,lpBits+=Pitch,surf+=sPitch) {
-        memcpy(surf,lpBits,Width);
-    }
-
-    VGA_Poll(0);
-}
-#endif
-
 
 /*********************************************************************
  *	DisplayDib	(DISPDIB.1)
@@ -132,31 +64,6 @@ WORD WINAPI DisplayDib(
 		WORD wFlags        /* [in] */
 	)
 {
-#if 0
-    WORD ret;
-
-    if (wFlags&DISPLAYDIB_END) {
-        if (dispdib_multi) DISPDIB_End();
-        dispdib_multi = 0;
-        return DISPLAYDIB_NOERROR;
-    }
-    if (!dispdib_multi) {
-        ret=DISPDIB_Begin(wFlags);
-        if (ret) return ret;
-    }
-    if (wFlags&DISPLAYDIB_BEGIN) dispdib_multi = 1;
-    if (!(wFlags&DISPLAYDIB_NOPALETTE)) {
-        DISPDIB_Palette(lpbi);
-    }
-    /* FIXME: not sure if it's valid to draw images in DISPLAYDIB_BEGIN, so... */
-    if (lpBits) {
-        DISPDIB_Show(&(lpbi->bmiHeader),lpBits,wFlags);
-    }
-    if (!(wFlags&DISPLAYDIB_NOWAIT)) {
-        FIXME("wait not implemented\n");
-    }
-    if (!dispdib_multi) DISPDIB_End();
-#endif
     FIXME( "broken, should be rewritten using ddraw\n" );
     return DISPLAYDIB_NOERROR;
 }
