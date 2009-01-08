0128b10a3c0304a547d5340f64a750e3bbcf51c8
Alexandre Julliard <julliard@winehq.org>
2009-01-08 13:13:05 +0100

    user32: Avoid a pointer cast in a trace.

--- dlls/user32/dialog16.c
+++ dlls/user32/dialog16.c
@@ -768,8 +768,8 @@ HWND16 WINAPI CreateDialogParam16( HINSTANCE16 hInst, LPCSTR dlgTemplate,
     HGLOBAL16 hmem;
     LPCVOID data;
 
-    TRACE("%04x,%s,%04x,%08x,%ld\n",
-          hInst, debugstr_a(dlgTemplate), owner, (DWORD)dlgProc, param );
+    TRACE("%04x,%s,%04x,%p,%ld\n",
+          hInst, debugstr_a(dlgTemplate), owner, dlgProc, param );
 
     if (!(hRsrc = FindResource16( hInst, dlgTemplate, (LPSTR)RT_DIALOG ))) return 0;
     if (!(hmem = LoadResource16( hInst, hRsrc ))) return 0;
