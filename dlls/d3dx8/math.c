952d10a67f2268e4be4a008a3fc1d0a18415247b
David Adam <david.adam.cnrs@gmail.com>
2009-02-05 18:53:09 +0100

    d3dx8: Remove useless includes.

--- dlls/d3dx8/math.c
+++ dlls/d3dx8/math.c
@@ -17,10 +17,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
-#include <assert.h>
 
 #define NONAMELESSUNION
 
