00a5a722a9d352569982e62919ec8d6971f32842
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2009-02-20 21:04:19 +1100

    mshtml: Implement IHTMLCurrentStyle_get_textDecoration.

--- dlls/mshtml/htmlcurstyle.c
+++ dlls/mshtml/htmlcurstyle.c
@@ -370,8 +370,8 @@ static HRESULT WINAPI HTMLCurrentStyle_get_textAlign(IHTMLCurrentStyle *iface, B
 static HRESULT WINAPI HTMLCurrentStyle_get_textDecoration(IHTMLCurrentStyle *iface, BSTR *p)
 {
     HTMLCurrentStyle *This = HTMLCURSTYLE_THIS(iface);
-    FIXME("(%p)->(%p)\n", This, p);
-    return E_NOTIMPL;
+    TRACE("(%p)->(%p)\n", This, p);
+    return get_nsstyle_attr(This->nsstyle, STYLEID_TEXT_DECORATION, p);
 }
 
 static HRESULT WINAPI HTMLCurrentStyle_get_display(IHTMLCurrentStyle *iface, BSTR *p)
