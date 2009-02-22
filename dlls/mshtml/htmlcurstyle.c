e64b198348f6d147ff00441c1967b319908cf0ca
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2009-02-22 12:49:14 +1100

    mshtml: Implement IHTMLCurrentStyle_get_height.

--- dlls/mshtml/htmlcurstyle.c
+++ dlls/mshtml/htmlcurstyle.c
@@ -328,8 +328,8 @@ static HRESULT WINAPI HTMLCurrentStyle_get_width(IHTMLCurrentStyle *iface, VARIA
 static HRESULT WINAPI HTMLCurrentStyle_get_height(IHTMLCurrentStyle *iface, VARIANT *p)
 {
     HTMLCurrentStyle *This = HTMLCURSTYLE_THIS(iface);
-    FIXME("(%p)->(%p)\n", This, p);
-    return E_NOTIMPL;
+    TRACE("(%p)->(%p)\n", This, p);
+    return get_nsstyle_attr_var(This->nsstyle, STYLEID_HEIGHT, p, 0);
 }
 
 static HRESULT WINAPI HTMLCurrentStyle_get_paddingLeft(IHTMLCurrentStyle *iface, VARIANT *p)
