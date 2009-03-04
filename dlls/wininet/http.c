96b639dc8a5311aa10f298e629ff2e64aad8d060
Hans Leidekker <hans@meelstraat.net>
2009-03-04 12:42:43 +0100

    wininet: Remove MSG_PEEK hack from HTTP_GetResponseHeaders.
    Fixes Quicken 2009. Aric said he added this hack back in 2002
    to work around a bug in the IE6 installer, which installs fine
    now without this hack.

--- dlls/wininet/http.c
+++ dlls/wininet/http.c
@@ -3833,17 +3833,12 @@ static INT HTTP_GetResponseHeaders(LPWININETHTTPREQW lpwhr, BOOL clear)
 
     do {
         /*
-         * HACK peek at the buffer
-         */
-        buflen = MAX_REPLY_LEN;
-        NETCON_recv(&lpwhr->netConnection, buffer, buflen, MSG_PEEK, &rc);
-
-        /*
          * We should first receive 'HTTP/1.x nnn OK' where nnn is the status code.
          */
-        memset(buffer, 0, MAX_REPLY_LEN);
+        buflen = MAX_REPLY_LEN;
         if (!NETCON_getNextLine(&lpwhr->netConnection, bufferA, &buflen))
             goto lend;
+        rc += buflen;
         MultiByteToWideChar( CP_ACP, 0, bufferA, buflen, buffer, MAX_REPLY_LEN );
 
         /* split the version from the status code */
