e41e1f8f84f10d6a580dcf1070324eaea36c7f97
Huw Davies <huw@codeweavers.com>
2009-03-11 10:01:03 +0000

    gdi32/tests: Remove a cut and paste error.

--- dlls/gdi32/tests/bitmap.c
+++ dlls/gdi32/tests/bitmap.c
@@ -1649,7 +1649,6 @@ static void test_GetDIBits(void)
     ok(bm.bmType == 0 ||
        broken(bm.bmType == 21072), /* win9x */
        "wrong bmType %d\n", bm.bmType);
-    ok(bm.bmType == 0, "wrong bmType %d\n", bm.bmType);
     ok(bm.bmWidth == 16, "wrong bmWidth %d\n", bm.bmWidth);
     ok(bm.bmHeight == 16, "wrong bmHeight %d\n", bm.bmHeight);
     ok(bm.bmWidthBytes == BITMAP_GetWidthBytes(bm.bmWidth, bm.bmBitsPixel), "wrong bmWidthBytes %d\n", bm.bmWidthBytes);
