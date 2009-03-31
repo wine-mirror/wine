b4ee082d8a211a2f921aa0f00ac6ec148d54ece3
Rob Shearman <robertshearman@gmail.com>
2009-03-31 13:37:58 +0100

    include: Add TCP and IPv6 option defines to ws2ipdef.h.
    Add more IP option defines.
    
    Add defines for address string lengths for IPv4 and IPv6.

--- include/ws2ipdef.h
+++ include/ws2ipdef.h
@@ -91,7 +91,16 @@ typedef struct _INTERFACE_INFO
 #define IP_BLOCK_SOURCE                 17
 #define IP_UNBLOCK_SOURCE               18
 #define IP_PKTINFO                      19
+#define IP_HOPLIMIT                     21
 #define IP_RECEIVE_BROADCAST            22
+#define IP_RECVIF                       24
+#define IP_RECVDSTADDR                  25
+#define IP_IFLIST                       28
+#define IP_ADD_IFLIST                   29
+#define IP_DEL_IFLIST                   30
+#define IP_UNICAST_IF                   31
+#define IP_RTHDR                        32
+#define IP_RECVRTHDR                    38
 #else
 #define WS_IP_OPTIONS                   1
 #define WS_IP_HDRINCL                   2
@@ -108,7 +117,16 @@ typedef struct _INTERFACE_INFO
 #define WS_IP_BLOCK_SOURCE              17
 #define WS_IP_UNBLOCK_SOURCE            18
 #define WS_IP_PKTINFO                   19
+#define WS_IP_HOPLIMIT                  21
 #define WS_IP_RECEIVE_BROADCAST         22
+#define WS_IP_RECVIF                    24
+#define WS_IP_RECVDSTADDR               25
+#define WS_IP_IFLIST                    28
+#define WS_IP_ADD_IFLIST                29
+#define WS_IP_DEL_IFLIST                30
+#define WS_IP_UNICAST_IF                31
+#define WS_IP_RTHDR                     32
+#define WS_IP_RECVRTHDR                 38
 #endif /* USE_WS_PREFIX */
 
 typedef struct WS(sockaddr_in6)
@@ -143,6 +161,105 @@ struct WS(ip_msfilter) {
     WS(u_long)         imsf_numsrc;
     struct WS(in_addr) imsf_slist[1];
 } WS(IP_MSFILTER), *WS(PIP_MSFILTER);
+
+#ifndef USE_WS_PREFIX
+#define IPV6_OPTIONS                    1
+#define IPV6_HDRINCL                    2
+#define IPV6_UNICAST_HOPS               4
+#define IPV6_MULTICAST_IF               9
+#define IPV6_MULTICAST_HOPS             10
+#define IPV6_MULTICAST_LOOP             11
+#define IPV6_ADD_MEMBERSHIP             12
+#define IPV6_JOIN_GROUP                 IPV6_ADD_MEMBERSHIP
+#define IPV6_DROP_MEMBERSHIP            13
+#define IPV6_LEAVE_GROUP                IPV6_DROP_MEMBERSHIP
+#define IPV6_DONTFRAG                   14
+#define IPV6_PKTINFO                    19
+#define IPV6_HOPLIMIT                   21
+#define IPV6_PROTECTION_LEVEL           23
+#define IPV6_RECVIF                     24
+#define IPV6_RECVDSTADDR                25
+#define IPV6_CHECKSUM                   26
+#define IPV6_V6ONLY                     27
+#define IPV6_IFLIST                     28
+#define IPV6_ADD_IFLIST                 29
+#define IPV6_DEL_IFLIST                 30
+#define IPV6_UNICAST_IF                 31
+#define IPV6_RTHDR                      32
+#define IPV6_RECVRTHDR                  38
+#else
+#define WS_IPV6_OPTIONS                 1
+#define WS_IPV6_HDRINCL                 2
+#define WS_IPV6_UNICAST_HOPS            4
+#define WS_IPV6_MULTICAST_IF            9
+#define WS_IPV6_MULTICAST_HOPS          10
+#define WS_IPV6_MULTICAST_LOOP          11
+#define WS_IPV6_ADD_MEMBERSHIP          12
+#define WS_IPV6_DROP_MEMBERSHIP         13
+#define WS_IPV6_LEAVE_GROUP             WS_IPV6_DROP_MEMBERSHIP
+#define WS_IPV6_DONTFRAG                14
+#define WS_IPV6_PKTINFO                 19
+#define WS_IPV6_HOPLIMIT                21
+#define WS_IPV6_PROTECTION_LEVEL        23
+#define WS_IPV6_RECVIF                  24
+#define WS_IPV6_RECVDSTADDR             25
+#define WS_IPV6_CHECKSUM                26
+#define WS_IPV6_V6ONLY                  27
+#define WS_IPV6_IFLIST                  28
+#define WS_IPV6_ADD_IFLIST              29
+#define WS_IPV6_DEL_IFLIST              30
+#define WS_IPV6_UNICAST_IF              31
+#define WS_IPV6_RTHDR                   32
+#define WS_IPV6_RECVRTHDR               38
+#endif /* USE_WS_PREFIX */
+
+#ifndef USE_WS_PREFIX
+#define TCP_OFFLOAD_NO_PREFERENCE       0
+#define TCP_OFFLOAD_NOT_PREFERRED       1
+#define TCP_OFFLOAD_PREFERRED           2
+#else
+#define WS_TCP_OFFLOAD_NO_PREFERENCE    0
+#define WS_TCP_OFFLOAD_NOT_PREFERRED    1
+#define WS_TCP_OFFLOAD_PREFERRED        2
+#endif /* USE_WS_PREFIX */
+
+#ifndef USE_WS_PREFIX
+/* TCP_NODELAY is defined elsewhere */
+#define TCP_EXPEDITED_1122              2
+#define TCP_KEEPALIVE                   3
+#define TCP_MAXSEG                      4
+#define TCP_MAXRT                       5
+#define TCP_STDURG                      6
+#define TCP_NOURG                       7
+#define TCP_ATMARK                      8
+#define TCP_NOSYNRETRIES                9
+#define TCP_TIMESTAMPS                  10
+#define TCP_OFFLOAD_PREFERENCE          11
+#define TCP_CONGESTION_ALGORITHM        12
+#define TCP_DELAY_FIN_ACK               13
+#else
+/* WS_TCP_NODELAY is defined elsewhere */
+#define WS_TCP_EXPEDITED_1122           2
+#define WS_TCP_KEEPALIVE                3
+#define WS_TCP_MAXSEG                   4
+#define WS_TCP_MAXRT                    5
+#define WS_TCP_STDURG                   6
+#define WS_TCP_NOURG                    7
+#define WS_TCP_ATMARK                   8
+#define WS_TCP_NOSYNRETRIES             9
+#define WS_TCP_TIMESTAMPS               10
+#define WS_TCP_OFFLOAD_PREFERENCE       11
+#define WS_TCP_CONGESTION_ALGORITHM     12
+#define WS_TCP_DELAY_FIN_ACK            13
+#endif /* USE_WS_PREFIX */
+
+#ifndef USE_WS_PREFIX
+#define INET_ADDRSTRLEN         22
+#define INET6_ADDRSTRLEN        65
+#else
+#define WS_INET_ADDRSTRLEN      22
+#define WS_INET6_ADDRSTRLEN     65
+#endif /* USE_WS_PREFIX */
 
 #ifdef __cplusplus
 }
