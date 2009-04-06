3a195139c24647633f2c9213cd893465392b7b94
Alexandre Julliard <julliard@winehq.org>
2009-04-06 20:58:10 +0200

    msvcrt: Fix scanf to correctly parse pointers on 64-bit.

--- dlls/msvcrt/scanf.h
+++ dlls/msvcrt/scanf.h
@@ -151,6 +151,8 @@ _FUNCTION_ {
             switch(*format) {
 	    case 'p':
 	    case 'P': /* pointer. */
+                if (sizeof(void *) == sizeof(LONGLONG)) I64_prefix = 1;
+                /* fall through */
 	    case 'x':
 	    case 'X': /* hexadecimal integer. */
 		base = 16;
@@ -224,7 +226,7 @@ _FUNCTION_ {
                     if (!suppress) {
 #define _SET_NUMBER_(type) *va_arg(ap, type*) = negative ? -cur : cur
 			if (I64_prefix) _SET_NUMBER_(LONGLONG);
-			else if (l_prefix) _SET_NUMBER_(long int);
+			else if (l_prefix) _SET_NUMBER_(LONG);
 			else if (h_prefix) _SET_NUMBER_(short int);
 			else _SET_NUMBER_(int);
 		    }
