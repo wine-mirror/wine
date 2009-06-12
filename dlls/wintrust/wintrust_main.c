abacfc0f451cf1247c619167ba4dcb16bd7ac95a
Juan Lang <juan.lang@gmail.com>
2009-06-12 09:21:35 -0700

    wintrust: Check for the presence of required members of WINTRUST_BLOB_INFO rather than requiring callers to be compiled with the same SDK version.

--- dlls/wintrust/wintrust_main.c
+++ dlls/wintrust/wintrust_main.c
@@ -303,7 +303,8 @@ static HRESULT WINAPI WINTRUST_CertVerifyObjTrust(CRYPT_PROVIDER_DATA *data)
     {
     case WTD_CHOICE_BLOB:
         if (data->pWintrustData->u.pBlob &&
-         data->pWintrustData->u.pBlob->cbStruct == sizeof(WINTRUST_BLOB_INFO) &&
+         WVT_IS_CBSTRUCT_GT_MEMBEROFFSET(WINTRUST_BLOB_INFO,
+         data->pWintrustData->u.pBlob->cbStruct, pbMemObject) &&
          data->pWintrustData->u.pBlob->cbMemObject ==
          sizeof(CERT_VERIFY_CERTIFICATE_TRUST) &&
          data->pWintrustData->u.pBlob->pbMemObject)
