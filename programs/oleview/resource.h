313dd0c3a4ba1fd9576820c2adb508f7204d2bef
Michael Stefaniuc <mstefani@redhat.de>
2009-07-03 01:36:00 +0200

    oleview: Build language resource files separately.
    Do not include them from an other rc file.

--- programs/oleview/resource.h
+++ programs/oleview/resource.h
@@ -18,6 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include <windef.h>
+#include <winuser.h>
+
 #define IDS_APPNAME                1
 #define IDS_APPTITLE               2
 #define IDM_MENU                   3
