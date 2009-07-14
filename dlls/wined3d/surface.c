3c2fa6aea46a33a48cae13e3af1b9c52f60c2883
Stefan Dösinger <stefan@codeweavers.com>
2009-07-14 19:01:48 +0200

    wined3d: Set the depth blit helper texture address mode to clamp.
    On ATI cards we use the driver's GL_ARB_texture_non_power_of_two emulation to
    support conditional NP2 textures without having to deal with the denormalized
    coordinates. The default GL_TEXTURE_2D address mode is incompatible with
    GL_ARB_texture_rectangle however.

--- dlls/wined3d/surface.c
+++ dlls/wined3d/surface.c
@@ -4418,6 +4418,9 @@ void surface_load_ds_location(IWineD3DSurface *iface, DWORD location) {
                     0, 0, This->currentDesc.Width, This->currentDesc.Height, 0);
             glTexParameteri(bind_target, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
             glTexParameteri(bind_target, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+            glTexParameteri(bind_target, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(bind_target, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+            glTexParameteri(bind_target, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
             glTexParameteri(bind_target, GL_DEPTH_TEXTURE_MODE_ARB, GL_LUMINANCE);
             glBindTexture(bind_target, old_binding);
 
