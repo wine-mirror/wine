9c7ded0973e5956ef8668301e76314b351c7aa6f
Juan Lang <juan.lang@gmail.com>
2009-08-12 18:15:37 -0700

    wcmd: Only execute non-empty commands.

--- programs/cmd/builtins.c
+++ programs/cmd/builtins.c
@@ -1061,7 +1061,7 @@ void WCMD_part_execute(CMD_LIST **cmdList, WCHAR *firstcmd, WCHAR *variable,
       /* FIXME: Only if previous call worked for && or failed for || */
       if ((*cmdList)->prevDelim == CMD_ONFAILURE ||
           (*cmdList)->prevDelim != CMD_ONSUCCESS) {
-        if (processThese) {
+        if (processThese && (*cmdList)->command) {
           WCMD_execute ((*cmdList)->command, (*cmdList)->redirects, variable,
                         value, cmdList);
         }
