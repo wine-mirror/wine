d8d2aa96faf8cce53e06f3fdbf5029c84e530f22
Alexandre Julliard <julliard@winehq.org>
2009-08-18 16:54:14 +0200

    ddeml: Fix packing of the thunk structure.

--- dlls/ddeml.dll16/ddeml.c
+++ dlls/ddeml.dll16/ddeml.c
@@ -156,6 +156,7 @@ static HDDEDATA	CALLBACK WDML_InvokeCallback16(DWORD pfn16, UINT uType, UINT uFm
  * WDML_InvokeCallback16.
  * The code below is mainly to create the thunks themselved
  */
+#include "pshpack1.h"
 static struct ddeml_thunk
 {
     BYTE        popl_eax;        /* popl  %eax (return address) */
@@ -166,6 +167,7 @@ static struct ddeml_thunk
     DWORD       callback;
     DWORD       instId;          /* instance ID */
 } *DDEML16_Thunks;
+#include "poppack.h"
 
 static CRITICAL_SECTION ddeml_cs;
 static CRITICAL_SECTION_DEBUG critsect_debug =
