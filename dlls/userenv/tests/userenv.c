b374a72d5129d40359a6c96b8f398da2b410ca1f
Alexandre Julliard <julliard@winehq.org>
2009-08-19 12:09:23 +0200

    userenv/tests: Don't use string debugging functions for non-debug usage.

--- dlls/userenv/tests/userenv.c
+++ dlls/userenv/tests/userenv.c
@@ -39,94 +39,6 @@ struct profile_item
     const int todo[4];
 };
 
-/* Debugging functions from wine/libs/wine/debug.c, slightly modified */
-
-/* allocate some tmp string space */
-/* FIXME: this is not 100% thread-safe */
-static char *get_tmp_space( int size )
-{
-    static char *list[32];
-    static long pos;
-    char *ret;
-    int idx;
-
-    idx = ++pos % (sizeof(list)/sizeof(list[0]));
-    if (list[idx])
-        ret = HeapReAlloc( GetProcessHeap(), 0, list[idx], size );
-    else
-        ret = HeapAlloc( GetProcessHeap(), 0, size );
-    if (ret) list[idx] = ret;
-    return ret;
-}
-
-/* default implementation of wine_dbgstr_wn */
-static const char *default_dbgstr_wn( const WCHAR *str, int n, BOOL quotes )
-{
-    char *dst, *res;
-
-    if (!HIWORD(str))
-    {
-        if (!str) return "(null)";
-        res = get_tmp_space( 6 );
-        sprintf( res, "#%04x", LOWORD(str) );
-        return res;
-    }
-    if (n == -1) n = lstrlenW(str);
-    if (n < 0) n = 0;
-    else if (n > 200) n = 200;
-    dst = res = get_tmp_space( n * 5 + 7 );
-    if (quotes)
-    {
-        *dst++ = 'L';
-        *dst++ = '"';
-    }
-    while (n-- > 0)
-    {
-        WCHAR c = *str++;
-        switch (c)
-        {
-        case '\n': *dst++ = '\\'; *dst++ = 'n'; break;
-        case '\r': *dst++ = '\\'; *dst++ = 'r'; break;
-        case '\t': *dst++ = '\\'; *dst++ = 't'; break;
-        case '"':  *dst++ = '\\'; *dst++ = '"'; break;
-        case '\\': *dst++ = '\\'; *dst++ = '\\'; break;
-        default:
-            if (c >= ' ' && c <= 126)
-                *dst++ = (char)c;
-            else
-            {
-                *dst++ = '\\';
-                sprintf(dst,"%04x",c);
-                dst+=4;
-            }
-        }
-    }
-    if (quotes) *dst++ = '"';
-    if (*str)
-    {
-        *dst++ = '.';
-        *dst++ = '.';
-        *dst++ = '.';
-    }
-    *dst = 0;
-    return res;
-}
-
-const char *wine_dbgstr_wn( const WCHAR *s, int n )
-{
-    return default_dbgstr_wn(s, n, TRUE);
-}
-
-const char *wine_dbgstr_w( const WCHAR *s )
-{
-    return default_dbgstr_wn( s, -1, TRUE);
-}
-
-static const char *userenv_dbgstr_w( const WCHAR *s )
-{
-    return default_dbgstr_wn( s, -1, FALSE);
-}
-
 /* Helper function for retrieving environment variables */
 static BOOL get_env(const WCHAR * env, const char * var, char ** result)
 {
@@ -139,8 +51,7 @@ static BOOL get_env(const WCHAR * env, const char * var, char ** result)
     varlen = strlen(var);
     do
     {
-        envlen = lstrlenW(p);
-        sprintf(buf, "%s", userenv_dbgstr_w(p));
+        envlen = WideCharToMultiByte( CP_ACP, 0, p, -1, buf, sizeof(buf), NULL, NULL ) - 1;
         if (CompareStringA(GetThreadLocale(), NORM_IGNORECASE|LOCALE_USE_CP_ACP, buf, min(envlen, varlen), var, varlen) == CSTR_EQUAL)
         {
             if (buf[varlen] == '=')
