6bcf724adcc62cb34932d31912f221eb81e585cf
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2009-08-21 21:03:49 +1000

    mshtml: Implement HTMLCurrentStyle_get_borderLeftWidth.

--- dlls/mshtml/htmlcurstyle.c
+++ dlls/mshtml/htmlcurstyle.c
@@ -300,8 +300,8 @@ static HRESULT WINAPI HTMLCurrentStyle_get_borderBottomWidth(IHTMLCurrentStyle *
 static HRESULT WINAPI HTMLCurrentStyle_get_borderLeftWidth(IHTMLCurrentStyle *iface, VARIANT *p)
 {
     HTMLCurrentStyle *This = HTMLCURSTYLE_THIS(iface);
-    FIXME("(%p)->(%p)\n", This, p);
-    return E_NOTIMPL;
+    TRACE("(%p)->(%p)\n", This, p);
+    return get_nsstyle_attr_var(This->nsstyle, STYLEID_BORDER_LEFT_WIDTH, p, 0);
 }
 
 static HRESULT WINAPI HTMLCurrentStyle_get_left(IHTMLCurrentStyle *iface, VARIANT *p)
