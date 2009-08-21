157c1629c1c9403f7a771b113ff4ea8ac63acc78
Aric Stewart <aric@codeweavers.com>
2009-08-21 09:39:15 -0500

    msacm32: In acmDriverDetails if we are unable to load the driver then return MMSYSERR_NOTSUPPORTED as windows does.

--- dlls/msacm32/driver.c
+++ dlls/msacm32/driver.c
@@ -305,6 +305,8 @@ MMRESULT WINAPI acmDriverDetailsW(HACMDRIVERID hadid, PACMDRIVERDETAILSW padd, D
         paddw.cbStruct = min(padd->cbStruct, sizeof(*padd));
         memcpy(padd, &paddw, paddw.cbStruct);
     }
+    else if (mmr == MMSYSERR_NODRIVER)
+        return MMSYSERR_NOTSUPPORTED;
 
     return mmr;
 }
