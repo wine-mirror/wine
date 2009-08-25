0a77124844fb33d833d6749ab1c107e8b2dc9780
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2009-08-25 14:45:23 +1000

    mshtml: Implement IHTMLCurrentStyle_get_backgroundColor.

--- dlls/mshtml/htmlcurstyle.c
+++ dlls/mshtml/htmlcurstyle.c
@@ -151,8 +151,8 @@ static HRESULT WINAPI HTMLCurrentStyle_get_color(IHTMLCurrentStyle *iface, VARIA
 static HRESULT WINAPI HTMLCurrentStyle_get_backgroundColor(IHTMLCurrentStyle *iface, VARIANT *p)
 {
     HTMLCurrentStyle *This = HTMLCURSTYLE_THIS(iface);
-    FIXME("(%p)->(%p)\n", This, p);
-    return E_NOTIMPL;
+    TRACE("(%p)->(%p)\n", This, p);
+    return get_nsstyle_attr_var(This->nsstyle, STYLEID_BACKGROUND_COLOR, p, 0);
 }
 
 static HRESULT WINAPI HTMLCurrentStyle_get_fontFamily(IHTMLCurrentStyle *iface, BSTR *p)
