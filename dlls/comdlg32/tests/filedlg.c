5d38ddc208854db6981635c6aaeb66160ea5940d
Ge van Geldorp <ggeldorp@vmware.com>
2009-08-31 09:15:35 +0200

    comdlg32/tests: Fix test on Win7.

--- dlls/comdlg32/tests/filedlg.c
+++ dlls/comdlg32/tests/filedlg.c
@@ -634,9 +634,11 @@ static void test_ok(void)
     OPENFILENAME ofn = { sizeof(OPENFILENAME)};
     char filename[1024] = {0};
     char tmpfilename[ MAX_PATH];
+    char curdir[MAX_PATH];
     int i;
     DWORD ret;
 
+    ok(GetCurrentDirectoryA(sizeof(curdir), curdir) != 0, "Failed to get current dir err %d\n", GetLastError());
     if (!GetTempFileNameA(".", "txt", 0, tmpfilename)) {
         skip("Failed to create a temporary file name\n");
         return;
@@ -659,6 +661,7 @@ static void test_ok(void)
         ok(ret == ok_testcases[i].expclose, "testid %d: GetOpenFileName returned %#x\n", i, ret);
         ret = CommDlgExtendedError();
         ok(!ret, "CommDlgExtendedError returned %#x\n", ret);
+        ok(SetCurrentDirectoryA(curdir), "Failed to restore current dir err %d\n", GetLastError());
     }
     ret =  DeleteFileA( tmpfilename);
     ok( ret, "Failed to delete temporary file %s err %d\n", tmpfilename, GetLastError());
