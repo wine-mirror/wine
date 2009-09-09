ecc6a9519ff75023fbb3be27a3594ef2c8640edd
Henri Verbeet <hverbeet@codeweavers.com>
2009-09-09 18:12:39 +0200

    d3d10: Add a debug function for D3D10_SHADER_VARIABLE_TYPE.

--- dlls/d3d10/effect.c
+++ dlls/d3d10/effect.c
@@ -486,7 +486,7 @@ static HRESULT parse_fx10_type(struct d3d10_effect_type *t, const char *ptr, con
         TRACE("Type description: %#x.\n", tmp);
         TRACE("\tcolumns: %u.\n", t->column_count);
         TRACE("\trows: %u.\n", t->row_count);
-        TRACE("\tbasetype: %#x.\n", t->basetype);
+        TRACE("\tbasetype: %s.\n", debug_d3d10_shader_variable_type(t->basetype));
         TRACE("\tclass: %s.\n", debug_d3d10_shader_variable_class(t->type_class));
         TRACE("\tunknown bits: %#x.\n", tmp & ~(D3D10_FX10_TYPE_COLUMN_MASK | D3D10_FX10_TYPE_ROW_MASK
                 | D3D10_FX10_TYPE_BASETYPE_MASK | D3D10_FX10_TYPE_CLASS_MASK));
