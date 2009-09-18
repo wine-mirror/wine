wine-mirror/wine

commit 42c83b9dc7b5094384e718926eddd4b297c7bbae

comctl32/trackbar: Fix tic count calculation.

dlls/comctl32/trackbar.c
@@ -151,8 +151,12 @@ static void TRACKBAR_RecalculateTics (TRACKBAR_INFO *infoPtr)
     int tic;
     unsigned nrTics, i;
 
-    if (infoPtr->uTicFreq && infoPtr->lRangeMax >= infoPtr->lRangeMin)
-    	nrTics=(infoPtr->lRangeMax - infoPtr->lRangeMin)/infoPtr->uTicFreq;
+    if (infoPtr->uTicFreq && infoPtr->lRangeMax >= infoPtr->lRangeMin) {
+        nrTics=(infoPtr->lRangeMax - infoPtr->lRangeMin)/infoPtr->uTicFreq;
+        /* don't add extra tic if there's no remainder */
+        if ((infoPtr->lRangeMax - infoPtr->lRangeMin) % infoPtr->uTicFreq == 0)
+          nrTics--;
+    }
     else {
         Free (infoPtr->tics);
         infoPtr->tics = NULL;
@@ -1042,10 +1046,7 @@ TRACKBAR_GetNumTics (const TRACKBAR_INFO *infoPtr)
     if (GetWindowLongW (infoPtr->hwndSelf, GWL_STYLE) & TBS_NOTICKS)
         return 0;
 
-    if(infoPtr->uNumTics == 0)
-        return 2;
-    else
-        return infoPtr->uNumTics + 1;
+    return infoPtr->uNumTics + 2;
 }
 
 

dlls/comctl32/tests/trackbar.c
@@ -30,6 +30,7 @@
 #define PARENT_SEQ_INDEX 0
 #define TRACKBAR_SEQ_INDEX 1
 
+HWND hWndParent;
 
 static struct msg_sequence *sequences[NUM_MSG_SEQUENCE];
 
@@ -846,9 +847,7 @@ static void test_tic_placement(HWND hWndTrackbar){
     r = SendMessage(hWndTrackbar, TBM_GETTIC, 2,0);
     expect(4, r);
     r = SendMessage(hWndTrackbar, TBM_GETTIC, 4,0);
-    todo_wine{
-        expect(-1, r);
-    }
+    expect(-1, r);
 
     /* test TBM_GETTICPIC */
     r = SendMessage(hWndTrackbar, TBM_GETTICPOS, 0, 0);
@@ -967,11 +966,27 @@ static void test_ignore_selection(HWND hWndTrackbar){
     ok_sequence(sequences, PARENT_SEQ_INDEX, parent_empty_test_seq, "parent ignore selection setting test sequence", FALSE);
 }
 
+static void test_initial_state(void)
+{
+    HWND hWnd;
+    DWORD ret;
+
+    hWnd = create_trackbar(0, hWndParent);
+
+    ret = SendMessage(hWnd, TBM_GETNUMTICS, 0, 0);
+    expect(2, ret);
+    ret = SendMessage(hWnd, TBM_GETTIC, 0, 0);
+    expect(-1, ret);
+    ret = SendMessage(hWnd, TBM_GETTICPOS, 0, 0);
+    expect(-1, ret);
+
+    DestroyWindow(hWnd);
+}
+
 START_TEST(trackbar)
 {
     DWORD style = WS_VISIBLE | TBS_TOOLTIPS | TBS_ENABLESELRANGE | TBS_FIXEDLENGTH | TBS_AUTOTICKS;
     HWND hWndTrackbar;
-    HWND hWndParent;
 
     init_msg_sequences(sequences, NUM_MSG_SEQUENCE);
     InitCommonControls();
@@ -1035,6 +1050,8 @@ START_TEST(trackbar)
     test_ignore_selection(hWndTrackbar);
 
     DestroyWindow(hWndTrackbar);
+
+    test_initial_state();
 
     DestroyWindow(hWndParent);
 }

commit 0f670168e4b52b974cf4df4bbfdaef8442ea8cea

comctl32/trackbar: Protect from negative tic count in recalculation helper.
Fix for regression introduced in 42c83b9dc7b5094384e718926eddd4b297c7bbae

dlls/comctl32/trackbar.c
@@ -155,7 +155,7 @@ static void TRACKBAR_RecalculateTics (TRACKBAR_INFO *infoPtr)
     if (infoPtr->uTicFreq && infoPtr->lRangeMax >= infoPtr->lRangeMin) {
         nrTics=(infoPtr->lRangeMax - infoPtr->lRangeMin)/infoPtr->uTicFreq;
         /* don't add extra tic if there's no remainder */
-        if ((infoPtr->lRangeMax - infoPtr->lRangeMin) % infoPtr->uTicFreq == 0)
+        if (nrTics && ((infoPtr->lRangeMax - infoPtr->lRangeMin) % infoPtr->uTicFreq == 0))
           nrTics--;
     }
     else {

dlls/comctl32/tests/trackbar.c
@@ -785,22 +785,41 @@ static void test_tic_settings(HWND hWndTrackbar){
     /* test TBM_SETTICFREQ */
     SendMessage(hWndTrackbar, TBM_SETRANGE, TRUE, MAKELONG(0, 10));
     SendMessage(hWndTrackbar, TBM_SETTICFREQ, 2, 0);
-    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0,0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
     expect(6, r);
     SendMessage(hWndTrackbar, TBM_SETTICFREQ, 5, 0);
-    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0,0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
     expect(3, r);
     SendMessage(hWndTrackbar, TBM_SETTICFREQ, 15, 0);
-    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0,0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
     expect(2, r);
 
     /* test TBM_GETNUMTICS */
     /* since TIC FREQ is 15, there should be only 2 tics now */
-    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0,0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
     expect(2, r);
 
     ok_sequence(sequences, TRACKBAR_SEQ_INDEX, tic_settings_test_seq, "tic settings test sequence", TRUE);
     ok_sequence(sequences, PARENT_SEQ_INDEX, parent_tic_settings_test_seq, "parent tic settings test sequence", TRUE);
+
+    /* range [0,0], freq = 1 */
+    SendMessage(hWndTrackbar, TBM_SETRANGEMAX, TRUE, 0);
+    SendMessage(hWndTrackbar, TBM_SETRANGEMIN, TRUE, 0);
+    SendMessage(hWndTrackbar, TBM_SETTICFREQ, 1, 0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
+    expect(2, r);
+    /* range [0,1], freq = 1 */
+    SendMessage(hWndTrackbar, TBM_SETRANGEMAX, TRUE, 1);
+    SendMessage(hWndTrackbar, TBM_SETRANGEMIN, TRUE, 0);
+    SendMessage(hWndTrackbar, TBM_SETTICFREQ, 1, 0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
+    expect(2, r);
+    /* range [0,2], freq = 1 */
+    SendMessage(hWndTrackbar, TBM_SETRANGEMAX, TRUE, 2);
+    SendMessage(hWndTrackbar, TBM_SETRANGEMIN, TRUE, 0);
+    SendMessage(hWndTrackbar, TBM_SETTICFREQ, 1, 0);
+    r = SendMessage(hWndTrackbar, TBM_GETNUMTICS, 0, 0);
+    expect(3, r);
 }
 
 static void test_tic_placement(HWND hWndTrackbar){