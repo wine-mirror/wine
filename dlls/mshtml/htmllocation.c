3b5ed104bf358bb0ecd876e7f170f458ddc52249
Andrew Eikum <aeikum@codeweavers.com>
2009-09-28 13:31:08 -0500

    mshtml: Check for null pointers in IHTMLLocation_get*.

--- dlls/mshtml/htmllocation.c
+++ dlls/mshtml/htmllocation.c
@@ -168,6 +168,10 @@ static HRESULT WINAPI HTMLLocation_get_protocol(IHTMLLocation *iface, BSTR *p)
 {
     HTMLLocation *This = HTMLLOCATION_THIS(iface);
     FIXME("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
+
     return E_NOTIMPL;
 }
 
@@ -182,6 +186,10 @@ static HRESULT WINAPI HTMLLocation_get_host(IHTMLLocation *iface, BSTR *p)
 {
     HTMLLocation *This = HTMLLOCATION_THIS(iface);
     FIXME("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
+
     return E_NOTIMPL;
 }
 
@@ -196,6 +204,10 @@ static HRESULT WINAPI HTMLLocation_get_hostname(IHTMLLocation *iface, BSTR *p)
 {
     HTMLLocation *This = HTMLLOCATION_THIS(iface);
     FIXME("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
+
     return E_NOTIMPL;
 }
 
@@ -210,6 +222,10 @@ static HRESULT WINAPI HTMLLocation_get_port(IHTMLLocation *iface, BSTR *p)
 {
     HTMLLocation *This = HTMLLOCATION_THIS(iface);
     FIXME("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
+
     return E_NOTIMPL;
 }
 
@@ -230,6 +246,9 @@ static HRESULT WINAPI HTMLLocation_get_pathname(IHTMLLocation *iface, BSTR *p)
     HRESULT hres;
 
     TRACE("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
 
     hres = get_url(This, &doc_url);
     if(FAILED(hres))
@@ -271,6 +290,10 @@ static HRESULT WINAPI HTMLLocation_get_search(IHTMLLocation *iface, BSTR *p)
 {
     HTMLLocation *This = HTMLLOCATION_THIS(iface);
     FIXME("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
+
     return E_NOTIMPL;
 }
 
@@ -285,6 +308,10 @@ static HRESULT WINAPI HTMLLocation_get_hash(IHTMLLocation *iface, BSTR *p)
 {
     HTMLLocation *This = HTMLLOCATION_THIS(iface);
     FIXME("(%p)->(%p)\n", This, p);
+
+    if(!p)
+        return E_POINTER;
+
     return E_NOTIMPL;
 }
 
