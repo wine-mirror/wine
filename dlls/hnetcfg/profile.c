1e867f3647a56c83a2374ca2aff1e9a847a85a7d
Hans Leidekker <hans@codeweavers.com>
2009-10-12 13:38:57 +0200

    hnetcfg: Add stub implementations of INetFwServices and INetFwService.

--- dlls/hnetcfg/profile.c
+++ dlls/hnetcfg/profile.c
@@ -271,8 +271,8 @@ static HRESULT WINAPI fw_profile_get_Services(
 {
     fw_profile *This = impl_from_INetFwProfile( iface );
 
-    FIXME("%p, %p\n", This, Services);
-    return E_NOTIMPL;
+    TRACE("%p, %p\n", This, Services);
+    return NetFwServices_create( NULL, (void **)Services );
 }
 
 static HRESULT WINAPI fw_profile_get_AuthorizedApplications(
