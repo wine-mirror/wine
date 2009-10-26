48033db67ecc14e6efde0f9020014434a5d356c2
Paul Vriens <Paul.Vriens.Wine@gmail.com>
2009-10-26 20:06:13 +0100

    advapi32/tests: Add some basic eventlog tests.

---
+++ dlls/advapi32/tests/eventlog.c
@@ -0,0 +1,102 @@
+/*
+ * Unit tests for Event Logging functions
+ *
+ * Copyright (c) 2009 Paul Vriens
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdarg.h>
+
+#include "windef.h"
+#include "winbase.h"
+#include "winerror.h"
+#include "winnt.h"
+
+#include "wine/test.h"
+
+static void test_open_close(void)
+{
+    HANDLE handle;
+    BOOL ret;
+
+    SetLastError(0xdeadbeef);
+    ret = CloseEventLog(NULL);
+    todo_wine
+    {
+    ok(!ret, "Expected failure\n");
+    ok(GetLastError() == ERROR_INVALID_HANDLE ||
+       GetLastError() == ERROR_NOACCESS, /* W2K */
+       "Expected ERROR_INVALID_HANDLE, got %d\n", GetLastError());
+    }
+
+    SetLastError(0xdeadbeef);
+    handle = OpenEventLogA(NULL, NULL);
+    todo_wine
+    {
+    ok(handle == NULL, "Didn't expect a handle\n");
+    ok(GetLastError() == ERROR_INVALID_PARAMETER, "Expected ERROR_INVALID_PARAMETER, got %d\n", GetLastError());
+    }
+
+    SetLastError(0xdeadbeef);
+    handle = OpenEventLogA("IDontExist", NULL);
+    todo_wine
+    {
+    ok(handle == NULL, "Didn't expect a handle\n");
+    ok(GetLastError() == ERROR_INVALID_PARAMETER, "Expected ERROR_INVALID_PARAMETER, got %d\n", GetLastError());
+    }
+
+    SetLastError(0xdeadbeef);
+    handle = OpenEventLogA("IDontExist", "deadbeef");
+    todo_wine
+    {
+    ok(handle == NULL, "Didn't expect a handle\n");
+    ok(GetLastError() == RPC_S_SERVER_UNAVAILABLE ||
+       GetLastError() == RPC_S_INVALID_NET_ADDR, /* Some Vista and Win7 */
+       "Expected RPC_S_SERVER_UNAVAILABLE, got %d\n", GetLastError());
+    }
+
+    /* This one opens the Application log */
+    handle = OpenEventLogA(NULL, "deadbeef");
+    ok(handle != NULL, "Expected a handle\n");
+    ret = CloseEventLog(handle);
+    ok(ret, "Expected success\n");
+    /* Close a second time */
+    SetLastError(0xdeadbeef);
+    ret = CloseEventLog(handle);
+    todo_wine
+    {
+    ok(!ret, "Expected failure\n");
+    ok(GetLastError() == ERROR_INVALID_HANDLE, "Expected ERROR_INVALID_HANDLE, got %d\n", GetLastError());
+    }
+
+    handle = OpenEventLogA(NULL, "Application");
+    ok(handle != NULL, "Expected a handle\n");
+    CloseEventLog(handle);
+}
+
+START_TEST(eventlog)
+{
+    SetLastError(0xdeadbeef);
+    CloseEventLog(NULL);
+    if (GetLastError() == ERROR_CALL_NOT_IMPLEMENTED)
+    {
+        win_skip("Event log functions are not implemented\n");
+        return;
+    }
+
+    /* Parameters only */
+    test_open_close();
+}
