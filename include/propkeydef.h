8c278a0d82c22e8cb85896ef61e5975575f8b452
Maarten Lankhorst <m.b.lankhorst@gmail.com>
2009-11-13 14:48:53 +0100

    include: Fix propkeydef.h header.

--- include/propkeydef.h
+++ include/propkeydef.h
@@ -33,18 +33,18 @@
 #ifdef INITGUID
 #ifdef __cplusplus
 #define DEFINE_PROPERTYKEY(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8, pid) \
-        EXTERN_C const PROPERTYKEY name DECLSPEC_HIDDEN DECLSPEC_ANY; \
+        EXTERN_C const PROPERTYKEY name DECLSPEC_HIDDEN DECLSPEC_SELECTANY; \
         EXTERN_C const PROPERTYKEY name = \
         { { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }, pid }
 #else
 #define DEFINE_PROPERTYKEY(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8, pid) \
-        const PROPERTYKEY name DECLSPEC_HIDDEN DECLSPEC_ANY; \
+        const PROPERTYKEY name DECLSPEC_HIDDEN DECLSPEC_SELECTANY; \
         const PROPERTYKEY name = \
         { { l, w1, w2, { b1, b2,  b3,  b4,  b5,  b6,  b7,  b8 } }, pid }
 #endif
 #else
 #define DEFINE_PROPERTYKEY(name, l, w1, w2, b1, b2, b3, b4, b5, b6, b7, b8, pid) \
-    EXTERN_C const PROPERTYKEY name DECLSPEC_HIDDEN DECLSPEC_ANY
+    EXTERN_C const PROPERTYKEY name DECLSPEC_HIDDEN DECLSPEC_SELECTANY
 #endif
 
 #ifndef IsEqualPropertyKey
