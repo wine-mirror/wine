6b8c0532180dab0d6cd95ba7e6a6bca334096b7d
Juan Lang <juan.lang@gmail.com>
2009-11-30 12:56:02 -0800

    crypt32: Fix test failures.

--- dlls/crypt32/tests/chain.c
+++ dlls/crypt32/tests/chain.c
@@ -3034,7 +3034,8 @@ static const SimpleChainStatusCheck simpleStatus27[] = {
 static const CERT_TRUST_STATUS elementStatus27Broken[] = {
  { CERT_TRUST_NO_ERROR, CERT_TRUST_HAS_NAME_MATCH_ISSUER },
  { CERT_TRUST_IS_UNTRUSTED_ROOT,
-   CERT_TRUST_IS_SELF_SIGNED | CERT_TRUST_HAS_NAME_MATCH_ISSUER },
+   CERT_TRUST_IS_SELF_SIGNED | CERT_TRUST_HAS_NAME_MATCH_ISSUER |
+   CERT_TRUST_HAS_VALID_NAME_CONSTRAINTS },
 };
 static const SimpleChainStatusCheck simpleStatus27Broken[] = {
  { sizeof(elementStatus27Broken) / sizeof(elementStatus27Broken[0]),
@@ -3408,7 +3409,7 @@ static ChainCheck chainCheckEmbeddedNullBroken = {
      CERT_TRUST_HAS_NOT_DEFINED_NAME_CONSTRAINT,
      CERT_TRUST_HAS_PREFERRED_ISSUER },
    { CERT_TRUST_IS_UNTRUSTED_ROOT | CERT_TRUST_HAS_NOT_DEFINED_NAME_CONSTRAINT,
-     0 },
+     CERT_TRUST_HAS_VALID_NAME_CONSTRAINTS },
    1, simpleStatus27Broken },
  0 };
 
