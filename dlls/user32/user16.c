da57abcb02598dae7a121d8d4a3db0043dc3901b
Alexandre Julliard <julliard@winehq.org>
2009-12-23 19:49:27 +0100

    user32: Move a few 16-bit stubs to user16.c.

--- dlls/user32/user16.c
+++ dlls/user32/user16.c
@@ -2876,6 +2876,46 @@ BOOL16 WINAPI SystemParametersInfo16( UINT16 uAction, UINT16 uParam,
 
 
 /***********************************************************************
+ *		USER_489 (USER.489)
+ */
+LONG WINAPI stub_USER_489(void)
+{
+    FIXME("stub\n");
+    return 0;
+}
+
+
+/***********************************************************************
+ *		USER_490 (USER.490)
+ */
+LONG WINAPI stub_USER_490(void)
+{
+    FIXME("stub\n");
+    return 0;
+}
+
+
+/***********************************************************************
+ *		USER_492 (USER.492)
+ */
+LONG WINAPI stub_USER_492(void)
+{
+    FIXME("stub\n");
+    return 0;
+}
+
+
+/***********************************************************************
+ *		USER_496 (USER.496)
+ */
+LONG WINAPI stub_USER_496(void)
+{
+    FIXME("stub\n");
+    return 0;
+}
+
+
+/***********************************************************************
  *           FormatMessage   (USER.606)
  */
 DWORD WINAPI FormatMessage16(
