8bc6ff9d906dd99703a4c79a06299b1cba5104cc
Dan Kegel <dank@kegel.com>
2010-02-16 12:44:16 -0800

    cmd: Add test to show ~dp0 should be directory containing batch file.

--- programs/cmd/tests/test_builtins.cmd
+++ programs/cmd/tests/test_builtins.cmd
@@ -14,3 +14,12 @@ echo ------------ Testing 'set' --------------
 echo set "FOO=bar" should not include the quotes in the variable value
 set "FOO=bar"
 echo %FOO%
+
+echo ------------ Testing variable expansion --------------
+echo ~dp0 should be directory containing batch file
+echo %~dp0
+mkdir dummydir
+cd dummydir
+echo %~dp0
+cd ..
+rmdir dummydir
