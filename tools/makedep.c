133d4cde265cee06c6cebfb8665e0ced6cf80498
Alexandre Julliard <julliard@winehq.org>
2010-03-16 13:27:51 +0100

    makedep: Output source dependency for bison files needed for parallel makes.

--- tools/makedep.c
+++ tools/makedep.c
@@ -262,6 +262,18 @@ static INCL_FILE *find_src_file( const char *name )
 }
 
 /*******************************************************************
+ *         find_include_file
+ */
+static INCL_FILE *find_include_file( const char *name )
+{
+    INCL_FILE *file;
+
+    LIST_FOR_EACH_ENTRY( file, &includes, INCL_FILE, entry )
+        if (!strcmp( name, file->name )) return file;
+    return NULL;
+}
+
+/*******************************************************************
  *         add_include
  *
  * Add an include file if it doesn't already exists.
@@ -829,6 +841,10 @@ static int output_src( FILE *file, INCL_FILE *pFile, int *column )
         *ext++ = 0;
         if (!strcmp( ext, "y" ))  /* yacc file */
         {
+            /* add source file dependency for parallel makes */
+            char *header = strmake( "%s.tab.h", obj );
+            if (find_include_file( header )) fprintf( file, "%s.tab.c: %s\n", obj, header );
+            free( header );
             *column += fprintf( file, "%s.tab.o: %s.tab.c", obj, obj );
         }
         else if (!strcmp( ext, "l" ))  /* lex file */
