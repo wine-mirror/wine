da00e1558e8c52e69fb5ed3e6588dcdd2f9fc7d0
Francois Gouget <fgouget@free.fr>
2010-03-20 15:25:14 +0100

    hlink/tests: Add a trailing '\n' to an ok() call.

--- dlls/hlink/tests/browse_ctx.c
+++ dlls/hlink/tests/browse_ctx.c
@@ -123,7 +123,7 @@ static void test_BrowseWindowInfo(void)
 
     hres = IHlinkBrowseContext_GetBrowseWindowInfo(bc, &bwinfo_get);
     ok(hres == S_OK, "GetBrowseWindowInfo failed: 0x%08x\n", hres);
-    ok(!memcmp(&bwinfo_set, &bwinfo_get, sizeof(HLBWINFO)), "Set and Get differ");
+    ok(!memcmp(&bwinfo_set, &bwinfo_get, sizeof(HLBWINFO)), "Set and Get differ\n");
 }
 
 START_TEST(browse_ctx)
