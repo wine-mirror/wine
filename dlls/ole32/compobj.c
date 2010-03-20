975c820fce6401aae6aaffb3711028ec12933c4e
Vincent Povirk <vincent@codeweavers.com>
2010-03-20 13:40:22 -0500

    ole32: Don't register an IMallocSpy object when the ole trace is used.
    There's no need for this. Any useful information we could get out of it
    is availble from the olemalloc channel, and it means that the ole channel
    changes behavior in a way that's visible to programs.

--- dlls/ole32/compobj.c
+++ dlls/ole32/compobj.c
@@ -4153,11 +4153,9 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID fImpLoad)
     case DLL_PROCESS_ATTACH:
         hProxyDll = hinstDLL;
         COMPOBJ_InitProcess();
-	if (TRACE_ON(ole)) CoRegisterMallocSpy((LPVOID)-1);
 	break;
 
     case DLL_PROCESS_DETACH:
-        if (TRACE_ON(ole)) CoRevokeMallocSpy();
         OLEDD_UnInitialize();
         COMPOBJ_UninitProcess();
         RPC_UnregisterAllChannelHooks();
