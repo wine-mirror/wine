83680cd094c869ce0d8d04aaccfe89861a11c1f7
Christian Costa <titan.costa@wanadoo.fr>
2010-04-06 21:45:02 +0200

    d3dx9_36: Fix indentation.

--- dlls/d3dx9_36/mesh.c
+++ dlls/d3dx9_36/mesh.c
@@ -57,13 +57,13 @@ done we've got an intersection of the ray with the box.
     div = 1.0f / praydirection->x;
     if ( div >= 0.0f )
     {
-     tmin = ( pmin->x - prayposition->x ) * div;
-     tmax = ( pmax->x - prayposition->x ) * div;
+        tmin = ( pmin->x - prayposition->x ) * div;
+        tmax = ( pmax->x - prayposition->x ) * div;
     }
     else
     {
-     tmin = ( pmax->x - prayposition->x ) * div;
-     tmax = ( pmin->x - prayposition->x ) * div;
+        tmin = ( pmax->x - prayposition->x ) * div;
+        tmax = ( pmin->x - prayposition->x ) * div;
     }
 
     if ( tmax < 0.0f ) return FALSE;
@@ -71,13 +71,13 @@ done we've got an intersection of the ray with the box.
     div = 1.0f / praydirection->y;
     if ( div >= 0.0f )
     {
-     tymin = ( pmin->y - prayposition->y ) * div;
-     tymax = ( pmax->y - prayposition->y ) * div;
+        tymin = ( pmin->y - prayposition->y ) * div;
+        tymax = ( pmax->y - prayposition->y ) * div;
     }
     else
     {
-     tymin = ( pmax->y - prayposition->y ) * div;
-     tymax = ( pmin->y - prayposition->y ) * div;
+        tymin = ( pmax->y - prayposition->y ) * div;
+        tymax = ( pmin->y - prayposition->y ) * div;
     }
 
     if ( ( tymax < 0.0f ) || ( tmin > tymax ) || ( tymin > tmax ) ) return FALSE;
@@ -88,13 +88,13 @@ done we've got an intersection of the ray with the box.
     div = 1.0f / praydirection->z;
     if ( div >= 0.0f )
     {
-     tzmin = ( pmin->z - prayposition->z ) * div;
-     tzmax = ( pmax->z - prayposition->z ) * div;
+        tzmin = ( pmin->z - prayposition->z ) * div;
+        tzmax = ( pmax->z - prayposition->z ) * div;
     }
     else
     {
-     tzmin = ( pmax->z - prayposition->z ) * div;
-     tzmax = ( pmin->z - prayposition->z ) * div;
+        tzmin = ( pmax->z - prayposition->z ) * div;
+        tzmax = ( pmin->z - prayposition->z ) * div;
     }
 
     if ( (tzmax < 0.0f ) || ( tmin > tzmax ) || ( tzmin > tmax ) ) return FALSE;
@@ -180,7 +180,7 @@ UINT WINAPI D3DXGetFVFVertexSize(DWORD FVF)
     UINT i;
     UINT numTextures = (FVF & D3DFVF_TEXCOUNT_MASK) >> D3DFVF_TEXCOUNT_SHIFT;
 
-    if (FVF & D3DFVF_NORMAL) size +=  sizeof(D3DXVECTOR3);
+    if (FVF & D3DFVF_NORMAL) size += sizeof(D3DXVECTOR3);
     if (FVF & D3DFVF_DIFFUSE) size += sizeof(DWORD);
     if (FVF & D3DFVF_SPECULAR) size += sizeof(DWORD);
     if (FVF & D3DFVF_PSIZE) size += sizeof(DWORD);
