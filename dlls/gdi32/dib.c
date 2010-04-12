wine-mirror/wine

commit 2854884a428e222cbb9078d875068a72f25365c4

gdi32: Always create a context for DIB_RGB_COLORS.

dlls/gdi32/dib.c
@@ -336,17 +336,17 @@ INT WINAPI SetDIBits( HDC hdc, HBITMAP hbitmap, UINT startscan,
     BITMAPOBJ *bitmap;
     INT result = 0;
 
+    if (coloruse == DIB_RGB_COLORS) hdc = CreateCompatibleDC(0);
+
     if (!(dc = get_dc_ptr( hdc )))
-    {
-        if (coloruse == DIB_RGB_COLORS) FIXME( "shouldn't require a DC for DIB_RGB_COLORS\n" );
         return 0;
-    }
 
     update_dc( dc );
 
     if (!(bitmap = GDI_GetObjPtr( hbitmap, OBJ_BITMAP )))
     {
         release_dc_ptr( dc );
+        if (coloruse == DIB_RGB_COLORS) DeleteDC(hdc);
         return 0;
     }
 
@@ -365,6 +365,7 @@ INT WINAPI SetDIBits( HDC hdc, HBITMAP hbitmap, UINT startscan,
  done:
     GDI_ReleaseObj( hbitmap );
     release_dc_ptr( dc );
+    if (coloruse == DIB_RGB_COLORS) DeleteDC(hdc);
     return result;
 }
 

commit 352e2ff6eb5116d2ac91db9258ae288733e3a271

gdi32: Use supplied hdc for DIB_RGB_COLORS if it's valid.
This is about fixing regression introduced with 2854884a428e222cbb9078d875068a72f25365c4.
Initial patch fixed MSVS 2005 toolbars blackness, this one doesn't break a fix,
and fixes regressed Animation control glitches (and probably reported games related problems).

dlls/gdi32/dib.c
@@ -332,21 +332,26 @@ INT WINAPI SetDIBits( HDC hdc, HBITMAP hbitmap, UINT startscan,
 		      UINT lines, LPCVOID bits, const BITMAPINFO *info,
 		      UINT coloruse )
 {
-    DC *dc;
+    DC *dc = get_dc_ptr( hdc );
+    BOOL delete_hdc = FALSE;
     BITMAPOBJ *bitmap;
     INT result = 0;
 
-    if (coloruse == DIB_RGB_COLORS) hdc = CreateCompatibleDC(0);
+    if (coloruse == DIB_RGB_COLORS && !dc)
+    {
+        hdc = CreateCompatibleDC(0);
+        dc = get_dc_ptr( hdc );
+        delete_hdc = TRUE;
+    }
 
-    if (!(dc = get_dc_ptr( hdc )))
-        return 0;
+    if (!dc) return 0;
 
     update_dc( dc );
 
     if (!(bitmap = GDI_GetObjPtr( hbitmap, OBJ_BITMAP )))
     {
         release_dc_ptr( dc );
-        if (coloruse == DIB_RGB_COLORS) DeleteDC(hdc);
+        if (delete_hdc) DeleteDC(hdc);
         return 0;
     }
 
@@ -365,7 +370,7 @@ INT WINAPI SetDIBits( HDC hdc, HBITMAP hbitmap, UINT startscan,
  done:
     GDI_ReleaseObj( hbitmap );
     release_dc_ptr( dc );
-    if (coloruse == DIB_RGB_COLORS) DeleteDC(hdc);
+    if (delete_hdc) DeleteDC(hdc);
     return result;
 }
 