0ceed8beb17a69b14ff3b24d44a7c0d07c485846
Maarten Lankhorst <m.b.lankhorst@gmail.com>
2010-04-21 13:45:29 +0200

    winenas.drv: Clean up DriverProc.

--- dlls/winenas.drv/nas.c
+++ dlls/winenas.drv/nas.c
@@ -35,51 +35,29 @@
 #include "mmddk.h"
 #include "nas.h"
 
-#ifdef HAVE_NAS
-
-/**************************************************************************
- * 				NAS_drvOpen			[internal]
- */
-static LRESULT NAS_drvOpen(LPSTR str)
-{
-    return 1;
-}
-
-/**************************************************************************
- * 				NAS_drvClose			[internal]
- */
-static LRESULT NAS_drvClose(DWORD_PTR dwDevID)
-{
-    return 1;
-}
-#endif /* #ifdef HAVE_NAS */
-
-
 /**************************************************************************
  * 				DriverProc (WINENAS.@)
  */
 LRESULT CALLBACK NAS_DriverProc(DWORD_PTR dwDevID, HDRVR hDriv, UINT wMsg,
                                 LPARAM dwParam1, LPARAM dwParam2)
 {
-/* EPP     TRACE("(%08lX, %04X, %08lX, %08lX, %08lX)\n",  */
-/* EPP 	  dwDevID, hDriv, wMsg, dwParam1, dwParam2); */
-
     switch(wMsg) {
 #ifdef HAVE_NAS
     case DRV_LOAD:		NAS_WaveInit();
-/*    				NAS_MidiInit(); FIXME: no midi */
 				return 1;
     case DRV_FREE:	        return NAS_WaveClose();
-    case DRV_OPEN:		return NAS_drvOpen((LPSTR)dwParam1);
-    case DRV_CLOSE:		return NAS_drvClose(dwDevID);
-    case DRV_ENABLE:		return 1;
-    case DRV_DISABLE:		return 1;
-    case DRV_QUERYCONFIGURE:	return 1;
+    case DRV_OPEN:
+    case DRV_CLOSE:
+    case DRV_ENABLE:
+    case DRV_DISABLE:
+    case DRV_QUERYCONFIGURE:
+        return 1;
     case DRV_CONFIGURE:		MessageBoxA(0, "NAS MultiMedia Driver !", "NAS Driver", MB_OK);	return 1;
-    case DRV_INSTALL:		return DRVCNF_RESTART;
-    case DRV_REMOVE:		return DRVCNF_RESTART;
+    case DRV_INSTALL:
+    case DRV_REMOVE:
+        return DRV_SUCCESS;
 #endif
     default:
-	return DefDriverProc(dwDevID, hDriv, wMsg, dwParam1, dwParam2);
+	return 0;
     }
 }
