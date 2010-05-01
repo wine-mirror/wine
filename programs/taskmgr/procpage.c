2aa11ff62eb1b1c32f9cba17449f72cd726ab386
Gerald Pfeifer <gerald@pfeifer.com>
2010-05-01 16:48:48 +0200

    taskmgr: Shed one parameter and one local variable from ProcessPageOnNotify.

--- programs/taskmgr/procpage.c
+++ programs/taskmgr/procpage.c
@@ -148,9 +148,8 @@ static void ProcessPageShowContextMenu(DWORD dwProcessId)
     DestroyMenu(hMenu);
 }
 
-static void ProcessPageOnNotify(WPARAM wParam, LPARAM lParam)
+static void ProcessPageOnNotify(LPARAM lParam)
 {
-    int                idctrl;
     LPNMHDR            pnmh;
     LPNMLISTVIEW       pnmv;
     NMLVDISPINFOW*     pnmdi;
@@ -163,7 +162,6 @@ static void ProcessPageOnNotify(WPARAM wParam, LPARAM lParam)
     static const WCHAR wszFmt02D[] = {'%','0','2','d',0};
     static const WCHAR wszUnitK[] = {' ','K',0};
 
-    idctrl = (int) wParam;
     pnmh = (LPNMHDR) lParam;
     pnmv = (LPNMLISTVIEW) lParam;
     pnmdi = (NMLVDISPINFOW*) lParam;
@@ -542,8 +540,7 @@ ProcessPageWndProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         break;
 
     case WM_NOTIFY:
-
-        ProcessPageOnNotify(wParam, lParam);
+        ProcessPageOnNotify(lParam);
         break;
     }
 
