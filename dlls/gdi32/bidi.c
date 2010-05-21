ec62c33d79dd99e4ced80a1f248b1d6d98366b8a
Aric Stewart <aric@codeweavers.com>
2010-05-21 15:11:34 -0500

    gdi32: Handle a BiDi string being shaped into more or less glyphs than characters in the string.

--- dlls/gdi32/bidi.c
+++ dlls/gdi32/bidi.c
@@ -352,7 +352,8 @@ BOOL BIDI_Reorder(
                 LPWSTR lpOutString, /* [out] Reordered string */
                 INT uCountOut,  /* [in] Size of output buffer */
                 UINT *lpOrder, /* [out] Logical -> Visual order map */
-                WORD **lpGlyphs /* [out] reordered, mirrored, shaped glyphs to display */
+                WORD **lpGlyphs, /* [out] reordered, mirrored, shaped glyphs to display */
+                INT *cGlyphs /* [out] number of glyphs generated */
     )
 {
     WORD *chartype;
@@ -637,6 +638,8 @@ BOOL BIDI_Reorder(
 
         done += i;
     }
+    if (cGlyphs)
+        *cGlyphs = glyph_i;
 
     HeapFree(GetProcessHeap(), 0, chartype);
     HeapFree(GetProcessHeap(), 0, levels);
