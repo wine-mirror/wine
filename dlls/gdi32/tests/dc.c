b4d58fe58faf960f6914a91b4f9ca0052f90689a
Andrew Nguyen <anguyen@codeweavers.com>
2010-06-15 00:26:20 -0500

    winex11.drv: Report a non-zero COLORRES capability value.

--- dlls/gdi32/tests/dc.c
+++ dlls/gdi32/tests/dc.c
@@ -556,6 +556,45 @@ static void test_boundsrect_invalid(void)
     DeleteDC(hdc);
 }
 
+static void test_desktop_colorres(void)
+{
+    HDC hdc = GetDC(NULL);
+    int bitspixel, colorres;
+
+    bitspixel = GetDeviceCaps(hdc, BITSPIXEL);
+    ok(bitspixel != 0, "Expected to get valid BITSPIXEL capability value\n");
+
+    colorres = GetDeviceCaps(hdc, COLORRES);
+    ok(colorres != 0 ||
+       broken(colorres == 0), /* Win9x */
+       "Expected to get valid COLORRES capability value\n");
+
+    if (colorres)
+    {
+        switch (bitspixel)
+        {
+        case 8:
+            ok(colorres == 18,
+               "Expected COLORRES to be 18, got %d\n", colorres);
+            break;
+        case 16:
+            ok(colorres == 16,
+               "Expected COLORRES to be 16, got %d\n", colorres);
+            break;
+        case 24:
+        case 32:
+            ok(colorres == 24,
+               "Expected COLORRES to be 24, got %d\n", bitspixel);
+            break;
+        default:
+            ok(0, "Got unknown BITSPIXEL %d with COLORRES %d\n", bitspixel, colorres);
+            break;
+        }
+    }
+
+    DeleteDC(hdc);
+}
+
 START_TEST(dc)
 {
     test_savedc();
@@ -565,4 +604,5 @@ START_TEST(dc)
     test_DC_bitmap();
     test_DeleteDC();
     test_boundsrect_invalid();
+    test_desktop_colorres();
 }
