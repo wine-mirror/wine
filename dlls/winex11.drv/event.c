02674b2b95a4a9125af44470c14ccbcef9608c8d
Paul Chitescu <paulc@voip.null.ro>
2010-06-16 11:06:15 +0300

    winex11.drv: Post a WM_SYSCOMMAND SC_CLOSE when the window manager asks to close a window.

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -553,11 +553,7 @@ static void handle_wm_protocols( HWND hwnd, XClientMessageEvent *event )
                 }
             }
 
-            /* Simulate pressing Alt+F4 */
-            keybd_event(VK_MENU, 0, 0, 0);
-            keybd_event(VK_F4, 0, 0, 0);
-            keybd_event(VK_F4, 0, KEYEVENTF_KEYUP, 0);
-            keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0);
+            PostMessageW( hwnd, WM_SYSCOMMAND, SC_CLOSE, 0 );
         }
     }
     else if (protocol == x11drv_atom(WM_TAKE_FOCUS))
