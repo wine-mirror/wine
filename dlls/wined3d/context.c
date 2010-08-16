wine-mirror/wine

commit 20f51c29a99299384e840e148f02aae66bfb45be

wined3d: Setup target if offscreen rendering setting changed.

dlls/wined3d/context.c
@@ -2242,8 +2242,8 @@ static void context_setup_target(IWineD3DDeviceImpl *device,
     const struct StateEntry *StateTable = device->StateTable;
 
     if (!target) return;
-    else if (context->current_rt == target) return;
     render_offscreen = surface_is_offscreen(target);
+    if (context->current_rt == target && render_offscreen == old_render_offscreen) return;
 
     context_set_render_offscreen(context, StateTable, render_offscreen);
 

dlls/wined3d/swapchain.c
@@ -334,9 +334,6 @@ static HRESULT WINAPI IWineD3DSwapChainImpl_Present(IWineD3DSwapChain *iface, CO
         surface_load_location(This->back_buffers[0], SFLAG_INTEXTURE, NULL);
         surface_modify_location(This->back_buffers[0], SFLAG_INDRAWABLE, FALSE);
         This->render_to_fbo = TRUE;
-
-        /* Force the context manager to update the render target configuration next draw. */
-        context->current_rt = NULL;
     }
 
     if(This->render_to_fbo)

commit e0528084b19a6f3c27d1af20aabfd512c0dca95b

wined3d: Call set_render_offscreen at the end of context_setup_target.
This fixes a regression caused by 20f51c29a99299384e840e148f02aae66bfb45be.

dlls/wined3d/context.c
@@ -2278,8 +2278,6 @@ static void context_setup_target(IWineD3DDeviceImpl *device,
     render_offscreen = surface_is_offscreen(target);
     if (context->current_rt == target && render_offscreen == old_render_offscreen) return;
 
-    context_set_render_offscreen(context, StateTable, render_offscreen);
-
     /* To compensate the lack of format switching with some offscreen rendering methods and on onscreen buffers
      * the alpha blend state changes with different render target formats. */
     if (!context->current_rt)
@@ -2347,6 +2345,7 @@ static void context_setup_target(IWineD3DDeviceImpl *device,
 
     context->draw_buffer_dirty = TRUE;
     context->current_rt = target;
+    context_set_render_offscreen(context, StateTable, render_offscreen);
 }
 
 /*****************************************************************************