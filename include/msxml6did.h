3780b082398a7d324729a9dbde411157286d3c40
Nikolay Sivov <nsivov@codeweavers.com>
2010-08-30 01:29:29 +0400

    include: Add xml DOMDocument60 definition.

---
+++ include/msxml6did.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2010 Nikolay Sivov for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef __MSXML6DID_H__
+#define __MSXML6DID_H__
+
+#define DISPID_XMLDOM_DOCUMENT3                0x000000ce
+#define DISPID_XMLDOM_DOCUMENT3_VALIDATENODE   0x000000cf
+#define DISPID_XMLDOM_DOCUMENT3_IMPORTNODE     0x000000d0
+#define DISPID_XMLDOM_DOCUMENT3__TOP           0x000000d1
+
+#endif /* __MSXML6DID_H__ */
