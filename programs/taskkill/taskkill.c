8e540ed26fb934999800c8d5e2c2afbeaef920a6
Andrew Nguyen <anguyen@codeweavers.com>
2010-09-21 02:17:09 -0500

    taskkill: Add stub implementation.

---
+++ programs/taskkill/taskkill.c
@@ -0,0 +1,31 @@
+/*
+ * Task termination utility
+ *
+ * Copyright 2010 Andrew Nguyen
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <windows.h>
+#include <wine/debug.h>
+#include <wine/unicode.h>
+
+WINE_DEFAULT_DEBUG_CHANNEL(taskkill);
+
+int wmain(int argc, WCHAR *argv[])
+{
+    WINE_FIXME("taskkill.exe is not implemented\n");
+    return 0;
+}
