7ccf6c28b31ed2486600e0b23649d3fecaab0248
Damjan Jovanovic <damjan.jov@gmail.com>
2010-10-08 07:21:23 +0200

    comdlg32: Fix a stack overflow.

--- dlls/comdlg32/filedlg.c
+++ dlls/comdlg32/filedlg.c
@@ -3349,6 +3349,9 @@ static int FILEDLG95_LOOKIN_InsertItemAfterParent(HWND hwnd,LPITEMIDLIST pidl)
 
   TRACE("\n");
 
+  if (pidl == pidlParent)
+    return -1;
+
   iParentPos = FILEDLG95_LOOKIN_SearchItem(hwnd,(WPARAM)pidlParent,SEARCH_PIDL);
 
   if(iParentPos < 0)
