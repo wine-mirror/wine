d369136a9664900d7bea3b47a7fb835e158a35b6
Henri Verbeet <hverbeet@codeweavers.com>
2010-10-08 13:12:16 +0200

    wined3d: Recognize the SM4 imul opcode.

--- dlls/wined3d/shader.c
+++ dlls/wined3d/shader.c
@@ -70,6 +70,7 @@ static const char * const shader_opcode_names[] =
     /* WINED3DSIH_IF            */ "if",
     /* WINED3DSIH_IFC           */ "ifc",
     /* WINED3DSIH_IGE           */ "ige",
+    /* WINED3DSIH_IMUL          */ "imul",
     /* WINED3DSIH_LABEL         */ "label",
     /* WINED3DSIH_LIT           */ "lit",
     /* WINED3DSIH_LOG           */ "log",
