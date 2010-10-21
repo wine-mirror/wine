wine-mirror/wine

commit 776a8662f088e75e88284cc0fc8e72d768771a6f

msxml3: Expose the XML parser SAX callback interface.
This eliminates a few function calls; libxml will call this indirectly
through the old method.  By writing our own callback functions or
wrapping the default ones we can manipulate the xmlDoc as it's being
parsed, allowing us to match the way msxml parses.

dlls/msxml3/domdoc.c
@@ -50,6 +50,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(msxml);
 
 #include <libxml/xpathInternals.h>
 #include <libxml/xmlsave.h>
+#include <libxml/SAX2.h>
 
 /* not defined in older versions */
 #define XML_SAVE_FORMAT     1
@@ -287,21 +288,47 @@ xmlNodePtr xmldoc_unlink_xmldecl(xmlDocPtr doc)
     return node;
 }
 
-static xmlDocPtr doparse( char *ptr, int len, const char *encoding )
+static xmlDocPtr doparse(domdoc* This, char *ptr, int len)
 {
     xmlDocPtr doc;
+    static xmlSAXHandler sax_handler = {
+        xmlSAX2InternalSubset,          /* internalSubset */
+        xmlSAX2IsStandalone,            /* isStandalone */
+        xmlSAX2HasInternalSubset,       /* hasInternalSubset */
+        xmlSAX2HasExternalSubset,       /* hasExternalSubset */
+        xmlSAX2ResolveEntity,           /* resolveEntity */
+        xmlSAX2GetEntity,               /* getEntity */
+        xmlSAX2EntityDecl,              /* entityDecl */
+        xmlSAX2NotationDecl,            /* notationDecl */
+        xmlSAX2AttributeDecl,           /* attributeDecl */
+        xmlSAX2ElementDecl,             /* elementDecl */
+        xmlSAX2UnparsedEntityDecl,      /* unparsedEntityDecl */
+        xmlSAX2SetDocumentLocator,      /* setDocumentLocator */
+        xmlSAX2StartDocument,           /* startDocument */
+        xmlSAX2EndDocument,             /* endDocument */
+        xmlSAX2StartElement,            /* startElement */
+        xmlSAX2EndElement,              /* endElement */
+        xmlSAX2Reference,               /* reference */
+        xmlSAX2Characters,              /* characters */
+        NULL,                           /* TODO: ignorableWhitespace */
+        xmlSAX2ProcessingInstruction,   /* processingInstruction */
+        xmlSAX2Comment,                 /* comment */
+        NULL,                           /* TODO: warning */
+        NULL,                           /* TODO: error */
+        NULL,                           /* TODO: fatalError */
+        xmlSAX2GetParameterEntity,      /* getParameterEntity */
+        xmlSAX2CDataBlock,              /* cdataBlock */
+        xmlSAX2ExternalSubset,          /* externalSubset */
+        0,                              /* initialized */
+        NULL,                           /* _private */
+        xmlSAX2StartElementNs,          /* startElementNs */
+        xmlSAX2EndElementNs,            /* endElementNs */
+        NULL                            /* TODO: serror */
+    };
 
-#ifdef HAVE_XMLREADMEMORY
-    /*
-     * use xmlReadMemory if possible so we can suppress
-     * writing errors to stderr
-     */
-    doc = xmlReadMemory( ptr, len, NULL, encoding,
-                           XML_PARSE_NOERROR | XML_PARSE_NOWARNING | XML_PARSE_NOBLANKS );
-#else
-    doc = xmlParseMemory( ptr, len );
-#endif
+    doc = xmlSAXParseMemoryWithData(&sax_handler, ptr, len, 0, This);
 
+    /* TODO: put this in one of the SAX callbacks */
     /* create first child as a <?xml...?> */
     if (doc && doc->standalone != -1)
     {
@@ -531,7 +558,7 @@ static HRESULT WINAPI domdoc_IPersistStreamInit_Load(
     len = GlobalSize(hglobal);
     ptr = GlobalLock(hglobal);
     if (len != 0)
-        xmldoc = doparse(ptr, len, NULL);
+        xmldoc = doparse(This, ptr, len);
     GlobalUnlock(hglobal);
 
     if (!xmldoc)
@@ -1718,7 +1745,7 @@ static HRESULT domdoc_onDataAvailable(void *obj, char *ptr, DWORD len)
     domdoc *This = obj;
     xmlDocPtr xmldoc;
 
-    xmldoc = doparse( ptr, len, NULL );
+    xmldoc = doparse(This, ptr, len);
     if(xmldoc) {
         xmldoc->_private = create_priv();
         return attach_xmldoc(This, xmldoc);
@@ -1956,7 +1983,7 @@ static HRESULT WINAPI domdoc_loadXML(
 
         if ( bstrXML && bstr_to_utf8( bstrXML, &str, &len ) )
         {
-            xmldoc = doparse( str, len, "UTF-8" );
+            xmldoc = doparse(This, str, len);
             heap_free( str );
             if ( !xmldoc )
                 This->error = E_FAIL;

commit fd9bcfcfe812c0c4f13f9c081e8d029d3966b605

msxml3: Fix a regression bug in domdoc_loadXML().
Caused by commit 776a8662f088e75e88284cc0fc8e72d768771a6f. Fixes parse
failures for BSTR's where encoding is explicitly UTF-16.

dlls/msxml3/domdoc.c
@@ -51,6 +51,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(msxml);
 #include <libxml/xpathInternals.h>
 #include <libxml/xmlsave.h>
 #include <libxml/SAX2.h>
+#include <libxml/parserInternals.h>
 
 /* not defined in older versions */
 #define XML_SAVE_FORMAT     1
@@ -321,9 +322,10 @@ static void sax_characters(void *ctx, const xmlChar *ch, int len)
     xmlSAX2Characters(ctx, ch, len);
 }
 
-static xmlDocPtr doparse(domdoc* This, char *ptr, int len)
+static xmlDocPtr doparse(domdoc* This, char *ptr, int len, xmlChar const* encoding)
 {
-    xmlDocPtr doc;
+    xmlDocPtr doc = NULL;
+    xmlParserCtxtPtr pctx;
     static xmlSAXHandler sax_handler = {
         xmlSAX2InternalSubset,          /* internalSubset */
         xmlSAX2IsStandalone,            /* isStandalone */
@@ -358,8 +360,33 @@ static xmlDocPtr doparse(domdoc* This, char *ptr, int len)
         xmlSAX2EndElementNs,            /* endElementNs */
         NULL                            /* TODO: serror */
     };
+    xmlInitParser();
 
-    doc = xmlSAXParseMemoryWithData(&sax_handler, ptr, len, 0, This);
+    pctx = xmlCreateMemoryParserCtxt(ptr, len);
+    if (!pctx)
+    {
+        ERR("Failed to create parser context\n");
+        return NULL;
+    }
+
+	if (pctx->sax) xmlFree(pctx->sax);
+    pctx->sax = &sax_handler;
+	pctx->_private = This;
+    pctx->recovery = 0;
+    pctx->encoding = xmlStrdup(encoding);
+    xmlParseDocument(pctx);
+
+    if (pctx->wellFormed)
+    {
+        doc = pctx->myDoc;
+    }
+    else
+    {
+       xmlFreeDoc(pctx->myDoc);
+       pctx->myDoc = NULL;
+    }
+    pctx->sax = NULL;
+    xmlFreeParserCtxt(pctx);
 
     /* TODO: put this in one of the SAX callbacks */
     /* create first child as a <?xml...?> */
@@ -597,7 +624,7 @@ static HRESULT WINAPI domdoc_IPersistStreamInit_Load(
     len = GlobalSize(hglobal);
     ptr = GlobalLock(hglobal);
     if (len != 0)
-        xmldoc = doparse(This, ptr, len);
+        xmldoc = doparse(This, ptr, len, NULL);
     GlobalUnlock(hglobal);
 
     if (!xmldoc)
@@ -1789,7 +1816,7 @@ static HRESULT domdoc_onDataAvailable(void *obj, char *ptr, DWORD len)
     domdoc *This = obj;
     xmlDocPtr xmldoc;
 
-    xmldoc = doparse(This, ptr, len);
+    xmldoc = doparse(This, ptr, len, NULL);
     if(xmldoc) {
         xmldoc->_private = create_priv();
         return attach_xmldoc(This, xmldoc);
@@ -2017,6 +2044,7 @@ static HRESULT WINAPI domdoc_loadXML(
     VARIANT_BOOL* isSuccessful )
 {
     domdoc *This = impl_from_IXMLDOMDocument3( iface );
+    static const xmlChar encoding[] = "UTF-8";
     xmlDocPtr xmldoc = NULL;
     HRESULT hr = S_FALSE, hr2;
     char *str;
@@ -2032,10 +2060,13 @@ static HRESULT WINAPI domdoc_loadXML(
 
         if ( bstrXML && bstr_to_utf8( bstrXML, &str, &len ) )
         {
-            xmldoc = doparse(This, str, len);
+            xmldoc = doparse(This, str, len, encoding);
             heap_free( str );
             if ( !xmldoc )
+            {
                 This->error = E_FAIL;
+                TRACE("failed to parse document\n");
+            }
             else
             {
                 hr = This->error = S_OK;