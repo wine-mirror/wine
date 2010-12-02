65c84d773f6026a32d9be52668ffccadb00d50b0
Peter Urbanec <winehq.org@urbanec.net>
2010-12-02 23:41:48 +1100

    opencl: Initial stub implementation of OpenCL 1.0.

---
+++ dlls/opencl/opencl.c
@@ -0,0 +1,30 @@
+/*
+ * OpenCL.dll proxy for native OpenCL implementation.
+ *
+ * Copyright 2010 Peter Urbanec
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "config.h"
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(opencl);
+
+#if defined(HAVE_CL_OPENCL_H)
+#include <CL/opencl.h>
+#elif defined(HAVE_OPENCL_OPENCL_H)
+#include <OpenCL/opencl.h>
+#endif
