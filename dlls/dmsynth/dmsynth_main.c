ca110e4d71e729334e3b71360d81da61022be426
Alexandre Julliard <julliard@winehq.org>
2010-12-03 13:52:05 +0100

    dmsynth: Convert dll registration to the IRegistrar mechanism.

--- dlls/dmsynth/dmsynth_main.c
+++ dlls/dmsynth/dmsynth_main.c
@@ -23,9 +23,11 @@
 #include <stdio.h>
 
 #include "dmsynth_private.h"
+#include "rpcproxy.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(dmsynth);
 
+static HINSTANCE instance;
 LONG DMSYNTH_refCount = 0;
 
 typedef struct {
@@ -137,6 +139,7 @@ static IClassFactoryImpl SynthSink_CF = {&SynthSinkCF_Vtbl};
  */
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) {
 	if (fdwReason == DLL_PROCESS_ATTACH) {
+                instance = hinstDLL;
 		DisableThreadLibraryCalls(hinstDLL);
 		/* FIXME: Initialisation */
 	} else if (fdwReason == DLL_PROCESS_DETACH) {
@@ -178,6 +181,22 @@ HRESULT WINAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, LPVOID *ppv)
 
     WARN("(%s,%s,%p): no interface found.\n", debugstr_dmguid(rclsid), debugstr_dmguid(riid), ppv);
     return CLASS_E_CLASSNOTAVAILABLE;
+}
+
+/***********************************************************************
+ *		DllRegisterServer (DMSYNTH.@)
+ */
+HRESULT WINAPI DllRegisterServer(void)
+{
+    return __wine_register_resources( instance, NULL );
+}
+
+/***********************************************************************
+ *		DllUnregisterServer (DMSYNTH.@)
+ */
+HRESULT WINAPI DllUnregisterServer(void)
+{
+    return __wine_unregister_resources( instance, NULL );
 }
 
 /******************************************************************
