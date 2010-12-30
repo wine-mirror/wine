b7e78af4de0559b2802ff10ab0414799155ada77
Jacek Caban <jacek@codeweavers.com>
2010-12-30 23:29:11 +0100

    mshtml: Added moz-safe-about to the list of Gecko internal protocols.

--- dlls/mshtml/nsio.c
+++ dlls/mshtml/nsio.c
@@ -2677,7 +2677,7 @@ static nsresult NSAPI nsIOService_GetProtocolFlags(nsIIOService *iface, const ch
 
 static BOOL is_gecko_special_uri(const char *spec)
 {
-    static const char *special_schemes[] = {"chrome:", "jar:", "resource:", "javascript:", "wyciwyg:"};
+    static const char *special_schemes[] = {"chrome:", "jar:", "moz-safe-about", "resource:", "javascript:", "wyciwyg:"};
     int i;
 
     for(i=0; i < sizeof(special_schemes)/sizeof(*special_schemes); i++) {
