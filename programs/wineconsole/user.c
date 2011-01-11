2e5c0a3acb0ca62dec67c30b9d734f68ab3b7a3e
Andrew Nguyen <anguyen@codeweavers.com>
2011-01-11 05:11:48 -0600

    wineconsole: Pass an output parameter when calling ReadConsoleOutputCharacterW.

--- programs/wineconsole/user.c
+++ programs/wineconsole/user.c
@@ -690,7 +690,9 @@ static void	WCUSER_CopySelectionToClipboard(const struct inner_data* data)
 	for (y = 0; y < h; y++, c.Y++)
 	{
 	    LPWSTR end;
-            ReadConsoleOutputCharacterW(data->hConOut, p, w - 1, c, NULL);
+	    DWORD count;
+
+	    ReadConsoleOutputCharacterW(data->hConOut, p, w - 1, c, &count);
 
 	    /* strip spaces from the end of the line */
 	    end = p + w - 1;
