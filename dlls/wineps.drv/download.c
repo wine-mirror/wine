69f88da1879cc850113e5f55d0cb85fd13ea2701
Alexander Kochetkov <al.kochet@gmail.com>
2011-01-14 03:18:19 +0300

    wineps.drv: Disable world transform for fonts in GM_COMPATIBLE mode.

--- dlls/wineps.drv/download.c
+++ dlls/wineps.drv/download.c
@@ -20,6 +20,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <math.h>
 #include <stdarg.h>
 #include <stdio.h>
 
@@ -270,6 +271,12 @@ BOOL PSDRV_WriteSetDownloadFont(PSDRV_PDEVICE *physDev)
 
     /* Retrieve the world -> device transform */
     GetTransform(physDev->hdc, 0x204, &xform);
+
+    if(GetGraphicsMode(physDev->hdc) == GM_COMPATIBLE)
+    {
+        xform.eM11 = xform.eM22 = fabs(xform.eM22);
+        xform.eM21 = xform.eM12 = 0;
+    }
 
     physDev->font.size.xx = ps_round(ppem * xform.eM11);
     physDev->font.size.xy = ps_round(ppem * xform.eM12);
