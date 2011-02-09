b667582a71a0a1258328d4cd094e057e263ef703
Austin English <austinenglish@gmail.com>
2011-02-08 16:06:02 -0800

    gdi32/tests: Make sure to use return value (LLVM/Clang).

--- dlls/gdi32/tests/path.c
+++ dlls/gdi32/tests/path.c
@@ -89,6 +89,7 @@ static void test_widenpath(void)
     Polyline(hdc, pnt, 6);
     EndPath(hdc);
     ret = WidenPath(hdc);
+    ok(ret == TRUE, "WidenPath failed: %d", GetLastError());
     nSize = GetPath(hdc, NULL, NULL, 0);
     ok(nSize > 6, "WidenPath should compute a widdened path with a 1px wide pen. Path length is %d, should be more than 6\n", nSize);
 
