5ef58c11ce28c963908672f5c5c1fde866272ba5
Dmitry Timoshkov <dmitry@codeweavers.com>
2011-03-22 15:11:40 +0800

    winex11.drv: Do not use any window types except _NET_WM_WINDOW_TYPE_NORMAL.
    Main window of the Atom Zombie Smasher game has WS_DLGFRAME style set,
    but Metacity allows making fullscreen ONLY normal windows, so do not
    use any window types except _NET_WM_WINDOW_TYPE_NORMAL.

--- dlls/winex11.drv/window.c
+++ dlls/winex11.drv/window.c
@@ -1169,17 +1169,7 @@ static void set_wm_hints( Display *display, struct x11drv_win_data *data )
     /* size hints */
     set_size_hints( display, data, style );
 
-    /* set the WM_WINDOW_TYPE */
-    if (style & WS_THICKFRAME) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_NORMAL);
-    else if (ex_style & WS_EX_APPWINDOW) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_NORMAL);
-    else if (style & WS_MINIMIZEBOX) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_NORMAL);
-    else if (style & WS_DLGFRAME) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_DIALOG);
-    else if (ex_style & WS_EX_DLGMODALFRAME) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_DIALOG);
-    /* many window managers don't handle utility windows very well, so we don't use TYPE_UTILITY here */
-    else if (ex_style & WS_EX_TOOLWINDOW) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_NORMAL);
-    else if ((style & WS_POPUP) && owner) window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_DIALOG);
-    else window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_NORMAL);
-
+    window_type = x11drv_atom(_NET_WM_WINDOW_TYPE_NORMAL);
     XChangeProperty(display, data->whole_window, x11drv_atom(_NET_WM_WINDOW_TYPE),
 		    XA_ATOM, 32, PropModeReplace, (unsigned char*)&window_type, 1);
 
