0adc1b60d872b5f06cf4a33bb9bfe1a783518a5d
Vincent Povirk <vincent@codeweavers.com>
2011-03-29 12:43:06 -0500

    winejack.drv: Fix build.

--- dlls/winejack.drv/audio.c
+++ dlls/winejack.drv/audio.c
@@ -34,6 +34,7 @@
  */
 
 #include "config.h"
+#include "wine/port.h"
 
 #include <stdlib.h>
 #include <stdarg.h>
@@ -47,6 +48,7 @@
 #include "winbase.h"
 #include "winnls.h"
 #include "wingdi.h"
+#include "winuser.h"
 #include "winerror.h"
 #include "mmddk.h"
 #include "dsound.h"
