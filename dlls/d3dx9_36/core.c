62c1fb0941a723b399f6f31e91d4a3cd10eb5574
Rico Schüller <kgbricola@web.de>
2011-03-30 12:51:40 +0200

    d3dx9: Remove typedef from ID3DXBufferImpl.

--- dlls/d3dx9_36/core.c
+++ dlls/d3dx9_36/core.c
@@ -30,23 +30,23 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(d3dx);
 
-typedef struct ID3DXBufferImpl
+struct ID3DXBufferImpl
 {
     ID3DXBuffer ID3DXBuffer_iface;
     LONG ref;
 
     DWORD *buffer;
     DWORD bufferSize;
-} ID3DXBufferImpl;
+};
 
-static inline ID3DXBufferImpl *impl_from_ID3DXBuffer(ID3DXBuffer *iface)
+static inline struct ID3DXBufferImpl *impl_from_ID3DXBuffer(ID3DXBuffer *iface)
 {
-    return CONTAINING_RECORD(iface, ID3DXBufferImpl, ID3DXBuffer_iface);
+    return CONTAINING_RECORD(iface, struct ID3DXBufferImpl, ID3DXBuffer_iface);
 }
 
 static HRESULT WINAPI ID3DXBufferImpl_QueryInterface(ID3DXBuffer *iface, REFIID riid, void **ppobj)
 {
-    ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
+    struct ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
 
     if (IsEqualGUID(riid, &IID_IUnknown)
       || IsEqualGUID(riid, &IID_ID3DXBuffer))
@@ -62,7 +62,7 @@ static HRESULT WINAPI ID3DXBufferImpl_QueryInterface(ID3DXBuffer *iface, REFIID 
 
 static ULONG WINAPI ID3DXBufferImpl_AddRef(ID3DXBuffer *iface)
 {
-    ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
+    struct ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
     ULONG ref = InterlockedIncrement(&This->ref);
 
     TRACE("(%p) : AddRef from %d\n", This, ref - 1);
@@ -72,7 +72,7 @@ static ULONG WINAPI ID3DXBufferImpl_AddRef(ID3DXBuffer *iface)
 
 static ULONG WINAPI ID3DXBufferImpl_Release(ID3DXBuffer *iface)
 {
-    ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
+    struct ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
     ULONG ref = InterlockedDecrement(&This->ref);
 
     TRACE("(%p) : ReleaseRef to %d\n", This, ref);
@@ -87,13 +87,13 @@ static ULONG WINAPI ID3DXBufferImpl_Release(ID3DXBuffer *iface)
 
 static LPVOID WINAPI ID3DXBufferImpl_GetBufferPointer(ID3DXBuffer *iface)
 {
-    ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
+    struct ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
     return This->buffer;
 }
 
 static DWORD WINAPI ID3DXBufferImpl_GetBufferSize(ID3DXBuffer *iface)
 {
-    ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
+    struct ID3DXBufferImpl *This = impl_from_ID3DXBuffer(iface);
     return This->bufferSize;
 }
 
@@ -108,7 +108,7 @@ const ID3DXBufferVtbl D3DXBuffer_Vtbl =
 
 HRESULT WINAPI D3DXCreateBuffer(DWORD NumBytes, LPD3DXBUFFER* ppBuffer)
 {
-    ID3DXBufferImpl *object;
+    struct ID3DXBufferImpl *object;
 
     object = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(*object));
     if (object == NULL)
