8f33a4d32d8383f9616297f973f3a5e7539688e6
Dylan Smith <dylan.ah.smith@gmail.com>
2011-04-19 13:59:46 -0400

    include: Add d3dx9mesh.h X template extensions.

--- include/d3dx9mesh.h
+++ include/d3dx9mesh.h
@@ -788,4 +788,170 @@ DEFINE_GUID(DXFILEOBJ_EffectInstance,           0xe331f7e4, 0x0559, 0x4cc2, 0x8e
 DEFINE_GUID(DXFILEOBJ_AnimTicksPerSecond,       0x9e415a43, 0x7ba6, 0x4a73, 0x87, 0x43, 0xb7, 0x3d, 0x47, 0xe8, 0x84, 0x76);
 DEFINE_GUID(DXFILEOBJ_CompressedAnimationSet,   0x7f9b00b3, 0xf125, 0x4890, 0x87, 0x6e, 0x1c, 0x42, 0xbf, 0x69, 0x7c, 0x4d);
 
+#define XSKINEXP_TEMPLATES \
+        "xof 0303txt 0032\
+        template XSkinMeshHeader \
+        { \
+            <3CF169CE-FF7C-44ab-93C0-F78F62D172E2> \
+            WORD nMaxSkinWeightsPerVertex; \
+            WORD nMaxSkinWeightsPerFace; \
+            WORD nBones; \
+        } \
+        template VertexDuplicationIndices \
+        { \
+            <B8D65549-D7C9-4995-89CF-53A9A8B031E3> \
+            DWORD nIndices; \
+            DWORD nOriginalVertices; \
+            array DWORD indices[nIndices]; \
+        } \
+        template FaceAdjacency \
+        { \
+            <A64C844A-E282-4756-8B80-250CDE04398C> \
+            DWORD nIndices; \
+            array DWORD indices[nIndices]; \
+        } \
+        template SkinWeights \
+        { \
+            <6F0D123B-BAD2-4167-A0D0-80224F25FABB> \
+            STRING transformNodeName; \
+            DWORD nWeights; \
+            array DWORD vertexIndices[nWeights]; \
+            array float weights[nWeights]; \
+            Matrix4x4 matrixOffset; \
+        } \
+        template Patch \
+        { \
+            <A3EB5D44-FC22-429D-9AFB-3221CB9719A6> \
+            DWORD nControlIndices; \
+            array DWORD controlIndices[nControlIndices]; \
+        } \
+        template PatchMesh \
+        { \
+            <D02C95CC-EDBA-4305-9B5D-1820D7704BBF> \
+            DWORD nVertices; \
+            array Vector vertices[nVertices]; \
+            DWORD nPatches; \
+            array Patch patches[nPatches]; \
+            [ ... ] \
+        } \
+        template PatchMesh9 \
+        { \
+            <B9EC94E1-B9A6-4251-BA18-94893F02C0EA> \
+            DWORD Type; \
+            DWORD Degree; \
+            DWORD Basis; \
+            DWORD nVertices; \
+            array Vector vertices[nVertices]; \
+            DWORD nPatches; \
+            array Patch patches[nPatches]; \
+            [ ... ] \
+        } template EffectFloats \
+        { \
+            <F1CFE2B3-0DE3-4e28-AFA1-155A750A282D> \
+            DWORD nFloats; \
+            array float Floats[nFloats]; \
+        } \
+        template EffectString \
+        { \
+            <D55B097E-BDB6-4c52-B03D-6051C89D0E42> \
+            STRING Value; \
+        } \
+        template EffectDWord \
+        { \
+            <622C0ED0-956E-4da9-908A-2AF94F3CE716> \
+            DWORD Value; \
+        } template EffectParamFloats \
+        { \
+            <3014B9A0-62F5-478c-9B86-E4AC9F4E418B> \
+            STRING ParamName; \
+            DWORD nFloats; \
+            array float Floats[nFloats]; \
+        } template EffectParamString \
+        { \
+            <1DBC4C88-94C1-46ee-9076-2C28818C9481> \
+            STRING ParamName; \
+            STRING Value; \
+        } \
+        template EffectParamDWord \
+        { \
+            <E13963BC-AE51-4c5d-B00F-CFA3A9D97CE5> \
+            STRING ParamName; \
+            DWORD Value; \
+        } \
+        template EffectInstance \
+        { \
+            <E331F7E4-0559-4cc2-8E99-1CEC1657928F> \
+            STRING EffectFilename; \
+            [ ... ] \
+        } template AnimTicksPerSecond \
+        { \
+            <9E415A43-7BA6-4a73-8743-B73D47E88476> \
+            DWORD AnimTicksPerSecond; \
+        } \
+        template CompressedAnimationSet \
+        { \
+            <7F9B00B3-F125-4890-876E-1C42BF697C4D> \
+            DWORD CompressedBlockSize; \
+            FLOAT TicksPerSec; \
+            DWORD PlaybackType; \
+            DWORD BufferLength; \
+            array DWORD CompressedData[BufferLength]; \
+        } "
+
+#define XEXTENSIONS_TEMPLATES \
+        "xof 0303txt 0032\
+        template FVFData \
+        { \
+            <B6E70A0E-8EF9-4e83-94AD-ECC8B0C04897> \
+            DWORD dwFVF; \
+            DWORD nDWords; \
+            array DWORD data[nDWords]; \
+        } \
+        template VertexElement \
+        { \
+            <F752461C-1E23-48f6-B9F8-8350850F336F> \
+            DWORD Type; \
+            DWORD Method; \
+            DWORD Usage; \
+            DWORD UsageIndex; \
+        } \
+        template DeclData \
+        { \
+            <BF22E553-292C-4781-9FEA-62BD554BDD93> \
+            DWORD nElements; \
+            array VertexElement Elements[nElements]; \
+            DWORD nDWords; \
+            array DWORD data[nDWords]; \
+        } \
+        template PMAttributeRange \
+        { \
+            <917E0427-C61E-4a14-9C64-AFE65F9E9844> \
+            DWORD iFaceOffset; \
+            DWORD nFacesMin; \
+            DWORD nFacesMax; \
+            DWORD iVertexOffset; \
+            DWORD nVerticesMin; \
+            DWORD nVerticesMax; \
+        } \
+        template PMVSplitRecord \
+        { \
+            <574CCC14-F0B3-4333-822D-93E8A8A08E4C> \
+            DWORD iFaceCLW; \
+            DWORD iVlrOffset; \
+            DWORD iCode; \
+        } \
+        template PMInfo \
+        { \
+            <B6C3E656-EC8B-4b92-9B62-681659522947> \
+            DWORD nAttributes; \
+            array PMAttributeRange attributeRanges[nAttributes]; \
+            DWORD nMaxValence; \
+            DWORD nMinLogicalVertices; \
+            DWORD nMaxLogicalVertices; \
+            DWORD nVSplits; \
+            array PMVSplitRecord splitRecords[nVSplits]; \
+            DWORD nAttributeMispredicts; \
+            array DWORD attributeMispredicts[nAttributeMispredicts]; \
+        } "
+
 #endif /* __WINE_D3DX9MESH_H */
