a247c7fd496302e10faff8f5b90feb7f71bdfed1
Marcus Meissner <meissner@suse.de>
2011-04-26 19:11:44 +0200

    sane.ds: Mark internal functions as hidden.

--- dlls/sane.ds/sane_i.h
+++ dlls/sane.ds/sane_i.h
@@ -52,7 +52,7 @@ MAKE_FUNCPTR(sane_strstatus)
 #include "winuser.h"
 #include "twain.h"
 
-extern HINSTANCE SANE_instance;
+extern HINSTANCE SANE_instance DECLSPEC_HIDDEN;
 
 #define TWCC_CHECKSTATUS     (TWCC_CUSTOMBASE + 1)
 
@@ -85,157 +85,157 @@ struct tagActiveDS
 } activeDS;
 
 /* Helper functions */
-extern TW_UINT16 SANE_SaneCapability (pTW_CAPABILITY pCapability, TW_UINT16 action);
-extern TW_UINT16 SANE_SaneSetDefaults (void);
+extern TW_UINT16 SANE_SaneCapability (pTW_CAPABILITY pCapability, TW_UINT16 action) DECLSPEC_HIDDEN;
+extern TW_UINT16 SANE_SaneSetDefaults (void) DECLSPEC_HIDDEN;
 
 /* Implementation of operation triplets
  * From Application to Source (Control Information) */
-TW_UINT16 SANE_CapabilityGet (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+TW_UINT16 SANE_CapabilityGet (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CapabilityGetCurrent
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CapabilityGetDefault
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CapabilityQuerySupport
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CapabilityReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CapabilitySet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CustomDSDataGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CustomDSDataSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_AutomaticCaptureDirectory
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ChangeDirectory
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemCopy
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_CreateDirectory
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemDelete
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FormatMedia
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemGetClose
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemGetFirstFile
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemGetInfo
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemGetNextFile
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_FileSystemRename
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ProcessEvent
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_PassThrough
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_PendingXfersEndXfer
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_PendingXfersGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_PendingXfersReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_PendingXfersStopFeeder
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXferGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXferGetDefault
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXferReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXferSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXfer2Get
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXfer2GetDefault
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXfer2Reset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupFileXfer2Set
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_SetupMemXferGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_GetDSStatus
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_DisableDSUserInterface
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_EnableDSUserInterface
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_EnableDSUIOnly
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_XferGroupGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_XferGroupSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 
 /* Implementation of operation triplets
  * From Application to Source (Image Information) */
 TW_UINT16 SANE_CIEColorGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ExtImageInfoGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_GrayResponseReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_GrayResponseSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageFileXferGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageInfoGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageLayoutGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageLayoutGetDefault
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageLayoutReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageLayoutSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageMemXferGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_ImageNativeXferGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_JPEGCompressionGet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_JPEGCompressionGetDefault
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_JPEGCompressionReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_JPEGCompressionSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_Palette8Get
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_Palette8GetDefault
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_Palette8Reset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_Palette8Set
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_RGBResponseReset
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 TW_UINT16 SANE_RGBResponseSet
-    (pTW_IDENTITY pOrigin, TW_MEMREF pData);
+    (pTW_IDENTITY pOrigin, TW_MEMREF pData) DECLSPEC_HIDDEN;
 
 /* UI function */
-BOOL DoScannerUI(void);
-HWND ScanningDialogBox(HWND dialog, LONG progress);
+BOOL DoScannerUI(void) DECLSPEC_HIDDEN;
+HWND ScanningDialogBox(HWND dialog, LONG progress) DECLSPEC_HIDDEN;
 
 /* Option functions */
 #ifdef SONAME_LIBSANE
-SANE_Status sane_option_get_int(SANE_Handle h, const char *option_name, SANE_Int *val);
-SANE_Status sane_option_set_int(SANE_Handle h, const char *option_name, SANE_Int val, SANE_Int *status);
-SANE_Status sane_option_get_str(SANE_Handle h, const char *option_name, SANE_String val, size_t len, SANE_Int *status);
-SANE_Status sane_option_set_str(SANE_Handle h, const char *option_name, SANE_String val, SANE_Int *status);
-SANE_Status sane_option_probe_resolution(SANE_Handle h, const char *option_name, SANE_Int *minval, SANE_Int *maxval, SANE_Int *quant);
-SANE_Status sane_option_probe_mode(SANE_Handle h, SANE_String_Const **choices, char *current, int current_size);
+SANE_Status sane_option_get_int(SANE_Handle h, const char *option_name, SANE_Int *val) DECLSPEC_HIDDEN;
+SANE_Status sane_option_set_int(SANE_Handle h, const char *option_name, SANE_Int val, SANE_Int *status) DECLSPEC_HIDDEN;
+SANE_Status sane_option_get_str(SANE_Handle h, const char *option_name, SANE_String val, size_t len, SANE_Int *status) DECLSPEC_HIDDEN;
+SANE_Status sane_option_set_str(SANE_Handle h, const char *option_name, SANE_String val, SANE_Int *status) DECLSPEC_HIDDEN;
+SANE_Status sane_option_probe_resolution(SANE_Handle h, const char *option_name, SANE_Int *minval, SANE_Int *maxval, SANE_Int *quant) DECLSPEC_HIDDEN;
+SANE_Status sane_option_probe_mode(SANE_Handle h, SANE_String_Const **choices, char *current, int current_size) DECLSPEC_HIDDEN;
 SANE_Status sane_option_probe_scan_area(SANE_Handle h, const char *option_name, SANE_Fixed *val,
-                                        SANE_Unit *unit, SANE_Fixed *min, SANE_Fixed *max, SANE_Fixed *quant);
-SANE_Status sane_option_get_bool(SANE_Handle h, const char *option_name, SANE_Bool *val, SANE_Int *status);
-SANE_Status sane_option_set_bool(SANE_Handle h, const char *option_name, SANE_Bool val, SANE_Int *status);
-SANE_Status sane_option_set_fixed(SANE_Handle h, const char *option_name, SANE_Fixed val, SANE_Int *status);
-TW_UINT16 sane_status_to_twcc(SANE_Status rc);
-BOOL convert_sane_res_to_twain(double sane_res, SANE_Unit unit, TW_FIX32 *twain_res, TW_UINT16 twtype);
+                                        SANE_Unit *unit, SANE_Fixed *min, SANE_Fixed *max, SANE_Fixed *quant) DECLSPEC_HIDDEN;
+SANE_Status sane_option_get_bool(SANE_Handle h, const char *option_name, SANE_Bool *val, SANE_Int *status) DECLSPEC_HIDDEN;
+SANE_Status sane_option_set_bool(SANE_Handle h, const char *option_name, SANE_Bool val, SANE_Int *status) DECLSPEC_HIDDEN;
+SANE_Status sane_option_set_fixed(SANE_Handle h, const char *option_name, SANE_Fixed val, SANE_Int *status) DECLSPEC_HIDDEN;
+TW_UINT16 sane_status_to_twcc(SANE_Status rc) DECLSPEC_HIDDEN;
+BOOL convert_sane_res_to_twain(double sane_res, SANE_Unit unit, TW_FIX32 *twain_res, TW_UINT16 twtype) DECLSPEC_HIDDEN;
 #endif
 
 
