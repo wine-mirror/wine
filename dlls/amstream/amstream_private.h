5c29f78e6c49434af7f117ae658e876942bcbf9c
Marcus Meissner <marcus@jet.franken.de>
2011-05-01 20:44:46 +0200

    amstream: Mark internal symbols as hidden.

--- dlls/amstream/amstream_private.h
+++ dlls/amstream/amstream_private.h
@@ -34,9 +34,9 @@
 #include "dshow.h"
 #include "mmstream.h"
 
-HRESULT AM_create(IUnknown *pUnkOuter, LPVOID *ppObj);
-HRESULT MediaStreamFilter_create(IUnknown *pUnkOuter, LPVOID *ppObj);
-HRESULT MediaStream_create(IMultiMediaStream* Parent, const MSPID* pPurposeId, STREAM_TYPE StreamType, IMediaStream** ppMediaStream);
-HRESULT DirectDrawMediaStream_create(IMultiMediaStream* Parent, const MSPID* pPurposeId, STREAM_TYPE StreamType, IMediaStream** ppMediaStream);
+HRESULT AM_create(IUnknown *pUnkOuter, LPVOID *ppObj) DECLSPEC_HIDDEN;
+HRESULT MediaStreamFilter_create(IUnknown *pUnkOuter, LPVOID *ppObj) DECLSPEC_HIDDEN;
+HRESULT MediaStream_create(IMultiMediaStream* Parent, const MSPID* pPurposeId, STREAM_TYPE StreamType, IMediaStream** ppMediaStream) DECLSPEC_HIDDEN;
+HRESULT DirectDrawMediaStream_create(IMultiMediaStream* Parent, const MSPID* pPurposeId, STREAM_TYPE StreamType, IMediaStream** ppMediaStream) DECLSPEC_HIDDEN;
 
 #endif /* __AMSTREAM_PRIVATE_INCLUDED__ */
