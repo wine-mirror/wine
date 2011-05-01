ea85bc950999d303bdc0a0706787e9a6d5003025
Marcus Meissner <marcus@jet.franken.de>
2011-05-01 14:51:29 +0200

    winex11.drv: Assertion was 1 off (Coverity).

--- dlls/winex11.drv/event.c
+++ dlls/winex11.drv/event.c
@@ -190,7 +190,7 @@ static inline void free_event_data( XEvent *event )
  */
 void X11DRV_register_event_handler( int type, x11drv_event_handler handler, const char *name )
 {
-    assert( type <= MAX_EVENT_HANDLERS );
+    assert( type < MAX_EVENT_HANDLERS );
     assert( !handlers[type] || handlers[type] == handler );
     handlers[type] = handler;
     event_names[type] = name;
