bda648189a1921b394cf449123e05630db35d47f
Marcus Meissner <meissner@suse.de>
2011-05-13 17:01:53 +0200

    dnsapi: Mark internal symbols with hidden visibility.

--- dlls/dnsapi/dnsapi.h
+++ dlls/dnsapi/dnsapi.h
@@ -141,11 +141,11 @@ static inline LPSTR dns_strdup_ua( const char *src )
     return dst;
 }
 
-const char *dns_type_to_str( unsigned short );
+const char *dns_type_to_str( unsigned short ) DECLSPEC_HIDDEN;
 
 #ifdef HAVE_RESOLV
-int dns_ns_initparse( const u_char *, int, ns_msg * );
-int dns_ns_parserr( ns_msg *, ns_sect, int, ns_rr * );
-int dns_ns_name_skip( const u_char **, const u_char * );
-int dns_ns_name_uncompress( const u_char *, const u_char *, const u_char *, char *, size_t );
+int dns_ns_initparse( const u_char *, int, ns_msg * ) DECLSPEC_HIDDEN;
+int dns_ns_parserr( ns_msg *, ns_sect, int, ns_rr * ) DECLSPEC_HIDDEN;
+int dns_ns_name_skip( const u_char **, const u_char * ) DECLSPEC_HIDDEN;
+int dns_ns_name_uncompress( const u_char *, const u_char *, const u_char *, char *, size_t ) DECLSPEC_HIDDEN;
 #endif
