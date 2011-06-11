f2ac486e21c64b664dea527e2621e3c1d830c2cc
Jay Yang <jkelleyy@gmail.com>
2011-06-11 17:42:17 -0400

    explorer: Implement the explorer using IExplorerBrowser.

--- programs/explorer/explorer.c
+++ programs/explorer/explorer.c
@@ -3,6 +3,7 @@
  *
  * Copyright 2004 CodeWeavers, Mike Hearn
  * Copyright 2005,2006 CodeWeavers, Aric Stewart
+ * Copyright 2011 Jay Yang
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -19,10 +20,30 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <windows.h>
+#define COBJMACROS
 
 #include "wine/unicode.h"
+#include "wine/debug.h"
 #include "explorer_private.h"
+#include "resource.h"
+
+#include <initguid.h>
+#include <windows.h>
+#include <shobjidl.h>
+#include <shlobj.h>
+
+
+WINE_DEFAULT_DEBUG_CHANNEL(explorer);
+
+#define EXPLORER_INFO_INDEX 0
+
+#define DEFAULT_WIDTH 640
+#define DEFAULT_HEIGHT 480
+
+
+static const WCHAR EXPLORER_CLASS[] = {'W','I','N','E','_','E','X','P','L','O','R','E','R','\0'};
+
+HINSTANCE explorer_hInstance;
 
 typedef struct parametersTAG {
     BOOL    explorer_mode;
@@ -30,8 +51,116 @@ typedef struct parametersTAG {
     WCHAR   selection[MAX_PATH];
 } parameters_struct;
 
+typedef struct
+{
+    IExplorerBrowser *browser;
+} explorer_info;
 
-static int CopyPathString(LPWSTR target, LPWSTR source)
+static void make_explorer_window(IShellFolder* startFolder)
+{
+    RECT explorerRect;
+    HWND window;
+    FOLDERSETTINGS fs;
+    explorer_info *info;
+    HRESULT hres;
+    WCHAR explorer_title[100];
+    LoadStringW(explorer_hInstance,IDS_EXPLORER_TITLE,explorer_title,
+                sizeof(explorer_title)/sizeof(WCHAR));
+    info = HeapAlloc(GetProcessHeap(),0,sizeof(explorer_info));
+    if(!info)
+    {
+        WINE_ERR("Could not allocate a explorer_info struct\n");
+        return;
+    }
+    hres = CoCreateInstance(&CLSID_ExplorerBrowser,NULL,CLSCTX_INPROC_SERVER,
+                            &IID_IExplorerBrowser,(LPVOID*)&info->browser);
+    if(!SUCCEEDED(hres))
+    {
+        WINE_ERR("Could not obtain an instance of IExplorerBrowser\n");
+        HeapFree(GetProcessHeap(),0,info);
+        return;
+    }
+
+    window = CreateWindowW(EXPLORER_CLASS,explorer_title,WS_OVERLAPPEDWINDOW,
+                           CW_USEDEFAULT,CW_USEDEFAULT,DEFAULT_WIDTH,
+                           DEFAULT_HEIGHT,NULL,NULL,explorer_hInstance,NULL);
+    fs.ViewMode = FVM_DETAILS;
+    fs.fFlags = FWF_AUTOARRANGE;
+    explorerRect.left = 0;
+    explorerRect.top = 0;
+    explorerRect.right = DEFAULT_WIDTH;
+    explorerRect.bottom = DEFAULT_HEIGHT;
+
+    IExplorerBrowser_Initialize(info->browser,window,&explorerRect,&fs);
+    IExplorerBrowser_SetOptions(info->browser,EBO_SHOWFRAMES);
+    SetWindowLongPtrW(window,EXPLORER_INFO_INDEX,(LONG_PTR)info);
+    IExplorerBrowser_BrowseToObject(info->browser,(IUnknown*)startFolder,
+                                    SBSP_ABSOLUTE);
+    ShowWindow(window,SW_SHOWDEFAULT);
+    UpdateWindow(window);
+}
+
+static void update_window_size(explorer_info *info, int height, int width)
+{
+    RECT new_rect;
+    new_rect.left = 0;
+    new_rect.top = 0;
+    new_rect.right = width;
+    new_rect.bottom = height;
+    IExplorerBrowser_SetRect(info->browser,NULL,new_rect);
+}
+
+static void do_exit(int code)
+{
+    OleUninitialize();
+    ExitProcess(code);
+}
+
+LRESULT CALLBACK explorer_wnd_proc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    explorer_info *info
+        = (explorer_info*)GetWindowLongPtrW(hwnd,EXPLORER_INFO_INDEX);
+    IExplorerBrowser *browser = NULL;
+
+    if(info)
+        browser = info->browser;
+    switch(uMsg)
+    {
+    case WM_DESTROY:
+        IExplorerBrowser_Release(browser);
+        HeapFree(GetProcessHeap(),0,info);
+        PostQuitMessage(0);
+        break;
+    case WM_QUIT:
+        do_exit(wParam);
+    case WM_SIZE:
+        update_window_size(info,HIWORD(lParam),LOWORD(lParam));
+        break;
+    default:
+        return DefWindowProcW(hwnd,uMsg,wParam,lParam);
+    }
+    return 0;
+}
+
+static void register_explorer_window_class(void)
+{
+    WNDCLASSEXW window_class;
+    window_class.cbSize = sizeof(WNDCLASSEXW);
+    window_class.style = 0;
+    window_class.cbClsExtra = 0;
+    window_class.cbWndExtra = sizeof(LONG_PTR);
+    window_class.lpfnWndProc = explorer_wnd_proc;
+    window_class.hInstance = explorer_hInstance;
+    window_class.hIcon = NULL;
+    window_class.hCursor = NULL;
+    window_class.hbrBackground = NULL;
+    window_class.lpszMenuName = NULL;
+    window_class.lpszClassName = EXPLORER_CLASS;
+    window_class.hIconSm = NULL;
+    RegisterClassExW(&window_class);
+}
+
+static int copy_path_string(LPWSTR target, LPWSTR source)
 {
     INT i = 0;
 
@@ -54,7 +183,7 @@ static int CopyPathString(LPWSTR target, LPWSTR source)
 }
 
 
-static void CopyPathRoot(LPWSTR root, LPWSTR path)
+static void copy_path_root(LPWSTR root, LPWSTR path)
 {
     LPWSTR p,p2;
     INT i = 0;
@@ -86,7 +215,7 @@ static void CopyPathRoot(LPWSTR root, LPWSTR path)
  * [/root,object] Specifies the root level of the view
  * [/select,object] parent folder is opened and specified object is selected
  */
-static void ParseCommandLine(LPWSTR commandline,parameters_struct *parameters)
+static void parse_command_line(LPWSTR commandline,parameters_struct *parameters)
 {
     static const WCHAR arg_n[] = {'/','n'};
     static const WCHAR arg_e[] = {'/','e',','};
@@ -113,15 +242,15 @@ static void ParseCommandLine(LPWSTR commandline,parameters_struct *parameters)
         else if (strncmpW(p, arg_root, sizeof(arg_root)/sizeof(WCHAR))==0)
         {
             p += sizeof(arg_root)/sizeof(WCHAR);
-            p+=CopyPathString(parameters->root,p);
+            p+=copy_path_string(parameters->root,p);
         }
         else if (strncmpW(p, arg_select, sizeof(arg_select)/sizeof(WCHAR))==0)
         {
             p += sizeof(arg_select)/sizeof(WCHAR);
-            p+=CopyPathString(parameters->selection,p);
+            p+=copy_path_string(parameters->selection,p);
             if (!parameters->root[0])
-                CopyPathRoot(parameters->root,
-                        parameters->selection);
+                copy_path_root(parameters->root,
+                               parameters->selection);
         }
         else if (strncmpW(p, arg_desktop, sizeof(arg_desktop)/sizeof(WCHAR))==0)
         {
@@ -136,7 +265,7 @@ static void ParseCommandLine(LPWSTR commandline,parameters_struct *parameters)
     if (p2 && *p2)
     {
         /* left over command line is generally the path to be opened */
-        CopyPathString(parameters->root,p2);
+        copy_path_string(parameters->root,p2);
     }
 }
 
@@ -145,56 +274,29 @@ int WINAPI wWinMain(HINSTANCE hinstance,
                     LPWSTR cmdline,
                     int cmdshow)
 {
-    STARTUPINFOW si;
-    PROCESS_INFORMATION info;
+
     parameters_struct   parameters;
-    BOOL rc;
-    static const WCHAR winefile[] = {'\\','w','i','n','e','f','i','l','e','.','e','x','e',0};
-    static const WCHAR space[] = {' ',0};
-    WCHAR app[MAX_PATH];
-    LPWSTR winefile_commandline = NULL;
-    DWORD len = 0;
+    HRESULT hres;
+    MSG msg;
+    IShellFolder *folder;
 
     memset(&parameters,0,sizeof(parameters));
-    memset(&si,0,sizeof(STARTUPINFOW));
-
-    ParseCommandLine(cmdline,&parameters);
-    len = GetSystemDirectoryW( NULL, 0 ) + sizeof(winefile)/sizeof(WCHAR);
-
-    if (parameters.selection[0]) len += lstrlenW(parameters.selection) + 2;
-    else if (parameters.root[0]) len += lstrlenW(parameters.root) + 3;
-
-    winefile_commandline = HeapAlloc(GetProcessHeap(),0,len*sizeof(WCHAR));
-    GetSystemDirectoryW( app, MAX_PATH - sizeof(winefile)/sizeof(WCHAR) );
-    strcatW( app, winefile );
-    strcpyW( winefile_commandline, app );
-
-    if (parameters.selection[0])
+    explorer_hInstance = hinstance;
+    parse_command_line(cmdline,&parameters);
+    hres = OleInitialize(NULL);
+    if(!SUCCEEDED(hres))
     {
-        lstrcatW(winefile_commandline,space);
-        lstrcatW(winefile_commandline,parameters.selection);
+        WINE_ERR("Could not initialize COM\n");
+        ExitProcess(EXIT_FAILURE);
     }
-    else if (parameters.root[0])
+    register_explorer_window_class();
+    SHGetDesktopFolder(&folder);
+    make_explorer_window(folder);
+    IShellFolder_Release(folder);
+    while(GetMessageW( &msg, NULL, 0, 0 ) != 0)
     {
-        lstrcatW(winefile_commandline,space);
-        lstrcatW(winefile_commandline,parameters.root);
-        if (winefile_commandline[lstrlenW(winefile_commandline)-1]!='\\')
-        {
-            static const WCHAR slash[] = {'\\',0};
-            lstrcatW(winefile_commandline,slash);
-        }
+	    TranslateMessage(&msg);
+	    DispatchMessageW(&msg);
     }
-
-    rc = CreateProcessW(app, winefile_commandline, NULL, NULL, FALSE, 0, NULL,
-                        parameters.root[0] ? parameters.root:NULL, &si, &info);
-
-    HeapFree(GetProcessHeap(),0,winefile_commandline);
-
-    if (!rc)
-        return 0;
-
-    CloseHandle(info.hThread);
-    WaitForSingleObject(info.hProcess,INFINITE);
-    CloseHandle(info.hProcess);
     return 0;
 }
