wine-mirror/wine

commit 96b758f7b37033bf382ce40dd3310965d3ac3f76

d3d: Map D3DRS_ZBIAS to WINED3DRS_DEPTHBIAS.
ZBIAS was removed in d3d9 and replaced with DEPTHBIAS. Currently
wined3d implements both independently which will cause trouble if we
want to set the depthbias value depending on the depth stencil format.

dlls/d3d8/device.c
@@ -1373,11 +1373,25 @@ static HRESULT WINAPI IDirect3DDevice8Impl_SetRenderState(IDirect3DDevice8 *ifac
 {
     IDirect3DDevice8Impl *This = impl_from_IDirect3DDevice8(iface);
     HRESULT hr;
+    union
+    {
+        DWORD d;
+        float f;
+    } wined3d_value;
 
     TRACE("iface %p, state %#x, value %#x.\n", iface, State, Value);
 
     wined3d_mutex_lock();
-    hr = IWineD3DDevice_SetRenderState(This->WineD3DDevice, State, Value);
+    switch (State)
+    {
+        case D3DRS_ZBIAS:
+            wined3d_value.f = Value / -16.0f;
+            hr = IWineD3DDevice_SetRenderState(This->WineD3DDevice, WINED3DRS_DEPTHBIAS, wined3d_value.d);
+            break;
+
+        default:
+            hr = IWineD3DDevice_SetRenderState(This->WineD3DDevice, State, Value);
+    }
     wined3d_mutex_unlock();
 
     return hr;
@@ -1388,11 +1402,25 @@ static HRESULT WINAPI IDirect3DDevice8Impl_GetRenderState(IDirect3DDevice8 *ifac
 {
     IDirect3DDevice8Impl *This = impl_from_IDirect3DDevice8(iface);
     HRESULT hr;
+    union
+    {
+        DWORD d;
+        float f;
+    } wined3d_value;
 
     TRACE("iface %p, state %#x, value %p.\n", iface, State, pValue);
 
     wined3d_mutex_lock();
-    hr = IWineD3DDevice_GetRenderState(This->WineD3DDevice, State, pValue);
+    switch (State)
+    {
+        case D3DRS_ZBIAS:
+            hr = IWineD3DDevice_GetRenderState(This->WineD3DDevice, WINED3DRS_DEPTHBIAS, &wined3d_value.d);
+            if (SUCCEEDED(hr)) *pValue = -wined3d_value.f * 16.0f;
+            break;
+
+        default:
+        hr = IWineD3DDevice_GetRenderState(This->WineD3DDevice, State, pValue);
+    }
     wined3d_mutex_unlock();
 
     return hr;

dlls/ddraw/device.c
@@ -2374,6 +2374,20 @@ IDirect3DDeviceImpl_7_GetRenderState(IDirect3DDevice7 *iface,
             hr = DDERR_INVALIDPARAMS;
             break;
 
+        case D3DRENDERSTATE_ZBIAS:
+        {
+            union
+            {
+                DWORD d;
+                float f;
+            } wined3d_value;
+
+            hr = IWineD3DDevice_GetRenderState(This->wineD3DDevice,
+                                               WINED3DRS_DEPTHBIAS,
+                                               &wined3d_value.d);
+            if (SUCCEEDED(hr)) *Value = -wined3d_value.f * 16.0f;
+        }
+
         default:
             if (RenderStateType >= D3DRENDERSTATE_STIPPLEPATTERN00
                     && RenderStateType <= D3DRENDERSTATE_STIPPLEPATTERN31)
@@ -2687,6 +2701,20 @@ IDirect3DDeviceImpl_7_SetRenderState(IDirect3DDevice7 *iface,
             WARN("Render state %#x is invalid in d3d7.\n", RenderStateType);
             hr = DDERR_INVALIDPARAMS;
             break;
+
+        case D3DRENDERSTATE_ZBIAS:
+        {
+            union
+            {
+                DWORD d;
+                float f;
+            } wined3d_value;
+            wined3d_value.f = Value / -16.0;
+            hr = IWineD3DDevice_SetRenderState(This->wineD3DDevice,
+                                               WINED3DRS_DEPTHBIAS,
+                                               wined3d_value.d);
+            break;
+        }
 
         default:
             if (RenderStateType >= D3DRENDERSTATE_STIPPLEPATTERN00

dlls/wined3d/state.c
@@ -1383,36 +1383,6 @@ static void state_linepattern(DWORD state, struct wined3d_stateblock *stateblock
     }
 }
 
-static void state_zbias(DWORD state, struct wined3d_stateblock *stateblock, struct wined3d_context *context)
-{
-    union {
-        DWORD d;
-        float f;
-    } tmpvalue;
-
-    if (stateblock->state.render_states[WINED3DRS_ZBIAS])
-    {
-        tmpvalue.d = stateblock->state.render_states[WINED3DRS_ZBIAS];
-        TRACE("ZBias value %f\n", tmpvalue.f);
-        glPolygonOffset(0, -tmpvalue.f);
-        checkGLcall("glPolygonOffset(0, -Value)");
-        glEnable(GL_POLYGON_OFFSET_FILL);
-        checkGLcall("glEnable(GL_POLYGON_OFFSET_FILL);");
-        glEnable(GL_POLYGON_OFFSET_LINE);
-        checkGLcall("glEnable(GL_POLYGON_OFFSET_LINE);");
-        glEnable(GL_POLYGON_OFFSET_POINT);
-        checkGLcall("glEnable(GL_POLYGON_OFFSET_POINT);");
-    } else {
-        glDisable(GL_POLYGON_OFFSET_FILL);
-        checkGLcall("glDisable(GL_POLYGON_OFFSET_FILL);");
-        glDisable(GL_POLYGON_OFFSET_LINE);
-        checkGLcall("glDisable(GL_POLYGON_OFFSET_LINE);");
-        glDisable(GL_POLYGON_OFFSET_POINT);
-        checkGLcall("glDisable(GL_POLYGON_OFFSET_POINT);");
-    }
-}
-
-
 static void state_normalize(DWORD state, struct wined3d_stateblock *stateblock, struct wined3d_context *context)
 {
     if(isStateDirty(context, STATE_VDECL)) {
@@ -5061,7 +5031,6 @@ const struct StateEntryTemplate misc_state_template[] = {
     { STATE_RENDER(WINED3DRS_SUBPIXEL),                   { STATE_RENDER(WINED3DRS_SUBPIXEL),                   state_subpixel      }, WINED3D_GL_EXT_NONE             },
     { STATE_RENDER(WINED3DRS_SUBPIXELX),                  { STATE_RENDER(WINED3DRS_SUBPIXELX),                  state_subpixelx     }, WINED3D_GL_EXT_NONE             },
     { STATE_RENDER(WINED3DRS_STIPPLEDALPHA),              { STATE_RENDER(WINED3DRS_STIPPLEDALPHA),              state_stippledalpha }, WINED3D_GL_EXT_NONE             },
-    { STATE_RENDER(WINED3DRS_ZBIAS),                      { STATE_RENDER(WINED3DRS_ZBIAS),                      state_zbias         }, WINED3D_GL_EXT_NONE             },
     { STATE_RENDER(WINED3DRS_STIPPLEENABLE),              { STATE_RENDER(WINED3DRS_STIPPLEENABLE),              state_stippleenable }, WINED3D_GL_EXT_NONE             },
     { STATE_RENDER(WINED3DRS_MIPMAPLODBIAS),              { STATE_RENDER(WINED3DRS_MIPMAPLODBIAS),              state_mipmaplodbias }, WINED3D_GL_EXT_NONE             },
     { STATE_RENDER(WINED3DRS_ANISOTROPY),                 { STATE_RENDER(WINED3DRS_ANISOTROPY),                 state_anisotropy    }, WINED3D_GL_EXT_NONE             },
@@ -5793,6 +5762,7 @@ static void validate_state_table(struct StateEntry *state_table)
         { 17,  18},
         { 21,  21},
         { 42,  45},
+        { 47,  47},
         { 61, 127},
         {149, 150},
         {169, 169},

dlls/wined3d/stateblock.c
@@ -1141,7 +1141,6 @@ void stateblock_init_default_state(struct wined3d_stateblock *stateblock)
     tmpfloat.f = 1.0f;
     state->render_states[WINED3DRS_FOGDENSITY] = tmpfloat.d;
     state->render_states[WINED3DRS_EDGEANTIALIAS] = FALSE;
-    state->render_states[WINED3DRS_ZBIAS] = 0;
     state->render_states[WINED3DRS_RANGEFOGENABLE] = FALSE;
     state->render_states[WINED3DRS_STENCILENABLE] = FALSE;
     state->render_states[WINED3DRS_STENCILFAIL] = WINED3DSTENCILOP_KEEP;

dlls/wined3d/utils.c
@@ -1962,7 +1962,6 @@ const char *debug_d3drenderstate(WINED3DRENDERSTATETYPE state)
         D3DSTATE_TO_STR(WINED3DRS_EDGEANTIALIAS);
         D3DSTATE_TO_STR(WINED3DRS_COLORKEYENABLE);
         D3DSTATE_TO_STR(WINED3DRS_MIPMAPLODBIAS);
-        D3DSTATE_TO_STR(WINED3DRS_ZBIAS);
         D3DSTATE_TO_STR(WINED3DRS_RANGEFOGENABLE);
         D3DSTATE_TO_STR(WINED3DRS_ANISOTROPY);
         D3DSTATE_TO_STR(WINED3DRS_FLUSHBATCH);

include/wine/wined3d.idl
@@ -319,7 +319,6 @@ typedef enum _WINED3DRENDERSTATETYPE
     WINED3DRS_EDGEANTIALIAS                 = 40, /* d3d7, d3d8 */
     WINED3DRS_COLORKEYENABLE                = 41, /* d3d7 */
     WINED3DRS_MIPMAPLODBIAS                 = 46, /* d3d7 */
-    WINED3DRS_ZBIAS                         = 47, /* d3d7, d3d8 */
     WINED3DRS_RANGEFOGENABLE                = 48,
     WINED3DRS_ANISOTROPY                    = 49, /* d3d7 */
     WINED3DRS_FLUSHBATCH                    = 50, /* d3d7 */

commit ddc29c40e66f686190a40ac4bc2578e81785f56f

wined3d: Make D3DRS_ZBIAS work.
D3DRS_ZBIAS is poorly defined, but it makes sense that the bias should be
format independent. Looking at application behaviour, it seems to include a
slope scale factor as well. This fixes a couple of regressions introduced by
96b758f7b37033bf382ce40dd3310965d3ac3f76, although it was broken before as
well, just in a different way.

dlls/d3d8/d3d8_main.c
@@ -45,7 +45,7 @@ IDirect3D8* WINAPI DECLSPEC_HOTPATCH Direct3DCreate8(UINT SDKVersion) {
 
     object->IDirect3D8_iface.lpVtbl = &Direct3D8_Vtbl;
     object->ref = 1;
-    object->WineD3D = wined3d_create(8, 0, &object->IDirect3D8_iface);
+    object->WineD3D = wined3d_create(8, WINED3D_LEGACY_DEPTH_BIAS, &object->IDirect3D8_iface);
 
     TRACE("Created Direct3D object @ %p, WineObj @ %p\n", object, object->WineD3D);
 

dlls/d3d8/device.c
@@ -1445,22 +1445,11 @@ static HRESULT WINAPI IDirect3DDevice8Impl_GetClipPlane(IDirect3DDevice8 *iface,
     return hr;
 }
 
-/* This factor is the result of a trial-and-error search. Both ZBIAS and DEPTHBIAS require
- * guesswork by design. d3d9 apps usually use a DEPTHBIAS of -0.00002(Mass Effect 2, WoW).
- * d3d8 apps(Final Fantasy XI) set ZBIAS to 15 and still expect the depth test to sort
- * objects properly. */
-static const float zbias_factor = -0.000005f;
-
 static HRESULT WINAPI IDirect3DDevice8Impl_SetRenderState(IDirect3DDevice8 *iface,
         D3DRENDERSTATETYPE State, DWORD Value)
 {
     IDirect3DDevice8Impl *This = impl_from_IDirect3DDevice8(iface);
     HRESULT hr;
-    union
-    {
-        DWORD d;
-        float f;
-    } wined3d_value;
 
     TRACE("iface %p, state %#x, value %#x.\n", iface, State, Value);
 
@@ -1468,8 +1457,7 @@ static HRESULT WINAPI IDirect3DDevice8Impl_SetRenderState(IDirect3DDevice8 *ifac
     switch (State)
     {
         case D3DRS_ZBIAS:
-            wined3d_value.f = Value * zbias_factor;
-            hr = wined3d_device_set_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, wined3d_value.d);
+            hr = wined3d_device_set_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, Value);
             break;
 
         default:
@@ -1485,11 +1473,6 @@ static HRESULT WINAPI IDirect3DDevice8Impl_GetRenderState(IDirect3DDevice8 *ifac
 {
     IDirect3DDevice8Impl *This = impl_from_IDirect3DDevice8(iface);
     HRESULT hr;
-    union
-    {
-        DWORD d;
-        float f;
-    } wined3d_value;
 
     TRACE("iface %p, state %#x, value %p.\n", iface, State, pValue);
 
@@ -1497,8 +1480,7 @@ static HRESULT WINAPI IDirect3DDevice8Impl_GetRenderState(IDirect3DDevice8 *ifac
     switch (State)
     {
         case D3DRS_ZBIAS:
-            hr = wined3d_device_get_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, &wined3d_value.d);
-            if (SUCCEEDED(hr)) *pValue = (DWORD)(wined3d_value.f / zbias_factor);
+            hr = wined3d_device_get_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, pValue);
             break;
 
         default:

dlls/ddraw/ddraw.c
@@ -6060,7 +6060,8 @@ HRESULT ddraw_init(IDirectDrawImpl *ddraw, WINED3DDEVTYPE device_type)
     ddraw->orig_width = GetSystemMetrics(SM_CXSCREEN);
     ddraw->orig_height = GetSystemMetrics(SM_CYSCREEN);
 
-    ddraw->wineD3D = wined3d_create(7, WINED3D_PALETTE_PER_SURFACE, &ddraw->IDirectDraw7_iface);
+    ddraw->wineD3D = wined3d_create(7, WINED3D_PALETTE_PER_SURFACE | WINED3D_LEGACY_DEPTH_BIAS,
+            &ddraw->IDirectDraw7_iface);
     if (!ddraw->wineD3D)
     {
         WARN("Failed to create a wined3d object.\n");

dlls/ddraw/device.c
@@ -2261,12 +2261,8 @@ static HRESULT WINAPI IDirect3DDeviceImpl_2_End(IDirect3DDevice2 *iface, DWORD d
  *  DDERR_INVALIDPARAMS if Value == NULL
  *
  *****************************************************************************/
-static const float zbias_factor = -0.000005f;
-
-static HRESULT
-IDirect3DDeviceImpl_7_GetRenderState(IDirect3DDevice7 *iface,
-                                     D3DRENDERSTATETYPE RenderStateType,
-                                     DWORD *Value)
+static HRESULT IDirect3DDeviceImpl_7_GetRenderState(IDirect3DDevice7 *iface,
+        D3DRENDERSTATETYPE RenderStateType, DWORD *Value)
 {
     IDirect3DDeviceImpl *This = (IDirect3DDeviceImpl *)iface;
     HRESULT hr;
@@ -2383,18 +2379,8 @@ IDirect3DDeviceImpl_7_GetRenderState(IDirect3DDevice7 *iface,
             break;
 
         case D3DRENDERSTATE_ZBIAS:
-        {
-            union
-            {
-                DWORD d;
-                float f;
-            } wined3d_value;
-
-            hr = wined3d_device_get_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, &wined3d_value.d);
-            if (SUCCEEDED(hr))
-                *Value = wined3d_value.f / zbias_factor;
+            hr = wined3d_device_get_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, Value);
             break;
-        }
 
         default:
             if (RenderStateType >= D3DRENDERSTATE_STIPPLEPATTERN00
@@ -2703,16 +2689,8 @@ IDirect3DDeviceImpl_7_SetRenderState(IDirect3DDevice7 *iface,
             break;
 
         case D3DRENDERSTATE_ZBIAS:
-        {
-            union
-            {
-                DWORD d;
-                float f;
-            } wined3d_value;
-            wined3d_value.f = Value * zbias_factor;
-            hr = wined3d_device_set_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, wined3d_value.d);
+            hr = wined3d_device_set_render_state(This->wined3d_device, WINED3DRS_DEPTHBIAS, Value);
             break;
-        }
 
         default:
             if (RenderStateType >= D3DRENDERSTATE_STIPPLEPATTERN00

dlls/wined3d/state.c
@@ -1706,7 +1706,8 @@ static void state_depthbias(DWORD state, struct wined3d_stateblock *stateblock, 
     if (stateblock->state.render_states[WINED3DRS_SLOPESCALEDEPTHBIAS]
             || stateblock->state.render_states[WINED3DRS_DEPTHBIAS])
     {
-        struct wined3d_surface *depth = stateblock->device->fb.depth_stencil;
+        const struct wined3d_device *device = stateblock->device;
+        const struct wined3d_surface *depth = device->fb.depth_stencil;
         float scale;
 
         union
@@ -1721,23 +1722,34 @@ static void state_depthbias(DWORD state, struct wined3d_stateblock *stateblock, 
         glEnable(GL_POLYGON_OFFSET_FILL);
         checkGLcall("glEnable(GL_POLYGON_OFFSET_FILL)");
 
-        if (depth)
+        if (device->wined3d->flags & WINED3D_LEGACY_DEPTH_BIAS)
         {
-            const struct wined3d_format *fmt = depth->resource.format;
-            scale = powf(2, fmt->depth_size) - 1;
-            TRACE("Depth format %s, using depthbias scale of %f\n",
-                  debug_d3dformat(fmt->id), scale);
+            float bias = -(float)const_bias.d;
+            glPolygonOffset(bias, bias);
+            checkGLcall("glPolygonOffset");
         }
         else
         {
-            /* The context manager will reapply this state on a depth stencil change */
-            TRACE("No depth stencil, using depthbias scale of 0.0\n");
-            scale = 0;
-        }
+            if (depth)
+            {
+                const struct wined3d_format *fmt = depth->resource.format;
+                scale = powf(2, fmt->depth_size) - 1;
+                TRACE("Depth format %s, using depthbias scale of %.8e.\n",
+                      debug_d3dformat(fmt->id), scale);
+            }
+            else
+            {
+                /* The context manager will reapply this state on a depth stencil change */
+                TRACE("No depth stencil, using depthbias scale of 0.0.\n");
+                scale = 0.0f;
+            }
 
-        glPolygonOffset(scale_bias.f, const_bias.f * scale);
-        checkGLcall("glPolygonOffset(...)");
-    } else {
+            glPolygonOffset(scale_bias.f, const_bias.f * scale);
+            checkGLcall("glPolygonOffset(...)");
+        }
+    }
+    else
+    {
         glDisable(GL_POLYGON_OFFSET_FILL);
         checkGLcall("glDisable(GL_POLYGON_OFFSET_FILL)");
     }

include/wine/wined3d.h
@@ -1236,6 +1236,7 @@ enum wined3d_sysval_semantic
 #define WINED3DDEVCAPS_NPATCHES                                 0x01000000
 
 #define WINED3D_PALETTE_PER_SURFACE                             0x00000001
+#define WINED3D_LEGACY_DEPTH_BIAS                               0x00000002
 
 /* dwDDFX */
 /* arithmetic stretching along y axis */