6ffd0668341c0410c11fa8654be2e51c5bbb4f0d
Henri Verbeet <hverbeet@codeweavers.com>
2011-07-19 12:56:15 +0200

    wined3d: Make the query parameter to wined3d_event_query_finish() const.

--- dlls/wined3d/query.c
+++ dlls/wined3d/query.c
@@ -110,7 +110,7 @@ static enum wined3d_event_query_result wined3d_event_query_test(const struct win
     return ret;
 }
 
-enum wined3d_event_query_result wined3d_event_query_finish(struct wined3d_event_query *query,
+enum wined3d_event_query_result wined3d_event_query_finish(const struct wined3d_event_query *query,
         struct wined3d_device *device)
 {
     struct wined3d_context *context;
--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -1041,7 +1041,7 @@ enum wined3d_event_query_result
 };
 
 void wined3d_event_query_destroy(struct wined3d_event_query *query) DECLSPEC_HIDDEN;
-enum wined3d_event_query_result wined3d_event_query_finish(struct wined3d_event_query *query,
+enum wined3d_event_query_result wined3d_event_query_finish(const struct wined3d_event_query *query,
         struct wined3d_device *device) DECLSPEC_HIDDEN;
 void wined3d_event_query_issue(struct wined3d_event_query *query, struct wined3d_device *device) DECLSPEC_HIDDEN;
 BOOL wined3d_event_query_supported(const struct wined3d_gl_info *gl_info) DECLSPEC_HIDDEN;
