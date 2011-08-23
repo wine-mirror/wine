0f67869d8402150ab9a555b4d705e137faf52483
Hans Leidekker <hans@codeweavers.com>
2011-08-23 14:41:27 +0200

    msi: Print a warning instead of a fixme if we fail to open the database.

--- dlls/msi/database.c
+++ dlls/msi/database.c
@@ -373,7 +373,7 @@ UINT MSI_OpenDatabaseW(LPCWSTR szDBPath, LPCWSTR szPersist, MSIDATABASE **pdb)
 
     if( FAILED( r ) || !stg )
     {
-        FIXME("open failed r = %08x for %s\n", r, debugstr_w(szDBPath));
+        WARN("open failed r = %08x for %s\n", r, debugstr_w(szDBPath));
         return ERROR_FUNCTION_FAILED;
     }
 
