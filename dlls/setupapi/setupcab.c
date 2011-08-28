3928f57658e43357f92e16292ee76032f0679a8c
Alasdair Sinclair <alasdairsinc@gmail.com>
2011-08-28 19:57:53 +0100

    setupapi: Fix magic comparison on x64.

--- dlls/setupapi/setupcab.c
+++ dlls/setupapi/setupcab.c
@@ -287,7 +287,7 @@ static INT_PTR CDECL sc_FNNOTIFY_A(FDINOTIFICATIONTYPE fdint, PFDINOTIFICATION p
 
   TRACE("(fdint == %d, pfdin == ^%p)\n", fdint, pfdin);
 
-  if (pfdin && pfdin->pv && (*((void **) pfdin->pv) == (void *)SC_HSC_A_MAGIC))
+  if (pfdin && pfdin->pv && (((PSC_HSC_A) pfdin->pv)->magic == SC_HSC_A_MAGIC))
     phsc = pfdin->pv;
   else {
     ERR("pv %p is not an SC_HSC_A.\n", (pfdin) ? pfdin->pv : NULL);
