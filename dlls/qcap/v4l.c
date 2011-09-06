b870cd1a162e84accbce1255be2394915214c0bf
Alexandre Julliard <julliard@winehq.org>
2011-09-06 12:12:05 +0200

    qcap: Include system headers before Windows headers.

--- dlls/qcap/v4l.c
+++ dlls/qcap/v4l.c
@@ -24,29 +24,14 @@
 
 #include "config.h"
 #include "wine/port.h"
-#include "wine/library.h"
 
 #define NONAMELESSSTRUCT
 #define NONAMELESSUNION
 #define COBJMACROS
 
 #include <stdarg.h>
-#include "windef.h"
-#include "winbase.h"
-#include "wtypes.h"
-#include "wingdi.h"
-#include "winuser.h"
-#include "dshow.h"
-#include "vfwmsgs.h"
-#include "amvideo.h"
-#include "wine/debug.h"
-
-#include "capture.h"
-#include "qcap_main.h"
-
 #include <stdio.h>
 #include <fcntl.h>
-
 #ifdef HAVE_SYS_IOCTL_H
 #include <sys/ioctl.h>
 #endif
@@ -68,6 +53,20 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+
+#include "windef.h"
+#include "winbase.h"
+#include "wtypes.h"
+#include "wingdi.h"
+#include "winuser.h"
+#include "dshow.h"
+#include "vfwmsgs.h"
+#include "amvideo.h"
+#include "wine/debug.h"
+#include "wine/library.h"
+
+#include "capture.h"
+#include "qcap_main.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(qcap_v4l);
 
