3eafe92ba31aa4369e652777762b0bd122fa39bb
Gerald Pfeifer <gerald@pfeifer.com>
2011-09-08 01:08:56 +0200

    mshtml: Add failure detection to set_downloading_proc.

--- dlls/mshtml/persist.c
+++ dlls/mshtml/persist.c
@@ -179,7 +179,7 @@ static void set_downloading_proc(task_t *_task)
         IDropTarget *drop_target = NULL;
 
         hres = IDocHostUIHandler_GetDropTarget(doc->hostui, NULL /* FIXME */, &drop_target);
-        if(drop_target) {
+        if(SUCCEEDED(hres)) {
             FIXME("Use IDropTarget\n");
             IDropTarget_Release(drop_target);
         }
