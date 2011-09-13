9db2d7c352de98a99adee48158e5cddd861afaad
Jacek Caban <jacek@codeweavers.com>
2011-09-13 11:41:03 +0200

    vbscript: Added interp_div implementation.

--- dlls/vbscript/interp.c
+++ dlls/vbscript/interp.c
@@ -692,8 +692,26 @@ static HRESULT interp_idiv(exec_ctx_t *ctx)
 
 static HRESULT interp_div(exec_ctx_t *ctx)
 {
-    FIXME("\n");
-    return E_NOTIMPL;
+    variant_val_t r, l;
+    VARIANT v;
+    HRESULT hres;
+
+    TRACE("\n");
+
+    hres = stack_pop_val(ctx, &r);
+    if(FAILED(hres))
+        return hres;
+
+    hres = stack_pop_val(ctx, &l);
+    if(SUCCEEDED(hres)) {
+        hres = VarDiv(l.v, r.v, &v);
+        release_val(&l);
+    }
+    release_val(&r);
+    if(FAILED(hres))
+        return hres;
+
+    return stack_push(ctx, &v);
 }
 
 static HRESULT interp_mul(exec_ctx_t *ctx)
