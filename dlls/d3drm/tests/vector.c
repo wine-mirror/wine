9fbd18cc346f0dc7e7518a232964d547ccee33f7
Francois Gouget <fgouget@free.fr>
2011-09-27 00:14:22 +0200

    d3drm/tests: Remove an unneeded assert.h include.

--- dlls/d3drm/tests/vector.c
+++ dlls/d3drm/tests/vector.c
@@ -17,9 +17,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <assert.h>
-#include "d3drmdef.h"
 #include <math.h>
+
+#include "d3drmdef.h"
 
 #include "wine/test.h"
 
