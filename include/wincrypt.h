a39e87532b2c8c33ea756cbbe7edccbffa7f76ff
Alexander Morozov <amorozov@etersoft.ru>
2011-11-02 15:16:07 +0400

    wincrypt.h: Fix a typo.

--- include/wincrypt.h
+++ include/wincrypt.h
@@ -593,7 +593,7 @@ typedef struct _CERT_REQUEST_INFO {
 
 typedef struct _CERT_KEYGEN_REQUEST_INFO {
     DWORD                dwVersion;
-    CERT_PUBLIC_KEY_INFO SubjectPubliceKeyInfo;
+    CERT_PUBLIC_KEY_INFO SubjectPublicKeyInfo;
     LPWSTR               pwszChallengeString;
 } CERT_KEYGEN_REQUEST_INFO, *PCERT_KEYGEN_REQUEST_INFO;
 
