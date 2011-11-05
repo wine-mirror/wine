f640ba231b9d7ecbd8d081d4b14c3af4792842f4
Andrew Talbot <andrew.talbot@talbotville.com>
2011-11-05 20:53:31 +0000

    dplayx: Remove extraneous braces from switch statement.

--- dlls/dplayx/dplay.c
+++ dlls/dplayx/dplay.c
@@ -648,23 +648,18 @@ HRESULT DP_HandleMessage( IDirectPlay2Impl* This, LPCVOID lpcMessageBody,
   {
     /* Name server needs to handle this request */
     case DPMSGCMD_ENUMSESSIONSREQUEST:
-    {
       /* Reply expected */
       NS_ReplyToEnumSessionsRequest( lpcMessageBody, lplpReply, lpdwMsgSize, This );
-
       break;
-    }
 
     /* Name server needs to handle this request */
     case DPMSGCMD_ENUMSESSIONSREPLY:
-    {
       /* No reply expected */
       NS_AddRemoteComputerAsNameServer( lpcMessageHeader,
                                         This->dp2->spData.dwSPHeaderSize,
                                         lpcMessageBody,
                                         This->dp2->lpNameServerData );
       break;
-    }
 
     case DPMSGCMD_REQUESTNEWPLAYERID:
     {
@@ -691,56 +686,43 @@ HRESULT DP_HandleMessage( IDirectPlay2Impl* This, LPCVOID lpcMessageBody,
 
       TRACE( "Allocating new playerid 0x%08x from remote request\n",
              lpReply->dpidNewPlayerId );
-
       break;
     }
 
     case DPMSGCMD_GETNAMETABLEREPLY:
     case DPMSGCMD_NEWPLAYERIDREPLY:
-    {
-
 #if 0
       if( wCommandId == DPMSGCMD_NEWPLAYERIDREPLY )
         DebugBreak();
 #endif
       DP_MSG_ReplyReceived( This, wCommandId, lpcMessageBody, dwMessageBodySize );
-
       break;
-    }
 
 #if 1
     case DPMSGCMD_JUSTENVELOPE:
-    {
       TRACE( "GOT THE SELF MESSAGE: %p -> 0x%08x\n", lpcMessageHeader, ((const DWORD *)lpcMessageHeader)[1] );
       NS_SetLocalAddr( This->dp2->lpNameServerData, lpcMessageHeader, 20 );
       DP_MSG_ReplyReceived( This, wCommandId, lpcMessageBody, dwMessageBodySize );
-    }
 #endif
 
     case DPMSGCMD_FORWARDADDPLAYER:
-    {
 #if 0
       DebugBreak();
 #endif
 #if 1
-    TRACE( "Sending message to self to get my addr\n" );
-    DP_MSG_ToSelf( This, 1 ); /* This is a hack right now */
+      TRACE( "Sending message to self to get my addr\n" );
+      DP_MSG_ToSelf( This, 1 ); /* This is a hack right now */
 #endif
       break;
-    }
 
     case DPMSGCMD_FORWARDADDPLAYERNACK:
-    {
       DP_MSG_ErrorReceived( This, wCommandId, lpcMessageBody, dwMessageBodySize );
       break;
-    }
 
     default:
-    {
       FIXME( "Unknown wCommandId %u. Ignoring message\n", wCommandId );
       DebugBreak();
       break;
-    }
   }
 
   /* FIXME: There is code in dplaysp.c to handle dplay commands. Move to here. */
