c6bb89a6d6cdfa4dcf59e4f63bd4c2452f709660
Juan Lang <juan.lang@gmail.com>
2011-11-09 17:42:20 -0800

    iphlpapi: Remove unneeded includes.

--- dlls/iphlpapi/iphlpapi_main.c
+++ dlls/iphlpapi/iphlpapi_main.c
@@ -23,12 +23,6 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#ifdef HAVE_SYS_SOCKET_H
-#include <sys/socket.h>
-#endif
-#ifdef HAVE_NET_IF_H
-#include <net/if.h>
-#endif
 #ifdef HAVE_NETINET_IN_H
 # include <netinet/in.h>
 #endif
