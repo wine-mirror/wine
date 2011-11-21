e6cf9fbcc2751e8a8a418b531f8e98980b304933
Alexandre Julliard <julliard@winehq.org>
2011-11-21 11:47:24 +0100

    comctl32: Merge imagelist definitions into the C file.

--- dlls/comctl32/imagelist.c
+++ dlls/comctl32/imagelist.c
@@ -52,14 +52,62 @@
 #include "commctrl.h"
 #include "comctl32.h"
 #include "commoncontrols.h"
-#include "imagelist.h"
 #include "wine/debug.h"
 #include "wine/exception.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(imagelist);
 
-
 #define MAX_OVERLAYIMAGE 15
+
+struct _IMAGELIST
+{
+    const struct IImageListVtbl *lpVtbl; /* 00: IImageList vtable */
+
+    INT         cCurImage;                 /* 04: ImageCount */
+    INT         cMaxImage;                 /* 08: maximages */
+    INT         cGrow;                     /* 0C: cGrow */
+    INT         cx;                        /* 10: cx */
+    INT         cy;                        /* 14: cy */
+    DWORD       x4;
+    UINT        flags;                     /* 1C: flags */
+    COLORREF    clrFg;                     /* 20: foreground color */
+    COLORREF    clrBk;                     /* 24: background color */
+
+
+    HBITMAP     hbmImage;                  /* 28: images Bitmap */
+    HBITMAP     hbmMask;                   /* 2C: masks  Bitmap */
+    HDC         hdcImage;                  /* 30: images MemDC  */
+    HDC         hdcMask;                   /* 34: masks  MemDC  */
+    INT         nOvlIdx[MAX_OVERLAYIMAGE]; /* 38: overlay images index */
+
+    /* not yet found out */
+    HBRUSH  hbrBlend25;
+    HBRUSH  hbrBlend50;
+    INT     cInitial;
+    UINT    uBitsPixel;
+    char   *has_alpha;
+
+    LONG        ref;                       /* reference count */
+};
+
+#define IMAGELIST_MAGIC 0x53414D58
+
+/* Header used by ImageList_Read() and ImageList_Write() */
+#include "pshpack2.h"
+typedef struct _ILHEAD
+{
+    USHORT	usMagic;
+    USHORT	usVersion;
+    WORD	cCurImage;
+    WORD	cMaxImage;
+    WORD	cGrow;
+    WORD	cx;
+    WORD	cy;
+    COLORREF	bkcolor;
+    WORD	flags;
+    SHORT	ovls[4];
+} ILHEAD;
+#include "poppack.h"
 
 /* internal image list data used for Drag & Drop operations */
 typedef struct
