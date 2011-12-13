445db0a54d4e8ec34c5a1a2b0f3fb6975b9369e3
Piotr Caban <piotr@codeweavers.com>
2011-12-13 15:14:13 +0100

    hlink: Fix leak in HlinkNavigateToStringReference.

--- dlls/hlink/hlink_main.c
+++ dlls/hlink/hlink_main.c
@@ -261,14 +261,16 @@ HRESULT WINAPI HlinkNavigateToStringReference( LPCWSTR pwzTarget,
     HRESULT r;
     IHlink *hlink = NULL;
 
-    FIXME("%s %s %p %08x %p %08x %p %p %p\n",
+    TRACE("%s %s %p %08x %p %08x %p %p %p\n",
           debugstr_w(pwzTarget), debugstr_w(pwzLocation), pihlsite,
           dwSiteData, pihlframe, grfHLNF, pibc, pibsc, pihlbc);
 
     r = HlinkCreateFromString( pwzTarget, pwzLocation, NULL, pihlsite,
                                dwSiteData, NULL, &IID_IHlink, (LPVOID*) &hlink );
-    if (SUCCEEDED(r))
+    if (SUCCEEDED(r)) {
         r = HlinkNavigate(hlink, pihlframe, grfHLNF, pibc, pibsc, pihlbc);
+        IHlink_Release(hlink);
+    }
 
     return r;
 }
