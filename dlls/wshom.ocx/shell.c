bf0e3a58517147d8711a1a757fe4dcd2d40a068c
Nikolay Sivov <nsivov@codeweavers.com>
2011-12-30 13:40:13 +0300

    wshom.ocx: Fix IWshShell3 definition.

--- dlls/wshom.ocx/shell.c
+++ dlls/wshom.ocx/shell.c
@@ -61,7 +61,6 @@ static ULONG WINAPI WshShell3_Release(IWshShell3 *iface)
 static HRESULT WINAPI WshShell3_GetTypeInfoCount(IWshShell3 *iface, UINT *pctinfo)
 {
     TRACE("(%p)\n", pctinfo);
-
     *pctinfo = 1;
     return S_OK;
 }
@@ -112,19 +111,73 @@ static HRESULT WINAPI WshShell3_Invoke(IWshShell3 *iface, DISPID dispIdMember, R
 
 static HRESULT WINAPI WshShell3_get_SpecialFolders(IWshShell3 *iface, IWshCollection **out_Folders)
 {
-    FIXME("(%p)\n", out_Folders);
+    FIXME("(%p): stub\n", out_Folders);
     return E_NOTIMPL;
 }
 
 static HRESULT WINAPI WshShell3_get_Environment(IWshShell3 *iface, VARIANT *Type, IWshEnvironment **out_Env)
 {
-    FIXME("(%p %p)\n", Type, out_Env);
+    FIXME("(%p %p): stub\n", Type, out_Env);
     return E_NOTIMPL;
 }
 
 static HRESULT WINAPI WshShell3_Run(IWshShell3 *iface, BSTR Command, VARIANT *WindowStyle, VARIANT *WaitOnReturn, int *out_ExitCode)
 {
-    FIXME("(%p %p %p)\n", WindowStyle, WaitOnReturn, out_ExitCode);
+    FIXME("(%p %p %p): stub\n", WindowStyle, WaitOnReturn, out_ExitCode);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_Popup(IWshShell3 *iface, BSTR Text, VARIANT* SecondsToWait, VARIANT *Title, VARIANT *Type, int *button)
+{
+    FIXME("(%s %p %p %p %p): stub\n", debugstr_w(Text), SecondsToWait, Title, Type, button);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_CreateShortcut(IWshShell3 *iface, BSTR PathLink, IDispatch** out_Shortcut)
+{
+    FIXME("(%s %p): stub\n", debugstr_w(PathLink), out_Shortcut);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_ExpandEnvironmentStrings(IWshShell3 *iface, BSTR Src, BSTR* out_Dst)
+{
+    FIXME("(%s %p): stub\n", debugstr_w(Src), out_Dst);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_RegRead(IWshShell3 *iface, BSTR Name, VARIANT* out_Value)
+{
+    FIXME("(%s %p): stub\n", debugstr_w(Name), out_Value);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_RegWrite(IWshShell3 *iface, BSTR Name, VARIANT *Value, VARIANT *Type)
+{
+    FIXME("(%s %p %p): stub\n", debugstr_w(Name), Value, Type);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_RegDelete(IWshShell3 *iface, BSTR Name)
+{
+    FIXME("(%s): stub\n", debugstr_w(Name));
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_LogEvent(IWshShell3 *iface, VARIANT *Type, BSTR Message, BSTR Target, VARIANT_BOOL *out_Success)
+{
+    FIXME("(%p %s %s %p): stub\n", Type, debugstr_w(Message), debugstr_w(Target), out_Success);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_AppActivate(IWshShell3 *iface, VARIANT *App, VARIANT *Wait, VARIANT_BOOL *out_Success)
+{
+    FIXME("(%p %p %p): stub\n", App, Wait, out_Success);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WshShell3_SendKeys(IWshShell3 *iface, BSTR Keys, VARIANT *Wait)
+{
+    FIXME("(%s %p): stub\n", debugstr_w(Keys), Wait);
     return E_NOTIMPL;
 }
 
@@ -138,7 +191,16 @@ static const IWshShell3Vtbl WshShell3Vtbl = {
     WshShell3_Invoke,
     WshShell3_get_SpecialFolders,
     WshShell3_get_Environment,
-    WshShell3_Run
+    WshShell3_Run,
+    WshShell3_Popup,
+    WshShell3_CreateShortcut,
+    WshShell3_ExpandEnvironmentStrings,
+    WshShell3_RegRead,
+    WshShell3_RegWrite,
+    WshShell3_RegDelete,
+    WshShell3_LogEvent,
+    WshShell3_AppActivate,
+    WshShell3_SendKeys
 };
 
 static IWshShell3 WshShell3 = { &WshShell3Vtbl };
