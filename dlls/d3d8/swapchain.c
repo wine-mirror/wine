ddfb19e913b145e237ddb4d771bb8e6649c81c1e
Henri Verbeet <hverbeet@codeweavers.com>
2012-03-14 22:04:31 +0100

    d3d8: Don't return a pointer to the implementation in IDirect3DSwapChain8Impl_QueryInterface().

--- dlls/d3d8/swapchain.c
+++ dlls/d3d8/swapchain.c
@@ -31,18 +31,18 @@ static inline IDirect3DSwapChain8Impl *impl_from_IDirect3DSwapChain8(IDirect3DSw
 static HRESULT WINAPI IDirect3DSwapChain8Impl_QueryInterface(IDirect3DSwapChain8 *iface,
         REFIID riid, void **ppobj)
 {
-    IDirect3DSwapChain8Impl *This = impl_from_IDirect3DSwapChain8(iface);
-
     TRACE("iface %p, riid %s, object %p.\n", iface, debugstr_guid(riid), ppobj);
 
-    if (IsEqualGUID(riid, &IID_IUnknown)
-        || IsEqualGUID(riid, &IID_IDirect3DSwapChain8)) {
+    if (IsEqualGUID(riid, &IID_IDirect3DSwapChain8)
+            || IsEqualGUID(riid, &IID_IUnknown))
+    {
         IUnknown_AddRef(iface);
-        *ppobj = This;
+        *ppobj = iface;
         return S_OK;
     }
 
-    WARN("(%p)->(%s,%p),not found\n", This, debugstr_guid(riid), ppobj);
+    WARN("%s not implemented, returning E_NOINTERFACE.\n", debugstr_guid(riid));
+
     *ppobj = NULL;
     return E_NOINTERFACE;
 }
