c1558bb7ef87f2f5898f7cac6d05975e50edc542
Akihiro Sagawa <sagawa.aki@gmail.com>
2012-03-21 23:02:14 +0900

    msvcrt/tests: Add more tests for _mbcjistojms.

--- dlls/msvcrt/tests/string.c
+++ dlls/msvcrt/tests/string.c
@@ -863,6 +863,7 @@ static void test_mbcjisjms(void)
     /* List of value-pairs to test. The test assumes the last pair to be {0, ..} */
     unsigned int jisjms[][2] = { {0x2020, 0}, {0x2021, 0}, {0x2120, 0}, {0x2121, 0x8140},
                                  {0x7f7f, 0}, {0x7f7e, 0}, {0x7e7f, 0}, {0x7e7e, 0xeffc},
+                                 {0x255f, 0x837e}, {0x2560, 0x8380}, {0x2561, 0x8381},
                                  {0x2121FFFF, 0}, {0x2223, 0x81a1}, {0x237e, 0x829e}, {0, 0}};
     int cp[] = { 932, 936, 939, 950, 1361, _MB_CP_SBCS };
     unsigned int i, j;
@@ -876,6 +877,11 @@ static void test_mbcjisjms(void)
             unsigned int ret, exp;
             ret = _mbcjistojms(jisjms[j][0]);
             exp = (cp[i] == 932) ? jisjms[j][1] : jisjms[j][0];
+            if (LOBYTE(exp) == 0x80 && cp[i] == 932) {
+                todo_wine ok(ret == exp, "Expected 0x%x, got 0x%x (0x%x, codepage=%d)\n",
+                   exp, ret, jisjms[j][0], cp[i]);
+                continue;
+            }
             ok(ret == exp, "Expected 0x%x, got 0x%x (0x%x, codepage=%d)\n",
                exp, ret, jisjms[j][0], cp[i]);
         }
