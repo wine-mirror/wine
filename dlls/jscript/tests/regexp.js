b3bafb60a278bfe23d67923d399d46f2da11dd46
Jacek Caban <jacek@codeweavers.com>
2012-03-30 18:19:48 +0200

    jscript: Fixed jsheap_grow implementation.

--- dlls/jscript/tests/regexp.js
+++ dlls/jscript/tests/regexp.js
@@ -44,6 +44,10 @@ ok(m[0] === "aa", "m[0] = " + m[0]);
 ok(RegExp.leftContext === " ", "RegExp.leftContext = " + RegExp.leftContext);
 ok(RegExp.rightContext === "baaa", "RegExp.rightContext = " + RegExp.rightContext);
 
+m = /^[^<]*(<(.|\s)+>)[^>]*$|^#(\w+)$/.exec(
+    "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+ok(m === null, "m is not null");
+
 re = /a+/g;
 ok(re.lastIndex === 0, "re.lastIndex = " + re.lastIndex);
 
