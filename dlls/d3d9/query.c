a84eaf3d2236836ea0fec6fce5ee216da38d7260
Henri Verbeet <hverbeet@codeweavers.com>
2012-04-01 13:12:05 +0200

    d3d9: Don't return a pointer to the implementation in IDirect3DQuery9Impl_QueryInterface().

--- dlls/d3d9/query.c
+++ dlls/d3d9/query.c
@@ -33,18 +33,18 @@ static inline IDirect3DQuery9Impl *impl_from_IDirect3DQuery9(IDirect3DQuery9 *if
 static HRESULT WINAPI IDirect3DQuery9Impl_QueryInterface(IDirect3DQuery9 *iface, REFIID riid,
         void **ppobj)
 {
-    IDirect3DQuery9Impl *This = impl_from_IDirect3DQuery9(iface);
-
     TRACE("iface %p, riid %s, object %p.\n", iface, debugstr_guid(riid), ppobj);
 
-    if (IsEqualGUID(riid, &IID_IUnknown)
-        || IsEqualGUID(riid, &IID_IDirect3DQuery9)) {
+    if (IsEqualGUID(riid, &IID_IDirect3DQuery9)
+            || IsEqualGUID(riid, &IID_IUnknown))
+    {
         IDirect3DQuery9_AddRef(iface);
-        *ppobj = This;
+        *ppobj = iface;
         return S_OK;
     }
 
-    WARN("(%p)->(%s,%p),not found\n", This, debugstr_guid(riid), ppobj);
+    WARN("%s not implemented, returning E_NOINTERFACE.\n", debugstr_guid(riid));
+
     *ppobj = NULL;
     return E_NOINTERFACE;
 }
