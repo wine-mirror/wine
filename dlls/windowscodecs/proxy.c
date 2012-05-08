b0e9548e3fe9c08d30913f0707ff12a9b7b37cc4
Vincent Povirk <vincent@codeweavers.com>
2012-05-08 10:10:35 -0500

    windowscodecs: Add wrapper functions for IWICImagingFactory methods.

--- dlls/windowscodecs/proxy.c
+++ dlls/windowscodecs/proxy.c
@@ -36,6 +36,134 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(wincodecs);
 
+HRESULT WINAPI IWICImagingFactory_CreateBitmapClipper_Proxy_W(IWICImagingFactory *pFactory,
+    IWICBitmapClipper **ppIBitmapClipper)
+{
+    return IWICImagingFactory_CreateBitmapClipper(pFactory, ppIBitmapClipper);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmapFlipRotator_Proxy_W(IWICImagingFactory *pFactory,
+    IWICBitmapFlipRotator **ppIBitmapFlipRotator)
+{
+    return IWICImagingFactory_CreateBitmapFlipRotator(pFactory, ppIBitmapFlipRotator);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmapFromHBITMAP_Proxy_W(IWICImagingFactory *pFactory,
+    HBITMAP hBitmap, HPALETTE hPalette, WICBitmapAlphaChannelOption options, IWICBitmap **ppIBitmap)
+{
+    return IWICImagingFactory_CreateBitmapFromHBITMAP(pFactory, hBitmap, hPalette, options, ppIBitmap);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmapFromHICON_Proxy_W(IWICImagingFactory *pFactory,
+    HICON hIcon, IWICBitmap **ppIBitmap)
+{
+    return IWICImagingFactory_CreateBitmapFromHICON(pFactory, hIcon, ppIBitmap);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmapFromMemory_Proxy_W(IWICImagingFactory *pFactory,
+    UINT uiWidth, UINT uiHeight, REFWICPixelFormatGUID pixelFormat, UINT cbStride,
+    UINT cbBufferSize, BYTE *pbBuffer, IWICBitmap **ppIBitmap)
+{
+    return IWICImagingFactory_CreateBitmapFromMemory(pFactory, uiWidth, uiHeight,
+        pixelFormat, cbStride, cbBufferSize, pbBuffer, ppIBitmap);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmapFromSource_Proxy_W(IWICImagingFactory *pFactory,
+    IWICBitmapSource *piBitmapSource, WICBitmapCreateCacheOption option, IWICBitmap **ppIBitmap)
+{
+    return IWICImagingFactory_CreateBitmapFromSource(pFactory, piBitmapSource, option, ppIBitmap);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmapScaler_Proxy_W(IWICImagingFactory *pFactory,
+    IWICBitmapScaler **ppIBitmapScaler)
+{
+    return IWICImagingFactory_CreateBitmapScaler(pFactory, ppIBitmapScaler);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateBitmap_Proxy_W(IWICImagingFactory *pFactory,
+    UINT uiWidth, UINT uiHeight, REFWICPixelFormatGUID pixelFormat,
+    WICBitmapCreateCacheOption option, IWICBitmap **ppIBitmap)
+{
+    return IWICImagingFactory_CreateBitmap(pFactory, uiWidth, uiHeight,
+        pixelFormat, option, ppIBitmap);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateComponentInfo_Proxy_W(IWICImagingFactory *pFactory,
+    REFCLSID clsidComponent, IWICComponentInfo **ppIInfo)
+{
+    return IWICImagingFactory_CreateComponentInfo(pFactory, clsidComponent, ppIInfo);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateDecoderFromFileHandle_Proxy_W(IWICImagingFactory *pFactory,
+    ULONG_PTR hFile, const GUID *pguidVendor, WICDecodeOptions metadataOptions, IWICBitmapDecoder **ppIDecoder)
+{
+    return IWICImagingFactory_CreateDecoderFromFileHandle(pFactory, hFile, pguidVendor, metadataOptions, ppIDecoder);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateDecoderFromFilename_Proxy_W(IWICImagingFactory *pFactory,
+    LPCWSTR wzFilename, const GUID *pguidVendor, DWORD dwDesiredAccess,
+    WICDecodeOptions metadataOptions, IWICBitmapDecoder **ppIDecoder)
+{
+    return IWICImagingFactory_CreateDecoderFromFilename(pFactory, wzFilename,
+        pguidVendor, dwDesiredAccess, metadataOptions, ppIDecoder);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateDecoderFromStream_Proxy_W(IWICImagingFactory *pFactory,
+    IStream *pIStream, const GUID *pguidVendor,
+    WICDecodeOptions metadataOptions, IWICBitmapDecoder **ppIDecoder)
+{
+    return IWICImagingFactory_CreateDecoderFromStream(pFactory, pIStream, pguidVendor, metadataOptions, ppIDecoder);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateEncoder_Proxy_W(IWICImagingFactory *pFactory,
+    REFGUID guidContainerFormat, const GUID *pguidVendor, IWICBitmapEncoder **ppIEncoder)
+{
+    return IWICImagingFactory_CreateEncoder(pFactory, guidContainerFormat, pguidVendor, ppIEncoder);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateFastMetadataEncoderFromDecoder_Proxy_W(IWICImagingFactory *pFactory,
+    IWICBitmapDecoder *pIDecoder, IWICFastMetadataEncoder **ppIFastEncoder)
+{
+    return IWICImagingFactory_CreateFastMetadataEncoderFromDecoder(pFactory, pIDecoder, ppIFastEncoder);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateFastMetadataEncoderFromFrameDecode_Proxy_W(IWICImagingFactory *pFactory,
+    IWICBitmapFrameDecode *pIFrameDecoder, IWICFastMetadataEncoder **ppIFastEncoder)
+{
+    return IWICImagingFactory_CreateFastMetadataEncoderFromFrameDecode(pFactory, pIFrameDecoder, ppIFastEncoder);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateFormatConverter_Proxy_W(IWICImagingFactory *pFactory,
+    IWICFormatConverter **ppIFormatConverter)
+{
+    return IWICImagingFactory_CreateFormatConverter(pFactory, ppIFormatConverter);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreatePalette_Proxy_W(IWICImagingFactory *pFactory,
+    IWICPalette **ppIPalette)
+{
+    return IWICImagingFactory_CreatePalette(pFactory, ppIPalette);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateQueryWriterFromReader_Proxy_W(IWICImagingFactory *pFactory,
+    IWICMetadataQueryReader *pIQueryReader, const GUID *pguidVendor,
+    IWICMetadataQueryWriter **ppIQueryWriter)
+{
+    return IWICImagingFactory_CreateQueryWriterFromReader(pFactory, pIQueryReader, pguidVendor, ppIQueryWriter);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateQueryWriter_Proxy_W(IWICImagingFactory *pFactory,
+    REFGUID guidMetadataFormat, const GUID *pguidVendor, IWICMetadataQueryWriter **ppIQueryWriter)
+{
+    return IWICImagingFactory_CreateQueryWriter(pFactory, guidMetadataFormat, pguidVendor, ppIQueryWriter);
+}
+
+HRESULT WINAPI IWICImagingFactory_CreateStream_Proxy_W(IWICImagingFactory *pFactory,
+    IWICStream **ppIWICStream)
+{
+    return IWICImagingFactory_CreateStream(pFactory, ppIWICStream);
+}
+
 HRESULT WINAPI WICCreateImagingFactory_Proxy(UINT SDKVersion, IWICImagingFactory **ppIImagingFactory)
 {
     TRACE("%x, %p\n", SDKVersion, ppIImagingFactory);
