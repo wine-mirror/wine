0b3b6ba26df9b072e72a3584409d1dcceecfb752
Alexandre Julliard <julliard@winehq.org>
2012-05-09 23:20:51 +0200

    wininet/tests: Mark a failing test as broken.

--- dlls/wininet/tests/internet.c
+++ dlls/wininet/tests/internet.c
@@ -284,8 +284,9 @@ static void test_max_conns(void)
 
     val = 0;
     res = InternetSetOptionA(NULL, INTERNET_OPTION_MAX_CONNS_PER_SERVER, &val, sizeof(val));
-    ok(!res, "InternetSetOptionA(INTERNET_OPTION_MAX_CONNS_PER_SERVER, 0) succeeded\n");
-    ok(GetLastError() == ERROR_BAD_ARGUMENTS, "GetLastError() = %u\n", GetLastError());
+    ok(!res || broken(res), /* <= w2k3 */
+       "InternetSetOptionA(INTERNET_OPTION_MAX_CONNS_PER_SERVER, 0) succeeded\n");
+    if (!res) ok(GetLastError() == ERROR_BAD_ARGUMENTS, "GetLastError() = %u\n", GetLastError());
 
     val = 2;
     res = InternetSetOptionA(NULL, INTERNET_OPTION_MAX_CONNS_PER_SERVER, &val, sizeof(val)-1);
