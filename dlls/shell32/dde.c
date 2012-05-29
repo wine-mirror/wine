83196d0cb3dedd00254d13e3be9a8be7f2f8eb1d
Oleksij Rempel <bug-track@fisher-privat.net>
2012-05-29 23:10:07 +0200

    shell32: Return empty list instead of NULL.

--- dlls/shell32/dde.c
+++ dlls/shell32/dde.c
@@ -82,6 +82,14 @@ static inline HDDEDATA Dde_OnRequest(UINT uFmt, HCONV hconv, HSZ hszTopic,
         FIXME( "returning fake program groups list\n" );
         return DdeCreateDataHandle( dwDDEInst, groups_data, sizeof(groups_data), 0, hszGroups, uFmt, 0 );
     }
+    else if (hszTopic == hszProgmanTopic && hszItem == hszProgmanService && uFmt == CF_TEXT)
+    {
+        static BYTE groups_data[] = "\r\n";
+        FIXME( "returning empty groups list\n" );
+	/* this is an workaround for an app wich expect some data
+	 * and cant handle NULL. */
+        return DdeCreateDataHandle( dwDDEInst, groups_data, sizeof(groups_data), 0, hszProgmanService, uFmt, 0 );
+    }
     FIXME( "%u %p %s %s: stub\n", uFmt, hconv, debugstr_hsz(hszTopic), debugstr_hsz(hszItem) );
     return NULL;
 }
