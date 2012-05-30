de5495ba86e0a99ed94b2894298ea6708dbbbe83
Jacek Caban <jacek@codeweavers.com>
2012-05-30 10:17:18 +0200

    jscript: Added missing port.h include to lex.c.

--- dlls/jscript/lex.c
+++ dlls/jscript/lex.c
@@ -16,7 +16,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <math.h>
+#include "config.h"
+#include "wine/port.h"
+
 #include <limits.h>
 
 #include "jscript.h"
