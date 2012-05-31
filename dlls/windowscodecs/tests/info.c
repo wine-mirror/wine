46991dc9fb74229011d3b427d1e4f13d39752752
Vincent Povirk <vincent@codeweavers.com>
2012-05-31 12:38:34 -0500

    windowscodecs: Add test for IWICMetadataReaderInfo.

--- dlls/windowscodecs/tests/info.c
+++ dlls/windowscodecs/tests/info.c
@@ -24,6 +24,7 @@
 #include "windef.h"
 #include "objbase.h"
 #include "wincodec.h"
+#include "wincodecsdk.h"
 #include "wine/test.h"
 
 static void test_decoder_info(void)
@@ -91,11 +92,153 @@ static void test_decoder_info(void)
     IWICImagingFactory_Release(factory);
 }
 
+static void test_reader_info(void)
+{
+    IWICImagingFactory *factory;
+    IWICComponentInfo *info;
+    IWICMetadataReaderInfo *reader_info;
+    HRESULT hr;
+    CLSID clsid;
+    GUID container_formats[10];
+    UINT count, size;
+    WICMetadataPattern *patterns;
+
+    hr = CoCreateInstance(&CLSID_WICImagingFactory, NULL, CLSCTX_INPROC_SERVER,
+        &IID_IWICImagingFactory, (void**)&factory);
+    ok(hr == S_OK, "CoCreateInstance failed, hr=%x\n", hr);
+    if (FAILED(hr)) return;
+
+    hr = IWICImagingFactory_CreateComponentInfo(factory, &CLSID_WICUnknownMetadataReader, &info);
+    todo_wine ok(hr == S_OK, "CreateComponentInfo failed, hr=%x\n", hr);
+
+    if (FAILED(hr))
+    {
+        IWICImagingFactory_Release(factory);
+        return;
+    }
+
+    hr = IWICComponentInfo_QueryInterface(info, &IID_IWICMetadataReaderInfo, (void**)&reader_info);
+    ok(hr == S_OK, "QueryInterface failed, hr=%x\n", hr);
+
+    hr = IWICMetadataReaderInfo_GetCLSID(reader_info, NULL);
+    ok(hr == E_INVALIDARG, "GetCLSID failed, hr=%x\n", hr);
+
+    hr = IWICMetadataReaderInfo_GetCLSID(reader_info, &clsid);
+    ok(hr == S_OK, "GetCLSID failed, hr=%x\n", hr);
+    ok(IsEqualGUID(&CLSID_WICUnknownMetadataReader, &clsid), "GetCLSID returned wrong result\n");
+
+    hr = IWICMetadataReaderInfo_GetMetadataFormat(reader_info, &clsid);
+    ok(hr == S_OK, "GetMetadataFormat failed, hr=%x\n", hr);
+    ok(IsEqualGUID(&GUID_MetadataFormatUnknown, &clsid), "GetMetadataFormat returned wrong result\n");
+
+    hr = IWICMetadataReaderInfo_GetContainerFormats(reader_info, 0, NULL, NULL);
+    ok(hr == E_INVALIDARG, "GetContainerFormats failed, hr=%x\n", hr);
+
+    count = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetContainerFormats(reader_info, 0, NULL, &count);
+    ok(hr == S_OK, "GetContainerFormats failed, hr=%x\n", hr);
+    ok(count == 0, "unexpected count %d\n", count);
+
+    hr = IWICMetadataReaderInfo_GetPatterns(reader_info, &GUID_ContainerFormatPng,
+        0, NULL, NULL, NULL);
+    ok(hr == E_INVALIDARG, "GetPatterns failed, hr=%x\n", hr);
+
+    count = size = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetPatterns(reader_info, &GUID_ContainerFormatPng,
+        0, NULL, &count, &size);
+    ok(hr == WINCODEC_ERR_COMPONENTNOTFOUND || broken(hr == S_OK) /* Windows XP */,
+        "GetPatterns failed, hr=%x\n", hr);
+    ok(count == 0xdeadbeef, "unexpected count %d\n", count);
+    ok(size == 0xdeadbeef, "unexpected size %d\n", size);
+
+    IWICMetadataReaderInfo_Release(reader_info);
+
+    IWICComponentInfo_Release(info);
+
+    hr = IWICImagingFactory_CreateComponentInfo(factory, &CLSID_WICXMBStructMetadataReader, &info);
+    ok(hr == S_OK, "CreateComponentInfo failed, hr=%x\n", hr);
+
+    hr = IWICComponentInfo_QueryInterface(info, &IID_IWICMetadataReaderInfo, (void**)&reader_info);
+    ok(hr == S_OK, "QueryInterface failed, hr=%x\n", hr);
+
+    hr = IWICMetadataReaderInfo_GetCLSID(reader_info, NULL);
+    ok(hr == E_INVALIDARG, "GetCLSID failed, hr=%x\n", hr);
+
+    hr = IWICMetadataReaderInfo_GetCLSID(reader_info, &clsid);
+    ok(hr == S_OK, "GetCLSID failed, hr=%x\n", hr);
+    ok(IsEqualGUID(&CLSID_WICXMBStructMetadataReader, &clsid), "GetCLSID returned wrong result\n");
+
+    hr = IWICMetadataReaderInfo_GetMetadataFormat(reader_info, &clsid);
+    ok(hr == S_OK, "GetMetadataFormat failed, hr=%x\n", hr);
+    ok(IsEqualGUID(&GUID_MetadataFormatXMPStruct, &clsid), "GetMetadataFormat returned wrong result\n");
+
+    hr = IWICMetadataReaderInfo_GetContainerFormats(reader_info, 0, NULL, NULL);
+    ok(hr == E_INVALIDARG, "GetContainerFormats failed, hr=%x\n", hr);
+
+    count = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetContainerFormats(reader_info, 0, NULL, &count);
+    ok(hr == S_OK, "GetContainerFormats failed, hr=%x\n", hr);
+    ok(count >= 2, "unexpected count %d\n", count);
+
+    count = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetContainerFormats(reader_info, 1, container_formats, &count);
+    ok(hr == S_OK, "GetContainerFormats failed, hr=%x\n", hr);
+    ok(count == 1, "unexpected count %d\n", count);
+
+    count = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetContainerFormats(reader_info, 10, container_formats, &count);
+    ok(hr == S_OK, "GetContainerFormats failed, hr=%x\n", hr);
+    ok(count == min(count, 10), "unexpected count %d\n", count);
+
+    count = size = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetPatterns(reader_info, &GUID_ContainerFormatPng,
+        0, NULL, &count, &size);
+    ok(hr == WINCODEC_ERR_COMPONENTNOTFOUND || broken(hr == S_OK) /* Windows XP */,
+        "GetPatterns failed, hr=%x\n", hr);
+    ok(count == 0xdeadbeef, "unexpected count %d\n", count);
+    ok(size == 0xdeadbeef, "unexpected size %d\n", size);
+
+    count = size = 0xdeadbeef;
+    hr = IWICMetadataReaderInfo_GetPatterns(reader_info, &GUID_MetadataFormatXMP,
+        0, NULL, &count, &size);
+    ok(hr == S_OK, "GetPatterns failed, hr=%x\n", hr);
+    ok(count == 1, "unexpected count %d\n", count);
+    ok(size > sizeof(WICMetadataPattern), "unexpected size %d\n", size);
+
+    if (hr == S_OK)
+    {
+        patterns = HeapAlloc(GetProcessHeap(), 0, size);
+
+        count = size = 0xdeadbeef;
+        hr = IWICMetadataReaderInfo_GetPatterns(reader_info, &GUID_MetadataFormatXMP,
+            size-1, patterns, &count, &size);
+        ok(hr == S_OK, "GetPatterns failed, hr=%x\n", hr);
+        ok(count == 1, "unexpected count %d\n", count);
+        ok(size > sizeof(WICMetadataPattern), "unexpected size %d\n", size);
+
+        count = size = 0xdeadbeef;
+        hr = IWICMetadataReaderInfo_GetPatterns(reader_info, &GUID_MetadataFormatXMP,
+            size, patterns, &count, &size);
+        ok(hr == S_OK, "GetPatterns failed, hr=%x\n", hr);
+        ok(count == 1, "unexpected count %d\n", count);
+        ok(size == sizeof(WICMetadataPattern) + patterns->Length * 2, "unexpected size %d\n", size);
+
+        HeapFree(GetProcessHeap(), 0, patterns);
+    }
+
+    IWICMetadataReaderInfo_Release(reader_info);
+
+    IWICComponentInfo_Release(info);
+
+    IWICImagingFactory_Release(factory);
+}
+
 START_TEST(info)
 {
     CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
 
     test_decoder_info();
+    test_reader_info();
 
     CoUninitialize();
 }
