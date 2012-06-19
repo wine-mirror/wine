6cc99681257f0825efa3fc5697fdd957ab9d950e
Lucas Zawacki <lfzawacki@gmail.com>
2012-06-19 11:42:08 -0300

    joy.cpl: Hardcode axis name strings that were in joy.rc.

--- dlls/joy.cpl/main.c
+++ dlls/joy.cpl/main.c
@@ -308,7 +308,9 @@ static void draw_joystick_axes(HWND hwnd, struct JoystickData* data)
     struct Joystick *joy;
     DIPROPRANGE propRange;
     HINSTANCE hinst = (HINSTANCE) GetWindowLongPtrW(hwnd, GWLP_HINSTANCE);
-    static WCHAR button_class[] = {'B','u','t','t','o','n','\0'};
+    static const WCHAR button_class[] = {'B','u','t','t','o','n','\0'};
+    static const WCHAR axes_names[TEST_MAX_AXES][7] = { {'X',',','Y','\0'}, {'R','x',',','R','y','\0'}, {'Z',',','R','z','\0'} };
+    static const DWORD axes_idc[TEST_MAX_AXES] = { IDC_TESTGROUPXY, IDC_TESTGROUPRXRY, IDC_TESTGROUPZRZ };
 
     /* Set axis range to ease the GUI visualization */
     for (i = 0; i < data->num_joysticks; i++)
@@ -326,6 +328,9 @@ static void draw_joystick_axes(HWND hwnd, struct JoystickData* data)
 
     for (i = 0; i < TEST_MAX_AXES; i++)
     {
+        /* Set axis box name */
+        SetWindowTextW(GetDlgItem(hwnd, axes_idc[i]), axes_names[i]);
+
         data->axes[i] = CreateWindowW( button_class, NULL, WS_CHILD | WS_VISIBLE,
             TEST_AXIS_X + TEST_NEXT_AXIS_X*i, TEST_AXIS_Y,
             TEST_AXIS_SIZE_X, TEST_AXIS_SIZE_Y,
