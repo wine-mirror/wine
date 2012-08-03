fc4b2f6ceca61ca7738b81f6e843509d79977314
Detlef Riekenberg <wine.dev@web.de>
2012-08-03 19:16:35 +0200

    mpr/tests: Handle result from the AppV virtual drive.

--- dlls/mpr/tests/mpr.c
+++ dlls/mpr/tests/mpr.c
@@ -42,7 +42,11 @@ static void test_WNetGetUniversalName(void)
         if(drive_type == DRIVE_REMOTE)
             ok(ret == WN_NO_ERROR, "WNetGetUniversalNameA failed: %08x\n", ret);
         else
-            ok(ret == ERROR_NOT_CONNECTED, "WNetGetUniversalNameA gave wrong error: %08x\n", ret);
+            /* WN_NO_NET_OR_BAD_PATH (DRIVE_FIXED) returned from the virtual drive (usual Q:)
+               created by the microsoft application virtualization client */
+            ok((ret == WN_NOT_CONNECTED) || (ret == WN_NO_NET_OR_BAD_PATH),
+                "WNetGetUniversalNameA(%s, ...) returned %u (drive_type: %u)\n",
+                driveA, ret, drive_type);
 
         ok(info_size == sizeof(buffer), "Got wrong size: %u\n", info_size);
 
@@ -53,7 +57,9 @@ static void test_WNetGetUniversalName(void)
         if(drive_type == DRIVE_REMOTE)
             ok(ret == WN_NO_ERROR, "WNetGetUniversalNameW failed: %08x\n", ret);
         else
-            ok(ret == ERROR_NOT_CONNECTED, "WNetGetUniversalNameW gave wrong error: %08x\n", ret);
+            ok((ret == WN_NOT_CONNECTED) || (ret == WN_NO_NET_OR_BAD_PATH),
+                "WNetGetUniversalNameW(%s, ...) returned %u (drive_type: %u)\n",
+                wine_dbgstr_w(driveW), ret, drive_type);
 
         ok(info_size == sizeof(buffer), "Got wrong size: %u\n", info_size);
     }
