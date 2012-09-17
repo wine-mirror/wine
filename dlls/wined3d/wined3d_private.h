d574d639a42e9b57a0a51a77bad4a277d0dc5080
Henri Verbeet <hverbeet@codeweavers.com>
2012-09-17 12:22:23 +0200

    wined3d: Recognize the SM4 dcl_maxOutputVertexCount opcode.

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -439,6 +439,7 @@ enum WINED3D_SHADER_INSTRUCTION_HANDLER
     WINED3DSIH_CRS,
     WINED3DSIH_CUT,
     WINED3DSIH_DCL,
+    WINED3DSIH_DCL_VERTICES_OUT,
     WINED3DSIH_DEF,
     WINED3DSIH_DEFB,
     WINED3DSIH_DEFI,
@@ -666,6 +667,7 @@ struct wined3d_shader_instruction
     union
     {
         struct wined3d_shader_semantic semantic;
+        UINT count;
     } declaration;
 };
 
