8776e1c0e1597fec169d169888660deb5bb08c2c
Henri Verbeet <hverbeet@codeweavers.com>
2012-11-24 17:17:09 +0100

    winex11: Detect broken NVIDIA RandR setups.

--- dlls/winex11.drv/xrandr.c
+++ dlls/winex11.drv/xrandr.c
@@ -24,6 +24,7 @@
 #include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(xrandr);
+WINE_DECLARE_DEBUG_CHANNEL(winediag);
 
 #ifdef SONAME_LIBXRANDR
 
@@ -416,6 +417,21 @@ static int xrandr12_init_modes(void)
     if (!output_info->nmode)
     {
         ERR("Output has no modes.\n");
+        goto done;
+    }
+
+    /* Recent (304.64, possibly earlier) versions of the nvidia driver only
+     * report a DFP's native mode through RandR 1.2 / 1.3. Standard DMT modes
+     * are only listed through RandR 1.0 / 1.1. This is completely useless,
+     * but NVIDIA considers this a feature, so it's unlikely to change. The
+     * best we can do is to fall back to RandR 1.0 and encourage users to
+     * consider more cooperative driver vendors when we detect such a
+     * configuration. */
+    if (output_info->nmode == 1 && XQueryExtension( gdi_display, "NV-CONTROL", &i, &j, &ret ))
+    {
+        ERR_(winediag)("Broken NVIDIA RandR detected, falling back to RandR 1.0. "
+                       "Please consider using the Nouveau driver instead.\n");
+        ret = -1;
         goto done;
     }
 
