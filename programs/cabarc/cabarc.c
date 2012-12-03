b1aeb9dca10772d78311cb645e424333f08576ba
Christian Costa <titan.costa@gmail.com>
2012-12-03 09:18:24 +0100

    cabarc: Add some debug messages.

--- programs/cabarc/cabarc.c
+++ programs/cabarc/cabarc.c
@@ -467,7 +467,11 @@ static int extract_cabinet( char *cab_dir )
     HFDI fdi = FDICreate( cab_alloc, cab_free, fdi_open, fdi_read,
                           fdi_write, fdi_close, fdi_lseek, cpuUNKNOWN, &erf );
 
-    if (!FDICopy( fdi, opt_cab_file, cab_dir, 0, extract_notify, NULL, NULL )) ret = GetLastError();
+    if (!FDICopy( fdi, opt_cab_file, cab_dir, 0, extract_notify, NULL, NULL ))
+    {
+        ret = GetLastError();
+        WINE_WARN("FDICopy() failed: code %u\n", ret);
+    }
     FDIDestroy( fdi );
     return ret;
 }
@@ -685,7 +689,6 @@ int wmain( int argc, WCHAR *argv[] )
         return 1;
     }
     file_part[-1] = 0;
-    cab_free( cab_file );
 
     /* map slash to backslash in all file arguments */
     for (i = 1; i < argc; i++)
@@ -713,6 +716,7 @@ int wmain( int argc, WCHAR *argv[] )
                 argv[--argc] = NULL;
             }
         }
+        WINE_TRACE("Extracting file(s) from cabinet %s\n", wine_dbgstr_a(cab_file));
         return extract_cabinet( buffer );
     default:
         usage();
