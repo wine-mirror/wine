a6289a21a1ca3aa184d012cae01764ea31831647
Henri Verbeet <hverbeet@codeweavers.com>
2012-12-10 21:54:34 +0100

    ddraw: Avoid LPDDDEVICEIDENTIFIER2.

--- include/ddraw.h
+++ include/ddraw.h
@@ -1788,7 +1788,7 @@ DECLARE_INTERFACE_(IDirectDraw7,IUnknown)
 /*60*/    STDMETHOD(GetSurfaceFromDC)(THIS_ HDC dc, struct IDirectDrawSurface7 **surface) PURE;
 /*64*/    STDMETHOD(RestoreAllSurfaces)(THIS) PURE;
 /*68*/    STDMETHOD(TestCooperativeLevel)(THIS) PURE;
-/*6c*/    STDMETHOD(GetDeviceIdentifier)(THIS_ LPDDDEVICEIDENTIFIER2 pDDDI, DWORD dwFlags) PURE;
+/*6c*/    STDMETHOD(GetDeviceIdentifier)(THIS_ DDDEVICEIDENTIFIER2 *identifier, DWORD flags) PURE;
           /* added in v7 */
 /*70*/    STDMETHOD(StartModeTest)(THIS_ LPSIZE pModes, DWORD dwNumModes, DWORD dwFlags) PURE;
 /*74*/    STDMETHOD(EvaluateMode)(THIS_ DWORD dwFlags, DWORD  *pTimeout) PURE;
