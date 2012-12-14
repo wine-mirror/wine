4f32f8e185949f2cb7bcee4f218f4724d5b823d5
Andrew Eikum <aeikum@codeweavers.com>
2012-12-14 08:47:11 -0600

    winealsa.drv: Be less strict about wValidBitsPerSample.

--- dlls/winealsa.drv/mmdevdrv.c
+++ dlls/winealsa.drv/mmdevdrv.c
@@ -976,10 +976,8 @@ static snd_pcm_format_t alsa_format(const WAVEFORMATEX *fmt)
            fmt->wBitsPerSample != fmtex->Samples.wValidBitsPerSample){
             if(fmtex->Samples.wValidBitsPerSample == 20 && fmt->wBitsPerSample == 24)
                 format = SND_PCM_FORMAT_S20_3LE;
-            else{
+            else
                 WARN("Unsupported ValidBits: %u\n", fmtex->Samples.wValidBitsPerSample);
-                format = SND_PCM_FORMAT_UNKNOWN;
-            }
         }
     }else if(fmt->wFormatTag == WAVE_FORMAT_IEEE_FLOAT ||
             (fmt->wFormatTag == WAVE_FORMAT_EXTENSIBLE &&
