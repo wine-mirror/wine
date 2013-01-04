732376d5b9c06987dca08c0894806611395fe802
Jacek Caban <jacek@codeweavers.com>
2013-01-04 11:34:15 +0100

    atl: Added support for category maps in AtlModuleRegisterServer.

--- dlls/atl/atl_main.c
+++ dlls/atl/atl_main.c
@@ -220,6 +220,17 @@ HRESULT WINAPI AtlModuleRegisterServer(_ATL_MODULEW* pM, BOOL bRegTypeLib, const
             hRes = obj->pfnUpdateRegistry(TRUE); /* register */
             if (FAILED(hRes))
                 return hRes;
+
+            if(pM->cbSize > ATLVer1Size) {
+                const struct _ATL_CATMAP_ENTRY *catmap;
+
+                catmap = ((const _ATL_OBJMAP_ENTRYW*)obj)->pfnGetCategoryMap();
+                if(catmap) {
+                    hRes = AtlRegisterClassCategoriesHelper(obj->pclsid, catmap, TRUE);
+                    if(FAILED(hRes))
+                        return hRes;
+                }
+            }
         }
     }
 
