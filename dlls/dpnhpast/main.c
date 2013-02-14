79e1723e72a196c614e1dc4467c036ce9f1f6c2e
Alexandre Julliard <julliard@winehq.org>
2013-02-14 21:36:39 +0100

    dpnhpast: Remove an unnecessary DllMain entry point.

--- dlls/dpnhpast/main.c
+++ dlls/dpnhpast/main.c
@@ -28,29 +28,6 @@
 WINE_DEFAULT_DEBUG_CHANNEL(dpnhpast);
 
 /******************************************************************
- *		DllMain
- *
- *
- */
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-	TRACE("(%p, %d, %p)\n",hinstDLL,fdwReason,lpvReserved);
-
-	if (fdwReason == DLL_PROCESS_ATTACH)
-	{
-            DisableThreadLibraryCalls(hinstDLL);
-		/* FIXME: Initialisation */
-	}
-	else if (fdwReason == DLL_PROCESS_DETACH)
-	{
-		/* FIXME: Cleanup */
-	}
-
-	return TRUE;
-}
-
-
-/******************************************************************
  *		DirectPlayNATHelpCreate (DPNHPAST.1)
  *
  *
