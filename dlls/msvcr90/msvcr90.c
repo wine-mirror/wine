5d88f780dd4bbbde0ff27075b58321d7347f6d4b
Piotr Caban <piotr@codeweavers.com>
2013-02-18 10:26:01 +0100

    msvcr90: Prefer builtin version.

--- dlls/msvcr90/msvcr90.c
+++ dlls/msvcr90/msvcr90.c
@@ -31,9 +31,6 @@ BOOL WINAPI DllMain(HINSTANCE hdll, DWORD reason, LPVOID reserved)
 {
     switch (reason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
-
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hdll);
         _set_printf_count_output(0);
