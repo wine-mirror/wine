a424968482d10356a5987bc08c66061dca5402e3
Vincent Povirk <vincent@codeweavers.com>
2013-03-12 15:54:41 -0500

    gdiplus: Correct EMF record size calculation.

--- dlls/gdiplus/metafile.c
+++ dlls/gdiplus/metafile.c
@@ -410,7 +410,7 @@ GpStatus WINGDIPAPI GdipPlayMetafileRecord(GDIPCONST GpMetafile *metafile,
             if (record)
             {
                 record->iType = recordType;
-                record->nSize = dataSize;
+                record->nSize = dataSize + 8;
                 memcpy(record->dParm, data, dataSize);
 
                 PlayEnhMetaFileRecord(metafile->playback_dc, metafile->handle_table,
