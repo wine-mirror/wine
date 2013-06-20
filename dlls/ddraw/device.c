05de3f8cee3758f87f66e1963d8d6a009e68e2b6
Stefan Dösinger <stefan@codeweavers.com>
2013-06-20 14:00:00 +0200

    ddraw: SPECULARENABLE defaults to true in IDirect3DDevice2.

--- dlls/ddraw/device.c
+++ dlls/ddraw/device.c
@@ -6728,6 +6728,8 @@ static HRESULT d3d_device_init(struct d3d_device *device, struct ddraw *ddraw,
             d3d_device_update_depth_stencil(device));
     if (version == 1) /* Color keying is initially enabled for version 1 devices. */
         wined3d_device_set_render_state(ddraw->wined3d_device, WINED3D_RS_COLORKEYENABLE, TRUE);
+    else if (version == 2)
+        wined3d_device_set_render_state(ddraw->wined3d_device, WINED3D_RS_SPECULARENABLE, TRUE);
 
     return D3D_OK;
 }
