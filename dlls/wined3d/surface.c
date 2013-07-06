b940ceb1b5350ff3f2970dd60feac8442a9a40b8
Stefan Dösinger <stefan@codeweavers.com>
2013-07-06 22:08:39 +0200

    wined3d: Remove another glFinish
    I don't think this is needed any more.

--- dlls/wined3d/surface.c
+++ dlls/wined3d/surface.c
@@ -825,9 +825,7 @@ static void surface_blt_fbo(const struct wined3d_device *device,
             dst_rect.left, dst_rect.top, dst_rect.right, dst_rect.bottom, GL_COLOR_BUFFER_BIT, gl_filter);
     checkGLcall("glBlitFramebuffer()");
 
-    if (wined3d_settings.cs_multithreaded)
-        gl_info->gl_ops.gl.p_glFinish();
-    else if (wined3d_settings.strict_draw_ordering
+    if (wined3d_settings.strict_draw_ordering
             || (dst_location == WINED3D_LOCATION_DRAWABLE
             && dst_surface->container->swapchain->front_buffer == dst_surface->container))
         gl_info->gl_ops.gl.p_glFlush();
