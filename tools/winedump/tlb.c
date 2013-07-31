f929d2c2bc2cf361673598e99e0cda7400635895
Piotr Caban <piotr@codeweavers.com>
2013-07-31 18:11:01 +0200

    winedump: Rename lib typelib segment to GuidHashTab.

--- tools/winedump/tlb.c
+++ tools/winedump/tlb.c
@@ -346,9 +346,9 @@ static int dump_msft_reftabs(seg_t *seg)
     return -1;
 }
 
-static int dump_msft_libtab(seg_t *seg)
+static int dump_msft_guidhashtab(seg_t *seg)
 {
-    print_begin_block("LibTab");
+    print_begin_block("GuidHashTab");
 
     dump_binary(seg->length); /* FIXME */
 
@@ -659,7 +659,7 @@ seg_t segdir[] = {
     {"ImpInfo",           dump_msft_impinfos, -1, -1},
     {"ImpFiles",          dump_msft_impfiles, -1, -1},
     {"RefTab",            dump_msft_reftabs, -1, -1},
-    {"LibTab",            dump_msft_libtab, -1, -1},
+    {"GuidHashTab",       dump_msft_guidhashtab, -1, -1},
     {"GuidTab",           dump_msft_guidtab, -1, -1},
     {"NameHashTab",       dump_msft_namehashtab, -1, -1},
     {"pNameTab",          dump_msft_nametab, -1, -1},
