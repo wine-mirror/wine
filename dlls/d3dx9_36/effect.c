34c70247906ca8fe965ced1e02c72f519e73eb04
Henri Verbeet <hverbeet@codeweavers.com>
2013-08-26 09:23:41 +0200

    d3dx9: Simplify D3DXCreateEffectCompilerFromResourceW().

--- dlls/d3dx9_36/effect.c
+++ dlls/d3dx9_36/effect.c
@@ -5704,26 +5704,19 @@ HRESULT WINAPI D3DXCreateEffectCompilerFromResourceW(HMODULE srcmodule, const WC
         ID3DXEffectCompiler **effectcompiler, ID3DXBuffer **parseerrors)
 {
     HRSRC resinfo;
+    void *buffer;
+    DWORD size;
 
-    TRACE("(%p, %s): relay\n", srcmodule, debugstr_w(srcresource));
+    TRACE("srcmodule %p, srcresource %s, defines %p, include %p, flags %#x, effectcompiler %p, parseerrors %p.\n",
+            srcmodule, debugstr_w(srcresource), defines, include, flags, effectcompiler, parseerrors);
 
-    resinfo = FindResourceW(srcmodule, srcresource, (LPCWSTR) RT_RCDATA);
+    if (!(resinfo = FindResourceW(srcmodule, srcresource, (const WCHAR *)RT_RCDATA)))
+        return D3DXERR_INVALIDDATA;
 
-    if (resinfo)
-    {
-        LPVOID buffer;
-        HRESULT ret;
-        DWORD size;
+    if (FAILED(load_resource_into_memory(srcmodule, resinfo, &buffer, &size)))
+        return D3DXERR_INVALIDDATA;
 
-        ret = load_resource_into_memory(srcmodule, resinfo, &buffer, &size);
-
-        if (FAILED(ret))
-            return D3DXERR_INVALIDDATA;
-
-        return D3DXCreateEffectCompiler(buffer, size, defines, include, flags, effectcompiler, parseerrors);
-    }
-
-    return D3DXERR_INVALIDDATA;
+    return D3DXCreateEffectCompiler(buffer, size, defines, include, flags, effectcompiler, parseerrors);
 }
 
 HRESULT WINAPI D3DXDisassembleEffect(ID3DXEffect *effect, BOOL enable_color_code, ID3DXBuffer **disassembly)
