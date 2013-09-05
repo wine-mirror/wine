b88daee3bce083f8121117e9d0bece715eb8490c
Stefan Dösinger <stefan@codeweavers.com>
2013-09-05 10:42:14 +0200

    ddraw: Create managed pool textures.
    The surfaces are already in WINED3D_POOL_MANAGED.

--- dlls/ddraw/surface.c
+++ dlls/ddraw/surface.c
@@ -5607,9 +5607,9 @@ HRESULT ddraw_surface_create_texture(struct ddraw_surface *surface, DWORD surfac
     else
         layers = 1;
 
-    /* DDSCAPS_SYSTEMMEMORY textures are in WINED3D_POOL_SYSTEM_MEM.
-     * Should I forward the MANAGED cap to the managed pool? */
-    if (desc->ddsCaps.dwCaps & DDSCAPS_SYSTEMMEMORY)
+    if (desc->ddsCaps.dwCaps2 & DDSCAPS2_TEXTUREMANAGE)
+        pool = WINED3D_POOL_MANAGED;
+    else if (desc->ddsCaps.dwCaps & DDSCAPS_SYSTEMMEMORY)
         pool = WINED3D_POOL_SYSTEM_MEM;
     else
         pool = WINED3D_POOL_DEFAULT;
