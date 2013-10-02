f31d2485beadc49f5e83770c69d9619bec05fd03
Henri Verbeet <hverbeet@codeweavers.com>
2013-10-02 09:55:25 +0200

    d3d9: Don't mask out internal wined3d usage flags in surface_init().

--- dlls/d3d9/surface.c
+++ dlls/d3d9/surface.c
@@ -424,7 +424,7 @@ HRESULT surface_init(struct d3d9_surface *surface, struct d3d9_device *device, U
 
     wined3d_mutex_lock();
     hr = wined3d_surface_create(device->wined3d_device, width, height, wined3dformat_from_d3dformat(format),
-            usage & WINED3DUSAGE_MASK, (enum wined3d_pool)pool, multisample_type, multisample_quality,
+            usage, (enum wined3d_pool)pool, multisample_type, multisample_quality,
             flags, surface, &d3d9_surface_wined3d_parent_ops, &surface->wined3d_surface);
     wined3d_mutex_unlock();
     if (FAILED(hr))
