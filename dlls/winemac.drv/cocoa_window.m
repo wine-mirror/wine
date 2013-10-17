771fcd894a62f53cb3862a9d97f28d2ce7c46a30
Kevin Eaves <sevae@roadrunner.com>
2013-10-17 01:54:00 -0700

    winemac: Restore the window title when the window style is changed.

--- dlls/winemac.drv/cocoa_window.m
+++ dlls/winemac.drv/cocoa_window.m
@@ -641,6 +641,7 @@ static inline NSUInteger adjusted_modifiers_for_option_behavior(NSUInteger modif
 
         if (newStyle != currentStyle)
         {
+            NSString* title = [[[self title] copy] autorelease];
             BOOL showingButtons = (currentStyle & (NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask)) != 0;
             BOOL shouldShowButtons = (newStyle & (NSClosableWindowMask | NSMiniaturizableWindowMask | NSResizableWindowMask)) != 0;
             if (shouldShowButtons != showingButtons && !((newStyle ^ currentStyle) & NSClosableWindowMask))
@@ -654,6 +655,9 @@ static inline NSUInteger adjusted_modifiers_for_option_behavior(NSUInteger modif
             }
             [self setStyleMask:newStyle];
             [self adjustFullScreenBehavior:[self collectionBehavior]];
+
+            if ([[self title] length] == 0 && [title length] > 0)
+                [self setTitle:title];
         }
 
         [self adjustFeaturesForState];
