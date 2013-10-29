8b11533dcf1724512f3fa17bd8650a6cfefbba41
Alexandre Julliard <julliard@winehq.org>
2013-10-29 10:23:56 +0100

    advapi32/tests: Use the function pointer to call RegDeleteKeyExA.

--- dlls/advapi32/tests/registry.c
+++ dlls/advapi32/tests/registry.c
@@ -2105,7 +2105,7 @@ static void test_redirection(void)
     err = RegOpenKeyExA(HKEY_CLASSES_ROOT, "Interface", 0, KEY_ALL_ACCESS, &native);
     ok(err == ERROR_SUCCESS, "got %i\n", err);
 
-    RegDeleteKeyExA(native, "AWineTest", 0, 0);
+    pRegDeleteKeyExA(native, "AWineTest", 0, 0);
 
     /* write subkey in opposite bit mode */
     err = RegOpenKeyExA(HKEY_CLASSES_ROOT, "Interface", 0, KEY_ALL_ACCESS | opposite, &op_key);
@@ -2128,7 +2128,7 @@ static void test_redirection(void)
             broken(err == ERROR_SUCCESS), /* before Win7, HKCR is reflected instead of redirected */
             "got %i\n", err);
 
-    err = RegDeleteKeyExA(op_key, "AWineTest", opposite, 0);
+    err = pRegDeleteKeyExA(op_key, "AWineTest", opposite, 0);
     ok(err == ERROR_SUCCESS, "got %i\n", err);
 
     RegCloseKey(op_key);
