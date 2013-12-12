wine-mirror/wine

commit 74e3f516bc331205474835811d3984b9c53b0f35

ddraw: Attach the first surface in the swap chain to the last one.

dlls/ddraw/surface.c
@@ -469,7 +469,8 @@ static void ddraw_surface_cleanup(struct ddraw_surface *surface)
 
         surf = surface->complex_array[i];
         surface->complex_array[i] = NULL;
-        ddraw_surface_cleanup(surf);
+        if (!surf->is_complex_root)
+            ddraw_surface_cleanup(surf);
     }
 
     if (surface->device1)
@@ -4724,19 +4725,22 @@ static HRESULT WINAPI ddraw_surface7_SetPalette(IDirectDrawSurface7 *iface, IDir
         DDSCAPS2 caps2 = { DDSCAPS_FLIP, 0, 0, 0 };
 
         surf = This;
-        while(1)
+        for (;;)
         {
             IDirectDrawSurface7 *attach;
-            HRESULT hr;
-            hr = ddraw_surface7_GetAttachedSurface(&surf->IDirectDrawSurface7_iface, &caps2, &attach);
-            if(hr != DD_OK)
+
+            if (FAILED(hr = ddraw_surface7_GetAttachedSurface(&surf->IDirectDrawSurface7_iface, &caps2, &attach)))
+                break;
+
+            surf = impl_from_IDirectDrawSurface7(attach);
+            if (surf == This)
             {
+                ddraw_surface7_Release(attach);
                 break;
             }
 
-            TRACE("Setting palette on %p\n", attach);
+            TRACE("Setting palette on %p.\n", attach);
             ddraw_surface7_SetPalette(attach, Pal);
-            surf = impl_from_IDirectDrawSurface7(attach);
             ddraw_surface7_Release(attach);
         }
     }
@@ -6002,6 +6006,7 @@ HRESULT ddraw_surface_create(struct ddraw *ddraw, const DDSURFACEDESC2 *surface_
             *attach = last;
             attach = &last->complex_array[0];
         }
+        *attach = root;
     }
 
     if (surface_desc->ddsCaps.dwCaps & DDSCAPS_PRIMARYSURFACE)

dlls/ddraw/tests/ddraw1.c
@@ -3605,10 +3605,9 @@ static void test_flip(void)
             "Got unexpected caps %#x.\n", surface_desc.ddsCaps.dwCaps);
 
     hr = IDirectDrawSurface_GetAttachedSurface(backbuffer3, &caps, &surface);
-    todo_wine ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
-    todo_wine ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
-    if (surface)
-        IDirectDrawSurface_Release(surface);
+    ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
+    ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
+    IDirectDrawSurface_Release(surface);
 
     memset(&surface_desc, 0, sizeof(surface_desc));
     surface_desc.dwSize = sizeof(surface_desc);

dlls/ddraw/tests/ddraw2.c
@@ -4286,10 +4286,9 @@ static void test_flip(void)
             "Got unexpected caps %#x.\n", surface_desc.ddsCaps.dwCaps);
 
     hr = IDirectDrawSurface_GetAttachedSurface(backbuffer3, &caps, &surface);
-    todo_wine ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
-    todo_wine ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
-    if (surface)
-        IDirectDrawSurface_Release(surface);
+    ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
+    ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
+    IDirectDrawSurface_Release(surface);
 
     memset(&surface_desc, 0, sizeof(surface_desc));
     surface_desc.dwSize = sizeof(surface_desc);

dlls/ddraw/tests/ddraw4.c
@@ -4890,10 +4890,9 @@ static void test_flip(void)
             "Got unexpected caps %#x.\n", surface_desc.ddsCaps.dwCaps);
 
     hr = IDirectDrawSurface4_GetAttachedSurface(backbuffer3, &caps, &surface);
-    todo_wine ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
-    todo_wine ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
-    if (surface)
-        IDirectDrawSurface4_Release(surface);
+    ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
+    ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
+    IDirectDrawSurface4_Release(surface);
 
     memset(&surface_desc, 0, sizeof(surface_desc));
     surface_desc.dwSize = sizeof(surface_desc);

dlls/ddraw/tests/ddraw7.c
@@ -4777,10 +4777,9 @@ static void test_flip(void)
             "Got unexpected caps %#x.\n", surface_desc.ddsCaps.dwCaps);
 
     hr = IDirectDrawSurface7_GetAttachedSurface(backbuffer3, &caps, &surface);
-    todo_wine ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
-    todo_wine ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
-    if (surface)
-        IDirectDrawSurface7_Release(surface);
+    ok(SUCCEEDED(hr), "Failed to get attached surface, hr %#x.\n", hr);
+    ok(surface == primary, "Got unexpected surface %p, expected %p.\n", surface, primary);
+    IDirectDrawSurface7_Release(surface);
 
     memset(&surface_desc, 0, sizeof(surface_desc));
     surface_desc.dwSize = sizeof(surface_desc);

commit 9651079ff9bd32547408d1b7b7e0e14ff994371e

ddraw: Don't set the palette on attached backbuffers in ddraw_surface7_SetPalette().
Tests show this is just wrong. This patch fixes a regression introduced by
commit 74e3f516bc331205474835811d3984b9c53b0f35.

dlls/ddraw/surface.c
@@ -4695,7 +4695,6 @@ static HRESULT WINAPI ddraw_surface7_SetPalette(IDirectDrawSurface7 *iface, IDir
     struct ddraw_surface *surface = impl_from_IDirectDrawSurface7(iface);
     struct ddraw_palette *palette_impl = unsafe_impl_from_IDirectDrawPalette(palette);
     struct ddraw_palette *prev;
-    HRESULT hr;
 
     TRACE("iface %p, palette %p.\n", iface, palette);
 
@@ -4720,34 +4719,6 @@ static HRESULT WINAPI ddraw_surface7_SetPalette(IDirectDrawSurface7 *iface, IDir
     if ((surface->surface_desc.ddsCaps.dwCaps & DDSCAPS_PRIMARYSURFACE) && surface->ddraw->wined3d_frontbuffer)
         wined3d_surface_set_palette(surface->ddraw->wined3d_frontbuffer,
                 palette_impl ? palette_impl->wineD3DPalette : NULL);
-
-    /* If this is a front buffer, also update the back buffers
-     * TODO: How do things work for palettized cube textures? */
-    if (surface->surface_desc.ddsCaps.dwCaps & DDSCAPS_FRONTBUFFER)
-    {
-        /* For primary surfaces the tree is just a list, so the simpler scheme fits too */
-        DDSCAPS2 caps2 = { DDSCAPS_FLIP, 0, 0, 0 };
-        struct ddraw_surface *current = surface;
-
-        for (;;)
-        {
-            IDirectDrawSurface7 *attach;
-
-            if (FAILED(hr = ddraw_surface7_GetAttachedSurface(&current->IDirectDrawSurface7_iface, &caps2, &attach)))
-                break;
-
-            current = impl_from_IDirectDrawSurface7(attach);
-            if (current == surface)
-            {
-                ddraw_surface7_Release(attach);
-                break;
-            }
-
-            TRACE("Setting palette on %p.\n", attach);
-            ddraw_surface7_SetPalette(attach, palette);
-            ddraw_surface7_Release(attach);
-        }
-    }
 
     wined3d_mutex_unlock();
 