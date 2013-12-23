136549dd58bba4f869690034b4d92042578db49b
Nikolay Sivov <nsivov@codeweavers.com>
2013-12-23 16:30:05 +0400

    include: Added ID2D1SimplifiedGeometrySink definition.

---
+++ include/d2dbasetypes.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2013 Nikolay Sivov for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+typedef struct D2D_POINT_2F
+{
+    FLOAT x;
+    FLOAT y;
+} D2D_POINT_2F;
