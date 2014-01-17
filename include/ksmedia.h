2190601ca20a5088a1cd6e50a0587e6fe1cf2c2c
Jacek Caban <jacek@codeweavers.com>
2014-01-17 14:40:50 +0100

    include: Fixed eConnTypeRJ11Modem declaration.

--- include/ksmedia.h
+++ include/ksmedia.h
@@ -57,7 +57,7 @@ typedef enum
     eConnTypeOtherAnalog,
     eConnTypeMultichannelAnalogDIN,
     eConnTypeXlrProfessional,
-    eConnTypeRj11Modem,
+    eConnTypeRJ11Modem,
     eConnTypeCombination
 } EPcxConnectionType;
 
