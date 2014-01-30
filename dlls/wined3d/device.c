82b38139eb21a6898e4621eb7405e99189c09b18
Henri Verbeet <hverbeet@codeweavers.com>
2014-01-30 17:32:04 +0100

    wined3d: Also destroy the logo and cursor textures in wined3d_device_reset() when resetting state.

--- dlls/wined3d/device.c
+++ dlls/wined3d/device.c
@@ -4137,7 +4137,19 @@ HRESULT CDECL wined3d_device_reset(struct wined3d_device *device,
     }
 
     if (reset_state)
+    {
+        if (device->logo_texture)
+        {
+            wined3d_texture_decref(device->logo_texture);
+            device->logo_texture = NULL;
+        }
+        if (device->cursor_texture)
+        {
+            wined3d_texture_decref(device->cursor_texture);
+            device->cursor_texture = NULL;
+        }
         state_unbind_resources(&device->state);
+    }
 
     if (device->fb.render_targets)
     {
