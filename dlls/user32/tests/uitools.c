6ac11c60f01bf4286915f8e55f150f936a4db6e7
Javier Cantero <jcantero@escomposlinux.org>
2014-03-25 18:38:49 +0100

    user32/tests: Add SubtractRect() test.

--- dlls/user32/tests/uitools.c
+++ dlls/user32/tests/uitools.c
@@ -63,7 +63,41 @@ static void test_FillRect(void)
     ReleaseDC(0, hdc);
 }
 
+static void test_SubtractRect(void)
+{
+    RECT rect1;
+    RECT rect2;
+    RECT rectr;
+    BOOL result;
+
+    /* case 1: source rectangles don't intersect */
+    SetRect(&rect1, 50, 50, 150, 100);
+    SetRect(&rect2, 250, 200, 1500, 1000);
+    result = SubtractRect(&rectr, &rect1, &rect2);
+    ok(result, "SubtractRect returned FALSE but subtraction should not be empty\n");
+    ok(result && rectr.left == 50 && rectr.top == 50 && rectr.right ==150
+        && rectr.bottom == 100, "wrong rect subtraction of SubtractRect "
+        "(dest rect={%d, %d, %d, %d})\n", rectr.left, rectr.top, rectr.right, rectr.bottom);
+
+    /* case 2: source rect 2 partially overlaps rect 1 */
+    SetRect(&rect1, 2431, 626, 3427, 1608);
+    SetRect(&rect2, 2499, 626, 3427, 1608);
+    result = SubtractRect(&rectr, &rect1, &rect2);
+    ok(result, "SubtractRect returned FALSE but subtraction should not be empty\n");
+    ok(result && rectr.left == 2431 && rectr.top == 626 && rectr.right == 2499
+        && rectr.bottom == 1608, "wrong rect subtraction of SubtractRect "
+        "(dest rect={%d, %d, %d, %d})\n", rectr.left, rectr.top, rectr.right, rectr.bottom);
+
+    /* case 3: source rect 2 completely overlaps rect 1 */
+    SetRect(&rect1, 250, 250, 400, 500);
+    SetRect(&rect2, 50, 50, 1500, 1000);
+    result = SubtractRect(&rectr, &rect1, &rect2);
+    ok(!result, "SubtractRect returned TRUE but subtraction should be empty "
+        "(dest rect={%d, %d, %d, %d})\n", rectr.left, rectr.top, rectr.right, rectr.bottom);
+}
+
 START_TEST(uitools)
 {
     test_FillRect();
+    test_SubtractRect();
 }
