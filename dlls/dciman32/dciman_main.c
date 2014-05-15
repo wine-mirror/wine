16352f0aeeb4a5429a9b9b78f75baa20cefd564d
Alexandre Julliard <julliard@winehq.org>
2014-05-15 14:37:14 +0200

    dciman32: Remove unnecessary DllMain implementation.

--- dlls/dciman32/dciman_main.c
+++ dlls/dciman32/dciman_main.c
@@ -31,18 +31,6 @@
 WINE_DEFAULT_DEBUG_CHANNEL(dciman);
 
 /***********************************************************************
- *           DllEntryPoint (DCIMAN32.@)
- *
- * DCIMAN32 initialisation routine.
- */
-BOOL WINAPI DllMain( HINSTANCE inst, DWORD reason, LPVOID reserved )
-{
-    if (reason == DLL_PROCESS_ATTACH) DisableThreadLibraryCalls( inst );
-    return TRUE;
-}
-
-
-/***********************************************************************
  *		DCIOpenProvider (DCIMAN32.@)
  */
 HDC WINAPI
