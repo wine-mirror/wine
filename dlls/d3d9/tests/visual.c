fc84368f87faec3d974158c2239274d7f0d94c7e
Axel Davy <axel.davy@ens.fr>
2015-01-07 14:21:48 +0100

    d3d9/tests: Fix volume_dxt5_test getPixelColor check.
    getPixelColor should be called before Present, because the device
    swapchain was created with discard mode.

--- dlls/d3d9/tests/visual.c
+++ dlls/d3d9/tests/visual.c
@@ -15991,8 +15991,6 @@ static void volume_dxt5_test(void)
     hr = IDirect3DDevice9_EndScene(device);
     ok(SUCCEEDED(hr), "Failed to end scene, hr %#x.\n", hr);
 
-    hr = IDirect3DDevice9_Present(device, NULL, NULL, NULL, NULL);
-    ok(SUCCEEDED(hr), "Failed to present, hr %#x.\n", hr);
     for (i = 0; i < 4; i++)
     {
         color = getPixelColor(device, 80 + 160 * i, 240);
@@ -16000,6 +15998,8 @@ static void volume_dxt5_test(void)
                 "Expected color 0x%08x, got 0x%08x, case %u.\n", expected_colors[i], color, i);
     }
 
+    hr = IDirect3DDevice9_Present(device, NULL, NULL, NULL, NULL);
+    ok(SUCCEEDED(hr), "Failed to present, hr %#x.\n", hr);
     IDirect3DVolumeTexture9_Release(texture);
     refcount = IDirect3DDevice9_Release(device);
     ok(!refcount, "Device has %u references left.\n", refcount);
