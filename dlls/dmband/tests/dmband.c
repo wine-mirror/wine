468cb274d145bfe6ba547a91af2e4bb3a4bf0c4d
Michael Stefaniuc <mstefani@redhat.de>
2015-01-15 12:02:26 +0100

    dmband/tests: Remove an unused assigment (PVS-Studio).

--- dlls/dmband/tests/dmband.c
+++ dlls/dmband/tests/dmband.c
@@ -126,7 +126,7 @@ static void test_COM_bandtrack(void)
     ok(hr == S_OK, "QueryInterface for IID_IPersistStream failed: %08x\n", hr);
     refcount = IPersistStream_AddRef(ps);
     ok(refcount == 4, "refcount == %u, expected 4\n", refcount);
-    refcount = IPersistStream_Release(ps);
+    IPersistStream_Release(ps);
 
     hr = IDirectMusicTrack_QueryInterface(dmbt, &IID_IUnknown, (void**)&unk);
     ok(hr == S_OK, "QueryInterface for IID_IUnknown failed: %08x\n", hr);
