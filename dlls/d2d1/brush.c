bbba52dc345c5c0ef30e8769e111c1f40f6e712e
Henri Verbeet <hverbeet@codeweavers.com>
2015-01-29 10:27:21 +0100

    d2d1: Implement d2d_linear_gradient_brush_GetTransform().

--- dlls/d2d1/brush.c
+++ dlls/d2d1/brush.c
@@ -377,16 +377,11 @@ static float STDMETHODCALLTYPE d2d_linear_gradient_brush_GetOpacity(ID2D1LinearG
 static void STDMETHODCALLTYPE d2d_linear_gradient_brush_GetTransform(ID2D1LinearGradientBrush *iface,
         D2D1_MATRIX_3X2_F *transform)
 {
-    static const D2D1_MATRIX_3X2_F identity =
-    {
-        1.0f, 0.0f,
-        0.0f, 1.0f,
-        0.0f, 0.0f,
-    };
+    struct d2d_brush *brush = impl_from_ID2D1LinearGradientBrush(iface);
 
-    FIXME("iface %p, transform %p stub!\n", iface, transform);
+    TRACE("iface %p, transform %p.\n", iface, transform);
 
-    *transform = identity;
+    *transform = brush->transform;
 }
 
 static void STDMETHODCALLTYPE d2d_linear_gradient_brush_SetStartPoint(ID2D1LinearGradientBrush *iface,
