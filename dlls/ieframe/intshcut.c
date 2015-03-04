51e3750feae9af56526d20ce854362bf0012d379
Michael Stefaniuc <mstefani@redhat.de>
2015-03-04 09:38:43 +0100

    ieframe: Remove unused assignments (PVS-Studio).

--- dlls/ieframe/intshcut.c
+++ dlls/ieframe/intshcut.c
@@ -504,7 +504,7 @@ static HRESULT WINAPI PersistFile_Load(IPersistFile *pFile, LPCOLESTR pszFileNam
                                           STGM_READWRITE | STGM_SHARE_EXCLUSIVE,
                                           &pPropStg);
 
-            r = get_profile_string(str_header, str_iconfile, pszFileName, &iconfile);
+            get_profile_string(str_header, str_iconfile, pszFileName, &iconfile);
             if (iconfile != NULL)
             {
                 PROPSPEC ps;
@@ -522,7 +522,7 @@ static HRESULT WINAPI PersistFile_Load(IPersistFile *pFile, LPCOLESTR pszFileNam
                 CoTaskMemFree(iconfile);
             }
 
-            r = get_profile_string(str_header, str_iconindex, pszFileName, &iconindexstring);
+            get_profile_string(str_header, str_iconindex, pszFileName, &iconindexstring);
 
             if (iconindexstring != NULL)
             {
