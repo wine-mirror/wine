c3fab65647dc7652fef18d5a1b94bccf7e37be0b
Henri Verbeet <hverbeet@codeweavers.com>
2015-03-12 10:08:32 +0100

    d3d10core: Implement d3d10_depthstencil_view_SetPrivateDataInterface().

--- dlls/d3d10core/view.c
+++ dlls/d3d10core/view.c
@@ -449,9 +449,11 @@ static HRESULT STDMETHODCALLTYPE d3d10_depthstencil_view_SetPrivateData(ID3D10De
 static HRESULT STDMETHODCALLTYPE d3d10_depthstencil_view_SetPrivateDataInterface(ID3D10DepthStencilView *iface,
         REFGUID guid, const IUnknown *data)
 {
-    FIXME("iface %p, guid %s, data %p stub!\n", iface, debugstr_guid(guid), data);
+    struct d3d10_depthstencil_view *view = impl_from_ID3D10DepthStencilView(iface);
 
-    return E_NOTIMPL;
+    TRACE("iface %p, guid %s, data %p.\n", iface, debugstr_guid(guid), data);
+
+    return d3d10_set_private_data_interface(&view->private_store, guid, data);
 }
 
 /* ID3D10View methods */
