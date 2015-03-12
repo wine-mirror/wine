3c65bff802c600a2b620c6d1fd50e3cef2b525ed
Henri Verbeet <hverbeet@codeweavers.com>
2015-03-12 10:08:34 +0100

    d3d10core: Implement d3d10_rendertarget_view_SetPrivateData().

--- dlls/d3d10core/d3d10core_private.h
+++ dlls/d3d10core/d3d10core_private.h
@@ -154,6 +154,7 @@ struct d3d10_rendertarget_view
     ID3D10RenderTargetView ID3D10RenderTargetView_iface;
     LONG refcount;
 
+    struct wined3d_private_store private_store;
     struct wined3d_rendertarget_view *wined3d_view;
     D3D10_RENDER_TARGET_VIEW_DESC desc;
     ID3D10Resource *resource;
