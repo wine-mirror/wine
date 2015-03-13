171d61a06a77f26ad417e68d407d62a799dc3402
Henri Verbeet <hverbeet@codeweavers.com>
2015-03-13 09:49:25 +0100

    d3d10core: Implement d3d10_shader_resource_view_SetPrivateDataInterface().

--- dlls/d3d10core/view.c
+++ dlls/d3d10core/view.c
@@ -956,9 +956,11 @@ static HRESULT STDMETHODCALLTYPE d3d10_shader_resource_view_SetPrivateData(ID3D1
 static HRESULT STDMETHODCALLTYPE d3d10_shader_resource_view_SetPrivateDataInterface(ID3D10ShaderResourceView *iface,
         REFGUID guid, const IUnknown *data)
 {
-    FIXME("iface %p, guid %s, data %p stub!\n", iface, debugstr_guid(guid), data);
+    struct d3d10_shader_resource_view *view = impl_from_ID3D10ShaderResourceView(iface);
 
-    return E_NOTIMPL;
+    TRACE("iface %p, guid %s, data %p.\n", iface, debugstr_guid(guid), data);
+
+    return d3d10_set_private_data_interface(&view->private_store, guid, data);
 }
 
 /* ID3D10View methods */
