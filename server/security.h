5c6e2032d89df7b46b304c9c1fec393424196835
André Hentschel <nerv@dawncrow.de>
2015-03-18 23:30:59 +0100

    server: Add header guarding for security.h.

--- server/security.h
+++ server/security.h
@@ -18,6 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#ifndef __WINE_SERVER_SECURITY_H
+#define __WINE_SERVER_SECURITY_H
+
 extern const LUID SeIncreaseQuotaPrivilege;
 extern const LUID SeSecurityPrivilege;
 extern const LUID SeTakeOwnershipPrivilege;
@@ -144,3 +147,5 @@ static inline void objattr_get_name( const struct object_attributes *objattr, st
     name->len = ((objattr->name_len) / sizeof(WCHAR)) * sizeof(WCHAR);
     name->str = (const WCHAR *)objattr + (sizeof(*objattr) + objattr->sd_len) / sizeof(WCHAR);
 }
+
+#endif  /* __WINE_SERVER_SECURITY_H */
