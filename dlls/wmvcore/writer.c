63dd502c3a9b476e576ebefd97b532df99c0a8dd
Jacek Caban <jacek@codeweavers.com>
2015-03-19 16:12:15 +0100

    wmvcore: Added IWMWriterAdvanced3 stub implementation.

--- dlls/wmvcore/writer.c
+++ dlls/wmvcore/writer.c
@@ -25,6 +25,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(wmvcore);
 
 typedef struct {
     IWMWriter IWMWriter_iface;
+    IWMWriterAdvanced3 IWMWriterAdvanced3_iface;
     LONG ref;
 } WMWriter;
 
@@ -43,6 +44,15 @@ static HRESULT WINAPI WMWriter_QueryInterface(IWMWriter *iface, REFIID riid, voi
     }else if(IsEqualGUID(&IID_IWMWriter, riid)) {
         TRACE("(%p)->(IID_IWMWriter %p)\n", This, ppv);
         *ppv = &This->IWMWriter_iface;
+    }else if(IsEqualGUID(&IID_IWMWriterAdvanced, riid)) {
+        TRACE("(%p)->(IID_IWMWriterAdvanced %p)\n", This, ppv);
+        *ppv = &This->IWMWriterAdvanced3_iface;
+    }else if(IsEqualGUID(&IID_IWMWriterAdvanced2, riid)) {
+        TRACE("(%p)->(IID_IWMWriterAdvanced2 %p)\n", This, ppv);
+        *ppv = &This->IWMWriterAdvanced3_iface;
+    }else if(IsEqualGUID(&IID_IWMWriterAdvanced3, riid)) {
+        TRACE("(%p)->(IID_IWMWriterAdvanced3 %p)\n", This, ppv);
+        *ppv = &This->IWMWriterAdvanced3_iface;
     }else {
         FIXME("Unsupported iface %s\n", debugstr_guid(riid));
         *ppv = NULL;
@@ -188,6 +198,160 @@ static const IWMWriterVtbl WMWriterVtbl = {
     WMWriter_Flush
 };
 
+static inline WMWriter *impl_from_IWMWriterAdvanced3(IWMWriterAdvanced3 *iface)
+{
+    return CONTAINING_RECORD(iface, WMWriter, IWMWriterAdvanced3_iface);
+}
+
+static HRESULT WINAPI WMWriterAdvanced_QueryInterface(IWMWriterAdvanced3 *iface, REFIID riid, void **ppv)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    return IWMWriter_QueryInterface(&This->IWMWriter_iface, riid, ppv);
+}
+
+static ULONG WINAPI WMWriterAdvanced_AddRef(IWMWriterAdvanced3 *iface)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    return IWMWriter_AddRef(&This->IWMWriter_iface);
+}
+
+static ULONG WINAPI WMWriterAdvanced_Release(IWMWriterAdvanced3 *iface)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    return IWMWriter_Release(&This->IWMWriter_iface);
+}
+
+static HRESULT WINAPI WMWriterAdvanced_GetSinkCount(IWMWriterAdvanced3 *iface, DWORD *sinks)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%p)\n", This, sinks);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_GetSink(IWMWriterAdvanced3 *iface, DWORD sink_num, IWMWriterSink **sink)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u %p)\n", This, sink_num, sink);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_AddSink(IWMWriterAdvanced3 *iface, IWMWriterSink *sink)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%p)\n", This, sink);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_RemoveSink(IWMWriterAdvanced3 *iface, IWMWriterSink *sink)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%p)\n", This, sink);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_WriteStreamSample(IWMWriterAdvanced3 *iface, WORD stream_num,
+        QWORD sample_time, DWORD sample_send_time, QWORD sample_duration, DWORD flags, INSSBuffer *sample)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u %s %u %s %x %p)\n", This, stream_num, wine_dbgstr_longlong(sample_time),
+          sample_send_time, wine_dbgstr_longlong(sample_duration), flags, sample);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_SetLiveSource(IWMWriterAdvanced3 *iface, BOOL is_live_source)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%x)\n", This, is_live_source);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_IsRealTime(IWMWriterAdvanced3 *iface, BOOL *real_time)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%p)\n", This, real_time);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_GetWriterTime(IWMWriterAdvanced3 *iface, QWORD *current_time)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%p)\n", This, current_time);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_GetStatistics(IWMWriterAdvanced3 *iface, WORD stream_num, WM_WRITER_STATISTICS *stats)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u %p)\n", This, stream_num, stats);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_SetSyncTolerance(IWMWriterAdvanced3 *iface, DWORD window)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u)\n", This, window);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced_GetSyncTolerance(IWMWriterAdvanced3 *iface, DWORD *window)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%p)\n", This, window);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced2_GetInputSetting(IWMWriterAdvanced3 *iface, DWORD input_num,
+        const WCHAR *name, WMT_ATTR_DATATYPE *time, BYTE *value, WORD *length)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u %s %p %p %p)\n", This, input_num, debugstr_w(name), time, value, length);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced2_SetInputSetting(IWMWriterAdvanced3 *iface, DWORD input_num,
+        const WCHAR *name, WMT_ATTR_DATATYPE type, const BYTE *value, WORD length)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u %s %d %p %u)\n", This, input_num, debugstr_w(name), type, value, length);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced3_GetStatisticsEx(IWMWriterAdvanced3 *iface, WORD stream_num,
+        WM_WRITER_STATISTICS_EX *stats)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)->(%u %p)\n", This, stream_num, stats);
+    return E_NOTIMPL;
+}
+
+static HRESULT WINAPI WMWriterAdvanced3_SetNonBlocking(IWMWriterAdvanced3 *iface)
+{
+    WMWriter *This = impl_from_IWMWriterAdvanced3(iface);
+    FIXME("(%p)\n", This);
+    return E_NOTIMPL;
+}
+
+static const IWMWriterAdvanced3Vtbl WMWriterAdvanced3Vtbl = {
+    WMWriterAdvanced_QueryInterface,
+    WMWriterAdvanced_AddRef,
+    WMWriterAdvanced_Release,
+    WMWriterAdvanced_GetSinkCount,
+    WMWriterAdvanced_GetSink,
+    WMWriterAdvanced_AddSink,
+    WMWriterAdvanced_RemoveSink,
+    WMWriterAdvanced_WriteStreamSample,
+    WMWriterAdvanced_SetLiveSource,
+    WMWriterAdvanced_IsRealTime,
+    WMWriterAdvanced_GetWriterTime,
+    WMWriterAdvanced_GetStatistics,
+    WMWriterAdvanced_SetSyncTolerance,
+    WMWriterAdvanced_GetSyncTolerance,
+    WMWriterAdvanced2_GetInputSetting,
+    WMWriterAdvanced2_SetInputSetting,
+    WMWriterAdvanced3_GetStatisticsEx,
+    WMWriterAdvanced3_SetNonBlocking
+};
+
 HRESULT WINAPI WMCreateWriter(IUnknown *reserved, IWMWriter **writer)
 {
     WMWriter *ret;
@@ -199,6 +363,7 @@ HRESULT WINAPI WMCreateWriter(IUnknown *reserved, IWMWriter **writer)
         return E_OUTOFMEMORY;
 
     ret->IWMWriter_iface.lpVtbl = &WMWriterVtbl;
+    ret->IWMWriterAdvanced3_iface.lpVtbl = &WMWriterAdvanced3Vtbl;
     ret->ref = 1;
 
     *writer = &ret->IWMWriter_iface;
