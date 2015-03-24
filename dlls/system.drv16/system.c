bd07c3af86362653fe827cf9ea4aa629a0676558
Francois Gouget <fgouget@free.fr>
2015-03-24 11:40:55 +0100

    system.drv16: Remove an unneeded NONAMELESSSTRUCT directive.

--- dlls/system.drv16/system.c
+++ dlls/system.drv16/system.c
@@ -18,9 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#define NONAMELESSUNION
-#define NONAMELESSSTRUCT
 #include <stdarg.h>
+
+#define NONAMELESSUNION
 
 #include "windef.h"
 #include "winbase.h"
