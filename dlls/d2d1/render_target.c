59e641aa70c280209f104656b69c6b68a1cc70bb
Henri Verbeet <hverbeet@codeweavers.com>
2015-03-30 09:17:02 +0200

    d2d1: Implement d2d_d3d_render_target_RestoreDrawingState().

--- dlls/d2d1/render_target.c
+++ dlls/d2d1/render_target.c
@@ -949,7 +949,17 @@ static void STDMETHODCALLTYPE d2d_d3d_render_target_SaveDrawingState(ID2D1Render
 static void STDMETHODCALLTYPE d2d_d3d_render_target_RestoreDrawingState(ID2D1RenderTarget *iface,
         ID2D1DrawingStateBlock *state_block)
 {
-    FIXME("iface %p, state_block %p stub!\n", iface, state_block);
+    struct d2d_state_block *state_block_impl = unsafe_impl_from_ID2D1DrawingStateBlock(state_block);
+    struct d2d_d3d_render_target *render_target = impl_from_ID2D1RenderTarget(iface);
+
+    TRACE("iface %p, state_block %p.\n", iface, state_block);
+
+    render_target->drawing_state = state_block_impl->drawing_state;
+    if (state_block_impl->text_rendering_params)
+        IDWriteRenderingParams_AddRef(state_block_impl->text_rendering_params);
+    if (render_target->text_rendering_params)
+        IDWriteRenderingParams_Release(render_target->text_rendering_params);
+    render_target->text_rendering_params = state_block_impl->text_rendering_params;
 }
 
 static void STDMETHODCALLTYPE d2d_d3d_render_target_PushAxisAlignedClip(ID2D1RenderTarget *iface,
