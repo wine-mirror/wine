1402201cd61b4412331c5c1f469a5940991b6a02
Francois Gouget <fgouget@free.fr>
2015-04-02 12:25:02 +0200

    regedit: Remove an unneeded NONAMELESSSTRUCT directive.

--- programs/regedit/treeview.c
+++ programs/regedit/treeview.c
@@ -22,7 +22,7 @@
 #define WIN32_LEAN_AND_MEAN     /* Exclude rarely-used stuff from Windows headers */
 
 #define NONAMELESSUNION
-#define NONAMELESSSTRUCT
+
 #include <windows.h>
 #include <commctrl.h>
 #include <stdlib.h>
