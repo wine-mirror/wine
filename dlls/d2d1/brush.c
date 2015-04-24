719dfff5bd87577ebcefe2b35aff8313e84fc00a
Henri Verbeet <hverbeet@codeweavers.com>
2015-04-24 19:31:36 +0200

    d2d1: Implement d2d_bitmap_brush_GetInterpolationMode().

--- dlls/d2d1/brush.c
+++ dlls/d2d1/brush.c
@@ -599,9 +599,11 @@ static D2D1_EXTEND_MODE STDMETHODCALLTYPE d2d_bitmap_brush_GetExtendModeY(ID2D1B
 
 static D2D1_BITMAP_INTERPOLATION_MODE STDMETHODCALLTYPE d2d_bitmap_brush_GetInterpolationMode(ID2D1BitmapBrush *iface)
 {
-    FIXME("iface %p stub!\n", iface);
+    struct d2d_brush *brush = impl_from_ID2D1BitmapBrush(iface);
 
-    return D2D1_BITMAP_INTERPOLATION_MODE_NEAREST_NEIGHBOR;
+    TRACE("iface %p.\n", iface);
+
+    return brush->u.bitmap.interpolation_mode;
 }
 
 static void STDMETHODCALLTYPE d2d_bitmap_brush_GetBitmap(ID2D1BitmapBrush *iface, ID2D1Bitmap **bitmap)
