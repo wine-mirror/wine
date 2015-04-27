abc84588d2ab3596db15c883b35c81ce9409d264
Henri Verbeet <hverbeet@codeweavers.com>
2015-04-27 16:39:35 +0200

    d2d1: Implement d2d_bitmap_brush_GetExtendModeY().

--- dlls/d2d1/brush.c
+++ dlls/d2d1/brush.c
@@ -612,9 +612,11 @@ static D2D1_EXTEND_MODE STDMETHODCALLTYPE d2d_bitmap_brush_GetExtendModeX(ID2D1B
 
 static D2D1_EXTEND_MODE STDMETHODCALLTYPE d2d_bitmap_brush_GetExtendModeY(ID2D1BitmapBrush *iface)
 {
-    FIXME("iface %p stub!\n", iface);
+    struct d2d_brush *brush = impl_from_ID2D1BitmapBrush(iface);
 
-    return D2D1_EXTEND_MODE_CLAMP;
+    TRACE("iface %p.\n", iface);
+
+    return brush->u.bitmap.extend_mode_y;
 }
 
 static D2D1_BITMAP_INTERPOLATION_MODE STDMETHODCALLTYPE d2d_bitmap_brush_GetInterpolationMode(ID2D1BitmapBrush *iface)
