d93781f29bb346f04f8489ae208b1ffe540fe9c6
Nikolay Sivov <nsivov@codeweavers.com>
2015-05-14 10:44:42 +0300

    oledlg: Forward OleUIAddVerbMenuA to OleUIAddVerbMenuW.

--- dlls/oledlg/oledlg_main.c
+++ dlls/oledlg/oledlg_main.c
@@ -98,18 +98,28 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID fImpLoad)
 /***********************************************************************
  *           OleUIAddVerbMenuA (OLEDLG.1)
  */
-BOOL WINAPI OleUIAddVerbMenuA(
-  LPOLEOBJECT lpOleObj, LPCSTR lpszShortType,
-  HMENU hMenu, UINT uPos, UINT uIDVerbMin, UINT uIDVerbMax,
-  BOOL bAddConvert, UINT idConvert, HMENU *lphMenu)
+BOOL WINAPI OleUIAddVerbMenuA(IOleObject *object, LPCSTR shorttype,
+    HMENU hMenu, UINT uPos, UINT uIDVerbMin, UINT uIDVerbMax,
+    BOOL addConvert, UINT idConvert, HMENU *lphMenu)
 {
-  FIXME("(%p, %s, %p, %d, %d, %d, %d, %d, %p): stub\n",
-    lpOleObj, debugstr_a(lpszShortType),
-    hMenu, uPos, uIDVerbMin, uIDVerbMax,
-    bAddConvert, idConvert, lphMenu
-  );
-  SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-  return FALSE;
+    WCHAR *shorttypeW = NULL;
+    BOOL ret;
+
+    TRACE("(%p, %s, %p, %d, %d, %d, %d, %d, %p)\n", object, debugstr_a(shorttype),
+        hMenu, uPos, uIDVerbMin, uIDVerbMax, addConvert, idConvert, lphMenu);
+
+    if (shorttype)
+    {
+        INT len = MultiByteToWideChar(CP_ACP, 0, shorttype, -1, NULL, 0);
+        shorttypeW = HeapAlloc(GetProcessHeap(), 0, len*sizeof(WCHAR));
+        if (shorttypeW)
+            MultiByteToWideChar(CP_ACP, 0, shorttype, -1, shorttypeW, len);
+    }
+
+    ret = OleUIAddVerbMenuW(object, shorttypeW, hMenu, uPos, uIDVerbMin, uIDVerbMax,
+        addConvert, idConvert, lphMenu);
+    HeapFree(GetProcessHeap(), 0, shorttypeW);
+    return ret;
 }
 
 /***********************************************************************
