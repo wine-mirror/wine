8fae7fd4cc40679187b6bb4b7a911a7161e9aa01
Michael Stefaniuc <mstefani@redhat.de>
2015-06-25 00:32:57 +0200

    dmband/tests: Add some basic IDirectMusicTrack8 tests.

--- dlls/dmband/tests/dmband.c
+++ dlls/dmband/tests/dmband.c
@@ -185,6 +185,44 @@ static void test_bandtrack(void)
             &IID_IDirectMusicTrack8, (void**)&dmt8);
     ok(hr == S_OK, "DirectMusicBandTrack create failed: %08x, expected S_OK\n", hr);
 
+    /* IDirectMusicTrack8 */
+    todo_wine {
+    hr = IDirectMusicTrack8_Init(dmt8, NULL);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_Init failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_InitPlay(dmt8, NULL, NULL, NULL, 0, 0);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_InitPlay failed: %08x\n", hr);
+    }
+    hr = IDirectMusicTrack8_EndPlay(dmt8, NULL);
+    ok(hr == S_OK, "IDirectMusicTrack8_EndPlay failed: %08x\n", hr);
+    todo_wine {
+    hr = IDirectMusicTrack8_Play(dmt8, NULL, 0, 0, 0, 0, NULL, NULL, 0);
+    ok(hr == DMUS_S_END, "IDirectMusicTrack8_Play failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_GetParam(dmt8, NULL, 0, NULL, NULL);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_GetParam failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_SetParam(dmt8, NULL, 0, NULL);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_SetParam failed: %08x\n", hr);
+    }
+    hr = IDirectMusicTrack8_IsParamSupported(dmt8, NULL);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_IsParamSupported failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_AddNotificationType(dmt8, NULL);
+    ok(hr == E_NOTIMPL, "IDirectMusicTrack8_AddNotificationType failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_RemoveNotificationType(dmt8, NULL);
+    ok(hr == E_NOTIMPL, "IDirectMusicTrack8_RemoveNotificationType failed: %08x\n", hr);
+    todo_wine {
+    hr = IDirectMusicTrack8_Clone(dmt8, 0, 0, NULL);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_Clone failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_PlayEx(dmt8, NULL, 0, 0, 0, 0, NULL, NULL, 0);
+    ok(hr == DMUS_S_END, "IDirectMusicTrack8_PlayEx failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_GetParamEx(dmt8, NULL, 0, NULL, NULL, NULL, 0);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_GetParamEx failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_SetParamEx(dmt8, NULL, 0, NULL, NULL, 0);
+    ok(hr == E_POINTER, "IDirectMusicTrack8_SetParamEx failed: %08x\n", hr);
+    }
+    hr = IDirectMusicTrack8_Compose(dmt8, NULL, 0, NULL);
+    ok(hr == E_NOTIMPL, "IDirectMusicTrack8_Compose failed: %08x\n", hr);
+    hr = IDirectMusicTrack8_Join(dmt8, NULL, 0, NULL, 0, NULL);
+    todo_wine ok(hr == E_POINTER, "IDirectMusicTrack8_Join failed: %08x\n", hr);
+
     /* IPersistStream */
     hr = IDirectMusicTrack8_QueryInterface(dmt8, &IID_IPersistStream, (void**)&ps);
     ok(hr == S_OK, "QueryInterface for IID_IPersistStream failed: %08x\n", hr);
