3981205c560e3119d19c8e078e95745e3fd78499
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2015-07-07 10:15:55 +1000

    d3drm: Add DllCanUnloadNow, DllGetClassObject stubs.

--- dlls/d3drm/d3drm.c
+++ dlls/d3drm/d3drm.c
@@ -1528,3 +1528,18 @@ HRESULT WINAPI Direct3DRMCreate(IDirect3DRM **d3drm)
 
     return S_OK;
 }
+
+HRESULT WINAPI DllCanUnloadNow(void)
+{
+    return S_FALSE;
+}
+
+HRESULT WINAPI DllGetClassObject(REFCLSID rclsid, REFIID riid, void **ppv)
+{
+    TRACE("(%s, %s, %p): stub\n", debugstr_guid(rclsid), debugstr_guid(riid), ppv);
+
+    if(!ppv)
+        return E_INVALIDARG;
+
+    return CLASS_E_CLASSNOTAVAILABLE;
+}
--- dlls/d3drm/d3drm.spec
+++ dlls/d3drm/d3drm.spec
@@ -19,5 +19,5 @@
 @ stdcall D3DRMVectorScale(ptr ptr float)
 @ stdcall D3DRMVectorSubtract(ptr ptr ptr)
 @ stdcall Direct3DRMCreate(ptr)
-@ stub DllCanUnloadNow
-@ stub DllGetClassObject
+@ stdcall -private DllCanUnloadNow()
+@ stdcall -private DllGetClassObject(ptr ptr ptr)
