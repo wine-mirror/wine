30c5f3bd6322fd86fb20416861ceab1227a04607
André Hentschel <nerv@dawncrow.de>
2015-10-04 21:18:32 +0200

    wineboot: Move a function call after a possible return.
    Signed-off-by: André Hentschel <nerv@dawncrow.de>

--- programs/wineboot/wineboot.c
+++ programs/wineboot/wineboot.c
@@ -321,9 +321,9 @@ static void create_environment_registry_keys( void )
     WCHAR buffer[60];
     const WCHAR *arch;
 
-    NtQuerySystemInformation( SystemCpuInformation, &sci, sizeof(sci), NULL );
-
     if (RegCreateKeyW( HKEY_LOCAL_MACHINE, EnvironW, &env_key )) return;
+
+    NtQuerySystemInformation( SystemCpuInformation, &sci, sizeof(sci), NULL );
 
     sprintfW( buffer, PercentDW, NtCurrentTeb()->Peb->NumberOfProcessors );
     set_reg_value( env_key, NumProcW, buffer );
