6410e8a7712315bea07fcbf9325b93f6fb268d76
Nikolay Sivov <nsivov@codeweavers.com>
2015-10-14 00:32:11 +0300

    d3d8/tests: Use BOOL variable for ShowCursor return value (PVS-Studio).
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/tests/device.c
+++ dlls/d3d8/tests/device.c
@@ -834,12 +834,12 @@ static void test_cursor(void)
     ok(SUCCEEDED(hr), "Failed to create cursor surface, hr %#x.\n", hr);
 
     /* Initially hidden */
-    hr = IDirect3DDevice8_ShowCursor(device, TRUE);
-    ok(hr == FALSE, "IDirect3DDevice8_ShowCursor returned %#08x\n", hr);
+    ret = IDirect3DDevice8_ShowCursor(device, TRUE);
+    ok(!ret, "IDirect3DDevice8_ShowCursor returned %d\n", ret);
 
     /* Not enabled without a surface*/
-    hr = IDirect3DDevice8_ShowCursor(device, TRUE);
-    ok(hr == FALSE, "IDirect3DDevice8_ShowCursor returned %#08x\n", hr);
+    ret = IDirect3DDevice8_ShowCursor(device, TRUE);
+    ok(!ret, "IDirect3DDevice8_ShowCursor returned %d\n", ret);
 
     /* Fails */
     hr = IDirect3DDevice8_SetCursorProperties(device, 0, 0, NULL);
@@ -857,12 +857,12 @@ static void test_cursor(void)
     ok(info.hCursor == cur, "The cursor handle is %p\n", info.hCursor); /* unchanged */
 
     /* Still hidden */
-    hr = IDirect3DDevice8_ShowCursor(device, TRUE);
-    ok(hr == FALSE, "IDirect3DDevice8_ShowCursor returned %#08x\n", hr);
+    ret = IDirect3DDevice8_ShowCursor(device, TRUE);
+    ok(!ret, "IDirect3DDevice8_ShowCursor returned %d\n", ret);
 
     /* Enabled now*/
-    hr = IDirect3DDevice8_ShowCursor(device, TRUE);
-    ok(hr == TRUE, "IDirect3DDevice8_ShowCursor returned %#08x\n", hr);
+    ret = IDirect3DDevice8_ShowCursor(device, TRUE);
+    ok(ret, "IDirect3DDevice8_ShowCursor returned %d\n", ret);
 
     memset(&info, 0, sizeof(info));
     info.cbSize = sizeof(info);
