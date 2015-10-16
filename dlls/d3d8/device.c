cdc24238b70e27d0c1daf80176b9cb44b34d8cfd
Riccardo Bortolato <rikyz619@gmail.com>
2015-10-16 13:59:15 +0200

    d3d8: Call wined3d_swapchain_get_front_buffer_data instead of wined3d_device_get_front_buffer_data in d3d8_device_GetFrontBuffer.
    Signed-off-by: Riccardo Bortolato <rikyz619@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/device.c
+++ dlls/d3d8/device.c
@@ -1158,7 +1158,7 @@ static HRESULT WINAPI d3d8_device_GetFrontBuffer(IDirect3DDevice8 *iface, IDirec
     }
 
     wined3d_mutex_lock();
-    hr = wined3d_device_get_front_buffer_data(device->wined3d_device, 0, dst_impl->wined3d_surface);
+    hr = wined3d_swapchain_get_front_buffer_data(device->implicit_swapchain->wined3d_swapchain, dst_impl->wined3d_surface);
     wined3d_mutex_unlock();
 
     return hr;
