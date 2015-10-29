ad4057803dd23be219f7e1406780e7a6adb937d9
Michael Stefaniuc <mstefani@redhat.de>
2015-10-29 01:01:28 +0100

    ddraw/tests: Use the proper type for a return value (PVS-Studio).
    Signed-off-by: Michael Stefaniuc <mstefani@redhat.de>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ddraw/tests/ddrawmodes.c
+++ dlls/ddraw/tests/ddrawmodes.c
@@ -932,8 +932,8 @@ static void testcooperativelevels_exclusive(void)
 
     /* rect_before_create is assumed to hold the screen rect */
     GetClientRect(hwnd, &window_rect);
-    rc = EqualRect(&rect_before_create, &window_rect);
-    ok(rc, "Fullscreen window has wrong size.\n");
+    success = EqualRect(&rect_before_create, &window_rect);
+    ok(success, "Fullscreen window has wrong size.\n");
 
     /* Set the focus window. Should fail */
     rc = IDirectDraw_SetCooperativeLevel(lpDD,
