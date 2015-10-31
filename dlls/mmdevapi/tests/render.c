bc9a95bb517b0aedb8d3e505a6bb579fe35b6bb0
Alexandre Julliard <julliard@winehq.org>
2015-10-31 15:02:29 +0900

    mmdevapi/tests: Avoid %ll printf format.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/mmdevapi/tests/render.c
+++ dlls/mmdevapi/tests/render.c
@@ -337,7 +337,8 @@ static void test_audioclient(void)
           (UINT)(t2/10000), (UINT)(t2 % 10000));
     ok(t2 >= t1 || broken(t2 >= t1/2 && pwfx->nSamplesPerSec > 48000) ||
             broken(t2 == 0) /* (!) win10 */,
-       "Latency < default period, delta %ldus (%llu vs %llu)\n", (long)((t2-t1)/10), t2, t1);
+       "Latency < default period, delta %dus (%x%08x vs %x%08x)\n",
+       (LONG)((t2-t1)/10), (DWORD)(t2 >> 32), (DWORD)t2, (DWORD)(t1 >> 32), (DWORD)t1);
     /* Native appears to add the engine period to the HW latency in shared mode */
     if(t2 == 0)
         win10 = TRUE;
