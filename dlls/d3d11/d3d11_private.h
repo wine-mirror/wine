b62720feb69063ffa36ef8c3de1aa8a23c28c375
Józef Kucia <jkucia@codeweavers.com>
2015-11-06 18:38:11 +0100

    d3d11: Implement d3d11_immediate_context_PSSetShaderResources().
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d11/d3d11_private.h
+++ dlls/d3d11/d3d11_private.h
@@ -207,6 +207,8 @@ struct d3d_shader_resource_view
 
 HRESULT d3d_shader_resource_view_create(struct d3d_device *device, ID3D11Resource *resource,
         const D3D11_SHADER_RESOURCE_VIEW_DESC *desc, struct d3d_shader_resource_view **view) DECLSPEC_HIDDEN;
+struct d3d_shader_resource_view *unsafe_impl_from_ID3D11ShaderResourceView(
+        ID3D11ShaderResourceView *iface) DECLSPEC_HIDDEN;
 struct d3d_shader_resource_view *unsafe_impl_from_ID3D10ShaderResourceView(
         ID3D10ShaderResourceView *iface) DECLSPEC_HIDDEN;
 
--- dlls/d3d11/view.c
+++ dlls/d3d11/view.c
@@ -1546,6 +1546,14 @@ HRESULT d3d_shader_resource_view_create(struct d3d_device *device, ID3D11Resourc
     return S_OK;
 }
 
+struct d3d_shader_resource_view *unsafe_impl_from_ID3D11ShaderResourceView(ID3D11ShaderResourceView *iface)
+{
+    if (!iface)
+        return NULL;
+    assert(iface->lpVtbl == &d3d11_shader_resource_view_vtbl);
+    return impl_from_ID3D11ShaderResourceView(iface);
+}
+
 struct d3d_shader_resource_view *unsafe_impl_from_ID3D10ShaderResourceView(ID3D10ShaderResourceView *iface)
 {
     if (!iface)
