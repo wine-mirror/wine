45e706a2a79beffe28eecd7ec01139a1c61431e2
Vincent Povirk <vincent@codeweavers.com>
2015-11-24 15:32:59 -0600

    comctl32/tests: Accept touchscreen-related messages in button tests.
    Signed-off-by: Vincent Povirk <vincent@codeweavers.com>
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/comctl32/tests/button.c
+++ dlls/comctl32/tests/button.c
@@ -205,6 +205,7 @@ static const struct message setfocus_seq[] =
 {
     { WM_IME_SETCONTEXT, sent|wparam|optional, 1 },
     { WM_IME_NOTIFY, sent|wparam|defwinproc|optional, 2 },
+    { BM_GETSTATE, sent|optional }, /* when touchscreen is present */
     { WM_SETFOCUS, sent|wparam },
     { WM_COMMAND, sent|wparam|parent, MAKEWPARAM(ID_BUTTON, BN_SETFOCUS) },
     { WM_APP, sent|wparam|lparam },
@@ -227,6 +228,7 @@ static const struct message setfocus_static_seq[] =
 {
     { WM_IME_SETCONTEXT, sent|wparam|optional, 1 },
     { WM_IME_NOTIFY, sent|wparam|defwinproc|optional, 2 },
+    { BM_GETSTATE, sent|optional }, /* when touchscreen is present */
     { WM_SETFOCUS, sent|wparam, 0 },
     { WM_COMMAND, sent|wparam|parent, MAKEWPARAM(ID_BUTTON, BN_SETFOCUS) },
     { WM_COMMAND, sent|wparam|parent|optional, MAKEWPARAM(ID_BUTTON, BN_CLICKED) }, /* radio button */
@@ -239,6 +241,7 @@ static const struct message setfocus_groupbox_seq[] =
 {
     { WM_IME_SETCONTEXT, sent|wparam|optional, 1 },
     { WM_IME_NOTIFY, sent|wparam|defwinproc|optional, 2 },
+    { BM_GETSTATE, sent|optional }, /* when touchscreen is present */
     { WM_SETFOCUS, sent|wparam, 0 },
     { WM_COMMAND, sent|wparam|parent, MAKEWPARAM(ID_BUTTON, BN_SETFOCUS) },
     { WM_COMMAND, sent|wparam|parent|optional, MAKEWPARAM(ID_BUTTON, BN_CLICKED) }, /* radio button */
@@ -262,6 +265,7 @@ static const struct message setfocus_ownerdraw_seq[] =
 {
     { WM_IME_SETCONTEXT, sent|wparam|optional, 1 },
     { WM_IME_NOTIFY, sent|wparam|defwinproc|optional, 2 },
+    { BM_GETSTATE, sent|optional }, /* when touchscreen is present */
     { WM_SETFOCUS, sent|wparam, 0 },
     { WM_DRAWITEM, sent|wparam|parent, ID_BUTTON },
     { WM_COMMAND, sent|wparam|parent, MAKEWPARAM(ID_BUTTON, BN_SETFOCUS) },
@@ -287,6 +291,7 @@ static const struct message lbuttondown_seq[] =
     { WM_LBUTTONDOWN, sent|wparam|lparam, 0, 0 },
     { WM_IME_SETCONTEXT, sent|wparam|defwinproc|optional, 1 },
     { WM_IME_NOTIFY, sent|wparam|defwinproc|optional, 2 },
+    { BM_GETSTATE, sent|defwinproc|optional }, /* when touchscreen is present */
     { WM_SETFOCUS, sent|wparam|defwinproc, 0 },
     { BM_SETSTATE, sent|wparam|defwinproc, TRUE },
     { 0 }
