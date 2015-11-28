82a9faae2c4f00d177971bd1e830dc89117e6607
Nikolay Sivov <nsivov@codeweavers.com>
2015-11-28 21:24:54 +0300

    avifil32: Use __ms_va* varargs variant for AVISave[A,W].
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/avifil32/api.c
+++ dlls/avifil32/api.c
@@ -2278,7 +2278,7 @@ HRESULT WINAPI AVIPutFileOnClipboard(PAVIFILE pfile)
 HRESULT WINAPIV AVISaveA(LPCSTR szFile, CLSID * pclsidHandler, AVISAVECALLBACK lpfnCallback,
                         int nStreams, PAVISTREAM pavi, LPAVICOMPRESSOPTIONS lpOptions, ...)
 {
-    va_list vl;
+    __ms_va_list vl;
     int i;
     HRESULT ret;
     PAVISTREAM *streams;
@@ -2300,13 +2300,13 @@ HRESULT WINAPIV AVISaveA(LPCSTR szFile, CLSID * pclsidHandler, AVISAVECALLBACK l
     streams[0] = pavi;
     options[0] = lpOptions;
 
-    va_start(vl, lpOptions);
+    __ms_va_start(vl, lpOptions);
     for (i = 1; i < nStreams; i++)
     {
         streams[i] = va_arg(vl, void *);
         options[i] = va_arg(vl, void *);
     }
-    va_end(vl);
+    __ms_va_end(vl);
 
     for (i = 0; i < nStreams; i++)
         TRACE("Pair[%d] - Stream = %p, Options = %p\n", i, streams[i], options[i]);
@@ -2321,7 +2321,7 @@ error:
 HRESULT WINAPIV AVISaveW(LPCWSTR szFile, CLSID * pclsidHandler, AVISAVECALLBACK lpfnCallback,
                         int nStreams, PAVISTREAM pavi, LPAVICOMPRESSOPTIONS lpOptions, ...)
 {
-    va_list vl;
+    __ms_va_list vl;
     int i;
     HRESULT ret;
     PAVISTREAM *streams;
@@ -2343,13 +2343,13 @@ HRESULT WINAPIV AVISaveW(LPCWSTR szFile, CLSID * pclsidHandler, AVISAVECALLBACK 
     streams[0] = pavi;
     options[0] = lpOptions;
 
-    va_start(vl, lpOptions);
+    __ms_va_start(vl, lpOptions);
     for (i = 1; i < nStreams; i++)
     {
         streams[i] = va_arg(vl, void *);
         options[i] = va_arg(vl, void *);
     }
-    va_end(vl);
+    __ms_va_end(vl);
 
     for (i = 0; i < nStreams; i++)
         TRACE("Pair[%d] - Stream = %p, Options = %p\n", i, streams[i], options[i]);
