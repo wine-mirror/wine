519717459d29d5e8419e4b45c0ffdcecd004a05c
Józef Kucia <jkucia@codeweavers.com>
2016-01-25 10:57:23 +0100

    wined3d: Set default sampler filtering to NEAREST.
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/device.c
+++ dlls/wined3d/device.c
@@ -790,11 +790,13 @@ static void create_default_sampler(struct wined3d_device *device)
     {
         GL_EXTCALL(glGenSamplers(1, &device->default_sampler));
         checkGLcall("glGenSamplers");
+        GL_EXTCALL(glSamplerParameteri(device->default_sampler, GL_TEXTURE_MAG_FILTER, GL_NEAREST));
+        GL_EXTCALL(glSamplerParameteri(device->default_sampler, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST));
         if (gl_info->supported[EXT_TEXTURE_SRGB_DECODE])
         {
             GL_EXTCALL(glSamplerParameteri(device->default_sampler, GL_TEXTURE_SRGB_DECODE_EXT, GL_SKIP_DECODE_EXT));
-            checkGLcall("glSamplerParamteri");
         }
+        checkGLcall("glSamplerParamteri");
     }
     else
     {
