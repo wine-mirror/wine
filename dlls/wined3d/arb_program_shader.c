dd4c1ccd9e7a046fe3e3f385d7d7fb5638190489
Józef Kucia <jkucia@codeweavers.com>
2016-01-27 23:43:42 +0100

    wined3d: Recognize SM4 round_z opcode.
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/arb_program_shader.c
+++ dlls/wined3d/arb_program_shader.c
@@ -5301,6 +5301,7 @@ static const SHADER_HANDLER shader_arb_instruction_handler_table[WINED3DSIH_TABL
     /* WINED3DSIH_RET                   */ shader_hw_ret,
     /* WINED3DSIH_ROUND_NI              */ NULL,
     /* WINED3DSIH_ROUND_PI              */ NULL,
+    /* WINED3DSIH_ROUND_Z               */ NULL,
     /* WINED3DSIH_RSQ                   */ shader_hw_scalar_op,
     /* WINED3DSIH_SAMPLE                */ NULL,
     /* WINED3DSIH_SAMPLE_GRAD           */ NULL,
