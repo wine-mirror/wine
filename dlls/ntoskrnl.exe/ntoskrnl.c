e177f869394783743d46cac47d200c659b620990
Sebastian Lackner <sebastian@fds-team.de>
2016-02-13 09:23:05 +0100

    ntoskrnl.exe: Use debugstr_us() to trace UNICODE_STRING.
    Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -1258,8 +1258,8 @@ NTSTATUS WINAPI IoReportResourceUsage(PUNICODE_STRING name, PDRIVER_OBJECT drv_o
                                       ULONG drv_size, PDRIVER_OBJECT dev_obj, PCM_RESOURCE_LIST dev_list,
                                       ULONG dev_size, BOOLEAN overwrite, PBOOLEAN detected)
 {
-    FIXME("(%s %p %p %u %p %p %u %d %p) stub\n", debugstr_w(name? name->Buffer : NULL),
-          drv_obj, drv_list, drv_size, dev_obj, dev_list, dev_size, overwrite, detected);
+    FIXME( "(%s, %p, %p, %u, %p, %p, %u, %d, %p): stub\n", debugstr_us(name),
+           drv_obj, drv_list, drv_size, dev_obj, dev_list, dev_size, overwrite, detected );
     return STATUS_NOT_IMPLEMENTED;
 }
 
