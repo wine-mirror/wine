77960ab755833f9e2eac01d3132c2206882d2698
Charles Davis <cdavis5x@gmail.com>
2016-02-29 01:49:58 -0700

    winebuild: Specify an object format to windres.
    Signed-off-by: Charles Davis <cdavis5x@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- tools/winebuild/res32.c
+++ tools/winebuild/res32.c
@@ -623,6 +623,7 @@ void output_res_o_file( DLLSPEC *spec )
 {
     unsigned int i;
     char *res_file = NULL;
+    const char *format;
     int fd;
     struct strarray *args;
 
@@ -681,7 +682,21 @@ void output_res_o_file( DLLSPEC *spec )
     free( output_buffer );
 
     args = find_tool( "windres", NULL );
+    switch (target_cpu)
+    {
+        case CPU_x86:
+            format = "pe-i386";
+            break;
+        case CPU_x86_64:
+            format = "pe-x86-64";
+            break;
+        default:
+            format = NULL;
+            break;
+    }
     strarray_add( args, "-i", res_file, "-o", output_file_name, NULL );
+    if (format)
+        strarray_add( args, "-F", format, NULL );
     spawn( args );
     strarray_free( args );
 
