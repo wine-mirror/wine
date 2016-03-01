4b05cac3911300f604d3a7674b723a91111fabec
Nikolay Sivov <nsivov@codeweavers.com>
2016-03-01 07:49:52 +0300

    gdi32: Use a macro instead of accessing FT_Face flags directly.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/gdi32/freetype.c
+++ dlls/gdi32/freetype.c
@@ -7458,7 +7458,7 @@ static BOOL get_bitmap_text_metrics(GdiFont *font)
         TM.tmUnderlined = font->underline;
         TM.tmStruckOut = font->strikeout;
         /* NB inverted meaning of TMPF_FIXED_PITCH */
-        TM.tmPitchAndFamily = ft_face->face_flags & FT_FACE_FLAG_FIXED_WIDTH ? 0 : TMPF_FIXED_PITCH;
+        TM.tmPitchAndFamily = FT_IS_FIXED_WIDTH(ft_face) ? 0 : TMPF_FIXED_PITCH;
         TM.tmCharSet = font->charset;
     }
 #undef TM
