cc0b8336f5eb15224ced5d424bb17125a7dc5d1c
Nikolay Sivov <nsivov@codeweavers.com>
2016-04-02 12:19:17 +0300

    t2embed: Implement TTIsEmbeddingEnabledForFacename().
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 847da79cf80b565a79933d92e0218b230b933a4f)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/t2embed/tests/t2embed.c
+++ dlls/t2embed/tests/t2embed.c
@@ -76,7 +76,30 @@ static void test_TTGetEmbeddingType(void)
     DeleteDC(hdc);
 }
 
+static void test_TTIsEmbeddingEnabledForFacename(void)
+{
+    BOOL status;
+    LONG ret;
+
+    ret = TTIsEmbeddingEnabledForFacename(NULL, NULL);
+    ok(ret == E_FACENAMEINVALID, "got %#x\n", ret);
+
+    status = 123;
+    ret = TTIsEmbeddingEnabledForFacename(NULL, &status);
+    ok(ret == E_FACENAMEINVALID, "got %#x\n", ret);
+    ok(status == 123, "got %d\n", status);
+
+    ret = TTIsEmbeddingEnabledForFacename("Arial", NULL);
+    ok(ret == E_PBENABLEDINVALID, "got %#x\n", ret);
+
+    status = 123;
+    ret = TTIsEmbeddingEnabledForFacename("Arial", &status);
+    ok(ret == E_NONE, "got %#x\n", ret);
+    ok(status != 123, "got %d\n", status);
+}
+
 START_TEST(t2embed)
 {
     test_TTGetEmbeddingType();
+    test_TTIsEmbeddingEnabledForFacename();
 }
