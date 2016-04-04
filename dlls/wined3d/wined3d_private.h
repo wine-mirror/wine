f131e9f2c00cd2cd545b4ac4e408538442bdebe6
Henri Verbeet <hverbeet@codeweavers.com>
2016-04-04 18:42:59 +0200

    wined3d: Also update the swap interval when not recreating the context in wined3d_device_reset().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 6b8fb1c22ea26f9fafda76133850af10ac3c7256)
    Conflicts:
    	dlls/wined3d/swapchain.c
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/wined3d/wined3d_private.h
+++ dlls/wined3d/wined3d_private.h
@@ -2876,6 +2876,7 @@ struct wined3d_context *swapchain_get_context(struct wined3d_swapchain *swapchai
 void swapchain_destroy_contexts(struct wined3d_swapchain *swapchain) DECLSPEC_HIDDEN;
 HDC swapchain_get_backup_dc(struct wined3d_swapchain *swapchain) DECLSPEC_HIDDEN;
 void swapchain_update_draw_bindings(struct wined3d_swapchain *swapchain) DECLSPEC_HIDDEN;
+void swapchain_update_swap_interval(struct wined3d_swapchain *swapchain) DECLSPEC_HIDDEN;
 
 /*****************************************************************************
  * Utility function prototypes
