80893bcf5236c5f0097ddd3aeda59b900b7edf80
Jacek Caban <jacek@codeweavers.com>
2016-04-07 15:33:27 +0200

    mshtml: Skip headers problematic for cache.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/mshtml/tests/xmlhttprequest.c
+++ dlls/mshtml/tests/xmlhttprequest.c
@@ -386,7 +386,6 @@ static void pump_msgs(BOOL *b)
 struct HEADER_TYPE {
     const char *key;
     const char *value;
-    BOOL skip_all_headers;
 };
 
 static void create_xmlhttprequest(IHTMLDocument2 *doc)
@@ -450,9 +449,6 @@ static void test_header(const struct HEADER_TYPE expect[], int num)
             "Expect %s: %s, got %s\n", expect[i].key, expect[i].value, wine_dbgstr_w(text));
         SysFreeString(key);
         SysFreeString(text);
-
-        if(expect[i].skip_all_headers)
-            continue;
 
         strcpy(buf, expect[i].key);
         strcat(buf, ": ");
@@ -585,7 +581,6 @@ static void test_sync_xhr(IHTMLDocument2 *doc, const char *xml_url, const char *
     LONG val;
     HRESULT hres;
     static const struct HEADER_TYPE expect_headers[] = {
-        {"Accept-Ranges", "bytes", TRUE},
         {"Content-Length", "51"},
         {"Content-Type", "application/xml"}
     };
