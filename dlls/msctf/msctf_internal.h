40cdfd2a6a8ac050bb2ad3cbc8bf25eecb6f8423
Jacek Caban <jacek@codeweavers.com>
2016-05-04 19:41:41 +0200

    msctf: Use generic sinks in Compartment object.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/msctf/msctf_internal.h
+++ dlls/msctf/msctf_internal.h
@@ -72,6 +72,7 @@ typedef struct {
     union {
         IUnknown *pIUnknown;
         ITfThreadMgrEventSink *pITfThreadMgrEventSink;
+        ITfCompartmentEventSink *pITfCompartmentEventSink;
     } interfaces;
 } Sink;
 
