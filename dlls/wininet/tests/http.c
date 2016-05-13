f6714f3832541250476989ad579523b4e8b28f67
Jacek Caban <jacek@codeweavers.com>
2016-05-13 18:46:21 +0200

    wininet: Updated winehq.org cert info.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wininet/tests/http.c
+++ dlls/wininet/tests/http.c
@@ -4876,14 +4876,11 @@ typedef struct {
 } cert_struct_test_t;
 
 static const cert_struct_test_t test_winehq_org_cert = {
-    "GT98380011\r\n"
-    "See www.rapidssl.com/resources/cps (c)14\r\n"
-    "Domain Control Validated - RapidSSL(R)\r\n"
     "*.winehq.org",
 
     "US\r\n"
     "GeoTrust Inc.\r\n"
-    "RapidSSL SHA256 CA - G3"
+    "RapidSSL SHA256 CA"
 };
 
 static const cert_struct_test_t test_winehq_com_cert = {
