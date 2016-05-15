8b5a9051bff83c6e8321920c8c43ca0e76ff6c50
Nikolay Sivov <nsivov@codeweavers.com>
2016-05-16 00:18:16 +0300

    webservices: Actually use channel type in WsCreateServiceProxyFromTemplate() (Clang).
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/webservices/proxy.c
+++ dlls/webservices/proxy.c
@@ -172,7 +172,7 @@ HRESULT WINAPI WsCreateServiceProxyFromTemplate( WS_CHANNEL_TYPE channel_type,
         return E_NOTIMPL;
     }
 
-    if ((hr = create_channel( type, binding, channel_props, channel_props_count, &channel )) != S_OK)
+    if ((hr = create_channel( channel_type, binding, channel_props, channel_props_count, &channel )) != S_OK)
         return hr;
 
     if ((hr = create_proxy( channel, properties, count, handle )) != S_OK)
