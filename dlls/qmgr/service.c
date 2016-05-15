4d6cba1bda556cf7d328c74e3fd88d2566227809
Francois Gouget <fgouget@free.fr>
2016-05-15 15:32:42 +0200

    qmgr: Fix a spelling error in a TRACE().
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/qmgr/service.c
+++ dlls/qmgr/service.c
@@ -148,7 +148,7 @@ ServiceMain(DWORD dwArgc, LPWSTR *lpszArgv)
     WaitForSingleObject(fileTxThread, INFINITE);
     UpdateStatus(SERVICE_STOPPED, NO_ERROR, 0);
     CloseHandle(stop_event);
-    TRACE("service stoped\n");
+    TRACE("service stopped\n");
 
     CoUninitialize();
 }
