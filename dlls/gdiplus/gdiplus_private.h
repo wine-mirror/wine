cde8d5fef88316e387c8b992481609850dfc2e99
Vincent Povirk <vincent@codeweavers.com>
2016-06-13 16:42:28 -0500

    gdiplus: Implement GdipGetImageAttributesAdjustedPalette.
    Signed-off-by: Vincent Povirk <vincent@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 7fbf0deedef8f582ad5b84682aa9e5fe7a8a7add)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/gdiplus/gdiplus_private.h
+++ dlls/gdiplus/gdiplus_private.h
@@ -176,6 +176,9 @@ extern GpStatus convert_pixels(INT width, INT height,
     INT dst_stride, BYTE *dst_bits, PixelFormat dst_format,
     INT src_stride, const BYTE *src_bits, PixelFormat src_format, ColorPalette *palette) DECLSPEC_HIDDEN;
 
+extern PixelFormat apply_image_attributes(const GpImageAttributes *attributes, LPBYTE data,
+    UINT width, UINT height, INT stride, ColorAdjustType type, PixelFormat fmt) DECLSPEC_HIDDEN;
+
 struct GpMatrix{
     REAL matrix[6];
 };
