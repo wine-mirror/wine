d78ed2219bfe7cbc93a9cae35d05df4cbc4932fb
Ken Thomases <ken@codeweavers.com>
2016-06-23 16:53:04 -0500

    winemac: Don't define kVK_RightCommand when building against the macOS 10.12 SDK.
    Apple added the same enum declaration to their headers, causing a build failure.
    
    Signed-off-by: Ken Thomases <ken@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit cac226200d88b7454747b5ee1016f06b89ce4aa6)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/winemac.drv/cocoa_window.m
+++ dlls/winemac.drv/cocoa_window.m
@@ -43,10 +43,12 @@ enum {
 #endif
 
 
+#if !defined(MAC_OS_X_VERSION_10_12) || MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_12
 /* Additional Mac virtual keycode, to complement those in Carbon's <HIToolbox/Events.h>. */
 enum {
     kVK_RightCommand              = 0x36, /* Invented for Wine; was unused */
 };
+#endif
 
 
 static NSUInteger style_mask_for_features(const struct macdrv_window_features* wf)
