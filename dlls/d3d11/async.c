cc6be0c49e7507a4352a72d1dcdc65f5b32b4cee
Józef Kucia <jkucia@codeweavers.com>
2016-07-07 12:22:28 +0200

    d3d11: Implement d3d10_query_GetDataSize().
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d11/async.c
+++ dlls/d3d11/async.c
@@ -283,9 +283,16 @@ static HRESULT STDMETHODCALLTYPE d3d10_query_GetData(ID3D10Query *iface, void *d
 
 static UINT STDMETHODCALLTYPE d3d10_query_GetDataSize(ID3D10Query *iface)
 {
-    FIXME("iface %p stub!\n", iface);
+    struct d3d_query *query = impl_from_ID3D10Query(iface);
+    unsigned int data_size;
 
-    return 0;
+    TRACE("iface %p.\n", iface);
+
+    wined3d_mutex_lock();
+    data_size = wined3d_query_get_data_size(query->wined3d_query);
+    wined3d_mutex_unlock();
+
+    return data_size;
 }
 
 /* ID3D10Query methods */
