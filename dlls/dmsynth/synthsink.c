wine-mirror/wine

commit 165f8d1fff39df4d51599bf31008c962d4867cd9

dmsynth: Simplify error handling when creating a DMSynthSink object.
Signed-off-by: Michael Stefaniuc <mstefani@redhat.de>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/dmsynth/synthsink.c
@@ -282,7 +282,7 @@ HRESULT WINAPI DMUSIC_CreateDirectMusicSynthSinkImpl(LPCGUID riid, LPVOID* ret_i
 
     obj->IDirectMusicSynthSink_iface.lpVtbl = &DirectMusicSynthSink_Vtbl;
     obj->IKsControl_iface.lpVtbl = &DMSynthSinkImpl_IKsControl_Vtbl;
-    obj->ref = 0;
+    obj->ref = 1;
 
     hr = CoCreateInstance(&CLSID_SystemClock, NULL, CLSCTX_INPROC_SERVER, &IID_IReferenceClock, (LPVOID*)&obj->latency_clock);
     if (FAILED(hr))
@@ -291,13 +291,8 @@ HRESULT WINAPI DMUSIC_CreateDirectMusicSynthSinkImpl(LPCGUID riid, LPVOID* ret_i
         return hr;
     }
 
-    hr = IDirectMusicSynthSinkImpl_QueryInterface((LPDIRECTMUSICSYNTHSINK)obj, riid, ret_iface);
-    if (FAILED(hr))
-    {
-        IReferenceClock_Release(obj->latency_clock);
-        HeapFree(GetProcessHeap(), 0, obj);
-        return hr;
-    }
+    hr = IDirectMusicSynthSink_QueryInterface(&obj->IDirectMusicSynthSink_iface, riid, ret_iface);
+    IDirectMusicSynthSink_Release(&obj->IDirectMusicSynthSink_iface);
 
-    return S_OK;
+    return hr;
 }

commit a1ee6fa8dd9506cf2b117aeff080d72e199ecd15

dmsynth: Fix a module refcounting regression.
Fixes a regression introduced by 165f8d1fff39df4d51599bf31008c962d4867cd9.

Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>
Signed-off-by: Michael Stefaniuc <mstefani@redhat.de>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/dmsynth/synthsink.c
@@ -291,6 +291,7 @@ HRESULT WINAPI DMUSIC_CreateDirectMusicSynthSinkImpl(LPCGUID riid, LPVOID* ret_i
         return hr;
     }
 
+    DMSYNTH_LockModule();
     hr = IDirectMusicSynthSink_QueryInterface(&obj->IDirectMusicSynthSink_iface, riid, ret_iface);
     IDirectMusicSynthSink_Release(&obj->IDirectMusicSynthSink_iface);
 