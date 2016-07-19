3fa736e97818947e518220e42addeeeeab1e0aec
Aaryaman Vasishta <jem456.vasishta@gmail.com>
2016-07-19 23:38:42 +0530

    d3drm: Fix IDirect3DRMViewport*::QueryInterface returning wrong HRESULT for invalid interfaces.
    Signed-off-by: Aaryaman Vasishta <jem456.vasishta@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3drm/viewport.c
+++ dlls/d3drm/viewport.c
@@ -66,8 +66,8 @@ static HRESULT WINAPI d3drm_viewport2_QueryInterface(IDirect3DRMViewport2 *iface
     else
     {
         *out = NULL;
-        WARN("%s not implemented, returning E_NOINTERFACE.\n", debugstr_guid(riid));
-        return E_NOINTERFACE;
+        WARN("%s not implemented, returning CLASS_E_CLASSNOTAVAILABLE.\n", debugstr_guid(riid));
+        return CLASS_E_CLASSNOTAVAILABLE;
     }
 
     IUnknown_AddRef((IUnknown *)*out);
