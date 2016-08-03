ccce887eb2b4fdd7dc26f21edb1e577a4f69d8c1
Nikolay Sivov <nsivov@codeweavers.com>
2016-08-03 14:19:04 +0300

    krnl386: Fix its vs it's typo.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/krnl386.exe16/local.c
+++ dlls/krnl386.exe16/local.c
@@ -814,7 +814,7 @@ static UINT16 LOCAL_Compact( HANDLE16 ds, UINT16 minfree, UINT16 flags )
                     size = pFinalArena->size;
                     LOCAL_RemoveFreeBlock(ptr, finalarena);
                     LOCAL_ShrinkArena( ds, finalarena, movesize );
-                    /* Copy the arena to it's new location */
+                    /* Copy the arena to its new location */
                     memcpy((char *)pFinalArena + ARENA_HEADER_SIZE,
                            (char *)pMoveArena + ARENA_HEADER_SIZE,
                            movesize - ARENA_HEADER_SIZE );
