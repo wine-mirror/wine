9853b59cd760d397963f6e1e4040c133ee001803
Henri Verbeet <hverbeet@codeweavers.com>
2016-08-29 10:49:03 +0200

    d3d8: Use wined3d_resource_unmap() in d3d8_vertexbuffer_Unlock().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/buffer.c
+++ dlls/d3d8/buffer.c
@@ -207,7 +207,7 @@ static HRESULT WINAPI d3d8_vertexbuffer_Unlock(IDirect3DVertexBuffer8 *iface)
     TRACE("iface %p.\n", iface);
 
     wined3d_mutex_lock();
-    wined3d_buffer_unmap(buffer->wined3d_buffer);
+    wined3d_resource_unmap(wined3d_buffer_get_resource(buffer->wined3d_buffer), 0);
     wined3d_mutex_unlock();
 
     return D3D_OK;
