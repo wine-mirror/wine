7846f0947a1df69ee6417af0030bfcfa5620e0c9
Nikolay Sivov <nsivov@codeweavers.com>
2016-09-13 21:54:20 +0300

    mshtml: Use proper constants as HRESULT return values.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/mshtml/main.c
+++ dlls/mshtml/main.c
@@ -382,7 +382,7 @@ HRESULT WINAPI RunHTMLApplication( HINSTANCE hinst, HINSTANCE hPrevInst,
                                LPSTR szCmdLine, INT nCmdShow )
 {
     FIXME("%p %p %s %d\n", hinst, hPrevInst, debugstr_a(szCmdLine), nCmdShow );
-    return 0;
+    return S_OK;
 }
 
 /***********************************************************************
