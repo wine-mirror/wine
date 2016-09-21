fffa143180de3889cc670be59201be741f7c0775
Michael Müller <michael@fds-team.de>
2016-09-21 15:00:41 +0200

    ntdll: Open current working directory with FILE_TRAVERSE access.
    Signed-off-by: Michael Müller <michael@fds-team.de>
    Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 6f2f307b1dcc8e4749eee05325961d77e3473fe1)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/ntdll/path.c
+++ dlls/ntdll/path.c
@@ -984,7 +984,8 @@ NTSTATUS WINAPI RtlSetCurrentDirectory_U(const UNICODE_STRING* dir)
     attr.SecurityDescriptor = NULL;
     attr.SecurityQualityOfService = NULL;
 
-    nts = NtOpenFile( &handle, SYNCHRONIZE, &attr, &io, 0, FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT );
+    nts = NtOpenFile( &handle, FILE_TRAVERSE | SYNCHRONIZE, &attr, &io, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                      FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT );
     if (nts != STATUS_SUCCESS) goto out;
 
     /* don't keep the directory handle open on removable media */
