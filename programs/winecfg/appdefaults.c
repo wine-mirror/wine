f94784ddbac9880789171d9f0126499485051249
Hans Leidekker <hans@codeweavers.com>
2016-10-04 13:28:33 +0200

    ntdll: SzCSDVersion is an empty string on Windows 8 and up.
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/winecfg/appdefaults.c
+++ programs/winecfg/appdefaults.c
@@ -48,9 +48,9 @@ static const struct
     const char *szProductType;
 } win_versions[] =
 {
-    { "win10",       "Windows 10",       10,  0, 0x3839,VER_PLATFORM_WIN32_NT, " ", 0, 0, "WinNT"},
-    { "win81",       "Windows 8.1",       6,  3, 0x2580,VER_PLATFORM_WIN32_NT, " ", 0, 0, "WinNT"},
-    { "win8",        "Windows 8",         6,  2, 0x23F0,VER_PLATFORM_WIN32_NT, " ", 0, 0, "WinNT"},
+    { "win10",       "Windows 10",       10,  0, 0x3839,VER_PLATFORM_WIN32_NT, "", 0, 0, "WinNT"},
+    { "win81",       "Windows 8.1",       6,  3, 0x2580,VER_PLATFORM_WIN32_NT, "", 0, 0, "WinNT"},
+    { "win8",        "Windows 8",         6,  2, 0x23F0,VER_PLATFORM_WIN32_NT, "", 0, 0, "WinNT"},
     { "win2008r2",   "Windows 2008 R2",   6,  1, 0x1DB1,VER_PLATFORM_WIN32_NT, "Service Pack 1", 1, 0, "ServerNT"},
     { "win7",        "Windows 7",         6,  1, 0x1DB1,VER_PLATFORM_WIN32_NT, "Service Pack 1", 1, 0, "WinNT"},
     { "win2008",     "Windows 2008",      6,  0, 0x1772,VER_PLATFORM_WIN32_NT, "Service Pack 2", 2, 0, "ServerNT"},
