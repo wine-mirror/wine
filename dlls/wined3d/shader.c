f1ea3fd5b3e201d8f4b632655c79c75008beb398
Józef Kucia <jkucia@codeweavers.com>
2016-11-23 14:36:02 +0100

    wined3d: Do not record sampler usage for UAVs.
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/shader.c
+++ dlls/wined3d/shader.c
@@ -1257,7 +1257,7 @@ static HRESULT shader_get_registers_used(struct wined3d_shader *shader, const st
                         ins.src[2].reg.idx[0].offset, reg_maps->sampler_map.count);
             }
             else if (ins.handler_idx == WINED3DSIH_LD
-                    || ins.handler_idx == WINED3DSIH_RESINFO)
+                    || (ins.handler_idx == WINED3DSIH_RESINFO && ins.src[1].reg.type == WINED3DSPR_RESOURCE))
             {
                 shader_record_sample(reg_maps, ins.src[1].reg.idx[0].offset,
                         WINED3D_SAMPLER_DEFAULT, reg_maps->sampler_map.count);
