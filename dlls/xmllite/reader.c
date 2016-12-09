831c202c38e97730c5446b95c8a542714febd99b
Nikolay Sivov <nsivov@codeweavers.com>
2016-12-09 16:38:34 +0300

    xmllite: Add maintainer entry.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/xmllite/reader.c
+++ dlls/xmllite/reader.c
@@ -1,7 +1,7 @@
 /*
  * IXmlReader implementation
  *
- * Copyright 2010, 2012-2013 Nikolay Sivov
+ * Copyright 2010, 2012-2013, 2016 Nikolay Sivov
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
