5e315aa8926fa49c44288b84cf3efc803e5f0976
Alex Henrie <alexhenrie24@gmail.com>
2017-01-28 15:34:04 -0700

    ddraw: Don't crash if writing out a new ddraw2 surface segfaults.
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit f6997e8fe0563da811d1627034dd95ab3a8e10af)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/ddraw/tests/ddraw2.c
+++ dlls/ddraw/tests/ddraw2.c
@@ -2119,6 +2119,8 @@ static void test_surface_qi(void)
     surface_desc.ddsCaps.dwCaps = DDSCAPS_TEXTURE;
     surface_desc.dwWidth = 512;
     surface_desc.dwHeight = 512;
+    hr = IDirectDraw2_CreateSurface(ddraw, &surface_desc, (IDirectDrawSurface **)0xdeadbeef, NULL);
+    ok(hr == E_INVALIDARG, "Got unexpected hr %#x.\n", hr);
     hr = IDirectDraw2_CreateSurface(ddraw, &surface_desc, &surface, NULL);
     ok(SUCCEEDED(hr), "Failed to create surface, hr %#x.\n", hr);
 
