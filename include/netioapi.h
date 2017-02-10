2831e0c38091a10c08e9b68a1e326d33cf310de7
André Hentschel <nerv@dawncrow.de>
2017-02-10 18:30:13 +0100

    iphlpapi: Add GetUnicastIpAddressEntry implementation.
    Signed-off-by: André Hentschel <nerv@dawncrow.de>
    Signed-off-by: Bruno Jesus <00cpxxx@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit ccdbceeefc196999a698fc1842d4f1bff651ec22)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- include/netioapi.h
+++ include/netioapi.h
@@ -82,6 +82,22 @@ typedef struct _MIB_IF_TABLE2
     MIB_IF_ROW2 Table[1];
 } MIB_IF_TABLE2, *PMIB_IF_TABLE2;
 
+typedef struct _MIB_UNICASTIPADDRESS_ROW
+{
+    SOCKADDR_INET       Address;
+    NET_LUID            InterfaceLuid;
+    NET_IFINDEX         InterfaceIndex;
+    NL_PREFIX_ORIGIN    PrefixOrigin;
+    NL_SUFFIX_ORIGIN    SuffixOrigin;
+    ULONG               ValidLifetime;
+    ULONG               PreferredLifetime;
+    UINT8               OnLinkPrefixLength;
+    BOOLEAN             SkipAsSource;
+    NL_DAD_STATE        DadState;
+    SCOPE_ID            ScopeId;
+    LARGE_INTEGER       CreationTimeStamp;
+} MIB_UNICASTIPADDRESS_ROW, *PMIB_UNICASTIPADDRESS_ROW;
+
 DWORD WINAPI ConvertInterfaceGuidToLuid(const GUID*,NET_LUID*);
 DWORD WINAPI ConvertInterfaceIndexToLuid(NET_IFINDEX,NET_LUID*);
 DWORD WINAPI ConvertInterfaceLuidToGuid(const NET_LUID*,GUID*);
@@ -92,5 +108,6 @@ DWORD WINAPI ConvertInterfaceNameToLuidA(const char*,NET_LUID*);
 DWORD WINAPI ConvertInterfaceNameToLuidW(const WCHAR*,NET_LUID*);
 void WINAPI FreeMibTable(void*);
 DWORD WINAPI GetIfEntry2(MIB_IF_ROW2*);
+DWORD WINAPI GetUnicastIpAddressEntry(MIB_UNICASTIPADDRESS_ROW*);
 
 #endif /* __WINE_NETIOAPI_H */
