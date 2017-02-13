f6b1e1235caa04a1e78cef9cb9f0951d41f5b71f
Nikolay Sivov <nsivov@codeweavers.com>
2017-02-13 14:16:03 +0300

    hlink: Properly return interface pointers.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/hlink/hlink_main.c
+++ dlls/hlink/hlink_main.c
@@ -528,7 +528,7 @@ static HRESULT WINAPI HLinkCF_fnQueryInterface ( LPCLASSFACTORY iface,
     if (IsEqualIID(riid, &IID_IUnknown) ||
         IsEqualIID(riid, &IID_IClassFactory))
     {
-        *ppvObj = This;
+        *ppvObj = &This->IClassFactory_iface;
         return S_OK;
     }
 
