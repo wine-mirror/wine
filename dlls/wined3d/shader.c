84e3b8b933906423cc9f0e7f80b1f5ecd1c0b10a
Józef Kucia <jkucia@codeweavers.com>
2017-02-15 14:27:36 +0100

    wined3d: Record sampler usage for WINED3DSIH_LD_RAW.
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/shader.c
+++ dlls/wined3d/shader.c
@@ -1354,6 +1354,7 @@ static HRESULT shader_get_registers_used(struct wined3d_shader *shader, const st
                         ins.src[2].reg.idx[0].offset, reg_maps->sampler_map.count);
             }
             else if (ins.handler_idx == WINED3DSIH_LD
+                    || (ins.handler_idx == WINED3DSIH_LD_RAW && ins.src[1].reg.type == WINED3DSPR_RESOURCE)
                     || (ins.handler_idx == WINED3DSIH_RESINFO && ins.src[1].reg.type == WINED3DSPR_RESOURCE))
             {
                 shader_record_sample(reg_maps, ins.src[1].reg.idx[0].offset,
