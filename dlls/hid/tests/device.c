b89e3f80ae7a640ccd2d70aef67369273ec6abf6
Michael Stefaniuc <mstefani@redhat.de>
2017-02-15 10:54:12 +0100

    hid/tests: Remove duplicated for condition (coccinellery).
    Signed-off-by: Michael Stefaniuc <mstefani@redhat.de>
    Signed-off-by: Aric Stewart <aric@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/hid/tests/device.c
+++ dlls/hid/tests/device.c
@@ -358,7 +358,7 @@ static void test_get_input_report(void)
         {
             ok(data[0] == 0, "Report ID (0) is not the first byte of the data\n");
             report[0] = 0;
-            for (i = 0; i < Caps.InputReportByteLength && i < Caps.InputReportByteLength; i++)
+            for (i = 0; i < Caps.InputReportByteLength; i++)
             {
                 char bytestr[5];
                 sprintf(bytestr, "%x ", (BYTE)data[i]);
