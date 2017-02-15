1b3cbf52b2175426a55592f2891b85e55329a2bf
Michael Stefaniuc <mstefani@redhat.de>
2017-02-15 10:51:31 +0100

    kernel32: Remove always true if conditions (coccinellery).
    Signed-off-by: Michael Stefaniuc <mstefani@redhat.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/kernel32/time.c
+++ dlls/kernel32/time.c
@@ -1308,11 +1308,8 @@ BOOL WINAPI FileTimeToDosDateTime( const FILETIME *ft, LPWORD fatdate,
     }
     unixtime = t;
     tm = gmtime( &unixtime );
-    if (fattime)
-        *fattime = (tm->tm_hour << 11) + (tm->tm_min << 5) + (tm->tm_sec / 2);
-    if (fatdate)
-        *fatdate = ((tm->tm_year - 80) << 9) + ((tm->tm_mon + 1) << 5)
-                   + tm->tm_mday;
+    *fattime = (tm->tm_hour << 11) + (tm->tm_min << 5) + (tm->tm_sec / 2);
+    *fatdate = ((tm->tm_year - 80) << 9) + ((tm->tm_mon + 1) << 5) + tm->tm_mday;
     return TRUE;
 }
 
