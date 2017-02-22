d369857cb4dce467b27606cba5bde4c58b790e41
Francois Gouget <fgouget@free.fr>
2017-02-22 11:26:51 +0100

    xmllite: Add __WINE_ALLOC_SIZE attributes to heap_xxx() functions.
    And standardize their parameter names.
    
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/xmllite/xmllite_private.h
+++ dlls/xmllite/xmllite_private.h
@@ -22,14 +22,15 @@
 #define __XMLLITE_PRIVATE__
 
 /* memory allocation functions */
-static inline void *heap_alloc(size_t len)
+
+static inline void* __WINE_ALLOC_SIZE(1) heap_alloc(size_t size)
 {
-    return HeapAlloc(GetProcessHeap(), 0, len);
+    return HeapAlloc(GetProcessHeap(), 0, size);
 }
 
-static inline void *heap_realloc(void *mem, size_t len)
+static inline void* __WINE_ALLOC_SIZE(2) heap_realloc(void *mem, size_t size)
 {
-    return HeapReAlloc(GetProcessHeap(), 0, mem, len);
+    return HeapReAlloc(GetProcessHeap(), 0, mem, size);
 }
 
 static inline BOOL heap_free(void *mem)
