wine-mirror/wine

commit 2641c5c28f407fb157028c8e7dda5e306b6b313c

wined3d: Pass a texture and sub-resource index to context_acquire().
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/drawprim.c
@@ -427,7 +427,7 @@ void draw_primitive(struct wined3d_device *device, const struct wined3d_state *s
 
     if (!(rtv = fb->render_targets[0]))
         rtv = fb->depth_stencil;
-    context = context_acquire(device, wined3d_rendertarget_view_get_surface(rtv));
+    context = context_acquire(device, wined3d_texture_from_resource(rtv->resource), rtv->sub_resource_idx);
     if (!context->valid)
     {
         context_release(context);
@@ -587,7 +587,7 @@ void dispatch_compute(struct wined3d_device *device, const struct wined3d_state 
     const struct wined3d_gl_info *gl_info;
     struct wined3d_context *context;
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     if (!context->valid)
     {
         context_release(context);

dlls/wined3d/arb_program_shader.c
@@ -4755,7 +4755,7 @@ static void shader_arb_destroy(struct wined3d_shader *shader)
 
         if (shader_data->num_gl_shaders)
         {
-            struct wined3d_context *context = context_acquire(device, NULL);
+            struct wined3d_context *context = context_acquire(device, NULL, 0);
 
             for (i = 0; i < shader_data->num_gl_shaders; ++i)
             {
@@ -4779,7 +4779,7 @@ static void shader_arb_destroy(struct wined3d_shader *shader)
 
         if (shader_data->num_gl_shaders)
         {
-            struct wined3d_context *context = context_acquire(device, NULL);
+            struct wined3d_context *context = context_acquire(device, NULL, 0);
 
             for (i = 0; i < shader_data->num_gl_shaders; ++i)
             {
@@ -7798,7 +7798,7 @@ static void arbfp_blit_surface(struct wined3d_device *device, enum wined3d_blit_
     struct wined3d_color_key alpha_test_key;
 
     /* Activate the destination context, set it up for blitting */
-    context = context_acquire(device, dst_surface);
+    context = context_acquire(device, dst_texture, dst_sub_resource_idx);
 
     /* Now load the surface */
     if (wined3d_settings.offscreen_rendering_mode != ORM_FBO

dlls/wined3d/buffer.c
@@ -744,7 +744,7 @@ static void buffer_unload(struct wined3d_resource *resource)
     {
         struct wined3d_context *context;
 
-        context = context_acquire(resource->device, NULL);
+        context = context_acquire(resource->device, NULL, 0);
 
         wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM);
         wined3d_buffer_invalidate_location(buffer, WINED3D_LOCATION_BUFFER);
@@ -776,7 +776,7 @@ static void wined3d_buffer_destroy_object(void *object)
 
     if (buffer->buffer_object)
     {
-        context = context_acquire(buffer->resource.device, NULL);
+        context = context_acquire(buffer->resource.device, NULL, 0);
         buffer_destroy_buffer_object(buffer, context);
         context_release(context);
 
@@ -1039,7 +1039,7 @@ static HRESULT wined3d_buffer_map(struct wined3d_buffer *buffer, UINT offset, UI
         {
             if (!(buffer->locations & WINED3D_LOCATION_SYSMEM))
             {
-                context = context_acquire(device, NULL);
+                context = context_acquire(device, NULL, 0);
                 wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM);
                 context_release(context);
             }
@@ -1051,7 +1051,7 @@ static HRESULT wined3d_buffer_map(struct wined3d_buffer *buffer, UINT offset, UI
         {
             const struct wined3d_gl_info *gl_info;
 
-            context = context_acquire(device, NULL);
+            context = context_acquire(device, NULL, 0);
             gl_info = context->gl_info;
 
             if (!(flags & WINED3D_MAP_DISCARD))
@@ -1156,7 +1156,7 @@ static void wined3d_buffer_unmap(struct wined3d_buffer *buffer)
         const struct wined3d_gl_info *gl_info;
         struct wined3d_context *context;
 
-        context = context_acquire(device, NULL);
+        context = context_acquire(device, NULL, 0);
         gl_info = context->gl_info;
 
         buffer_bind(buffer, context);
@@ -1206,7 +1206,7 @@ HRESULT wined3d_buffer_copy(struct wined3d_buffer *dst_buffer, unsigned int dst_
 
     device = dst_buffer->resource.device;
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     gl_info = context->gl_info;
 
     dst_location = wined3d_buffer_get_memory(dst_buffer, &dst, dst_buffer->locations);
@@ -1300,7 +1300,7 @@ static void buffer_resource_preload(struct wined3d_resource *resource)
 {
     struct wined3d_context *context;
 
-    context = context_acquire(resource->device, NULL);
+    context = context_acquire(resource->device, NULL, 0);
     wined3d_buffer_load(buffer_from_resource(resource), context, NULL);
     context_release(context);
 }

dlls/wined3d/context.c
@@ -1376,7 +1376,8 @@ void context_restore(struct wined3d_context *context, struct wined3d_surface *re
             || context->current_rt.sub_resource_idx != surface_get_sub_resource_idx(restore))
     {
         context_release(context);
-        context = context_acquire(restore->container->resource.device, restore);
+        context = context_acquire(restore->container->resource.device,
+                restore->container, surface_get_sub_resource_idx(restore));
     }
 
     context_release(context);
@@ -3675,53 +3676,47 @@ static void context_setup_target(struct wined3d_context *context,
     context_set_render_offscreen(context, render_offscreen);
 }
 
-struct wined3d_context *context_acquire(const struct wined3d_device *device, struct wined3d_surface *target)
+struct wined3d_context *context_acquire(const struct wined3d_device *device,
+        struct wined3d_texture *texture, unsigned int sub_resource_idx)
 {
     struct wined3d_context *current_context = context_get_current();
-    struct wined3d_texture *target_texture;
-    unsigned int target_sub_resource_idx;
     struct wined3d_context *context;
 
-    TRACE("device %p, target %p.\n", device, target);
+    TRACE("device %p, texture %p, sub_resource_idx %u.\n", device, texture, sub_resource_idx);
 
     if (current_context && current_context->destroyed)
         current_context = NULL;
 
-    if (target)
-    {
-        target_texture = target->container;
-        target_sub_resource_idx = surface_get_sub_resource_idx(target);
-    }
-    else
+    if (!texture)
     {
         if (current_context
                 && current_context->current_rt.texture
                 && current_context->device == device)
         {
-            target_texture = current_context->current_rt.texture;
-            target_sub_resource_idx = current_context->current_rt.sub_resource_idx;
+            texture = current_context->current_rt.texture;
+            sub_resource_idx = current_context->current_rt.sub_resource_idx;
         }
         else
         {
             struct wined3d_swapchain *swapchain = device->swapchains[0];
 
             if (swapchain->back_buffers)
-                target_texture = swapchain->back_buffers[0];
+                texture = swapchain->back_buffers[0];
             else
-                target_texture = swapchain->front_buffer;
-            target_sub_resource_idx = 0;
+                texture = swapchain->front_buffer;
+            sub_resource_idx = 0;
         }
     }
 
-    if (current_context && current_context->current_rt.texture == target_texture)
+    if (current_context && current_context->current_rt.texture == texture)
     {
         context = current_context;
     }
-    else if (target_texture->swapchain)
+    else if (texture->swapchain)
     {
         TRACE("Rendering onscreen.\n");
 
-        context = swapchain_get_context(target_texture->swapchain);
+        context = swapchain_get_context(texture->swapchain);
     }
     else
     {
@@ -3737,7 +3732,7 @@ struct wined3d_context *context_acquire(const struct wined3d_device *device, str
 
     context_enter(context);
     context_update_window(context);
-    context_setup_target(context, target_texture, target_sub_resource_idx);
+    context_setup_target(context, texture, sub_resource_idx);
     if (!context->valid) return context;
 
     if (context != current_context)

dlls/wined3d/device.c
@@ -232,7 +232,10 @@ void device_clear_render_targets(struct wined3d_device *device, UINT rt_count, c
     BOOL render_offscreen;
     unsigned int i;
 
-    context = context_acquire(device, target);
+    if (target)
+        context = context_acquire(device, target->container, rtv->sub_resource_idx);
+    else
+        context = context_acquire(device, NULL, 0);
     if (!context->valid)
     {
         context_release(context);
@@ -959,7 +962,7 @@ static void wined3d_device_delete_opengl_contexts(struct wined3d_device *device)
         device->shader_backend->shader_destroy(shader);
     }
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     device->blitter->free_private(device);
     device->shader_backend->shader_free_private(device);
     destroy_dummy_textures(device, context);
@@ -1093,7 +1096,7 @@ HRESULT CDECL wined3d_device_init_3d(struct wined3d_device *device,
     device->swapchains[0] = swapchain;
     device_init_swapchain_state(device, swapchain);
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
 
     create_dummy_textures(device, context);
     create_default_samplers(device, context);
@@ -3211,7 +3214,7 @@ HRESULT CDECL wined3d_device_process_vertices(struct wined3d_device *device,
         FIXME("Output vertex declaration not implemented yet.\n");
 
     /* Need any context to write to the vbo. */
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     gl_info = context->gl_info;
 
     vs = state->shader[WINED3D_SHADER_TYPE_VERTEX];
@@ -3471,7 +3474,7 @@ HRESULT CDECL wined3d_device_end_scene(struct wined3d_device *device)
         return WINED3DERR_INVALIDCALL;
     }
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     /* We only have to do this if we need to read the, swapbuffers performs a flush for us */
     context->gl_info->gl_ops.gl.p_glFlush();
     /* No checkGLcall here to avoid locking the lock just for checking a call that hardly ever
@@ -3657,7 +3660,7 @@ static HRESULT wined3d_device_update_texture_3d(struct wined3d_device *device,
         return WINED3DERR_INVALIDCALL;
     }
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
 
     /* Only a prepare, since we're uploading entire volumes. */
     wined3d_texture_prepare_texture(dst_texture, context, FALSE);
@@ -3744,7 +3747,7 @@ HRESULT CDECL wined3d_device_update_texture(struct wined3d_device *device,
     }
 
     /* Make sure that the destination texture is loaded. */
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     wined3d_texture_load(dst_texture, context, FALSE);
     context_release(context);
 
@@ -4150,7 +4153,7 @@ void CDECL wined3d_device_update_sub_resource(struct wined3d_device *device, str
             return;
         }
 
-        context = context_acquire(resource->device, NULL);
+        context = context_acquire(resource->device, NULL, 0);
         if (!wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_BUFFER))
         {
             ERR("Failed to load buffer location.\n");
@@ -4193,7 +4196,7 @@ void CDECL wined3d_device_update_sub_resource(struct wined3d_device *device, str
     addr.buffer_object = 0;
     addr.addr = data;
 
-    context = context_acquire(resource->device, NULL);
+    context = context_acquire(resource->device, NULL, 0);
 
     /* Only load the sub-resource for partial updates. */
     if (!box || (!box->left && !box->top && !box->front

dlls/wined3d/glsl_shader.c
@@ -8822,7 +8822,7 @@ static void shader_glsl_destroy(struct wined3d_shader *shader)
         return;
     }
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     gl_info = context->gl_info;
 
     TRACE("Deleting linked programs.\n");

dlls/wined3d/query.c
@@ -87,7 +87,8 @@ static enum wined3d_event_query_result wined3d_event_query_test(const struct win
         return WINED3D_EVENT_QUERY_WRONG_THREAD;
     }
 
-    context = context_acquire(device, context_get_rt_surface(query->context));
+    context = context_acquire(device, query->context->current_rt.texture,
+            query->context->current_rt.sub_resource_idx);
     gl_info = context->gl_info;
 
     if (gl_info->supported[ARB_SYNC])
@@ -162,7 +163,8 @@ enum wined3d_event_query_result wined3d_event_query_finish(const struct wined3d_
         return WINED3D_EVENT_QUERY_WRONG_THREAD;
     }
 
-    context = context_acquire(device, context_get_rt_surface(query->context));
+    context = context_acquire(device, query->context->current_rt.texture,
+            query->context->current_rt.sub_resource_idx);
 
     if (gl_info->supported[ARB_SYNC])
     {
@@ -217,17 +219,18 @@ void wined3d_event_query_issue(struct wined3d_event_query *query, const struct w
         if (!query->context->gl_info->supported[ARB_SYNC] && query->context->tid != GetCurrentThreadId())
         {
             context_free_event_query(query);
-            context = context_acquire(device, NULL);
+            context = context_acquire(device, NULL, 0);
             context_alloc_event_query(context, query);
         }
         else
         {
-            context = context_acquire(device, context_get_rt_surface(query->context));
+            context = context_acquire(device, query->context->current_rt.texture,
+                    query->context->current_rt.sub_resource_idx);
         }
     }
     else
     {
-        context = context_acquire(device, NULL);
+        context = context_acquire(device, NULL, 0);
         context_alloc_event_query(context, query);
     }
 
@@ -382,7 +385,8 @@ static BOOL wined3d_occlusion_query_ops_poll(struct wined3d_query *query, DWORD 
         return TRUE;
     }
 
-    context = context_acquire(device, context_get_rt_surface(oq->context));
+    context = context_acquire(device, oq->context->current_rt.texture,
+            oq->context->current_rt.sub_resource_idx);
     gl_info = context->gl_info;
 
     GL_EXTCALL(glGetQueryObjectuiv(oq->id, GL_QUERY_RESULT_AVAILABLE, &available));
@@ -495,12 +499,13 @@ static void wined3d_occlusion_query_ops_issue(struct wined3d_query *query, DWORD
                 FIXME("Wrong thread, can't restart query.\n");
 
                 context_free_occlusion_query(oq);
-                context = context_acquire(query->device, NULL);
+                context = context_acquire(query->device, NULL, 0);
                 context_alloc_occlusion_query(context, oq);
             }
             else
             {
-                context = context_acquire(device, context_get_rt_surface(oq->context));
+                context = context_acquire(device, oq->context->current_rt.texture,
+                        oq->context->current_rt.sub_resource_idx);
 
                 GL_EXTCALL(glEndQuery(GL_SAMPLES_PASSED));
                 checkGLcall("glEndQuery()");
@@ -510,7 +515,7 @@ static void wined3d_occlusion_query_ops_issue(struct wined3d_query *query, DWORD
         {
             if (oq->context)
                 context_free_occlusion_query(oq);
-            context = context_acquire(query->device, NULL);
+            context = context_acquire(query->device, NULL, 0);
             context_alloc_occlusion_query(context, oq);
         }
 
@@ -532,7 +537,8 @@ static void wined3d_occlusion_query_ops_issue(struct wined3d_query *query, DWORD
             }
             else
             {
-                context = context_acquire(device, context_get_rt_surface(oq->context));
+                context = context_acquire(device, oq->context->current_rt.texture,
+                        oq->context->current_rt.sub_resource_idx);
 
                 GL_EXTCALL(glEndQuery(GL_SAMPLES_PASSED));
                 checkGLcall("glEndQuery()");
@@ -561,7 +567,8 @@ static BOOL wined3d_timestamp_query_ops_poll(struct wined3d_query *query, DWORD 
         return TRUE;
     }
 
-    context = context_acquire(device, context_get_rt_surface(tq->context));
+    context = context_acquire(device, tq->context->current_rt.texture,
+            tq->context->current_rt.sub_resource_idx);
     gl_info = context->gl_info;
 
     GL_EXTCALL(glGetQueryObjectuiv(tq->id, GL_QUERY_RESULT_AVAILABLE, &available));
@@ -597,7 +604,7 @@ static void wined3d_timestamp_query_ops_issue(struct wined3d_query *query, DWORD
     {
         if (tq->context)
             context_free_timestamp_query(tq);
-        context = context_acquire(query->device, NULL);
+        context = context_acquire(query->device, NULL, 0);
         gl_info = context->gl_info;
         context_alloc_timestamp_query(context, tq);
         GL_EXTCALL(glQueryCounter(tq->id, GL_TIMESTAMP));

dlls/wined3d/sampler.c
@@ -39,7 +39,7 @@ static void wined3d_sampler_destroy_object(void *object)
     const struct wined3d_gl_info *gl_info;
     struct wined3d_context *context;
 
-    context = context_acquire(sampler->device, NULL);
+    context = context_acquire(sampler->device, NULL, 0);
     gl_info = context->gl_info;
     GL_EXTCALL(glDeleteSamplers(1, &sampler->name));
     context_release(context);
@@ -77,7 +77,7 @@ static void wined3d_sampler_init(struct wined3d_sampler *sampler, struct wined3d
     sampler->parent = parent;
     sampler->desc = *desc;
 
-    context = context_acquire(device, NULL);
+    context = context_acquire(device, NULL, 0);
     gl_info = context->gl_info;
 
     GL_EXTCALL(glGenSamplers(1, &sampler->name));

commit f4f2d4052b12ba15428cd9477b414b95e664857f

wined3d: Do not crash when attempting to issue draw call with no attachments.
The is broken since by 2641c5c28f407fb157028c8e7dda5e306b6b313c.

Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/drawprim.c
@@ -427,7 +427,10 @@ void draw_primitive(struct wined3d_device *device, const struct wined3d_state *s
 
     if (!(rtv = fb->render_targets[0]))
         rtv = fb->depth_stencil;
-    context = context_acquire(device, wined3d_texture_from_resource(rtv->resource), rtv->sub_resource_idx);
+    if (rtv)
+        context = context_acquire(device, wined3d_texture_from_resource(rtv->resource), rtv->sub_resource_idx);
+    else
+        context = context_acquire(device, NULL, 0);
     if (!context->valid)
     {
         context_release(context);