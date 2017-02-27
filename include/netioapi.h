08cae050154a3706df1e418f526d943ab77b9b58
Bruno Jesus <00cpxxx@gmail.com>
2017-02-26 22:28:09 -0300

    include: Add missing GetIfTable2 to netioapi.h.
    Signed-off-by: Bruno Jesus <00cpxxx@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/netioapi.h
+++ include/netioapi.h
@@ -108,6 +108,7 @@ DWORD WINAPI ConvertInterfaceNameToLuidA(const char*,NET_LUID*);
 DWORD WINAPI ConvertInterfaceNameToLuidW(const WCHAR*,NET_LUID*);
 void WINAPI FreeMibTable(void*);
 DWORD WINAPI GetIfEntry2(MIB_IF_ROW2*);
+DWORD WINAPI GetIfTable2(MIB_IF_TABLE2**);
 DWORD WINAPI GetUnicastIpAddressEntry(MIB_UNICASTIPADDRESS_ROW*);
 
 #endif /* __WINE_NETIOAPI_H */
