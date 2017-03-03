wine-mirror/wine

commit f7d40d45da7a1bc0accd58bca6d6da4e34861937

ntdll: NtFlushBuffersFile should fail if the file handle has no write access.
Signed-off-by: Paul Gofman <gofmanp@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/ntdll/file.c
@@ -3423,7 +3423,7 @@ NTSTATUS WINAPI NtFlushBuffersFile( HANDLE hFile, IO_STATUS_BLOCK* IoStatusBlock
     {
         ret = COMM_FlushBuffersFile( fd );
     }
-    else
+    else if (ret != STATUS_ACCESS_DENIED)
     {
         SERVER_START_REQ( flush )
         {

dlls/ntdll/tests/file.c
@@ -4429,7 +4429,6 @@ static void test_flush_buffers_file(void)
     ok(status == STATUS_SUCCESS, "expected STATUS_SUCCESS, got %#x.\n", status);
 
     status = pNtFlushBuffersFile(hfileread, &io_status_block);
-    todo_wine
     ok(status == STATUS_ACCESS_DENIED, "expected STATUS_ACCESS_DENIED, got %#x.\n", status);
 
     status = pNtFlushBuffersFile(NULL, &io_status_block);

commit c530fb9e28208db784f70b3794cbb16f2ce56ca1

ntdll: Fix NtFlushBuffersFile() for files with FILE_APPEND_DATA access.
Fixes a regression introduced by commit
f7d40d45da7a1bc0accd58bca6d6da4e34861937.

Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/ntdll/file.c
@@ -3418,6 +3418,8 @@ NTSTATUS WINAPI NtFlushBuffersFile( HANDLE hFile, IO_STATUS_BLOCK* IoStatusBlock
     int fd, needs_close;
 
     ret = server_get_unix_fd( hFile, FILE_WRITE_DATA, &fd, &needs_close, &type, NULL );
+    if (ret == STATUS_ACCESS_DENIED)
+        ret = server_get_unix_fd( hFile, FILE_APPEND_DATA, &fd, &needs_close, &type, NULL );
 
     if (!ret && type == FD_TYPE_SERIAL)
     {

dlls/ntdll/tests/file.c
@@ -4441,7 +4441,6 @@ static void test_flush_buffers_file(void)
     ok(hfile != INVALID_HANDLE_VALUE, "could not open temp file, error %d.\n", GetLastError());
 
     status = pNtFlushBuffersFile(hfile, &io_status_block);
-    todo_wine
     ok(status == STATUS_SUCCESS, "expected STATUS_SUCCESS, got %#x.\n", status);
 
     CloseHandle(hfile);