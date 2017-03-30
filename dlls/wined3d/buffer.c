wine-mirror/wine

commit 15d53761a5fbfc12fc5f9974c029dace00eab33d

wined3d: Do not pin system memory in wined3d_buffer_load_location().
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/buffer.c
@@ -604,7 +604,6 @@ BOOL wined3d_buffer_load_location(struct wined3d_buffer *buffer,
             GL_EXTCALL(glGetBufferSubData(buffer->buffer_type_hint, 0, buffer->resource.size,
                     buffer->resource.heap_memory));
             checkGLcall("buffer download");
-            buffer->flags |= WINED3D_BUFFER_PIN_SYSMEM;
             break;
 
         case WINED3D_LOCATION_BUFFER:
@@ -623,7 +622,8 @@ BOOL wined3d_buffer_load_location(struct wined3d_buffer *buffer,
 /* Context activation is done by the caller. */
 BYTE *wined3d_buffer_load_sysmem(struct wined3d_buffer *buffer, struct wined3d_context *context)
 {
-    wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM);
+    if (wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM))
+        buffer->flags |= WINED3D_BUFFER_PIN_SYSMEM;
     return buffer->resource.heap_memory;
 }
 
@@ -655,7 +655,6 @@ DWORD wined3d_buffer_get_memory(struct wined3d_buffer *buffer,
 static void buffer_unload(struct wined3d_resource *resource)
 {
     struct wined3d_buffer *buffer = buffer_from_resource(resource);
-    DWORD flags = buffer->flags;
 
     TRACE("buffer %p.\n", buffer);
 
@@ -665,11 +664,7 @@ static void buffer_unload(struct wined3d_resource *resource)
 
         context = context_acquire(resource->device, NULL);
 
-        /* Download the buffer, but don't permanently enable double buffering. */
         wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM);
-        if (!(flags & WINED3D_BUFFER_PIN_SYSMEM))
-            buffer->flags &= ~WINED3D_BUFFER_PIN_SYSMEM;
-
         wined3d_buffer_invalidate_location(buffer, WINED3D_LOCATION_BUFFER);
         buffer_destroy_buffer_object(buffer, context);
         buffer_clear_dirty_areas(buffer);
@@ -825,7 +820,12 @@ static void buffer_conversion_upload(struct wined3d_buffer *buffer, struct wined
     unsigned int i, j, range_idx, start, end, vertex_count;
     BYTE *data;
 
-    wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM);
+    if (!wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM))
+    {
+        ERR("Failed to load system memory.\n");
+        return;
+    }
+    buffer->flags |= WINED3D_BUFFER_PIN_SYSMEM;
 
     /* Now for each vertex in the buffer that needs conversion. */
     vertex_count = buffer->resource.size / buffer->stride;
@@ -1106,6 +1106,7 @@ static HRESULT wined3d_buffer_map(struct wined3d_buffer *buffer, UINT offset, UI
                     {
                         TRACE("Falling back to doublebuffered operation.\n");
                         wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM);
+                        buffer->flags |= WINED3D_BUFFER_PIN_SYSMEM;
                     }
                     TRACE("New pointer is %p.\n", buffer->resource.heap_memory);
                     buffer->map_ptr = NULL;

commit cd4223b0a43185c3477597e57447a6435d811657

wined3d: Only drop WINED3D_MAP_DISCARD right before mapping the buffer object in wined3d_buffer_map().
Dropping WINED3D_MAP_DISCARD too early means we go through the system memory
path unnecessarily. Worse, as mentioned in an earlier comment, Darkstar One
depends on the entire buffer being uploaded in case of WINED3D_MAP_DISCARD
maps, and dropping the flag too early prevents that as well. This fixes a
regression introduced by commit 15d53761a5fbfc12fc5f9974c029dace00eab33d.

Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/buffer.c
@@ -1004,14 +1004,6 @@ static HRESULT wined3d_buffer_map(struct wined3d_buffer *buffer, UINT offset, UI
 
     TRACE("buffer %p, offset %u, size %u, data %p, flags %#x.\n", buffer, offset, size, data, flags);
 
-    /* Filter redundant WINED3D_MAP_DISCARD maps. The 3DMark2001 multitexture
-     * fill rate test seems to depend on this. When we map a buffer with
-     * GL_MAP_INVALIDATE_BUFFER_BIT, the driver is free to discard the
-     * previous contents of the buffer. The r600g driver only does this when
-     * the buffer is currently in use, while the proprietary NVIDIA driver
-     * appears to do this unconditionally. */
-    if (buffer->flags & WINED3D_BUFFER_DISCARD)
-        flags &= ~WINED3D_MAP_DISCARD;
     count = ++buffer->resource.map_count;
 
     if (buffer->buffer_object)
@@ -1063,6 +1055,16 @@ static HRESULT wined3d_buffer_map(struct wined3d_buffer *buffer, UINT offset, UI
             if (count == 1)
             {
                 buffer_bind(buffer, context);
+
+                /* Filter redundant WINED3D_MAP_DISCARD maps. The 3DMark2001
+                 * multitexture fill rate test seems to depend on this. When
+                 * we map a buffer with GL_MAP_INVALIDATE_BUFFER_BIT, the
+                 * driver is free to discard the previous contents of the
+                 * buffer. The r600g driver only does this when the buffer is
+                 * currently in use, while the proprietary NVIDIA driver
+                 * appears to do this unconditionally. */
+                if (buffer->flags & WINED3D_BUFFER_DISCARD)
+                    flags &= ~WINED3D_MAP_DISCARD;
 
                 if (gl_info->supported[ARB_MAP_BUFFER_RANGE])
                 {