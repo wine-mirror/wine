wine-mirror/wine

commit 9ffacc434e90d80dbdfb107b10f02a759f2266f3

wined3d: Load all layers for render target views.
For layered rendering to array textures.

Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/view.c
@@ -398,6 +398,25 @@ void wined3d_rendertarget_view_get_drawable_size(const struct wined3d_rendertarg
     }
 }
 
+void wined3d_rendertarget_view_load_location(struct wined3d_rendertarget_view *view,
+        struct wined3d_context *context, DWORD location)
+{
+    struct wined3d_resource *resource = view->resource;
+    unsigned int i, sub_resource_idx;
+    struct wined3d_texture *texture;
+
+    if (resource->type == WINED3D_RTYPE_BUFFER)
+    {
+        wined3d_buffer_load_location(buffer_from_resource(resource), context, location);
+        return;
+    }
+
+    texture = texture_from_resource(resource);
+    sub_resource_idx = view->sub_resource_idx;
+    for (i = 0; i < view->layer_count; ++i, sub_resource_idx += texture->level_count)
+        wined3d_texture_load_location(texture, sub_resource_idx, context, location);
+}
+
 static void wined3d_render_target_view_cs_init(void *object)
 {
     struct wined3d_rendertarget_view *view = object;

dlls/wined3d/drawprim.c
@@ -507,7 +507,7 @@ void draw_primitive(struct wined3d_device *device, const struct wined3d_state *s
         rt = wined3d_texture_from_resource(rtv->resource);
         if (state->render_states[WINED3D_RS_COLORWRITEENABLE])
         {
-            wined3d_texture_load_location(rt, rtv->sub_resource_idx, context, rtv->resource->draw_binding);
+            wined3d_rendertarget_view_load_location(rtv, context, rtv->resource->draw_binding);
             wined3d_texture_invalidate_location(rt, rtv->sub_resource_idx, ~rtv->resource->draw_binding);
         }
         else
@@ -527,7 +527,7 @@ void draw_primitive(struct wined3d_device *device, const struct wined3d_state *s
         struct wined3d_surface *ds = wined3d_rendertarget_view_get_surface(dsv);
 
         if (state->render_states[WINED3D_RS_ZWRITEENABLE] || state->render_states[WINED3D_RS_ZENABLE])
-            wined3d_texture_load_location(ds->container, dsv->sub_resource_idx, context, location);
+            wined3d_rendertarget_view_load_location(dsv, context, location);
         else
             wined3d_texture_prepare_location(ds->container, dsv->sub_resource_idx, context, location);
     }

dlls/wined3d/wined3d_private.h
@@ -3458,6 +3458,8 @@ static inline struct wined3d_surface *wined3d_rendertarget_view_get_surface(
 
 void wined3d_rendertarget_view_get_drawable_size(const struct wined3d_rendertarget_view *view,
         const struct wined3d_context *context, unsigned int *width, unsigned int *height) DECLSPEC_HIDDEN;
+void wined3d_rendertarget_view_load_location(struct wined3d_rendertarget_view *view,
+        struct wined3d_context *context, DWORD location) DECLSPEC_HIDDEN;
 
 struct wined3d_shader_resource_view
 {

commit 583255f2b2ff97e26348b3db4d00a9fe80b28688

wined3d: Use correct layer count for 3D textures.
Fixes a regression introduced by commit
9ffacc434e90d80dbdfb107b10f02a759f2266f3.

Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/view.c
@@ -422,7 +422,7 @@ void wined3d_rendertarget_view_prepare_location(struct wined3d_rendertarget_view
         struct wined3d_context *context, DWORD location)
 {
     struct wined3d_resource *resource = view->resource;
-    unsigned int i, sub_resource_idx;
+    unsigned int i, sub_resource_idx, layer_count;
     struct wined3d_texture *texture;
 
     if (resource->type == WINED3D_RTYPE_BUFFER)
@@ -433,7 +433,8 @@ void wined3d_rendertarget_view_prepare_location(struct wined3d_rendertarget_view
 
     texture = texture_from_resource(resource);
     sub_resource_idx = view->sub_resource_idx;
-    for (i = 0; i < view->layer_count; ++i, sub_resource_idx += texture->level_count)
+    layer_count = resource->type != WINED3D_RTYPE_TEXTURE_3D ? view->layer_count : 1;
+    for (i = 0; i < layer_count; ++i, sub_resource_idx += texture->level_count)
         wined3d_texture_prepare_location(texture, sub_resource_idx, context, location);
 }
 
@@ -441,7 +442,7 @@ void wined3d_rendertarget_view_load_location(struct wined3d_rendertarget_view *v
         struct wined3d_context *context, DWORD location)
 {
     struct wined3d_resource *resource = view->resource;
-    unsigned int i, sub_resource_idx;
+    unsigned int i, sub_resource_idx, layer_count;
     struct wined3d_texture *texture;
 
     if (resource->type == WINED3D_RTYPE_BUFFER)
@@ -452,14 +453,15 @@ void wined3d_rendertarget_view_load_location(struct wined3d_rendertarget_view *v
 
     texture = texture_from_resource(resource);
     sub_resource_idx = view->sub_resource_idx;
-    for (i = 0; i < view->layer_count; ++i, sub_resource_idx += texture->level_count)
+    layer_count = resource->type != WINED3D_RTYPE_TEXTURE_3D ? view->layer_count : 1;
+    for (i = 0; i < layer_count; ++i, sub_resource_idx += texture->level_count)
         wined3d_texture_load_location(texture, sub_resource_idx, context, location);
 }
 
 void wined3d_rendertarget_view_validate_location(struct wined3d_rendertarget_view *view, DWORD location)
 {
     struct wined3d_resource *resource = view->resource;
-    unsigned int i, sub_resource_idx;
+    unsigned int i, sub_resource_idx, layer_count;
     struct wined3d_texture *texture;
 
     if (resource->type == WINED3D_RTYPE_BUFFER)
@@ -470,7 +472,8 @@ void wined3d_rendertarget_view_validate_location(struct wined3d_rendertarget_vie
 
     texture = texture_from_resource(resource);
     sub_resource_idx = view->sub_resource_idx;
-    for (i = 0; i < view->layer_count; ++i, sub_resource_idx += texture->level_count)
+    layer_count = resource->type != WINED3D_RTYPE_TEXTURE_3D ? view->layer_count : 1;
+    for (i = 0; i < layer_count; ++i, sub_resource_idx += texture->level_count)
         wined3d_texture_validate_location(texture, sub_resource_idx, location);
 }
 