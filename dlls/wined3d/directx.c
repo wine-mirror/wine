08e68f4fe852022622de7545ec0d3d9870ead795
Józef Kucia <jkucia@codeweavers.com>
2017-04-25 12:25:50 +0200

    wined3d: Initialize geometry shader limits when OpenGL 3.2 is supported.
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/directx.c
+++ dlls/wined3d/directx.c
@@ -3655,8 +3655,7 @@ static void wined3d_adapter_init_limits(struct wined3d_gl_info *gl_info)
         gl_info->limits.samplers[WINED3D_SHADER_TYPE_DOMAIN] = gl_max;
         TRACE("Max domain samplers: %u.\n", gl_info->limits.samplers[WINED3D_SHADER_TYPE_DOMAIN]);
     }
-    if ((!gl_info->supported[WINED3D_GL_LEGACY_CONTEXT] || gl_info->supported[ARB_GEOMETRY_SHADER4])
-            && gl_info->supported[ARB_UNIFORM_BUFFER_OBJECT])
+    if (gl_info->supported[WINED3D_GL_VERSION_3_2] && gl_info->supported[ARB_UNIFORM_BUFFER_OBJECT])
     {
         gl_info->gl_ops.gl.p_glGetIntegerv(GL_MAX_GEOMETRY_UNIFORM_BLOCKS, &gl_max);
         gl_info->limits.uniform_blocks[WINED3D_SHADER_TYPE_GEOMETRY] = min(gl_max, WINED3D_MAX_CBS);
