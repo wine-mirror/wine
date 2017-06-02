d20d9b7fa9881cfc340f4adb6f42efbaf51d0c57
Alexandre Julliard <julliard@winehq.org>
2017-06-02 17:20:33 +0200

    explorer: Don't forward the system tray to the display driver in desktop mode.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 082e4981631189c5903f838a0bc23e137c1cce17)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- programs/explorer/systray.c
+++ programs/explorer/systray.c
@@ -565,7 +565,7 @@ static BOOL handle_incoming(HWND hwndSource, COPYDATASTRUCT *cds)
                                buffer, buffer + cbMaskBits);
     }
 
-    /* try forward to x11drv first */
+    /* try forwarding to the display driver first */
     if (cds->dwData == NIM_ADD || !(icon = get_icon( nid.hWnd, nid.uID )))
     {
         if (wine_notify_icon && ((ret = wine_notify_icon( cds->dwData, &nid )) != -1))
@@ -901,7 +901,7 @@ void initialize_systray( HMODULE graphics_driver, BOOL using_root, BOOL arg_enab
     WNDCLASSEXW class;
     static const WCHAR classname[] = {'S','h','e','l','l','_','T','r','a','y','W','n','d',0};
 
-    wine_notify_icon = (void *)GetProcAddress( graphics_driver, "wine_notify_icon" );
+    if (using_root) wine_notify_icon = (void *)GetProcAddress( graphics_driver, "wine_notify_icon" );
 
     icon_cx = GetSystemMetrics( SM_CXSMICON ) + 2*ICON_BORDER;
     icon_cy = GetSystemMetrics( SM_CYSMICON ) + 2*ICON_BORDER;
