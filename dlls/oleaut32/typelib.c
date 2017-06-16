0dde88202a97578ffd346ae24d904a4f89cfb9b9
Nils Kuhnhenn <kuhnhenn.nils@gmail.com>
2017-06-16 16:05:07 +0200

    oleaut32: Use variable with the correct type in LIST_FOR_EACH_ENTRY_SAFE macro.
    Signed-off-by: Sebastian Lackner <sebastian@fds-team.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/oleaut32/typelib.c
+++ dlls/oleaut32/typelib.c
@@ -4728,10 +4728,9 @@ static ULONG WINAPI ITypeLib2_fnRelease( ITypeLib2 *iface)
     if (!ref)
     {
       TLBImpLib *pImpLib, *pImpLibNext;
-      TLBRefType *ref_type;
+      TLBRefType *ref_type, *ref_type_next;
       TLBString *tlbstr, *tlbstr_next;
       TLBGuid *tlbguid, *tlbguid_next;
-      void *cursor2;
       int i;
 
       /* remove cache entry */
@@ -4781,7 +4780,7 @@ static ULONG WINAPI ITypeLib2_fnRelease( ITypeLib2 *iface)
           heap_free(pImpLib);
       }
 
-      LIST_FOR_EACH_ENTRY_SAFE(ref_type, cursor2, &This->ref_list, TLBRefType, entry)
+      LIST_FOR_EACH_ENTRY_SAFE(ref_type, ref_type_next, &This->ref_list, TLBRefType, entry)
       {
           list_remove(&ref_type->entry);
           heap_free(ref_type);
