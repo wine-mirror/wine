60c0372223f419650e1e6fcf7d2156a23fcebcfd
Hugh McMaster <hugh.mcmaster@outlook.com>
2017-06-29 13:32:26 +0000

    reg/tests: Test comma placement when parsing hex data.
    Signed-off-by: Hugh McMaster <hugh.mcmaster@outlook.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/reg/tests/reg.c
+++ programs/reg/tests/reg.c
@@ -2089,6 +2089,16 @@ static void test_import(void)
     todo_wine ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
     todo_wine verify_reg_nonexist(hkey, "Wine62b");
 
+    test_import_str("REGEDIT4\n\n"
+                    "[HKEY_CURRENT_USER\\" KEY_BASE "]\n"
+                    "\"Wine63a\"=hex(7):4c,69,6e,65,20,\\\n"
+                    "  ,63,6f,6e,63,61,74,65,6e,61,74,69,6f,6e,00,00\n"
+                    "\"Wine63b\"=hex(7):4c,69,6e,65,20,\\\n"
+                    "  63,,6f,6e,63,61,74,65,6e,61,74,69,6f,6e,00,00\n\n", &r);
+    todo_wine ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
+    todo_wine verify_reg_nonexist(hkey, "Wine63a");
+    todo_wine verify_reg_nonexist(hkey, "Wine63b");
+
     err = RegCloseKey(hkey);
     todo_wine ok(err == ERROR_SUCCESS, "got %d, expected 0\n", err);
 
@@ -3362,6 +3372,22 @@ static void test_unicode_import(void)
                     "\"Wine62b\"=hex(7):4c,00,69,00,6e,00,65,00,20,00\\", &r);
     todo_wine ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
     todo_wine verify_reg_nonexist(hkey, "Wine62b");
+
+    test_import_wstr("\xef\xbb\xbfWindows Registry Editor Version 5.00\n\n"
+                     "[HKEY_CURRENT_USER\\" KEY_BASE "]\n"
+                     "\"Wine63a\"=hex(7):4c,00,69,00,6e,00,65,00,20,00,\\\n"
+                     "  ,63,00,6f,00,6e,00,\\\n"
+                     "  63,00,61,00,74,00,\\\n"
+                     "  65,00,6e,00,\\\n"
+                     "  61,00,74,00,69,00,6f,00,6e,00,00,00,00,00\n"
+                     "\"Wine63b\"=hex(7):4c,00,69,00,6e,00,65,00,20,00,\\\n"
+                     "  63,,00,6f,00,6e,00,\\\n"
+                     "  63,00,61,00,74,00,\\\n"
+                     "  65,00,6e,00,\\\n"
+                     "  61,00,74,00,69,00,6f,00,6e,00,00,00,00,00\n\n", &r);
+    todo_wine ok(r == REG_EXIT_SUCCESS, "got exit code %d, expected 0\n", r);
+    todo_wine verify_reg_nonexist(hkey, "Wine63a");
+    todo_wine verify_reg_nonexist(hkey, "Wine63b");
 
     err = RegCloseKey(hkey);
     todo_wine ok(err == ERROR_SUCCESS, "got %d, expected 0\n", err);
