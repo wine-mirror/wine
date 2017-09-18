10a80b8c1b7ed2945d701796cec3c1a4782c2702
Nikolay Sivov <nsivov@codeweavers.com>
2017-09-18 10:42:44 +0300

    gdiplus: Implement playback for EmfPlusRecordTypeSetPixelOffsetMode.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Vincent Povirk <vincent@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/gdiplus/metafile.c
+++ dlls/gdiplus/metafile.c
@@ -1832,6 +1832,10 @@ GpStatus WINGDIPAPI GdipPlayMetafileRecord(GDIPCONST GpMetafile *metafile,
 
             break;
         }
+        case EmfPlusRecordTypeSetPixelOffsetMode:
+        {
+            return GdipSetPixelOffsetMode(real_metafile->playback_graphics, (flags >> 8) & 0xf);
+        }
         default:
             FIXME("Not implemented for record type %x\n", recordType);
             return NotImplemented;
