398d42585e705572e105ec03deca3fb2b663724b
Nikolay Sivov <nsivov@codeweavers.com>
2017-09-25 10:59:35 +0300

    include: Added stgprop.h.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/ntquery.h
+++ include/ntquery.h
@@ -19,8 +19,7 @@
 #ifndef __WINE_NTQUERY_H__
 #define __WINE_NTQUERY_H__
 
-/* FIXME: #include <stgprop.h> */
-
+#include "stgprop.h"
 #include <pshpack4.h>
 
 typedef struct _CI_STATE
@@ -43,28 +42,6 @@ typedef struct _CI_STATE
 } CI_STATE;
 
 #include <poppack.h>
-
-#define PSGUID_STORAGE {0xb725f130, 0x47ef, 0x101a, {0xa5, 0xf1, 0x02, 0x60, 0x8c, 0x9e, 0xeb, 0xac}}
-#define PID_STG_DIRECTORY      ((PROPID)0x02)
-#define PID_STG_CLASSID        ((PROPID)0x03)
-#define PID_STG_STORAGETYPE    ((PROPID)0x04)
-#define PID_STG_VOLUME_ID      ((PROPID)0x05)
-#define PID_STG_PARENT_WORKID  ((PROPID)0x06)
-#define PID_STG_SECONDARYSTORE ((PROPID)0x07)
-#define PID_STG_FILEINDEX      ((PROPID)0x08)
-#define PID_STG_LASTCHANGEUSN  ((PROPID)0x09)
-#define PID_STG_NAME           ((PROPID)0x0a)
-#define PID_STG_PATH           ((PROPID)0x0b)
-#define PID_STG_SIZE           ((PROPID)0x0c)
-#define PID_STG_ATTRIBUTES     ((PROPID)0x0d)
-#define PID_STG_WRITETIME      ((PROPID)0x0e)
-#define PID_STG_CREATETIME     ((PROPID)0x0f)
-#define PID_STG_ACCESSTIME     ((PROPID)0x10)
-#define PID_STG_CHANGETIME     ((PROPID)0x11)
-#define PID_STG_CONTENTS       ((PROPID)0x13)
-#define PID_STG_SHORTNAME      ((PROPID)0x14)
-#define PID_STG_MAX            PID_STG_SHORTNAME
-
 
 #ifdef __cplusplus
 extern "C" {
