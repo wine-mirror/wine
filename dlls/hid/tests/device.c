d4ea289dc189520735f226cf8282dd1c1e0d3d0b
Andrey Gusev <andrey.goosev@gmail.com>
2017-10-09 15:10:22 +0300

    hid/tests: Remove redundant comparison.
    Signed-off-by: Andrey Gusev <andrey.goosev@gmail.com>
    Signed-off-by: Aric Stewart <aric@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/hid/tests/device.c
+++ dlls/hid/tests/device.c
@@ -167,7 +167,7 @@ static void process_data(HIDP_CAPS Caps, PHIDP_PREPARSED_DATA ppd, CHAR *data, D
         {
             ULONG usage_length = 100;
             status = HidP_GetUsages(HidP_Input, i, 0, button_pages, &usage_length, ppd, data, data_length);
-            ok (status == HIDP_STATUS_SUCCESS || (status != HIDP_STATUS_SUCCESS && usage_length == 0),
+            ok (status == HIDP_STATUS_SUCCESS || usage_length == 0,
                 "HidP_GetUsages failed (%x) but usage length still %i\n", status, usage_length);
             if (usage_length)
             {
