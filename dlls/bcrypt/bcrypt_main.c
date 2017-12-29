5c70c155d7f1928719f0abeb84e56134fa4e94d4
Alexandre Julliard <julliard@winehq.org>
2017-12-29 11:35:37 +0100

    bcrypt: macOS version 10.8 is needed for encryption support.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/bcrypt/bcrypt_main.c
+++ dlls/bcrypt/bcrypt_main.c
@@ -767,7 +767,7 @@ NTSTATUS WINAPI BCryptHash( BCRYPT_ALG_HANDLE algorithm, UCHAR *secret, ULONG se
     return BCryptDestroyHash( handle );
 }
 
-#if defined(HAVE_GNUTLS_CIPHER_INIT) || defined(HAVE_COMMONCRYPTO_COMMONCRYPTOR_H) && MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+#if defined(HAVE_GNUTLS_CIPHER_INIT) || defined(HAVE_COMMONCRYPTO_COMMONCRYPTOR_H) && MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
 static ULONG get_block_size( enum alg_id alg )
 {
     ULONG ret = 0, size = sizeof(ret);
@@ -896,7 +896,7 @@ static NTSTATUS key_destroy( struct key *key )
     HeapFree( GetProcessHeap(), 0, key );
     return STATUS_SUCCESS;
 }
-#elif defined(HAVE_COMMONCRYPTO_COMMONCRYPTOR_H) && MAC_OS_X_VERSION_MAX_ALLOWED >= 1070
+#elif defined(HAVE_COMMONCRYPTO_COMMONCRYPTOR_H) && MAC_OS_X_VERSION_MAX_ALLOWED >= 1080
 struct key
 {
     struct object  hdr;
