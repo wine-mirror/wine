b662f0c2ec14e29767487d10acf5cdb9837a3e09
Michael Stefaniuc <mstefani@winehq.org>
2018-01-30 23:41:03 +0100

    ieframe: Use the global HeapAlloc() wrappers.
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ieframe/ieframe.h
+++ dlls/ieframe/ieframe.h
@@ -38,6 +38,7 @@
 #include "docobjectservice.h"
 
 #include "wine/unicode.h"
+#include "wine/heap.h"
 #include "wine/list.h"
 
 typedef struct ConnectionPoint ConnectionPoint;
@@ -332,26 +333,6 @@ static inline void lock_module(void) {
 
 static inline void unlock_module(void) {
     InterlockedDecrement(&module_ref);
-}
-
-static inline void* __WINE_ALLOC_SIZE(1) heap_alloc(size_t size)
-{
-    return HeapAlloc(GetProcessHeap(), 0, size);
-}
-
-static inline void* __WINE_ALLOC_SIZE(1) heap_alloc_zero(size_t size)
-{
-    return HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size);
-}
-
-static inline void* __WINE_ALLOC_SIZE(2) heap_realloc(void *mem, size_t size)
-{
-    return HeapReAlloc(GetProcessHeap(), 0, mem, size);
-}
-
-static inline BOOL heap_free(void *mem)
-{
-    return HeapFree(GetProcessHeap(), 0, mem);
 }
 
 static inline LPWSTR heap_strdupW(LPCWSTR str)
