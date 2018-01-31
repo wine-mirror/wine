bfc0f3bdf12680ce498d30a10d27fc4c9a6b357e
Henri Verbeet <hverbeet@codeweavers.com>
2018-02-01 03:07:34 +0330

    wined3d: Use more resource access flags in wined3d_texture_use_pbo().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/texture.c
+++ dlls/wined3d/texture.c
@@ -32,8 +32,8 @@ WINE_DECLARE_DEBUG_CHANNEL(winediag);
 
 static BOOL wined3d_texture_use_pbo(const struct wined3d_texture *texture, const struct wined3d_gl_info *gl_info)
 {
-    return texture->resource.pool == WINED3D_POOL_DEFAULT
-            && texture->resource.access & WINED3D_RESOURCE_ACCESS_MAP
+    return (texture->resource.access & (WINED3D_RESOURCE_ACCESS_CPU
+            | WINED3D_RESOURCE_ACCESS_MAP)) == WINED3D_RESOURCE_ACCESS_MAP
             && gl_info->supported[ARB_PIXEL_BUFFER_OBJECT]
             && !texture->resource.format->conv_byte_count
             && !(texture->flags & (WINED3D_TEXTURE_PIN_SYSMEM | WINED3D_TEXTURE_COND_NP2_EMULATED));
