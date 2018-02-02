fee6ec8cf3d2cad4d8d0896256794056e37379f9
Nikolay Sivov <nsivov@codeweavers.com>
2018-02-02 08:39:41 +0300

    comctl32/hotkey: Use HeapAlloc() wrappers.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/comctl32/hotkey.c
+++ dlls/comctl32/hotkey.c
@@ -38,6 +38,7 @@
 #include "commctrl.h"
 #include "comctl32.h"
 #include "wine/debug.h"
+#include "wine/heap.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(hotkey);
 
@@ -241,7 +242,7 @@ HOTKEY_Destroy (HOTKEY_INFO *infoPtr)
 {
     /* free hotkey info data */
     SetWindowLongPtrW (infoPtr->hwndSelf, 0, 0);
-    Free (infoPtr);
+    heap_free (infoPtr);
     return 0;
 }
 
@@ -410,7 +411,7 @@ HOTKEY_NCCreate (HWND hwnd, const CREATESTRUCTW *lpcs)
                     dwExStyle | WS_EX_CLIENTEDGE);
 
     /* allocate memory for info structure */
-    infoPtr = Alloc (sizeof(HOTKEY_INFO));
+    infoPtr = heap_alloc_zero (sizeof(*infoPtr));
     SetWindowLongPtrW(hwnd, 0, (DWORD_PTR)infoPtr);
 
     /* initialize info structure */
