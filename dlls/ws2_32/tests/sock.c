67b690dba94d9671a8b5b33e51bf3102133ec100
André Hentschel <nerv@dawncrow.de>
2018-02-02 20:34:26 +0100

    ws2_32/tests: Don't test WSAGetLastError() value on success.
    This cuts the test output from around 1600 to around 1100 lines
    
    Signed-off-by: André Hentschel <nerv@dawncrow.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ws2_32/tests/sock.c
+++ dlls/ws2_32/tests/sock.c
@@ -1250,7 +1250,8 @@ static void test_WithWSAStartup(void)
             res = getsockname(sock, (struct sockaddr *)&saddr, &size);
             error = WSAGetLastError();
             ok(res == SOCKET_ERROR, "Test[%d]: getsockname should have failed\n", i);
-            ok(error == WSAENOTSOCK, "Test[%d]: expected 10038, got %d\n", i, error);
+            if (res == SOCKET_ERROR)
+                ok(error == WSAENOTSOCK, "Test[%d]: expected 10038, got %d\n", i, error);
         }
     }
 
