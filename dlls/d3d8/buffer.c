5fc8b43c8a2da3408ecdc3010d53c3b6b521ea04
Henri Verbeet <hverbeet@codeweavers.com>
2018-02-08 02:40:48 +0330

    d3d8: Use wined3d_buffer_create() in vertexbuffer_init().
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/buffer.c
+++ dlls/d3d8/buffer.c
@@ -273,25 +273,29 @@ static const struct wined3d_parent_ops d3d8_vertexbuffer_wined3d_parent_ops =
 HRESULT vertexbuffer_init(struct d3d8_vertexbuffer *buffer, struct d3d8_device *device,
         UINT size, DWORD usage, DWORD fvf, D3DPOOL pool)
 {
-    enum wined3d_pool wined3d_pool;
-    DWORD wined3d_usage;
+    struct wined3d_buffer_desc desc;
     HRESULT hr;
 
-    wined3d_pool = pool;
-    wined3d_usage = usage & WINED3DUSAGE_MASK;
     if (pool == D3DPOOL_SCRATCH)
     {
-        wined3d_pool = WINED3D_POOL_SYSTEM_MEM;
-        wined3d_usage |= WINED3DUSAGE_SCRATCH;
+        WARN("Vertex buffer with D3DPOOL_SCRATCH requested.\n");
+        return D3DERR_INVALIDCALL;
     }
 
     buffer->IDirect3DVertexBuffer8_iface.lpVtbl = &Direct3DVertexBuffer8_Vtbl;
     d3d8_resource_init(&buffer->resource);
     buffer->fvf = fvf;
 
+    desc.byte_width = size;
+    desc.usage = usage & WINED3DUSAGE_MASK;
+    desc.bind_flags = WINED3D_BIND_VERTEX_BUFFER;
+    desc.access = wined3daccess_from_d3dpool(pool);
+    desc.misc_flags = 0;
+    desc.structure_byte_stride = 0;
+
     wined3d_mutex_lock();
-    hr = wined3d_buffer_create_vb(device->wined3d_device, size, wined3d_usage, wined3d_pool,
-            buffer, &d3d8_vertexbuffer_wined3d_parent_ops, &buffer->wined3d_buffer);
+    hr = wined3d_buffer_create(device->wined3d_device, &desc, NULL, buffer,
+            &d3d8_vertexbuffer_wined3d_parent_ops, &buffer->wined3d_buffer);
     wined3d_mutex_unlock();
     if (FAILED(hr))
     {
