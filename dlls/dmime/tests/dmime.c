156aceebbe29560c4ff561185b92a533230b2ad4
Michael Stefaniuc <mstefani@winehq.org>
2018-03-11 22:20:42 +0100

    dmime: Reimplement ParseDescriptor for DMGraph.
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dmime/tests/dmime.c
+++ dlls/dmime/tests/dmime.c
@@ -770,13 +770,11 @@ static void test_parsedescriptor(void)
         memset(&desc, 0, sizeof(desc));
         hr = IDirectMusicObject_ParseDescriptor(dmo, stream, &desc);
         if (forms[i].needs_size) {
-            todo_wine_if(forms[i].form == DMUS_FOURCC_TOOLGRAPH_FORM)
-                ok(hr == E_INVALIDARG, "ParseDescriptor failed: %08x, expected E_INVALIDARG\n", hr);
+            ok(hr == E_INVALIDARG, "ParseDescriptor failed: %08x, expected E_INVALIDARG\n", hr);
             desc.dwSize = sizeof(desc);
             hr = IDirectMusicObject_ParseDescriptor(dmo, stream, &desc);
         }
-        todo_wine_if(forms[i].form == DMUS_FOURCC_TOOLGRAPH_FORM)
-            ok(hr == S_OK, "ParseDescriptor failed: %08x, expected S_OK\n", hr);
+        ok(hr == S_OK, "ParseDescriptor failed: %08x, expected S_OK\n", hr);
         ok(desc.dwValidData == DMUS_OBJ_CLASS, "Got valid data %#x, expected DMUS_OBJ_CLASS\n",
                 desc.dwValidData);
         ok(IsEqualGUID(&desc.guidClass, forms[i].clsid), "Got class guid %s, expected CLSID_%s\n",
@@ -790,13 +788,11 @@ static void test_parsedescriptor(void)
         desc.dwSize = sizeof(desc);
         hr = IDirectMusicObject_ParseDescriptor(dmo, stream, &desc);
         if (forms[i].needs_size)
-            todo_wine_if(forms[i].form == DMUS_FOURCC_TOOLGRAPH_FORM)
-                ok(hr == DMUS_E_CHUNKNOTFOUND,
+            ok(hr == DMUS_E_CHUNKNOTFOUND,
                     "ParseDescriptor failed: %08x, expected DMUS_E_CHUNKNOTFOUND\n", hr);
         else
             ok(hr == E_FAIL, "ParseDescriptor failed: %08x, expected E_FAIL\n", hr);
-        todo_wine_if(forms[i].form == DMUS_FOURCC_TOOLGRAPH_FORM)
-            ok(!desc.dwValidData, "Got valid data %#x, expected 0\n", desc.dwValidData);
+        ok(!desc.dwValidData, "Got valid data %#x, expected 0\n", desc.dwValidData);
 
         /* All desc chunks */
         alldesc[1] = forms[i].form;
@@ -839,9 +835,8 @@ static void test_parsedescriptor(void)
         desc.dwSize = sizeof(desc);
         hr = IDirectMusicObject_ParseDescriptor(dmo, stream, &desc);
         ok(hr == S_OK, "ParseDescriptor failed: %08x, expected S_OK\n", hr);
-        todo_wine_if(forms[i].form == DMUS_FOURCC_TOOLGRAPH_FORM)
-            ok(desc.dwValidData == DMUS_OBJ_CLASS, "Got valid data %#x, expected DMUS_OBJ_CLASS\n",
-                    desc.dwValidData);
+        ok(desc.dwValidData == DMUS_OBJ_CLASS, "Got valid data %#x, expected DMUS_OBJ_CLASS\n",
+                desc.dwValidData);
         IStream_Release(stream);
 
         /* INFO list with INAM */
