ecbb4fa3ea2d3a9e1d3f560c7e0dea008439db5b
Louis Lenders <xerox.xerox2000x@gmail.com>
2018-03-23 13:23:57 +0100

    wevtapi: Add stub for EvtExportLog.
    Signed-off-by: Louis Lenders <xerox.xerox2000x@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/winevt.h
+++ include/winevt.h
@@ -114,7 +114,8 @@ typedef struct _EVT_VARIANT {
 
 typedef DWORD (WINAPI *EVT_SUBSCRIBE_CALLBACK)(EVT_SUBSCRIBE_NOTIFY_ACTION Action,
                                                PVOID UserContext, EVT_HANDLE Event);
-
+BOOL WINAPI EvtExportLog(EVT_HANDLE session, const WCHAR *path, const WCHAR *query,
+                         const WCHAR *file, DWORD flags);
 BOOL WINAPI EvtGetChannelConfigProperty(EVT_HANDLE ChannelConfig,
                                         EVT_CHANNEL_CONFIG_PROPERTY_ID PropertyId,
                                         DWORD Flags, DWORD PropertyValueBufferSize,
