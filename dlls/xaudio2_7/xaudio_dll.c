b747d6f6ccdf1699a9242a570d681fa246de592e
Andrew Eikum <aeikum@codeweavers.com>
2018-03-26 13:14:20 -0500

    xaudio2: Prefer builtin version.
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/xaudio2_7/xaudio_dll.c
+++ dlls/xaudio2_7/xaudio_dll.c
@@ -89,8 +89,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD reason, void *pReserved)
 
     switch (reason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
     case DLL_PROCESS_ATTACH:
         instance = hinstDLL;
         DisableThreadLibraryCalls( hinstDLL );
