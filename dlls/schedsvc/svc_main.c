0f77a4cb8207dfdae6344e07d3e3d2217274094b
Dmitry Timoshkov <dmitry@baikal.ru>
2018-04-24 16:13:20 +0800

    schedsvc: Print an error if the service couldn't start monitoring the tasks directory.
    This happens for old prefixes with disabled updates.
    
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/schedsvc/svc_main.c
+++ dlls/schedsvc/svc_main.c
@@ -50,7 +50,14 @@ static DWORD WINAPI tasks_monitor_thread(void *arg)
 
     htasks = CreateFileW(path, FILE_LIST_DIRECTORY, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
                          NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
-    if (htasks == INVALID_HANDLE_VALUE) return -1;
+    if (htasks == INVALID_HANDLE_VALUE)
+    {
+        ERR("Couldn't start monitoring %s for tasks, error %u\n", debugstr_w(path), GetLastError());
+        /* Probably this is an old prefix with disabled updates */
+        if (GetLastError() == ERROR_PATH_NOT_FOUND || GetLastError() == ERROR_FILE_NOT_FOUND)
+            ERR("Please create the directory manually\n");
+        return -1;
+    }
 
     memset(&ov, 0, sizeof(ov));
     ov.hEvent = CreateEventW(NULL, FALSE, FALSE, NULL);
