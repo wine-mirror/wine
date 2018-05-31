a981edf0bc7c828e6b55fdc73b51a2f457043c4a
Zebediah Figura <zfigura@codeweavers.com>
2018-05-31 09:47:49 -0500

    dbghelp: Determine the wine loader name from the target process's architecture.
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dbghelp/dbghelp_private.h
+++ dlls/dbghelp/dbghelp_private.h
@@ -403,6 +403,8 @@ struct process
 
     unsigned                    buffer_size;
     void*                       buffer;
+
+    BOOL                        is_64bit;
 };
 
 struct line_info
@@ -609,7 +611,7 @@ extern void         module_reset_debug_info(struct module* module) DECLSPEC_HIDD
 extern BOOL         module_remove(struct process* pcs,
                                   struct module* module) DECLSPEC_HIDDEN;
 extern void         module_set_module(struct module* module, const WCHAR* name) DECLSPEC_HIDDEN;
-extern const WCHAR *get_wine_loader_name(void) DECLSPEC_HIDDEN;
+extern WCHAR *      get_wine_loader_name(struct process *pcs) DECLSPEC_HIDDEN;
 
 /* msc.c */
 extern BOOL         pe_load_debug_directory(const struct process* pcs,
