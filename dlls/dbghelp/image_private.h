bb53152faab705d972de3763ddd9bb3351e92460
Zebediah Figura <zfigura@codeweavers.com>
2018-06-04 16:51:31 -0500

    dbghelp: Read the Elf_Dyn struct corresponding to the target's architecture.
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dbghelp/image_private.h
+++ dlls/dbghelp/image_private.h
@@ -59,11 +59,9 @@ typedef struct section              macho_section;
 #ifdef __ELF__
 
 #ifdef _WIN64
-#define         Elf_Dyn         Elf64_Dyn
 #define         Elf_Sym         Elf64_Sym
 #define         Elf_auxv_t      Elf64_auxv_t
 #else
-#define         Elf_Dyn         Elf32_Dyn
 #define         Elf_Sym         Elf32_Sym
 #define         Elf_auxv_t      Elf32_auxv_t
 #endif
