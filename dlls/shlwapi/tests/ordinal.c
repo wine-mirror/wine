75236a636bb7497bb282e3558aef8bf1d5199640
Marcus Meissner <meissner@suse.de>
2018-06-26 16:12:35 +0200

    shlwapi: SHGetIniStringW buffer size is in characters.
    Signed-off-by: Marcus Meissner <meissner@suse.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/shlwapi/tests/ordinal.c
+++ dlls/shlwapi/tests/ordinal.c
@@ -2724,8 +2724,8 @@ static void test_SHGetIniString(void)
     if(0){
         /* these crash on Windows */
         pSHGetIniStringW(NULL, NULL, NULL, 0, NULL);
-        pSHGetIniStringW(NULL, AKeyW, out, sizeof(out), pathW);
-        pSHGetIniStringW(TestAppW, AKeyW, NULL, sizeof(out), pathW);
+        pSHGetIniStringW(NULL, AKeyW, out, ARRAY_SIZE(out), pathW);
+        pSHGetIniStringW(TestAppW, AKeyW, NULL, ARRAY_SIZE(out), pathW);
     }
 
     ret = pSHGetIniStringW(TestAppW, AKeyW, out, 0, pathW);
@@ -2734,21 +2734,21 @@ static void test_SHGetIniString(void)
     /* valid arguments */
     out[0] = 0;
     SetLastError(0xdeadbeef);
-    ret = pSHGetIniStringW(TestAppW, NULL, out, sizeof(out), pathW);
+    ret = pSHGetIniStringW(TestAppW, NULL, out, ARRAY_SIZE(out), pathW);
     ok(ret == 4, "SHGetIniStringW should have given 4, instead: %d\n", ret);
     ok(!lstrcmpW(out, AKeyW), "Expected %s, got: %s, %d\n",
                 wine_dbgstr_w(AKeyW), wine_dbgstr_w(out), GetLastError());
 
-    ret = pSHGetIniStringW(TestAppW, AKeyW, out, sizeof(out), pathW);
+    ret = pSHGetIniStringW(TestAppW, AKeyW, out, ARRAY_SIZE(out), pathW);
     ok(ret == 1, "SHGetIniStringW should have given 1, instead: %d\n", ret);
     ok(!strcmp_wa(out, "1"), "Expected L\"1\", got: %s\n", wine_dbgstr_w(out));
 
-    ret = pSHGetIniStringW(TestAppW, AnotherKeyW, out, sizeof(out), pathW);
+    ret = pSHGetIniStringW(TestAppW, AnotherKeyW, out, ARRAY_SIZE(out), pathW);
     ok(ret == 4, "SHGetIniStringW should have given 4, instead: %d\n", ret);
     ok(!strcmp_wa(out, "asdf"), "Expected L\"asdf\", got: %s\n", wine_dbgstr_w(out));
 
     out[0] = 1;
-    ret = pSHGetIniStringW(TestAppW, JunkKeyW, out, sizeof(out), pathW);
+    ret = pSHGetIniStringW(TestAppW, JunkKeyW, out, ARRAY_SIZE(out), pathW);
     ok(ret == 0, "SHGetIniStringW should have given 0, instead: %d\n", ret);
     ok(*out == 0, "Expected L\"\", got: %s\n", wine_dbgstr_w(out));
 
