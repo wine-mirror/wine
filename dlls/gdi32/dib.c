478a3d64ef7630f16e6c618375a76ad665c7cf63
Alex Henrie <alexhenrie24@gmail.com>
2018-07-05 08:39:25 +0200

    gdi32: Add DECLSPEC_HOTPATCH to GetDIBits.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=45199
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/gdi32/dib.c
+++ dlls/gdi32/dib.c
@@ -1208,7 +1208,7 @@ BITMAPINFO *copy_packed_dib( const BITMAPINFO *src_info, UINT usage )
  *    Success: Number of scan lines copied from bitmap
  *    Failure: 0
  */
-INT WINAPI GetDIBits(
+INT WINAPI DECLSPEC_HOTPATCH GetDIBits(
     HDC hdc,         /* [in]  Handle to device context */
     HBITMAP hbitmap, /* [in]  Handle to bitmap */
     UINT startscan,  /* [in]  First scan line to set in dest bitmap */
