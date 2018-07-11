c13f6df6a303a86ea4314d54d0715907104afd22
Piotr Caban <piotr@codeweavers.com>
2018-07-11 17:46:33 +0200

    inetcpl.cpl: Add support for setting automatic proxy configuration.
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/inetcpl.cpl/connections.c
+++ dlls/inetcpl.cpl/connections.c
@@ -31,6 +31,7 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(inetcpl);
 
+static const WCHAR auto_config_url[] = {'A','u','t','o','C','o','n','f','i','g','U','R','L',0};
 static const WCHAR internet_settings[] = {'S','o','f','t','w','a','r','e','\\',
     'M','i','c','r','o','s','o','f','t','\\','W','i','n','d','o','w','s','\\',
     'C','u','r','r','e','n','t','V','e','r','s','i','o','n','\\',
@@ -44,7 +45,9 @@ static const WCHAR default_connection_settings[] = {'D','e','f','a','u','l','t',
 static BOOL initdialog_done;
 
 #define CONNECTION_SETTINGS_VERSION 0x46
-#define MANUAL_PROXY 0x2
+#define CONNECTION_SETTINGS_MANUAL_PROXY 0x2
+#define CONNECTION_SETTINGS_PAC_SCRIPT 0x4
+#define CONNECTION_SETTINGS_WPAD 0x8
 
 typedef struct {
     DWORD version;
@@ -61,10 +64,11 @@ typedef struct {
 } connection_settings;
 
 static DWORD create_connection_settings(BOOL manual_proxy, const WCHAR *proxy_server,
-        connection_settings **ret)
+        BOOL use_wpad, BOOL use_pac_script, const WCHAR *pac_url, connection_settings **ret)
 {
     DWORD size = FIELD_OFFSET(connection_settings, data), pos;
     DWORD proxy_server_len;
+    DWORD pac_url_len;
 
     size += sizeof(DWORD);
     if(proxy_server)
@@ -77,6 +81,15 @@ static DWORD create_connection_settings(BOOL manual_proxy, const WCHAR *proxy_se
     else
         proxy_server_len = 0;
     size += proxy_server_len;
+    if(pac_url)
+    {
+        pac_url_len = WideCharToMultiByte(CP_UTF8, 0, pac_url, -1,
+                NULL, 0, NULL, NULL);
+        if(!pac_url_len) return 0;
+        pac_url_len--;
+    }
+    else
+        pac_url_len = 0;
     size += sizeof(DWORD)*10;
 
     *ret = heap_alloc_zero(size);
@@ -84,7 +97,9 @@ static DWORD create_connection_settings(BOOL manual_proxy, const WCHAR *proxy_se
 
     (*ret)->version = CONNECTION_SETTINGS_VERSION;
     (*ret)->flags = 1;
-    if(manual_proxy) (*ret)->flags |= MANUAL_PROXY;
+    if(manual_proxy) (*ret)->flags |= CONNECTION_SETTINGS_MANUAL_PROXY;
+    if(use_pac_script) (*ret)->flags |= CONNECTION_SETTINGS_PAC_SCRIPT;
+    if(use_wpad) (*ret)->flags |= CONNECTION_SETTINGS_WPAD;
     ((DWORD*)(*ret)->data)[0] = proxy_server_len;
     pos = sizeof(DWORD);
     if(proxy_server_len)
@@ -93,6 +108,15 @@ static DWORD create_connection_settings(BOOL manual_proxy, const WCHAR *proxy_se
                 (char*)(*ret)->data+pos, proxy_server_len, NULL, NULL);
         pos += proxy_server_len;
     }
+    pos += sizeof(DWORD); /* skip proxy bypass list */
+    ((DWORD*)((*ret)->data+pos))[0] = pac_url_len;
+    pos += sizeof(DWORD);
+    if(pac_url_len)
+    {
+        WideCharToMultiByte(CP_UTF8, 0, pac_url, -1,
+                (char*)(*ret)->data+pos, pac_url_len, NULL, NULL);
+        pos += pac_url_len;
+    }
     return size;
 }
 
@@ -100,9 +124,12 @@ static void connections_on_initdialog(HWND hwnd)
 {
     DWORD type, size, enabled;
     WCHAR address[INTERNET_MAX_URL_LENGTH], *port;
-    HKEY hkey;
+    WCHAR pac_url[INTERNET_MAX_URL_LENGTH];
+    HKEY hkey, con;
     LONG res;
 
+    SendMessageW(GetDlgItem(hwnd, IDC_EDIT_PAC_SCRIPT),
+            EM_LIMITTEXT, INTERNET_MAX_URL_LENGTH, 0);
     SendMessageW(GetDlgItem(hwnd, IDC_EDIT_PROXY_SERVER),
             EM_LIMITTEXT, INTERNET_MAX_URL_LENGTH-10, 0);
     SendMessageW(GetDlgItem(hwnd, IDC_EDIT_PROXY_PORT), EM_LIMITTEXT, 8, 0);
@@ -119,10 +146,45 @@ static void connections_on_initdialog(HWND hwnd)
     res = RegQueryValueExW(hkey, proxy_server, NULL, &type, (BYTE*)address, &size);
     if(res || type != REG_SZ)
         address[0] = 0;
+    size = sizeof(pac_url);
+    res = RegQueryValueExW(hkey, auto_config_url, NULL, &type, (BYTE*)pac_url, &size);
+    if(res || type != REG_SZ)
+        pac_url[0] = 0;
+
+    res = RegOpenKeyW(hkey, connections, &con);
     RegCloseKey(hkey);
+    if(!res)
+    {
+        connection_settings *settings = NULL;
+        size = 0;
+
+        while((res = RegQueryValueExW(con, default_connection_settings, NULL, &type,
+                        (BYTE*)settings, &size)) == ERROR_MORE_DATA || !settings)
+        {
+            connection_settings *new_settings = heap_realloc(settings, size);
+            if(!new_settings)
+            {
+                RegCloseKey(con);
+                heap_free(settings);
+                return;
+            }
+            settings = new_settings;
+        }
+        RegCloseKey(con);
+
+        if(!res && type == REG_BINARY)
+        {
+            if(settings->version != CONNECTION_SETTINGS_VERSION)
+                FIXME("unexpected structure version (%x)\n", settings->version);
+            else if(settings->flags & CONNECTION_SETTINGS_WPAD)
+                CheckDlgButton(hwnd, IDC_USE_WPAD, BST_CHECKED);
+        }
+        heap_free(settings);
+    }
 
     TRACE("ProxyEnable = %x\n", enabled);
     TRACE("ProxyServer = %s\n", wine_dbgstr_w(address));
+    TRACE("AutoConfigURL = %s\n", wine_dbgstr_w(auto_config_url));
 
     if(enabled)
     {
@@ -141,6 +203,13 @@ static void connections_on_initdialog(HWND hwnd)
     if(port)
         SetDlgItemTextW(hwnd, IDC_EDIT_PROXY_PORT, port);
 
+    if(pac_url[0])
+    {
+        CheckDlgButton(hwnd, IDC_USE_PAC_SCRIPT, BST_CHECKED);
+        EnableWindow(GetDlgItem(hwnd, IDC_EDIT_PAC_SCRIPT), TRUE);
+        SetDlgItemTextW(hwnd, IDC_EDIT_PAC_SCRIPT, pac_url);
+    }
+
     return;
 }
 
@@ -150,11 +219,22 @@ static INT_PTR connections_on_command(HWND hwnd, WPARAM wparam)
 
     switch (wparam)
     {
+        case IDC_USE_PAC_SCRIPT:
+            checked = IsDlgButtonChecked(hwnd, IDC_USE_PAC_SCRIPT);
+            EnableWindow(GetDlgItem(hwnd, IDC_EDIT_PAC_SCRIPT), checked);
+            break;
         case IDC_USE_PROXY_SERVER:
             checked = IsDlgButtonChecked(hwnd, IDC_USE_PROXY_SERVER);
             EnableWindow(GetDlgItem(hwnd, IDC_EDIT_PROXY_SERVER), checked);
             EnableWindow(GetDlgItem(hwnd, IDC_EDIT_PROXY_PORT), checked);
-            /* fallthrough */
+    }
+
+    switch (wparam)
+    {
+        case IDC_USE_WPAD:
+        case IDC_USE_PAC_SCRIPT:
+        case IDC_USE_PROXY_SERVER:
+        case MAKEWPARAM(IDC_EDIT_PAC_SCRIPT, EN_CHANGE):
         case MAKEWPARAM(IDC_EDIT_PROXY_SERVER, EN_CHANGE):
         case MAKEWPARAM(IDC_EDIT_PROXY_PORT, EN_CHANGE):
             if(initdialog_done)
@@ -168,9 +248,11 @@ static INT_PTR connections_on_command(HWND hwnd, WPARAM wparam)
 static INT_PTR connections_on_notify(HWND hwnd, WPARAM wparam, LPARAM lparam)
 {
     connection_settings *default_connection;
-    WCHAR addr[INTERNET_MAX_URL_LENGTH];
+    WCHAR proxy[INTERNET_MAX_URL_LENGTH];
+    WCHAR pac_script[INTERNET_MAX_URL_LENGTH];
     PSHNOTIFY *psn = (PSHNOTIFY*)lparam;
-    DWORD addr_len, port_len, enabled, size;
+    DWORD proxy_len, port_len, pac_script_len;
+    DWORD use_proxy, use_pac_script, use_wpad, size;
     LRESULT res;
     HKEY hkey, con;
 
@@ -181,32 +263,32 @@ static INT_PTR connections_on_notify(HWND hwnd, WPARAM wparam, LPARAM lparam)
     if(res)
         return FALSE;
 
-    enabled = IsDlgButtonChecked(hwnd, IDC_USE_PROXY_SERVER);
+    use_proxy = IsDlgButtonChecked(hwnd, IDC_USE_PROXY_SERVER);
     res = RegSetValueExW(hkey, proxy_enable, 0, REG_DWORD,
-            (BYTE*)&enabled, sizeof(enabled));
+            (BYTE*)&use_proxy, sizeof(use_proxy));
     if(res)
     {
         RegCloseKey(hkey);
         return FALSE;
     }
-    TRACE("ProxyEnable set to %x\n", enabled);
+    TRACE("ProxyEnable set to %x\n", use_proxy);
 
-    addr_len = GetDlgItemTextW(hwnd, IDC_EDIT_PROXY_SERVER,
-            addr, sizeof(addr)/sizeof(addr[0]));
-    if(addr_len)
+    proxy_len = GetDlgItemTextW(hwnd, IDC_EDIT_PROXY_SERVER,
+            proxy, sizeof(proxy)/sizeof(proxy[0]));
+    if(proxy_len)
     {
-        addr[addr_len++] = ':';
+        proxy[proxy_len++] = ':';
         port_len = GetDlgItemTextW(hwnd, IDC_EDIT_PROXY_PORT,
-            addr+addr_len, sizeof(addr)/sizeof(addr[0])-addr_len);
+            proxy+proxy_len, sizeof(proxy)/sizeof(proxy[0])-proxy_len);
         if(!port_len)
         {
-            addr[addr_len++] = '8';
-            addr[addr_len++] = '0';
-            addr[addr_len] = 0;
+            proxy[proxy_len++] = '8';
+            proxy[proxy_len++] = '0';
+            proxy[proxy_len] = 0;
         }
 
         res = RegSetValueExW(hkey, proxy_server, 0, REG_SZ,
-                (BYTE*)addr, (addr_len+port_len)*sizeof(WCHAR));
+                (BYTE*)proxy, (proxy_len+port_len)*sizeof(WCHAR));
     }
     else
     {
@@ -219,14 +301,39 @@ static INT_PTR connections_on_notify(HWND hwnd, WPARAM wparam, LPARAM lparam)
         RegCloseKey(hkey);
         return FALSE;
     }
-    TRACE("ProxtServer set to %s\n", wine_dbgstr_w(addr));
+    TRACE("ProxtServer set to %s\n", wine_dbgstr_w(proxy));
+
+    use_pac_script = IsDlgButtonChecked(hwnd, IDC_USE_PAC_SCRIPT);
+    pac_script_len = GetDlgItemTextW(hwnd, IDC_EDIT_PAC_SCRIPT,
+            pac_script, sizeof(pac_script)/sizeof(pac_script[0]));
+    if(!pac_script_len) use_pac_script = FALSE;
+    if(use_pac_script)
+    {
+        res = RegSetValueExW(hkey, auto_config_url, 0, REG_SZ,
+                (BYTE*)pac_script, pac_script_len*sizeof(WCHAR));
+    }
+    else
+    {
+        res = RegDeleteValueW(hkey, auto_config_url);
+        if(res == ERROR_FILE_NOT_FOUND)
+            res = ERROR_SUCCESS;
+    }
+    if(res)
+    {
+        RegCloseKey(hkey);
+        return FALSE;
+    }
+    TRACE("AutoConfigURL set to %s\n", wine_dbgstr_w(use_pac_script ? pac_script : NULL));
+
+    use_wpad = IsDlgButtonChecked(hwnd, IDC_USE_WPAD);
 
     res = RegCreateKeyExW(hkey, connections, 0, NULL, 0, KEY_WRITE, NULL, &con, NULL);
     RegCloseKey(hkey);
     if(res)
         return FALSE;
 
-    size = create_connection_settings(enabled, addr, &default_connection);
+    size = create_connection_settings(use_proxy, proxy, use_wpad,
+        use_pac_script, pac_script, &default_connection);
     if(!size)
     {
         RegCloseKey(con);
