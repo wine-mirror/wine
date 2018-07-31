3ce304e60100962503bcded0adb69659f849d1b4
Zebediah Figura <z.figura12@gmail.com>
2018-07-31 11:17:34 -0500

    dbghelp: Use WOW64_CONTEXT instead of CONTEXT for i386 backend.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dbghelp/cpu_arm.c
+++ dlls/dbghelp/cpu_arm.c
@@ -63,13 +63,14 @@ enum st_mode {stm_start, stm_arm, stm_done};
  * modify (at least) context.Pc using unwind information
  * either out of debug info (dwarf), or simple Lr trace
  */
-static BOOL fetch_next_frame(struct cpu_stack_walk* csw,
-                               CONTEXT* context, DWORD_PTR curr_pc)
+static BOOL fetch_next_frame(struct cpu_stack_walk* csw, union ctx *pcontext,
+    DWORD_PTR curr_pc)
 {
     DWORD_PTR               xframe;
+    CONTEXT *context = &pcontext->ctx;
     DWORD                   oldReturn = context->Lr;
 
-    if (dwarf2_virtual_unwind(csw, curr_pc, context, &xframe))
+    if (dwarf2_virtual_unwind(csw, curr_pc, pcontext, &xframe))
     {
         context->Sp = xframe;
         context->Pc = oldReturn;
@@ -82,7 +83,8 @@ static BOOL fetch_next_frame(struct cpu_stack_walk* csw,
     return TRUE;
 }
 
-static BOOL arm_stack_walk(struct cpu_stack_walk* csw, LPSTACKFRAME64 frame, CONTEXT* context)
+static BOOL arm_stack_walk(struct cpu_stack_walk *csw, STACKFRAME64 *frame,
+    union ctx *context)
 {
     unsigned    deltapc = curr_count <= 1 ? 0 : 4;
 
@@ -109,8 +111,8 @@ static BOOL arm_stack_walk(struct cpu_stack_walk* csw, LPSTACKFRAME64 frame, CON
     }
     else
     {
-        if (context->Sp != frame->AddrStack.Offset) FIXME("inconsistent Stack Pointer\n");
-        if (context->Pc != frame->AddrPC.Offset) FIXME("inconsistent Program Counter\n");
+        if (context->ctx.Sp != frame->AddrStack.Offset) FIXME("inconsistent Stack Pointer\n");
+        if (context->ctx.Pc != frame->AddrPC.Offset) FIXME("inconsistent Program Counter\n");
 
         if (frame->AddrReturn.Offset == 0) goto done_err;
         if (!fetch_next_frame(csw, context, frame->AddrPC.Offset - deltapc))
@@ -120,10 +122,10 @@ static BOOL arm_stack_walk(struct cpu_stack_walk* csw, LPSTACKFRAME64 frame, CON
     memset(&frame->Params, 0, sizeof(frame->Params));
 
     /* set frame information */
-    frame->AddrStack.Offset = context->Sp;
-    frame->AddrReturn.Offset = context->Lr;
-    frame->AddrFrame.Offset = context->R11;
-    frame->AddrPC.Offset = context->Pc;
+    frame->AddrStack.Offset = context->ctx.Sp;
+    frame->AddrReturn.Offset = context->ctx.Lr;
+    frame->AddrFrame.Offset = context->ctx.R11;
+    frame->AddrPC.Offset = context->ctx.Pc;
 
     frame->Far = TRUE;
     frame->Virtual = TRUE;
@@ -144,7 +146,8 @@ done_err:
     return FALSE;
 }
 #else
-static BOOL arm_stack_walk(struct cpu_stack_walk* csw, LPSTACKFRAME64 frame, CONTEXT* context)
+static BOOL arm_stack_walk(struct cpu_stack_walk *csw, STACKFRAME64 *frame,
+    union ctx *context)
 {
     return FALSE;
 }
@@ -159,9 +162,11 @@ static unsigned arm_map_dwarf_register(unsigned regno, BOOL eh_frame)
     return CV_ARM_NOREG;
 }
 
-static void* arm_fetch_context_reg(CONTEXT* ctx, unsigned regno, unsigned* size)
+static void *arm_fetch_context_reg(union ctx *pctx, unsigned regno, unsigned *size)
 {
 #ifdef __arm__
+    CONTEXT *ctx = &pctx->ctx;
+
     switch (regno)
     {
     case CV_ARM_R0 +  0: *size = sizeof(ctx->R0); return &ctx->R0;
--- dlls/dbghelp/dbghelp_private.h
+++ dlls/dbghelp/dbghelp_private.h
@@ -511,6 +511,12 @@ struct dump_context
     MINIDUMP_CALLBACK_INFORMATION*      cb;
 };
 
+union ctx
+{
+    CONTEXT ctx;
+    WOW64_CONTEXT x86;
+};
+
 enum cpu_addr {cpu_addr_pc, cpu_addr_stack, cpu_addr_frame};
 struct cpu
 {
@@ -523,7 +529,8 @@ struct cpu
                             enum cpu_addr, ADDRESS64* addr);
 
     /* stack manipulation */
-    BOOL        (*stack_walk)(struct cpu_stack_walk* csw, LPSTACKFRAME64 frame, CONTEXT* context);
+    BOOL        (*stack_walk)(struct cpu_stack_walk *csw, STACKFRAME64 *frame,
+                              union ctx *ctx);
 
     /* module manipulation */
     void*       (*find_runtime_function)(struct module*, DWORD64 addr);
@@ -532,7 +539,7 @@ struct cpu
     unsigned    (*map_dwarf_register)(unsigned regno, BOOL eh_frame);
 
     /* context related manipulation */
-    void*       (*fetch_context_reg)(CONTEXT* context, unsigned regno, unsigned* size);
+    void *      (*fetch_context_reg)(union ctx *ctx, unsigned regno, unsigned *size);
     const char* (*fetch_regname)(unsigned regno);
 
     /* minidump per CPU extension */
@@ -645,8 +652,8 @@ struct pdb_cmd_pair {
     const char*         name;
     DWORD*              pvalue;
 };
-extern BOOL         pdb_virtual_unwind(struct cpu_stack_walk* csw, DWORD_PTR ip,
-                                       CONTEXT* context, struct pdb_cmd_pair* cpair) DECLSPEC_HIDDEN;
+extern BOOL pdb_virtual_unwind(struct cpu_stack_walk *csw, DWORD_PTR ip,
+    union ctx *context, struct pdb_cmd_pair *cpair) DECLSPEC_HIDDEN;
 
 /* path.c */
 extern BOOL         path_find_symbol_file(const struct process* pcs, PCSTR full_path,
@@ -684,8 +691,8 @@ extern BOOL         stabs_parse(struct module* module, unsigned long load_offset
 extern BOOL         dwarf2_parse(struct module* module, unsigned long load_offset,
                                  const struct elf_thunk_area* thunks,
                                  struct image_file_map* fmap) DECLSPEC_HIDDEN;
-extern BOOL         dwarf2_virtual_unwind(struct cpu_stack_walk* csw, DWORD_PTR ip,
-                                          CONTEXT* context, ULONG_PTR* cfa) DECLSPEC_HIDDEN;
+extern BOOL dwarf2_virtual_unwind(struct cpu_stack_walk *csw, DWORD_PTR ip,
+    union ctx *ctx, ULONG_PTR *cfa) DECLSPEC_HIDDEN;
 
 /* stack.c */
 extern BOOL         sw_read_mem(struct cpu_stack_walk* csw, DWORD64 addr, void* ptr, DWORD sz) DECLSPEC_HIDDEN;
