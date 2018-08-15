91da7132e929d24e14c9cc5e4279b36699debf1f
Kai Krakow <kai@kaishome.de>
2018-08-15 20:29:05 +0200

    winebus.sys: Do not print HID report read errors.
    Device reports may come in faster than our consumers could possibly read
    them, this is especially true for multi-axis events: When you move a
    stick across its range, it will always generate at least two events, one
    for the x axis, and one for the y axis. This is not really an error
    situation, so let's just discard the error message.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=43125
    Signed-off-by: Kai Krakow <kai@kaishome.de>
    Signed-off-by: Aric Stewart <aric@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit d744f367d263a131feee96e103fb8220e8400b53)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/winebus.sys/main.c
+++ dlls/winebus.sys/main.c
@@ -603,9 +603,6 @@ void process_hid_report(DEVICE_OBJECT *device, BYTE *report, DWORD length)
             ext->buffer_size = length;
     }
 
-    if (!ext->last_report_read)
-        ERR_(hid_report)("Device reports coming in too fast, last report not read yet!\n");
-
     memcpy(ext->last_report, report, length);
     ext->last_report_size = length;
     ext->last_report_read = FALSE;
