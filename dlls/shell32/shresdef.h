79d90550ec4ae58e60de22812d54be24f0ae1d30
Alex Henrie <alexhenrie24@gmail.com>
2018-08-14 22:09:58 -0600

    shell32: Add window icon.
    Adapted from new-window.svg in the Tango Icon Library.
    
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 5ee4e0027c941aa74da00166aa5f07bfd292b1b3)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/shell32/shresdef.h
+++ dlls/shell32/shresdef.h
@@ -167,6 +167,7 @@
 
 #define IDI_SHELL_FILE               1
 #define IDI_SHELL_DOCUMENT           2
+#define IDI_SHELL_WINDOW             3
 #define IDI_SHELL_FOLDER             4
 #define IDI_SHELL_FOLDER_OPEN        5
 #define IDI_SHELL_5_12_FLOPPY        6
