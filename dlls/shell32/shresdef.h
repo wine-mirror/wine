2e1af12407db78c5554314430a2d781f24d8f483
Alex Henrie <alexhenrie24@gmail.com>
2018-08-29 23:37:53 -0600

    shell32: Add print icon.
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 2e703dbc7a70f123cb4987648fc38d8d22c5b892)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/shell32/shresdef.h
+++ dlls/shell32/shresdef.h
@@ -308,6 +308,7 @@
 #define IDI_SHELL_RENAME           242
 #define IDI_SHELL_COPY             243
 #define IDI_SHELL_UPLOAD           244
+#define IDI_SHELL_PRINT            245
 #define IDI_SHELL_EMPTY_RECYCLE_BIN2       254
 #define IDI_SHELL_NEW_NETWORK_FOLDER       258
 #define IDI_SHELL_WRITE_DISC       260
