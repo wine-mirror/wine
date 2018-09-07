a71e91f28eb884d7e5bda7b768cd6c6cd8652f89
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2018-09-07 00:58:41 +0000

    mfplat: Implement MFCreateMemoryBuffer.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=45715
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit e379bfd34edcaa889cdd911f1dc61e93762343e5)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/mfplat/main.c
+++ dlls/mfplat/main.c
@@ -2323,3 +2323,179 @@ HRESULT WINAPI MFCreateStreamDescriptor(DWORD identifier, DWORD count,
     *descriptor = &object->IMFStreamDescriptor_iface;
     return S_OK;
 }
+
+
+typedef struct _mfbuffer
+{
+    IMFMediaBuffer IMFMediaBuffer_iface;
+    LONG ref;
+
+    BYTE *buffer;
+    DWORD max_length;
+    DWORD current;
+} mfbuffer;
+
+static inline mfbuffer *impl_from_IMFMediaBuffer(IMFMediaBuffer *iface)
+{
+    return CONTAINING_RECORD(iface, mfbuffer, IMFMediaBuffer_iface);
+}
+
+static HRESULT WINAPI mfbuffer_QueryInterface(IMFMediaBuffer *iface, REFIID riid, void **out)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+
+    TRACE("(%p)->(%s %p)\n", This, debugstr_guid(riid), out);
+
+    if(IsEqualGUID(riid, &IID_IUnknown) ||
+       IsEqualGUID(riid, &IID_IMFMediaBuffer))
+    {
+        *out = &This->IMFMediaBuffer_iface;
+    }
+    else
+    {
+        FIXME("(%s, %p)\n", debugstr_guid(riid), out);
+        *out = NULL;
+        return E_NOINTERFACE;
+    }
+
+    IUnknown_AddRef((IUnknown*)*out);
+    return S_OK;
+}
+
+static ULONG WINAPI mfbuffer_AddRef(IMFMediaBuffer *iface)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+    ULONG ref = InterlockedIncrement(&This->ref);
+
+    TRACE("(%p) ref=%u\n", This, ref);
+
+    return ref;
+}
+
+static ULONG WINAPI mfbuffer_Release(IMFMediaBuffer *iface)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+    ULONG ref = InterlockedDecrement(&This->ref);
+
+    TRACE("(%p) ref=%u\n", This, ref);
+
+    if (!ref)
+    {
+        heap_free(This->buffer);
+        heap_free(This);
+    }
+
+    return ref;
+}
+
+static HRESULT WINAPI mfbuffer_Lock(IMFMediaBuffer *iface, BYTE **buffer, DWORD *max, DWORD *current)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+
+    TRACE("%p, %p %p, %p\n", This, buffer, max, current);
+
+    if(!buffer)
+        return E_INVALIDARG;
+
+    *buffer = This->buffer;
+    if(max)
+        *max = This->max_length;
+    if(current)
+        *current = This->current;
+
+    return S_OK;
+}
+
+static HRESULT WINAPI mfbuffer_Unlock(IMFMediaBuffer *iface)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+
+    TRACE("%p\n", This);
+
+    return S_OK;
+}
+
+static HRESULT WINAPI mfbuffer_GetCurrentLength(IMFMediaBuffer *iface, DWORD *current)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+
+    TRACE("%p\n", This);
+
+    if(!current)
+        return E_INVALIDARG;
+
+    *current = This->current;
+
+    return S_OK;
+}
+
+static HRESULT WINAPI mfbuffer_SetCurrentLength(IMFMediaBuffer *iface, DWORD current)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+
+    TRACE("%p, %u\n", This, current);
+
+    if(current > This->max_length)
+        return E_INVALIDARG;
+
+    This->current = current;
+
+    return S_OK;
+}
+
+static HRESULT WINAPI mfbuffer_GetMaxLength(IMFMediaBuffer *iface, DWORD *max)
+{
+    mfbuffer *This = impl_from_IMFMediaBuffer(iface);
+
+    TRACE("%p, %p\n", This, max);
+
+    if(!max)
+        return E_INVALIDARG;
+
+    *max = This->max_length;
+
+    return S_OK;
+}
+
+static const IMFMediaBufferVtbl mfbuffer_vtbl =
+{
+    mfbuffer_QueryInterface,
+    mfbuffer_AddRef,
+    mfbuffer_Release,
+    mfbuffer_Lock,
+    mfbuffer_Unlock,
+    mfbuffer_GetCurrentLength,
+    mfbuffer_SetCurrentLength,
+    mfbuffer_GetMaxLength
+};
+
+HRESULT WINAPI MFCreateMemoryBuffer(DWORD max_length, IMFMediaBuffer **buffer)
+{
+    mfbuffer *object;
+    BYTE *bytes;
+
+    TRACE("%u, %p\n", max_length, buffer);
+
+    if(!buffer)
+        return E_INVALIDARG;
+
+    object = heap_alloc( sizeof(*object) );
+    if(!object)
+        return E_OUTOFMEMORY;
+
+    bytes = heap_alloc( max_length );
+    if(!bytes)
+    {
+        heap_free(object);
+        return E_OUTOFMEMORY;
+    }
+
+    object->ref = 1;
+    object->max_length = max_length;
+    object->current = 0;
+    object->buffer = bytes;
+    object->IMFMediaBuffer_iface.lpVtbl = &mfbuffer_vtbl;
+    *buffer = &object->IMFMediaBuffer_iface;
+
+    return S_OK;
+}
--- include/mfapi.h
+++ include/mfapi.h
@@ -67,6 +67,7 @@ HRESULT WINAPI MFCancelWorkItem(MFWORKITEM_KEY key);
 HRESULT WINAPI MFCreateAttributes(IMFAttributes **attributes, UINT32 size);
 HRESULT WINAPI MFCreateEventQueue(IMFMediaEventQueue **queue);
 HRESULT WINAPI MFCreateMediaType(IMFMediaType **type);
+HRESULT WINAPI MFCreateMemoryBuffer(DWORD max_length, IMFMediaBuffer **buffer);
 HRESULT WINAPI MFGetTimerPeriodicity(DWORD *periodicity);
 HRESULT WINAPI MFTEnum(GUID category, UINT32 flags, MFT_REGISTER_TYPE_INFO *input_type,
                        MFT_REGISTER_TYPE_INFO *output_type, IMFAttributes *attributes,
