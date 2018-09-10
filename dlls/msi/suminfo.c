wine-mirror/wine

commit bf5589311de8ac2c74e3bd73bef32337a2ce8df2

msi: Make MsiGetSummaryInformation() RPC-compatible.
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Hans Leidekker <hans@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/msi/suminfo.c
@@ -523,23 +523,16 @@ UINT WINAPI MsiGetSummaryInformationW( MSIHANDLE hDatabase,
         db = msihandle2msiinfo( hDatabase, MSIHANDLETYPE_DATABASE );
         if( !db )
         {
-            MSIHANDLE remote;
-            HRESULT hr;
+            MSIHANDLE remote, remote_suminfo;
 
             if (!(remote = msi_get_remote(hDatabase)))
                 return ERROR_INVALID_HANDLE;
 
-            hr = remote_DatabaseGetSummaryInformation(remote, uiUpdateCount, pHandle);
+            ret = remote_DatabaseGetSummaryInformation(remote, uiUpdateCount, &remote_suminfo);
+            if (!ret)
+                *pHandle = alloc_msi_remote_handle(remote_suminfo);
 
-            if (FAILED(hr))
-            {
-                if (HRESULT_FACILITY(hr) == FACILITY_WIN32)
-                    return HRESULT_CODE(hr);
-
-                return ERROR_FUNCTION_FAILED;
-            }
-
-            return ERROR_SUCCESS;
+            return ret;
         }
     }
 

dlls/msi/winemsi.idl
@@ -67,7 +67,7 @@ interface IWineMsiRemote
 
     MSICONDITION remote_DatabaseIsTablePersistent( [in] MSIHANDLE db, [in] LPCWSTR table );
     UINT remote_DatabaseGetPrimaryKeys( [in] MSIHANDLE db, [in, string] LPCWSTR table, [out] struct wire_record **keys );
-    HRESULT remote_DatabaseGetSummaryInformation( [in] MSIHANDLE db, [in] UINT updatecount, [out] MSIHANDLE *suminfo );
+    UINT remote_DatabaseGetSummaryInformation( [in] MSIHANDLE db, [in] UINT updatecount, [out] MSIHANDLE *suminfo );
     UINT remote_DatabaseOpenView( [in] MSIHANDLE db, [in, string] LPCWSTR query, [out] MSIHANDLE *view );
 
     MSIHANDLE remote_GetActiveDatabase( [in] MSIHANDLE hinst );

dlls/msi/tests/custom.c
@@ -243,7 +243,7 @@ static void test_props(MSIHANDLE hinst)
 
 static void test_db(MSIHANDLE hinst)
 {
-    MSIHANDLE hdb, view, rec, rec2;
+    MSIHANDLE hdb, view, rec, rec2, suminfo;
     char buffer[10];
     DWORD sz;
     UINT r;
@@ -411,6 +411,15 @@ static void test_db(MSIHANDLE hinst)
     ok(hinst, !strcmp(buffer, "Name"), "got '%s'\n", buffer);
 
     r = MsiCloseHandle(rec);
+    ok(hinst, !r, "got %u\n", r);
+
+    r = MsiGetSummaryInformationA(hdb, NULL, 1, NULL);
+    ok(hinst, r == ERROR_INVALID_PARAMETER, "got %u\n", r);
+
+    r = MsiGetSummaryInformationA(hdb, NULL, 1, &suminfo);
+    ok(hinst, !r, "got %u\n", r);
+
+    r = MsiCloseHandle(suminfo);
     ok(hinst, !r, "got %u\n", r);
 
     r = MsiCloseHandle(hdb);

dlls/msi/database.c
@@ -1922,10 +1922,9 @@ UINT __cdecl remote_DatabaseGetPrimaryKeys(MSIHANDLE db, LPCWSTR table, struct w
     return r;
 }
 
-HRESULT __cdecl remote_DatabaseGetSummaryInformation(MSIHANDLE db, UINT updatecount, MSIHANDLE *suminfo)
+UINT __cdecl remote_DatabaseGetSummaryInformation(MSIHANDLE db, UINT updatecount, MSIHANDLE *suminfo)
 {
-    UINT r = MsiGetSummaryInformationW(db, NULL, updatecount, suminfo);
-    return HRESULT_FROM_WIN32(r);
+    return MsiGetSummaryInformationW(db, NULL, updatecount, suminfo);
 }
 
 UINT __cdecl remote_DatabaseOpenView(MSIHANDLE db, LPCWSTR query, MSIHANDLE *view)

commit ce2ebd72750ca85e7307372de1a73831ad9e73a8

msi: Handle the remote case in MsiSummaryInfoGetProperty.
This patch fixes the regression introduced by bf5589311de8ac2c74e3bd73bef32337a2ce8df2.

Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
Signed-off-by: Hans Leidekker <hans@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/msi/suminfo.c
@@ -741,7 +741,29 @@ UINT WINAPI MsiSummaryInfoGetPropertyA(
     }
 
     if (!(si = msihandle2msiinfo( handle, MSIHANDLETYPE_SUMMARYINFO )))
-        return ERROR_INVALID_HANDLE;
+    {
+        MSIHANDLE remote;
+        WCHAR *buf = NULL;
+
+        if (!(remote = msi_get_remote( handle )))
+            return ERROR_INVALID_HANDLE;
+
+        __TRY
+        {
+            r = remote_SummaryInfoGetProperty( remote, uiProperty, puiDataType, piValue, pftValue, &buf );
+        }
+        __EXCEPT(rpc_filter)
+        {
+            r = GetExceptionCode();
+        }
+        __ENDTRY
+
+        if (!r)
+            r = msi_strncpyWtoA( buf, -1, szValueBuf, pcchValueBuf, TRUE );
+
+        midl_user_free( buf );
+        return r;
+    }
 
     str.unicode = FALSE;
     str.str.a = szValueBuf;
@@ -769,7 +791,29 @@ UINT WINAPI MsiSummaryInfoGetPropertyW(
     }
 
     if (!(si = msihandle2msiinfo( handle, MSIHANDLETYPE_SUMMARYINFO )))
-        return ERROR_INVALID_HANDLE;
+    {
+        MSIHANDLE remote;
+        WCHAR *buf = NULL;
+
+        if (!(remote = msi_get_remote( handle )))
+            return ERROR_INVALID_HANDLE;
+
+        __TRY
+        {
+            r = remote_SummaryInfoGetProperty( remote, uiProperty, puiDataType, piValue, pftValue, &buf );
+        }
+        __EXCEPT(rpc_filter)
+        {
+            r = GetExceptionCode();
+        }
+        __ENDTRY
+
+        if (!r)
+            r = msi_strncpyW( buf, -1, szValueBuf, pcchValueBuf );
+
+        midl_user_free( buf );
+        return r;
+    }
 
     str.unicode = TRUE;
     str.str.w = szValueBuf;
@@ -1149,4 +1193,22 @@ UINT msi_load_suminfo_properties( MSIPACKAGE *package )
 UINT __cdecl s_remote_SummaryInfoGetPropertyCount( MSIHANDLE suminfo, UINT *count )
 {
     return MsiSummaryInfoGetPropertyCount( suminfo, count );
+}
+
+UINT __cdecl s_remote_SummaryInfoGetProperty( MSIHANDLE suminfo, UINT property, UINT *type,
+                                              INT *value, FILETIME *ft, LPWSTR *buf )
+{
+    WCHAR empty[1];
+    DWORD size = 0;
+    UINT r;
+
+    r = MsiSummaryInfoGetPropertyW( suminfo, property, type, value, ft, empty, &size );
+    if (r == ERROR_MORE_DATA)
+    {
+        size++;
+        *buf = midl_user_allocate( size * sizeof(WCHAR) );
+        if (!*buf) return ERROR_OUTOFMEMORY;
+        r = MsiSummaryInfoGetPropertyW( suminfo, property, type, value, ft, *buf, &size );
+    }
+    return r;
 }

dlls/msi/winemsi.idl
@@ -75,6 +75,8 @@ interface IWineMsiRemote
     UINT remote_DatabaseOpenView( [in] MSIHANDLE db, [in, string] LPCWSTR query, [out] MSIHANDLE *view );
 
     UINT remote_SummaryInfoGetPropertyCount( [in] MSIHANDLE suminfo, [out] UINT *count );
+    UINT remote_SummaryInfoGetProperty( [in] MSIHANDLE suminfo, [in] UINT property, [out] UINT *type, [out] INT *value,
+                                        [out] FILETIME *ft, [out, string] LPWSTR *buf );
 
     MSIHANDLE remote_GetActiveDatabase( [in] MSIHANDLE hinst );
     UINT remote_GetProperty( [in] MSIHANDLE hinst, [in, string] LPCWSTR property, [out, string] LPWSTR *value, [out] DWORD *size );

dlls/msi/tests/custom.c
@@ -464,7 +464,6 @@ static void test_db(MSIHANDLE hinst)
     ok(hinst, count == 5, "got %u\n", count);
 
     r = MsiSummaryInfoGetPropertyA(suminfo, 0, NULL, NULL, NULL, NULL, NULL);
-todo_wine
     ok(hinst, r == RPC_X_NULL_REF_POINTER, "got %u\n", r);
 
     type = 0xdeadbeef;
@@ -472,11 +471,8 @@ todo_wine
     strcpy(buffer, "deadbeef");
     sz = sizeof(buffer);
     r = MsiSummaryInfoGetPropertyA(suminfo, PID_AUTHOR, &type, &int_value, &ft, buffer, &sz);
-todo_wine
     ok(hinst, !r, "got %u\n", r);
-todo_wine
     ok(hinst, type == 0, "got %u\n", type);
-todo_wine
     ok(hinst, int_value == 0, "got %u\n", int_value);
     ok(hinst, sz == sizeof(buffer), "got %u\n", sz);
     ok(hinst, !lstrcmpA(buffer, "deadbeef"), "got %s\n", buffer);
@@ -486,11 +482,8 @@ todo_wine
     strcpy(buffer, "deadbeef");
     sz = sizeof(buffer);
     r = MsiSummaryInfoGetPropertyA(suminfo, PID_CODEPAGE, &type, &int_value, &ft, buffer, &sz);
-todo_wine
     ok(hinst, !r, "got %u\n", r);
-todo_wine
     ok(hinst, type == 0, "got %u\n", type);
-todo_wine
     ok(hinst, int_value == 0, "got %u\n", int_value);
     ok(hinst, sz == sizeof(buffer), "got %u\n", sz);
     ok(hinst, !lstrcmpA(buffer, "deadbeef"), "got %s\n", buffer);