99fdd9e61c4a68660bdc67b292783476da68d082
Francois Gouget <fgouget@free.fr>
2018-09-14 16:47:32 +0200

    cmd: A spelling fix in a comment.
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/cmd/builtins.c
+++ programs/cmd/builtins.c
@@ -1628,7 +1628,7 @@ static void WCMD_part_execute(CMD_LIST **cmdList, const WCHAR *firstcmd,
           }
           if (curPosition == *cmdList) *cmdList = (*cmdList)->nextcommand;
 
-        /* If we were in an IF statement and we didnt find an else and yet we get back to
+        /* If we were in an IF statement and we didn't find an else and yet we get back to
            the same bracket depth as the IF, then the IF statement is over. This is required
            to handle nested ifs properly                                                     */
         } else if (isIF && (*cmdList)->bracketDepth == myDepth) {
