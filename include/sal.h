7fb58748e6789787e7579f8f75f3cb5337a87e8c
Stefan Dösinger <stefan@codeweavers.com>
2018-10-05 13:43:38 +0100

    include: Add check macros needed for VS2017.
    Signed-off-by: Stefan Dösinger <stefan@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/sal.h
+++ include/sal.h
@@ -19,6 +19,11 @@
 #ifndef __WINE_SAL_H__
 #define __WINE_SAL_H__
 
+
+#define _Always_(exp)
+
+#define _At_buffer_(a, b, c, d)
+
 #define _Check_return_
 
 #define _Deref_out_
@@ -26,6 +31,11 @@
 #define _Deref_out_opt_z_
 #define _Deref_post_opt_cap_(count)
 #define _Deref_post_opt_valid_
+#define _Deref_post_valid_
+#define _Deref_post_z_
+#define _Deref_prepost_opt_z_
+
+#define _Field_range_(min, max)
 
 #define _In_
 #define _In_bytecount_(count)
@@ -33,15 +43,23 @@
 #define _In_opt_
 #define _In_opt_count_(count)
 #define _In_opt_z_
+#define _In_range_(min, max)
 #define _In_reads_(count)
 #define _In_reads_bytes_(count)
+#define _In_reads_bytes_opt_(count)
 #define _In_reads_opt_z_(count)
+#define _In_reads_or_z_(count)
+#define _In_reads_or_z_opt_(count)
 #define _In_reads_z_(count)
 #define _In_z_
 
 #define _Inout_
 #define _Inout_cap_(count)
 #define _Inout_opt_
+#define _Inout_opt_z_
+#define _Inout_updates_(count)
+#define _Inout_updates_bytes_(count)
+#define _Inout_updates_opt_(count)
 #define _Inout_updates_z_(count)
 #define _Inout_z_
 #define _Inout_z_bytecap_(count)
@@ -60,35 +78,61 @@
 #define _Out_opt_z_cap_(count)
 #define _Out_opt_z_cap_post_count_(count1, count2)
 #define _Out_writes_(count)
+#define _Out_writes_bytes_(count)
+#define _Out_writes_bytes_all_(count)
+#define _Out_writes_bytes_all_opt_(count)
+#define _Out_writes_bytes_opt_(count)
 #define _Out_writes_bytes_to_(count1, count2)
+#define _Out_writes_bytes_to_opt_(count1, count2)
 #define _Out_writes_opt_(count)
 #define _Out_writes_opt_z_(count)
 #define _Out_writes_to_(count1, count2)
+#define _Out_writes_to_opt_(count1, count2)
 #define _Out_writes_z_(count)
 #define _Out_z_cap_(count)
 #define _Out_z_cap_c_(count)
 #define _Out_z_cap_post_count_(count1, count2)
 
 #define _Outptr_result_buffer_(count)
+#define _Outptr_result_buffer_maybenull_(count)
+#define _Outptr_result_maybenull_
 #define _Outptr_result_maybenull_z_
+#define _Outptr_result_z_
 
+#define _Param_(p)
+
+#define _Post_equal_to_(exp)
 #define _Post_invalid_
+#define _Post_maybez_
+#define _Post_readable_size_(count)
+#define _Post_satisfies_(exp)
 #define _Post_writable_byte_size_(count)
 #define _Post_z_
 
 #define _Pre_maybenull_
 #define _Pre_notnull_
+#define _Pre_opt_z_
 #define _Pre_writable_size_(count)
+#define _Pre_z_
 
 #define _Printf_format_string_
+#define _Printf_format_string_params_(count)
 
 #define _Ret_maybenull_
 #define _Ret_maybenull_z_
+#define _Ret_notnull_
 #define _Ret_opt_
 #define _Ret_opt_z_cap_(count)
 #define _Ret_writes_bytes_maybenull_(count)
 #define _Ret_z_
 
+#define _Scanf_format_string_
+#define _Scanf_format_string_params_(count)
+#define _Scanf_s_format_string_
+#define _Scanf_s_format_string_params_(count)
+
 #define _Success_(exp)
+
+#define _When_(exp1, exp2)
 
 #endif
