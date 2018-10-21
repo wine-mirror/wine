7c0430bb557be16bb548586fbef5afda75c661b4
Gijs Vermeulen <gijsvrm@gmail.com>
2018-10-22 00:53:44 +0200

    include: Add FMS* definitions.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=45079
    Signed-off-by: Gijs Vermeulen <gijsvrm@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/wfext.h
+++ include/wfext.h
@@ -57,6 +57,104 @@ extern "C" {
 #define FM_GETFILESEL         WINELIB_NAME_AW(FM_GETFILESEL)
 #define FM_GETFILESELLFN      WINELIB_NAME_AW(FM_GETFILESELLFN)
 
+typedef struct _FMS_GETFILESELA
+{
+    FILETIME ftTime;
+    DWORD    dwSize;
+    BYTE     bAttr;
+    CHAR     szName[260];
+} FMS_GETFILESELA, *LPFMS_GETFILESELA;
+
+typedef struct _FMS_GETFILESELW
+{
+    FILETIME ftTime;
+    DWORD    dwSize;
+    BYTE     bAttr;
+    WCHAR    szName[260];
+} FMS_GETFILESELW, *LPFMS_GETFILESELW;
+
+#define FMS_GETFILESEL        WINELIB_NAME_AW(FMS_GETFILESEL)
+#define LPFMS_GETFILESEL      WINELIB_NAME_AW(LPFMS_GETFILESEL)
+
+typedef struct _FMS_GETDRIVEINFOA
+{
+    DWORD dwTotalSpace;
+    DWORD dwFreeSpace;
+    CHAR  szPath[260];
+    CHAR  szVolume[14];
+    CHAR  szShare[128];
+} FMS_GETDRIVEINFOA, *LPFMS_GETDRIVEINFOA;
+
+typedef struct _FMS_GETDRIVEINFOW
+{
+    DWORD dwTotalSpace;
+    DWORD dwFreeSpace;
+    WCHAR szPath[260];
+    WCHAR szVolume[14];
+    WCHAR szShare[128];
+} FMS_GETDRIVEINFOW, *LPFMS_GETDRIVEINFOW;
+
+#define FMS_GETDRIVEINFO      WINELIB_NAME_AW(FMS_GETDRIVEINFO)
+#define LPFMS_GETDRIVEINFO    WINELIB_NAME_AW(LPFMS_GETDRIVEINFO)
+
+typedef struct _FMS_LOADA
+{
+    DWORD dwSize;
+    CHAR  szMenuName[MENU_TEXT_LEN];
+    HMENU hMenu;
+    UINT  wMenuDelta;
+} FMS_LOADA, *LPFMS_LOADA;
+
+typedef struct _FMS_LOADW
+{
+    DWORD dwSize;
+    WCHAR szMenuName[MENU_TEXT_LEN];
+    HMENU hMenu;
+    UINT  wMenuDelta;
+} FMS_LOADW, *LPFMS_LOADW;
+
+#define FMS_LOAD              WINELIB_NAME_AW(FMS_LOAD)
+#define LPFMS_LOAD            WINELIB_NAME_AW(LPFMS_LOAD)
+
+typedef struct tagEXT_BUTTON
+{
+    WORD idCommand;
+    WORD idsHelp;
+    WORD fsStyle;
+} EXT_BUTTON, *LPEXT_BUTTON;
+
+typedef struct tagFMS_TOOLBARLOAD
+{
+    DWORD        dwSize;
+    LPEXT_BUTTON lpButtons;
+    WORD         cButtons;
+    WORD         cBitmaps;
+    WORD         idBitmap;
+    HBITMAP      hBitmap;
+} FMS_TOOLBARLOAD, *LPFMS_TOOLBARLOAD;
+
+typedef struct tagFMS_HELPSTRINGA
+{
+    INT   idCommand;
+    HMENU hMenu;
+    CHAR  szHelp[128];
+} FMS_HELPSTRINGA, *LPFMS_HELPSTRINGA;
+
+typedef struct tagFMS_HELPSTRINGW
+{
+    INT   idCommand;
+    HMENU hMenu;
+    WCHAR szHelp[128];
+} FMS_HELPSTRINGW, *LPFMS_HELPSTRINGW;
+
+#define FMS_HELPSTRING        WINELIB_NAME_AW(FMS_HELPSTRING)
+#define LPFMS_HELPSTRING      WINELIB_NAME_AW(LPFMS_HELPSTRING)
+
+typedef DWORD (WINAPI *FM_EXT_PROC)(HWND,WORD,LONG);
+typedef DWORD (WINAPI *FM_UNDELETE_PROCA)(HWND,LPSTR);
+typedef DWORD (WINAPI *FM_UNDELETE_PROCW)(HWND,LPWSTR);
+DECL_WINELIB_TYPE_AW(FM_UNDELETE_PROC);
+
 LONG WINAPI FMExtensionProc(HWND,WORD,LONG);
 LONG WINAPI FMExtensionProcW(HWND,WORD,LONG);
 
