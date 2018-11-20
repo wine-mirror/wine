488432317206bc816432af0dd740e18979e37e58
Michael Stefaniuc <mstefani@winehq.org>
2018-11-20 20:47:20 +0100

    iphlpapi: Remove a redundant NULL check before HeapFree().
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/iphlpapi/ipstats.c
+++ dlls/iphlpapi/ipstats.c
@@ -2787,8 +2787,7 @@ DWORD build_udp6_table( UDP_TABLE_CLASS class, void **tablep, BOOL order, HANDLE
                     break;
             }
             HeapFree( GetProcessHeap(), 0, map );
-            if (addr_scopes)
-                HeapFree( GetProcessHeap(), 0, addr_scopes );
+            HeapFree( GetProcessHeap(), 0, addr_scopes );
             fclose( fp );
         }
         else ret = ERROR_NOT_SUPPORTED;
