b9d50242a639ce22aaca52b7446958f006ea8c28
Henri Verbeet <hverbeet@codeweavers.com>
2018-11-21 16:47:24 +0330

    d3d8: Disallow rendertarget and depth/stencil usage on index buffers.
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/tests/device.c
+++ dlls/d3d8/tests/device.c
@@ -9130,16 +9130,14 @@ static void test_resource_access(void)
 
         hr = IDirect3DDevice8_CreateIndexBuffer(device, 16, tests[i].usage,
                 tests[i].format == FORMAT_COLOUR ? D3DFMT_INDEX32 : D3DFMT_INDEX16, tests[i].pool, &ib);
-        todo_wine_if(tests[i].pool != D3DPOOL_SCRATCH && tests[i].usage & ~D3DUSAGE_DYNAMIC)
-            ok(hr == (tests[i].pool == D3DPOOL_SCRATCH || (tests[i].usage & ~D3DUSAGE_DYNAMIC)
-                    ? D3DERR_INVALIDCALL : D3D_OK), "Test %u: Got unexpected hr %#x.\n", i, hr);
+        ok(hr == (tests[i].pool == D3DPOOL_SCRATCH || (tests[i].usage & ~D3DUSAGE_DYNAMIC)
+                ? D3DERR_INVALIDCALL : D3D_OK), "Test %u: Got unexpected hr %#x.\n", i, hr);
         if (FAILED(hr))
             continue;
 
         hr = IDirect3DIndexBuffer8_GetDesc(ib, &ib_desc);
         ok(hr == D3D_OK, "Test %u: Got unexpected hr %#x.\n", i, hr);
-        todo_wine_if(tests[i].usage & ~D3DUSAGE_DYNAMIC)
-            ok(ib_desc.Usage == tests[i].usage, "Test %u: Got unexpected usage %#x.\n", i, ib_desc.Usage);
+        ok(ib_desc.Usage == tests[i].usage, "Test %u: Got unexpected usage %#x.\n", i, ib_desc.Usage);
         ok(ib_desc.Pool == tests[i].pool, "Test %u: Got unexpected pool %#x.\n", i, ib_desc.Pool);
 
         hr = IDirect3DIndexBuffer8_Lock(ib, 0, 0, &data, 0);
