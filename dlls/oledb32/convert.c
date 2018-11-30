4d525212a9cbb8f63587635a3e5b2b9d94248e09
Alex Henrie <alexhenrie24@gmail.com>
2018-11-30 00:14:16 -0700

    oledb32: Turn variable 'format' into a static constant.
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/oledb32/convert.c
+++ dlls/oledb32/convert.c
@@ -619,7 +619,7 @@ static HRESULT WINAPI convert_DataConvert(IDataConvert* iface,
         {
             WCHAR szBuff[39];
             const GUID *id = (const GUID *)src;
-            WCHAR format[] = {
+            static const WCHAR format[] = {
                 '{','%','0','8','X','-','%','0','4','X','-','%','0','4','X','-',
                 '%','0','2','X','%','0','2','X','-',
                 '%','0','2','X','%','0','2','X','%','0','2','X','%','0','2','X','%','0','2','X','%','0','2','X','}',0};
