90a391135570d4d2b20e6a84483e4573b3c97549
Michael Stefaniuc <mstefani@winehq.org>
2018-12-05 18:57:41 +0100

    d3dx9/tests: Use SetRectEmpty() instead of open coding it.
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>
    Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3dx9_36/tests/core.c
+++ dlls/d3dx9_36/tests/core.c
@@ -621,7 +621,7 @@ static void test_ID3DXFont(IDirect3DDevice9 *device)
         ok(height == tests[i].font_height, "Got unexpected height %u.\n", height);
         }
 
-        SetRect(&rect, 0, 0, 0, 0);
+        SetRectEmpty(&rect);
         height = ID3DXFont_DrawTextW(font, sprite, testW, size, &rect,
                 DT_LEFT | DT_CALCRECT, 0xffffffff);
         todo_wine ok(height == tests[i].font_height, "Got unexpected height %u.\n", height);
