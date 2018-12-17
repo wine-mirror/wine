9e99382bc38beea72c6d3fd61057c88dbba48601
Nikolay Sivov <nsivov@codeweavers.com>
2018-12-17 08:16:10 +0300

    shell32/tests: Fix some leaks (Valgrind).
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/shell32/tests/shlfolder.c
+++ dlls/shell32/tests/shlfolder.c
@@ -5165,6 +5165,8 @@ static void test_GetDefaultColumn(void)
         hr = IShellFolder2_GetDefaultColumn(folder, 0, &sort, NULL);
         ok(hr == E_NOTIMPL, "Unexpected hr %#x.\n", hr);
         ok(sort == 123, "Unexpected default column.\n");
+
+        IShellFolder2_Release(folder);
     }
 
     CoUninitialize();
