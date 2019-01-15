wine-mirror/wine

commit d498780294c635f67aee823bc030151a6b75bee4

ntoskrnl.exe: Handle already-loaded drivers in handle_bus_relations().
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/ntoskrnl.exe/ntoskrnl.c
@@ -3756,7 +3756,10 @@ static void handle_bus_relations( DEVICE_OBJECT *device )
     strcpyW( buffer, servicesW );
     strcatW( buffer, driver );
     RtlInitUnicodeString( &string, buffer );
-    if (ZwLoadDriver( &string ) != STATUS_SUCCESS)
+    status = ZwLoadDriver( &string );
+    if (status == STATUS_IMAGE_ALREADY_LOADED)
+        return;
+    else if (status != STATUS_SUCCESS)
     {
         ERR_(plugplay)( "Failed to load driver %s\n", debugstr_w(driver) );
         return;

commit 48504a2cc7f6b0c6f66d0ae4a76c8b01130234bc

ntoskrnl.exe: Properly handle already-loaded drivers in handle_bus_relations().
This fixes a regression introduced by d498780294c635f67aee823bc030151a6b75bee4.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46469
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/ntoskrnl.exe/ntoskrnl.c
@@ -3758,9 +3758,7 @@ static void handle_bus_relations( DEVICE_OBJECT *device )
     strcatW( buffer, driver );
     RtlInitUnicodeString( &string, buffer );
     status = ZwLoadDriver( &string );
-    if (status == STATUS_IMAGE_ALREADY_LOADED)
-        return;
-    else if (status != STATUS_SUCCESS)
+    if (status != STATUS_SUCCESS && status != STATUS_IMAGE_ALREADY_LOADED)
     {
         ERR_(plugplay)( "Failed to load driver %s\n", debugstr_w(driver) );
         return;