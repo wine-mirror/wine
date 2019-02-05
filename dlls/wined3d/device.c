wine-mirror/wine

commit 7163fbba85b0f27f5bedb22291a6f53d2b507cdd

wined3d: Remove stateblock handling from wined3d_device_multiply_transform().
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/device.c
@@ -1529,26 +1529,21 @@ void CDECL wined3d_device_get_transform(const struct wined3d_device *device,
 void CDECL wined3d_device_multiply_transform(struct wined3d_device *device,
         enum wined3d_transform_state state, const struct wined3d_matrix *matrix)
 {
-    const struct wined3d_matrix *mat;
-    struct wined3d_matrix temp;
+    struct wined3d_matrix *mat;
 
     TRACE("device %p, state %s, matrix %p.\n", device, debug_d3dtstype(state), matrix);
 
-    /* Note: Using 'updateStateBlock' rather than 'stateblock' in the code
-     * below means it will be recorded in a state block change, but it
-     * works regardless where it is recorded.
-     * If this is found to be wrong, change to StateBlock. */
     if (state > HIGHEST_TRANSFORMSTATE)
     {
         WARN("Unhandled transform state %#x.\n", state);
         return;
     }
 
-    mat = &device->update_state->transforms[state];
-    multiply_matrix(&temp, mat, matrix);
-
-    /* Apply change via set transform - will reapply to eg. lights this way. */
-    wined3d_device_set_transform(device, state, &temp);
+    /* Tests show that stateblock recording is ignored; the change goes directly
+     * into the primary stateblock. */
+    mat = &device->state.transforms[state];
+    multiply_matrix(mat, mat, matrix);
+    wined3d_cs_emit_set_transform(device->cs, state, matrix);
 }
 
 /* Note lights are real special cases. Although the device caps state only

dlls/d3d8/tests/device.c
@@ -9295,7 +9295,6 @@ static void test_multiply_transform(void)
 
         hr = IDirect3DDevice8_GetTransform(device, tests[i], &ret_mat);
         ok(hr == D3D_OK, "Test %u: Got unexpected hr %#x.\n", i, hr);
-todo_wine
         ok(!memcmp(&ret_mat, &mat2, sizeof(mat2)), "Test %u: Got unexpected transform matrix.\n", i);
 
         hr = IDirect3DDevice8_CaptureStateBlock(device, stateblock);

dlls/d3d9/tests/device.c
@@ -13129,7 +13129,6 @@ static void test_multiply_transform(void)
 
         hr = IDirect3DDevice9_GetTransform(device, tests[i], &ret_mat);
         ok(hr == D3D_OK, "Test %u: Got unexpected hr %#x.\n", i, hr);
-todo_wine
         ok(!memcmp(&ret_mat, &mat2, sizeof(mat2)), "Test %u: Got unexpected transform matrix.\n", i);
 
         hr = IDirect3DStateBlock9_Capture(stateblock);

dlls/ddraw/tests/ddraw7.c
@@ -15397,7 +15397,6 @@ static void test_multiply_transform(void)
 
         hr = IDirect3DDevice7_GetTransform(device, tests[i], &ret_mat);
         ok(hr == D3D_OK, "Test %u: Got unexpected hr %#x.\n", i, hr);
-todo_wine
         ok(!memcmp(&ret_mat, &mat2, sizeof(mat2)), "Test %u: Got unexpected transform matrix.\n", i);
 
         hr = IDirect3DDevice7_CaptureStateBlock(device, stateblock);

commit f2578fc1b1d54b26185813cd1fe94f5b2804c5d3

wined3d: Set the correct matrix in wined3d_device_multiply_transform().
Fixes a regression introduced by 7163fbba85b0f27f5bedb22291a6f53d2b507cdd.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46565
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/device.c
@@ -1544,7 +1544,7 @@ void CDECL wined3d_device_multiply_transform(struct wined3d_device *device,
      * into the primary stateblock. */
     mat = &device->state.transforms[state];
     multiply_matrix(mat, mat, matrix);
-    wined3d_cs_emit_set_transform(device->cs, state, matrix);
+    wined3d_cs_emit_set_transform(device->cs, state, mat);
 }
 
 /* Note lights are real special cases. Although the device caps state only