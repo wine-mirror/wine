8b24bc95b9a9a506c1685407aa02c459cfea26d5
Piotr Caban <piotr@codeweavers.com>
2019-02-22 16:41:20 +0100

    winebuild: Recognize armv7a target.
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- tools/widl/widl.c
+++ tools/widl/widl.c
@@ -290,6 +290,7 @@ static void set_target( const char *target )
         { "armv5",   CPU_ARM },
         { "armv6",   CPU_ARM },
         { "armv7",   CPU_ARM },
+        { "armv7a",  CPU_ARM },
         { "arm64",   CPU_ARM64 },
         { "aarch64", CPU_ARM64 },
     };
--- tools/winebuild/utils.c
+++ tools/winebuild/utils.c
@@ -66,6 +66,7 @@ static const struct
     { "armv5",   CPU_ARM },
     { "armv6",   CPU_ARM },
     { "armv7",   CPU_ARM },
+    { "armv7a",  CPU_ARM },
     { "arm64",   CPU_ARM64 },
     { "aarch64", CPU_ARM64 },
 };
