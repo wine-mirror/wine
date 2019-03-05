wine-mirror/wine

commit 5800c9ed23a5c720843445d91c8547925493df8a

vbscript: Treat \r as a newline separator.
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Jacek Caban <jacek@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/vbscript/lex.c
@@ -383,13 +383,14 @@ static int parse_hex_literal(parser_ctx_t *ctx, LONG *ret)
 
 static void skip_spaces(parser_ctx_t *ctx)
 {
-    while(*ctx->ptr == ' ' || *ctx->ptr == '\t' || *ctx->ptr == '\r')
+    while(*ctx->ptr == ' ' || *ctx->ptr == '\t')
         ctx->ptr++;
 }
 
 static int comment_line(parser_ctx_t *ctx)
 {
-    ctx->ptr = strchrW(ctx->ptr, '\n');
+    static const WCHAR newlineW[] = {'\n','\r',0};
+    ctx->ptr = strpbrkW(ctx->ptr, newlineW);
     if(ctx->ptr)
         ctx->ptr++;
     else
@@ -421,6 +422,7 @@ static int parse_next_token(void *lval, parser_ctx_t *ctx)
 
     switch(c) {
     case '\n':
+    case '\r':
         ctx->ptr++;
         return tNL;
     case '\'':

dlls/vbscript/tests/run.c
@@ -2394,6 +2394,18 @@ static void run_tests(void)
     ok(FAILED(hres), "script didn't fail\n");
     todo_wine CHECK_CALLED(OnScriptError);
 
+    SET_EXPECT(global_success_d);
+    SET_EXPECT(global_success_i);
+    parse_script_a("' comment\r"
+                   "Sub testsub(arg)\r"
+                   "If arg = 1 Then\r\r"
+                   "Call reportSuccess()\n\n"
+                   "End If\r\n"
+                   "End Sub\n\r"
+                   "Call testsub(1)");
+    CHECK_CALLED(global_success_d);
+    CHECK_CALLED(global_success_i);
+
     run_from_res("lang.vbs");
     run_from_res("api.vbs");
     run_from_res("regexp.vbs");

commit 1bd7dbfbc42af85ad5309759d6311427f2860de9

vbscript: Handle carriage return in more places.
This fixes a regression introduced by 5800c9ed23a5c720843445d91c8547925493df8a.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46772
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Jacek Caban <jacek@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/vbscript/lex.c
@@ -225,7 +225,7 @@ static int parse_string_literal(parser_ctx_t *ctx, const WCHAR **ret)
     int len = 0;
 
     while(ctx->ptr < ctx->end) {
-        if(*ctx->ptr == '\n') {
+        if(*ctx->ptr == '\n' || *ctx->ptr == '\r') {
             FIXME("newline inside string literal\n");
             return 0;
         }
@@ -497,11 +497,14 @@ int parser_lex(void *lval, parser_ctx_t *ctx)
         ret = parse_next_token(lval, ctx);
         if(ret == '_') {
             skip_spaces(ctx);
-            if(*ctx->ptr != '\n') {
+            if(*ctx->ptr != '\n' && *ctx->ptr != '\r') {
                 FIXME("'_' not followed by newline\n");
                 return 0;
             }
-            ctx->ptr++;
+            if(*ctx->ptr == '\r')
+                ctx->ptr++;
+            if(*ctx->ptr == '\n')
+                ctx->ptr++;
             continue;
         }
         if(ret != tNL || ctx->last_token != tNL)

dlls/vbscript/tests/run.c
@@ -2481,6 +2481,9 @@ static void run_tests(void)
     SET_EXPECT(global_success_d);
     SET_EXPECT(global_success_i);
     parse_script_a("' comment\r"
+                   "x = _\r3\r"
+                   "x = _\n3\r"
+                   "x = _\r\n3\r"
                    "Sub testsub(arg)\r"
                    "If arg = 1 Then\r\r"
                    "Call reportSuccess()\n\n"