18fe24ae08ae3786530ac4bc7a5058e4b63a1f84
Zebediah Figura <z.figura12@gmail.com>
2019-03-12 00:32:58 -0500

    quartz/tests: Add some tests for IBaseFilter_FindPin() on the DirectSound renderer.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/quartz/tests/dsoundrender.c
+++ dlls/quartz/tests/dsoundrender.c
@@ -26,6 +26,8 @@
 #include "amaudio.h"
 #include "wine/test.h"
 
+static const WCHAR sink_id[] = {'A','u','d','i','o',' ','I','n','p','u','t',' ','p','i','n',' ','(','r','e','n','d','e','r','e','d',')',0};
+
 static IBaseFilter *create_dsound_render(void)
 {
     IBaseFilter *filter = NULL;
@@ -243,6 +245,39 @@ todo_wine
     ok(!ref, "Got outstanding refcount %d.\n", ref);
 }
 
+static void test_find_pin(void)
+{
+    static const WCHAR inW[] = {'I','n',0};
+    static const WCHAR input_pinW[] = {'i','n','p','u','t',' ','p','i','n',0};
+    IBaseFilter *filter = create_dsound_render();
+    IEnumPins *enum_pins;
+    IPin *pin, *pin2;
+    HRESULT hr;
+    ULONG ref;
+
+    hr = IBaseFilter_FindPin(filter, inW, &pin);
+    ok(hr == VFW_E_NOT_FOUND, "Got hr %#x.\n", hr);
+
+    hr = IBaseFilter_FindPin(filter, input_pinW, &pin);
+    ok(hr == VFW_E_NOT_FOUND, "Got hr %#x.\n", hr);
+
+    hr = IBaseFilter_FindPin(filter, sink_id, &pin);
+    ok(hr == S_OK, "Got hr %#x.\n", hr);
+
+    hr = IBaseFilter_EnumPins(filter, &enum_pins);
+    ok(hr == S_OK, "Got hr %#x.\n", hr);
+
+    hr = IEnumPins_Next(enum_pins, 1, &pin2, NULL);
+    ok(hr == S_OK, "Got hr %#x.\n", hr);
+    ok(pin == pin2, "Expected pin %p, got %p.\n", pin2, pin);
+    IPin_Release(pin);
+    IPin_Release(pin2);
+
+    IEnumPins_Release(enum_pins);
+    ref = IBaseFilter_Release(filter);
+    ok(!ref, "Got outstanding refcount %d.\n", ref);
+}
+
 static void test_pin(IPin *pin)
 {
     IMemInputPin *mpin = NULL;
@@ -320,6 +355,7 @@ START_TEST(dsoundrender)
     test_property_bag();
     test_interfaces();
     test_enum_pins();
+    test_find_pin();
     test_basefilter();
 
     CoUninitialize();
