bea86f01805b253567a2ee408cd03d6e445ba65d
Józef Kucia <jkucia@codeweavers.com>
2019-03-19 15:59:33 +0100

    winevulkan: Enable VK_EXT_swapchain_colorspace.
    Signed-off-by: Józef Kucia <jkucia@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winevulkan/vulkan_thunks.c
+++ dlls/winevulkan/vulkan_thunks.c
@@ -3698,6 +3698,7 @@ static const char * const vk_device_extensions[] =
 
 static const char * const vk_instance_extensions[] =
 {
+    "VK_EXT_swapchain_colorspace",
     "VK_KHR_device_group_creation",
     "VK_KHR_get_physical_device_properties2",
     "VK_KHR_surface",
