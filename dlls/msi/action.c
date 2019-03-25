wine-mirror/wine

commit 15f852015a0332313821804cc1fbad79216408c6

msi: Disable filesystem redirection only when really needed.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46846
Signed-off-by: Hans Leidekker <hans@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/msi/action.c
@@ -718,50 +718,6 @@ MSIFOLDER *msi_get_loaded_folder( MSIPACKAGE *package, const WCHAR *dir )
     return NULL;
 }
 
-/*
- * Recursively create all directories in the path.
- * shamelessly stolen from setupapi/queue.c
- */
-BOOL msi_create_full_path( const WCHAR *path )
-{
-    BOOL ret = TRUE;
-    WCHAR *new_path;
-    int len;
-
-    new_path = msi_alloc( (strlenW( path ) + 1) * sizeof(WCHAR) );
-    strcpyW( new_path, path );
-
-    while ((len = strlenW( new_path )) && new_path[len - 1] == '\\')
-    new_path[len - 1] = 0;
-
-    while (!CreateDirectoryW( new_path, NULL ))
-    {
-        WCHAR *slash;
-        DWORD last_error = GetLastError();
-        if (last_error == ERROR_ALREADY_EXISTS) break;
-        if (last_error != ERROR_PATH_NOT_FOUND)
-        {
-            ret = FALSE;
-            break;
-        }
-        if (!(slash = strrchrW( new_path, '\\' )))
-        {
-            ret = FALSE;
-            break;
-        }
-        len = slash - new_path;
-        new_path[len] = 0;
-        if (!msi_create_full_path( new_path ))
-        {
-            ret = FALSE;
-            break;
-        }
-        new_path[len] = '\\';
-    }
-    msi_free( new_path );
-    return ret;
-}
-
 void msi_ui_progress( MSIPACKAGE *package, int a, int b, int c, int d )
 {
     MSIRECORD *row;
@@ -843,8 +799,9 @@ static UINT ITERATE_CreateFolders(MSIRECORD *row, LPVOID param)
     TRACE("folder is %s\n", debugstr_w(full_path));
 
     folder = msi_get_loaded_folder( package, dir );
-    if (folder->State == FOLDER_STATE_UNINITIALIZED) msi_create_full_path( full_path );
+    if (folder->State == FOLDER_STATE_UNINITIALIZED) msi_create_full_path( package, full_path );
     folder->State = FOLDER_STATE_CREATED;
+
     return ERROR_SUCCESS;
 }
 
@@ -2054,37 +2011,6 @@ static UINT ITERATE_CostFinalizeConditions(MSIRECORD *row, LPVOID param)
     return ERROR_SUCCESS;
 }
 
-VS_FIXEDFILEINFO *msi_get_disk_file_version( LPCWSTR filename )
-{
-    static const WCHAR name[] = {'\\',0};
-    VS_FIXEDFILEINFO *ptr, *ret;
-    LPVOID version;
-    DWORD versize, handle;
-    UINT sz;
-
-    versize = GetFileVersionInfoSizeW( filename, &handle );
-    if (!versize)
-        return NULL;
-
-    version = msi_alloc( versize );
-    if (!version)
-        return NULL;
-
-    GetFileVersionInfoW( filename, 0, versize, version );
-
-    if (!VerQueryValueW( version, name, (LPVOID *)&ptr, &sz ))
-    {
-        msi_free( version );
-        return NULL;
-    }
-
-    ret = msi_alloc( sz );
-    memcpy( ret, ptr, sz );
-
-    msi_free( version );
-    return ret;
-}
-
 int msi_compare_file_versions( VS_FIXEDFILEINFO *fi, const WCHAR *version )
 {
     DWORD ms, ls;
@@ -2108,33 +2034,6 @@ int msi_compare_font_versions( const WCHAR *ver1, const WCHAR *ver2 )
     if (ms1 > ms2) return 1;
     else if (ms1 < ms2) return -1;
     return 0;
-}
-
-DWORD msi_get_disk_file_size( LPCWSTR filename )
-{
-    HANDLE file;
-    DWORD size;
-
-    file = CreateFileW( filename, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL );
-    if (file == INVALID_HANDLE_VALUE)
-        return INVALID_FILE_SIZE;
-
-    size = GetFileSize( file, NULL );
-    CloseHandle( file );
-    return size;
-}
-
-BOOL msi_file_hash_matches( MSIFILE *file )
-{
-    UINT r;
-    MSIFILEHASHINFO hash;
-
-    hash.dwFileHashInfoSize = sizeof(MSIFILEHASHINFO);
-    r = msi_get_filehash( file->TargetPath, &hash );
-    if (r != ERROR_SUCCESS)
-        return FALSE;
-
-    return !memcmp( &hash, &file->hash, sizeof(MSIFILEHASHINFO) );
 }
 
 static WCHAR *create_temp_dir( MSIDATABASE *db )
@@ -2262,17 +2161,17 @@ static UINT calculate_file_cost( MSIPACKAGE *package )
         set_target_path( package, file );
 
         if ((comp->assembly && !comp->assembly->installed) ||
-            GetFileAttributesW(file->TargetPath) == INVALID_FILE_ATTRIBUTES)
+            msi_get_file_attributes( package, file->TargetPath ) == INVALID_FILE_ATTRIBUTES)
         {
             comp->Cost += file->FileSize;
             continue;
         }
-        file_size = msi_get_disk_file_size( file->TargetPath );
+        file_size = msi_get_disk_file_size( package, file->TargetPath );
         TRACE("%s (size %u)\n", debugstr_w(file->TargetPath), file_size);
 
         if (file->Version)
         {
-            if ((file_version = msi_get_disk_file_version( file->TargetPath )))
+            if ((file_version = msi_get_disk_file_version( package, file->TargetPath )))
             {
                 if (msi_compare_file_versions( file_version, file->Version ) < 0)
                 {
@@ -2281,7 +2180,7 @@ static UINT calculate_file_cost( MSIPACKAGE *package )
                 msi_free( file_version );
                 continue;
             }
-            else if ((font_version = msi_font_version_from_file( file->TargetPath )))
+            else if ((font_version = msi_get_font_file_version( package, file->TargetPath )))
             {
                 if (msi_compare_font_versions( font_version, file->Version ) < 0)
                 {
@@ -2296,6 +2195,7 @@ static UINT calculate_file_cost( MSIPACKAGE *package )
             comp->Cost += file->FileSize - file_size;
         }
     }
+
     return ERROR_SUCCESS;
 }
 
@@ -3754,6 +3654,24 @@ static BOOL CALLBACK Typelib_EnumResNameProc( HMODULE hModule, LPCWSTR lpszType,
     return TRUE;
 }
 
+static HMODULE msi_load_library( MSIPACKAGE *package, const WCHAR *filename, DWORD flags )
+{
+    HMODULE module;
+    msi_disable_fs_redirection( package );
+    module = LoadLibraryExW( filename, NULL, flags );
+    msi_revert_fs_redirection( package );
+    return module;
+}
+
+static HRESULT msi_load_typelib( MSIPACKAGE *package, const WCHAR *filename, REGKIND kind, ITypeLib **lib )
+{
+    HRESULT hr;
+    msi_disable_fs_redirection( package );
+    hr = LoadTypeLibEx( filename, kind, lib );
+    msi_revert_fs_redirection( package );
+    return hr;
+}
+
 static UINT ITERATE_RegisterTypeLibraries(MSIRECORD *row, LPVOID param)
 {
     MSIPACKAGE* package = param;
@@ -3784,7 +3702,7 @@ static UINT ITERATE_RegisterTypeLibraries(MSIRECORD *row, LPVOID param)
     }
     MSI_ProcessMessage(package, INSTALLMESSAGE_ACTIONDATA, row);
 
-    module = LoadLibraryExW( file->TargetPath, NULL, LOAD_LIBRARY_AS_DATAFILE );
+    module = msi_load_library( package, file->TargetPath, LOAD_LIBRARY_AS_DATAFILE );
     if (module)
     {
         LPCWSTR guid;
@@ -3821,7 +3739,7 @@ static UINT ITERATE_RegisterTypeLibraries(MSIRECORD *row, LPVOID param)
     }
     else
     {
-        hr = LoadTypeLibEx(file->TargetPath, REGKIND_REGISTER, &tlib);
+        hr = msi_load_typelib( package, file->TargetPath, REGKIND_REGISTER, &tlib );
         if (FAILED(hr))
         {
             ERR("Failed to load type library: %08x\n", hr);
@@ -3932,7 +3850,7 @@ static WCHAR *get_link_file( MSIPACKAGE *package, MSIRECORD *row )
         return NULL;
     }
     /* may be needed because of a bug somewhere else */
-    msi_create_full_path( link_folder );
+    msi_create_full_path( package, link_folder );
 
     filename = msi_dup_record_field( row, 3 );
     msi_reduce_to_long_filename( filename );
@@ -3965,7 +3883,7 @@ WCHAR *msi_build_icon_path( MSIPACKAGE *package, const WCHAR *icon_name )
         msi_free( appdata );
     }
     dest = msi_build_directory_name( 3, folder, szInstaller, package->ProductCode );
-    msi_create_full_path( dest );
+    msi_create_full_path( package, dest );
     path = msi_build_directory_name( 2, dest, icon_name );
     msi_free( folder );
     msi_free( dest );
@@ -4069,10 +3987,14 @@ static UINT ITERATE_CreateShortcuts(MSIRECORD *row, LPVOID param)
         full_path = msi_get_target_folder( package, wkdir );
         if (full_path) IShellLinkW_SetWorkingDirectory( sl, full_path );
     }
-    link_file = get_link_file(package, row);
 
+    link_file = get_link_file(package, row);
     TRACE("Writing shortcut to %s\n", debugstr_w(link_file));
+
+    msi_disable_fs_redirection( package );
     IPersistFile_Save(pf, link_file, FALSE);
+    msi_revert_fs_redirection( package );
+
     msi_free(link_file);
 
 err:
@@ -4130,12 +4052,8 @@ static UINT ITERATE_RemoveShortcuts( MSIRECORD *row, LPVOID param )
     MSI_ProcessMessage(package, INSTALLMESSAGE_ACTIONDATA, row);
 
     link_file = get_link_file( package, row );
-
     TRACE("Removing shortcut file %s\n", debugstr_w( link_file ));
-    if (!DeleteFileW( link_file ))
-    {
-        WARN("Failed to remove shortcut file %u\n", GetLastError());
-    }
+    if (!msi_delete_file( package, link_file )) WARN("Failed to remove shortcut file %u\n", GetLastError());
     msi_free( link_file );
 
     return ERROR_SUCCESS;
@@ -4163,51 +4081,49 @@ static UINT ACTION_RemoveShortcuts( MSIPACKAGE *package )
 
 static UINT ITERATE_PublishIcon(MSIRECORD *row, LPVOID param)
 {
-    MSIPACKAGE* package = param;
-    HANDLE the_file;
-    LPWSTR FilePath;
-    LPCWSTR FileName;
-    CHAR buffer[1024];
+    MSIPACKAGE *package = param;
+    HANDLE handle;
+    WCHAR *icon_path;
+    const WCHAR *filename;
+    char buffer[1024];
     DWORD sz;
     UINT rc;
 
-    FileName = MSI_RecordGetString(row,1);
-    if (!FileName)
+    filename = MSI_RecordGetString( row, 1 );
+    if (!filename)
     {
-        ERR("Unable to get FileName\n");
+        ERR("Unable to get filename\n");
         return ERROR_SUCCESS;
     }
 
-    FilePath = msi_build_icon_path(package, FileName);
+    icon_path = msi_build_icon_path( package, filename );
 
-    TRACE("Creating icon file at %s\n",debugstr_w(FilePath));
+    TRACE("Creating icon file at %s\n", debugstr_w(icon_path));
 
-    the_file = CreateFileW(FilePath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS,
-                        FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (the_file == INVALID_HANDLE_VALUE)
+    handle = msi_create_file( package, icon_path, GENERIC_WRITE, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL );
+    if (handle == INVALID_HANDLE_VALUE)
     {
-        ERR("Unable to create file %s\n",debugstr_w(FilePath));
-        msi_free(FilePath);
+        ERR("Unable to create file %s\n", debugstr_w(icon_path));
+        msi_free( icon_path );
         return ERROR_SUCCESS;
     }
 
-    do 
+    do
     {
-        DWORD write;
+        DWORD count;
         sz = 1024;
-        rc = MSI_RecordReadStream(row,2,buffer,&sz);
+        rc = MSI_RecordReadStream( row, 2, buffer, &sz );
         if (rc != ERROR_SUCCESS)
         {
             ERR("Failed to get stream\n");
-            DeleteFileW(FilePath);
+            msi_delete_file( package, icon_path );
             break;
         }
-        WriteFile(the_file,buffer,sz,&write,NULL);
+        WriteFile( handle, buffer, sz, &count, NULL );
     } while (sz == 1024);
 
-    msi_free(FilePath);
-    CloseHandle(the_file);
+    msi_free( icon_path );
+    CloseHandle( handle );
 
     return ERROR_SUCCESS;
 }
@@ -5405,11 +5321,11 @@ static UINT ITERATE_UnpublishIcon( MSIRECORD *row, LPVOID param )
     if ((icon_path = msi_build_icon_path( package, icon )))
     {
         TRACE("removing icon file %s\n", debugstr_w(icon_path));
-        DeleteFileW( icon_path );
+        msi_delete_file( package, icon_path );
         if ((p = strrchrW( icon_path, '\\' )))
         {
             *p = 0;
-            RemoveDirectoryW( icon_path );
+            msi_remove_directory( package, icon_path );
         }
         msi_free( icon_path );
     }
@@ -7827,9 +7743,18 @@ static UINT ACTION_MigrateFeatureStates( MSIPACKAGE *package )
     return ERROR_SUCCESS;
 }
 
-static void bind_image( const char *filename, const char *path )
+static BOOL msi_bind_image( MSIPACKAGE *package, const char *filename, const char *path )
 {
-    if (!BindImageEx( 0, filename, path, NULL, NULL ))
+    BOOL ret;
+    msi_disable_fs_redirection( package );
+    ret = BindImage( filename, path, NULL );
+    msi_revert_fs_redirection( package );
+    return ret;
+}
+
+static void bind_image( MSIPACKAGE *package, const char *filename, const char *path )
+{
+    if (!msi_bind_image( package, filename, path ))
     {
         WARN("failed to bind image %u\n", GetLastError());
     }
@@ -7851,8 +7776,9 @@ static UINT ITERATE_BindImage( MSIRECORD *rec, LPVOID param )
         return ERROR_SUCCESS;
     }
     if (!(filenameA = strdupWtoA( file->TargetPath ))) return ERROR_SUCCESS;
+
     path_list = msi_split_string( paths, ';' );
-    if (!path_list) bind_image( filenameA, NULL );
+    if (!path_list) bind_image( package, filenameA, NULL );
     else
     {
         for (i = 0; path_list[i] && path_list[i][0]; i++)
@@ -7860,7 +7786,7 @@ static UINT ITERATE_BindImage( MSIRECORD *rec, LPVOID param )
             deformat_string( package, path_list[i], &pathW );
             if ((pathA = strdupWtoA( pathW )))
             {
-                bind_image( filenameA, pathA );
+                bind_image( package, filenameA, pathA );
                 msi_free( pathA );
             }
             msi_free( pathW );
@@ -7868,6 +7794,7 @@ static UINT ITERATE_BindImage( MSIRECORD *rec, LPVOID param )
     }
     msi_free( path_list );
     msi_free( filenameA );
+
     return ERROR_SUCCESS;
 }
 
@@ -7884,10 +7811,8 @@ static UINT ACTION_BindImage( MSIPACKAGE *package )
     {
         r = MSI_IterateRecords( view, NULL, ITERATE_BindImage, package );
         msiobj_release( &view->hdr );
-        if (r != ERROR_SUCCESS)
-            return r;
     }
-    return ERROR_SUCCESS;
+    return r;
 }
 
 static UINT msi_unimplemented_action_stub( MSIPACKAGE *package, LPCSTR action, LPCWSTR table )
@@ -8034,11 +7959,7 @@ StandardActions[] =
 static UINT ACTION_HandleStandardAction(MSIPACKAGE *package, LPCWSTR action)
 {
     UINT rc = ERROR_FUNCTION_NOT_CALLED;
-    void *cookie;
     UINT i;
-
-    if (is_wow64 && package->platform == PLATFORM_X64)
-        Wow64DisableWow64FsRedirection(&cookie);
 
     i = 0;
     while (StandardActions[i].action != NULL)
@@ -8075,9 +7996,6 @@ static UINT ACTION_HandleStandardAction(MSIPACKAGE *package, LPCWSTR action)
         i++;
     }
 
-    if (is_wow64 && package->platform == PLATFORM_X64)
-        Wow64RevertWow64FsRedirection(cookie);
-
     return rc;
 }
 
@@ -8086,12 +8004,6 @@ UINT ACTION_PerformAction(MSIPACKAGE *package, const WCHAR *action)
     UINT rc;
 
     TRACE("Performing action (%s)\n", debugstr_w(action));
-
-    if (!msi_init_assembly_caches( package ))
-    {
-        ERR("can't initialize assembly caches\n");
-        return ERROR_FUNCTION_FAILED;
-    }
 
     package->action_progress_increment = 0;
     rc = ACTION_HandleStandardAction(package, action);

dlls/msi/install.c
@@ -571,7 +571,7 @@ UINT MSI_SetTargetPathW( MSIPACKAGE *package, LPCWSTR szFolder, LPCWSTR szFolder
 
     TRACE("%p %s %s\n", package, debugstr_w(szFolder), debugstr_w(szFolderPath));
 
-    attrib = GetFileAttributesW(szFolderPath);
+    attrib = msi_get_file_attributes( package, szFolderPath );
     /* native MSI tests writeability by making temporary files at each drive */
     if (attrib != INVALID_FILE_ATTRIBUTES &&
         (attrib & FILE_ATTRIBUTE_OFFLINE || attrib & FILE_ATTRIBUTE_READONLY))

dlls/msi/custom.c
@@ -1188,7 +1188,7 @@ static UINT HANDLE_CustomType21_22( MSIPACKAGE *package, const WCHAR *source, co
         return ERROR_FUNCTION_FAILED;
     }
 
-    hFile = CreateFileW(file->TargetPath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+    hFile = msi_create_file( package, file->TargetPath, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING, 0 );
     if (hFile == INVALID_HANDLE_VALUE) return ERROR_FUNCTION_FAILED;
 
     sz = GetFileSize(hFile, &szHighWord);

dlls/msi/font.c
@@ -84,7 +84,7 @@ static const WCHAR regfont2[] =
  * Code based off of code located here
  * http://www.codeproject.com/gdi/fontnamefromfile.asp
  */
-static WCHAR *load_ttf_name_id( const WCHAR *filename, DWORD id )
+static WCHAR *load_ttf_name_id( MSIPACKAGE *package, const WCHAR *filename, DWORD id )
 {
     TT_TABLE_DIRECTORY tblDir;
     BOOL bFound = FALSE;
@@ -96,8 +96,10 @@ static WCHAR *load_ttf_name_id( const WCHAR *filename, DWORD id )
     LPWSTR ret = NULL;
     int i;
 
-    handle = CreateFileW(filename ,GENERIC_READ, 0, NULL, OPEN_EXISTING,
-                    FILE_ATTRIBUTE_NORMAL, 0 );
+    if (package)
+        handle = msi_create_file( package, filename, GENERIC_READ, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL );
+    else
+        handle = CreateFileW( filename, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0 );
     if (handle == INVALID_HANDLE_VALUE)
     {
         ERR("Unable to open font file %s\n", debugstr_w(filename));
@@ -175,12 +177,12 @@ end:
     return ret;
 }
 
-static WCHAR *font_name_from_file( const WCHAR *filename )
+static WCHAR *font_name_from_file( MSIPACKAGE *package, const WCHAR *filename )
 {
     static const WCHAR truetypeW[] = {' ','(','T','r','u','e','T','y','p','e',')',0};
     WCHAR *name, *ret = NULL;
 
-    if ((name = load_ttf_name_id( filename, NAME_ID_FULL_FONT_NAME )))
+    if ((name = load_ttf_name_id( package, filename, NAME_ID_FULL_FONT_NAME )))
     {
         if (!name[0])
         {
@@ -196,12 +198,12 @@ static WCHAR *font_name_from_file( const WCHAR *filename )
     return ret;
 }
 
-WCHAR *msi_font_version_from_file( const WCHAR *filename )
+WCHAR *msi_get_font_file_version( MSIPACKAGE *package, const WCHAR *filename )
 {
     static const WCHAR fmtW[] = {'%','u','.','%','u','.','0','.','0',0};
     WCHAR *version, *p, *q, *ret = NULL;
 
-    if ((version = load_ttf_name_id( filename, NAME_ID_VERSION )))
+    if ((version = load_ttf_name_id( package, filename, NAME_ID_VERSION )))
     {
         int len, major = 0, minor = 0;
         if ((p = strchrW( version, ';' ))) *p = 0;
@@ -258,7 +260,7 @@ static UINT ITERATE_RegisterFonts(MSIRECORD *row, LPVOID param)
     RegCreateKeyW(HKEY_LOCAL_MACHINE,regfont2,&hkey2);
 
     if (MSI_RecordIsNull(row,2))
-        name = font_name_from_file( file->TargetPath );
+        name = font_name_from_file( package, file->TargetPath );
     else
         name = msi_dup_record_field(row,2);
 
@@ -341,7 +343,7 @@ static UINT ITERATE_UnregisterFonts( MSIRECORD *row, LPVOID param )
     RegCreateKeyW( HKEY_LOCAL_MACHINE, regfont2, &hkey2 );
 
     if (MSI_RecordIsNull( row, 2 ))
-        name = font_name_from_file( file->TargetPath );
+        name = font_name_from_file( package, file->TargetPath );
     else
         name = msi_dup_record_field( row, 2 );
 

dlls/msi/media.c
@@ -439,12 +439,11 @@ static INT_PTR cabinet_copy_file(FDINOTIFICATIONTYPE fdint,
     attrs = attrs & (FILE_ATTRIBUTE_READONLY|FILE_ATTRIBUTE_HIDDEN|FILE_ATTRIBUTE_SYSTEM);
     if (!attrs) attrs = FILE_ATTRIBUTE_NORMAL;
 
-    handle = CreateFileW(path, GENERIC_READ | GENERIC_WRITE, 0,
-                         NULL, CREATE_ALWAYS, attrs, NULL);
+    handle = msi_create_file( data->package, path, GENERIC_READ | GENERIC_WRITE, 0, CREATE_ALWAYS, attrs );
     if (handle == INVALID_HANDLE_VALUE)
     {
         DWORD err = GetLastError();
-        DWORD attrs2 = GetFileAttributesW(path);
+        DWORD attrs2 = msi_get_file_attributes( data->package, path );
 
         if (attrs2 == INVALID_FILE_ATTRIBUTES)
         {
@@ -454,8 +453,8 @@ static INT_PTR cabinet_copy_file(FDINOTIFICATIONTYPE fdint,
         else if (err == ERROR_ACCESS_DENIED && (attrs2 & FILE_ATTRIBUTE_READONLY))
         {
             TRACE("removing read-only attribute on %s\n", debugstr_w(path));
-            SetFileAttributesW( path, attrs2 & ~FILE_ATTRIBUTE_READONLY );
-            handle = CreateFileW(path, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, attrs, NULL);
+            msi_set_file_attributes( data->package, path, attrs2 & ~FILE_ATTRIBUTE_READONLY );
+            handle = msi_create_file( data->package, path, GENERIC_READ | GENERIC_WRITE, 0, CREATE_ALWAYS, attrs );
 
             if (handle != INVALID_HANDLE_VALUE) goto done;
             err = GetLastError();
@@ -481,8 +480,8 @@ static INT_PTR cabinet_copy_file(FDINOTIFICATIONTYPE fdint,
             handle = CreateFileW(tmpfileW, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, attrs, NULL);
 
             if (handle != INVALID_HANDLE_VALUE &&
-                MoveFileExW(path, NULL, MOVEFILE_DELAY_UNTIL_REBOOT) &&
-                MoveFileExW(tmpfileW, path, MOVEFILE_DELAY_UNTIL_REBOOT))
+                msi_move_file( data->package, path, NULL, MOVEFILE_DELAY_UNTIL_REBOOT ) &&
+                msi_move_file( data->package, tmpfileW, path, MOVEFILE_DELAY_UNTIL_REBOOT ))
             {
                 data->package->need_reboot_at_end = 1;
             }

dlls/msi/msi.c
@@ -3298,7 +3298,7 @@ UINT WINAPI MsiGetFileVersionW( LPCWSTR path, LPWSTR verbuf, LPDWORD verlen,
     if (ret == ERROR_RESOURCE_DATA_NOT_FOUND && verlen)
     {
         int len;
-        WCHAR *version = msi_font_version_from_file( path );
+        WCHAR *version = msi_get_font_file_version( NULL, path );
         if (!version) return ERROR_FILE_INVALID;
         len = strlenW( version );
         if (len >= *verlen) ret = ERROR_MORE_DATA;
@@ -4107,14 +4107,17 @@ extern VOID WINAPI MD5Init( MD5_CTX *);
 extern VOID WINAPI MD5Update( MD5_CTX *, const unsigned char *, unsigned int );
 extern VOID WINAPI MD5Final( MD5_CTX *);
 
-UINT msi_get_filehash( const WCHAR *path, MSIFILEHASHINFO *hash )
+UINT msi_get_filehash( MSIPACKAGE *package, const WCHAR *path, MSIFILEHASHINFO *hash )
 {
     HANDLE handle, mapping;
     void *p;
     DWORD length;
     UINT r = ERROR_FUNCTION_FAILED;
 
-    handle = CreateFileW( path, GENERIC_READ, FILE_SHARE_READ|FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL );
+    if (package)
+        handle = msi_create_file( package, path, GENERIC_READ, FILE_SHARE_READ|FILE_SHARE_DELETE, OPEN_EXISTING, 0 );
+    else
+        handle = CreateFileW( path, GENERIC_READ, FILE_SHARE_READ|FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL );
     if (handle == INVALID_HANDLE_VALUE)
     {
         WARN("can't open file %u\n", GetLastError());
@@ -4171,7 +4174,7 @@ UINT WINAPI MsiGetFileHashW( LPCWSTR szFilePath, DWORD dwOptions,
     if (pHash->dwFileHashInfoSize < sizeof *pHash)
         return ERROR_INVALID_PARAMETER;
 
-    return msi_get_filehash( szFilePath, pHash );
+    return msi_get_filehash( NULL, szFilePath, pHash );
 }
 
 /***********************************************************************

commit a3dfd2f251f336f1cc570863edb60251227cdf5d

msi: Don't fail the BindImage action if the BindImage table is absent.
This fixes a regression introduced with
15f852015a0332313821804cc1fbad79216408c6 and observable with several
installers, including dotnet472.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=46885
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Hans Leidekker <hans@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/msi/action.c
@@ -7809,10 +7809,10 @@ static UINT ACTION_BindImage( MSIPACKAGE *package )
     r = MSI_DatabaseOpenViewW( package->db, query, &view );
     if (r == ERROR_SUCCESS)
     {
-        r = MSI_IterateRecords( view, NULL, ITERATE_BindImage, package );
+        MSI_IterateRecords( view, NULL, ITERATE_BindImage, package );
         msiobj_release( &view->hdr );
     }
-    return r;
+    return ERROR_SUCCESS;
 }
 
 static UINT msi_unimplemented_action_stub( MSIPACKAGE *package, LPCSTR action, LPCWSTR table )

dlls/msi/tests/install.c
@@ -135,6 +135,7 @@ static const CHAR install_exec_seq_dat[] = "Action\tCondition\tSequence\n"
                                            "ResolveSource\t\t950\n"
                                            "MoveFiles\t\t1700\n"
                                            "InstallFiles\t\t4000\n"
+                                           "BindImage\t\t4100\n"
                                            "DuplicateFiles\t\t4500\n"
                                            "WriteEnvironmentStrings\t\t4550\n"
                                            "CreateShortcuts\t\t4600\n"