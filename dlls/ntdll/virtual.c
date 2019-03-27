12be24af8cab0e5f78795b164ec8847bafc30852
Andrew Eikum <aeikum@codeweavers.com>
2019-03-27 11:33:25 -0500

    ntdll: Fix memory region check in NtQueryVirtualMemory.
    Patch by buggy@ifel.se.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=45218
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntdll/virtual.c
+++ dlls/ntdll/virtual.c
@@ -2756,7 +2756,7 @@ static int get_free_mem_state_callback( void *start, size_t size, void *arg )
     MEMORY_BASIC_INFORMATION *info = arg;
     void *end = (char *)start + size;
 
-    if ((char *)info->BaseAddress + info->RegionSize < (char *)start) return 0;
+    if ((char *)info->BaseAddress + info->RegionSize <= (char *)start) return 0;
 
     if (info->BaseAddress >= end)
     {
