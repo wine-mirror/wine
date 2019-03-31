905e7b0e2ef99c7ccfa5a3dda0844d5ea40afc77
Tim Schumacher <timschumi@gmx.de>
2019-03-31 13:02:27 +0200

    winejoystick: Add support for wheel axes.
    Wheel, gas, and brake are replacements for X, Y and Z in wheel-like
    joysticks. Remap them internally to send inputs as X, Y, and Z.
    
    Signed-off-by: Tim Schumacher <timschumi@gmx.de>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winejoystick.drv/joystick_linux.c
+++ dlls/winejoystick.drv/joystick_linux.c
@@ -270,10 +270,13 @@ LRESULT driver_joyGetDevCaps(DWORD_PTR dwDevID, LPJOYCAPSW lpCaps, DWORD dwSize)
 	    switch (jstck->axesMap[i]) {
 	    case 0: /* X */
 	    case 1: /* Y */
+	    case 8: /* Wheel */
+	    case 9: /* Gas */
 		lpCaps->wNumAxes++;
 		break;
 	    case 2: /* Z */
 	    case 6: /* Throttle */
+	    case 10: /* Brake */
 		lpCaps->wNumAxes++;
 		lpCaps->wCaps |= JOYCAPS_HASZ;
 		break;
@@ -322,13 +325,16 @@ LRESULT driver_joyGetPosEx(DWORD_PTR dwDevID, LPJOYINFOEX lpInfo)
 	if (ev.type == (JS_EVENT_AXIS)) {
 	    switch (jstck->axesMap[ev.number]) {
 	    case 0: /* X */
+	    case 8: /* Wheel */
 		jstck->x = ev.value;
 		break;
 	    case 1: /* Y */
+	    case 9: /* Gas */
 		jstck->y = ev.value;
 		break;
 	    case 2: /* Z */
 	    case 6: /* Throttle */
+	    case 10: /* Brake */
 		jstck->z = ev.value;
 		break;
 	    case 5: /* Rz */
