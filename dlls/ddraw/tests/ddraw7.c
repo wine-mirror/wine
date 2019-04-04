5fd14a4e6273089f3ecae6608240278ead563ae8
Zebediah Figura <z.figura12@gmail.com>
2019-04-04 18:37:58 -0500

    ddraw: Forbid applying a stateblock while recording.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ddraw/tests/ddraw7.c
+++ dlls/ddraw/tests/ddraw7.c
@@ -15866,7 +15866,7 @@ static void test_begin_end_state_block(void)
     ok(hr == D3DERR_INBEGINSTATEBLOCK, "Got unexpected hr %#x.\n", hr);
 
     hr = IDirect3DDevice7_ApplyStateBlock(device, stateblock);
-    todo_wine ok(hr == D3DERR_INBEGINSTATEBLOCK, "Got unexpected hr %#x.\n", hr);
+    ok(hr == D3DERR_INBEGINSTATEBLOCK, "Got unexpected hr %#x.\n", hr);
 
     hr = IDirect3DDevice7_CaptureStateBlock(device, stateblock);
     todo_wine ok(hr == D3DERR_INBEGINSTATEBLOCK, "Got unexpected hr %#x.\n", hr);
@@ -15886,7 +15886,7 @@ static void test_begin_end_state_block(void)
 
     hr = IDirect3DDevice7_GetRenderState(device, D3DRENDERSTATE_LIGHTING, &value);
     ok(hr == D3D_OK, "Got unexpected hr %#x.\n", hr);
-    todo_wine ok(value == TRUE, "Got unexpected value %#x.\n", value);
+    ok(value == TRUE, "Got unexpected value %#x.\n", value);
 
     refcount = IDirect3DDevice7_Release(device);
     ok(!refcount, "Device has %u references left.\n", refcount);
