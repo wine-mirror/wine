e407dae3c7d0041f3643ac47f88bab268fc0ead8
Zebediah Figura <z.figura12@gmail.com>
2019-04-18 23:01:25 -0500

    qedit/tests: Add some tests for IBaseFilter::FindPin() on the null renderer.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/qedit/tests/nullrenderer.c
+++ dlls/qedit/tests/nullrenderer.c
@@ -22,6 +22,8 @@
 #include "dshow.h"
 #include "wine/test.h"
 
+static const WCHAR sink_id[] = {'I','n',0};
+
 static IBaseFilter *create_null_renderer(void)
 {
     IBaseFilter *filter = NULL;
@@ -170,6 +172,34 @@ static void test_enum_pins(void)
     ok(!ref, "Got outstanding refcount %d.\n", ref);
 }
 
+static void test_find_pin(void)
+{
+    static const WCHAR input_pinW[] = {'i','n','p','u','t',' ','p','i','n',0};
+    IBaseFilter *filter = create_null_renderer();
+    IEnumPins *enum_pins;
+    IPin *pin, *pin2;
+    HRESULT hr;
+    ULONG ref;
+
+    hr = IBaseFilter_FindPin(filter, input_pinW, &pin);
+    ok(hr == VFW_E_NOT_FOUND, "Got hr %#x.\n", hr);
+
+    hr = IBaseFilter_EnumPins(filter, &enum_pins);
+    ok(hr == S_OK, "Got hr %#x.\n", hr);
+
+    hr = IBaseFilter_FindPin(filter, sink_id, &pin);
+    ok(hr == S_OK, "Got hr %#x.\n", hr);
+    hr = IEnumPins_Next(enum_pins, 1, &pin2, NULL);
+    ok(hr == S_OK, "Got hr %#x.\n", hr);
+    ok(pin2 == pin, "Expected pin %p, got %p.\n", pin, pin2);
+    IPin_Release(pin2);
+    IPin_Release(pin);
+
+    IEnumPins_Release(enum_pins);
+    ref = IBaseFilter_Release(filter);
+    ok(!ref, "Got outstanding refcount %d.\n", ref);
+}
+
 START_TEST(nullrenderer)
 {
     IBaseFilter *filter;
@@ -188,6 +218,7 @@ START_TEST(nullrenderer)
 
     test_interfaces();
     test_enum_pins();
+    test_find_pin();
 
     CoUninitialize();
 }
