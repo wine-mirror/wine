44be6a8294ea6b0a4349cbbbd33463aa511df9f2
Alexandre Julliard <julliard@winehq.org>
2019-05-29 09:10:01 +0200

    include/msvcrt: Add snprintf() compatibility macro.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/msvcrt/stdio.h
+++ include/msvcrt/stdio.h
@@ -298,6 +298,7 @@ static inline int unlink(const char* path) { return _unlink(path); }
 #define _UNLINK_DEFINED
 #endif
 static inline int vsnprintf(char *buffer, size_t size, const char *format, __ms_va_list args) { return _vsnprintf(buffer,size,format,args); }
+#define snprintf _snprintf
 
 static inline wint_t fgetwchar(void) { return _fgetwchar(); }
 static inline wint_t fputwchar(wint_t wc) { return _fputwchar(wc); }
