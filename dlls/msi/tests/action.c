3b621e6d6d0db956864907bc7960cdd20428b033
Alexandre Julliard <julliard@winehq.org>
2019-05-30 15:04:21 +0200

    msi/tests: Don't skip a 64-bit test on Wine.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/msi/tests/action.c
+++ dlls/msi/tests/action.c
@@ -5642,7 +5642,7 @@ static void test_register_typelib(void)
     }
 
     /* UnregisterTypeLibraries action fails in 64-bit Windows <= 7 */
-    if (sizeof(void *) == 8)
+    if (sizeof(void *) == 8 && strcmp( winetest_platform, "wine" ))
     {
         win_skip("broken on 64-bit Windows\n");
         return;
