ee0b66adc482cb43e8b0e53bc1bd9825a63b1996
Alexandre Julliard <julliard@winehq.org>
2019-06-03 10:32:47 +0200

    dpvoice: Build with msvcrt.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dpvoice/server.c
+++ dlls/dpvoice/server.c
@@ -17,7 +17,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
-#include "config.h"
 
 #include <stdarg.h>
 
@@ -28,7 +27,6 @@
 #include "winuser.h"
 #include "objbase.h"
 
-#include "wine/unicode.h"
 #include "wine/debug.h"
 
 #include "dvoice.h"
