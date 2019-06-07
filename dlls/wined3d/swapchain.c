0781596cb66f20befacf8c607ef19a0cc9edbb05
Henri Verbeet <hverbeet@codeweavers.com>
2019-06-07 17:22:27 +0430

    wined3d: Move the "tid" field from struct wined3d_context to struct wined3d_context_gl.
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/swapchain.c
+++ dlls/wined3d/swapchain.c
@@ -1126,7 +1126,7 @@ struct wined3d_context *swapchain_get_context(struct wined3d_swapchain *swapchai
 
     for (i = 0; i < swapchain->num_contexts; ++i)
     {
-        if (swapchain->context[i]->tid == tid)
+        if (wined3d_context_gl(swapchain->context[i])->tid == tid)
             return swapchain->context[i];
     }
 
