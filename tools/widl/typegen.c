wine-mirror/wine

commit 02976b655a5d9ad8ee25dbfb2051acb5b7431381

widl: Use IsSimpleRef only for explicitly declared arrays.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47285
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

tools/widl/typegen.c
@@ -1064,9 +1064,16 @@ static unsigned char get_parameter_fc( const var_t *var, int is_return, unsigned
             case TGT_UNION:
             case TGT_USER_TYPE:
             case TGT_RANGE:
+                *flags |= MustFree;
+                if (!is_in && is_out) server_size = type_memsize( ref );
+                break;
             case TGT_ARRAY:
-                *flags |= IsSimpleRef | MustFree;
-                *typestring_offset = ref->typestring_offset;
+                *flags |= MustFree;
+                if (!type_array_is_decl_as_ptr(ref))
+                {
+                    *flags |= IsSimpleRef;
+                    *typestring_offset = ref->typestring_offset;
+                }
                 if (!is_in && is_out) server_size = type_memsize( ref );
                 break;
             case TGT_STRING:

commit 0afbcee924d0a9a7461291ea389c5a78d3ed2227

widl: Mark unions, user types, and ranges as IsSimpleRef.
This fixes a regression introduced by 02976b655a5d9ad8ee25dbfb2051acb5b7431381.
That patch should only have affected arrays.

Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

tools/widl/typegen.c
@@ -1064,7 +1064,8 @@ static unsigned char get_parameter_fc( const var_t *var, int is_return, unsigned
             case TGT_UNION:
             case TGT_USER_TYPE:
             case TGT_RANGE:
-                *flags |= MustFree;
+                *flags |= MustFree | IsSimpleRef;
+                *typestring_offset = ref->typestring_offset;
                 if (!is_in && is_out) server_size = type_memsize( ref );
                 break;
             case TGT_ARRAY: