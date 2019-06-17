wine-mirror/wine

commit 8d018d8d1dadf9d731690567b9c98d5a9eae37db

usp10: Make sure metrics cache is used with glyph indices only.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=44410
Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/usp10/usp10.c
@@ -3416,35 +3416,41 @@ HRESULT WINAPI ScriptPlaceOpenType( HDC hdc, SCRIPT_CACHE *psc, SCRIPT_ANALYSIS 
     if (pABC) memset(pABC, 0, sizeof(ABC));
     for (i = 0; i < cGlyphs; i++)
     {
+        WORD glyph;
         ABC abc;
+
+        /* FIXME: set to more reasonable values */
+        pGoffset[i].du = pGoffset[i].dv = 0;
+
         if (pGlyphProps[i].sva.fZeroWidth)
         {
             abc.abcA = abc.abcB = abc.abcC = 0;
+            if (piAdvance) piAdvance[i] = 0;
+            continue;
         }
-        else if (!get_cache_glyph_widths(psc, pwGlyphs[i], &abc))
+
+        if (psa->fNoGlyphIndex)
         {
-            BOOL ret;
+            if (FAILED(hr = ScriptGetCMap(hdc, psc, &pwGlyphs[i], 1, 0, &glyph))) return hr;
+        }
+        else
+            glyph = pwGlyphs[i];
+
+        if (!get_cache_glyph_widths(psc, glyph, &abc))
+        {
             if (!hdc) return E_PENDING;
             if (get_cache_pitch_family(psc) & TMPF_TRUETYPE)
             {
-                if (psa->fNoGlyphIndex)
-                    ret = GetCharABCWidthsW(hdc, pwGlyphs[i], pwGlyphs[i], &abc);
-                else
-                    ret = GetCharABCWidthsI(hdc, 0, 1, (WORD *)&pwGlyphs[i], &abc);
-                if (!ret) return S_FALSE;
+                if (!GetCharABCWidthsI(hdc, glyph, 1, NULL, &abc)) return S_FALSE;
             }
             else
             {
                 INT width;
-                if (psa->fNoGlyphIndex)
-                    ret = GetCharWidth32W(hdc, pwGlyphs[i], pwGlyphs[i], &width);
-                else
-                    ret = GetCharWidthI(hdc, 0, 1, (WORD *)&pwGlyphs[i], &width);
-                if (!ret) return S_FALSE;
+                if (!GetCharWidthI(hdc, glyph, 1, NULL, &width)) return S_FALSE;
                 abc.abcB = width;
                 abc.abcA = abc.abcC = 0;
             }
-            set_cache_glyph_widths(psc, pwGlyphs[i], &abc);
+            set_cache_glyph_widths(psc, glyph, &abc);
         }
         if (pABC)
         {
@@ -3452,8 +3458,6 @@ HRESULT WINAPI ScriptPlaceOpenType( HDC hdc, SCRIPT_CACHE *psc, SCRIPT_ANALYSIS 
             pABC->abcB += abc.abcB;
             pABC->abcC += abc.abcC;
         }
-        /* FIXME: set to more reasonable values */
-        pGoffset[i].du = pGoffset[i].dv = 0;
         if (piAdvance) piAdvance[i] = abc.abcA + abc.abcB + abc.abcC;
     }
 

commit ac0c1d41d1773f5029cc73ae9bd4cfb2ef358b80

usp10: Fix advance width when glyph is missing.
This fixes a regression introduced by 8d018d8d1dadf9d731690567b9c98d5a9eae37db.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47327
Signed-off-by: Akihiro Sagawa <sagawa.aki@gmail.com>
Signed-off-by: Aric Stewart <aric@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/usp10/usp10.c
@@ -3434,9 +3434,27 @@ HRESULT WINAPI ScriptPlaceOpenType( HDC hdc, SCRIPT_CACHE *psc, SCRIPT_ANALYSIS 
             if (FAILED(hr = ScriptGetCMap(hdc, psc, &pwGlyphs[i], 1, 0, &glyph))) return hr;
         }
         else
+        {
+            hr = S_OK;
             glyph = pwGlyphs[i];
+        }
 
-        if (!get_cache_glyph_widths(psc, glyph, &abc))
+        if (hr == S_FALSE)
+        {
+            if (!hdc) return E_PENDING;
+            if (get_cache_pitch_family(psc) & TMPF_TRUETYPE)
+            {
+                if (!GetCharABCWidthsW(hdc, pwGlyphs[i], pwGlyphs[i], &abc)) return S_FALSE;
+            }
+            else
+            {
+                INT width;
+                if (!GetCharWidthW(hdc, pwGlyphs[i], pwGlyphs[i], &width)) return S_FALSE;
+                abc.abcB = width;
+                abc.abcA = abc.abcC = 0;
+            }
+        }
+        else if (!get_cache_glyph_widths(psc, glyph, &abc))
         {
             if (!hdc) return E_PENDING;
             if (get_cache_pitch_family(psc) & TMPF_TRUETYPE)

dlls/usp10/tests/usp10.c
@@ -2199,11 +2199,11 @@ static void test_ScriptPlace(HDC hdc)
             {
                 ok(offset[i].du == 0, "[%d] expected 0, got %d\n", i, offset[i].du);
                 ok(offset[i].dv == 0, "[%d] expected 0, got %d\n", i, offset[i].dv);
-                todo_wine ok(widths[i] > tm.tmAveCharWidth, "[%d] expected greater than %d, got %d\n",
+                ok(widths[i] > tm.tmAveCharWidth, "[%d] expected greater than %d, got %d\n",
                    i, tm.tmAveCharWidth, widths[i]);
                 total += widths[i];
             }
-            todo_wine ok(total == sz.cx, "expected %d, got %d\n", sz.cx, total);
+            ok(total == sz.cx, "expected %d, got %d\n", sz.cx, total);
         }
         else
             skip("Associated font is unavailable\n");