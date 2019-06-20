42c6ac27e88918ff33a62cfb72659dfde562b851
Alexandre Julliard <julliard@winehq.org>
2019-06-20 09:11:52 +0200

    shdocvw: Build with msvcrt.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/shdocvw/shdocvw_main.c
+++ dlls/shdocvw/shdocvw_main.c
@@ -20,12 +20,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "config.h"
-
 #include <stdarg.h>
 #include <stdio.h>
 
-#include "wine/unicode.h"
 #include "wine/debug.h"
 
 #include "shdocvw.h"
