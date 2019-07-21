b72d6d1d86fccae000241809a0da37269c9d5632
Peter Dons Tychsen <pdt@dontech.dk>
2019-07-21 23:38:02 +0200

    msvcrt: Do not use casting in min/max constants.
    Signed-off-by: Peter Dons Tychsen <pdt@dontech.dk>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/msvcrt/limits.h
+++ include/msvcrt/limits.h
@@ -30,13 +30,13 @@
 #define LONG_MAX    0x7fffffffL
 #define ULONG_MAX   0xffffffffUL
 
-#define LLONG_MAX   (((__int64)0x7fffffff << 32) | 0xffffffff)
+#define LLONG_MAX   0x7fffffffffffffffLL
 #define LLONG_MIN   (-LLONG_MAX-1)
-#define ULLONG_MAX  (((unsigned __int64)0xffffffff << 32) | 0xffffffff)
+#define ULLONG_MAX  0xffffffffffffffffULL
 
-#define _I64_MAX    (((__int64)0x7fffffff << 32) | 0xffffffff)
+#define _I64_MAX    0x7fffffffffffffffLL
 #define _I64_MIN    (-_I64_MAX-1)
-#define _UI64_MAX   (((unsigned __int64)0xffffffff << 32) | 0xffffffff)
+#define _UI64_MAX   0xffffffffffffffffULL
 
 #define I64_MIN  _I64_MIN
 #define I64_MAX  _I64_MAX
