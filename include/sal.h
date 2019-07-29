889050b1a3a35728f33d0832f324aa1aa22fc9a8
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2019-07-29 23:07:38 +0000

    include: Add more defines in sal.h.
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/sal.h
+++ include/sal.h
@@ -22,25 +22,34 @@
 
 #define _Always_(exp)
 
+#define _At_
 #define _At_buffer_(a, b, c, d)
 
 #define _Check_return_
 #define _Check_return_opt_
 
+#define _COM_Outptr_
+#define _COM_Outptr_result_maybenull_
+
 #define _Deref_out_
 #define _Deref_out_z_
 #define _Deref_out_opt_
 #define _Deref_out_opt_z_
+#define _Deref_post_maybenull_
 #define _Deref_post_opt_cap_(count)
 #define _Deref_post_opt_valid_
 #define _Deref_post_opt_z_cap_(count)
 #define _Deref_post_valid_
 #define _Deref_post_z_
+#define _Deref_pre_maybenull_
+#define _Deref_pre_z_
 #define _Deref_prepost_opt_z_
 #define _Deref_ret_z_
 
 #define _Field_range_(min, max)
 
+#define __in
+#define __in_opt
 #define _In_
 #define _In_bytecount_(count)
 #define _In_bytecount_c_(count)
@@ -66,6 +75,7 @@
 #define _In_reads_(count)
 #define _In_reads_bytes_(count)
 #define _In_reads_bytes_opt_(count)
+#define _In_reads_opt_(count)
 #define _In_reads_opt_z_(count)
 #define _In_reads_or_z_(count)
 #define _In_reads_or_z_opt_(count)
@@ -76,6 +86,7 @@
 #define _In_z_count_(count)
 #define _In_z_count_c_(count)
 
+#define __inout
 #define _Inout_
 #define _Inout_bytecap_x_(count)
 #define _Inout_cap_(count)
@@ -85,6 +96,7 @@
 #define _Inout_opt_z_bytecap_(count)
 #define _Inout_updates_(count)
 #define _Inout_updates_bytes_(count)
+#define _Inout_updates_bytes_all_opt_(count)
 #define _Inout_updates_opt_(count)
 #define _Inout_updates_z_(count)
 #define _Inout_updates_opt_z_(count)
@@ -95,7 +107,10 @@
 
 #define _Null_terminated_
 
+#define __out
+#define __out_opt
 #define _Out_
+#define _Outptr_
 #define _Out_bytecap_(count)
 #define _Out_bytecap_c_(count)
 #define _Out_bytecap_x_(count)
@@ -129,6 +144,7 @@
 #define _Out_opt_z_cap_post_count_(count1, count2)
 #define _Out_ptrdiff_cap_(count)
 #define _Out_writes_(count)
+#define _Out_writes_all_(count)
 #define _Out_writes_bytes_(count)
 #define _Out_writes_bytes_all_(count)
 #define _Out_writes_bytes_all_opt_(count)
@@ -150,10 +166,12 @@
 #define _Out_z_cap_x_(count)
 #define _Out_z_cap_post_count_(count1, count2)
 
+#define _Outptr_opt_
 #define _Outptr_result_buffer_(count)
 #define _Outptr_result_buffer_maybenull_(count)
 #define _Outptr_result_maybenull_
 #define _Outptr_result_maybenull_z_
+#define _Outptr_result_nullonfailure_
 #define _Outptr_result_z_
 
 #define _Param_(p)
@@ -164,21 +182,30 @@
 #define _Post_invalid_
 #define _Post_maybez_
 #define _Post_ptr_invalid_
+#define _Post_readable_byte_size_(count)
 #define _Post_readable_size_(count)
+#define _Post_same_lock_(exp, exp1)
 #define _Post_satisfies_(exp)
 #define _Post_writable_byte_size_(count)
+#define _Post_writable_size_(count)
 #define _Post_z_
 
 #define _Pre_cap_for_(count)
 #define _Pre_maybenull_
 #define _Pre_notnull_
+#define _Pre_null_
 #define _Pre_opt_z_
+#define _Pre_valid_
 #define _Pre_writable_size_(count)
 #define _Pre_z_
 #define _Prepost_z_
 
 #define _Printf_format_string_
 #define _Printf_format_string_params_(count)
+
+#define _Reserved_
+
+#define _Result_nullonfailure_
 
 #define _Ret_maybenull_
 #define _Ret_maybenull_z_
@@ -187,8 +214,11 @@
 #define _Ret_opt_bytecap_(count)
 #define _Ret_opt_bytecap_x_(count)
 #define _Ret_opt_z_cap_(count)
+#define _Ret_range_(exp,exp2)
 #define _Ret_writes_bytes_maybenull_(count)
 #define _Ret_z_
+
+#define _Return_type_success_(count)
 
 #define _Scanf_format_string_
 #define _Scanf_format_string_params_(count)
