826aae15a2f689e53617e5bb1d9e6647715c69db
Francois Gouget <fgouget@free.fr>
2019-08-08 15:24:18 +0200

    server: A spelling and slight rewording fix in a comment.
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- server/async.c
+++ server/async.c
@@ -50,7 +50,7 @@ struct async
     struct iosb         *iosb;            /* I/O status block */
     obj_handle_t         wait_handle;     /* pre-allocated wait handle */
     unsigned int         signaled :1;
-    unsigned int         pending :1;      /* request is succesfully queued, but pending */
+    unsigned int         pending :1;      /* request successfully queued, but pending */
     unsigned int         direct_result :1;/* a flag if we're passing result directly from request instead of APC  */
     struct completion   *completion;      /* completion associated with fd */
     apc_param_t          comp_key;        /* completion key associated with fd */
