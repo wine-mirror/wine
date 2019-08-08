66873d5fec583b11582a8c7a21985214a436a586
Francois Gouget <fgouget@free.fr>
2019-08-08 15:21:54 +0200

    winex11.drv: A spelling fix and slight rewording in a comment.
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winex11.drv/display.c
+++ dlls/winex11.drv/display.c
@@ -233,7 +233,7 @@ static BOOL X11DRV_InitAdapter(HKEY video_hkey, INT video_index, INT gpu_index, 
     RegCreateKeyExW(HKEY_CURRENT_CONFIG, adapter_keyW, 0, NULL, REG_OPTION_VOLATILE, KEY_WRITE, NULL, &hkey, NULL);
 
     /* Write GPU instance path so that we can find the GPU instance via adapters quickly. Another way is trying to match
-     * them via the GUID in Device Paramters/VideoID, but it would required enumrating all GPU instances */
+     * them via the GUID in Device Parameters/VideoID, but it would require enumerating all GPU instances */
     sprintfW(bufferW, gpu_instance_fmtW, gpu->vendor_id, gpu->device_id, gpu->subsys_id, gpu->revision_id, gpu_index);
     if (RegSetValueExW(hkey, gpu_idW, 0, REG_SZ, (const BYTE *)bufferW, (strlenW(bufferW) + 1) * sizeof(WCHAR)))
         goto done;
