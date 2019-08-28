3bdadd4ab487367245dd61f402107086d3cf6030
Zhiyi Zhang <zzhang@codeweavers.com>
2019-08-28 21:14:28 +0430

    wined3d: Truncate stencil reference values to the stencil component size.
    As opposed to OpenGL, which clamps them. This fixes a regression introduced by
    commit a9e199e5ad76c0540381f25011d6051ace3e7c0a.
    
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 124f48d3114fe86f5feda2ce200d87b97c4009de)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/wined3d/cs.c
+++ dlls/wined3d/cs.c
@@ -1155,9 +1155,12 @@ static void wined3d_cs_exec_set_depth_stencil_view(struct wined3d_cs *cs, const 
         device_invalidate_state(device, STATE_RENDER(WINED3D_RS_STENCILWRITEMASK));
         device_invalidate_state(device, STATE_RENDER(WINED3D_RS_DEPTHBIAS));
     }
-    else if (prev && prev->format->depth_bias_scale != op->view->format->depth_bias_scale)
+    else if (prev)
     {
-        device_invalidate_state(device, STATE_RENDER(WINED3D_RS_DEPTHBIAS));
+        if (prev->format->depth_bias_scale != op->view->format->depth_bias_scale)
+            device_invalidate_state(device, STATE_RENDER(WINED3D_RS_DEPTHBIAS));
+        if (prev->format->stencil_size != op->view->format->stencil_size)
+            device_invalidate_state(device, STATE_RENDER(WINED3D_RS_STENCILREF));
     }
 
     device_invalidate_state(device, STATE_FRAMEBUFFER);
