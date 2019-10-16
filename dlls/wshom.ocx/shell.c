46dcaa5b93f264a75b0c924eb72d86574b6fdd64
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2019-10-16 10:06:11 +0000

    wshom.ocx: Correct out parameter for _NewEnum.
    Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wshom.ocx/shell.c
+++ dlls/wshom.ocx/shell.c
@@ -698,7 +698,7 @@ static HRESULT WINAPI WshCollection_get_length(IWshCollection *iface, LONG *coun
     return E_NOTIMPL;
 }
 
-static HRESULT WINAPI WshCollection__NewEnum(IWshCollection *iface, IUnknown *Enum)
+static HRESULT WINAPI WshCollection__NewEnum(IWshCollection *iface, IUnknown **Enum)
 {
     WshCollection *This = impl_from_IWshCollection(iface);
     FIXME("(%p)->(%p): stub\n", This, Enum);
