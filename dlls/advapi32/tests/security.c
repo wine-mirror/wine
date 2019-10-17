70bb6551553345ace6ab329a513d9d97492f0591
Gijs Vermeulen <gijsvrm@codeweavers.com>
2019-10-17 13:47:30 +0200

    advapi32/tests: Fix test_process_security with non-English locale.
    Signed-off-by: Gijs Vermeulen <gijsvrm@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/advapi32/tests/security.c
+++ dlls/advapi32/tests/security.c
@@ -3004,7 +3004,10 @@ static void test_process_security(void)
     ret = LookupAccountSidA( NULL, UsersSid, account, &acc_size, domain, &dom_size, &use );
     ok(ret, "LookupAccountSid failed with %d\n", ret);
     todo_wine ok(use == SidTypeGroup, "expect SidTypeGroup, got %d\n", use);
-    todo_wine ok(!strcmp(account, "None"), "expect None, got %s\n", account);
+    if (PRIMARYLANGID(GetSystemDefaultLangID()) != LANG_ENGLISH)
+        skip("Non-English locale (test with hardcoded 'None')\n");
+    else
+        todo_wine ok(!strcmp(account, "None"), "expect None, got %s\n", account);
 
     res = GetTokenInformation( token, TokenUser, NULL, 0, &size );
     ok(!res, "Expected failure, got %d\n", res);
