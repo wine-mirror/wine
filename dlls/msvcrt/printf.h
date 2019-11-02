97557ae168109492e73b5d47b005a3e6c466a954
Piotr Caban <piotr@codeweavers.com>
2019-11-02 14:39:28 +0100

    msvcrt: Enable printf %z, %t and %j formats.
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/msvcrt/printf.h
+++ dlls/msvcrt/printf.h
@@ -515,15 +515,17 @@ int FUNC_NAME(pf_printf)(FUNC_NAME(puts_clbk) pf_puts, void *puts_ctx, const API
                     break;
             } else if(*p == 'w')
                 flags.WideString = *p++;
-#if _MSVCR_VER >= 140
-            else if(*p == 'z' || *p == 't')
+#if _MSVCR_VER == 0 || _MSVCR_VER >= 140
+            else if((*p == 'z' || *p == 't') && p[1] && strchr("diouxX", p[1]))
                 flags.IntegerNative = *p++;
-            else if(*p == 'T')
-                flags.NaturalString = *p++;
             else if(*p == 'j') {
                 flags.IntegerDouble++;
                 p++;
             }
+#endif
+#if _MSVCR_VER >= 140
+            else if(*p == 'T')
+                flags.NaturalString = *p++;
 #endif
             else if((*p == 'F' || *p == 'N') && legacy_msvcrt_compat)
                 p++; /* ignore */
