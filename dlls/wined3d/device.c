wine-mirror/wine

commit 96547e622b04f403bc0c78d649275d1f844ed40e

wined3d: Reset the CS state before cleaning up the device state.
Otherwise the CS might later try to unbind a resource that has already been
freed.

Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/device.c
@@ -1140,6 +1140,7 @@ void wined3d_device_uninit_3d(struct wined3d_device *device)
         wined3d_texture_decref(texture);
     }
 
+    wined3d_cs_emit_reset_state(device->cs);
     state_cleanup(&device->state);
     for (i = 0; i < device->adapter->d3d_info.limits.max_rt_count; ++i)
     {

commit 7b1581a7841a94bb4f4aa27bfa6125d749afa433

wined3d: Also reset the device state to the default in wined3d_device_uninit_3d().
Fixes a regression introduced by 96547e622b04f403bc0c78d649275d1f844ed40e.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=48072
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/device.c
@@ -1142,6 +1142,8 @@ void wined3d_device_uninit_3d(struct wined3d_device *device)
 
     wined3d_cs_emit_reset_state(device->cs);
     state_cleanup(&device->state);
+    memset(&device->state, 0, sizeof(device->state));
+    state_init(&device->state, &device->fb, &device->adapter->d3d_info, WINED3D_STATE_INIT_DEFAULT);
     for (i = 0; i < device->adapter->d3d_info.limits.max_rt_count; ++i)
     {
         wined3d_device_set_rendertarget_view(device, i, NULL, FALSE);

dlls/ddraw/tests/ddraw2.c
@@ -1012,7 +1012,7 @@ static void test_coop_level_d3d_state(void)
     hr = IDirect3DDevice2_EndScene(device);
     ok(hr == DD_OK, "Got unexpected hr %#x.\n", hr);
     color = get_surface_color(rt, 320, 240);
-    todo_wine ok(compare_color(color, 0x0000ff80, 1), "Got unexpected color 0x%08x.\n", color);
+    ok(compare_color(color, 0x0000ff80, 1), "Got unexpected color 0x%08x.\n", color);
 
     destroy_viewport(device, viewport);
     destroy_material(background);

dlls/ddraw/tests/ddraw4.c
@@ -1197,7 +1197,7 @@ static void test_coop_level_d3d_state(void)
     hr = IDirect3DDevice3_EndScene(device);
     ok(hr == DD_OK, "Got unexpected hr %#x.\n", hr);
     color = get_surface_color(rt, 320, 240);
-    todo_wine ok(compare_color(color, 0x0000ff80, 1), "Got unexpected color 0x%08x.\n", color);
+    ok(compare_color(color, 0x0000ff80, 1), "Got unexpected color 0x%08x.\n", color);
 
     destroy_viewport(device, viewport);
     IDirectDrawSurface4_Release(surface);

dlls/ddraw/tests/ddraw7.c
@@ -1099,7 +1099,7 @@ static void test_coop_level_d3d_state(void)
     hr = IDirect3DDevice7_EndScene(device);
     ok(hr == DD_OK, "Got unexpected hr %#x.\n", hr);
     color = get_surface_color(rt, 320, 240);
-    todo_wine ok(compare_color(color, 0x0000ff80, 1), "Got unexpected color 0x%08x.\n", color);
+    ok(compare_color(color, 0x0000ff80, 1), "Got unexpected color 0x%08x.\n", color);
 
     IDirectDrawSurface7_Release(surface);
     IDirectDrawSurface7_Release(rt);