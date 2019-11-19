aed088df11990ee3bbe7a7c14dded16a9a5f2c2e
Hans Leidekker <hans@codeweavers.com>
2019-11-19 14:45:00 +0100

    dhcpcsvc: Implement DhcpRequestParams.
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dhcpcsvc/dhcpcsvc.c
+++ dlls/dhcpcsvc/dhcpcsvc.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2011 Stefan Leichter
+ * Copyright 2019 Hans Leidekker for CodeWeavers
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,7 +21,12 @@
 #include "windef.h"
 #include "winbase.h"
 #include "dhcpcsdk.h"
+#include "winioctl.h"
+#define WINE_MOUNTMGR_EXTENSIONS
+#include "ddk/mountmgr.h"
+
 #include "wine/debug.h"
+#include "wine/heap.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(dhcpcsvc);
 
@@ -51,11 +57,71 @@ DWORD WINAPI DhcpCApiInitialize(LPDWORD version)
     return ERROR_SUCCESS;
 }
 
-DWORD WINAPI DhcpRequestParams( DWORD flags, void *reserved, WCHAR *adaptername, DHCPCAPI_CLASSID *classid,
-                                DHCPCAPI_PARAMS_ARRAY sendparams, DHCPCAPI_PARAMS_ARRAY recdparams,
-                                BYTE *buffer, DWORD *size, WCHAR *requestidstr )
+DWORD WINAPI DhcpRequestParams( DWORD flags, void *reserved, WCHAR *adapter, DHCPCAPI_CLASSID *class_id,
+                                DHCPCAPI_PARAMS_ARRAY send_params, DHCPCAPI_PARAMS_ARRAY recv_params, BYTE *buf,
+                                DWORD *buflen, WCHAR *request_id )
 {
-    FIXME("(%08x, %p, %s, %p, %u, %u, %p, %p, %s): stub\n", flags, reserved, debugstr_w(adaptername), classid,
-            sendparams.nParams, recdparams.nParams, buffer, size, debugstr_w(requestidstr));
-    return ERROR_SUCCESS;
+    struct mountmgr_dhcp_request_params *query;
+    DWORD i, size, err = ERROR_OUTOFMEMORY;
+    BYTE *src, *dst;
+    HANDLE mgr;
+
+    TRACE( "(%08x, %p, %s, %p, %u, %u, %p, %p, %s)\n", flags, reserved, debugstr_w(adapter), class_id,
+           send_params.nParams, recv_params.nParams, buf, buflen, debugstr_w(request_id) );
+
+    if (!adapter || lstrlenW(adapter) > IF_MAX_STRING_SIZE || !buflen) return ERROR_INVALID_PARAMETER;
+    if (flags != DHCPCAPI_REQUEST_SYNCHRONOUS) FIXME( "unsupported flags %08x\n", flags );
+
+    for (i = 0; i < send_params.nParams; i++)
+        FIXME( "send option %u not supported\n", send_params.Params->OptionId );
+
+    mgr = CreateFileW( MOUNTMGR_DOS_DEVICE_NAME, GENERIC_READ|GENERIC_WRITE, FILE_SHARE_READ|FILE_SHARE_WRITE, NULL,
+                       OPEN_EXISTING, 0, 0 );
+    if (mgr == INVALID_HANDLE_VALUE) return GetLastError();
+
+    size = FIELD_OFFSET(struct mountmgr_dhcp_request_params, params[recv_params.nParams]) + *buflen;
+    if (!(query = heap_alloc_zero( size ))) goto done;
+
+    for (i = 0; i < recv_params.nParams; i++) query->params[i].id = recv_params.Params[i].OptionId;
+    query->count = recv_params.nParams;
+    lstrcpyW( query->adapter, adapter );
+
+    if (!DeviceIoControl( mgr, IOCTL_MOUNTMGR_QUERY_DHCP_REQUEST_PARAMS, query, size, query, size, NULL, NULL ))
+    {
+        err = GetLastError();
+        if (err == ERROR_MORE_DATA) *buflen = query->size - (size - *buflen);
+        goto done;
+    }
+
+    dst = buf;
+    for (i = 0; i < query->count; i++)
+    {
+        if (buf)
+        {
+            recv_params.Params[i].OptionId = query->params[i].id;
+            recv_params.Params[i].IsVendor = FALSE; /* FIXME */
+            if (query->params[i].size)
+            {
+                src = (BYTE *)query + query->params[i].offset;
+                memcpy( dst, src, query->params[i].size );
+
+                recv_params.Params[i].Data = dst;
+                recv_params.Params[i].nBytesData = query->params[i].size;
+            }
+            else
+            {
+                recv_params.Params[i].Data = NULL;
+                recv_params.Params[i].nBytesData = 0;
+            }
+        }
+        dst += query->params[i].size;
+    }
+
+    *buflen = dst - buf;
+    err = ERROR_SUCCESS;
+
+done:
+    heap_free( query );
+    CloseHandle( mgr );
+    return err;
 }
