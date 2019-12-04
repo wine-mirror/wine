3f53d75362a9a7c82555729c3cc10e75c171b111
Zebediah Figura <z.figura12@gmail.com>
2019-12-03 22:23:25 -0600

    d3d8: Update the primary stateblock in d3d8_device_MultiplyTransform().
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/device.c
+++ dlls/d3d8/device.c
@@ -1662,6 +1662,7 @@ static HRESULT WINAPI d3d8_device_MultiplyTransform(IDirect3DDevice8 *iface,
 
     /* Note: D3DMATRIX is compatible with struct wined3d_matrix. */
     wined3d_mutex_lock();
+    wined3d_stateblock_multiply_transform(device->state, state, (const struct wined3d_matrix *)matrix);
     wined3d_device_multiply_transform(device->wined3d_device, state, (const struct wined3d_matrix *)matrix);
     wined3d_mutex_unlock();
 
