c7019a8887a3b035ce6b1ff6b113a8b5c0cc5e04
Zebediah Figura <z.figura12@gmail.com>
2020-01-27 11:06:43 -0600

    kernelbase: Implement ReOpenFile().
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=47668
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/kernel32/tests/file.c
+++ dlls/kernel32/tests/file.c
@@ -59,6 +59,7 @@ static BOOL (WINAPI *pRtlDosPathNameToNtPathName_U)(LPCWSTR, PUNICODE_STRING, PW
 static NTSTATUS (WINAPI *pRtlAnsiStringToUnicodeString)(PUNICODE_STRING, PCANSI_STRING, BOOLEAN);
 static BOOL (WINAPI *pSetFileInformationByHandle)(HANDLE, FILE_INFO_BY_HANDLE_CLASS, void*, DWORD);
 static BOOL (WINAPI *pGetQueuedCompletionStatusEx)(HANDLE, OVERLAPPED_ENTRY*, ULONG, ULONG*, DWORD, BOOL);
+static HANDLE (WINAPI *pReOpenFile)(HANDLE, DWORD, DWORD, DWORD);
 static void (WINAPI *pRtlInitAnsiString)(PANSI_STRING,PCSZ);
 static void (WINAPI *pRtlFreeUnicodeString)(PUNICODE_STRING);
 static BOOL (WINAPI *pSetFileCompletionNotificationModes)(HANDLE, UCHAR);
@@ -111,6 +112,7 @@ static void InitFunctionPointers(void)
     pGetFinalPathNameByHandleW = (void *) GetProcAddress(hkernel32, "GetFinalPathNameByHandleW");
     pSetFileInformationByHandle = (void *) GetProcAddress(hkernel32, "SetFileInformationByHandle");
     pGetQueuedCompletionStatusEx = (void *) GetProcAddress(hkernel32, "GetQueuedCompletionStatusEx");
+    pReOpenFile = (void *) GetProcAddress(hkernel32, "ReOpenFile");
     pSetFileCompletionNotificationModes = (void *)GetProcAddress(hkernel32, "SetFileCompletionNotificationModes");
     pFindFirstStreamW = (void *)GetProcAddress(hkernel32, "FindFirstStreamW");
 }
@@ -4410,6 +4412,104 @@ static void test_SetFileValidData(void)
     DeleteFileA(filename);
 }
 
+static void test_ReOpenFile(void)
+{
+    char path[MAX_PATH], filename[MAX_PATH], buffer[4];
+    HANDLE file, new;
+    unsigned int i;
+    DWORD size;
+    BOOL ret;
+
+    static const DWORD invalid_attributes[] =
+    {
+        FILE_ATTRIBUTE_ARCHIVE,
+        FILE_ATTRIBUTE_ENCRYPTED,
+        FILE_ATTRIBUTE_HIDDEN,
+        FILE_ATTRIBUTE_NORMAL,
+        FILE_ATTRIBUTE_OFFLINE,
+        FILE_ATTRIBUTE_READONLY,
+        FILE_ATTRIBUTE_SYSTEM,
+        FILE_ATTRIBUTE_TEMPORARY,
+    };
+
+    static const DWORD valid_attributes[] =
+    {
+        FILE_FLAG_BACKUP_SEMANTICS,
+        FILE_FLAG_NO_BUFFERING,
+        FILE_FLAG_OVERLAPPED,
+        FILE_FLAG_RANDOM_ACCESS,
+        FILE_FLAG_SEQUENTIAL_SCAN,
+        FILE_FLAG_WRITE_THROUGH,
+    };
+
+    if (!pReOpenFile)
+    {
+        win_skip("ReOpenFile() is not available\n");
+        return;
+    }
+
+    GetTempPathA(sizeof(path), path);
+    GetTempFileNameA(path, "tst", 0, filename);
+
+    file = CreateFileA(filename, GENERIC_WRITE, FILE_SHARE_READ, NULL,
+                       CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0);
+    ok(file != INVALID_HANDLE_VALUE, "failed to create file, error %u\n", GetLastError());
+    ret = WriteFile(file, "foo", 4, &size, NULL);
+    ok(ret, "failed to write file, error %u\n", GetLastError());
+
+    for (i = 0; i < ARRAY_SIZE(invalid_attributes); ++i)
+    {
+        SetLastError(0xdeadbeef);
+        new = pReOpenFile(file, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, invalid_attributes[i]);
+        ok(new == INVALID_HANDLE_VALUE, "got %p\n", new);
+        ok(GetLastError() == ERROR_INVALID_PARAMETER, "got error %u\n", GetLastError());
+    }
+
+    new = pReOpenFile(file, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, 0);
+    ok(new != INVALID_HANDLE_VALUE, "got error %u\n", GetLastError());
+
+    ret = ReadFile(new, buffer, sizeof(buffer), &size, NULL);
+    ok(ret, "failed to read file, error %u\n", GetLastError());
+    ok(size == 4, "got size %u\n", size);
+    ok(!strcmp(buffer, "foo"), "got wrong data\n");
+    CloseHandle(new);
+
+    for (i = 0; i < ARRAY_SIZE(valid_attributes); ++i)
+    {
+        new = pReOpenFile(file, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, valid_attributes[i]);
+        ok(new != INVALID_HANDLE_VALUE, "got error %u\n", GetLastError());
+        CloseHandle(new);
+    }
+
+    SetLastError(0xdeadbeef);
+    new = pReOpenFile(file, GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0);
+    ok(new == INVALID_HANDLE_VALUE, "got %p\n", new);
+    ok(GetLastError() == ERROR_SHARING_VIOLATION, "got error %u\n", GetLastError());
+
+    CloseHandle(file);
+    ret = DeleteFileA(filename);
+    ok(ret, "failed to delete file, error %u\n", GetLastError());
+
+    file = CreateNamedPipeA("\\\\.\\pipe\\test_pipe", PIPE_ACCESS_DUPLEX, 0, 1, 1000, 1000, 1000, NULL);
+    ok(file != INVALID_HANDLE_VALUE, "failed to create pipe, error %u\n", GetLastError());
+
+    new = pReOpenFile(file, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE, 0);
+    todo_wine ok(new != INVALID_HANDLE_VALUE, "got error %u\n", GetLastError());
+
+    ret = WriteFile(file, "foo", 4, &size, NULL);
+    todo_wine ok(ret, "failed to write file, error %u\n", GetLastError());
+    ret = ReadFile(new, buffer, sizeof(buffer), &size, NULL);
+    todo_wine ok(ret, "failed to read file, error %u\n", GetLastError());
+    if (ret)
+    {
+        ok(size == 4, "got size %u\n", size);
+        ok(!strcmp(buffer, "foo"), "got wrong data\n");
+    }
+
+    CloseHandle(new);
+    CloseHandle(file);
+}
+
 static void test_WriteFileGather(void)
 {
     char temp_path[MAX_PATH], filename[MAX_PATH];
@@ -5483,4 +5583,5 @@ START_TEST(file)
     test_file_readonly_access();
     test_find_file_stream();
     test_SetFileTime();
+    test_ReOpenFile();
 }
