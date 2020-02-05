9b35837e3759213a1841b71c6089efcd4596e42e
Zhiyi Zhang <zzhang@codeweavers.com>
2020-02-05 15:28:09 +0800

    wined3d: Add GPU information for NVIDIA GeForce GTX 1650 SUPER.
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit c8aad3407c67536d70cbb802bb4f871d56eb500e)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/wined3d/adapter_gl.c
+++ dlls/wined3d/adapter_gl.c
@@ -1359,6 +1359,7 @@ cards_nvidia_binary[] =
     {"RTX 2070",                    CARD_NVIDIA_GEFORCE_RTX2070},   /* GeForce 2000 - highend */
     {"RTX 2060",                    CARD_NVIDIA_GEFORCE_RTX2060},   /* GeForce 2000 - highend */
     {"GTX 1660 Ti",                 CARD_NVIDIA_GEFORCE_GTX1660TI}, /* GeForce 1600 - highend */
+    {"GTX 1650 SUPER",              CARD_NVIDIA_GEFORCE_GTX1650SUPER}, /* GeForce 1600 - midend high */
     {"TITAN V",                     CARD_NVIDIA_TITANV},            /* GeForce 1000 - highend */
     {"TITAN X (Pascal)",            CARD_NVIDIA_TITANX_PASCAL},     /* GeForce 1000 - highend */
     {"GTX 1080 Ti",                 CARD_NVIDIA_GEFORCE_GTX1080TI}, /* GeForce 1000 - highend */
