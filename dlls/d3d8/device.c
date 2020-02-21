84f9bc17161f6c2bf1be7516ca206da94b225f1c
Zebediah Figura <z.figura12@gmail.com>
2020-02-21 17:04:46 -0600

    d3d8: Stop setting the device state in d3d8_device_SetPixelShaderConstant().
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3d8/device.c
+++ dlls/d3d8/device.c
@@ -3290,8 +3290,6 @@ static HRESULT WINAPI d3d8_device_SetPixelShaderConstant(IDirect3DDevice8 *iface
 
     wined3d_mutex_lock();
     hr = wined3d_stateblock_set_ps_consts_f(device->update_state, start_register, count, data);
-    if (SUCCEEDED(hr) && !device->recording)
-        hr = wined3d_device_set_ps_consts_f(device->wined3d_device, start_register, count, data);
     wined3d_mutex_unlock();
 
     return hr;
