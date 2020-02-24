wine-mirror/wine

commit 5ca9da2c0197ac1260975c1b7b643dbd11185715

gdiplus: Make GdipGetNearestColor print FIXME only if indexed bitmap is associated with the graphics object.
Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
Signed-off-by: Vincent Povirk <vincent@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/gdiplus/graphics.c
@@ -4775,13 +4775,20 @@ GpStatus WINGDIPAPI GdipGetInterpolationMode(GpGraphics *graphics,
 /* FIXME: Need to handle color depths less than 24bpp */
 GpStatus WINGDIPAPI GdipGetNearestColor(GpGraphics *graphics, ARGB* argb)
 {
-    FIXME("(%p, %p): Passing color unmodified\n", graphics, argb);
+    TRACE("(%p, %p)\n", graphics, argb);
 
     if(!graphics || !argb)
         return InvalidParameter;
 
     if(graphics->busy)
         return ObjectBusy;
+
+    if (graphics->image->type == ImageTypeBitmap)
+    {
+        GpBitmap *bitmap = (GpBitmap *)graphics->image;
+        if (IsIndexedPixelFormat(bitmap->format))
+            FIXME("(%p, %p): Passing color unmodified\n", graphics, argb);
+    }
 
     return Ok;
 }

commit 83e95a609c8ee598ddd8f1054974689e47e5168b

gdiplus: Fix a possible NULL pointer reference in GdipGetNearestColor().
Fix a regression from 5ca9da2c0197ac1260975c1b7b643dbd11185715.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=48657
Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
Signed-off-by: Vincent Povirk <vincent@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/gdiplus/graphics.c
@@ -4783,7 +4783,7 @@ GpStatus WINGDIPAPI GdipGetNearestColor(GpGraphics *graphics, ARGB* argb)
     if(graphics->busy)
         return ObjectBusy;
 
-    if (graphics->image->type == ImageTypeBitmap)
+    if (graphics->image && graphics->image->type == ImageTypeBitmap)
     {
         static int once;
         GpBitmap *bitmap = (GpBitmap *)graphics->image;