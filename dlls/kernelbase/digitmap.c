b83af7c763b40280650990d4ca6385ad35143b00
Alexandre Julliard <julliard@winehq.org>
2020-03-13 11:42:31 +0100

    unicode: Update data tables to Unicode 13.0.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/kernelbase/digitmap.c
+++ dlls/kernelbase/digitmap.c
@@ -1,5 +1,5 @@
 /* Unicode digit folding mappings */
-/* generated from http://www.unicode.org/Public/12.1.0/ucd/UCD.zip:UnicodeData.txt */
+/* generated from https://www.unicode.org/Public/13.0.0/ucd/UCD.zip:UnicodeData.txt */
 /* DO NOT EDIT!! */
 
 #include "windef.h"
