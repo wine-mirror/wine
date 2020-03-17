0a9ca2d3f23baa8b245a91993a27aebe81655fc8
Jacek Caban <jacek@codeweavers.com>
2020-03-17 15:01:17 +0100

    corecrt_startup.h: Add onexit table declarations.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/msvcrt/corecrt_startup.h
+++ include/msvcrt/corecrt_startup.h
@@ -22,6 +22,18 @@ typedef enum _crt_app_type
     _crt_gui_app
 } _crt_app_type;
 
+typedef void (__cdecl *_PVFV)(void);
+typedef int (__cdecl *_PIFV)(void);
+typedef void (__cdecl *_PVFI)(int);
+
+typedef struct _onexit_table_t {
+    _PVFV *_first;
+    _PVFV *_last;
+    _PVFV *_end;
+} _onexit_table_t;
+
+typedef int (__cdecl *_onexit_t)(void);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -34,10 +46,13 @@ _ACRTIMP void __cdecl __wgetmainargs(int *, wchar_t ***, wchar_t ***, int, int *
 
 _ACRTIMP errno_t   __cdecl _configure_narrow_argv(_crt_argv_mode);
 _ACRTIMP errno_t   __cdecl _configure_wide_argv(_crt_argv_mode);
+_ACRTIMP int       __cdecl _execute_onexit_table(_onexit_table_t*);
 _ACRTIMP char    **__cdecl _get_initial_narrow_environment(void);
 _ACRTIMP wchar_t **__cdecl _get_initial_wide_environment(void);
 _ACRTIMP int       __cdecl _initialize_narrow_environment(void);
+_ACRTIMP int       __cdecl _initialize_onexit_table(_onexit_table_t*);
 _ACRTIMP int       __cdecl _initialize_wide_environment(void);
+_ACRTIMP int       __cdecl _register_onexit_function(_onexit_table_t*,_onexit_t);
 _ACRTIMP void      __cdecl _set_app_type(_crt_app_type);
 
 #ifdef __cplusplus
