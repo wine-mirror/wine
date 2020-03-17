4694801e4f58f0e856327d251ac98cfb573fd83b
Jacek Caban <jacek@codeweavers.com>
2020-03-17 20:10:40 +0100

    dbghelp: Move elf_check_alternate implementation to module.c.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dbghelp/image_private.h
+++ dlls/dbghelp/image_private.h
@@ -52,6 +52,9 @@
 #ifndef SHT_NULL
 #define SHT_NULL        0
 #endif
+#ifndef NT_GNU_BUILD_ID
+#define NT_GNU_BUILD_ID 3
+#endif
 #endif
 
 /* structure holding information while handling an ELF image
@@ -129,6 +132,10 @@ struct image_section_map
     struct image_file_map*      fmap;
     long                        sidx;
 };
+
+BOOL image_check_alternate(struct image_file_map* fmap, const struct module* module) DECLSPEC_HIDDEN;
+
+BOOL elf_map_handle(HANDLE handle, struct image_file_map* fmap) DECLSPEC_HIDDEN;
 
 struct image_file_map_ops
 {
