e95d0813fdfcfcac2bb1761e62e1d54e6f73cd6d
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2020-03-18 18:31:52 +0200

    ntdll: Store the default process activation context into the PEB.
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntdll/actctx.c
+++ dlls/ntdll/actctx.c
@@ -4942,6 +4942,8 @@ void actctx_init(void)
     ctx.lpResourceName = (LPCWSTR)CREATEPROCESS_MANIFEST_RESOURCE_ID;
 
     if (!RtlCreateActivationContext( &handle, &ctx )) process_actctx = check_actctx(handle);
+
+    NtCurrentTeb()->Peb->ActivationContextData = process_actctx;
 }
 
 
