dbe8d61c7055d0bc5713f90b0e9ce3f3389a8144
Dmitry Timoshkov <dmitry@baikal.ru>
2020-03-27 17:06:16 +0800

    adsldp: Add more traces.
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/adsldp/adsldp.c
+++ dlls/adsldp/adsldp.c
@@ -611,6 +611,7 @@ static HRESULT WINAPI ldapns_Get(IADs *iface, BSTR name, VARIANT *prop)
 
                 for (idx = 0; idx < count; idx++)
                 {
+                    TRACE("=> %s\n", debugstr_w(ldap->attrs[i].values[idx]));
                     V_VT(&item) = VT_BSTR;
                     V_BSTR(&item) = SysAllocString(ldap->attrs[i].values[idx]);
                     if (!V_BSTR(&item))
@@ -633,6 +634,7 @@ fail:
             }
             else
             {
+                TRACE("=> %s\n", debugstr_w(ldap->attrs[i].values[0]));
                 V_BSTR(prop) = SysAllocString(ldap->attrs[i].values[0]);
                 if (!V_BSTR(prop)) return E_OUTOFMEMORY;
                 V_VT(prop) = VT_BSTR;
@@ -1178,7 +1180,10 @@ static HRESULT WINAPI search_ExecuteSearch(IDirectorySearch *iface, LPWSTR filte
         }
 
         for (i = 0; i < count; i++)
+        {
+            TRACE("=> %s\n", debugstr_w(names[i]));
             props[i] = names[i];
+        }
 
         props[count] = NULL;
     }
