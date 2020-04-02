c1e282d0f9eaa6835df25b93fa5a313479723434
Francois Gouget <fgouget@codeweavers.com>
2020-04-02 11:09:57 +0200

    gameux/tests: Remove unneeded parentheses around a gamestatistics string.
    wine_dbgstr_w() already quotes the string.
    
    Signed-off-by: Francois Gouget <fgouget@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/gameux/tests/gamestatistics.c
+++ dlls/gameux/tests/gamestatistics.c
@@ -295,17 +295,17 @@ static void test_gamestatisticsmgr( void )
         /* verify values with these which we stored before*/
         hr = IGameStatistics_GetCategoryTitle(gs, 0, &lpName);
         ok(hr == S_OK, "getting category title failed\n");
-        ok(lstrcmpW(lpName, sCategory0)==0, "getting category title returned invalid string (%s)\n", wine_dbgstr_w(lpName));
+        ok(lstrcmpW(lpName, sCategory0)==0, "getting category title returned invalid string %s\n", wine_dbgstr_w(lpName));
         CoTaskMemFree(lpName);
 
         hr = IGameStatistics_GetCategoryTitle(gs, 1, &lpName);
         ok(hr == S_OK, "getting category title failed\n");
-        ok(lstrcmpW(lpName, sCategory1)==0, "getting category title returned invalid string (%s)\n", wine_dbgstr_w(lpName));
+        ok(lstrcmpW(lpName, sCategory1)==0, "getting category title returned invalid string %s\n", wine_dbgstr_w(lpName));
         CoTaskMemFree(lpName);
 
         hr = IGameStatistics_GetCategoryTitle(gs, 2, &lpName);
         ok(hr == S_OK, "getting category title failed\n");
-        ok(lstrcmpW(lpName, sCategory2)==0, "getting category title returned invalid string (%s)\n", wine_dbgstr_w(lpName));
+        ok(lstrcmpW(lpName, sCategory2)==0, "getting category title returned invalid string %s\n", wine_dbgstr_w(lpName));
         CoTaskMemFree(lpName);
 
         /* check result if category doesn't exists */
