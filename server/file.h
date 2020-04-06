af89b53cef4c0b8cff1ea39fd612c8d1116b020a
Piotr Caban <piotr@codeweavers.com>
2020-04-06 17:21:07 +0200

    server: Use monotonic clock for SetTimer timeouts.
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- server/file.h
+++ server/file.h
@@ -140,6 +140,12 @@ static inline abstime_t timeout_to_abstime( timeout_t timeout )
     return timeout > 0 ? timeout : timeout - monotonic_time;
 }
 
+static inline timeout_t abstime_to_timeout( abstime_t abstime )
+{
+    if (abstime > 0) return abstime;
+    return -abstime < monotonic_time ? 0 : abstime + monotonic_time;
+}
+
 extern void set_current_time( void );
 extern struct timeout_user *add_timeout_user( timeout_t when, timeout_callback func, void *private );
 extern void remove_timeout_user( struct timeout_user *user );
