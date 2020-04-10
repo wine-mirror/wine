341fe50c287b0f421de16aa138af724f559dd880
Hans Leidekker <hans@codeweavers.com>
2020-04-10 22:18:33 +0200

    secur32: Add application protocol stubs for macOS.
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/secur32/schannel_macosx.c
+++ dlls/secur32/schannel_macosx.c
@@ -1194,6 +1194,18 @@ void schan_imp_free_certificate_credentials(schan_credentials *c)
 {
 }
 
+void schan_imp_set_application_protocols(schan_imp_session session, unsigned char *buffer, unsigned int buflen)
+{
+    FIXME("no support for application protocols on this platform\n");
+}
+
+SECURITY_STATUS schan_imp_get_application_protocol(schan_imp_session session,
+                                                   SecPkgContext_ApplicationProtocol *protocol)
+{
+    FIXME("no support for application protocols on this platform\n");
+    return SEC_E_UNSUPPORTED_FUNCTION;
+}
+
 BOOL schan_imp_init(void)
 {
     TRACE("()\n");
