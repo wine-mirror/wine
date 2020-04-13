7b138e929d48366aaafeef4587d880ae586b0ec3
Dmitry Timoshkov <dmitry@baikal.ru>
2020-04-13 18:07:08 +0800

    adsldp: Fix mapping for 1.3.6.1.4.1.1466.115.121.1.26.
    According to rfc4517 1.3.6.1.4.1.1466.115.121.1.26 is IA5 String,
    and in an AD DC managed by Samba it's used for LDAP attributes
    "loginShell" and "unixHomeDirectory", so I assumed that a proper
    type for it is ADSTYPE_CASE_EXACT_STRING. However testing under
    Windows 10 shows that it's ADSTYPE_CASE_IGNORE_STRING there.
    
    Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/adsldp/schema.c
+++ dlls/adsldp/schema.c
@@ -96,7 +96,7 @@ ADSTYPEENUM get_schema_type(const WCHAR *name, const struct attribute_type *at, 
     if (!wcscmp(type->syntax, L"1.3.6.1.4.1.1466.115.121.1.24"))
         return ADSTYPE_UTC_TIME;
     if (!wcscmp(type->syntax, L"1.3.6.1.4.1.1466.115.121.1.26"))
-        return ADSTYPE_CASE_EXACT_STRING;
+        return ADSTYPE_CASE_IGNORE_STRING;
     if (!wcscmp(type->syntax, L"1.3.6.1.4.1.1466.115.121.1.27"))
         return ADSTYPE_INTEGER;
     if (!wcscmp(type->syntax, L"1.3.6.1.4.1.1466.115.121.1.38"))
