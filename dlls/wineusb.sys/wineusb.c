3c392094449b7c15bfc98a24fbda6d0970a6085d
Zebediah Figura <z.figura12@gmail.com>
2020-04-15 09:43:18 -0500

    wineusb.sys: New stub driver.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

---
+++ dlls/wineusb.sys/wineusb.c
@@ -0,0 +1,61 @@
+/*
+ * USB root device enumerator using libusb
+ *
+ * Copyright 2020 Zebediah Figura
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <assert.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <libusb.h>
+
+#include "ntstatus.h"
+#define WIN32_NO_STATUS
+#include "windef.h"
+#include "winioctl.h"
+#include "winternl.h"
+#include "ddk/wdm.h"
+#include "ddk/usb.h"
+#include "ddk/usbioctl.h"
+#include "wine/asm.h"
+#include "wine/debug.h"
+#include "wine/list.h"
+#include "wine/unicode.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(wineusb);
+
+static void WINAPI driver_unload(DRIVER_OBJECT *driver)
+{
+    libusb_exit(NULL);
+}
+
+NTSTATUS WINAPI DriverEntry(DRIVER_OBJECT *driver, UNICODE_STRING *path)
+{
+    int err;
+
+    TRACE("driver %p, path %s.\n", driver, debugstr_w(path->Buffer));
+
+    if ((err = libusb_init(NULL)))
+    {
+        ERR("Failed to initialize libusb: %s\n", libusb_strerror(err));
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    driver->DriverUnload = driver_unload;
+
+    return STATUS_SUCCESS;
+}
