fd8e83eab08cb370849edc964d4054a1f428ef37
Jeff Smith <whydoubt@gmail.com>
2020-04-16 11:40:38 -0500

    jscript/tests: Fix some function-to-string failure message typos.
    Signed-off-by: Jeff Smith <whydoubt@gmail.com>
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/jscript/tests/api.js
+++ dlls/jscript/tests/api.js
@@ -2303,11 +2303,11 @@ ok(Math.floor(Math.SQRT1_2*100) === 70, "modified Math.SQRT1_2 = " + Math.SQRT1_
 ok(isNaN.toString() === "\nfunction isNaN() {\n    [native code]\n}\n",
    "isNaN.toString = '" + isNaN.toString() + "'");
 ok(Array.toString() === "\nfunction Array() {\n    [native code]\n}\n",
-   "isNaN.toString = '" + Array.toString() + "'");
+   "Array.toString = '" + Array.toString() + "'");
 ok(Function.toString() === "\nfunction Function() {\n    [native code]\n}\n",
-   "isNaN.toString = '" + Function.toString() + "'");
+   "Function.toString = '" + Function.toString() + "'");
 ok(Function.prototype.toString() === "\nfunction prototype() {\n    [native code]\n}\n",
-   "isNaN.toString = '" + Function.prototype.toString() + "'");
+   "Function.prototype.toString = '" + Function.prototype.toString() + "'");
 ok("".substr.toString() === "\nfunction substr() {\n    [native code]\n}\n",
    "''.substr.toString = '" + "".substr.toString() + "'");
 
