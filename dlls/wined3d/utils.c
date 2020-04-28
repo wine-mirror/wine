359d53d4d1b89ea631c2f5bf7bed1c4c7c3351de
Henri Verbeet <hverbeet@codeweavers.com>
2020-04-29 02:46:59 +0430

    wined3d: Add Vulkan format information for WINED3DFMT_D24_UNORM_S8_UINT.
    On some implementations we could use VK_FORMAT_D24_UNORM_S8_UINT
    instead, but that's not universally supported.
    
    Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wined3d/utils.c
+++ dlls/wined3d/utils.c
@@ -4203,6 +4203,7 @@ static void init_vulkan_format_info(struct wined3d_format_vk *format,
         {WINED3DFMT_D32_FLOAT_S8X24_UINT,       VK_FORMAT_D32_SFLOAT_S8_UINT,      },
         {WINED3DFMT_R32_FLOAT_X8X24_TYPELESS,   VK_FORMAT_D32_SFLOAT_S8_UINT,      },
         {WINED3DFMT_X32_TYPELESS_G8X24_UINT,    VK_FORMAT_D32_SFLOAT_S8_UINT,      },
+        {WINED3DFMT_D24_UNORM_S8_UINT,          VK_FORMAT_D32_SFLOAT_S8_UINT,      },
     };
     VkFormat vk_format = VK_FORMAT_UNDEFINED;
     VkImageFormatProperties image_properties;
