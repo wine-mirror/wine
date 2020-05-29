efdbac559d8371998879c33090cd9f862d18f4f1
Paul Gofman <pgofman@codeweavers.com>
2020-05-29 10:53:07 +0300

    ntoskrnl.exe: Add stub for ExUnregisterCallback() function.
    Signed-off-by: Paul Gofman <pgofman@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 9effc3f96333d809ffba43b975fa062cbf8912d7)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -2066,6 +2066,11 @@ void * WINAPI ExRegisterCallback(PCALLBACK_OBJECT callback_object,
     return (void *)0xdeadbeef;
 }
 
+void WINAPI ExUnregisterCallback(void *callback_registration)
+{
+    FIXME("callback_registration %p stub.\n", callback_registration);
+}
+
 /***********************************************************************
  *           ExFreePool   (NTOSKRNL.EXE.@)
  */
