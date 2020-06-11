547d62da228a3846cf058bcc71fc8974356a96b8
Zebediah Figura <z.figura12@gmail.com>
2020-06-11 11:16:31 -0500

    setupapi: Store the original INF file path in a .pnf file.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/setupapi/misc.c
+++ dlls/setupapi/misc.c
@@ -31,6 +31,7 @@
 #include "softpub.h"
 #include "mscat.h"
 #include "shlobj.h"
+#include "shlwapi.h"
 
 #include "wine/debug.h"
 
@@ -962,10 +963,11 @@ BOOL WINAPI SetupCopyOEMInfW( PCWSTR source, PCWSTR location,
                               DWORD buffer_size, DWORD *required_size, WCHAR **filepart )
 {
     BOOL ret = FALSE;
-    WCHAR target[MAX_PATH], catalog_file[MAX_PATH], *p;
+    WCHAR target[MAX_PATH], catalog_file[MAX_PATH], pnf_path[MAX_PATH], *p;
     static const WCHAR inf[] = { '\\','i','n','f','\\',0 };
     static const WCHAR wszVersion[] = { 'V','e','r','s','i','o','n',0 };
     static const WCHAR wszCatalogFile[] = { 'C','a','t','a','l','o','g','F','i','l','e',0 };
+    FILE *pnf_file;
     unsigned int i;
     DWORD size;
     HINF hinf;
@@ -1068,6 +1070,19 @@ BOOL WINAPI SetupCopyOEMInfW( PCWSTR source, PCWSTR location,
 done:
     if (style & SP_COPY_DELETESOURCE)
         DeleteFileW( source );
+
+    if (ret)
+    {
+        wcscpy(pnf_path, target);
+        PathRemoveExtensionW(pnf_path);
+        PathAddExtensionW(pnf_path, L".pnf");
+        if ((pnf_file = _wfopen(pnf_path, L"w")))
+        {
+            fputws(PNF_HEADER, pnf_file);
+            fputws(source, pnf_file);
+            fclose(pnf_file);
+        }
+    }
 
     size = lstrlenW( target ) + 1;
     if (dest)
