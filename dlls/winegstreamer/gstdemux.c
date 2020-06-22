fd25ba65e0eb9fedfb2cdfa2b7a4b16e0401dfdf
Zebediah Figura <z.figura12@gmail.com>
2020-06-22 17:56:04 -0500

    winegstreamer: Avoid performing color matrix conversions in videoconvert.
    This reduces the time videoconvert takes to process I420 -> YV12 from 15 ms to 0.8 ms on my system.
    
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winegstreamer/gstdemux.c
+++ dlls/winegstreamer/gstdemux.c
@@ -1022,6 +1022,9 @@ static void init_new_decoded_pad(GstElement *bin, GstPad *pad, struct gstdemux *
             goto out;
         }
 
+        /* Avoid expensive color matrix conversions. */
+        gst_util_set_object_arg(G_OBJECT(vconv), "matrix-mode", "none");
+
         /* GStreamer outputs RGB video top-down, but DirectShow expects bottom-up. */
         if (!(flip = gst_element_factory_make("videoflip", NULL)))
         {
