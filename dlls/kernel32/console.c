d04baa29e296599f3736ab9bd7c830451eb3335d
Jacek Caban <jacek@codeweavers.com>
2020-07-07 16:07:15 +0200

    kernel32: Use IOCTL_CONDRV_GET_INPUT_INFO in CONSOLE_GetEditionMode.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/kernel32/console.c
+++ dlls/kernel32/console.c
@@ -1077,15 +1077,9 @@ unsigned CONSOLE_GetNumHistoryEntries(void)
  */
 BOOL CONSOLE_GetEditionMode(HANDLE hConIn, int* mode)
 {
-    unsigned ret = 0;
-    SERVER_START_REQ(get_console_input_info)
-    {
-        req->handle = console_handle_unmap(hConIn);
-        if ((ret = !wine_server_call_err( req )))
-            *mode = reply->edition_mode;
-    }
-    SERVER_END_REQ;
-    return ret;
+    struct condrv_input_info info;
+    return DeviceIoControl( hConIn, IOCTL_CONDRV_GET_INPUT_INFO, NULL, 0, &info, sizeof(info), NULL, NULL )
+        ? info.edition_mode : 0;
 }
 
 /******************************************************************
