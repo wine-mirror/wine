196cb7afef48f717794828b3ac00838c7aa013c8
Rémi Bernon <rbernon@codeweavers.com>
2020-08-05 09:08:10 +0200

    xaudio2_9: Use new FAudioCreateReverb9 interface.
    XAudio 2.9 and Windows 10 introduced a new SideDelay field in the middle
    of the XAUDIO2FX_REVERB_PARAMETERS structure, which causes other fields
    to be offseted and incorrectly read from the parameter buffer.
    
    FAudio 20.08 introduces this new interface to create reverb effects that
    use the new parameter structure layout.
    
    This fixes audio glitches in Dragon Quest Builders 2.
    
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/xaudio2_7/xapo.c
+++ dlls/xaudio2_7/xapo.c
@@ -313,6 +313,16 @@ static inline HRESULT get_fapo_from_clsid(REFCLSID clsid, FAPO **fapo)
             XAudio_Internal_Free,
             XAudio_Internal_Realloc
         );
+#if XAUDIO2_VER >= 9 && HAVE_FAUDIOCREATEREVERB9WITHCUSTOMALLOCATOREXT
+    if(IsEqualGUID(clsid, &CLSID_AudioReverb27))
+        return FAudioCreateReverb9WithCustomAllocatorEXT(
+            fapo,
+            0,
+            XAudio_Internal_Malloc,
+            XAudio_Internal_Free,
+            XAudio_Internal_Realloc
+        );
+#else
     if(IsEqualGUID(clsid, &CLSID_AudioReverb27))
         return FAudioCreateReverbWithCustomAllocatorEXT(
             fapo,
@@ -321,6 +331,7 @@ static inline HRESULT get_fapo_from_clsid(REFCLSID clsid, FAPO **fapo)
             XAudio_Internal_Free,
             XAudio_Internal_Realloc
         );
+#endif
 #endif
 #if XAUDIO2_VER >= 8 || defined XAPOFX1_VER
     if(IsEqualGUID(clsid, &CLSID_FXReverb) ||
