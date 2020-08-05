20455292bd7f4ae44a4e04744c25ce8b07658056
Michael Müller <michael@fds-team.de>
2020-08-05 12:18:32 +0300

    fsutil: Print out usage message.
    Fsutil doesn't do anything yet other than printing out usage message. This
    patch puts some thing in place in preparation for the implementation of
    hardlink subcommand.
    
    Signed-off-by: Vijay Kiran Kamuju <infyquest@gmail.com>
    Signed-off-by: Arkadiusz Hiler <ahiler@codeweavers.com>
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit dd6b64b246731046236982a54b9124af92e7ec24)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- programs/fsutil/main.c
+++ programs/fsutil/main.c
@@ -1,5 +1,6 @@
 /*
  * Copyright 2016 Austin English
+ * Copyright 2016 Michael Müller
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -16,18 +17,70 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#include <windows.h>
+
 #include "wine/debug.h"
+#include "resources.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(fsutil);
 
-int __cdecl wmain(int argc, WCHAR *argv[])
+static void output_write(const WCHAR *str, DWORD wlen)
 {
-    int i;
+    DWORD count, ret;
 
-    WINE_FIXME("stub:");
-    for (i = 0; i < argc; i++)
-        WINE_FIXME(" %s", wine_dbgstr_w(argv[i]));
-    WINE_FIXME("\n");
+    ret = WriteConsoleW(GetStdHandle(STD_OUTPUT_HANDLE), str, wlen, &count, NULL);
+    if (!ret)
+    {
+        DWORD len;
+        char  *msgA;
+
+        /* On Windows WriteConsoleW() fails if the output is redirected. So fall
+         * back to WriteFile(), assuming the console encoding is still the right
+         * one in that case.
+         */
+        len = WideCharToMultiByte(GetConsoleOutputCP(), 0, str, wlen, NULL, 0, NULL, NULL);
+        msgA = HeapAlloc(GetProcessHeap(), 0, len * sizeof(char));
+        if (!msgA) return;
+
+        WideCharToMultiByte(GetConsoleOutputCP(), 0, str, wlen, msgA, len, NULL, NULL);
+        WriteFile(GetStdHandle(STD_OUTPUT_HANDLE), msgA, len, &count, FALSE);
+        HeapFree(GetProcessHeap(), 0, msgA);
+    }
+}
+
+static int WINAPIV output_string(int msg, ...)
+{
+    WCHAR out[8192];
+    __ms_va_list arguments;
+    int len;
+
+    __ms_va_start(arguments, msg);
+    len = FormatMessageW(FORMAT_MESSAGE_FROM_HMODULE, NULL, msg, 0, out, ARRAY_SIZE(out), &arguments);
+    __ms_va_end(arguments);
+
+    if (len == 0 && GetLastError() != NO_ERROR)
+        WINE_FIXME("Could not format string: le=%u, msg=%d\n", GetLastError(), msg);
+    else
+        output_write(out, len);
 
     return 0;
+}
+
+int __cdecl wmain(int argc, WCHAR *argv[])
+{
+    int i, ret = 0;
+
+    TRACE("Command line:");
+    for (i = 0; i < argc; i++)
+        TRACE(" %s", debugstr_w(argv[i]));
+    TRACE("\n");
+
+    if (argc > 1)
+    {
+        FIXME("unsupported command %s\n", debugstr_w(argv[1]));
+        ret = 1;
+    }
+
+    output_string(STRING_USAGE);
+    return ret;
 }
