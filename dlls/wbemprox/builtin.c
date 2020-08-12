75fb68e42423362ae945c0c0554f0dcd4d2e169b
Rémi Bernon <rbernon@codeweavers.com>
2020-08-12 15:35:16 +0200

    wbemprox: Use empty strings instead of NULL.
    For Win32_PhysicalMemory PartNumber and Serial properties.
    
    Injustice 2 calls SysStringLen directly on the value without checking
    its type.
    
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wbemprox/builtin.c
+++ dlls/wbemprox/builtin.c
@@ -2920,8 +2920,8 @@ static enum fill_status fill_physicalmemory( struct table *table, const struct e
     rec->devicelocator        = L"DIMM 0";
     rec->formfactor           = 8; /* DIMM */
     rec->memorytype           = 9; /* RAM */
-    rec->partnumber           = NULL;
-    rec->serial               = NULL;
+    rec->partnumber           = L"";
+    rec->serial               = L"";
     if (!match_row( table, row, cond, &status )) free_row_values( table, row );
     else row++;
 
