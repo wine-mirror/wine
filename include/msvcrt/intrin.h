0e544824f507ee6efc6dac171cbf7b552c359eb7
Paul Gofman <pgofman@codeweavers.com>
2020-08-20 00:22:58 +0300

    include: Implement __cpuidex() function.
    Signed-off-by: Paul Gofman <pgofman@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/msvcrt/intrin.h
+++ include/msvcrt/intrin.h
@@ -12,9 +12,13 @@ extern "C" {
 #endif
 
 #if defined(__i386__) || defined(__x86_64__)
+static inline void __cpuidex(int info[4], int ax, int cx)
+{
+  __asm__ ("cpuid" : "=a"(info[0]), "=b" (info[1]), "=c"(info[2]), "=d"(info[3]) : "a"(ax), "c"(cx));
+}
 static inline void __cpuid(int info[4], int ax)
 {
-  __asm__ ("cpuid" : "=a"(info[0]), "=b" (info[1]), "=c"(info[2]), "=d"(info[3]) : "a"(ax), "c"(0));
+    return __cpuidex(info, ax, 0);
 }
 #endif
 
