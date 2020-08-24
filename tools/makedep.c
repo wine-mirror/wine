7571fa87df453e404d8b6ca58e2da95340156849
Alexandre Julliard <julliard@winehq.org>
2020-08-24 13:30:12 +0200

    makefiles: Don't implicitly import the module itself when -nodefaultlibs is used.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- tools/makedep.c
+++ tools/makedep.c
@@ -2212,7 +2212,11 @@ static struct strarray add_import_libs( const struct makefile *make, struct stra
         const char *lib = NULL;
 
         /* skip module's own importlib, its object files will be linked directly */
-        if (make->importlib && !is_unix && !strcmp( make->importlib, imports.str[i] )) continue;
+        if (make->importlib && !strcmp( make->importlib, imports.str[i] ))
+        {
+            if (!is_unix) continue;
+            if (strarray_exists( &make->extradllflags, "-nodefaultlibs" )) continue;
+        }
 
         for (j = 0; j < top_makefile->subdirs.count; j++)
         {
