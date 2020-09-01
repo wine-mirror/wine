ae415a7df78cbcf73f39c12d347e5bdf468d1895
Zebediah Figura <zfigura@codeweavers.com>
2020-09-01 10:52:41 -0500

    d3dcompiler: Unconditionally report special (pixel shader output) sysvals based on the semantic name.
    That is, do not check that the shader is a pixel shader and that the signature
    is an output signature. These semantic names can only be generated in such
    conditions; using the name for any other stage is invalid.
    
    Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
    Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d3dcompiler_43/reflection.c
+++ dlls/d3dcompiler_43/reflection.c
@@ -1608,7 +1608,7 @@ err_out:
     return hr;
 }
 
-static HRESULT d3dcompiler_parse_signature(struct d3dcompiler_shader_signature *s, struct dxbc_section *section, DWORD target)
+static HRESULT d3dcompiler_parse_signature(struct d3dcompiler_shader_signature *s, struct dxbc_section *section)
 {
     D3D11_SIGNATURE_PARAMETER_DESC *d;
     unsigned int string_data_offset;
@@ -1690,27 +1690,16 @@ static HRESULT d3dcompiler_parse_signature(struct d3dcompiler_shader_signature *
         d[i].ReadWriteMask = (mask >> 8) & 0xff;
         d[i].Mask = mask & 0xff;
 
-        /* pixel shaders have a special handling for SystemValueType in the output signature */
-        if (((target & D3DCOMPILER_SHADER_TARGET_SHADERTYPE_MASK) == 0xffff0000) && (section->tag == TAG_OSG5 || section->tag == TAG_OSGN))
-        {
-            TRACE("Pixelshader output signature fixup.\n");
-
-            if (d[i].Register == 0xffffffff)
-            {
-                if (!stricmp(d[i].SemanticName, "sv_depth"))
-                    d[i].SystemValueType = D3D_NAME_DEPTH;
-                else if (!stricmp(d[i].SemanticName, "sv_coverage"))
-                    d[i].SystemValueType = D3D_NAME_COVERAGE;
-                else if (!stricmp(d[i].SemanticName, "sv_depthgreaterequal"))
-                    d[i].SystemValueType = D3D_NAME_DEPTH_GREATER_EQUAL;
-                else if (!stricmp(d[i].SemanticName, "sv_depthlessequal"))
-                    d[i].SystemValueType = D3D_NAME_DEPTH_LESS_EQUAL;
-            }
-            else
-            {
-                d[i].SystemValueType = D3D_NAME_TARGET;
-            }
-        }
+        if (!stricmp(d[i].SemanticName, "sv_depth"))
+            d[i].SystemValueType = D3D_NAME_DEPTH;
+        else if (!stricmp(d[i].SemanticName, "sv_coverage"))
+            d[i].SystemValueType = D3D_NAME_COVERAGE;
+        else if (!stricmp(d[i].SemanticName, "sv_depthgreaterequal"))
+            d[i].SystemValueType = D3D_NAME_DEPTH_GREATER_EQUAL;
+        else if (!stricmp(d[i].SemanticName, "sv_depthlessequal"))
+            d[i].SystemValueType = D3D_NAME_DEPTH_LESS_EQUAL;
+        else if (!stricmp(d[i].SemanticName, "sv_target"))
+            d[i].SystemValueType = D3D_NAME_TARGET;
 
         TRACE("semantic: %s, semantic idx: %u, sysval_semantic %#x, "
                 "type %u, register idx: %u, use_mask %#x, input_mask %#x, stream %u\n",
@@ -1777,7 +1766,7 @@ static HRESULT d3dcompiler_shader_reflection_init(struct d3dcompiler_shader_refl
                     goto err_out;
                 }
 
-                hr = d3dcompiler_parse_signature(reflection->isgn, section, reflection->target);
+                hr = d3dcompiler_parse_signature(reflection->isgn, section);
                 if (FAILED(hr))
                 {
                     WARN("Failed to parse section ISGN.\n");
@@ -1795,7 +1784,7 @@ static HRESULT d3dcompiler_shader_reflection_init(struct d3dcompiler_shader_refl
                     goto err_out;
                 }
 
-                hr = d3dcompiler_parse_signature(reflection->osgn, section, reflection->target);
+                hr = d3dcompiler_parse_signature(reflection->osgn, section);
                 if (FAILED(hr))
                 {
                     WARN("Failed to parse section OSGN.\n");
@@ -1812,7 +1801,7 @@ static HRESULT d3dcompiler_shader_reflection_init(struct d3dcompiler_shader_refl
                     goto err_out;
                 }
 
-                hr = d3dcompiler_parse_signature(reflection->pcsg, section, reflection->target);
+                hr = d3dcompiler_parse_signature(reflection->pcsg, section);
                 if (FAILED(hr))
                 {
                     WARN("Failed to parse section PCSG.\n");
