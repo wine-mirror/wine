24021931a88ed917bc2b52c0914c059c2d8d2f30
Zebediah Figura <z.figura12@gmail.com>
2020-09-10 16:17:16 -0500

    server: Remove no longer used enumerate_handles().
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- server/handle.h
+++ server/handle.h
@@ -48,8 +48,6 @@ extern obj_handle_t open_object( struct process *process, obj_handle_t parent, u
                                  const struct object_ops *ops, const struct unicode_str *name,
                                  unsigned int attr );
 extern obj_handle_t find_inherited_handle( struct process *process, const struct object_ops *ops );
-extern obj_handle_t enumerate_handles( struct process *process, const struct object_ops *ops,
-                                       unsigned int *index );
 extern void close_process_handles( struct process *process );
 extern struct handle_table *alloc_handle_table( struct process *process, int count );
 extern struct handle_table *copy_handle_table( struct process *process, struct process *parent );
