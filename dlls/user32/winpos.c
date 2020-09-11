7b8cf3ad4ef35595cc1f67202ccf5368f5d8a6a3
Rémi Bernon <rbernon@codeweavers.com>
2020-09-11 11:52:01 +0200

    user32: Return TRUE from ShowWindow(SW_SHOW) if already visible.
    Instead of calling SendMessage, similarly to the SW_HIDE case.
    
    Based on a patch by Kimmo Myllyvirta <kimmo.myllyvirta@gmail.com>.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=39731
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit d50c310da96565dec98977e96a2d887097a6c1cf)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/user32/winpos.c
+++ dlls/user32/winpos.c
@@ -1231,6 +1231,9 @@ BOOL WINAPI ShowWindow( HWND hwnd, INT cmd )
     if ((cmd == SW_HIDE) && !(GetWindowLongW( hwnd, GWL_STYLE ) & WS_VISIBLE))
         return FALSE;
 
+    if ((cmd == SW_SHOW) && (GetWindowLongW( hwnd, GWL_STYLE ) & WS_VISIBLE))
+        return TRUE;
+
     return SendMessageW( hwnd, WM_WINE_SHOWWINDOW, cmd, 0 );
 }
 
