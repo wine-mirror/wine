c690a31acf55bea967336504d81237b475ee5a4d
Hans Leidekker <hans@codeweavers.com>
2020-10-08 18:02:43 +0200

    dssenh: Add stub implementations for a couple of functions.
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dssenh/dssenh.spec
+++ dlls/dssenh/dssenh.spec
@@ -1,27 +1,27 @@
-@ stub CPAcquireContext
-@ stub CPCreateHash
+@ stdcall CPAcquireContext(ptr str long ptr)
+@ stdcall CPCreateHash(ptr long ptr long ptr)
 @ stub CPDecrypt
-@ stub CPDeriveKey
-@ stub CPDestroyHash
-@ stub CPDestroyKey
+@ stdcall CPDeriveKey(ptr long ptr long ptr)
+@ stdcall CPDestroyHash(ptr ptr)
+@ stdcall CPDestroyKey(ptr ptr)
 @ stub CPDuplicateHash
 @ stub CPDuplicateKey
 @ stub CPEncrypt
-@ stub CPExportKey
-@ stub CPGenKey
+@ stdcall CPExportKey(ptr ptr ptr long long ptr ptr)
+@ stdcall CPGenKey(ptr long long ptr)
 @ stub CPGenRandom
-@ stub CPGetHashParam
+@ stdcall CPGetHashParam(ptr ptr long ptr ptr long)
 @ stub CPGetKeyParam
-@ stub CPGetProvParam
+@ stdcall CPGetProvParam(ptr long ptr ptr long)
 @ stub CPGetUserKey
-@ stub CPHashData
+@ stdcall CPHashData(ptr ptr ptr long long)
 @ stub CPHashSessionKey
-@ stub CPImportKey
-@ stub CPReleaseContext
+@ stdcall CPImportKey(ptr ptr long ptr long ptr)
+@ stdcall CPReleaseContext(ptr long)
 @ stub CPSetHashParam
 @ stub CPSetKeyParam
 @ stub CPSetProvParam
-@ stub CPSignHash
-@ stub CPVerifySignature
+@ stdcall CPSignHash(ptr ptr long wstr long ptr ptr)
+@ stdcall CPVerifySignature(ptr ptr ptr long ptr wstr long)
 @ stdcall -private DllRegisterServer()
 @ stdcall -private DllUnregisterServer()
--- dlls/dssenh/main.c
+++ dlls/dssenh/main.c
@@ -21,9 +21,84 @@
 
 #include "windef.h"
 #include "winbase.h"
+#include "wincrypt.h"
 #include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(dssenh);
+
+BOOL WINAPI CPAcquireContext( HCRYPTPROV *ret_prov, LPSTR container, DWORD flags, PVTableProvStruc vtable )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPReleaseContext( HCRYPTPROV hprov, DWORD flags )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPGetProvParam( HCRYPTPROV hprov, DWORD param, BYTE *data, DWORD *len, DWORD flags )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPGenKey( HCRYPTPROV hprov, ALG_ID algid, DWORD flags, HCRYPTKEY *ret_key )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPDestroyKey( HCRYPTPROV hprov, HCRYPTKEY hkey )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPImportKey( HCRYPTPROV hprov, const BYTE *data, DWORD len, HCRYPTKEY hpubkey, DWORD flags,
+                         HCRYPTKEY *ret_key )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPExportKey( HCRYPTPROV hprov, HCRYPTKEY hkey, HCRYPTKEY hexpkey, DWORD blobtype, DWORD flags,
+                         BYTE *data, DWORD *len )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPCreateHash( HCRYPTPROV hprov, ALG_ID algid, HCRYPTKEY hkey, DWORD flags, HCRYPTHASH *ret_hash )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPDestroyHash( HCRYPTPROV hprov, HCRYPTHASH hhash )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPHashData( HCRYPTPROV hprov, HCRYPTHASH hhash, const BYTE *data, DWORD len, DWORD flags )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPGetHashParam( HCRYPTPROV hprov, HCRYPTHASH hhash, DWORD param, BYTE *data, DWORD *len, DWORD flags )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPDeriveKey( HCRYPTPROV hprov, ALG_ID algid, HCRYPTHASH hhash, DWORD flags, HCRYPTKEY *ret_key )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPSignHash( HCRYPTPROV hprov, HCRYPTHASH hhash, DWORD keyspec, const WCHAR *desc, DWORD flags, BYTE *sig,
+                        DWORD *siglen )
+{
+    return FALSE;
+}
+
+BOOL WINAPI CPVerifySignature( HCRYPTPROV hprov, HCRYPTHASH hhash, const BYTE *sig, DWORD siglen, HCRYPTKEY hpubkey,
+                               const WCHAR *desc, DWORD flags )
+{
+    return FALSE;
+}
 
 BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
