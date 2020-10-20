7201688841806d9b4d37cf695ed081950a769961
Hans Leidekker <hans@codeweavers.com>
2020-10-20 10:13:19 +0200

    dssenh: No longer prefer native.
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dssenh/main.c
+++ dlls/dssenh/main.c
@@ -1038,8 +1038,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 
     switch (fdwReason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE;    /* prefer native version */
     case DLL_PROCESS_ATTACH:
         instance = hinstDLL;
         DisableThreadLibraryCalls(hinstDLL);
