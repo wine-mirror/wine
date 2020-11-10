3c4860b93db6f949a5a72a961cffd5a688605a0d
Michael Stefaniuc <mstefani@winehq.org>
2020-11-10 22:50:34 +0100

    icinfo: Use wide-char string literals.
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/icinfo/icinfo.c
+++ programs/icinfo/icinfo.c
@@ -58,40 +58,18 @@ int __cdecl wmain(int argc, WCHAR* argv[])
 {
     int i, n=0,doabout=0,doconfigure=0;
 
-    static const WCHAR header[] = {'C','u','r','r','e','n','t','l','y',' ','i','n','s','t','a','l','l','e','d',' ',
-                                   'V','i','d','e','o',' ','C','o','m','p','r','e','s','s','o','r','s',':','\n',0};
-    static const WCHAR close_flags[] = {')','\n',0};
-    static const WCHAR s_fmt[] = {'%','s',0};
-    static const WCHAR sspc_fmt[] = {'%','s',' ',0};
-    static const WCHAR fcc_fmt[] = {'%','c','%','c','%','c','%','c','.','%','c','%','c','%','c','%','c',':',' ','%','s','\n',0};
-    static const WCHAR desc_fmt[] = {'\t','s','z','D','e','s','c','r','i','p','t','i','o','n',':',' ','%','s','\n',0};
-    static const WCHAR flags_fmt[] = {'\t','d','w','F','l','a','g','s',':',' ','0','x','%','0','8','x',' ','(',0};
-    static const WCHAR version_fmt[] = {'\t','d','w','V','e','r','s','i','o','n',':',' ','0','x','%','0','8','x','\n',0};
-    static const WCHAR versicm_fmt[] = {'\t','d','w','V','e','r','s','i','o','n','I','C','M',':',' ','0','x','%','0','8','x','\n',0};
-    static const WCHAR VIDCF_QUALITY_W[] = {'V','I','D','C','F','_','Q','U','A','L','I','T','Y',0};
-    static const WCHAR VIDCF_CRUNCH_W[] = {'V','I','D','C','F','_','C','R','U','N','C','H',0};
-    static const WCHAR VIDCF_TEMPORAL_W[] = {'V','I','D','C','F','_','T','E','M','P','O','R','A','L',0};
-    static const WCHAR VIDCF_COMPRESSFRAMES_W[] = {'V','I','D','C','F','_','C','O','M','P','R','E','S','S','F','R','A','M','E','S',0};
-    static const WCHAR VIDCF_DRAW_W[] = {'V','I','D','C','F','_','D','R','A','W',0};
-    static const WCHAR VIDCF_FASTTEMPORALC_W[] = {'V','I','D','C','F','_','F','A','S','T','T','E','M','P','O','R','A','L','C',0};
-    static const WCHAR VIDCF_FASTTEMPORALD_W[] = {'V','I','D','C','F','_','F','A','S','T','T','E','M','P','O','R','A','L','D',0};
-    static const WCHAR VIDCF_QUALITYTIME_W[] = {'V','I','D','C','F','_','Q','U','A','L','I','T','Y','T','I','M','E',0};
-    static const WCHAR about[] = {'-','a','b','o','u','t','\0'};
-    static const WCHAR configure[] = {'-','c','o','n','f','i','g','u','r','e','\0'};
-    static const WCHAR unk_opt_fmt[] = {'U','n','k','n','o','w','n',' ','o','p','t','i','o','n',':',' ','%','s','\n',0};
-
     for (i = 1; i < argc; i++) {
-        if (!lstrcmpW(argv[i], about))
+        if (!lstrcmpW(argv[i], L"-about"))
             doabout = 1;
-        else if (!lstrcmpW(argv[i], configure))
+        else if (!lstrcmpW(argv[i], L"-configure"))
             doconfigure = 1;
         else {
-            mywprintf(unk_opt_fmt, argv[i]);
+            mywprintf(L"Unknown option: %s\n", argv[i]);
             return -1;
         }
     }
 
-    mywprintf(s_fmt, header);
+    mywprintf(L"%s", L"Currently installed Video Compressors:\n");
     while (1) {
     	ICINFO	ii;
 	HIC	hic;
@@ -106,25 +84,25 @@ int __cdecl wmain(int argc, WCHAR* argv[])
 	    continue;
 	}
 
-	mywprintf(fcc_fmt,
+        mywprintf(L"%c%c%c%c.%c%c%c%c: %s\n",
                   LOBYTE(ii.fccType),LOBYTE(ii.fccType>>8),LOBYTE(ii.fccType>>16),LOBYTE(ii.fccType>>24),
                   LOBYTE(ii.fccHandler),LOBYTE(ii.fccHandler>>8),LOBYTE(ii.fccHandler>>16),LOBYTE(ii.fccHandler>>24),
                   ii.szName);
-	mywprintf(flags_fmt,ii.dwFlags);
+        mywprintf(L"\tdwFlags: 0x%08x (",ii.dwFlags);
 
-	if (ii.dwFlags & VIDCF_QUALITY) mywprintf(sspc_fmt, VIDCF_QUALITY_W);
-	if (ii.dwFlags & VIDCF_CRUNCH) mywprintf(sspc_fmt, VIDCF_CRUNCH_W);
-	if (ii.dwFlags & VIDCF_TEMPORAL) mywprintf(sspc_fmt, VIDCF_TEMPORAL_W);
-	if (ii.dwFlags & VIDCF_COMPRESSFRAMES) mywprintf(sspc_fmt, VIDCF_COMPRESSFRAMES_W);
-	if (ii.dwFlags & VIDCF_DRAW) mywprintf(sspc_fmt, VIDCF_DRAW_W);
-	if (ii.dwFlags & VIDCF_FASTTEMPORALC) mywprintf(sspc_fmt, VIDCF_FASTTEMPORALC_W);
-	if (ii.dwFlags & VIDCF_FASTTEMPORALD) mywprintf(sspc_fmt, VIDCF_FASTTEMPORALD_W);
-	if (ii.dwFlags & VIDCF_QUALITYTIME) mywprintf(sspc_fmt, VIDCF_QUALITYTIME_W);
+        if (ii.dwFlags & VIDCF_QUALITY) mywprintf(L"%s ", L"VIDCF_QUALITY");
+        if (ii.dwFlags & VIDCF_CRUNCH) mywprintf(L"%s ", L"VIDCF_CRUNCH");
+        if (ii.dwFlags & VIDCF_TEMPORAL) mywprintf(L"%s ", L"VIDCF_TEMPORAL");
+        if (ii.dwFlags & VIDCF_COMPRESSFRAMES) mywprintf(L"%s ", L"VIDCF_COMPRESSFRAMES");
+        if (ii.dwFlags & VIDCF_DRAW) mywprintf(L"%s ", L"VIDCF_DRAW");
+        if (ii.dwFlags & VIDCF_FASTTEMPORALC) mywprintf(L"%s ", L"VIDCF_FASTTEMPORALC");
+        if (ii.dwFlags & VIDCF_FASTTEMPORALD) mywprintf(L"%s ", L"VIDCF_FASTTEMPORALD");
+        if (ii.dwFlags & VIDCF_QUALITYTIME) mywprintf(L"%s ", L"VIDCF_QUALITYTIME");
 
-	mywprintf(s_fmt, close_flags);
-	mywprintf(version_fmt,ii.dwVersion);
-	mywprintf(versicm_fmt,ii.dwVersionICM);
-	mywprintf(desc_fmt,ii.szDescription);
+        mywprintf(L"%s", L")\n");
+        mywprintf(L"\tdwVersion: 0x%08x\n", ii.dwVersion);
+        mywprintf(L"\tdwVersionICM: 0x%08x\n", ii.dwVersionICM);
+        mywprintf(L"\tszDescription: %s\n", ii.szDescription);
 	if (doabout) ICAbout(hic,0);
 	if (doconfigure && ICQueryConfigure(hic))
 		ICConfigure(hic,0);
