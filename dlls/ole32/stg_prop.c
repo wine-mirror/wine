c455053486e717b6518ec292444f0284ae442a2b
Nikolay Sivov <nsivov@codeweavers.com>
2020-11-13 00:07:05 +0300

    ole32/propstorage: Handle VT_I8/VT_UI8 when serializing properties.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ole32/stg_prop.c
+++ dlls/ole32/stg_prop.c
@@ -2103,6 +2103,7 @@ static HRESULT PropertyStorage_WritePropertyToStream(PropertyStorage_impl *This,
     HRESULT hr;
     LARGE_INTEGER seek;
     PROPERTYIDOFFSET propIdOffset;
+    ULARGE_INTEGER ularge;
     ULONG count;
 
     assert(var);
@@ -2159,6 +2160,13 @@ static HRESULT PropertyStorage_WritePropertyToStream(PropertyStorage_impl *This,
         StorageUtl_WriteDWord(&dwTemp, 0, var->u.lVal);
         hr = IStream_Write(This->stm, &dwTemp, sizeof(dwTemp), &count);
         bytesWritten = count;
+        break;
+    }
+    case VT_I8:
+    case VT_UI8:
+    {
+        StorageUtl_WriteULargeInteger(&ularge, 0, &var->u.uhVal);
+        hr = IStream_Write(This->stm, &ularge, sizeof(ularge), &bytesWritten);
         break;
     }
     case VT_LPSTR:
