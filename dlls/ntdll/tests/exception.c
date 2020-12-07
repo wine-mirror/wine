c073b1ad407ec285843c179221f1c9d0ccb0d375
Paul Gofman <pgofman@codeweavers.com>
2020-12-07 14:41:45 +0300

    ntdll/tests: Add a broken result for Win7u in test_extended_context().
    Fixes test failures on w7u testbot machines.
    
    Signed-off-by: Paul Gofman <pgofman@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntdll/tests/exception.c
+++ dlls/ntdll/tests/exception.c
@@ -7634,9 +7634,10 @@ static void test_extended_context(void)
     memset(&xs->YmmContext, 0xcc, sizeof(xs->YmmContext));
     bret = GetThreadContext(thread, context);
     ok(bret, "Got unexpected bret %#x, GetLastError() %u.\n", bret, GetLastError());
-    ok(xs->Mask == (sizeof(void *) == 4 ? 4 : 0), "Got unexpected Mask %s.\n", wine_dbgstr_longlong(xs->Mask));
+    ok(xs->Mask == (sizeof(void *) == 4 ? 4 : 0) || broken(sizeof(void *) == 4 && !xs->Mask) /* Win7u */,
+            "Got unexpected Mask %s.\n", wine_dbgstr_longlong(xs->Mask));
     for (i = 0; i < 16 * 4; ++i)
-        ok(((ULONG *)&xs->YmmContext)[i] == (sizeof(void *) == 4 ? (i < 8 * 4 ? 0 : 0x48484848) : 0xcccccccc),
+        ok(((ULONG *)&xs->YmmContext)[i] == (xs->Mask ? (i < 8 * 4 ? 0 : 0x48484848) : 0xcccccccc),
                 "Got unexpected value %#x, i %u.\n", ((ULONG *)&xs->YmmContext)[i], i);
 
     bret = ResumeThread(thread);
