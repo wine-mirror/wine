aa56ee08f6c436cb5d0cbebf6b2213d06570ac8a
Akihiro Sagawa <sagawa.aki@gmail.com>
2021-01-20 22:23:40 +0900

    winmm/tests: Expect TIME_BYTES format for TIME_MS request.
    Signed-off-by: Akihiro Sagawa <sagawa.aki@gmail.com>
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winmm/tests/wave.c
+++ dlls/winmm/tests/wave.c
@@ -519,14 +519,11 @@ static void check_position(int device, HWAVEOUT wout, DWORD bytes,
     rc=waveOutGetPosition(wout, &mmtime, sizeof(mmtime));
     ok(rc==MMSYSERR_NOERROR,
        "waveOutGetPosition(%s): rc=%s\n",dev_name(device),wave_out_error(rc));
-    if (mmtime.wType != TIME_MS && winetest_debug > 1)
-        trace("waveOutGetPosition(%s): TIME_MS not supported, returned %s\n",
-              dev_name(device), wave_time_format(mmtime.wType));
+    todo_wine ok(mmtime.wType == TIME_BYTES, "(waveOutGetPosition(%s): returned %s\n",
+       dev_name(device), wave_time_format(mmtime.wType));
     returned = time_to_bytes(&mmtime, pwfx);
-    ok(returned == bytes, "waveOutGetPosition(%s): returned %d ms, "
-       "(%d bytes), should be %d (%d bytes)\n", dev_name(device),
-       bytes_to_ms(returned, pwfx), returned,
-       bytes_to_ms(bytes, pwfx), bytes);
+    ok(returned == bytes, "waveOutGetPosition(%s): TIME_MS test failed\n",
+       dev_name(device));
 
     mmtime.wType = TIME_SMPTE;
     rc=waveOutGetPosition(wout, &mmtime, sizeof(mmtime));
