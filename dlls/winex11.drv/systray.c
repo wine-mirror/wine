02850d83bf064f05c38bf0e5b5ff645f5ccae555
Zebediah Figura <z.figura12@gmail.com>
2021-01-30 13:54:33 -0600

    winex11: Send systray messages with SendNotifyMessage().
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=50441
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 5c09d815f838ec349816840f6992187acd4f72bb)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/winex11.drv/systray.c
+++ dlls/winex11.drv/systray.c
@@ -451,7 +451,7 @@ static BOOL notify_owner( struct tray_icon *icon, UINT msg, LPARAM lparam )
     }
 
     TRACE( "relaying 0x%x\n", msg );
-    if (!PostMessageW( icon->owner, icon->callback_message, wp, lp ) &&
+    if (!SendNotifyMessageW( icon->owner, icon->callback_message, wp, lp ) &&
         (GetLastError() == ERROR_INVALID_WINDOW_HANDLE))
     {
         WARN( "application window was destroyed, removing icon %u\n", icon->id );
