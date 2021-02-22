edc4fb8e9f9817134370e7ff7e9f43cdb6be3ce7
Alexandre Julliard <julliard@winehq.org>
2021-02-22 09:24:08 +0100

    avrt: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/avrt/main.c
+++ dlls/avrt/main.c
@@ -38,22 +38,6 @@ static inline WCHAR *heap_strdupAW(const char *src)
     return dst;
 }
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    TRACE("(0x%p, %d, %p)\n", hinstDLL, fdwReason, lpvReserved);
-
-    switch (fdwReason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls(hinstDLL);
-            break;
-    }
-
-    return TRUE;
-}
-
 HANDLE WINAPI AvSetMmThreadCharacteristicsA(const char *name, DWORD *index)
 {
     WCHAR *nameW = NULL;
