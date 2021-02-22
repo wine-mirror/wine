7e33f3871b2f3f131907947381697b5dabff836e
Alexandre Julliard <julliard@winehq.org>
2021-02-22 09:24:58 +0100

    drmclien: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/drmclien/main.c
+++ dlls/drmclien/main.c
@@ -26,20 +26,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(drmclien);
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    switch (fdwReason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls(hinstDLL);
-            break;
-    }
-
-    return TRUE;
-}
-
 /***********************************************************************
  *		DllRegisterServer (drmclien.@)
  */
