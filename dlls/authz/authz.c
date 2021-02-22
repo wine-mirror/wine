1c5a00d26592037d11b91fa15d537a0eb3cc3fbb
Alexandre Julliard <julliard@winehq.org>
2021-02-22 09:24:04 +0100

    authz: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/authz/authz.c
+++ dlls/authz/authz.c
@@ -27,24 +27,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(authz);
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    TRACE("(0x%p, %d, %p)\n", hinstDLL, fdwReason, lpvReserved);
-
-    switch (fdwReason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls(hinstDLL);
-            break;
-        default:
-            break;
-    }
-
-    return TRUE;
-}
-
 /***********************************************************************
  *              AuthzInitializeResourceManager (AUTHZ.@)
  */
