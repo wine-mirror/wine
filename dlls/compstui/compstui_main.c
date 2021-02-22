3d154c7e2d3a1eba093cc99e79b69a2604197b66
Alexandre Julliard <julliard@winehq.org>
2021-02-22 09:24:16 +0100

    compstui: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/compstui/compstui_main.c
+++ dlls/compstui/compstui_main.c
@@ -31,25 +31,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(compstui);
 
-/*****************************************************
- *      DllMain
- */
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    TRACE("(%p, %d, %p)\n",hinstDLL, fdwReason, lpvReserved);
-
-    switch(fdwReason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;  /* prefer native version */
-
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls( hinstDLL );
-            break;
-    }
-    return TRUE;
-}
-
 /******************************************************************
  *      CommonPropertySheetUIA (COMPSTUI.@)
  *
