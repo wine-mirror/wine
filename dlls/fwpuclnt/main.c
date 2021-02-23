65124b6e83dd5cb61d0290a96d4086994f4ce1d7
Alexandre Julliard <julliard@winehq.org>
2021-02-23 10:05:55 +0100

    fwpuclnt: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/fwpuclnt/main.c
+++ dlls/fwpuclnt/main.c
@@ -26,24 +26,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(fwpuclnt);
 
-/*****************************************************
- * DllMain
- */
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    TRACE("(%p, %d, %p)\n", hinstDLL, fdwReason, lpvReserved);
-
-    switch (fdwReason)
-    {
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls( hinstDLL );
-        break;
-    }
-    return TRUE;
-}
-
 /***********************************************************************
  *   FwpmFreeMemory0 (FWPUCLNT.@)
  *
