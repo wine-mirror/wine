986c0a8cbba1af78b0bd9fbd300abd4abcd7853b
Alexandre Julliard <julliard@winehq.org>
2021-02-23 10:06:36 +0100

    mgmtapi: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/mgmtapi/mgmtapi.c
+++ dlls/mgmtapi/mgmtapi.c
@@ -23,21 +23,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(mgmtapi);
 
-BOOL WINAPI DllMain( HINSTANCE hinst, DWORD reason, LPVOID reserved )
-{
-    TRACE("%p, %u, %p\n", hinst, reason, reserved);
-
-    switch (reason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls( hinst );
-            break;
-    }
-    return TRUE;
-}
-
 BOOL WINAPI SnmpMgrTrapListen(HANDLE *available)
 {
     FIXME("stub: %p\n", available);
