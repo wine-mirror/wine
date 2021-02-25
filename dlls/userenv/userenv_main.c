e6a41c26697f1489690c818dc76e00d7a5008dd6
Alexandre Julliard <julliard@winehq.org>
2021-02-25 08:59:05 +0100

    userenv: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/userenv/userenv_main.c
+++ dlls/userenv/userenv_main.c
@@ -35,21 +35,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL( userenv );
 
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    TRACE("%p %d %p\n", hinstDLL, fdwReason, lpvReserved);
-
-    switch (fdwReason)
-    {
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls(hinstDLL);
-        break;
-    }
-    return TRUE;
-}
-
 static BOOL get_reg_value(WCHAR *env, HKEY hkey, const WCHAR *name, WCHAR *val, DWORD size)
 {
     DWORD type, res_size=0;
