96d1c58e7087dada6d5d9d45aff0dc07063b0625
Alexandre Julliard <julliard@winehq.org>
2021-02-25 08:59:48 +0100

    wsnmp32: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wsnmp32/wsnmp32.c
+++ dlls/wsnmp32/wsnmp32.c
@@ -24,21 +24,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(wsnmp32);
 
-BOOL WINAPI DllMain( HINSTANCE hinst, DWORD reason, LPVOID reserved )
-{
-    TRACE("%p, %u, %p\n", hinst, reason, reserved);
-
-    switch (reason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls( hinst );
-            break;
-    }
-    return TRUE;
-}
-
 SNMPAPI_STATUS WINAPI SnmpCleanup( void )
 {
     FIXME( "\n" );
