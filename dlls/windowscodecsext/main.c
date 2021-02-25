5b01a18dd38ac4fea6a6c3d13323b510c4c3c559
Alexandre Julliard <julliard@winehq.org>
2021-02-25 08:59:28 +0100

    windowscodecsext: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/windowscodecsext/main.c
+++ dlls/windowscodecsext/main.c
@@ -30,22 +30,6 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(wincodecs);
 
-BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
-{
-    TRACE("(%p, %u, %p)\n", instance, reason, reserved);
-
-    switch (reason)
-    {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
-        case DLL_PROCESS_ATTACH:
-            DisableThreadLibraryCalls(instance);
-            break;
-    }
-
-    return TRUE;
-}
-
 /******************************************************************
  * DllGetClassObject
  */
