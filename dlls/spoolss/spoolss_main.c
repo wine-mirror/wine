48b58cf1f416b1337c938715acfb23eb8700da3c
Alexandre Julliard <julliard@winehq.org>
2021-02-26 10:24:03 +0100

    spoolss: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/spoolss/spoolss_main.c
+++ dlls/spoolss/spoolss_main.c
@@ -46,8 +46,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
     TRACE("(%p, %d, %p)\n", hinstDLL, fdwReason, lpvReserved);
 
     switch (fdwReason) {
-        case DLL_WINE_PREATTACH:
-            return FALSE;  /* prefer native version */
         case DLL_PROCESS_ATTACH: {
             DisableThreadLibraryCalls(hinstDLL);
             break;
