96be0cfaabadfc521f00049d1d0ed3328789a88d
Alexandre Julliard <julliard@winehq.org>
2021-02-26 10:24:12 +0100

    vbscript: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/vbscript/vbscript_main.c
+++ dlls/vbscript/vbscript_main.c
@@ -272,8 +272,6 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
 
     switch(fdwReason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hInstDLL);
         vbscript_hinstance = hInstDLL;
