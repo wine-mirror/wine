cd6cc5695860d883cb4b4f3da9a853c3236e70cf
Alexandre Julliard <julliard@winehq.org>
2021-02-26 10:23:01 +0100

    msftedit: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/msftedit/msftedit_main.c
+++ dlls/msftedit/msftedit_main.c
@@ -46,8 +46,6 @@ BOOL WINAPI DllMain(HINSTANCE inst, DWORD reason, LPVOID reserved)
 
     switch(reason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE; /* prefer native version */
     case DLL_PROCESS_ATTACH:
         /* explicitly load riched20 since it creates the window classes at dll attach time */
         richedit = LoadLibraryW( L"riched20.dll" );
