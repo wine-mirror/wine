a9bfd7f584890f9d9a14f526dd41b7047a0a5412
Alexandre Julliard <julliard@winehq.org>
2021-02-26 10:21:57 +0100

    credui: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/credui/credui_main.c
+++ dlls/credui/credui_main.c
@@ -71,9 +71,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 
     switch (fdwReason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE;	/* prefer native version */
-
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hinstDLL);
         hinstCredUI = hinstDLL;
