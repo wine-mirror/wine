b8f6852bc5246e156f54e8a698372e73a1770071
Alexandre Julliard <julliard@winehq.org>
2021-02-26 10:24:31 +0100

    wshom.ocx: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wshom.ocx/wshom_main.c
+++ dlls/wshom.ocx/wshom_main.c
@@ -222,8 +222,6 @@ BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
 
     switch(fdwReason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE;  /* prefer native version */
     case DLL_PROCESS_ATTACH:
         wshom_instance = hInstDLL;
         DisableThreadLibraryCalls(wshom_instance);
