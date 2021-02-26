8b5bf20c4ab80f950726e7aac97d1ae194fb5ce9
Alexandre Julliard <julliard@winehq.org>
2021-02-26 10:22:56 +0100

    msctf: Use --prefer-native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/msctf/msctf.c
+++ dlls/msctf/msctf.c
@@ -556,8 +556,6 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD fdwReason, LPVOID fImpLoad)
     TRACE("%p 0x%x %p\n", hinst, fdwReason, fImpLoad);
     switch (fdwReason)
     {
-        case DLL_WINE_PREATTACH:
-            return FALSE;   /* prefer native version */
         case DLL_PROCESS_ATTACH:
             MSCTF_hinstance = hinst;
             tlsIndex = TlsAlloc();
