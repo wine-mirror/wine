095b40f40c1b4c14541764099eac17eb34305228
Alexandre Julliard <julliard@winehq.org>
2021-03-02 10:54:18 +0100

    dx8vb: Use --prefer_native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dx8vb/main.c
+++ dlls/dx8vb/main.c
@@ -44,8 +44,6 @@ BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, LPVOID reserved)
 
     switch (reason)
     {
-        case DLL_WINE_PREATTACH:
-            return FALSE;    /* prefer native version */
         case DLL_PROCESS_ATTACH:
             DisableThreadLibraryCalls(instance);
             instance_dx8vb = instance;
