d94b4a494b12b642597a84a33eb866128081bfa2
Dmitry Kislyuk <dimaki@rocketmail.com>
2021-03-01 18:31:26 -0600

    msctf: Quiet a spammy fixme in InputProcessorProfileMgr_GetActiveProfile.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=50744
    Signed-off-by: Dmitry Kislyuk <dimaki@rocketmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 0712b6b1a08a01ee397035372345f1ad38abe393)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/msctf/inputprocessor.c
+++ dlls/msctf/inputprocessor.c
@@ -866,8 +866,14 @@ static HRESULT WINAPI InputProcessorProfileMgr_UnregisterProfile(ITfInputProcess
 static HRESULT WINAPI InputProcessorProfileMgr_GetActiveProfile(ITfInputProcessorProfileMgr *iface, REFGUID catid,
         TF_INPUTPROCESSORPROFILE *pProfile)
 {
-    InputProcessorProfiles *This = impl_from_ITfInputProcessorProfileMgr(iface);
-    FIXME("(%p)->(%s %p)\n", This, debugstr_guid(catid), pProfile);
+    static int once;
+
+    if (!once++)
+    {
+        InputProcessorProfiles *This = impl_from_ITfInputProcessorProfileMgr(iface);
+        FIXME("(%p)->(%s %p)\n", This, debugstr_guid(catid), pProfile);
+    }
+
     return E_NOTIMPL;
 }
 
