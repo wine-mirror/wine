7b470425f74a23e988426bd8882175766be1c1f5
Alexandre Julliard <julliard@winehq.org>
2021-03-02 10:55:57 +0100

    qmgr: Use --prefer_native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/qmgr/qmgr_main.c
+++ dlls/qmgr/qmgr_main.c
@@ -47,8 +47,6 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 
     switch (fdwReason)
     {
-        case DLL_WINE_PREATTACH:
-            return FALSE;  /* prefer native version */
         case DLL_PROCESS_ATTACH:
             DisableThreadLibraryCalls(hinstDLL);
             hInst = hinstDLL;
