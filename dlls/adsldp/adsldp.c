55ae768fdcdb9286adde36198d519c04ebab1273
Alexandre Julliard <julliard@winehq.org>
2021-03-02 10:53:33 +0100

    adsldp: Use --prefer_native instead of DLL_WINE_PREATTACH.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/adsldp/adsldp.c
+++ dlls/adsldp/adsldp.c
@@ -2135,9 +2135,6 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, void *reserved)
 
     switch (reason)
     {
-    case DLL_WINE_PREATTACH:
-        return FALSE; /* prefer native version */
-
     case DLL_PROCESS_ATTACH:
         adsldp_hinst = hinst;
         DisableThreadLibraryCalls(hinst);
