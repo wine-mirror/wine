5a813f6f6e15bb00dcb64dde7907b08098d52d60
Jacek Caban <jacek@codeweavers.com>
2021-03-11 20:39:54 +0100

    mshtml: Initialize attribute object with compatibility mode.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/mshtml/htmlattr.c
+++ dlls/mshtml/htmlattr.c
@@ -497,6 +497,7 @@ HTMLDOMAttribute *unsafe_impl_from_IHTMLDOMAttribute(IHTMLDOMAttribute *iface)
 
 HRESULT HTMLDOMAttribute_Create(const WCHAR *name, HTMLElement *elem, DISPID dispid, HTMLDOMAttribute **attr)
 {
+    compat_mode_t compat_mode = elem ? dispex_compat_mode(&elem->node.event_target.dispex) : COMPAT_MODE_QUIRKS;
     HTMLAttributeCollection *col;
     HTMLDOMAttribute *ret;
     HRESULT hres;
@@ -511,8 +512,8 @@ HRESULT HTMLDOMAttribute_Create(const WCHAR *name, HTMLElement *elem, DISPID dis
     ret->dispid = dispid;
     ret->elem = elem;
 
-    init_dispex(&ret->dispex, (IUnknown*)&ret->IHTMLDOMAttribute_iface,
-            &HTMLDOMAttribute_dispex);
+    init_dispex_with_compat_mode(&ret->dispex, (IUnknown*)&ret->IHTMLDOMAttribute_iface,
+                                 &HTMLDOMAttribute_dispex, compat_mode);
 
     /* For attributes attached to an element, (elem,dispid) pair should be valid used for its operation. */
     if(elem) {
