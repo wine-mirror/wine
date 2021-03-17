31816a90c604f209387dda887a17ac9a8072a2a5
Alexandre Julliard <julliard@winehq.org>
2021-03-17 11:49:16 +0100

    ntoskrnl: Load driver dependencies from the driver directory.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -3691,7 +3691,7 @@ static HMODULE load_driver( const WCHAR *driver_name, const UNICODE_STRING *keyn
 
     TRACE( "loading driver %s\n", wine_dbgstr_w(str) );
 
-    module = LoadLibraryW( str );
+    module = LoadLibraryExW( str, 0, LOAD_WITH_ALTERED_SEARCH_PATH );
 
     if (module && load_image_notify_routine_count)
     {
