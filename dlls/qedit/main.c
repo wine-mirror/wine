b9393340f77508a0b45cd46e903aeb3850bf006e
Jacek Caban <jacek@codeweavers.com>
2021-03-25 21:40:05 +0100

    qedit: Use nameless unions.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/qedit/main.c
+++ dlls/qedit/main.c
@@ -17,6 +17,8 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
+#define WINE_NO_NAMELESS_EXTENSION
+
 #include "qedit_private.h"
 #include "rpcproxy.h"
 #include "wine/debug.h"
