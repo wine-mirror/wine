7ff591d7dee84be729c037e88a25cc307ad38cf2
Hans Leidekker <hans@codeweavers.com>
2021-03-30 10:35:30 +0200

    dnsapi/tests: Add a query test.
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dnsapi/tests/query.c
+++ dlls/dnsapi/tests/query.c
@@ -38,6 +38,11 @@ static void test_DnsQuery(void)
     DNS_RECORDW *rec;
     DNS_STATUS status;
 
+    rec = NULL;
+    status = DnsQuery_W(L"winehq.org", DNS_TYPE_A, DNS_QUERY_STANDARD, NULL, &rec, NULL);
+    ok(status == ERROR_SUCCESS, "DnsQuery_W(%s) => %d\n", wine_dbgstr_w(name), status);
+    DnsRecordListFree(rec, DnsFreeRecordList);
+
     status = DnsQuery_W(L"", DNS_TYPE_SRV, DNS_QUERY_STANDARD, NULL, &rec, NULL);
     ok(status == DNS_ERROR_RCODE_NAME_ERROR || status == DNS_INFO_NO_RECORDS || status == ERROR_INVALID_NAME /* XP */, "got %u\n", status);
 
