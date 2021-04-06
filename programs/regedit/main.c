8f2f59eee1ef872e22cd4b578cae49091f50ef9d
Alexandre Julliard <julliard@winehq.org>
2021-04-06 10:55:30 +0200

    regedit: 64-bit regedit.exe is in c:\windows.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=50933
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/regedit/main.c
+++ programs/regedit/main.c
@@ -148,15 +148,14 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     if (IsWow64Process( GetCurrentProcess(), &is_wow64 ) && is_wow64)
     {
+        static const WCHAR filename[] = L"C:\\windows\\regedit.exe";
         STARTUPINFOW si;
         PROCESS_INFORMATION pi;
-        WCHAR filename[MAX_PATH];
         void *redir;
         DWORD exit_code;
 
         memset( &si, 0, sizeof(si) );
         si.cb = sizeof(si);
-        GetModuleFileNameW( 0, filename, MAX_PATH );
 
         Wow64DisableWow64FsRedirection( &redir );
         if (CreateProcessW( filename, GetCommandLineW(), NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi ))
