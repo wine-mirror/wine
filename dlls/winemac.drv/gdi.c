66b593049c62978fbbb607117b78fde35f6db936
Piotr Caban <piotr@codeweavers.com>
2021-04-12 17:55:23 +0200

    winemac.drv: Take all online monitors into account when computing desktop rect.
    Signed-off-by: Piotr Caban <piotr@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winemac.drv/gdi.c
+++ dlls/winemac.drv/gdi.c
@@ -68,7 +68,7 @@ static void compute_desktop_rect(void)
     uint32_t count, i;
 
     desktop_rect = CGRectNull;
-    if (CGGetActiveDisplayList(ARRAY_SIZE(displayIDs), displayIDs, &count) != kCGErrorSuccess ||
+    if (CGGetOnlineDisplayList(ARRAY_SIZE(displayIDs), displayIDs, &count) != kCGErrorSuccess ||
         !count)
     {
         displayIDs[0] = CGMainDisplayID();
