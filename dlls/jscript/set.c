6cc5aa4286ed62fd603ac84b186f5eedf07a0744
Jacek Caban <jacek@codeweavers.com>
2021-04-27 18:12:01 +0200

    jscript: Add Map.prototype.get implementation.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/jscript/set.c
+++ dlls/jscript/set.c
@@ -231,8 +231,20 @@ static HRESULT Map_forEach(script_ctx_t *ctx, vdisp_t *jsthis, WORD flags, unsig
 static HRESULT Map_get(script_ctx_t *ctx, vdisp_t *jsthis, WORD flags, unsigned argc, jsval_t *argv,
         jsval_t *r)
 {
-    FIXME("%p\n", jsthis);
-    return E_NOTIMPL;
+    jsval_t key = argc >= 1 ? argv[0] : jsval_undefined();
+    struct jsval_map_entry *entry;
+    MapInstance *map;
+
+    if(!(map = get_map_this(jsthis))) return JS_E_MAP_EXPECTED;
+
+    TRACE("%p (%s)\n", map, debugstr_jsval(key));
+
+    if(!(entry = get_map_entry(map, key))) {
+        if(r) *r = jsval_undefined();
+        return S_OK;
+    }
+
+    return r ? jsval_copy(entry->value, r) : S_OK;
 }
 
 static HRESULT Map_set(script_ctx_t *ctx, vdisp_t *jsthis, WORD flags, unsigned argc, jsval_t *argv,
