beef84721d81980a93ae57a344294fe3ad86fd72
Zebediah Figura <z.figura12@gmail.com>
2021-05-03 21:42:53 -0500

    ntdll: Quiet the FIXME for unmapped NTSTATUS values.
    Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntdll/error.c
+++ dlls/ntdll/error.c
@@ -60,7 +60,7 @@ ULONG WINAPI RtlNtStatusToDosErrorNoTeb( NTSTATUS status )
 
     ret = map_status( status );
     if (ret == ERROR_MR_MID_NOT_FOUND && status != STATUS_MESSAGE_NOT_FOUND)
-        FIXME( "no mapping for %08x\n", status );
+        WARN( "no mapping for %08x\n", status );
     return ret;
 }
 
