7529f46a850bc43982b8c5a5a236c880049d274e
Zhiyi Zhang <zzhang@codeweavers.com>
2021-05-13 18:37:38 +0800

    include: Add fUseStandardBidi field in SCRIPT_CONTROL.
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/usp10.h
+++ include/usp10.h
@@ -98,7 +98,8 @@ typedef struct tag_SCRIPT_CONTROL {
   DWORD fNumericOverride	:1;
   DWORD fLegacyBidiClass	:1;
   DWORD fMergeNeutralItems	:1;
-  DWORD fReserved		:7;
+  DWORD fUseStandardBidi	:1;
+  DWORD fReserved		:6;
 } SCRIPT_CONTROL;
 
 typedef struct {
