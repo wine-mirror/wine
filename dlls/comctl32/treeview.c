02bfec6419dec45b018100526f327007d51168e7
Zhiyi Zhang <zzhang@codeweavers.com>
2021-06-22 16:20:56 +0800

    comctl32/treeview: Draw parent background before drawing control parts.
    Fix residual glyphs on background when drawing treeview buttons with themes that contains
    transparent button glyphs.
    
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/comctl32/treeview.c
+++ dlls/comctl32/treeview.c
@@ -2901,6 +2901,7 @@ TREEVIEW_Refresh(TREEVIEW_INFO *infoPtr, HDC hdc, const RECT *rc)
     HWND hwnd = infoPtr->hwnd;
     RECT rect = *rc;
     TREEVIEW_ITEM *item;
+    HTHEME theme;
 
     if (infoPtr->clientHeight == 0 || infoPtr->clientWidth == 0)
     {
@@ -2915,6 +2916,12 @@ TREEVIEW_Refresh(TREEVIEW_INFO *infoPtr, HDC hdc, const RECT *rc)
     {
 	ReleaseDC(hwnd, hdc);
 	return;
+    }
+
+    if (infoPtr->dwStyle & TVS_HASBUTTONS && (theme = GetWindowTheme(infoPtr->hwnd)))
+    {
+        if (IsThemeBackgroundPartiallyTransparent(theme, TVP_GLYPH, 0))
+            DrawThemeParentBackground(infoPtr->hwnd, hdc, NULL);
     }
 
     for (item = infoPtr->root->firstChild;
