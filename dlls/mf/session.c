7dba45f6ec7a25df69def7918ad408f07c88a20b
Nikolay Sivov <nsivov@codeweavers.com>
2021-06-23 14:03:06 +0300

    mf/session: Treat only started/paused/stopped as valid states for start command.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/mf/session.c
+++ dlls/mf/session.c
@@ -862,13 +862,11 @@ static void session_start(struct media_session *session, const GUID *time_format
             FIXME("Seeking is not implemented.\n");
             session_command_complete(session);
             break;
-        case SESSION_STATE_CLOSED:
+        default:
             IMFMediaEventQueue_QueueEventParamVar(session->event_queue, MESessionStarted, &GUID_NULL,
                     MF_E_INVALIDREQUEST, NULL);
             session_command_complete(session);
             break;
-        default:
-            ;
     }
 }
 
