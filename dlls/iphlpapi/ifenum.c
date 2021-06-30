ed79dd2125d306405b9486a5ae64092567c70634
Huw Davies <huw@codeweavers.com>
2021-06-30 09:59:23 +0100

    iphlpapi: For unknown interface types set the physical address length to zero.
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/iphlpapi/ifenum.c
+++ dlls/iphlpapi/ifenum.c
@@ -498,7 +498,7 @@ static DWORD getInterfacePhysicalByName(const char *name, PDWORD len, PBYTE addr
           break;
 #endif
         default:
-          addrLen = min(MAX_INTERFACE_PHYSADDR, sizeof(ifr.ifr_hwaddr.sa_data));
+          addrLen = 0;
           *type = MIB_IF_TYPE_OTHER;
       }
       if (addrLen > *len) {
