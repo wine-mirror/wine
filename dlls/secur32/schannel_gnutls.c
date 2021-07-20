wine-mirror/wine

commit b8aaf86b2dbb8ecb3f7094cc40a0df89bb2add27

secur32: Use blocking mode for DTLS sessions.
Signed-off-by: Hans Leidekker <hans@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/secur32/schannel_gnutls.c
@@ -292,10 +292,6 @@ static DWORD CDECL schan_get_enabled_protocols(void)
 
 static int pull_timeout(gnutls_transport_ptr_t transport, unsigned int timeout)
 {
-    struct schan_transport *t = (struct schan_transport *)transport;
-    SIZE_T count = 0;
-
-    if (callbacks->get_buffer(t, &t->in, &count)) return 1;
     return 0;
 }
 
@@ -309,7 +305,7 @@ static BOOL CDECL schan_create_session(schan_session *session, schan_credentials
 
     if (cred->enabled_protocols & (SP_PROT_DTLS1_0_CLIENT | SP_PROT_DTLS1_2_CLIENT))
     {
-        flags |= GNUTLS_DATAGRAM | GNUTLS_NONBLOCK;
+        flags |= GNUTLS_DATAGRAM;
     }
 
     err = pgnutls_init(s, flags);

dlls/secur32/schannel.c
@@ -879,11 +879,6 @@ static inline SIZE_T read_record_size(const BYTE *buf, SIZE_T header_size)
     return (buf[header_size - 2] << 8) | buf[header_size - 1];
 }
 
-static inline BOOL is_dtls_context(const struct schan_context *ctx)
-{
-    return (ctx->header_size == HEADER_SIZE_DTLS);
-}
-
 /***********************************************************************
  *              InitializeSecurityContextW
  */
@@ -1011,7 +1006,7 @@ static SECURITY_STATUS SEC_ENTRY schan_InitializeSecurityContextW(
                 return SEC_E_INCOMPLETE_MESSAGE;
             }
         }
-        else if (!is_dtls_context(ctx)) return SEC_E_INCOMPLETE_MESSAGE;
+        else return SEC_E_INCOMPLETE_MESSAGE;
 
         TRACE("Using expected_size %lu.\n", expected_size);
     }

commit 8ed81720e81b20a134d9cd5c6bb50183752d9e82

secur32: Set error flag on pull callback.
Fixes a regression introduced in b8aaf86b2dbb8ecb3f7094cc40a0df89bb2add27

Partial revert the pull_timeout function, which now return -1 and sets the
transport error to try again.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=51440
Signed-off-by: Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
Signed-off-by: Hans Leidekker <hans@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/secur32/schannel_gnutls.c
@@ -292,7 +292,15 @@ static DWORD CDECL schan_get_enabled_protocols(void)
 
 static int pull_timeout(gnutls_transport_ptr_t transport, unsigned int timeout)
 {
-    return 0;
+    struct schan_transport *t = (struct schan_transport*)transport;
+    gnutls_session_t s = (gnutls_session_t)callbacks->get_session_for_transport(t);
+    SIZE_T count = 0;
+
+    TRACE("\n");
+
+    if (callbacks->get_buffer(t, &t->in, &count)) return 1;
+    pgnutls_transport_set_errno(s, EAGAIN);
+    return -1;
 }
 
 static BOOL CDECL schan_create_session(schan_session *session, schan_credentials *cred)