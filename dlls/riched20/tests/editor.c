9f72c5601173728c52626342a1ec6c9a4c70670f
Francois Gouget <fgouget@free.fr>
2021-07-23 13:18:35 +0200

    riched20: Tweak the wording of a comment.
    Signed-off-by: Francois Gouget <fgouget@free.fr>
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/riched20/tests/editor.c
+++ dlls/riched20/tests/editor.c
@@ -8995,8 +8995,8 @@ static void test_EM_SELECTIONTYPE(void)
 START_TEST( editor )
 {
   BOOL ret;
-  /* Must explicitly LoadLibrary(). The test has no references to functions in
-   * RICHED20.DLL, so the linker doesn't actually link to it. */
+  /* Must explicitly LoadLibrary(). The test has no reference to functions in
+   * RICHED20.DLL, so the linker does not actually link to it. */
   hmoduleRichEdit = LoadLibraryA("riched20.dll");
   ok(hmoduleRichEdit != NULL, "error: %d\n", (int) GetLastError());
   is_lang_japanese = (PRIMARYLANGID(GetSystemDefaultLangID()) == LANG_JAPANESE);
