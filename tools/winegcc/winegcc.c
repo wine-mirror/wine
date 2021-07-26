de4c91e0a1ac65017190bead4c053ae65db8f80e
Alexandre Julliard <julliard@winehq.org>
2021-07-26 11:33:43 +0200

    Revert "winegcc: Support -Wl,foo=... style linker options."
    This reverts commit fcda0afdd429e11d75dc61f628e40a6c8973ce44.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=51413
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- tools/winegcc/winegcc.c
+++ tools/winegcc/winegcc.c
@@ -1982,7 +1982,7 @@ int main(int argc, char **argv)
                     if (strncmp("-Wl,", opts.args->base[i], 4) == 0)
 		    {
                         unsigned int j;
-                        strarray* Wl = strarray_fromstring(opts.args->base[i] + 4, ",=");
+                        strarray* Wl = strarray_fromstring(opts.args->base[i] + 4, ",");
                         for (j = 0; j < Wl->size; j++)
                         {
                             if (!strcmp(Wl->base[j], "--image-base") && j < Wl->size - 1)
