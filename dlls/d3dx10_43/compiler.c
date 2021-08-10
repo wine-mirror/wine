5279212cd93a00bdf04ebe5568df5e5e82098984
Nikolay Sivov <nsivov@codeweavers.com>
2021-08-10 15:56:42 +0200

    d3dx10: Partially implement D3DX10CreateEffectFromMemory().
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

---
+++ dlls/d3dx10_43/compiler.c
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Nikolay Sivov for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ *
+ */
+
+#include "wine/debug.h"
+
+#define COBJMACROS
+
+#include "d3d10_1.h"
+#include "d3dx10.h"
+#include "d3dcompiler.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(d3dx);
+
+
+HRESULT WINAPI D3DX10CreateEffectFromMemory(const void *data, SIZE_T datasize, const char *filename,
+        const D3D10_SHADER_MACRO *defines, ID3D10Include *include, const char *profile,
+        UINT shader_flags, UINT effect_flags, ID3D10Device *device, ID3D10EffectPool *effect_pool,
+        ID3DX10ThreadPump *pump, ID3D10Effect **effect, ID3D10Blob **errors, HRESULT *hresult)
+{
+    ID3D10Blob *code;
+    HRESULT hr;
+
+    TRACE("data %p, datasize %lu, filename %s, defines %p, include %p, profile %s, shader_flags %#x,"
+            "effect_flags %#x, device %p, effect_pool %p, pump %p, effect %p, errors %p, hresult %p.\n",
+            data, datasize, debugstr_a(filename), defines, include, debugstr_a(profile),
+            shader_flags, effect_flags, device, effect_pool, pump, effect, errors, hresult);
+
+    if (pump)
+        FIXME("Asynchronous mode is not supported.\n");
+
+    if (!include)
+        include = D3D_COMPILE_STANDARD_FILE_INCLUDE;
+
+    if (FAILED(hr = D3DCompile(data, datasize, filename, defines, include, "main", profile,
+            shader_flags, effect_flags, &code, errors)))
+    {
+        WARN("Effect compilation failed, hr %#x.\n", hr);
+        return hr;
+    }
+
+    hr = D3D10CreateEffectFromMemory(ID3D10Blob_GetBufferPointer(code), ID3D10Blob_GetBufferSize(code),
+            effect_flags, device, effect_pool, effect);
+    ID3D10Blob_Release(code);
+
+    return hr;
+}
