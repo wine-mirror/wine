ee99bd4bc1b7ef4c18d5254a76d72d3bd38f41e8
Alexandre Julliard <julliard@winehq.org>
2021-08-20 11:34:49 +0200

    ntdll: Always send a Wow context to the server for Wow64 processes.
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/ntdll/unix/thread.c
+++ dlls/ntdll/unix/thread.c
@@ -1025,6 +1025,9 @@ static void contexts_to_server( context_t server_contexts[2], CONTEXT *context )
         context_to_server( &server_contexts[count++], native_machine, native_context, native_machine );
         if (wow_context) context_to_server( &server_contexts[count++], main_image_info.Machine,
                                             wow_context, main_image_info.Machine );
+        else if (native_machine != main_image_info.Machine)
+            context_to_server( &server_contexts[count++], main_image_info.Machine,
+                               native_context, native_machine );
     }
     else
         context_to_server( &server_contexts[count++], native_machine,
