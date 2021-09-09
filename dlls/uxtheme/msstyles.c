f0143c1bc9b1186324fafa896fe242eab4da727c
Zhiyi Zhang <zzhang@codeweavers.com>
2021-09-09 14:46:24 +0800

    uxtheme: Get and set system metrics in 96 DPI.
    Fix a bug that font size may be smaller than normal after toggling theming.
    
    For example, the following steps will change the system metrics to incorrect values.
    1. Open winecfg, and set DPI to 192. Do not restart winecfg.
    2. Change the theme to Light. UXTHEME_BackupSystemMetrics() backs up system metrics in 96 DPI
    because the current DPI is still 96.
    3. Restart winecfg and turn off theming. UXTHEME_RestoreSystemMetrics() restores system metrics in
    192 DPI because the current DPI is 192. So system metrics will be scaled to 1/2 of the original size.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=37592
    Signed-off-by: Zhiyi Zhang <zzhang@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit 7290db3e7a9652fcc39717b2fb492071dd52c641)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/uxtheme/msstyles.c
+++ dlls/uxtheme/msstyles.c
@@ -830,12 +830,16 @@ static BOOL parse_handle_nonclient_size (struct PARSENONCLIENTSTATE* state,
 
 static void parse_apply_nonclient (struct PARSENONCLIENTSTATE* state)
 {
+    DPI_AWARENESS_CONTEXT old_context;
+
     if (state->metricsDirty)
     {
+        old_context = SetThreadDpiAwarenessContext(DPI_AWARENESS_CONTEXT_UNAWARE);
         SystemParametersInfoW (SPI_SETNONCLIENTMETRICS, sizeof (state->metrics),
             &state->metrics, 0);
         SystemParametersInfoW (SPI_SETICONTITLELOGFONT, sizeof (state->iconTitleFont),
             &state->iconTitleFont, 0);
+        SetThreadDpiAwarenessContext(old_context);
     }
 }
 
