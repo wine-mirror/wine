wine-mirror/wine

commit 0ea9fe999c1ed3a8b645476d97d2e0221be17bb5

winedbg: Build with msvcrt.
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

programs/winedbg/expr.c
@@ -18,8 +18,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "config.h"
-
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>

programs/winedbg/Makefile.in
@@ -2,7 +2,7 @@ MODULE    = winedbg.exe
 IMPORTS   = dbghelp advapi32
 DELAYIMPORTS = comdlg32 shell32 comctl32 user32 gdi32 ws2_32
 
-EXTRADLLFLAGS = -mconsole -mcygwin
+EXTRADLLFLAGS = -mconsole
 
 C_SRCS = \
 	be_arm.c \

programs/winedbg/break.c
@@ -20,7 +20,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "config.h"
 #include "debugger.h"
 #include "wine/debug.h"
 

programs/winedbg/crashdlg.c
@@ -26,8 +26,6 @@
 #include "shellapi.h"
 #include "psapi.h"
 
-#include "wine/unicode.h"
-
 #include "resource.h"
 
 #define MAX_PROGRAM_NAME_LENGTH 80
@@ -79,7 +77,7 @@ static WCHAR *get_program_name(HANDLE hProcess)
         return unidentified;
     }
 
-    programname = strrchrW(image_name, '\\');
+    programname = wcsrchr(image_name, '\\');
     if (programname != NULL)
         programname++;
     else
@@ -89,7 +87,7 @@ static WCHAR *get_program_name(HANDLE hProcess)
      * user-friendly program name */
 
     /* don't display a too long string to the user */
-    if (strlenW(programname) >= MAX_PROGRAM_NAME_LENGTH)
+    if (lstrlenW(programname) >= MAX_PROGRAM_NAME_LENGTH)
     {
         programname[MAX_PROGRAM_NAME_LENGTH - 4] = '.';
         programname[MAX_PROGRAM_NAME_LENGTH - 3] = '.';
@@ -382,7 +380,7 @@ int display_crash_dialog(void)
     hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dbg_curr_pid);
     g_ProgramName = get_program_name(hProcess);
     CloseHandle(hProcess);
-    if (!strcmpW( g_ProgramName, winedeviceW )) return TRUE;
+    if (!wcscmp( g_ProgramName, winedeviceW )) return TRUE;
     InitCommonControlsEx( &init );
     return DialogBoxW(GetModuleHandleW(NULL), MAKEINTRESOURCEW(IDD_CRASH_DLG), NULL, crash_dlg_proc);
 }

programs/winedbg/db_disasm64.c
@@ -33,12 +33,12 @@
 #include "debugger.h"
 
 #if defined(__GNUC__) && (__GNUC__ >= 3)
-static int             (*db_printf)(const char* format, ...) __attribute__((format (printf,1,2)));
+static int WINAPIV (*db_printf)(const char* format, ...) __attribute__((format (printf,1,2)));
 #else
-static int             (*db_printf)(const char* format, ...);
+static int WINAPIV (*db_printf)(const char* format, ...);
 #endif
 
-static int             no_printf(const char* format, ...) {return 0;}
+static int WINAPIV no_printf(const char* format, ...) {return 0;}
 
 typedef DWORD_PTR db_addr_t;
 typedef BOOL boolean_t;

programs/winedbg/dbg.y
@@ -21,9 +21,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "config.h"
-#include "wine/port.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>

programs/winedbg/debug.l
@@ -22,7 +22,6 @@
 %option noinput nounput always-interactive 8bit prefix="dbg_"
 
 %{
-#include "config.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>

programs/winedbg/debugger.h
@@ -447,9 +447,9 @@ extern struct dbg_type  types_find_type(DWORD64 linear, const char* name, enum S
 extern void	        dbg_outputW(const WCHAR* buffer, int len);
 extern const char*      dbg_W2A(const WCHAR* buffer, unsigned len);
 #ifdef __GNUC__
-extern int	        dbg_printf(const char* format, ...) __attribute__((format (printf,1,2)));
+extern int WINAPIV      dbg_printf(const char* format, ...) __attribute__((format (printf,1,2)));
 #else
-extern int	        dbg_printf(const char* format, ...);
+extern int WINAPIV      dbg_printf(const char* format, ...);
 #endif
 extern const struct dbg_internal_var* dbg_get_internal_var(const char*);
 extern BOOL             dbg_interrupt_debuggee(void);

commit e3fc699afa8a7712272a16d20ac5880db74b46be

winedbg: Fix result type in expression's computation.
All internal computation are made with DWORD_PTR and LONG_PTR,
so use corresponding internal type for the result.

Regression introduced in 0ea9fe999c1ed3a8b645476d97d2e0221be17bb5.

Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

programs/winedbg/expr.c
@@ -476,7 +476,7 @@ struct dbg_lvalue expr_eval(struct expr* exp)
         exp2 = expr_eval(exp->un.binop.exp2);
         if (exp1.type.id == dbg_itype_none || exp2.type.id == dbg_itype_none)
             RaiseException(DEBUG_STATUS_BAD_TYPE, 0, 0, NULL);
-        rtn.type.id = dbg_itype_signed_int;
+        rtn.type.id = dbg_itype_signed_long_int;
         rtn.type.module = 0;
         rtn.addr.Offset = (ULONG_PTR)&exp->un.binop.result;
         type1 = exp1.type;
@@ -609,7 +609,7 @@ struct dbg_lvalue expr_eval(struct expr* exp)
         exp1 = expr_eval(exp->un.unop.exp1);
         if (exp1.type.id == dbg_itype_none) RaiseException(DEBUG_STATUS_BAD_TYPE, 0, 0, NULL);
         rtn.addr.Offset = (ULONG_PTR)&exp->un.unop.result;
-        rtn.type.id     = dbg_itype_signed_int;
+        rtn.type.id     = dbg_itype_signed_long_int;
         rtn.type.module = 0;
         switch (exp->un.unop.unop_type)
 	{