wine-mirror/wine

commit 567beb6b2edba07051a75d45a9b2f1516568c0e7

server: Avoid leaking the poll output buffer if the request is terminated irregularly (Valgrind).
Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

server/sock.c
@@ -795,6 +795,7 @@ static void free_poll_req( void *private )
     release_object( req->async );
     release_object( req->iosb );
     list_remove( &req->entry );
+    free( req->output );
     free( req );
 }
 
@@ -849,6 +850,7 @@ static void complete_async_poll( struct poll_req *req, unsigned int status )
 
     /* pass 0 as result; client will set actual result size */
     async_request_complete( req->async, status, 0, req->count * sizeof(*req->output), req->output );
+    req->output = NULL;
 }
 
 static void complete_async_polls( struct sock *sock, int event, int error )

commit e6009c5b55200b614e328c4b38549bf8693e764b

server: Avoid touching the poll_req structure after calling async_request_complete() (Valgrind).
Fixes: 567beb6b2edba07051a75d45a9b2f1516568c0e7
Signed-off-by: Zebediah Figura <zfigura@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

server/sock.c
@@ -838,6 +838,7 @@ static int get_poll_flags( struct sock *sock, int event )
 
 static void complete_async_poll( struct poll_req *req, unsigned int status )
 {
+    struct poll_socket_output *output = req->output;
     unsigned int i;
 
     for (i = 0; i < req->count; ++i)
@@ -849,8 +850,8 @@ static void complete_async_poll( struct poll_req *req, unsigned int status )
     }
 
     /* pass 0 as result; client will set actual result size */
-    async_request_complete( req->async, status, 0, req->count * sizeof(*req->output), req->output );
     req->output = NULL;
+    async_request_complete( req->async, status, 0, req->count * sizeof(*output), output );
 }
 
 static void complete_async_polls( struct sock *sock, int event, int error )