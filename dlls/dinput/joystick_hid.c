62f5bbf3ed1c42238f56e35095ab88b9bd9697aa
Rémi Bernon <rbernon@codeweavers.com>
2021-11-08 09:34:58 +0100

    dinput: Enumerate HID joystick with dinput <= 7 and device type 0.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=51953
    Signed-off-by: Rémi Bernon <rbernon@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/dinput/joystick_hid.c
+++ dlls/dinput/joystick_hid.c
@@ -1470,7 +1470,7 @@ HRESULT hid_joystick_enum_device( DWORD type, DWORD flags, DIDEVICEINSTANCEW *in
 
     if (instance->dwSize != sizeof(DIDEVICEINSTANCEW))
         return S_FALSE;
-    if (version < 0x0800 && type != DIDEVTYPE_JOYSTICK)
+    if (version < 0x0800 && type != 0 && type != DIDEVTYPE_JOYSTICK)
         return S_FALSE;
     if (version >= 0x0800 && type != DI8DEVCLASS_ALL && type != DI8DEVCLASS_GAMECTRL)
         return S_FALSE;
