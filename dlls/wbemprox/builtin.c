b48ce9212e0e2d44da32090e5a72388bcc2ef1af
Louis Lenders <xerox.xerox2000x@gmail.com>
2021-11-08 23:26:14 +0100

    wbemprox: Add empty DNSDomain to Win32_NetworkAdapterConfiguration.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=51938
    Signed-off-by: Louis Lenders <xerox.xerox2000x@gmail.com>
    Signed-off-by: Hans Leidekker <hans@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>
    (cherry picked from commit d68ea16266eae2108e084f34b4ad4d47a4a51782)
    Signed-off-by: Michael Stefaniuc <mstefani@winehq.org>

--- dlls/wbemprox/builtin.c
+++ dlls/wbemprox/builtin.c
@@ -214,6 +214,7 @@ static const struct column col_networkadapterconfig[] =
     { L"DefaultIPGateway",     CIM_STRING|CIM_FLAG_ARRAY|COL_FLAG_DYNAMIC },
     { L"Description",          CIM_STRING|COL_FLAG_DYNAMIC },
     { L"DHCPEnabled",          CIM_BOOLEAN },
+    { L"DNSDomain",            CIM_STRING },
     { L"DNSHostName",          CIM_STRING|COL_FLAG_DYNAMIC },
     { L"DNSServerSearchOrder", CIM_STRING|CIM_FLAG_ARRAY|COL_FLAG_DYNAMIC },
     { L"Index",                CIM_UINT32|COL_FLAG_KEY },
@@ -623,6 +624,7 @@ struct record_networkadapterconfig
     const struct array *defaultipgateway;
     const WCHAR        *description;
     int                 dhcpenabled;
+    const WCHAR        *dnsdomain;
     const WCHAR        *dnshostname;
     const struct array *dnsserversearchorder;
     UINT32              index;
@@ -2942,6 +2944,7 @@ static enum fill_status fill_networkadapterconfig( struct table *table, const st
         rec->defaultipgateway     = get_defaultipgateway( aa->FirstGatewayAddress );
         rec->description          = heap_strdupW( aa->Description );
         rec->dhcpenabled          = -1;
+        rec->dnsdomain            = L"";
         rec->dnshostname          = get_dnshostname( aa->FirstUnicastAddress );
         rec->dnsserversearchorder = get_dnsserversearchorder( aa->FirstDnsServerAddress );
         rec->index                = aa->u.s.IfIndex;
