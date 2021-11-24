37265acd513961255205a0d775794dc4e3f9ff74
Huw Davies <huw@codeweavers.com>
2021-11-24 11:26:49 +0000

    winecoreaudio: Make the coreaudio_stream structure private to the unixlib.
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winecoreaudio.drv/coreaudio.c
+++ dlls/winecoreaudio.drv/coreaudio.c
@@ -72,6 +72,29 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(coreaudio);
 
+struct coreaudio_stream
+{
+    OSSpinLock lock;
+    AudioComponentInstance unit;
+    AudioConverterRef converter;
+    AudioStreamBasicDescription dev_desc; /* audio unit format, not necessarily the same as fmt */
+    AudioDeviceID dev_id;
+    EDataFlow flow;
+    AUDCLNT_SHAREMODE share;
+
+    BOOL playing;
+    UINT32 period_ms, period_frames;
+    UINT32 bufsize_frames, resamp_bufsize_frames;
+    UINT32 lcl_offs_frames, held_frames, wri_offs_frames, tmp_buffer_frames;
+    UINT32 cap_bufsize_frames, cap_offs_frames, cap_held_frames;
+    UINT32 wrap_bufsize_frames;
+    UINT64 written_frames;
+    INT32 getbuf_last;
+    WAVEFORMATEX *fmt;
+    BYTE *local_buffer, *cap_buffer, *wrap_buffer, *resamp_buffer, *tmp_buffer;
+    SIZE_T local_buffer_size, tmp_buffer_size;
+};
+
 static HRESULT osstatus_to_hresult(OSStatus sc)
 {
     switch(sc){
--- dlls/winecoreaudio.drv/unixlib.h
+++ dlls/winecoreaudio.drv/unixlib.h
@@ -18,28 +18,7 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-struct coreaudio_stream /* To be made private */
-{
-    OSSpinLock lock;
-    AudioComponentInstance unit;
-    AudioConverterRef converter;
-    AudioStreamBasicDescription dev_desc; /* audio unit format, not necessarily the same as fmt */
-    AudioDeviceID dev_id;
-    EDataFlow flow;
-    AUDCLNT_SHAREMODE share;
-
-    BOOL playing;
-    UINT32 period_ms, period_frames;
-    UINT32 bufsize_frames, resamp_bufsize_frames;
-    UINT32 lcl_offs_frames, held_frames, wri_offs_frames, tmp_buffer_frames;
-    UINT32 cap_bufsize_frames, cap_offs_frames, cap_held_frames;
-    UINT32 wrap_bufsize_frames;
-    UINT64 written_frames;
-    INT32 getbuf_last;
-    WAVEFORMATEX *fmt;
-    BYTE *local_buffer, *cap_buffer, *wrap_buffer, *resamp_buffer, *tmp_buffer;
-    SIZE_T local_buffer_size, tmp_buffer_size;
-};
+struct coreaudio_stream;
 
 struct endpoint
 {
