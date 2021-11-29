4180aa9f9bbdcfccb9dc9bb7172042ceee4d8905
Huw Davies <huw@codeweavers.com>
2021-11-29 11:26:41 +0000

    winecoreaudio: Move midi_in_prepare and midi_in_unprepare to the unixlib.
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Andrew Eikum <aeikum@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/winecoreaudio.drv/midi.c
+++ dlls/winecoreaudio.drv/midi.c
@@ -194,36 +194,6 @@ static DWORD MIDIIn_AddBuffer(WORD wDevID, LPMIDIHDR lpMidiHdr, DWORD dwSize)
     return MMSYSERR_NOERROR;
 }
 
-static DWORD MIDIIn_Prepare(WORD wDevID, LPMIDIHDR lpMidiHdr, DWORD dwSize)
-{
-    TRACE("wDevID=%d lpMidiHdr=%p dwSize=%d\n", wDevID, lpMidiHdr, dwSize);
-
-    if (dwSize < offsetof(MIDIHDR,dwOffset) || lpMidiHdr == 0 || lpMidiHdr->lpData == 0)
-	return MMSYSERR_INVALPARAM;
-    if (lpMidiHdr->dwFlags & MHDR_PREPARED)
-	return MMSYSERR_NOERROR;
-
-    lpMidiHdr->lpNext = 0;
-    lpMidiHdr->dwFlags |= MHDR_PREPARED;
-    lpMidiHdr->dwFlags &= ~(MHDR_DONE|MHDR_INQUEUE); /* flags cleared since w2k */
-    return MMSYSERR_NOERROR;
-}
-
-static DWORD MIDIIn_Unprepare(WORD wDevID, LPMIDIHDR lpMidiHdr, DWORD dwSize)
-{
-    TRACE("wDevID=%d lpMidiHdr=%p dwSize=%d\n", wDevID, lpMidiHdr, dwSize);
-
-    if (dwSize < offsetof(MIDIHDR,dwOffset) || lpMidiHdr == 0 || lpMidiHdr->lpData == 0)
-	return MMSYSERR_INVALPARAM;
-    if (!(lpMidiHdr->dwFlags & MHDR_PREPARED))
-	return MMSYSERR_NOERROR;
-    if (lpMidiHdr->dwFlags & MHDR_INQUEUE)
-	return MIDIERR_STILLPLAYING;
-
-    lpMidiHdr->dwFlags &= ~MHDR_PREPARED;
-    return MMSYSERR_NOERROR;
-}
-
 static DWORD MIDIIn_GetDevCaps(WORD wDevID, LPMIDIINCAPSW lpCaps, DWORD dwSize)
 {
     TRACE("wDevID=%d lpCaps=%p dwSize=%d\n", wDevID, lpCaps, dwSize);
@@ -467,10 +437,6 @@ DWORD WINAPI CoreAudio_midMessage(UINT wDevID, UINT wMsg, DWORD_PTR dwUser, DWOR
     switch (wMsg) {
         case MIDM_ADDBUFFER:
             return MIDIIn_AddBuffer(wDevID, (LPMIDIHDR)dwParam1, dwParam2);
-        case MIDM_PREPARE:
-            return MIDIIn_Prepare(wDevID, (LPMIDIHDR)dwParam1, dwParam2);
-        case MIDM_UNPREPARE:
-            return MIDIIn_Unprepare(wDevID, (LPMIDIHDR)dwParam1, dwParam2);
         case MIDM_GETDEVCAPS:
             return MIDIIn_GetDevCaps(wDevID, (LPMIDIINCAPSW) dwParam1, dwParam2);
         case MIDM_GETNUMDEVS:
