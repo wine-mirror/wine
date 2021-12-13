56a6a039180053dac12305a3aecae3c77b3119ac
Alex Henrie <alexhenrie24@gmail.com>
2021-12-12 19:04:10 -0700

    comctl32/tests: Permit larger propsheet buffers.
    Fixes a testbot failure on Windows 10 Hebrew.
    
    Signed-off-by: Alex Henrie <alexhenrie24@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/comctl32/tests/propsheet.c
+++ dlls/comctl32/tests/propsheet.c
@@ -92,7 +92,8 @@ static int CALLBACK sheet_callback(HWND hwnd, UINT msg, LPARAM lparam)
         size = SizeofResource(module, hrsrc);
         ok(size != 0, "Failed to get size of propsheet dialog resource\n");
         buffer_size = HeapSize(GetProcessHeap(), 0, (void *)lparam);
-        ok(buffer_size == 2 * size, "Unexpected template buffer size %u, resource size %u\n",
+        /* Hebrew Windows 10 allocates 2 * size + 8, all others allocate 2 * size */
+        ok(buffer_size >= 2 * size, "Unexpected template buffer size %u, resource size %u\n",
                 buffer_size, size);
         break;
       }
