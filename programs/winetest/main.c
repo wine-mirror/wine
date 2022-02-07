d34b3021b324cc5f8ffbcee63679bda33f576ab6
Eric Pouech <eric.pouech@gmail.com>
2022-02-07 08:41:13 +0100

    winetest: Enable compilation with long types.
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- programs/winetest/main.c
+++ programs/winetest/main.c
@@ -124,7 +124,7 @@ static char * get_file_version(char * file_name)
                 VS_FIXEDFILEINFO *pFixedVersionInfo;
                 UINT len;
                 if (VerQueryValueA(data, backslash, (LPVOID *)&pFixedVersionInfo, &len)) {
-                    sprintf(version, "%d.%d.%d.%d",
+                    sprintf(version, "%ld.%ld.%ld.%ld",
                             pFixedVersionInfo->dwFileVersionMS >> 16,
                             pFixedVersionInfo->dwFileVersionMS & 0xffff,
                             pFixedVersionInfo->dwFileVersionLS >> 16,
@@ -132,14 +132,14 @@ static char * get_file_version(char * file_name)
                 } else
                     sprintf(version, "version not found");
             } else
-                sprintf(version, "version error %u", GetLastError());
+                sprintf(version, "version error %lu", GetLastError());
             heap_free(data);
         } else
             sprintf(version, "version error %u", ERROR_OUTOFMEMORY);
     } else if (GetLastError() == ERROR_FILE_NOT_FOUND)
         sprintf(version, "dll is missing");
     else
-        sprintf(version, "version not present %u", GetLastError());
+        sprintf(version, "version not present %lu", GetLastError());
 
     return version;
 }
