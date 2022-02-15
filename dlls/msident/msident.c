ef587a763db2f5b3301a6c6b008a12b2c03cad73
Eric Pouech <eric.pouech@gmail.com>
2022-02-15 07:29:48 +0100

    msident: Enable compilation with long types.
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/msident/msident.c
+++ dlls/msident/msident.c
@@ -63,7 +63,7 @@ static ULONG WINAPI EnumUserIdentity_AddRef(IEnumUserIdentity *iface)
     EnumUserIdentity *This = impl_from_IEnumUserIdentity(iface);
     LONG ref = InterlockedIncrement(&This->ref);
 
-    TRACE("(%p) ref=%d\n", This, ref);
+    TRACE("(%p) ref=%ld\n", This, ref);
 
     return ref;
 }
@@ -73,7 +73,7 @@ static ULONG WINAPI EnumUserIdentity_Release(IEnumUserIdentity *iface)
     EnumUserIdentity *This = impl_from_IEnumUserIdentity(iface);
     LONG ref = InterlockedDecrement(&This->ref);
 
-    TRACE("(%p) ref=%d\n", This, ref);
+    TRACE("(%p) ref=%ld\n", This, ref);
 
     if(!ref)
         heap_free(This);
@@ -84,14 +84,14 @@ static ULONG WINAPI EnumUserIdentity_Release(IEnumUserIdentity *iface)
 static HRESULT WINAPI EnumUserIdentity_Next(IEnumUserIdentity *iface, ULONG celt, IUnknown **rgelt, ULONG *pceltFetched)
 {
     EnumUserIdentity *This = impl_from_IEnumUserIdentity(iface);
-    FIXME("(%p)->(%u %p %p)\n", This, celt, rgelt, pceltFetched);
+    FIXME("(%p)->(%lu %p %p)\n", This, celt, rgelt, pceltFetched);
     return E_NOTIMPL;
 }
 
 static HRESULT WINAPI EnumUserIdentity_Skip(IEnumUserIdentity *iface, ULONG celt)
 {
     EnumUserIdentity *This = impl_from_IEnumUserIdentity(iface);
-    FIXME("(%p)->(%u)\n", This, celt);
+    FIXME("(%p)->(%lu)\n", This, celt);
     return E_NOTIMPL;
 }
 
@@ -179,14 +179,14 @@ static HRESULT WINAPI UserIdentityManager_EnumIdentities(IUserIdentityManager *i
 
 static HRESULT WINAPI UserIdentityManager_ManageIdentities(IUserIdentityManager *iface, HWND hwndParent, DWORD dwFlags)
 {
-    FIXME("(%p %x)\n", hwndParent, dwFlags);
+    FIXME("(%p %lx)\n", hwndParent, dwFlags);
     return E_NOTIMPL;
 }
 
 static HRESULT WINAPI UserIdentityManager_Logon(IUserIdentityManager *iface, HWND hwndParent,
         DWORD dwFlags, IUserIdentity **ppIdentity)
 {
-    FIXME("(%p %x %p)\n", hwndParent, dwFlags, ppIdentity);
+    FIXME("(%p %lx %p)\n", hwndParent, dwFlags, ppIdentity);
     return E_USER_CANCELLED;
 }
 
