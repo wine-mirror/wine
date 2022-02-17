67d35e0342e4b5e93292f23f8b80c958de1c8b3c
Eric Pouech <eric.pouech@gmail.com>
2022-02-17 07:11:31 +0100

    qwave: Enable compilation with long types.
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/qwave/main.c
+++ dlls/qwave/main.c
@@ -39,7 +39,7 @@ BOOL WINAPI QOSCreateHandle(PQOS_VERSION version, PHANDLE handle)
 BOOL WINAPI QOSAddSocketToFlow(HANDLE handle, SOCKET socket, PSOCKADDR addr,
                                QOS_TRAFFIC_TYPE traffictype, DWORD flags, PQOS_FLOWID flowid)
 {
-    FIXME("%p, %lx, %p, %d, 0x%08x, %p stub!\n", handle, socket, addr, traffictype, flags, flowid);
+    FIXME("%p, %Ix, %p, %d, 0x%08lx, %p stub!\n", handle, socket, addr, traffictype, flags, flowid);
     SetLastError(ERROR_NOT_SUPPORTED);
     return FALSE;
 }
