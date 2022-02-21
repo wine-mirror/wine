5c22bc551c8f0956abc5a1d824e0925f5044e70b
Eric Pouech <eric.pouech@gmail.com>
2022-02-21 07:59:37 +0100

    wevtapi: Enable compilation with long types.
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/wevtapi/main.c
+++ dlls/wevtapi/main.c
@@ -32,13 +32,13 @@ static const WCHAR log_pathW[] = L"C:\\windows\\temp\\evt.log";
 
 EVT_HANDLE WINAPI EvtOpenSession(EVT_LOGIN_CLASS login_class, void *login, DWORD timeout, DWORD flags)
 {
-    FIXME("(%u %p %u %u) stub\n", login_class, login, timeout, flags);
+    FIXME("(%u %p %lu %lu) stub\n", login_class, login, timeout, flags);
     return NULL;
 }
 
 EVT_HANDLE WINAPI EvtOpenLog(EVT_HANDLE session, const WCHAR *path, DWORD flags)
 {
-    FIXME("(%p %s %u) stub\n", session, debugstr_w(path), flags);
+    FIXME("(%p %s %lu) stub\n", session, debugstr_w(path), flags);
     return NULL;
 }
 
@@ -49,7 +49,7 @@ BOOL WINAPI EvtGetChannelConfigProperty(EVT_HANDLE ChannelConfig,
                                         PEVT_VARIANT PropertyValueBuffer,
                                         DWORD *PropertyValueBufferUsed)
 {
-    FIXME("(%p %i %u %u %p %p) stub\n", ChannelConfig, PropertyId, Flags, PropertyValueBufferSize,
+    FIXME("(%p %i %lu %lu %p %p) stub\n", ChannelConfig, PropertyId, Flags, PropertyValueBufferSize,
           PropertyValueBuffer, PropertyValueBufferUsed);
 
     switch (PropertyId)
@@ -81,7 +81,7 @@ BOOL WINAPI EvtSetChannelConfigProperty(EVT_HANDLE ChannelConfig,
                                         DWORD Flags,
                                         PEVT_VARIANT PropertyValue)
 {
-    FIXME("(%p %i %u %p) stub\n", ChannelConfig, PropertyId, Flags, PropertyValue);
+    FIXME("(%p %i %lu %p) stub\n", ChannelConfig, PropertyId, Flags, PropertyValue);
     return TRUE;
 }
 
@@ -89,38 +89,38 @@ EVT_HANDLE WINAPI EvtSubscribe(EVT_HANDLE Session, HANDLE SignalEvent, LPCWSTR C
                                LPCWSTR Query, EVT_HANDLE Bookmark, PVOID context,
                                EVT_SUBSCRIBE_CALLBACK Callback, DWORD Flags)
 {
-    FIXME("(%p %p %s %s %p %p %p %u) stub\n", Session, SignalEvent, debugstr_w(ChannelPath),
+    FIXME("(%p %p %s %s %p %p %p %lu) stub\n", Session, SignalEvent, debugstr_w(ChannelPath),
           debugstr_w(Query), Bookmark, context, Callback, Flags);
     return NULL;
 }
 
 EVT_HANDLE WINAPI EvtOpenChannelEnum(EVT_HANDLE session, DWORD flags)
 {
-    FIXME("(%p %u) stub\n", session, flags);
+    FIXME("(%p %lu) stub\n", session, flags);
     return NULL;
 }
 
 BOOL WINAPI EvtNextChannelPath(EVT_HANDLE channel_enum, DWORD buffer_len, WCHAR *buffer, DWORD *used)
 {
-    FIXME("(%p %u %p %p) stub\n", channel_enum, buffer_len, buffer, used);
+    FIXME("(%p %lu %p %p) stub\n", channel_enum, buffer_len, buffer, used);
     return FALSE;
 }
 
 EVT_HANDLE WINAPI EvtOpenChannelConfig(EVT_HANDLE Session, LPCWSTR ChannelPath, DWORD Flags)
 {
-    FIXME("(%p %s %u) stub\n", Session, debugstr_w(ChannelPath), Flags);
+    FIXME("(%p %s %lu) stub\n", Session, debugstr_w(ChannelPath), Flags);
     return (EVT_HANDLE)0xdeadbeef;
 }
 
 EVT_HANDLE WINAPI EvtQuery(EVT_HANDLE session, const WCHAR *path, const WCHAR *query, DWORD flags)
 {
-    FIXME("(%p %s %s %u) stub\n", session, debugstr_w(path), debugstr_w(query), flags);
+    FIXME("(%p %s %s %lu) stub\n", session, debugstr_w(path), debugstr_w(query), flags);
     return NULL;
 }
 
 BOOL WINAPI EvtSaveChannelConfig(EVT_HANDLE channel, DWORD flags)
 {
-    FIXME("(%p,%08x) stub\n", channel, flags);
+    FIXME("(%p,%08lx) stub\n", channel, flags);
     return TRUE;
 }
 
@@ -132,14 +132,14 @@ BOOL WINAPI EvtClose(EVT_HANDLE handle)
 
 BOOL WINAPI EvtNext(EVT_HANDLE result_set, DWORD size, EVT_HANDLE *array, DWORD timeout, DWORD flags, DWORD *ret_count)
 {
-    FIXME("(%p %u %p %u %#x %p) stub!\n", result_set, size, array, timeout, flags, ret_count);
+    FIXME("(%p %lu %p %lu %#lx %p) stub!\n", result_set, size, array, timeout, flags, ret_count);
     SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
     return FALSE;
 }
 
 BOOL WINAPI EvtExportLog(EVT_HANDLE session, const WCHAR *path, const WCHAR *query, const WCHAR *file, DWORD flags)
 {
-    FIXME("(%p %s %s %s %#x) stub!\n", session, debugstr_w(path), debugstr_w(query), debugstr_w(file), flags);
+    FIXME("(%p %s %s %s %#lx) stub!\n", session, debugstr_w(path), debugstr_w(query), debugstr_w(file), flags);
     SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
     return FALSE;
 }
