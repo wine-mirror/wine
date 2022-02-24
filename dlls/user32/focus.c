fa2f52578bf1d9e386fa47c89e90671f9f9a8e6c
Jacek Caban <jacek@codeweavers.com>
2022-02-24 01:28:38 +0100

    user32: Use NtUserGetGUIThreadInfo in GetActiveWindow.
    Signed-off-by: Jacek Caban <jacek@codeweavers.com>
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/user32/focus.c
+++ dlls/user32/focus.c
@@ -342,15 +342,9 @@ BOOL WINAPI SetForegroundWindow( HWND hwnd )
  */
 HWND WINAPI GetActiveWindow(void)
 {
-    HWND ret = 0;
-
-    SERVER_START_REQ( get_thread_input )
-    {
-        req->tid = GetCurrentThreadId();
-        if (!wine_server_call_err( req )) ret = wine_server_ptr_handle( reply->active );
-    }
-    SERVER_END_REQ;
-    return ret;
+    GUITHREADINFO info;
+    info.cbSize = sizeof(info);
+    return NtUserGetGUIThreadInfo( GetCurrentThreadId(), &info ) ? info.hwndActive : 0;
 }
 
 
