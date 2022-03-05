754c5eb4cf22f16b1f8678db6a2d94f6a775dd01
Eric Pouech <eric.pouech@gmail.com>
2022-03-05 12:08:14 +0100

    slc/tests: Enable compilation with long types.
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/slc/tests/slc.c
+++ dlls/slc/tests/slc.c
@@ -33,30 +33,30 @@ static void test_SLGetWindowsInformationDWORD(void)
     HRESULT res;
 
     res = SLGetWindowsInformationDWORD(L"Nonexistent-License-Value", NULL);
-    ok(res == E_INVALIDARG, "expected E_INVALIDARG, got %08x\n", res);
+    ok(res == E_INVALIDARG, "expected E_INVALIDARG, got %08lx\n", res);
 
     res = SLGetWindowsInformationDWORD(NULL, &value);
-    ok(res == E_INVALIDARG, "expected E_INVALIDARG, got %08x\n", res);
+    ok(res == E_INVALIDARG, "expected E_INVALIDARG, got %08lx\n", res);
 
     value = 0xdeadbeef;
     res = SLGetWindowsInformationDWORD(L"Nonexistent-License-Value", &value);
-    ok(res == SL_E_VALUE_NOT_FOUND, "expected SL_E_VALUE_NOT_FOUND, got %08x\n", res);
-    ok(value == 0xdeadbeef, "expected value = 0xdeadbeef, got %u\n", value);
+    ok(res == SL_E_VALUE_NOT_FOUND, "expected SL_E_VALUE_NOT_FOUND, got %08lx\n", res);
+    ok(value == 0xdeadbeef, "expected value = 0xdeadbeef, got %lu\n", value);
 
     value = 0xdeadbeef;
     res = SLGetWindowsInformationDWORD(L"", &value);
     ok(res == SL_E_RIGHT_NOT_GRANTED || broken(res == 0xd000000d) /* Win 8 */,
-       "expected SL_E_RIGHT_NOT_GRANTED, got %08x\n", res);
-    ok(value == 0xdeadbeef, "expected value = 0xdeadbeef, got %u\n", value);
+       "expected SL_E_RIGHT_NOT_GRANTED, got %08lx\n", res);
+    ok(value == 0xdeadbeef, "expected value = 0xdeadbeef, got %lu\n", value);
 
     value = 0xdeadbeef;
     res = SLGetWindowsInformationDWORD(L"Kernel-MUI-Language-Allowed", &value);
-    ok(res == SL_E_DATATYPE_MISMATCHED, "expected SL_E_DATATYPE_MISMATCHED, got %08x\n", res);
-    ok(value == 0xdeadbeef, "expected value = 0xdeadbeef, got %u\n", value);
+    ok(res == SL_E_DATATYPE_MISMATCHED, "expected SL_E_DATATYPE_MISMATCHED, got %08lx\n", res);
+    ok(value == 0xdeadbeef, "expected value = 0xdeadbeef, got %lu\n", value);
 
     value = 0xdeadbeef;
     res = SLGetWindowsInformationDWORD(L"Kernel-MUI-Number-Allowed", &value);
-    ok(res == S_OK, "expected S_OK, got %u\n", res);
+    ok(res == S_OK, "expected S_OK, got %lu\n", res);
     ok(value != 0xdeadbeef, "expected value != 0xdeadbeef\n");
 }
 
