3720c45a46c5710a2ef27c463f1d9ec87a5a6d2e
Eric Pouech <eric.pouech@gmail.com>
2022-04-25 09:35:22 +0100

    include: Don't use long types in nsi.h.
    It'll make things simpler from unixlib when long types are enabled.
    
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>
    Signed-off-by: Huw Davies <huw@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/wine/nsi.h
+++ include/wine/nsi.h
@@ -32,26 +32,26 @@
 struct nsi_ndis_ifinfo_rw
 {
     GUID network_guid;
-    DWORD admin_status;
+    UINT admin_status;
     IF_COUNTED_STRING alias; /* .Length in bytes not including '\0' */
     IF_PHYSICAL_ADDRESS phys_addr;
     USHORT pad;
     IF_COUNTED_STRING name2;
-    DWORD unk;
+    UINT unk;
 };
 
 struct nsi_ndis_ifinfo_dynamic
 {
-    DWORD oper_status;
+    UINT oper_status;
     struct
     {
-        DWORD unk : 1;
-        DWORD not_media_conn : 1;
-        DWORD unk2 : 30;
+        UINT unk : 1;
+        UINT not_media_conn : 1;
+        UINT unk2 : 30;
     } flags;
-    DWORD media_conn_state;
-    DWORD unk;
-    DWORD mtu;
+    UINT media_conn_state;
+    UINT unk;
+    UINT mtu;
     ULONG64 xmit_speed;
     ULONG64 rcv_speed;
     ULONG64 in_errors;
@@ -79,23 +79,23 @@ struct nsi_ndis_ifinfo_dynamic
 
 struct nsi_ndis_ifinfo_static
 {
-    DWORD if_index;
+    UINT if_index;
     IF_COUNTED_STRING descr;
-    DWORD type;
-    DWORD access_type;
-    DWORD unk;
-    DWORD conn_type;
+    UINT type;
+    UINT access_type;
+    UINT unk;
+    UINT conn_type;
     GUID if_guid;
     USHORT conn_present;
     IF_PHYSICAL_ADDRESS perm_phys_addr;
     struct
     {
-        DWORD hw : 1;
-        DWORD filter : 1;
-        DWORD unk : 30;
+        UINT hw : 1;
+        UINT filter : 1;
+        UINT unk : 30;
     } flags;
-    DWORD media_type;
-    DWORD phys_medium_type;
+    UINT media_type;
+    UINT phys_medium_type;
 };
 
 /* Undocumented NSI IP tables */
@@ -108,33 +108,33 @@ struct nsi_ndis_ifinfo_static
 
 struct nsi_ip_cmpt_rw
 {
-    DWORD not_forwarding;
-    DWORD unk;
-    DWORD default_ttl;
-    DWORD unk2;
+    UINT not_forwarding;
+    UINT unk;
+    UINT default_ttl;
+    UINT unk2;
 };
 
 struct nsi_ip_cmpt_dynamic
 {
-    DWORD num_ifs;
-    DWORD num_routes;
-    DWORD unk;
-    DWORD num_addrs;
+    UINT num_ifs;
+    UINT num_routes;
+    UINT unk;
+    UINT num_addrs;
 };
 
 struct nsi_ip_icmpstats_dynamic
 {
-    DWORD in_msgs;
-    DWORD in_errors;
-    DWORD in_type_counts[256];
-    DWORD out_msgs;
-    DWORD out_errors;
-    DWORD out_type_counts[256];
+    UINT in_msgs;
+    UINT in_errors;
+    UINT in_type_counts[256];
+    UINT out_msgs;
+    UINT out_errors;
+    UINT out_type_counts[256];
 };
 
 struct nsi_ip_ipstats_dynamic
 {
-    DWORD unk[4];
+    UINT unk[4];
     ULONGLONG in_recv;
     ULONGLONG in_octets;
     ULONGLONG fwd_dgrams;
@@ -145,32 +145,32 @@ struct nsi_ip_ipstats_dynamic
     ULONGLONG out_octets;
     ULONGLONG unk4[6];
     ULONGLONG in_hdr_errs;
-    DWORD in_addr_errs;
-    DWORD in_unk_protos;
-    DWORD unk5;
-    DWORD reasm_reqds;
-    DWORD reasm_oks;
-    DWORD reasm_fails;
-    DWORD in_discards;
-    DWORD out_no_routes;
-    DWORD out_discards;
-    DWORD routing_discards;
-    DWORD frag_oks;
-    DWORD frag_fails;
-    DWORD frag_creates;
-    DWORD unk6[7];
+    UINT in_addr_errs;
+    UINT in_unk_protos;
+    UINT unk5;
+    UINT reasm_reqds;
+    UINT reasm_oks;
+    UINT reasm_fails;
+    UINT in_discards;
+    UINT out_no_routes;
+    UINT out_discards;
+    UINT routing_discards;
+    UINT frag_oks;
+    UINT frag_fails;
+    UINT frag_creates;
+    UINT unk6[7];
 };
 
 struct nsi_ip_ipstats_static
 {
-    DWORD reasm_timeout;
+    UINT reasm_timeout;
 };
 
 struct nsi_ipv4_unicast_key
 {
     NET_LUID luid;
     IN_ADDR addr;
-    DWORD pad;
+    UINT pad;
 };
 
 struct nsi_ipv6_unicast_key
@@ -181,18 +181,18 @@ struct nsi_ipv6_unicast_key
 
 struct nsi_ip_unicast_rw
 {
-    DWORD preferred_lifetime;
-    DWORD valid_lifetime;
-    DWORD prefix_origin;
-    DWORD suffix_origin;
-    DWORD on_link_prefix;
-    DWORD unk[2];
+    UINT preferred_lifetime;
+    UINT valid_lifetime;
+    UINT prefix_origin;
+    UINT suffix_origin;
+    UINT on_link_prefix;
+    UINT unk[2];
 };
 
 struct nsi_ip_unicast_dynamic
 {
-    DWORD scope_id;
-    DWORD dad_state;
+    UINT scope_id;
+    UINT dad_state;
 };
 
 struct nsi_ip_unicast_static
@@ -205,7 +205,7 @@ struct nsi_ipv4_neighbour_key
     NET_LUID luid;
     NET_LUID luid2;
     IN_ADDR addr;
-    DWORD pad;
+    UINT pad;
 };
 
 struct nsi_ipv6_neighbour_key
@@ -222,38 +222,38 @@ struct nsi_ip_neighbour_rw
 
 struct nsi_ip_neighbour_dynamic
 {
-    DWORD state;
-    DWORD time;
+    UINT state;
+    UINT time;
     struct
     {
         BOOLEAN is_router;
         BOOLEAN is_unreachable;
     } flags;
     USHORT phys_addr_len;
-    DWORD unk;
+    UINT unk;
 };
 
 struct nsi_ipv4_forward_key
 {
-    DWORD unk;
+    UINT unk;
     IN_ADDR prefix;
     BYTE prefix_len;
     BYTE unk2[3];
-    DWORD unk3[3];
+    UINT unk3[3];
     NET_LUID luid;
     NET_LUID luid2;
     IN_ADDR next_hop;
-    DWORD pad;
+    UINT pad;
 };
 
 struct nsi_ipv6_forward_key
 {
-    DWORD unk;
+    UINT unk;
     IN6_ADDR prefix;
     BYTE prefix_len;
     BYTE unk2[3];
-    DWORD unk3[3];
-    DWORD pad;
+    UINT unk3[3];
+    UINT pad;
     NET_LUID luid;
     NET_LUID luid2;
     IN6_ADDR next_hop;
@@ -261,37 +261,37 @@ struct nsi_ipv6_forward_key
 
 struct nsi_ip_forward_rw
 {
-    DWORD site_prefix_len;
-    DWORD valid_lifetime;
-    DWORD preferred_lifetime;
-    DWORD metric;
-    DWORD protocol;
+    UINT site_prefix_len;
+    UINT valid_lifetime;
+    UINT preferred_lifetime;
+    UINT metric;
+    UINT protocol;
     BYTE loopback;
     BYTE autoconf;
     BYTE publish;
     BYTE immortal;
     BYTE unk[4];
-    DWORD unk2;
+    UINT unk2;
 };
 
 struct nsi_ipv4_forward_dynamic
 {
-    DWORD age;
-    DWORD unk[3];
+    UINT age;
+    UINT unk[3];
     IN_ADDR addr2; /* often a repeat of prefix */
 };
 
 struct nsi_ipv6_forward_dynamic
 {
-    DWORD age;
-    DWORD unk[3];
+    UINT age;
+    UINT unk[3];
     IN6_ADDR addr2; /* often a repeat of prefix */
 };
 
 struct nsi_ip_forward_static
 {
-    DWORD origin;
-    DWORD if_index;
+    UINT origin;
+    UINT if_index;
 };
 
 /* Undocumented NSI TCP tables */
@@ -302,28 +302,28 @@ struct nsi_ip_forward_static
 
 struct nsi_tcp_stats_dynamic
 {
-    DWORD active_opens;
-    DWORD passive_opens;
-    DWORD attempt_fails;
-    DWORD est_rsts;
-    DWORD cur_est;
-    DWORD pad; /* ? */
+    UINT active_opens;
+    UINT passive_opens;
+    UINT attempt_fails;
+    UINT est_rsts;
+    UINT cur_est;
+    UINT pad; /* ? */
     ULONGLONG in_segs;
     ULONGLONG out_segs;
-    DWORD retrans_segs;
-    DWORD out_rsts;
-    DWORD in_errs;
-    DWORD num_conns;
-    DWORD unk[12];
+    UINT retrans_segs;
+    UINT out_rsts;
+    UINT in_errs;
+    UINT num_conns;
+    UINT unk[12];
 };
 
 struct nsi_tcp_stats_static
 {
-    DWORD rto_algo;
-    DWORD rto_min;
-    DWORD rto_max;
-    DWORD max_conns;
-    DWORD unk;
+    UINT rto_algo;
+    UINT rto_min;
+    UINT rto_max;
+    UINT max_conns;
+    UINT unk;
 };
 
 struct nsi_tcp_conn_key
@@ -334,14 +334,14 @@ struct nsi_tcp_conn_key
 
 struct nsi_tcp_conn_dynamic
 {
-    DWORD state;
-    DWORD unk[3];
+    UINT state;
+    UINT unk[3];
 };
 
 struct nsi_tcp_conn_static
 {
-    DWORD unk[3];
-    DWORD pid;
+    UINT unk[3];
+    UINT pid;
     ULONGLONG create_time;
     ULONGLONG mod_info;
 };
@@ -353,11 +353,11 @@ struct nsi_tcp_conn_static
 struct nsi_udp_stats_dynamic
 {
     ULONGLONG in_dgrams;
-    DWORD no_ports;
-    DWORD in_errs;
+    UINT no_ports;
+    UINT in_errs;
     ULONGLONG out_dgrams;
-    DWORD num_addrs;
-    DWORD unk[5];
+    UINT num_addrs;
+    UINT unk[5];
 };
 
 struct nsi_udp_endpoint_key
@@ -367,11 +367,11 @@ struct nsi_udp_endpoint_key
 
 struct nsi_udp_endpoint_static
 {
-    DWORD pid;
-    DWORD unk;
+    UINT pid;
+    UINT unk;
     ULONGLONG create_time;
-    DWORD flags;
-    DWORD unk2;
+    UINT flags;
+    UINT unk2;
     ULONGLONG mod_info;
 };
 
@@ -386,26 +386,26 @@ struct nsi_udp_endpoint_static
 struct nsiproxy_enumerate_all
 {
     NPI_MODULEID module;
-    DWORD first_arg;
-    DWORD second_arg;
-    DWORD table;
-    DWORD key_size;
-    DWORD rw_size;
-    DWORD dynamic_size;
-    DWORD static_size;
-    DWORD count;
+    UINT first_arg;
+    UINT second_arg;
+    UINT table;
+    UINT key_size;
+    UINT rw_size;
+    UINT dynamic_size;
+    UINT static_size;
+    UINT count;
 };
 
 /* input for IOCTL_NSIPROXY_WINE_GET_ALL_PARAMETERS */
 struct nsiproxy_get_all_parameters
 {
     NPI_MODULEID module;
-    DWORD first_arg;
-    DWORD table;
-    DWORD key_size;
-    DWORD rw_size;
-    DWORD dynamic_size;
-    DWORD static_size;
+    UINT first_arg;
+    UINT table;
+    UINT key_size;
+    UINT rw_size;
+    UINT dynamic_size;
+    UINT static_size;
     BYTE key[1]; /* key_size */
 };
 
@@ -413,11 +413,11 @@ struct nsiproxy_get_all_parameters
 struct nsiproxy_get_parameter
 {
     NPI_MODULEID module;
-    DWORD first_arg;
-    DWORD table;
-    DWORD key_size;
-    DWORD param_type;
-    DWORD data_offset;
+    UINT first_arg;
+    UINT table;
+    UINT key_size;
+    UINT param_type;
+    UINT data_offset;
     BYTE key[1]; /* key_size */
 };
 
@@ -431,9 +431,9 @@ struct nsiproxy_icmp_echo
     BYTE ttl;
     BYTE tos;
     BYTE flags;
-    DWORD opt_size;
-    DWORD req_size;
-    DWORD timeout;
+    UINT opt_size;
+    UINT req_size;
+    UINT timeout;
     BYTE data[1]; /* ((opt_size + 3) & ~3) + req_size */
 };
 
@@ -447,50 +447,50 @@ struct nsi_enumerate_all_ex
 {
     void *unknown[2];
     const NPI_MODULEID *module;
-    DWORD_PTR table;
-    DWORD first_arg;
-    DWORD second_arg;
+    UINT_PTR table;
+    UINT first_arg;
+    UINT second_arg;
     void *key_data;
-    DWORD key_size;
+    UINT key_size;
     void *rw_data;
-    DWORD rw_size;
+    UINT rw_size;
     void *dynamic_data;
-    DWORD dynamic_size;
+    UINT dynamic_size;
     void *static_data;
-    DWORD static_size;
-    DWORD_PTR count;
+    UINT static_size;
+    UINT_PTR count;
 };
 
 struct nsi_get_all_parameters_ex
 {
     void *unknown[2];
     const NPI_MODULEID *module;
-    DWORD_PTR table;
-    DWORD first_arg;
-    DWORD unknown2;
+    UINT_PTR table;
+    UINT first_arg;
+    UINT unknown2;
     const void *key;
-    DWORD key_size;
+    UINT key_size;
     void *rw_data;
-    DWORD rw_size;
+    UINT rw_size;
     void *dynamic_data;
-    DWORD dynamic_size;
+    UINT dynamic_size;
     void *static_data;
-    DWORD static_size;
+    UINT static_size;
 };
 
 struct nsi_get_parameter_ex
 {
     void *unknown[2];
     const NPI_MODULEID *module;
-    DWORD_PTR table;
-    DWORD first_arg;
-    DWORD unknown2;
+    UINT_PTR table;
+    UINT first_arg;
+    UINT unknown2;
     const void *key;
-    DWORD key_size;
-    DWORD_PTR param_type;
+    UINT key_size;
+    UINT_PTR param_type;
     void *data;
-    DWORD data_size;
-    DWORD data_offset;
+    UINT data_size;
+    UINT data_offset;
 };
 
 DWORD WINAPI NsiAllocateAndGetTable( DWORD unk, const NPI_MODULEID *module, DWORD table, void **key_data, DWORD key_size,
