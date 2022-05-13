26637de7cbc770cab20c6e35a828cc9e93fcfc9d
Philip Allison <mangobrain@gmail.com>
2022-05-13 21:54:23 +0100

    uiautomationcore: Fix header compatibility with C++.
    Rename arguments to UiaRaiseAutomationPropertyChangedEvent to avoid
    conflicts with reserved words when included from C++ applications.
    
    Signed-off-by: Philip Allison <mangobrain@googlemail.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- include/uiautomationcoreapi.h
+++ include/uiautomationcoreapi.h
@@ -69,7 +69,7 @@ HRESULT WINAPI UiaGetReservedNotSupportedValue(IUnknown **value);
 int WINAPI UiaLookupId(enum AutomationIdentifierType type, const GUID *guid);
 BOOL WINAPI UiaPatternRelease(HUIAPATTERNOBJECT hobj);
 HRESULT WINAPI UiaRaiseAutomationEvent(IRawElementProviderSimple *provider, EVENTID id);
-HRESULT WINAPI UiaRaiseAutomationPropertyChangedEvent(IRawElementProviderSimple *provider, PROPERTYID id, VARIANT old, VARIANT new);
+HRESULT WINAPI UiaRaiseAutomationPropertyChangedEvent(IRawElementProviderSimple *, PROPERTYID, VARIANT, VARIANT);
 void WINAPI UiaRegisterProviderCallback(UiaProviderCallback *pCallback);
 LRESULT WINAPI UiaReturnRawElementProvider(HWND hwnd, WPARAM wParam, LPARAM lParam, IRawElementProviderSimple *elprov);
 BOOL WINAPI UiaTextRangeRelease(HUIATEXTRANGE hobj);
