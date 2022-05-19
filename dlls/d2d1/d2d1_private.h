0f883c0925478a487467ace83a3dea8f6a435f3d
Ziqing Hui <zhui@codeweavers.com>
2022-05-19 11:15:05 +0800

    uuid: Add d2d1effectauthor.h.
    Signed-off-by: Ziqing Hui <zhui@codeweavers.com>
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>
    Signed-off-by: Alexandre Julliard <julliard@winehq.org>

--- dlls/d2d1/d2d1_private.h
+++ dlls/d2d1/d2d1_private.h
@@ -29,12 +29,12 @@
 #include <math.h>
 #define COBJMACROS
 #include "d2d1_3.h"
+#include "d2d1effectauthor.h"
 #include "d3d11_1.h"
 #ifdef D2D1_INIT_GUID
 #include "initguid.h"
 #endif
 #include "dwrite_2.h"
-#include "d2d1effectauthor.h"
 
 enum d2d_brush_type
 {
