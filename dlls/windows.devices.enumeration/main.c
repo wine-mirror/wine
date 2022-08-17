310225a2455cf94046abdc93f8d92e2f9a037f3d
Julian Klemann <jklemann@codeweavers.com>
2022-08-17 14:23:05 -0500

    windows.devices.enumeration: Create private header file.

--- dlls/windows.devices.enumeration/main.c
+++ dlls/windows.devices.enumeration/main.c
@@ -17,23 +17,10 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include <stdarg.h>
-
-#define COBJMACROS
-#include "windef.h"
-#include "winbase.h"
-#include "winstring.h"
-#include "wine/debug.h"
-#include "objbase.h"
-
 #include "initguid.h"
-#include "activation.h"
+#include "private.h"
 
-#define WIDL_using_Windows_Foundation
-#define WIDL_using_Windows_Foundation_Collections
-#include "windows.foundation.h"
-#define WIDL_using_Windows_Devices_Enumeration
-#include "windows.devices.enumeration.h"
+#include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(enumeration);
 
