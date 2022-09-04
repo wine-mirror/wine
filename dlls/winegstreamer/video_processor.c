5db72186206a5fb6a480a16438a3436566be2981
Rémi Bernon <rbernon@codeweavers.com>
2022-09-04 23:43:36 +0200

    winegstreamer: Avoid leaking attributes on video processor creation failure.

--- dlls/winegstreamer/video_processor.c
+++ dlls/winegstreamer/video_processor.c
@@ -667,6 +667,10 @@ HRESULT video_processor_create(REFIID riid, void **ret)
     return S_OK;
 
 failed:
+    if (impl->output_attributes)
+        IMFAttributes_Release(impl->output_attributes);
+    if (impl->attributes)
+        IMFAttributes_Release(impl->attributes);
     free(impl);
     return hr;
 }
