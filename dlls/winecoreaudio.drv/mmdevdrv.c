af1257f741cb2f18423e869f46677983842c1dd9
Davide Beatrici <git@davidebeatrici.dev>
2022-09-09 09:21:17 +0200

    winecoreaudio: Adapt "get_capture_buffer_params" struct to mmdevapi's.

--- dlls/winecoreaudio.drv/mmdevdrv.c
+++ dlls/winecoreaudio.drv/mmdevdrv.c
@@ -1357,7 +1357,7 @@ static HRESULT WINAPI AudioCaptureClient_GetBuffer(IAudioCaptureClient *iface,
     params.stream = This->stream;
     params.data = data;
     params.frames = frames;
-    params.flags = flags;
+    params.flags = (UINT *)flags;
     params.devpos = devpos;
     params.qpcpos = qpcpos;
     UNIX_CALL(get_capture_buffer, &params);
