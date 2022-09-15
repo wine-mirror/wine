wine-mirror/wine

commit b951c37b8791ceb052c9e159ad7927f75a72d667

d3d11: Use CRT allocation functions.
Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

dlls/d3d11/inputlayout.c
@@ -57,10 +57,10 @@ static HRESULT d3d11_input_layout_to_wined3d_declaration(const D3D11_INPUT_ELEME
         return E_FAIL;
     }
 
-    if (!(*wined3d_elements = heap_calloc(element_count, sizeof(**wined3d_elements))))
+    if (!(*wined3d_elements = calloc(element_count, sizeof(**wined3d_elements))))
     {
         ERR("Failed to allocate wined3d vertex element array memory.\n");
-        heap_free(is.elements);
+        free(is.elements);
         return E_OUTOFMEMORY;
     }
 
@@ -86,7 +86,7 @@ static HRESULT d3d11_input_layout_to_wined3d_declaration(const D3D11_INPUT_ELEME
             WARN("Unused input element %u.\n", i);
     }
 
-    heap_free(is.elements);
+    free(is.elements);
 
     return S_OK;
 }
@@ -312,7 +312,7 @@ static void STDMETHODCALLTYPE d3d_input_layout_wined3d_object_destroyed(void *pa
     struct d3d_input_layout *layout = parent;
 
     wined3d_private_store_cleanup(&layout->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_input_layout_wined3d_parent_ops =
@@ -344,7 +344,7 @@ static HRESULT d3d_input_layout_init(struct d3d_input_layout *layout, struct d3d
 
     hr = wined3d_vertex_declaration_create(device->wined3d_device, wined3d_elements, element_count,
             layout, &d3d_input_layout_wined3d_parent_ops, &layout->wined3d_decl);
-    heap_free(wined3d_elements);
+    free(wined3d_elements);
     if (FAILED(hr))
     {
         WARN("Failed to create wined3d vertex declaration, hr %#lx.\n", hr);
@@ -367,14 +367,14 @@ HRESULT d3d_input_layout_create(struct d3d_device *device,
     struct d3d_input_layout *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d_input_layout_init(object, device, element_descs, element_count,
             shader_byte_code, shader_byte_code_length)))
     {
         WARN("Failed to initialise input layout, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 

dlls/d3d11/async.c
@@ -180,7 +180,7 @@ static void STDMETHODCALLTYPE d3d_query_wined3d_object_destroyed(void *parent)
     struct d3d_query *query = parent;
 
     wined3d_private_store_cleanup(&query->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_query_wined3d_parent_ops =
@@ -488,13 +488,13 @@ HRESULT d3d_query_create(struct d3d_device *device, const D3D11_QUERY_DESC *desc
     if (is_predicate_type)
         predicate = TRUE;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d_query_init(object, device, desc, predicate)))
     {
         WARN("Failed to initialise predicate, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 

dlls/d3d11/buffer.c
@@ -396,7 +396,7 @@ static void STDMETHODCALLTYPE d3d_buffer_wined3d_object_released(void *parent)
     struct d3d_buffer *buffer = parent;
 
     if (buffer->dxgi_resource) IUnknown_Release(buffer->dxgi_resource);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_buffer_wined3d_parent_ops =
@@ -507,13 +507,13 @@ HRESULT d3d_buffer_create(struct d3d_device *device, const D3D11_BUFFER_DESC *de
     struct d3d_buffer *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d_buffer_init(object, device, desc, data)))
     {
         WARN("Failed to initialise buffer, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 

dlls/d3d11/d3d11_private.h
@@ -20,7 +20,6 @@
 #define __WINE_D3D11_PRIVATE_H
 
 #include "wine/debug.h"
-#include "wine/heap.h"
 
 #include <assert.h>
 

dlls/d3d11/device.c
@@ -41,7 +41,7 @@ static BOOL d3d_array_reserve(void **elements, SIZE_T *capacity, SIZE_T count, S
     if (new_capacity < count)
         new_capacity = count;
 
-    if (!(new_elements = heap_realloc(*elements, new_capacity * size)))
+    if (!(new_elements = realloc(*elements, new_capacity * size)))
         return FALSE;
 
     *elements = new_elements;
@@ -158,9 +158,9 @@ static void d3d_device_context_state_private_release(struct d3d_device_context_s
 
             d3d_device_remove_context_state(device, state);
         }
-        heap_free(state->entries);
+        free(state->entries);
         wined3d_device_decref(state->wined3d_device);
-        heap_free(state);
+        free(state);
     }
 }
 
@@ -378,7 +378,7 @@ static ULONG STDMETHODCALLTYPE d3d11_command_list_Release(ID3D11CommandList *ifa
         wined3d_command_list_decref(list->wined3d_list);
         wined3d_private_store_cleanup(&list->private_store);
         ID3D11Device2_Release(list->device);
-        heap_free(list);
+        free(list);
     }
 
     return refcount;
@@ -528,7 +528,7 @@ static ULONG STDMETHODCALLTYPE d3d11_device_context_Release(ID3D11DeviceContext1
         {
             wined3d_deferred_context_destroy(context->wined3d_context);
             d3d11_device_context_cleanup(context);
-            heap_free(context);
+            free(context);
         }
         ID3D11Device2_Release(device);
     }
@@ -2703,14 +2703,14 @@ static HRESULT STDMETHODCALLTYPE d3d11_device_context_FinishCommandList(ID3D11De
         return DXGI_ERROR_INVALID_CALL;
     }
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = wined3d_deferred_context_record_command_list(context->wined3d_context,
             !!restore, &object->wined3d_list)))
     {
         WARN("Failed to record wined3d command list, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -3814,14 +3814,14 @@ static HRESULT d3d11_deferred_context_create(struct d3d_device *device,
     if (flags)
         FIXME("Ignoring flags %#x.\n", flags);
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
     d3d11_device_context_init(object, device, D3D11_DEVICE_CONTEXT_DEFERRED);
 
     if (FAILED(hr = wined3d_deferred_context_create(device->wined3d_device, &object->wined3d_context)))
     {
         WARN("Failed to create wined3d deferred context, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -4385,7 +4385,7 @@ static HRESULT STDMETHODCALLTYPE d3d11_device_CreateDeviceContextState(ID3D11Dev
         return S_FALSE;
     }
 
-    if (!(state_impl = heap_alloc_zero(sizeof(*state_impl))))
+    if (!(state_impl = calloc(1, sizeof(*state_impl))))
     {
         wined3d_state_destroy(wined3d_state);
         hr = E_OUTOFMEMORY;
@@ -4600,7 +4600,7 @@ static ULONG STDMETHODCALLTYPE d3d_device_inner_Release(IUnknown *iface)
         {
             d3d_device_context_state_remove_entry(device->context_states[i], device);
         }
-        heap_free(device->context_states);
+        free(device->context_states);
         d3d11_device_context_cleanup(&device->immediate_context);
         if (device->wined3d_device)
         {
@@ -6247,7 +6247,7 @@ static HRESULT STDMETHODCALLTYPE d3d10_device_CreateGeometryShaderWithStreamOutp
     }
 
     if (output_stream_decl_count
-            && !(so_entries = heap_calloc(output_stream_decl_count, sizeof(*so_entries))))
+            && !(so_entries = calloc(output_stream_decl_count, sizeof(*so_entries))))
     {
         ERR("Failed to allocate D3D11 SO declaration array memory.\n");
         return E_OUTOFMEMORY;
@@ -6268,7 +6268,7 @@ static HRESULT STDMETHODCALLTYPE d3d10_device_CreateGeometryShaderWithStreamOutp
             if (output_stream_stride)
             {
                 WARN("Stride must be 0 when multiple output slots are used.\n");
-                heap_free(so_entries);
+                free(so_entries);
                 return E_INVALIDARG;
             }
         }
@@ -6276,7 +6276,7 @@ static HRESULT STDMETHODCALLTYPE d3d10_device_CreateGeometryShaderWithStreamOutp
 
     hr = d3d_geometry_shader_create(device, byte_code, byte_code_length,
             so_entries, output_stream_decl_count, &output_stream_stride, stride_count, 0, &object);
-    heap_free(so_entries);
+    free(so_entries);
     if (FAILED(hr))
         return hr;
 
@@ -6799,7 +6799,7 @@ static void CDECL device_parent_wined3d_device_created(struct wined3d_device_par
     wined3d_state = wined3d_device_get_state(device->wined3d_device);
     feature_level = d3d_feature_level_from_wined3d(wined3d_state_get_feature_level(wined3d_state));
 
-    if (!(state = heap_alloc_zero(sizeof(*state))))
+    if (!(state = calloc(1, sizeof(*state))))
     {
         ERR("Failed to create the initial device context state.\n");
         return;

dlls/d3d11/shader.c
@@ -247,7 +247,7 @@ static void STDMETHODCALLTYPE d3d_vertex_shader_wined3d_object_destroyed(void *p
     struct d3d_vertex_shader *shader = parent;
 
     wined3d_private_store_cleanup(&shader->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_vertex_shader_wined3d_parent_ops =
@@ -290,13 +290,13 @@ HRESULT d3d_vertex_shader_create(struct d3d_device *device, const void *byte_cod
     struct d3d_vertex_shader *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d_vertex_shader_init(object, device, byte_code, byte_code_length)))
     {
         WARN("Failed to initialise vertex shader, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -445,7 +445,7 @@ static void STDMETHODCALLTYPE d3d11_hull_shader_wined3d_object_destroyed(void *p
     struct d3d11_hull_shader *shader = parent;
 
     wined3d_private_store_cleanup(&shader->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d11_hull_shader_wined3d_parent_ops =
@@ -487,12 +487,12 @@ HRESULT d3d11_hull_shader_create(struct d3d_device *device, const void *byte_cod
     struct d3d11_hull_shader *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d11_hull_shader_init(object, device, byte_code, byte_code_length)))
     {
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -632,7 +632,7 @@ static void STDMETHODCALLTYPE d3d11_domain_shader_wined3d_object_destroyed(void 
     struct d3d11_domain_shader *shader = parent;
 
     wined3d_private_store_cleanup(&shader->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d11_domain_shader_wined3d_parent_ops =
@@ -674,12 +674,12 @@ HRESULT d3d11_domain_shader_create(struct d3d_device *device, const void *byte_c
     struct d3d11_domain_shader *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d11_domain_shader_init(object, device, byte_code, byte_code_length)))
     {
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -922,7 +922,7 @@ static void STDMETHODCALLTYPE d3d_geometry_shader_wined3d_object_destroyed(void 
     struct d3d_geometry_shader *shader = parent;
 
     wined3d_private_store_cleanup(&shader->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_geometry_shader_wined3d_parent_ops =
@@ -1161,14 +1161,14 @@ HRESULT d3d_geometry_shader_create(struct d3d_device *device, const void *byte_c
     struct d3d_geometry_shader *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d_geometry_shader_init(object, device, byte_code, byte_code_length,
             so_entries, so_entry_count, buffer_strides, buffer_stride_count, rasterizer_stream)))
     {
         WARN("Failed to initialise geometry shader, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -1420,7 +1420,7 @@ static void STDMETHODCALLTYPE d3d_pixel_shader_wined3d_object_destroyed(void *pa
     struct d3d_pixel_shader *shader = parent;
 
     wined3d_private_store_cleanup(&shader->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_pixel_shader_wined3d_parent_ops =
@@ -1463,13 +1463,13 @@ HRESULT d3d_pixel_shader_create(struct d3d_device *device, const void *byte_code
     struct d3d_pixel_shader *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d_pixel_shader_init(object, device, byte_code, byte_code_length)))
     {
         WARN("Failed to initialise pixel shader, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -1616,7 +1616,7 @@ static void STDMETHODCALLTYPE d3d11_compute_shader_wined3d_object_destroyed(void
     struct d3d11_compute_shader *shader = parent;
 
     wined3d_private_store_cleanup(&shader->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d11_compute_shader_wined3d_parent_ops =
@@ -1658,12 +1658,12 @@ HRESULT d3d11_compute_shader_create(struct d3d_device *device, const void *byte_
     struct d3d11_compute_shader *object;
     HRESULT hr;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     if (FAILED(hr = d3d11_compute_shader_init(object, device, byte_code, byte_code_length)))
     {
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -1730,7 +1730,7 @@ static ULONG STDMETHODCALLTYPE d3d11_class_linkage_Release(ID3D11ClassLinkage *i
         ID3D11Device2 *device = class_linkage->device;
 
         wined3d_private_store_cleanup(&class_linkage->private_store);
-        heap_free(class_linkage);
+        free(class_linkage);
 
         ID3D11Device2_Release(device);
     }
@@ -1819,7 +1819,7 @@ HRESULT d3d11_class_linkage_create(struct d3d_device *device, struct d3d11_class
 {
     struct d3d11_class_linkage *object;
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
         return E_OUTOFMEMORY;
 
     object->ID3D11ClassLinkage_iface.lpVtbl = &d3d11_class_linkage_vtbl;

dlls/d3d11/state.c
@@ -325,7 +325,7 @@ static void STDMETHODCALLTYPE d3d_blend_state_wined3d_object_destroyed(void *par
 
     wine_rb_remove(&device->blend_states, &state->entry);
     wined3d_private_store_cleanup(&state->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_blend_state_wined3d_parent_ops =
@@ -406,7 +406,7 @@ HRESULT d3d_blend_state_create(struct d3d_device *device, const D3D11_BLEND_DESC
         return S_OK;
     }
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
     {
         wined3d_mutex_unlock();
         return E_OUTOFMEMORY;
@@ -422,7 +422,7 @@ HRESULT d3d_blend_state_create(struct d3d_device *device, const D3D11_BLEND_DESC
     {
         ERR("Failed to insert blend state entry.\n");
         wined3d_private_store_cleanup(&object->private_store);
-        heap_free(object);
+        free(object);
         wined3d_mutex_unlock();
         return E_FAIL;
     }
@@ -452,7 +452,7 @@ HRESULT d3d_blend_state_create(struct d3d_device *device, const D3D11_BLEND_DESC
         WARN("Failed to create wined3d blend state, hr %#lx.\n", hr);
         wined3d_private_store_cleanup(&object->private_store);
         wine_rb_remove(&device->blend_states, &object->entry);
-        heap_free(object);
+        free(object);
         wined3d_mutex_unlock();
         return hr;
     }
@@ -730,7 +730,7 @@ static void STDMETHODCALLTYPE d3d_depthstencil_state_wined3d_object_destroyed(vo
 
     wine_rb_remove(&device->depthstencil_states, &state->entry);
     wined3d_private_store_cleanup(&state->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_depthstencil_state_wined3d_parent_ops =
@@ -799,7 +799,7 @@ HRESULT d3d_depthstencil_state_create(struct d3d_device *device, const D3D11_DEP
         return S_OK;
     }
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
     {
         wined3d_mutex_unlock();
         return E_OUTOFMEMORY;
@@ -815,7 +815,7 @@ HRESULT d3d_depthstencil_state_create(struct d3d_device *device, const D3D11_DEP
     {
         ERR("Failed to insert depth/stencil state entry.\n");
         wined3d_private_store_cleanup(&object->private_store);
-        heap_free(object);
+        free(object);
         wined3d_mutex_unlock();
         return E_FAIL;
     }
@@ -843,7 +843,7 @@ HRESULT d3d_depthstencil_state_create(struct d3d_device *device, const D3D11_DEP
         WARN("Failed to create wined3d depth/stencil state, hr %#lx.\n", hr);
         wined3d_private_store_cleanup(&object->private_store);
         wine_rb_remove(&device->depthstencil_states, &object->entry);
-        heap_free(object);
+        free(object);
         wined3d_mutex_unlock();
         return hr;
     }
@@ -1138,7 +1138,7 @@ static void STDMETHODCALLTYPE d3d_rasterizer_state_wined3d_object_destroyed(void
 
     wine_rb_remove(&device->rasterizer_states, &state->entry);
     wined3d_private_store_cleanup(&state->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_rasterizer_state_wined3d_parent_ops =
@@ -1235,7 +1235,7 @@ HRESULT d3d_rasterizer_state_create(struct d3d_device *device, const D3D11_RASTE
         return S_OK;
     }
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
     {
         wined3d_mutex_unlock();
         return E_OUTOFMEMORY;
@@ -1246,7 +1246,7 @@ HRESULT d3d_rasterizer_state_create(struct d3d_device *device, const D3D11_RASTE
     if (FAILED(hr))
     {
         WARN("Failed to initialise rasterizer state, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 
@@ -1524,7 +1524,7 @@ static void STDMETHODCALLTYPE d3d_sampler_wined3d_object_destroyed(void *parent)
 
     wine_rb_remove(&device->sampler_states, &state->entry);
     wined3d_private_store_cleanup(&state->private_store);
-    heap_free(parent);
+    free(parent);
 }
 
 static const struct wined3d_parent_ops d3d_sampler_wined3d_parent_ops =
@@ -1653,7 +1653,7 @@ HRESULT d3d_sampler_state_create(struct d3d_device *device, const D3D11_SAMPLER_
         return S_OK;
     }
 
-    if (!(object = heap_alloc_zero(sizeof(*object))))
+    if (!(object = calloc(1, sizeof(*object))))
     {
         wined3d_mutex_unlock();
         return E_OUTOFMEMORY;
@@ -1664,7 +1664,7 @@ HRESULT d3d_sampler_state_create(struct d3d_device *device, const D3D11_SAMPLER_
     if (FAILED(hr))
     {
         WARN("Failed to initialise sampler state, hr %#lx.\n", hr);
-        heap_free(object);
+        free(object);
         return hr;
     }
 

commit 7d20333faf8e93a3a16680cf1beadc9cdcb50c45

d3d11: Implement d3d11_input_layout_to_wined3d_declaration() on top of vkd3d_shader_parse_input_signature().
This was originally prompted by the fact that
wined3d_extract_shader_input_signature_from_dxbc() allocates elements with
HeapAlloc(), but d3d11_input_layout_to_wined3d_declaration() attempts to free
them with free(). That's a regression introduced by commit
b951c37b8791ceb052c9e159ad7927f75a72d667. Since we're touching the code
though, we may as well use vkd3d_shader_parse_input_signature(), and get rid
of wined3d_extract_shader_input_signature_from_dxbc().

dlls/d3d11/Makefile.in
@@ -1,6 +1,7 @@
 MODULE    = d3d11.dll
 IMPORTLIB = d3d11
 IMPORTS   = dxguid uuid dxgi wined3d
+EXTRAINCL = $(VKD3D_PE_CFLAGS)
 
 C_SRCS = \
 	async.c \

dlls/d3d11/inputlayout.c
@@ -19,27 +19,28 @@
 
 #include "d3d11_private.h"
 #include "winternl.h"
+#include <vkd3d_shader.h>
 
 WINE_DEFAULT_DEBUG_CHANNEL(d3d11);
 
-static BOOL is_vs_sysval_semantic(const struct wined3d_shader_signature_element *e)
+static BOOL is_vs_sysval_semantic(const struct vkd3d_shader_signature_element *e)
 {
     return !stricmp(e->semantic_name, "sv_instanceid") || !stricmp(e->semantic_name, "sv_vertexid");
 }
 
 static unsigned int find_input_element(const D3D11_INPUT_ELEMENT_DESC *element_descs, unsigned int element_count,
-        struct wined3d_shader_signature_element *ise)
+        const struct vkd3d_shader_signature_element *ise)
 {
     const D3D11_INPUT_ELEMENT_DESC *f;
     unsigned int i;
 
-    if (ise->stream_idx)
+    if (ise->stream_index)
         return element_count;
 
     for (i = 0; i < element_count; ++i)
     {
         f = &element_descs[i];
-        if (!stricmp(ise->semantic_name, f->SemanticName) && ise->semantic_idx == f->SemanticIndex)
+        if (!stricmp(ise->semantic_name, f->SemanticName) && ise->semantic_index == f->SemanticIndex)
             return i;
     }
     return element_count;
@@ -54,22 +55,23 @@ static HRESULT d3d11_input_layout_to_wined3d_declaration(const D3D11_INPUT_ELEME
         UINT element_count, const void *shader_byte_code, SIZE_T shader_byte_code_length,
         struct wined3d_vertex_element **wined3d_elements)
 {
-    struct wined3d_shader_signature_element *ise;
-    struct wined3d_shader_signature is;
+    const struct vkd3d_shader_code dxbc = {shader_byte_code, shader_byte_code_length};
+    struct vkd3d_shader_signature_element *ise;
     const D3D11_INPUT_ELEMENT_DESC *f;
+    struct vkd3d_shader_signature is;
     unsigned int i, index;
-    HRESULT hr;
+    int ret;
 
-    if (FAILED(hr = wined3d_extract_shader_input_signature_from_dxbc(&is, shader_byte_code, shader_byte_code_length)))
+    if ((ret = vkd3d_shader_parse_input_signature(&dxbc, &is, NULL)) < 0)
     {
-        ERR("Failed to extract input signature.\n");
+        ERR("Failed to extract input signature, ret %d.\n", ret);
         return E_FAIL;
     }
 
     if (!(*wined3d_elements = calloc(element_count, sizeof(**wined3d_elements))))
     {
         ERR("Failed to allocate wined3d vertex element array memory.\n");
-        free(is.elements);
+        vkd3d_shader_free_shader_signature(&is);
         return E_OUTOFMEMORY;
     }
 
@@ -96,18 +98,18 @@ static HRESULT d3d11_input_layout_to_wined3d_declaration(const D3D11_INPUT_ELEME
     {
         if ((index = find_input_element(element_descs, element_count, &ise[i])) < element_count)
         {
-            (*wined3d_elements)[index].output_slot = ise[i].register_idx;
+            (*wined3d_elements)[index].output_slot = ise[i].register_index;
         }
         else if (!is_vs_sysval_semantic(&ise[i]))
         {
-            WARN("Input element %s%u not found in shader signature.\n", ise[i].semantic_name, ise[i].semantic_idx);
+            WARN("Input element %s%u not found in shader signature.\n", ise[i].semantic_name, ise[i].semantic_index);
             free(*wined3d_elements);
-            free(is.elements);
+            vkd3d_shader_free_shader_signature(&is);
             return E_INVALIDARG;
         }
     }
 
-    free(is.elements);
+    vkd3d_shader_free_shader_signature(&is);
 
     return S_OK;
 }

dlls/wined3d/shader_sm4.c
@@ -2159,26 +2159,3 @@ HRESULT shader_extract_from_dxbc(struct wined3d_shader *shader,
 
     return hr;
 }
-
-static HRESULT shader_isgn_chunk_handler(const char *data, DWORD data_size, DWORD tag, void *ctx)
-{
-    struct wined3d_shader_signature *is = ctx;
-
-    if (tag != TAG_ISGN)
-        return S_OK;
-
-    if (is->elements)
-    {
-        FIXME("Multiple shader signatures.\n");
-        return S_OK;
-    }
-
-    return shader_parse_signature(tag, data, data_size, is);
-}
-
-HRESULT CDECL wined3d_extract_shader_input_signature_from_dxbc(struct wined3d_shader_signature *signature,
-        const void *code, SIZE_T code_size)
-{
-    memset(signature, 0, sizeof(*signature));
-    return parse_dxbc(code, code_size, shader_isgn_chunk_handler, signature);
-}

dlls/wined3d/wined3d.spec
@@ -319,8 +319,6 @@
 @ cdecl wined3d_vertex_declaration_get_parent(ptr)
 @ cdecl wined3d_vertex_declaration_incref(ptr)
 
-@ cdecl wined3d_extract_shader_input_signature_from_dxbc(ptr ptr long)
-
 @ cdecl vkd3d_create_instance(ptr ptr)
 @ cdecl vkd3d_instance_decref(ptr)
 @ cdecl vkd3d_instance_get_vk_instance(ptr)

dlls/wined3d/wined3d_private.h
@@ -778,6 +778,52 @@ enum wined3d_shader_conditional_op
     WINED3D_SHADER_CONDITIONAL_OP_Z  = 1
 };
 
+enum wined3d_sysval_semantic
+{
+    WINED3D_SV_POSITION                  = 0x01,
+    WINED3D_SV_CLIP_DISTANCE             = 0x02,
+    WINED3D_SV_CULL_DISTANCE             = 0x03,
+    WINED3D_SV_RENDER_TARGET_ARRAY_INDEX = 0x04,
+    WINED3D_SV_VIEWPORT_ARRAY_INDEX      = 0x05,
+    WINED3D_SV_VERTEX_ID                 = 0x06,
+    WINED3D_SV_PRIMITIVE_ID              = 0x07,
+    WINED3D_SV_INSTANCE_ID               = 0x08,
+    WINED3D_SV_IS_FRONT_FACE             = 0x09,
+    WINED3D_SV_SAMPLE_INDEX              = 0x0a,
+    WINED3D_SV_TESS_FACTOR_QUADEDGE      = 0x0b,
+    WINED3D_SV_TESS_FACTOR_QUADINT       = 0x0c,
+    WINED3D_SV_TESS_FACTOR_TRIEDGE       = 0x0d,
+    WINED3D_SV_TESS_FACTOR_TRIINT        = 0x0e,
+    WINED3D_SV_TESS_FACTOR_LINEDET       = 0x0f,
+    WINED3D_SV_TESS_FACTOR_LINEDEN       = 0x10,
+};
+
+enum wined3d_component_type
+{
+    WINED3D_TYPE_UNKNOWN = 0x0,
+    WINED3D_TYPE_UINT    = 0x1,
+    WINED3D_TYPE_INT     = 0x2,
+    WINED3D_TYPE_FLOAT   = 0x3,
+};
+
+struct wined3d_shader_signature_element
+{
+    const char *semantic_name;
+    unsigned int semantic_idx;
+    unsigned int stream_idx;
+    enum wined3d_sysval_semantic sysval_semantic;
+    enum wined3d_component_type component_type;
+    unsigned int register_idx;
+    uint32_t mask;
+    unsigned int min_precision;
+};
+
+struct wined3d_shader_signature
+{
+    unsigned int element_count;
+    struct wined3d_shader_signature_element *elements;
+};
+
 #define WINED3D_SM1_VS  0xfffeu
 #define WINED3D_SM1_PS  0xffffu
 #define WINED3D_SM4_PS  0x0000u

include/wine/wined3d.h
@@ -811,34 +811,6 @@ enum wined3d_decl_usage
     WINED3D_DECL_USAGE_SAMPLE               = 13
 };
 
-enum wined3d_sysval_semantic
-{
-    WINED3D_SV_POSITION                     = 1,
-    WINED3D_SV_CLIP_DISTANCE                = 2,
-    WINED3D_SV_CULL_DISTANCE                = 3,
-    WINED3D_SV_RENDER_TARGET_ARRAY_INDEX    = 4,
-    WINED3D_SV_VIEWPORT_ARRAY_INDEX         = 5,
-    WINED3D_SV_VERTEX_ID                    = 6,
-    WINED3D_SV_PRIMITIVE_ID                 = 7,
-    WINED3D_SV_INSTANCE_ID                  = 8,
-    WINED3D_SV_IS_FRONT_FACE                = 9,
-    WINED3D_SV_SAMPLE_INDEX                 = 10,
-    WINED3D_SV_TESS_FACTOR_QUADEDGE         = 11,
-    WINED3D_SV_TESS_FACTOR_QUADINT          = 12,
-    WINED3D_SV_TESS_FACTOR_TRIEDGE          = 13,
-    WINED3D_SV_TESS_FACTOR_TRIINT           = 14,
-    WINED3D_SV_TESS_FACTOR_LINEDET          = 15,
-    WINED3D_SV_TESS_FACTOR_LINEDEN          = 16,
-};
-
-enum wined3d_component_type
-{
-    WINED3D_TYPE_UNKNOWN = 0,
-    WINED3D_TYPE_UINT    = 1,
-    WINED3D_TYPE_INT     = 2,
-    WINED3D_TYPE_FLOAT   = 3,
-};
-
 enum wined3d_scanline_ordering
 {
     WINED3D_SCANLINE_ORDERING_UNKNOWN       = 0,
@@ -2113,24 +2085,6 @@ struct wined3d_sampler_desc
     BOOL srgb_decode;
 };
 
-struct wined3d_shader_signature_element
-{
-    const char *semantic_name;
-    unsigned int semantic_idx;
-    unsigned int stream_idx;
-    enum wined3d_sysval_semantic sysval_semantic;
-    enum wined3d_component_type component_type;
-    unsigned int register_idx;
-    DWORD mask;
-    unsigned int min_precision;
-};
-
-struct wined3d_shader_signature
-{
-    UINT element_count;
-    struct wined3d_shader_signature_element *elements;
-};
-
 struct wined3d_shader_desc
 {
     const DWORD *byte_code;
@@ -2929,9 +2883,6 @@ HRESULT __cdecl wined3d_vertex_declaration_create_from_fvf(struct wined3d_device
 ULONG __cdecl wined3d_vertex_declaration_decref(struct wined3d_vertex_declaration *declaration);
 void * __cdecl wined3d_vertex_declaration_get_parent(const struct wined3d_vertex_declaration *declaration);
 ULONG __cdecl wined3d_vertex_declaration_incref(struct wined3d_vertex_declaration *declaration);
-
-HRESULT __cdecl wined3d_extract_shader_input_signature_from_dxbc(struct wined3d_shader_signature *signature,
-        const void *byte_code, SIZE_T byte_code_size);
 
 static inline void wined3d_streaming_buffer_init(struct wined3d_streaming_buffer *buffer, unsigned int bind_flags)
 {