6b16ead607ed9426ec38d3db123bc732c309b90e
Zebediah Figura <zfigura@codeweavers.com>
2022-09-16 12:55:18 -0500

    ntdll: The async handle passed to set_async_direct_result() cannot be NULL.

--- dlls/ntdll/unix/sync.c
+++ dlls/ntdll/unix/sync.c
@@ -2533,21 +2533,22 @@ NTSTATUS WINAPI NtWaitForAlertByThreadId( const void *address, const LARGE_INTEG
 /* Notify direct completion of async and close the wait handle if it is no longer needed.
  * This function is a no-op (returns status as-is) if the supplied handle is NULL.
  */
-void set_async_direct_result( HANDLE *optional_handle, NTSTATUS status, ULONG_PTR information, BOOL mark_pending )
+void set_async_direct_result( HANDLE *async_handle, NTSTATUS status, ULONG_PTR information, BOOL mark_pending )
 {
     NTSTATUS ret;
 
-    if (!*optional_handle) return;
+    /* if we got STATUS_ALERTED, we must have a valid async handle */
+    assert( *async_handle );
 
     SERVER_START_REQ( set_async_direct_result )
     {
-        req->handle       = wine_server_obj_handle( *optional_handle );
+        req->handle       = wine_server_obj_handle( *async_handle );
         req->status       = status;
         req->information  = information;
         req->mark_pending = mark_pending;
         ret = wine_server_call( req );
         if (ret == STATUS_SUCCESS)
-            *optional_handle = wine_server_ptr_handle( reply->handle );
+            *async_handle = wine_server_ptr_handle( reply->handle );
     }
     SERVER_END_REQ;
 
