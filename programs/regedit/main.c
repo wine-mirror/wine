99b3862e67b0841e2eb4f5f0d099b0403917c581
Rémi Bernon <rbernon@codeweavers.com>
2022-09-29 18:03:26 +0200

    regedit: Call InitCommonControls after command-line has been processed.
    Avoid triggering the load of comctl32 early.

--- programs/regedit/main.c
+++ programs/regedit/main.c
@@ -132,8 +132,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     HACCEL hAccel;
     BOOL is_wow64;
 
-    InitCommonControls();
-
     if (ProcessCmdLine(GetCommandLineW())) {
         return 0;
     }
@@ -160,6 +158,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         else WINE_ERR( "failed to restart 64-bit %s, err %ld\n", wine_dbgstr_w(filename), GetLastError() );
         Wow64RevertWow64FsRedirection( redir );
     }
+
+    InitCommonControls();
 
     /* Initialize global strings */
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, ARRAY_SIZE(szTitle));
