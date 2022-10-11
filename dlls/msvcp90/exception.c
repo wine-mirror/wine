95b8a3a1c93e1fe1ad2913c22f98c4f9b9719bb8
Paul Gofman <pgofman@codeweavers.com>
2022-10-11 15:36:36 -0500

    msvcp140: Import __ExceptionPtrAssign implementation.

--- dlls/msvcp90/exception.c
+++ dlls/msvcp90/exception.c
@@ -1028,6 +1028,22 @@ void __cdecl __ExceptionPtrCopy(exception_ptr *ep, const exception_ptr *copy)
         InterlockedIncrement(copy->ref);
 }
 
+/*********************************************************************
+ * ?__ExceptionPtrAssign@@YAXPAXPBX@Z
+ * ?__ExceptionPtrAssign@@YAXPEAXPEBX@Z
+ */
+void __cdecl __ExceptionPtrAssign(exception_ptr *ep, const exception_ptr *assign)
+{
+    TRACE("(%p %p)\n", ep, assign);
+
+    /* don't destroy object stored in ep */
+    if (ep->ref)
+        InterlockedDecrement(ep->ref);
+
+    *ep = *assign;
+    if (ep->ref)
+        InterlockedIncrement(ep->ref);
+}
 #endif
 
 #if _MSVCP_VER >= 70 || defined(_MSVCIRT)
