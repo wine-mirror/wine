480da372b25b9a432129859b9facacc6436a5a30
Brendan Shanks <bshanks@codeweavers.com>
2022-10-18 11:01:34 -0700

    xinput1_3: Set thread name for hid_update_thread_proc.

--- dlls/xinput1_3/main.c
+++ dlls/xinput1_3/main.c
@@ -707,6 +707,8 @@ static DWORD WINAPI hid_update_thread_proc(void *param)
     HWND hwnd;
     MSG msg;
 
+    SetThreadDescription(GetCurrentThread(), L"wine_xinput_hid_update");
+
     RegisterClassExW(&cls);
     hwnd = CreateWindowExW(0, cls.lpszClassName, NULL, 0, 0, 0, 0, 0,
                            HWND_MESSAGE, NULL, NULL, NULL);
