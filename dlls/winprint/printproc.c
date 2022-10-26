b7d7b23a617bdbb4177830c074cc0c8807ec9bce
Piotr Caban <piotr@codeweavers.com>
2022-10-26 13:12:27 +0200

    winprint: Add stub dll.

---
+++ dlls/winprint/printproc.c
@@ -0,0 +1,64 @@
+/*
+ * Print processor implementation.
+ *
+ * Copyright 2022 Piotr Caban for CodeWeavers
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include "windows.h"
+#include "winspool.h"
+#include "ddk/winsplp.h"
+
+#include "wine/debug.h"
+
+WINE_DEFAULT_DEBUG_CHANNEL(winprint);
+
+BOOL WINAPI EnumPrintProcessorDatatypesW(WCHAR *server, WCHAR *name, DWORD level,
+        BYTE *datatypes, DWORD size, DWORD *needed, DWORD *no)
+{
+    FIXME("%s, %s, %ld, %p, %ld, %p, %p\n", debugstr_w(server), debugstr_w(name),
+            level, datatypes, size, needed, no);
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    return FALSE;
+}
+
+HANDLE WINAPI OpenPrintProcessor(WCHAR *port, PRINTPROCESSOROPENDATA *open_data)
+{
+    FIXME("%s, %p\n", debugstr_w(port), open_data);
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    return NULL;
+}
+
+BOOL WINAPI ClosePrintProcessor(HANDLE pp)
+{
+    FIXME("%p\n", pp);
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    return FALSE;
+}
+
+BOOL WINAPI PrintDocumentOnPrintProcessor(HANDLE pp, WCHAR *doc_name)
+{
+    FIXME("%p, %s\n", pp, debugstr_w(doc_name));
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    return FALSE;
+}
+
+BOOL WINAPI ControlPrintProcessor(HANDLE pp, DWORD cmd)
+{
+    FIXME("%p, %ld\n", pp, cmd);
+    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
+    return FALSE;
+}
---
+++ dlls/winprint/winprint.spec
@@ -0,0 +1,6 @@
+@ stdcall ClosePrintProcessor(ptr)
+@ stdcall ControlPrintProcessor(ptr long)
+@ stdcall EnumPrintProcessorDatatypesW(ptr ptr long ptr long ptr ptr)
+@ stub GetPrintProcessorCapabilities
+@ stdcall OpenPrintProcessor(ptr ptr)
+@ stdcall PrintDocumentOnPrintProcessor(ptr ptr)
