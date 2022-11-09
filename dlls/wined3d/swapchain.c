wine-mirror/wine

commit a2c03e23565779d2743f5750c37de0be3f7222bf

wined3d: Set name of internal threads.

dlls/wined3d/swapchain.c
@@ -2194,6 +2194,8 @@ static DWORD WINAPI wined3d_set_window_state(void *ctx)
     struct wined3d_window_state *s = ctx;
     bool filter;
 
+    SetThreadDescription(GetCurrentThread(), L"wined3d_set_window_state");
+
     filter = wined3d_filter_messages(s->window, TRUE);
 
     if (s->set_style)

dlls/wined3d/cs.c
@@ -3354,6 +3354,7 @@ static DWORD WINAPI wined3d_cs_run(void *ctx)
     bool run = true;
 
     TRACE("Started.\n");
+    SetThreadDescription(GetCurrentThread(), L"wined3d_cs");
 
     /* Copy the module handle to a local variable to avoid racing with the
      * thread freeing "cs" before the FreeLibraryAndExitThread() call. */

dlls/wined3d/directx.c
@@ -1069,6 +1069,8 @@ static DWORD CALLBACK notification_thread_func(void *stop_event)
     struct wined3d_video_memory_info info;
     HRESULT hr;
 
+    SetThreadDescription(GetCurrentThread(), L"wined3d_budget_change_notification");
+
     while (TRUE)
     {
         wined3d_mutex_lock();

commit efcb73441428705cdd49bf41bd396c4e829ac5c8

wined3d: Set the window state thread description only if it's actually a new thread.
Fixes: a2c03e23565779d2743f5750c37de0be3f7222bf

dlls/wined3d/swapchain.c
@@ -2200,8 +2200,6 @@ static DWORD WINAPI set_window_state_thread(void *ctx)
     struct wined3d_window_state *s = ctx;
     bool filter;
 
-    SetThreadDescription(GetCurrentThread(), L"wined3d_set_window_state");
-
     filter = wined3d_filter_messages(s->window, TRUE);
 
     if (s->set_style)
@@ -2229,11 +2227,18 @@ static void set_window_state(struct wined3d_window_state *s)
      * position can potentially deadlock if that thread isn't processing
      * messages. */
     if (window_tid == tid)
+    {
         set_window_state_thread(s);
-    else if (!(thread = CreateThread(NULL, 0, set_window_state_thread, s, 0, NULL)))
-        ERR("Failed to create thread.\n");
-    else
+    }
+    else if ((thread = CreateThread(NULL, 0, set_window_state_thread, s, 0, NULL)))
+    {
+        SetThreadDescription(thread, L"wined3d_set_window_state");
         CloseHandle(thread);
+    }
+    else
+    {
+        ERR("Failed to create thread.\n");
+    }
 }
 
 HRESULT wined3d_swapchain_state_setup_fullscreen(struct wined3d_swapchain_state *state,