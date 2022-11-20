db684bff7fc9b918fe80d8b2c68ab41ae78a094d
Mohamad Al-Jaf <mohamadaljaf@gmail.com>
2022-11-19 20:42:14 -0500

    windows.system.profile.systemmanufacturers: Add stub DLL.

---
+++ dlls/windows.system.profile.systemmanufacturers/private.h
@@ -0,0 +1,31 @@
+/* WinRT Windows.System.Profile.SystemManufacturers Implementation
+ *
+ * Copyright (C) 2022 Mohamad Al-Jaf
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <stdarg.h>
+#include <stddef.h>
+
+#define COBJMACROS
+#include "windef.h"
+#include "winbase.h"
+#include "winstring.h"
+
+#include "activation.h"
+
+#define WIDL_using_Windows_System_Profile_SystemManufacturers
+#include "windows.system.profile.systemmanufacturers.h"
