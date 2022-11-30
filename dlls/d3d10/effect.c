eb3e4b79acb73d9724dc6f978f927c4f9a3c3181
Nikolay Sivov <nsivov@codeweavers.com>
2022-11-30 17:26:34 +0300

    d3d10/effect: Add 'neg' instruction support for expressions.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

--- dlls/d3d10/effect.c
+++ dlls/d3d10/effect.c
@@ -207,6 +207,15 @@ struct preshader_instr
 
 typedef void (*pres_op_func)(float **args, unsigned int n, const struct preshader_instr *instr);
 
+static void pres_neg(float **args, unsigned int n, const struct preshader_instr *instr)
+{
+    float *retval = args[1];
+    unsigned int i;
+
+    for (i = 0; i < instr->comp_count; ++i)
+        retval[i] = -args[0][i];
+}
+
 static void pres_ftou(float **args, unsigned int n, const struct preshader_instr *instr)
 {
     float *retval = args[1];
@@ -255,6 +264,7 @@ struct preshader_op_info
 
 static const struct preshader_op_info preshader_ops[] =
 {
+    { 0x101, "neg",  pres_neg  },
     { 0x133, "ftou", pres_ftou },
     { 0x200, "min",  pres_min  },
     { 0x201, "max",  pres_max  },
