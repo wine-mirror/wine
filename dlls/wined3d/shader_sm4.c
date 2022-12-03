a942b560525cc47275f34f32c80e147c10a5c8ab
Eric Pouech <eric.pouech@gmail.com>
2022-12-03 09:59:11 +0100

    wined3d: Transfer long types' enabling at file level.
    Signed-off-by: Eric Pouech <eric.pouech@gmail.com>

--- dlls/wined3d/shader_sm4.c
+++ dlls/wined3d/shader_sm4.c
@@ -15,6 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
+#define WINE_NO_LONG_TYPES /* temporary */
 
 #include "wined3d_private.h"
 
