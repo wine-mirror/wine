7cab61754c307f16b5ccb1d6b743481cd366fc93
Eric Pouech <eric.pouech@gmail.com>
2022-12-08 10:44:00 +0100

    wined3d: Enable long types in nvidia_texture_shader.c.

--- dlls/wined3d/nvidia_texture_shader.c
+++ dlls/wined3d/nvidia_texture_shader.c
@@ -19,7 +19,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
-#define WINE_NO_LONG_TYPES /* temporary */
 
 #include <stdio.h>
 
