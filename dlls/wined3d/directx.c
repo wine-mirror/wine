cbbf1f054c9e6ecd56dce57563527118281264a0
Max Figura <maxfigura5061@gmail.com>
2022-12-12 17:29:11 -0600

    wined3d: Move the WINED3D_RS_WRAP6 stub to wined3d_device_apply_stateblock.

--- dlls/wined3d/directx.c
+++ dlls/wined3d/directx.c
@@ -2817,7 +2817,6 @@ static const struct wined3d_state_entry_template misc_state_template_no3d[] =
     {STATE_RENDER(WINED3D_RS_WRAP3),                      {STATE_VDECL}},
     {STATE_RENDER(WINED3D_RS_WRAP4),                      {STATE_VDECL}},
     {STATE_RENDER(WINED3D_RS_WRAP5),                      {STATE_VDECL}},
-    {STATE_RENDER(WINED3D_RS_WRAP6),                      {STATE_VDECL}},
     {STATE_RENDER(WINED3D_RS_EXTENTS),                    {STATE_VDECL}},
     {STATE_RENDER(WINED3D_RS_COLORKEYBLENDENABLE),        {STATE_VDECL}},
     {STATE_RENDER(WINED3D_RS_SOFTWAREVERTEXPROCESSING),   {STATE_VDECL}},
