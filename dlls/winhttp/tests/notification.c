wine-mirror/wine

commit aa8f97e8294b0618de0e8a45ff2adef0c4e516da

winhttp: Receive server reply in send_request().

dlls/winhttp/request.c
@@ -800,6 +800,13 @@ BOOL WINAPI WinHttpQueryHeaders( HINTERNET hrequest, DWORD level, const WCHAR *n
         SetLastError( ERROR_WINHTTP_INCORRECT_HANDLE_TYPE );
         return FALSE;
     }
+    if (request->state < REQUEST_RESPONSE_STATE_RESPONSE_RECEIVED && !(level & WINHTTP_QUERY_FLAG_REQUEST_HEADERS)
+            && ((level & ~QUERY_MODIFIER_MASK) != WINHTTP_QUERY_REQUEST_METHOD))
+    {
+        release_object( &request->hdr );
+        SetLastError( ERROR_WINHTTP_INCORRECT_HANDLE_STATE );
+        return FALSE;
+    }
 
     ret = query_headers( request, level, name, buffer, buflen, index );
 
@@ -2213,7 +2220,9 @@ static DWORD send_request( struct request *request, const WCHAR *headers, DWORD 
     int bytes_sent;
     DWORD ret, len;
 
+    request->read_reply_status = ERROR_WINHTTP_INCORRECT_HANDLE_STATE;
     request->read_reply_len = 0;
+    request->state = REQUEST_RESPONSE_STATE_NONE;
 
     if (request->flags & REQUEST_FLAG_WEBSOCKET_UPGRADE
         && request->websocket_set_send_buffer_size < MIN_WEBSOCKET_SEND_BUFFER_SIZE)
@@ -2293,6 +2302,8 @@ static DWORD send_request( struct request *request, const WCHAR *headers, DWORD 
         request->optional_len = optional_len;
         len += optional_len;
     }
+    netconn_set_timeout( request->netconn, FALSE, request->receive_response_timeout );
+    request->read_reply_status = read_reply( request );
     send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_REQUEST_SENT, &len, sizeof(len) );
 
 end:
@@ -2839,15 +2850,20 @@ static DWORD receive_response( struct request *request, BOOL async )
 {
     DWORD ret, size, query, status;
 
-    if (!request->netconn) return ERROR_WINHTTP_INCORRECT_HANDLE_STATE;
+    TRACE( "request state %d.\n", request->state );
 
-    netconn_set_timeout( request->netconn, FALSE, request->receive_response_timeout );
+    if (request->state >= REQUEST_RESPONSE_STATE_RESPONSE_RECEIVED)
+    {
+        ret = ERROR_WINHTTP_INCORRECT_HANDLE_STATE;
+        goto done;
+    }
+
     for (;;)
     {
         send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_RECEIVING_RESPONSE, NULL, 0 );
         send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_RESPONSE_RECEIVED,
                        &request->read_reply_len, sizeof(request->read_reply_len) );
-        if ((ret = read_reply( request ))) break;
+        if ((ret = request->read_reply_status)) break;
 
         size = sizeof(DWORD);
         query = WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER;
@@ -2885,8 +2901,12 @@ static DWORD receive_response( struct request *request, BOOL async )
         break;
     }
 
-    if (request->netconn) netconn_set_timeout( request->netconn, FALSE, request->receive_timeout );
-
+done:
+    if (!ret)
+    {
+        request->state = REQUEST_RESPONSE_STATE_RESPONSE_RECEIVED;
+        if (request->netconn) netconn_set_timeout( request->netconn, FALSE, request->receive_timeout );
+    }
     if (async)
     {
         if (!ret) send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_HEADERS_AVAILABLE, NULL, 0 );

dlls/winhttp/winhttp_private.h
@@ -174,6 +174,12 @@ enum request_flags
     REQUEST_FLAG_WEBSOCKET_UPGRADE = 0x01,
 };
 
+enum request_response_state
+{
+    REQUEST_RESPONSE_STATE_NONE,
+    REQUEST_RESPONSE_STATE_RESPONSE_RECEIVED,
+};
+
 struct request
 {
     struct object_header hdr;
@@ -221,6 +227,8 @@ struct request
     unsigned int websocket_receive_buffer_size;
     unsigned int websocket_send_buffer_size, websocket_set_send_buffer_size;
     int read_reply_len;
+    DWORD read_reply_status;
+    enum request_response_state state;
 };
 
 enum socket_state

dlls/winhttp/session.c
@@ -1271,6 +1271,7 @@ HINTERNET WINAPI WinHttpOpenRequest( HINTERNET hconnect, const WCHAR *verb, cons
     request->websocket_receive_buffer_size = connect->session->websocket_receive_buffer_size;
     request->websocket_send_buffer_size = connect->session->websocket_send_buffer_size;
     request->websocket_set_send_buffer_size = request->websocket_send_buffer_size;
+    request->read_reply_status = ERROR_WINHTTP_INCORRECT_HANDLE_STATE;
 
     if (!verb || !verb[0]) verb = L"GET";
     if (!(request->verb = wcsdup( verb ))) goto end;

dlls/winhttp/tests/winhttp.c
@@ -3277,14 +3277,14 @@ static void test_websocket(int port)
     ret = WinHttpQueryHeaders(request, WINHTTP_QUERY_UPGRADE, NULL, &header, &size, NULL);
     error = GetLastError();
     ok(!ret, "success\n");
-    todo_wine ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
+    ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
 
     size = sizeof(header);
     SetLastError(0xdeadbeef);
     ret = WinHttpQueryHeaders(request, WINHTTP_QUERY_CONNECTION, NULL, &header, &size, NULL);
     error = GetLastError();
     ok(!ret, "success\n");
-    todo_wine ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
+    ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
 
     index = 0;
     size = sizeof(buf);
@@ -3312,14 +3312,14 @@ static void test_websocket(int port)
     ret = WinHttpQueryHeaders(request, WINHTTP_QUERY_UPGRADE, NULL, &header, &size, NULL);
     error = GetLastError();
     ok(!ret, "success\n");
-    todo_wine ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
+    ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
 
     size = sizeof(header);
     SetLastError(0xdeadbeef);
     ret = WinHttpQueryHeaders(request, WINHTTP_QUERY_CONNECTION, NULL, &header, &size, NULL);
     error = GetLastError();
     ok(!ret, "success\n");
-    todo_wine ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
+    ok(error == ERROR_WINHTTP_INCORRECT_HANDLE_STATE, "got %lu\n", error);
 
     index = 0;
     buf[0] = 0;

commit 3f51fcf8ac40296265f0bf88ed51e0778f6b266a

winhttp: Only read server reply in send_request() if the whole request is sent.
Fixes a regression introduced by
aa8f97e8294b0618de0e8a45ff2adef0c4e516da.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=54184

dlls/winhttp/request.c
@@ -2216,9 +2216,9 @@ static DWORD send_request( struct request *request, const WCHAR *headers, DWORD 
 {
     struct connect *connect = request->connect;
     struct session *session = connect->session;
+    DWORD ret, len, buflen, content_length;
     char *wire_req;
     int bytes_sent;
-    DWORD ret, len;
 
     TRACE( "request state %d.\n", request->state );
 
@@ -2305,14 +2305,29 @@ static DWORD send_request( struct request *request, const WCHAR *headers, DWORD 
         request->optional_len = optional_len;
         len += optional_len;
     }
-    netconn_set_timeout( request->netconn, FALSE, request->receive_response_timeout );
-    request->read_reply_status = read_reply( request );
     send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_REQUEST_SENT, &len, sizeof(len) );
 
-    if (request->state == REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED)
-        request->state = REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REQUEST_SENT;
+    buflen = sizeof(content_length);
+    if (query_headers( request, WINHTTP_QUERY_FLAG_REQUEST_HEADERS | WINHTTP_QUERY_CONTENT_LENGTH
+                       | WINHTTP_QUERY_FLAG_NUMBER, NULL, &content_length, &buflen, NULL ))
+        content_length = total_len;
+
+    if (content_length <= optional_len)
+    {
+        netconn_set_timeout( request->netconn, FALSE, request->receive_response_timeout );
+        request->read_reply_status = read_reply( request );
+        if (request->state == REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED)
+            request->state = REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REPLY_RECEIVED;
+        else
+            request->state = REQUEST_RESPONSE_STATE_REPLY_RECEIVED;
+    }
     else
-        request->state = REQUEST_RESPONSE_STATE_REQUEST_SENT;
+    {
+        if (request->state == REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED)
+            request->state = REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REQUEST_SENT;
+        else
+            request->state = REQUEST_RESPONSE_STATE_REQUEST_SENT;
+    }
 
 end:
     if (async)
@@ -2895,10 +2910,24 @@ static DWORD receive_response( struct request *request )
 
     case REQUEST_RESPONSE_STATE_REQUEST_SENT:
         send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_RECEIVING_RESPONSE, NULL, 0 );
+        if (async_mode)
+        {
+            request->state = REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REQUEST_SENT;
+            return queue_receive_response( request );
+        }
+        /* fallthrough */
+    case REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REQUEST_SENT:
+        netconn_set_timeout( request->netconn, FALSE, request->receive_response_timeout );
+        request->read_reply_status = read_reply( request );
+        request->state = REQUEST_RESPONSE_STATE_REPLY_RECEIVED;
         break;
 
-    case REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REQUEST_SENT:
-        request->state = REQUEST_RESPONSE_STATE_REQUEST_SENT;
+    case REQUEST_RESPONSE_STATE_REPLY_RECEIVED:
+        send_callback( &request->hdr, WINHTTP_CALLBACK_STATUS_RECEIVING_RESPONSE, NULL, 0 );
+        break;
+
+    case REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REPLY_RECEIVED:
+        request->state = REQUEST_RESPONSE_STATE_REPLY_RECEIVED;
         break;
 
     default:

dlls/winhttp/winhttp_private.h
@@ -181,6 +181,8 @@ enum request_response_state
     REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED,
     REQUEST_RESPONSE_STATE_REQUEST_SENT,
     REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REQUEST_SENT,
+    REQUEST_RESPONSE_STATE_REPLY_RECEIVED,
+    REQUEST_RESPONSE_STATE_READ_RESPONSE_QUEUED_REPLY_RECEIVED,
     REQUEST_RESPONSE_RECURSIVE_REQUEST,
     REQUEST_RESPONSE_STATE_RESPONSE_RECEIVED,
 };

dlls/winhttp/tests/notification.c
@@ -1852,6 +1852,8 @@ struct test_recursion_context
     DWORD main_thread_id;
     DWORD receive_response_thread_id;
     BOOL headers_available;
+    DWORD total_len;
+    BYTE *send_buffer;
 };
 
 /* The limit is 128 before Win7 and 3 on newer Windows. */
@@ -1872,17 +1874,29 @@ static void CALLBACK test_recursion_callback( HINTERNET handle, DWORD_PTR contex
         case WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE:
             if (status == context->call_receive_response_status)
             {
-                context->receive_response_thread_id = GetCurrentThreadId();
-                ret = WinHttpReceiveResponse( context->request, NULL );
-                ok( ret, "failed to receive response, GetLastError() %lu\n", GetLastError() );
+                if (context->total_len)
+                {
+                    ret = WinHttpWriteData( context->request, context->send_buffer, context->total_len, NULL );
+                    ok(ret, "failed.\n");
+                }
+                else
+                {
+                    context->receive_response_thread_id = GetCurrentThreadId();
+                    ret = WinHttpReceiveResponse( context->request, NULL );
+                    ok( ret, "failed to receive response, GetLastError() %lu\n", GetLastError() );
+                }
             }
             break;
 
+        case WINHTTP_CALLBACK_STATUS_WRITE_COMPLETE:
+            trace("WINHTTP_CALLBACK_STATUS_WRITE_COMPLETE thread %04lx.\n", GetCurrentThreadId());
+            context->receive_response_thread_id = GetCurrentThreadId();
+            ret = WinHttpReceiveResponse( context->request, NULL );
+            ok( ret, "failed to receive response, GetLastError() %lu\n", GetLastError() );
+            break;
+
         case WINHTTP_CALLBACK_STATUS_HEADERS_AVAILABLE:
-            if (context->call_receive_response_status == WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE)
-                ok( GetCurrentThreadId() == context->receive_response_thread_id,
-                    "expected callback to be called from the same thread, got %lx.\n", GetCurrentThreadId() );
-            else
+            if (context->call_receive_response_status != WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE)
                 ok( GetCurrentThreadId() != context->main_thread_id,
                     "expected callback to be called from the other thread, got main.\n" );
             context->headers_available = TRUE;
@@ -1933,7 +1947,6 @@ static void CALLBACK test_recursion_callback( HINTERNET handle, DWORD_PTR contex
             InterlockedDecrement( &context->recursion_count );
             break;
         case WINHTTP_CALLBACK_STATUS_RECEIVING_RESPONSE:
-        case WINHTTP_CALLBACK_STATUS_RESPONSE_RECEIVED:
             if (!context->headers_available
                 && context->call_receive_response_status == WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE)
                 ok( GetCurrentThreadId() == context->receive_response_thread_id,
@@ -1979,7 +1992,10 @@ static void test_recursion(void)
     ok( ret, "failed to receive response, GetLastError() %lu\n", GetLastError() );
 
     context.call_receive_response_status = WINHTTP_CALLBACK_STATUS_SENDREQUEST_COMPLETE;
-    ret = WinHttpSendRequest( request, NULL, 0, NULL, 0, 0, (DWORD_PTR)&context );
+    context.total_len = 1;
+    context.send_buffer = &b;
+    b = 0;
+    ret = WinHttpSendRequest( request, NULL, 0, NULL, 0, context.total_len, (DWORD_PTR)&context );
     err = GetLastError();
     if (!ret && (err == ERROR_WINHTTP_CANNOT_CONNECT || err == ERROR_WINHTTP_TIMEOUT))
     {
@@ -1994,6 +2010,8 @@ static void test_recursion(void)
     ok( ret, "failed to send request, GetLastError() %lu\n", GetLastError() );
 
     WaitForSingleObject( context.wait, INFINITE );
+    context.total_len = 0;
+    context.send_buffer = NULL;
 
     size = sizeof(status);
     ret = WinHttpQueryHeaders( request, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER, NULL,