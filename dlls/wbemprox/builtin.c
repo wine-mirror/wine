87758b72b95dd694dd786f23a00f47ea873dc1f5
Hans Leidekker <hans@codeweavers.com>
2022-12-23 10:40:35 +0100

    wbemprox: Implement GetBinaryValue() method.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=52878

--- dlls/wbemprox/builtin.c
+++ dlls/wbemprox/builtin.c
@@ -422,6 +422,7 @@ static const struct column col_stdregprov[] =
     { L"CreateKey",      CIM_FLAG_ARRAY|COL_FLAG_METHOD },
     { L"EnumKey",        CIM_FLAG_ARRAY|COL_FLAG_METHOD },
     { L"EnumValues",     CIM_FLAG_ARRAY|COL_FLAG_METHOD },
+    { L"GetBinaryValue", CIM_FLAG_ARRAY|COL_FLAG_METHOD },
     { L"GetStringValue", CIM_FLAG_ARRAY|COL_FLAG_METHOD },
     { L"SetStringValue", CIM_FLAG_ARRAY|COL_FLAG_METHOD },
     { L"SetDWORDValue",  CIM_FLAG_ARRAY|COL_FLAG_METHOD },
@@ -871,6 +872,7 @@ struct record_stdregprov
     class_method *createkey;
     class_method *enumkey;
     class_method *enumvalues;
+    class_method *getbinaryvalue;
     class_method *getstringvalue;
     class_method *setstringvalue;
     class_method *setdwordvalue;
@@ -980,6 +982,11 @@ static const struct record_param data_param[] =
     { L"StdRegProv", L"EnumValues", -1, L"ReturnValue", CIM_UINT32 },
     { L"StdRegProv", L"EnumValues", -1, L"sNames", CIM_STRING|CIM_FLAG_ARRAY },
     { L"StdRegProv", L"EnumValues", -1, L"Types", CIM_SINT32|CIM_FLAG_ARRAY },
+    { L"StdRegProv", L"GetBinaryValue", 1, L"hDefKey", CIM_SINT32, 0x80000002 },
+    { L"StdRegProv", L"GetBinaryValue", 1, L"sSubKeyName", CIM_STRING },
+    { L"StdRegProv", L"GetBinaryValue", 1, L"sValueName", CIM_STRING },
+    { L"StdRegProv", L"GetBinaryValue", -1, L"ReturnValue", CIM_UINT32 },
+    { L"StdRegProv", L"GetBinaryValue", -1, L"uValue", CIM_UINT8|CIM_FLAG_ARRAY },
     { L"StdRegProv", L"GetStringValue", 1, L"hDefKey", CIM_SINT32, 0x80000002 },
     { L"StdRegProv", L"GetStringValue", 1, L"sSubKeyName", CIM_STRING },
     { L"StdRegProv", L"GetStringValue", 1, L"sValueName", CIM_STRING },
@@ -1047,6 +1054,7 @@ static const struct record_stdregprov data_stdregprov[] =
         reg_create_key,
         reg_enum_key,
         reg_enum_values,
+        reg_get_binaryvalue,
         reg_get_stringvalue,
         reg_set_stringvalue,
         reg_set_dwordvalue,
