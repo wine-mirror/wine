31792f43701d74589e2f06dc5b6446a84d5d4614
Francois Gouget <fgouget@codeweavers.com>
2023-01-17 19:39:41 +0100

    nsi: Add some margin to the tables size in NsiAllocateAndGetTable().
    The table size typically keeps increasing such that in some cases the
    size we got is already too small by the time we try to get the table
    content in the next iteration.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=54328

--- dlls/nsi/nsi.c
+++ dlls/nsi/nsi.c
@@ -71,6 +71,7 @@ DWORD WINAPI NsiAllocateAndGetTable( DWORD unk, const NPI_MODULEID *module, DWOR
         err = NsiEnumerateObjectsAllParameters( unk, 0, module, table, NULL, 0, NULL, 0, NULL, 0, NULL, 0, &num );
         if (err) return err;
         err = ERROR_OUTOFMEMORY; /* fail if this is the last attempt */
+        num += num >> 4; /* the tables may grow before the next iteration; get ahead */
     }
 
     if (!err)
