cfbe8aed4823275aa3724d3eb4c266ca1f5d0bbc
Jinoh Kang <jinoh.kang.kr@gmail.com>
2023-01-21 11:17:38 +0900

    ntoskrnl.exe/tests: Use loopback address in test_wsk_listen_socket.
    Today, the driver_netio test server binds to the wildcard address
    (0.0.0.0).  This may trigger a firewall alert on Windows 7, and the
    alert UI window may interfere with user32:msg tests.
    
    Fix this by binding to the loopback address (127.0.0.1) instead.
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=53891
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=54202

--- dlls/ntoskrnl.exe/tests/driver_netio.c
+++ dlls/ntoskrnl.exe/tests/driver_netio.c
@@ -235,6 +235,7 @@ static void test_wsk_listen_socket(void)
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
     addr.sin_port = htons(SERVER_LISTEN_PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 
     IoReuseIrp(wsk_irp, STATUS_UNSUCCESSFUL);
     IoSetCompletionRoutine(wsk_irp, irp_completion_routine, &irp_complete_event, TRUE, TRUE, TRUE);
