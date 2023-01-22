1f1f99025d92e8e8b4428f3efc103ba7be9b63b8
Zhiyi Zhang <zzhang@codeweavers.com>
2023-01-22 12:39:53 +0800

    comctl32/listview: Repaint when changing view styles.
    Changing listview view styles should repaint according to the tests in test_WM_STYLECHANGED().
    
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=54215

--- dlls/comctl32/listview.c
+++ dlls/comctl32/listview.c
@@ -11212,6 +11212,7 @@ static INT LISTVIEW_StyleChanged(LISTVIEW_INFO *infoPtr, WPARAM wStyleType,
                                  const STYLESTRUCT *lpss)
 {
     UINT uNewView, uOldView;
+    BOOL repaint = FALSE;
     UINT style;
 
     TRACE("styletype %Ix, styleOld %#lx, styleNew %#lx\n",
@@ -11235,6 +11236,8 @@ static INT LISTVIEW_StyleChanged(LISTVIEW_INFO *infoPtr, WPARAM wStyleType,
     if (uNewView != uOldView)
     {
     	HIMAGELIST himl;
+
+        repaint = TRUE;
 
         /* LVM_SETVIEW doesn't change window style bits within LVS_TYPEMASK,
            changing style updates current view only when view bits change. */
@@ -11296,6 +11299,9 @@ static INT LISTVIEW_StyleChanged(LISTVIEW_INFO *infoPtr, WPARAM wStyleType,
 
     /* add scrollbars if needed */
     LISTVIEW_UpdateScroll(infoPtr);
+
+    if (repaint)
+        LISTVIEW_InvalidateList(infoPtr);
 
     return 0;
 }
