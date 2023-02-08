cd2002fd23f4cf862c7a7835bc19f53338caa77f
Alex Henrie <alexhenrie24@gmail.com>
2023-02-07 22:05:18 -0700

    httpapi: Avoid calling RtlInitUnicodeString on a static constant.

--- dlls/httpapi/httpapi_main.c
+++ dlls/httpapi/httpapi_main.c
@@ -185,7 +185,7 @@ ULONG WINAPI HttpSetServiceConfiguration( HANDLE handle, HTTP_SERVICE_CONFIG_ID 
 ULONG WINAPI HttpCreateHttpHandle(HANDLE *handle, ULONG reserved)
 {
     OBJECT_ATTRIBUTES attr = {sizeof(attr)};
-    UNICODE_STRING string;
+    UNICODE_STRING string = RTL_CONSTANT_STRING(L"\\Device\\Http\\ReqQueue");
     IO_STATUS_BLOCK iosb;
 
     TRACE("handle %p, reserved %#lx.\n", handle, reserved);
@@ -193,7 +193,6 @@ ULONG WINAPI HttpCreateHttpHandle(HANDLE *handle, ULONG reserved)
     if (!handle)
         return ERROR_INVALID_PARAMETER;
 
-    RtlInitUnicodeString(&string, L"\\Device\\Http\\ReqQueue");
     attr.ObjectName = &string;
     return RtlNtStatusToDosError(NtCreateFile(handle, 0, &attr, &iosb, NULL,
             FILE_ATTRIBUTE_NORMAL, 0, FILE_OPEN, FILE_NON_DIRECTORY_FILE, NULL, 0));
@@ -741,7 +740,7 @@ ULONG WINAPI HttpCreateRequestQueue(HTTPAPI_VERSION version, const WCHAR *name,
         SECURITY_ATTRIBUTES *sa, ULONG flags, HANDLE *handle)
 {
     OBJECT_ATTRIBUTES attr = {sizeof(attr)};
-    UNICODE_STRING string;
+    UNICODE_STRING string = RTL_CONSTANT_STRING(L"\\Device\\Http\\ReqQueue");
     IO_STATUS_BLOCK iosb;
 
     TRACE("version %u.%u, name %s, sa %p, flags %#lx, handle %p.\n",
@@ -753,7 +752,6 @@ ULONG WINAPI HttpCreateRequestQueue(HTTPAPI_VERSION version, const WCHAR *name,
     if (flags)
         FIXME("Unhandled flags %#lx.\n", flags);
 
-    RtlInitUnicodeString(&string, L"\\Device\\Http\\ReqQueue");
     attr.ObjectName = &string;
     if (sa && sa->bInheritHandle)
         attr.Attributes |= OBJ_INHERIT;
