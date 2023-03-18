cbc25006388af5c729ff459792630d2598549450
Nikolay Sivov <nsivov@codeweavers.com>
2023-03-18 20:53:50 +0100

    d3d10/effect: Add support for itof instruction.

--- dlls/d3d10/effect.c
+++ dlls/d3d10/effect.c
@@ -252,6 +252,15 @@ static void pres_cos(float **args, unsigned int n, const struct preshader_instr 
         retval[i] = cos(args[0][i]);
 }
 
+static void pres_itof(float **args, unsigned int n, const struct preshader_instr *instr)
+{
+    float *retval = args[1];
+    unsigned int i;
+
+    for (i = 0; i < instr->comp_count; ++i)
+        retval[i] = *(int *)&args[0][i];
+}
+
 static void pres_ftou(float **args, unsigned int n, const struct preshader_instr *instr)
 {
     float *retval = args[1];
@@ -335,6 +344,7 @@ static const struct preshader_op_info preshader_ops[] =
     { 0x104, "frc",  pres_frc  },
     { 0x108, "sin",  pres_sin  },
     { 0x109, "cos",  pres_cos  },
+    { 0x130, "itof", pres_itof },
     { 0x133, "ftou", pres_ftou },
     { 0x137, "ftob", pres_ftob },
     { 0x200, "min",  pres_min  },
