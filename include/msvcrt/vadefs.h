wine-mirror/wine

commit f4c3801495f4fe17a10e2f21339de3b511c78339

include: Move __builtin_ms_va_list definitions to vadefs.h.

include/msvcrt/vadefs.h
@@ -27,7 +27,15 @@
 #define _ADDRESSOF(v) (&(v))
 #endif
 
-#if defined(__GNUC__) || defined(__clang__)
+#if defined (__GNUC__) && (defined(__x86_64__) || (defined(__aarch64__) && __has_attribute(ms_abi)))
+
+typedef __builtin_ms_va_list va_list;
+#define _crt_va_start(v,l)  __builtin_ms_va_start(v,l)
+#define _crt_va_arg(v,l)    __builtin_va_arg(v,l)
+#define _crt_va_end(v)      __builtin_ms_va_end(v)
+#define _crt_va_copy(d,s)   __builtin_ms_va_copy(d,s)
+
+#elif defined(__GNUC__) || defined(__clang__)
 
 typedef __builtin_va_list va_list;
 #define _crt_va_start(v,l)  __builtin_va_start(v,l)

include/msvcrt/corecrt.h
@@ -122,18 +122,6 @@
 # endif
 #endif
 
-#if (defined(__x86_64__) || (defined(__aarch64__) && __has_attribute(ms_abi))) && defined (__GNUC__)
-# include <stdarg.h>
-# undef va_list
-# undef va_start
-# undef va_end
-# undef va_copy
-# define va_list __builtin_ms_va_list
-# define va_start(list,arg) __builtin_ms_va_start(list,arg)
-# define va_end(list) __builtin_ms_va_end(list)
-# define va_copy(dest,src) __builtin_ms_va_copy(dest,src)
-#endif
-
 #ifndef WINAPIV
 # if defined(__arm__) && defined (__GNUC__) && !defined(__SOFTFP__) && !defined(__MINGW32__) && !defined(__CYGWIN__)
 #  define WINAPIV __attribute__((pcs("aapcs")))

commit cf4b4ff642494251ab3d3c7e3c8fb77c5e4b78fd

include: Include corecrt.h in vadefs.h.
For the __has_attribute() fallback.

Fixes: f4c3801495f4fe17a10e2f21339de3b511c78339

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=54666

include/msvcrt/vadefs.h
@@ -21,6 +21,8 @@
 #ifndef _INC_VADEFS
 #define _INC_VADEFS
 
+#include <corecrt.h>
+
 #ifdef __cplusplus
 #define _ADDRESSOF(v) (&reinterpret_cast<const char &>(v))
 #else