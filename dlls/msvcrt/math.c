e5808e203628416bd884720b3c91fc9d73706fe8
Alexandre Julliard <julliard@winehq.org>
2023-04-03 15:30:58 +0200

    msvcrt: Use the modf()/modff() implementation from the bundled musl library.

--- dlls/msvcrt/math.c
+++ dlls/msvcrt/math.c
@@ -2009,44 +2009,6 @@ float CDECL floorf( float x )
     return u.f;
 }
 
-/*********************************************************************
- *      modff (MSVCRT.@)
- *
- * Copied from musl: src/math/modff.c
- */
-float CDECL modff( float x, float *iptr )
-{
-    union {float f; UINT32 i;} u = {x};
-    UINT32 mask;
-    int e = (u.i >> 23 & 0xff) - 0x7f;
-
-    /* no fractional part */
-    if (e >= 23) {
-        *iptr = x;
-        if (e == 0x80 && u.i << 9 != 0) { /* nan */
-            return x;
-        }
-        u.i &= 0x80000000;
-        return u.f;
-    }
-    /* no integral part */
-    if (e < 0) {
-        u.i &= 0x80000000;
-        *iptr = u.f;
-        return x;
-    }
-
-    mask = 0x007fffff >> e;
-    if ((u.i & mask) == 0) {
-        *iptr = x;
-        u.i &= 0x80000000;
-        return u.f;
-    }
-    u.i &= ~mask;
-    *iptr = u.f;
-    return x - u.f;
-}
-
 #endif
 
 /*********************************************************************
@@ -5051,44 +5013,6 @@ float CDECL fmaf( float x, float y, float z )
     else
         u.i--;
     return u.f;
-}
-
-/*********************************************************************
- *		modf (MSVCRT.@)
- *
- * Copied from musl: src/math/modf.c
- */
-double CDECL modf( double x, double *iptr )
-{
-    union {double f; UINT64 i;} u = {x};
-    UINT64 mask;
-    int e = (u.i >> 52 & 0x7ff) - 0x3ff;
-
-    /* no fractional part */
-    if (e >= 52) {
-        *iptr = x;
-        if (e == 0x400 && u.i << 12 != 0) /* nan */
-            return x;
-        u.i &= 1ULL << 63;
-        return u.f;
-    }
-
-    /* no integral part*/
-    if (e < 0) {
-        u.i &= 1ULL << 63;
-        *iptr = u.f;
-        return x;
-    }
-
-    mask = -1ULL >> 12 >> e;
-    if ((u.i & mask) == 0) {
-        *iptr = x;
-        u.i &= 1ULL << 63;
-        return u.f;
-    }
-    u.i &= ~mask;
-    *iptr = u.f;
-    return x - u.f;
 }
 
 #if defined(__i386__) || defined(__x86_64__)
