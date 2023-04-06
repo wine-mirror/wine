ac637b323e4d4e861f30152715a8a9569b1433ba
Zhiyi Zhang <zzhang@codeweavers.com>
2023-04-06 18:15:58 +0800

    wined3d: Add GPU information for AMD Raphael.
    (cherry picked from commit 9909631f23c037e90fe3ef1ccfeebfff2804d7ed)

--- dlls/wined3d/directx.c
+++ dlls/wined3d/directx.c
@@ -540,6 +540,7 @@ static const struct wined3d_gpu_description gpu_description_table[] =
     {HW_VENDOR_AMD,        CARD_AMD_RADEON_PRO_V620,       "Radeon Pro V620",                  DRIVER_AMD_RX,          32768},
     {HW_VENDOR_AMD,        CARD_AMD_RADEON_PRO_V620_VF,    "Radeon Pro V620 VF",               DRIVER_AMD_RX,          32768},
     {HW_VENDOR_AMD,        CARD_AMD_VANGOGH,               "AMD VANGOGH",                      DRIVER_AMD_RX,           4096},
+    {HW_VENDOR_AMD,        CARD_AMD_RAPHAEL,               "AMD Radeon(TM) Graphics",          DRIVER_AMD_RX,           4096},
 
     /* Red Hat */
     {HW_VENDOR_REDHAT,     CARD_REDHAT_VIRGL,              "Red Hat VirtIO GPU",                                        DRIVER_REDHAT_VIRGL,  1024},
