3099e6afc5d69cf842c72e07fd6bb8ee789e8741
Piotr Caban <piotr@codeweavers.com>
2023-04-13 20:26:45 +0200

    wineps: Handle EMR_SETTEXTJUSTIFICATION record in spool files.

--- dlls/wineps.drv/printproc.c
+++ dlls/wineps.drv/printproc.c
@@ -1614,6 +1614,7 @@ static int WINAPI hmf_proc(HDC hdc, HANDLETABLE *htable,
     case EMR_SELECTCLIPPATH:
     case EMR_EXTSELECTCLIPRGN:
     case EMR_SETLAYOUT:
+    case EMR_SETTEXTJUSTIFICATION:
         return PlayEnhMetaFileRecord(data->pdev->dev.hdc, htable, rec, n);
     default:
         FIXME("unsupported record: %ld\n", rec->iType);
