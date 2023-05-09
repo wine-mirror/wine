17a0e19489e14107f8ed7bcad9a07cb75e566ead
Alexandre Julliard <julliard@winehq.org>
2023-05-09 14:25:58 +0200

    server: Make x86_64 a supported architecture on ARM64.

--- server/registry.c
+++ server/registry.c
@@ -1857,6 +1857,7 @@ static void init_supported_machines(void)
     if (prefix_type == PREFIX_64BIT)
     {
         supported_machines[count++] = IMAGE_FILE_MACHINE_ARM64;
+        supported_machines[count++] = IMAGE_FILE_MACHINE_AMD64;
         supported_machines[count++] = IMAGE_FILE_MACHINE_I386;
     }
     supported_machines[count++] = IMAGE_FILE_MACHINE_ARMNT;
