5c939222d339f02264f92183a973d4471c495425
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2023-05-22 19:03:22 +0300

    mshtml: Release ref from the element when detaching the plugin host.
    When the plugin host is created, its only ref is being held by the element
    it is associated with.
    
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dlls/mshtml/pluginhost.c
+++ dlls/mshtml/pluginhost.c
@@ -2672,14 +2672,15 @@ void detach_plugin_host(PluginHost *host)
 
     release_plugin_ifaces(host);
 
-    if(host->element) {
-        host->element->plugin_host = NULL;
-        host->element = NULL;
-    }
-
     list_remove(&host->entry);
     list_init(&host->entry);
     host->doc = NULL;
+
+    if(host->element) {
+        host->element->plugin_host = NULL;
+        host->element = NULL;
+        IOleClientSite_Release(&host->IOleClientSite_iface);
+    }
 }
 
 HRESULT create_plugin_host(HTMLDocumentNode *doc, HTMLPluginContainer *container)
