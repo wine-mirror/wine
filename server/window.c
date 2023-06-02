wine-mirror/wine

commit 33617af8145b6fadfe41d48968a728ff57b3064b

server: Don't invalidate the newly exposed child region on the parent if it doesn't clip children.
This partly reverts e42eaaaf3a873d29b14f8f3a3617df2d0d4210ca.

Signed-off-by: Jinoh Kang <jinoh.kang.kr@gmail.com>

server/window.c
@@ -1766,10 +1766,8 @@ static struct region *expose_window( struct window *win, const rectangle_t *old_
         offset_region( new_vis_rgn, win->window_rect.left - old_window_rect->left,
                        win->window_rect.top - old_window_rect->top  );
 
-        if (is_composited ? union_region( new_vis_rgn, old_vis_rgn, new_vis_rgn ) :
-            (win->parent->style & WS_CLIPCHILDREN) ?
-            subtract_region( new_vis_rgn, old_vis_rgn, new_vis_rgn ) :
-            xor_region( new_vis_rgn, old_vis_rgn, new_vis_rgn ))
+        if (is_composited ? union_region( new_vis_rgn, old_vis_rgn, new_vis_rgn )
+                          : subtract_region( new_vis_rgn, old_vis_rgn, new_vis_rgn ))
         {
             if (!is_region_empty( new_vis_rgn ))
             {

dlls/user32/tests/msg.c
@@ -9302,8 +9302,6 @@ static void subtest_swp_paint_regions_( int line, int wrap_toplevel, LPCSTR pare
             }
             else
             {
-                todo_wine_if( !EqualRect( &rect_old, &rect_new ) &&
-                              (extest->style & WS_CLIPCHILDREN) == 0 && !is_composited )
                 ok( !!rgn_ok, "Parent update region shall match expected region\n" );
             }
 
@@ -9365,8 +9363,6 @@ static void subtest_swp_paint_regions_( int line, int wrap_toplevel, LPCSTR pare
             }
             else
             {
-                todo_wine_if( !EqualRect( &rect_old, &rect_new ) &&
-                              (extest->style & WS_CLIPCHILDREN) == 0 && !is_composited )
                 ok( !!rgn_ok, "Child update region shall match expected region\n" );
             }
 

commit 078b0219aa4a3ead156e94b5d31202d58b0bb504

server: Update parent window region when exposing previously invisible window.
Fixes: 33617af8145b6fadfe41d48968a728ff57b3064b
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=54949

server/window.c
@@ -1766,8 +1766,9 @@ static struct region *expose_window( struct window *win, const rectangle_t *old_
         offset_region( new_vis_rgn, win->window_rect.left - old_window_rect->left,
                        win->window_rect.top - old_window_rect->top  );
 
-        if (is_composited ? union_region( new_vis_rgn, old_vis_rgn, new_vis_rgn )
-                          : subtract_region( new_vis_rgn, old_vis_rgn, new_vis_rgn ))
+        if (is_region_empty( old_vis_rgn ) ||
+            (is_composited ? union_region( new_vis_rgn, old_vis_rgn, new_vis_rgn )
+                           : subtract_region( new_vis_rgn, old_vis_rgn, new_vis_rgn )))
         {
             if (!is_region_empty( new_vis_rgn ))
             {

dlls/user32/tests/msg.c
@@ -5793,7 +5793,7 @@ static void test_messages(void)
     test_WM_SETREDRAW(hchild);
 
     ShowWindow(hchild, SW_SHOW);
-    ok_sequence(WmShowChildSeq, "ShowWindow(SW_SHOW):child", TRUE);
+    ok_sequence(WmShowChildSeq, "ShowWindow(SW_SHOW):child", FALSE);
 
     /* check parent messages too */
     log_all_parent_messages++;
@@ -9467,12 +9467,10 @@ static void test_swp_paint_region_on_show(void)
         "GetUpdateRgn on child shall succeed\n" );
 
     result = GetRgnBox( hrgn_actual, &rect_actual );
-    todo_wine_if (result == NULLREGION)
     ok( result == SIMPLEREGION, "GetRgnBox (on parent) returned %d\n", result );
     if (result == COMPLEXREGION) dump_region( hrgn_actual );
 
     rect_expect = rect_1;
-    todo_wine_if (IsRectEmpty( &rect_actual ))
     ok( EqualRect( &rect_actual, &rect_expect ), "parent update region: got %s, expected %s\n",
         wine_dbgstr_rect( &rect_actual ), wine_dbgstr_rect( &rect_expect ) );
 
@@ -9506,12 +9504,10 @@ static void test_swp_paint_region_on_show(void)
         "GetUpdateRgn on child shall succeed\n" );
 
     result = GetRgnBox( hrgn_actual, &rect_actual );
-    todo_wine_if (result == NULLREGION)
     ok( result == SIMPLEREGION, "GetRgnBox (on parent) returned %d\n", result );
     if (result == COMPLEXREGION) dump_region( hrgn_actual );
 
     rect_expect = rect_2;
-    todo_wine_if (IsRectEmpty( &rect_actual ))
     ok( EqualRect( &rect_actual, &rect_expect ), "parent update region: got %s, expected %s\n",
         wine_dbgstr_rect( &rect_actual ), wine_dbgstr_rect( &rect_expect ) );
 
@@ -9565,12 +9561,10 @@ static void test_swp_paint_region_on_extend_zerosize(void)
         "GetUpdateRgn on child shall succeed\n" );
 
     result = GetRgnBox( hrgn_actual, &rect_actual );
-    todo_wine_if (result == NULLREGION)
     ok( result == SIMPLEREGION, "GetRgnBox (on parent) returned %d\n", result );
     if (result == COMPLEXREGION) dump_region( hrgn_actual );
 
     rect_expect = rect_1;
-    todo_wine_if (IsRectEmpty( &rect_actual ))
     ok( EqualRect( &rect_actual, &rect_expect ), "parent update region: got %s, expected %s\n",
         wine_dbgstr_rect( &rect_actual ), wine_dbgstr_rect( &rect_expect ) );
 