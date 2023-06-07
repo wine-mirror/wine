1dd6ea7049f857f0d4e7a78ec8314c3bac71625b
Alexandre Julliard <julliard@winehq.org>
2023-06-07 16:12:02 +0200

    wininet/tests: Update expected WineHQ certificate.

--- dlls/wininet/tests/http.c
+++ dlls/wininet/tests/http.c
@@ -6666,9 +6666,7 @@ typedef struct {
 static const cert_struct_test_t test_winehq_org_cert = {
     "US\r\n"
     "Minnesota\r\n"
-    "Saint Paul\r\n"
-    "\"CodeWeavers, Inc.\"\r\n"
-    "IT\r\n"
+    "CodeWeavers\r\n"
     "*.winehq.org",
 
     "US\r\n"
