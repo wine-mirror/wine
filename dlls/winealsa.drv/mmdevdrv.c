fbb03f929bf07ca1373fa17a3f487e8e1b3e2ffd
Davide Beatrici <git@davidebeatrici.dev>
2023-06-30 22:52:23 +0200

    winealsa: Export get_device_name_from_guid().

--- dlls/winealsa.drv/mmdevdrv.c
+++ dlls/winealsa.drv/mmdevdrv.c
@@ -247,7 +247,7 @@ end:
     return params.result;
 }
 
-static BOOL get_device_name_from_guid(GUID *guid, char **name, EDataFlow *flow)
+BOOL WINAPI get_device_name_from_guid(GUID *guid, char **name, EDataFlow *flow)
 {
     HKEY devices_key;
     UINT i = 0;
