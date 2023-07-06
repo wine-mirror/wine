23a4ff05cc206a5f02050bdbce02d2295c4fb792
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2023-07-06 20:37:22 +1000

    shdocvw: Remove DECLSPEC_HIDDEN usage.

--- dlls/shdocvw/shdocvw.h
+++ dlls/shdocvw/shdocvw.h
@@ -41,12 +41,12 @@
  * Shell Instance Objects
  */
 extern HRESULT SHDOCVW_GetShellInstanceObjectClassObject(REFCLSID rclsid, 
-    REFIID riid, LPVOID *ppvClassObj) DECLSPEC_HIDDEN;
+    REFIID riid, LPVOID *ppvClassObj);
 
 /**********************************************************************
  * Dll lifetime tracking declaration for shdocvw.dll
  */
-extern LONG SHDOCVW_refCount DECLSPEC_HIDDEN;
+extern LONG SHDOCVW_refCount;
 static inline void SHDOCVW_LockModule(void) { InterlockedIncrement( &SHDOCVW_refCount ); }
 static inline void SHDOCVW_UnlockModule(void) { InterlockedDecrement( &SHDOCVW_refCount ); }
 
