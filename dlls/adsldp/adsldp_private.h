ba0841defa9ff23988569cc68fd921340026d47e
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2023-07-10 18:59:28 +1000

    adsldp: Remove DECLSPEC_HIDDEN usage.

--- dlls/adsldp/adsldp_private.h
+++ dlls/adsldp/adsldp_private.h
@@ -47,9 +47,9 @@ struct attribute_type
     int single_value;
 };
 
-DWORD map_ldap_error(DWORD) DECLSPEC_HIDDEN;
-struct attribute_type *load_schema(LDAP *ld, ULONG *, ULONG *) DECLSPEC_HIDDEN;
-ADSTYPEENUM get_schema_type(const WCHAR *, const struct attribute_type *, ULONG, ULONG) DECLSPEC_HIDDEN;
-void free_attribute_types(struct attribute_type *, ULONG) DECLSPEC_HIDDEN;
+DWORD map_ldap_error(DWORD);
+struct attribute_type *load_schema(LDAP *ld, ULONG *, ULONG *);
+ADSTYPEENUM get_schema_type(const WCHAR *, const struct attribute_type *, ULONG, ULONG);
+void free_attribute_types(struct attribute_type *, ULONG);
 
 #endif
