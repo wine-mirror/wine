f65705df9d19efdfdd3e4ae3edca961470dcc1b6
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2023-07-12 17:41:25 +1000

    d3dx10_43: Remove DECLSPEC_HIDDEN usage.

--- dlls/d3dx10_43/dxhelpers.h
+++ dlls/d3dx10_43/dxhelpers.h
@@ -16,18 +16,18 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-extern HRESULT load_file(const WCHAR *path, void **data, DWORD *size) DECLSPEC_HIDDEN;
+extern HRESULT load_file(const WCHAR *path, void **data, DWORD *size);
 extern HRESULT load_resourceA(HMODULE module, const char *resource,
-        void **data, DWORD *size) DECLSPEC_HIDDEN;
+        void **data, DWORD *size);
 extern HRESULT load_resourceW(HMODULE module, const WCHAR *resource,
-        void **data, DWORD *size) DECLSPEC_HIDDEN;
+        void **data, DWORD *size);
 
-extern HRESULT get_image_info(const void *data, SIZE_T size, D3DX10_IMAGE_INFO *img_info) DECLSPEC_HIDDEN;
+extern HRESULT get_image_info(const void *data, SIZE_T size, D3DX10_IMAGE_INFO *img_info);
 
 extern void init_load_info(const D3DX10_IMAGE_LOAD_INFO *load_info,
-        D3DX10_IMAGE_LOAD_INFO *out) DECLSPEC_HIDDEN;
+        D3DX10_IMAGE_LOAD_INFO *out);
 /* Returns array of D3D10_SUBRESOURCE_DATA structures followed by textures data. */
 extern HRESULT load_texture_data(const void *data, SIZE_T size, D3DX10_IMAGE_LOAD_INFO *load_info,
-        D3D10_SUBRESOURCE_DATA **resource_data) DECLSPEC_HIDDEN;
+        D3D10_SUBRESOURCE_DATA **resource_data);
 extern HRESULT create_d3d_texture(ID3D10Device *device, D3DX10_IMAGE_LOAD_INFO *load_info,
-        D3D10_SUBRESOURCE_DATA *resource_data, ID3D10Resource **texture) DECLSPEC_HIDDEN;
+        D3D10_SUBRESOURCE_DATA *resource_data, ID3D10Resource **texture);
