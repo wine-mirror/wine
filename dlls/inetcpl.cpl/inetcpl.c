ed412aeb81128187d08506d31378164680268013
Alexandre Julliard <julliard@winehq.org>
2023-07-13 11:58:05 +0200

    inetcpl.cpl: Use nameless unions/structs.

--- dlls/inetcpl.cpl/inetcpl.c
+++ dlls/inetcpl.cpl/inetcpl.c
@@ -19,7 +19,6 @@
  *
  */
 
-#define NONAMELESSUNION
 #define COBJMACROS
 #define CONST_VTABLE
 
@@ -108,25 +107,25 @@ static void display_cpl_sheets(HWND parent)
     /* Fill out all PROPSHEETPAGE */
     psp[id].dwSize = sizeof (PROPSHEETPAGEW);
     psp[id].hInstance = hcpl;
-    psp[id].u.pszTemplate = MAKEINTRESOURCEW(IDD_GENERAL);
+    psp[id].pszTemplate = MAKEINTRESOURCEW(IDD_GENERAL);
     psp[id].pfnDlgProc = general_dlgproc;
     id++;
 
     psp[id].dwSize = sizeof (PROPSHEETPAGEW);
     psp[id].hInstance = hcpl;
-    psp[id].u.pszTemplate = MAKEINTRESOURCEW(IDD_SECURITY);
+    psp[id].pszTemplate = MAKEINTRESOURCEW(IDD_SECURITY);
     psp[id].pfnDlgProc = security_dlgproc;
     id++;
 
     psp[id].dwSize = sizeof (PROPSHEETPAGEW);
     psp[id].hInstance = hcpl;
-    psp[id].u.pszTemplate = MAKEINTRESOURCEW(IDD_CONTENT);
+    psp[id].pszTemplate = MAKEINTRESOURCEW(IDD_CONTENT);
     psp[id].pfnDlgProc = content_dlgproc;
     id++;
 
     psp[id].dwSize = sizeof (PROPSHEETPAGEW);
     psp[id].hInstance = hcpl;
-    psp[id].u.pszTemplate = MAKEINTRESOURCEW(IDD_CONNECTIONS);
+    psp[id].pszTemplate = MAKEINTRESOURCEW(IDD_CONNECTIONS);
     psp[id].pfnDlgProc = connections_dlgproc;
     id++;
 
@@ -135,10 +134,10 @@ static void display_cpl_sheets(HWND parent)
     psh.dwFlags = PSH_PROPSHEETPAGE | PSH_USEICONID | PSH_USECALLBACK;
     psh.hwndParent = parent;
     psh.hInstance = hcpl;
-    psh.u.pszIcon = MAKEINTRESOURCEW(ICO_MAIN);
+    psh.pszIcon = MAKEINTRESOURCEW(ICO_MAIN);
     psh.pszCaption = MAKEINTRESOURCEW(IDS_CPL_NAME);
     psh.nPages = id;
-    psh.u3.ppsp = psp;
+    psh.ppsp = psp;
     psh.pfnCallback = propsheet_callback;
 
     /* display the dialog */
