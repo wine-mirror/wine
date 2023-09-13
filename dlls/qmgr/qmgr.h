47526fbf1342b69eb5344a292f3fc8d441b324e1
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2023-09-13 18:04:17 +1000

    qmgr: Remove DECLSPEC_HIDDEN usage.

--- dlls/qmgr/qmgr.h
+++ dlls/qmgr/qmgr.h
@@ -96,23 +96,23 @@ typedef struct
     IClassFactory IClassFactory_iface;
 } ClassFactoryImpl;
 
-extern HANDLE stop_event DECLSPEC_HIDDEN;
-extern ClassFactoryImpl BITS_ClassFactory DECLSPEC_HIDDEN;
-extern BackgroundCopyManagerImpl globalMgr DECLSPEC_HIDDEN;
+extern HANDLE stop_event;
+extern ClassFactoryImpl BITS_ClassFactory;
+extern BackgroundCopyManagerImpl globalMgr;
 
-HRESULT BackgroundCopyManagerConstructor(LPVOID *ppObj) DECLSPEC_HIDDEN;
+HRESULT BackgroundCopyManagerConstructor(LPVOID *ppObj);
 HRESULT BackgroundCopyJobConstructor(LPCWSTR displayName, BG_JOB_TYPE type,
-                                     GUID *pJobId, BackgroundCopyJobImpl **job) DECLSPEC_HIDDEN;
+                                     GUID *pJobId, BackgroundCopyJobImpl **job);
 HRESULT enum_copy_job_create(BackgroundCopyManagerImpl *qmgr,
-        IEnumBackgroundCopyJobs **enumjob) DECLSPEC_HIDDEN;
+        IEnumBackgroundCopyJobs **enumjob);
 HRESULT BackgroundCopyFileConstructor(BackgroundCopyJobImpl *owner,
                                       LPCWSTR remoteName, LPCWSTR localName,
-                                      BackgroundCopyFileImpl **file) DECLSPEC_HIDDEN;
-HRESULT EnumBackgroundCopyFilesConstructor(BackgroundCopyJobImpl*, IEnumBackgroundCopyFiles**) DECLSPEC_HIDDEN;
-DWORD WINAPI fileTransfer(void *param) DECLSPEC_HIDDEN;
-void processJob(BackgroundCopyJobImpl *job) DECLSPEC_HIDDEN;
-BOOL processFile(BackgroundCopyFileImpl *file, BackgroundCopyJobImpl *job) DECLSPEC_HIDDEN;
-BOOL transitionJobState(BackgroundCopyJobImpl *job, BG_JOB_STATE from, BG_JOB_STATE to) DECLSPEC_HIDDEN;
+                                      BackgroundCopyFileImpl **file);
+HRESULT EnumBackgroundCopyFilesConstructor(BackgroundCopyJobImpl*, IEnumBackgroundCopyFiles**);
+DWORD WINAPI fileTransfer(void *param);
+void processJob(BackgroundCopyJobImpl *job);
+BOOL processFile(BackgroundCopyFileImpl *file, BackgroundCopyJobImpl *job);
+BOOL transitionJobState(BackgroundCopyJobImpl *job, BG_JOB_STATE from, BG_JOB_STATE to);
 
 /* Little helper functions */
 static inline WCHAR *co_strdupW(const WCHAR *src)
