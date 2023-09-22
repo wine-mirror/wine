94386b4fbafcaf7b0dd1871a20c447d17c53d5c0
Rémi Bernon <rbernon@codeweavers.com>
2023-09-22 11:28:43 +0200

    dmime: Get rid of the IDirectMusicSegmentState8Impl typedef.

--- dlls/dmime/segmentstate.c
+++ dlls/dmime/segmentstate.c
@@ -1,5 +1,4 @@
-/* IDirectMusicSegmentState8 Implementation
- *
+/*
  * Copyright (C) 2003-2004 Rok Mandeljc
  *
  * This program is free software; you can redistribute it and/or
@@ -22,19 +21,20 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(dmime);
 
-typedef struct IDirectMusicSegmentState8Impl {
+struct segment_state
+{
     IDirectMusicSegmentState8 IDirectMusicSegmentState8_iface;
     LONG ref;
-} IDirectMusicSegmentState8Impl;
+};
 
-static inline IDirectMusicSegmentState8Impl *impl_from_IDirectMusicSegmentState8(IDirectMusicSegmentState8 *iface)
+static inline struct segment_state *impl_from_IDirectMusicSegmentState8(IDirectMusicSegmentState8 *iface)
 {
-    return CONTAINING_RECORD(iface, IDirectMusicSegmentState8Impl, IDirectMusicSegmentState8_iface);
+    return CONTAINING_RECORD(iface, struct segment_state, IDirectMusicSegmentState8_iface);
 }
 
 static HRESULT WINAPI segment_state_QueryInterface(IDirectMusicSegmentState8 *iface, REFIID riid, void **ppobj)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
 
     TRACE("(%p, %s, %p)\n", This, debugstr_dmguid(riid), ppobj);
 
@@ -58,7 +58,7 @@ static HRESULT WINAPI segment_state_QueryInterface(IDirectMusicSegmentState8 *if
 
 static ULONG WINAPI segment_state_AddRef(IDirectMusicSegmentState8 *iface)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     ULONG ref = InterlockedIncrement(&This->ref);
 
     TRACE("(%p): %ld\n", This, ref);
@@ -68,7 +68,7 @@ static ULONG WINAPI segment_state_AddRef(IDirectMusicSegmentState8 *iface)
 
 static ULONG WINAPI segment_state_Release(IDirectMusicSegmentState8 *iface)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     ULONG ref = InterlockedDecrement(&This->ref);
 
     TRACE("(%p): %ld\n", This, ref);
@@ -80,35 +80,35 @@ static ULONG WINAPI segment_state_Release(IDirectMusicSegmentState8 *iface)
 
 static HRESULT WINAPI segment_state_GetRepeats(IDirectMusicSegmentState8 *iface, DWORD *pdwRepeats)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %p): stub\n", This, pdwRepeats);
     return S_OK;
 }
 
 static HRESULT WINAPI segment_state_GetSegment(IDirectMusicSegmentState8 *iface, IDirectMusicSegment **ppSegment)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %p): stub\n", This, ppSegment);
     return S_OK;
 }
 
 static HRESULT WINAPI segment_state_GetStartTime(IDirectMusicSegmentState8 *iface, MUSIC_TIME *pmtStart)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %p): stub\n", This, pmtStart);
     return S_OK;
 }
 
 static HRESULT WINAPI segment_state_GetSeek(IDirectMusicSegmentState8 *iface, MUSIC_TIME *pmtSeek)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %p): stub\n", This, pmtSeek);
     return S_OK;
 }
 
 static HRESULT WINAPI segment_state_GetStartPoint(IDirectMusicSegmentState8 *iface, MUSIC_TIME *pmtStart)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %p): stub\n", This, pmtStart);
     return S_OK;
 }
@@ -116,7 +116,7 @@ static HRESULT WINAPI segment_state_GetStartPoint(IDirectMusicSegmentState8 *ifa
 static HRESULT WINAPI segment_state_SetTrackConfig(IDirectMusicSegmentState8 *iface,
         REFGUID rguidTrackClassID, DWORD dwGroupBits, DWORD dwIndex, DWORD dwFlagsOn, DWORD dwFlagsOff)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %s, %ld, %ld, %ld, %ld): stub\n", This, debugstr_dmguid(rguidTrackClassID), dwGroupBits, dwIndex, dwFlagsOn, dwFlagsOff);
     return S_OK;
 }
@@ -124,7 +124,7 @@ static HRESULT WINAPI segment_state_SetTrackConfig(IDirectMusicSegmentState8 *if
 static HRESULT WINAPI segment_state_GetObjectInPath(IDirectMusicSegmentState8 *iface, DWORD dwPChannel,
         DWORD dwStage, DWORD dwBuffer, REFGUID guidObject, DWORD dwIndex, REFGUID iidInterface, void **ppObject)
 {
-    IDirectMusicSegmentState8Impl *This = impl_from_IDirectMusicSegmentState8(iface);
+    struct segment_state *This = impl_from_IDirectMusicSegmentState8(iface);
     FIXME("(%p, %ld, %ld, %ld, %s, %ld, %s, %p): stub\n", This, dwPChannel, dwStage, dwBuffer, debugstr_dmguid(guidObject), dwIndex, debugstr_dmguid(iidInterface), ppObject);
     return S_OK;
 }
@@ -146,7 +146,7 @@ static const IDirectMusicSegmentState8Vtbl segment_state_vtbl =
 /* for ClassFactory */
 HRESULT create_dmsegmentstate(REFIID riid, void **ret_iface)
 {
-    IDirectMusicSegmentState8Impl* obj;
+    struct segment_state *obj;
     HRESULT hr;
 
     *ret_iface = NULL;
