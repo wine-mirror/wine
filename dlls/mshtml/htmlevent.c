4d569803963ef44802865f6aad24d53e15e6d1b8
Gabriel Ivăncescu <gabrielopcode@gmail.com>
2023-10-02 21:39:22 +0300

    mshtml: Call handle_event on the proper dispex object.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=55701
    Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

--- dlls/mshtml/htmlevent.c
+++ dlls/mshtml/htmlevent.c
@@ -3889,7 +3889,7 @@ static HRESULT dispatch_event_object(EventTarget *event_target, DOMEvent *event,
             vtbl = dispex_get_vtbl(&target_chain[i]->dispex);
             if(!vtbl->handle_event)
                 continue;
-            hres = vtbl->handle_event(&event_target->dispex, event->event_id, event->nsevent, &prevent_default);
+            hres = vtbl->handle_event(&target_chain[i]->dispex, event->event_id, event->nsevent, &prevent_default);
             if(FAILED(hres) || event->stop_propagation)
                 break;
             if(prevent_default)
