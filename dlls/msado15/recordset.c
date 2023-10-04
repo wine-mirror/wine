8a4e235efca17dbd46ba382e1787203e8247b2d4
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2023-10-04 16:55:11 +1100

    msado15: Move to newly created row in Recordset::AddNew.
    It's valid to have a recordset with existing rows.  AddNew needs to
    move to newly created row, so existing won't be used.

--- dlls/msado15/recordset.c
+++ dlls/msado15/recordset.c
@@ -1587,7 +1587,7 @@ static HRESULT WINAPI recordset_AddNew( _Recordset *iface, VARIANT field_list, V
     if (recordset->state == adStateClosed) return MAKE_ADO_HRESULT( adErrObjectClosed );
 
     if (!resize_recordset( recordset, recordset->count + 1 )) return E_OUTOFMEMORY;
-    recordset->index++;
+    recordset->index = recordset->count - 1;
     recordset->editmode = adEditAdd;
     return S_OK;
 }
