wine-mirror/wine

commit c065b4fe0b3622e0c737a4c7c1c514273ad2d8a7

wined3d: Store push constants in wined3d_buffers in struct wined3d_state.
We will need them in buffers for Vulkan, as well as for software
vertex shaders. Currently we simply store them in sysmem buffers and
read directly back out of them to load GL uniforms.

This does decrease memory usage a bit for d3d10+.

dlls/wined3d/context_gl.c
@@ -4838,7 +4838,7 @@ static void draw_primitive_immediate_mode(struct wined3d_context_gl *context_gl,
     /* Immediate mode drawing can't make use of indices in a VBO - get the
      * data from the index buffer. */
     if (idx_size)
-        idx_data = wined3d_buffer_load_sysmem(state->index_buffer, &context_gl->c) + state->index_offset;
+        idx_data = (uint8_t *)wined3d_buffer_load_sysmem(state->index_buffer, &context_gl->c) + state->index_offset;
 
     ops = &d3d_info->ffp_attrib_ops;
 

dlls/wined3d/arb_program_shader.c
@@ -520,10 +520,14 @@ static void shader_arb_load_np2fixup_constants(const struct arb_ps_np2fixup_info
 
 /* Context activation is done by the caller. */
 static void shader_arb_ps_local_constants(const struct arb_ps_compiled_shader *gl_shader,
-        const struct wined3d_context_gl *context_gl, const struct wined3d_state *state, unsigned int rt_height)
+        struct wined3d_context_gl *context_gl, const struct wined3d_state *state, unsigned int rt_height)
 {
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
+    struct wined3d_device *device = context_gl->c.device;
+    const struct wined3d_ivec4 *int_consts;
     unsigned char i;
+
+    int_consts = wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_PS_I], &context_gl->c);
 
     for(i = 0; i < gl_shader->numbumpenvmatconsts; i++)
     {
@@ -571,9 +575,9 @@ static void shader_arb_ps_local_constants(const struct arb_ps_compiled_shader *g
         if(gl_shader->int_consts[i] != WINED3D_CONST_NUM_UNUSED)
         {
             float val[4];
-            val[0] = (float)state->ps_consts_i[i].x;
-            val[1] = (float)state->ps_consts_i[i].y;
-            val[2] = (float)state->ps_consts_i[i].z;
+            val[0] = (float)int_consts[i].x;
+            val[1] = (float)int_consts[i].y;
+            val[2] = (float)int_consts[i].z;
             val[3] = -1.0f;
 
             GL_EXTCALL(glProgramLocalParameter4fvARB(GL_FRAGMENT_PROGRAM_ARB, gl_shader->int_consts[i], val));
@@ -584,9 +588,11 @@ static void shader_arb_ps_local_constants(const struct arb_ps_compiled_shader *g
 
 /* Context activation is done by the caller. */
 static void shader_arb_vs_local_constants(const struct arb_vs_compiled_shader *gl_shader,
-        const struct wined3d_context_gl *context_gl, const struct wined3d_state *state)
+        struct wined3d_context_gl *context_gl, const struct wined3d_state *state)
 {
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
+    struct wined3d_device *device = context_gl->c.device;
+    const struct wined3d_ivec4 *int_consts;
     float position_fixup[4];
     unsigned char i;
 
@@ -596,14 +602,16 @@ static void shader_arb_vs_local_constants(const struct arb_vs_compiled_shader *g
 
     if (!gl_shader->num_int_consts) return;
 
+    int_consts = wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_VS_I], &context_gl->c);
+
     for (i = 0; i < WINED3D_MAX_CONSTS_I; ++i)
     {
         if(gl_shader->int_consts[i] != WINED3D_CONST_NUM_UNUSED)
         {
             float val[4];
-            val[0] = (float)state->vs_consts_i[i].x;
-            val[1] = (float)state->vs_consts_i[i].y;
-            val[2] = (float)state->vs_consts_i[i].z;
+            val[0] = (float)int_consts[i].x;
+            val[1] = (float)int_consts[i].y;
+            val[2] = (float)int_consts[i].z;
             val[3] = -1.0f;
 
             GL_EXTCALL(glProgramLocalParameter4fvARB(GL_VERTEX_PROGRAM_ARB, gl_shader->int_consts[i], val));
@@ -627,6 +635,7 @@ static void shader_arb_load_constants_internal(struct shader_arb_priv *priv, str
 {
     const struct wined3d_d3d_info *d3d_info = context_gl->c.d3d_info;
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
+    struct wined3d_device *device = context_gl->c.device;
 
     if (!from_shader_select)
     {
@@ -670,8 +679,10 @@ static void shader_arb_load_constants_internal(struct shader_arb_priv *priv, str
         const struct arb_vs_compiled_shader *gl_shader = priv->compiled_vprog;
 
         /* Load DirectX 9 float constants for vertex shader */
-        priv->highest_dirty_vs_const = shader_arb_load_constants_f(vshader, gl_info, GL_VERTEX_PROGRAM_ARB,
-                priv->highest_dirty_vs_const, state->vs_consts_f, priv->vshader_const_dirty);
+        priv->highest_dirty_vs_const = shader_arb_load_constants_f(vshader,
+                gl_info, GL_VERTEX_PROGRAM_ARB, priv->highest_dirty_vs_const,
+                wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_VS_F], &context_gl->c),
+                priv->vshader_const_dirty);
         shader_arb_vs_local_constants(gl_shader, context_gl, state);
     }
 
@@ -682,8 +693,10 @@ static void shader_arb_load_constants_internal(struct shader_arb_priv *priv, str
         UINT rt_height = state->fb.render_targets[0]->height;
 
         /* Load DirectX 9 float constants for pixel shader */
-        priv->highest_dirty_ps_const = shader_arb_load_constants_f(pshader, gl_info, GL_FRAGMENT_PROGRAM_ARB,
-                priv->highest_dirty_ps_const, state->ps_consts_f, priv->pshader_const_dirty);
+        priv->highest_dirty_ps_const = shader_arb_load_constants_f(pshader,
+                gl_info, GL_FRAGMENT_PROGRAM_ARB, priv->highest_dirty_ps_const,
+                wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_PS_F], &context_gl->c),
+                priv->pshader_const_dirty);
         shader_arb_ps_local_constants(gl_shader, context_gl, state, rt_height);
 
         if (context_gl->c.constant_update_mask & WINED3D_SHADER_CONST_PS_NP2_FIXUP)
@@ -4406,13 +4419,19 @@ static struct arb_vs_compiled_shader *find_arb_vshader(struct wined3d_shader *sh
 }
 
 static void find_arb_ps_compile_args(const struct wined3d_state *state,
-        const struct wined3d_context_gl *context_gl, const struct wined3d_shader *shader,
+        struct wined3d_context_gl *context_gl, const struct wined3d_shader *shader,
         struct arb_ps_compile_args *args)
 {
     const struct wined3d_d3d_info *d3d_info = context_gl->c.d3d_info;
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
+    struct wined3d_device *device = context_gl->c.device;
+    const struct wined3d_ivec4 *int_consts;
+    const BOOL *bool_consts;
     int i;
     WORD int_skip;
+
+    bool_consts = wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_PS_B], &context_gl->c);
+    int_consts = wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_PS_I], &context_gl->c);
 
     find_ps_compile_args(state, shader, context_gl->c.stream_info.position_transformed, &args->super, &context_gl->c);
 
@@ -4421,7 +4440,7 @@ static void find_arb_ps_compile_args(const struct wined3d_state *state,
 
     for (i = 0; i < WINED3D_MAX_CONSTS_B; ++i)
     {
-        if (state->ps_consts_b[i])
+        if (bool_consts[i])
             args->bools |= ( 1u << i);
     }
 
@@ -4454,23 +4473,28 @@ static void find_arb_ps_compile_args(const struct wined3d_state *state,
         }
         else
         {
-            args->loop_ctrl[i][0] = state->ps_consts_i[i].x;
-            args->loop_ctrl[i][1] = state->ps_consts_i[i].y;
-            args->loop_ctrl[i][2] = state->ps_consts_i[i].z;
+            args->loop_ctrl[i][0] = int_consts[i].x;
+            args->loop_ctrl[i][1] = int_consts[i].y;
+            args->loop_ctrl[i][2] = int_consts[i].z;
         }
     }
 }
 
 static void find_arb_vs_compile_args(const struct wined3d_state *state,
-        const struct wined3d_context_gl *context_gl, const struct wined3d_shader *shader,
+        struct wined3d_context_gl *context_gl, const struct wined3d_shader *shader,
         struct arb_vs_compile_args *args)
 {
     const struct wined3d_d3d_info *d3d_info = context_gl->c.d3d_info;
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
     const struct wined3d_device *device = shader->device;
     const struct wined3d_adapter *adapter = device->adapter;
+    const struct wined3d_ivec4 *int_consts;
+    const BOOL *bool_consts;
     int i;
     WORD int_skip;
+
+    bool_consts = wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_VS_B], &context_gl->c);
+    int_consts = wined3d_buffer_load_sysmem(device->push_constants[WINED3D_PUSH_CONSTANTS_VS_I], &context_gl->c);
 
     find_vs_compile_args(state, shader, &args->super, &context_gl->c);
 
@@ -4500,10 +4524,9 @@ static void find_arb_vs_compile_args(const struct wined3d_state *state,
 
     /* This forces all local boolean constants to 1 to make them stateblock independent */
     args->clip.boolclip.bools = shader->reg_maps.local_bool_consts;
-    /* TODO: Figure out if it would be better to store bool constants as bitmasks in the stateblock */
     for (i = 0; i < WINED3D_MAX_CONSTS_B; ++i)
     {
-        if (state->vs_consts_b[i])
+        if (bool_consts[i])
             args->clip.boolclip.bools |= (1u << i);
     }
 
@@ -4531,9 +4554,9 @@ static void find_arb_vs_compile_args(const struct wined3d_state *state,
         }
         else
         {
-            args->loop_ctrl[i][0] = state->vs_consts_i[i].x;
-            args->loop_ctrl[i][1] = state->vs_consts_i[i].y;
-            args->loop_ctrl[i][2] = state->vs_consts_i[i].z;
+            args->loop_ctrl[i][0] = int_consts[i].x;
+            args->loop_ctrl[i][1] = int_consts[i].y;
+            args->loop_ctrl[i][2] = int_consts[i].z;
         }
     }
 }

dlls/wined3d/buffer.c
@@ -667,7 +667,7 @@ BOOL wined3d_buffer_load_location(struct wined3d_buffer *buffer,
 }
 
 /* Context activation is done by the caller. */
-BYTE *wined3d_buffer_load_sysmem(struct wined3d_buffer *buffer, struct wined3d_context *context)
+void *wined3d_buffer_load_sysmem(struct wined3d_buffer *buffer, struct wined3d_context *context)
 {
     if (wined3d_buffer_load_location(buffer, context, WINED3D_LOCATION_SYSMEM))
         buffer->resource.pin_sysmem = 1;

dlls/wined3d/cs.c
@@ -416,7 +416,6 @@ struct wined3d_cs_push_constants
     enum wined3d_push_constants type;
     unsigned int start_idx;
     unsigned int count;
-    BYTE constants[1];
 };
 
 struct wined3d_cs_reset_state
@@ -2107,74 +2106,93 @@ void wined3d_device_context_emit_set_feature_level(struct wined3d_device_context
     wined3d_device_context_submit(context, WINED3D_CS_QUEUE_DEFAULT);
 }
 
-static const struct
+static const struct push_constant_info
 {
-    size_t offset;
     size_t size;
+    unsigned int max_count;
     DWORD mask;
 }
 wined3d_cs_push_constant_info[] =
 {
-    /* WINED3D_PUSH_CONSTANTS_VS_F */
-    {FIELD_OFFSET(struct wined3d_state, vs_consts_f), sizeof(struct wined3d_vec4),  WINED3D_SHADER_CONST_VS_F},
-    /* WINED3D_PUSH_CONSTANTS_PS_F */
-    {FIELD_OFFSET(struct wined3d_state, ps_consts_f), sizeof(struct wined3d_vec4),  WINED3D_SHADER_CONST_PS_F},
-    /* WINED3D_PUSH_CONSTANTS_VS_I */
-    {FIELD_OFFSET(struct wined3d_state, vs_consts_i), sizeof(struct wined3d_ivec4), WINED3D_SHADER_CONST_VS_I},
-    /* WINED3D_PUSH_CONSTANTS_PS_I */
-    {FIELD_OFFSET(struct wined3d_state, ps_consts_i), sizeof(struct wined3d_ivec4), WINED3D_SHADER_CONST_PS_I},
-    /* WINED3D_PUSH_CONSTANTS_VS_B */
-    {FIELD_OFFSET(struct wined3d_state, vs_consts_b), sizeof(BOOL),                 WINED3D_SHADER_CONST_VS_B},
-    /* WINED3D_PUSH_CONSTANTS_PS_B */
-    {FIELD_OFFSET(struct wined3d_state, ps_consts_b), sizeof(BOOL),                 WINED3D_SHADER_CONST_PS_B},
+    [WINED3D_PUSH_CONSTANTS_VS_F] = {sizeof(struct wined3d_vec4),  WINED3D_MAX_VS_CONSTS_F, WINED3D_SHADER_CONST_VS_F},
+    [WINED3D_PUSH_CONSTANTS_PS_F] = {sizeof(struct wined3d_vec4),  WINED3D_MAX_PS_CONSTS_F, WINED3D_SHADER_CONST_PS_F},
+    [WINED3D_PUSH_CONSTANTS_VS_I] = {sizeof(struct wined3d_ivec4), WINED3D_MAX_CONSTS_I,    WINED3D_SHADER_CONST_VS_I},
+    [WINED3D_PUSH_CONSTANTS_PS_I] = {sizeof(struct wined3d_ivec4), WINED3D_MAX_CONSTS_I,    WINED3D_SHADER_CONST_PS_I},
+    [WINED3D_PUSH_CONSTANTS_VS_B] = {sizeof(BOOL),                 WINED3D_MAX_CONSTS_B,    WINED3D_SHADER_CONST_VS_B},
+    [WINED3D_PUSH_CONSTANTS_PS_B] = {sizeof(BOOL),                 WINED3D_MAX_CONSTS_B,    WINED3D_SHADER_CONST_PS_B},
 };
 
-static void wined3d_cs_st_push_constants(struct wined3d_device_context *context, enum wined3d_push_constants p,
-        unsigned int start_idx, unsigned int count, const void *constants)
+static bool prepare_push_constant_buffer(struct wined3d_device *device, enum wined3d_push_constants type)
 {
-    struct wined3d_cs *cs = wined3d_cs_from_context(context);
-    struct wined3d_device *device = cs->c.device;
-    unsigned int context_count;
-    unsigned int i;
-    size_t offset;
+    const struct push_constant_info *info = &wined3d_cs_push_constant_info[type];
+    HRESULT hr;
 
-    if (p == WINED3D_PUSH_CONSTANTS_VS_F)
-        device->shader_backend->shader_update_float_vertex_constants(device, start_idx, count);
-    else if (p == WINED3D_PUSH_CONSTANTS_PS_F)
-        device->shader_backend->shader_update_float_pixel_constants(device, start_idx, count);
-
-    offset = wined3d_cs_push_constant_info[p].offset + start_idx * wined3d_cs_push_constant_info[p].size;
-    memcpy((BYTE *)&cs->state + offset, constants, count * wined3d_cs_push_constant_info[p].size);
-    for (i = 0, context_count = device->context_count; i < context_count; ++i)
+    const struct wined3d_buffer_desc desc =
     {
-        device->contexts[i]->constant_update_mask |= wined3d_cs_push_constant_info[p].mask;
+        .byte_width = info->max_count * info->size,
+        .bind_flags = 0,
+        .access = WINED3D_RESOURCE_ACCESS_CPU | WINED3D_RESOURCE_ACCESS_MAP_R | WINED3D_RESOURCE_ACCESS_MAP_W,
+    };
+
+    if (!device->push_constants[type] && FAILED(hr = wined3d_buffer_create(device,
+            &desc, NULL, NULL, &wined3d_null_parent_ops, &device->push_constants[type])))
+    {
+        ERR("Failed to create push constant buffer, hr %#lx.\n", hr);
+        return false;
     }
+
+    return true;
 }
 
 static void wined3d_cs_exec_push_constants(struct wined3d_cs *cs, const void *data)
 {
     const struct wined3d_cs_push_constants *op = data;
+    struct wined3d_device *device = cs->c.device;
+    unsigned int context_count, i;
 
-    wined3d_cs_st_push_constants(&cs->c, op->type, op->start_idx, op->count, op->constants);
+    /* The constant buffers were already updated; this op is just to mark the
+     * constants as invalid in the device state. */
+
+    if (op->type == WINED3D_PUSH_CONSTANTS_VS_F)
+        device->shader_backend->shader_update_float_vertex_constants(device, op->start_idx, op->count);
+    else if (op->type == WINED3D_PUSH_CONSTANTS_PS_F)
+        device->shader_backend->shader_update_float_pixel_constants(device, op->start_idx, op->count);
+
+    for (i = 0, context_count = device->context_count; i < context_count; ++i)
+        device->contexts[i]->constant_update_mask |= wined3d_cs_push_constant_info[op->type].mask;
 }
 
-static void wined3d_cs_mt_push_constants(struct wined3d_device_context *context, enum wined3d_push_constants p,
-        unsigned int start_idx, unsigned int count, const void *constants)
+static void wined3d_device_context_emit_push_constants(struct wined3d_device_context *context,
+        enum wined3d_push_constants type, unsigned int start_idx, unsigned int count)
 {
     struct wined3d_cs *cs = wined3d_cs_from_context(context);
     struct wined3d_cs_push_constants *op;
-    size_t size;
 
-    size = count * wined3d_cs_push_constant_info[p].size;
-    op = wined3d_device_context_require_space(&cs->c, FIELD_OFFSET(struct wined3d_cs_push_constants, constants[size]),
-            WINED3D_CS_QUEUE_DEFAULT);
+    op = wined3d_device_context_require_space(&cs->c, sizeof(*op), WINED3D_CS_QUEUE_DEFAULT);
     op->opcode = WINED3D_CS_OP_PUSH_CONSTANTS;
-    op->type = p;
+    op->type = type;
     op->start_idx = start_idx;
     op->count = count;
-    memcpy(op->constants, constants, size);
 
     wined3d_device_context_submit(&cs->c, WINED3D_CS_QUEUE_DEFAULT);
+}
+
+void wined3d_device_context_push_constants(struct wined3d_device_context *context,
+        enum wined3d_push_constants type, unsigned int start_idx,
+        unsigned int count, const void *constants)
+{
+    const struct push_constant_info *info = &wined3d_cs_push_constant_info[type];
+    unsigned int byte_offset = start_idx * info->size;
+    unsigned int byte_size = count * info->size;
+    struct wined3d_box box;
+
+    if (!prepare_push_constant_buffer(context->device, type))
+        return;
+
+    wined3d_box_set(&box, byte_offset, 0, byte_offset + byte_size, 1, 0, 1);
+    wined3d_device_context_emit_update_sub_resource(context,
+            &context->device->push_constants[type]->resource, 0, &box, constants, byte_size, byte_size);
+    wined3d_device_context_emit_push_constants(context, type, start_idx, count);
 }
 
 static void wined3d_cs_exec_reset_state(struct wined3d_cs *cs, const void *data)
@@ -3145,7 +3163,6 @@ static const struct wined3d_device_context_ops wined3d_cs_st_ops =
     wined3d_cs_st_require_space,
     wined3d_cs_st_submit,
     wined3d_cs_st_finish,
-    wined3d_cs_st_push_constants,
     wined3d_cs_map_upload_bo,
     wined3d_cs_unmap_upload_bo,
     wined3d_cs_issue_query,
@@ -3272,7 +3289,6 @@ static const struct wined3d_device_context_ops wined3d_cs_mt_ops =
     wined3d_cs_mt_require_space,
     wined3d_cs_mt_submit,
     wined3d_cs_mt_finish,
-    wined3d_cs_mt_push_constants,
     wined3d_cs_map_upload_bo,
     wined3d_cs_unmap_upload_bo,
     wined3d_cs_issue_query,
@@ -4099,12 +4115,6 @@ static void wined3d_deferred_context_finish(struct wined3d_device_context *conte
     ERR("Ignoring finish() on a deferred context.\n");
 }
 
-static void wined3d_deferred_context_push_constants(struct wined3d_device_context *context,
-        enum wined3d_push_constants p, unsigned int start_idx, unsigned int count, const void *constants)
-{
-    FIXME("context %p, p %#x, start_idx %u, count %u, constants %p, stub!\n", context, p, start_idx, count, constants);
-}
-
 static struct wined3d_deferred_upload *deferred_context_get_upload(struct wined3d_deferred_context *deferred,
         struct wined3d_resource *resource, unsigned int sub_resource_idx)
 {
@@ -4276,7 +4286,6 @@ static const struct wined3d_device_context_ops wined3d_deferred_context_ops =
     wined3d_deferred_context_require_space,
     wined3d_deferred_context_submit,
     wined3d_deferred_context_finish,
-    wined3d_deferred_context_push_constants,
     wined3d_deferred_context_map_upload_bo,
     wined3d_deferred_context_unmap_upload_bo,
     wined3d_deferred_context_issue_query,

commit 333a0be42d74269f0e17366532af04c688482af2

wined3d: Fix handling of system-memory GL vertex buffers.
Fixes: c065b4fe0b3622e0c737a4c7c1c514273ad2d8a7

dlls/wined3d/context_gl.c
@@ -5399,7 +5399,8 @@ void wined3d_context_gl_load_tex_coords(const struct wined3d_context_gl *context
             gl_info->gl_ops.gl.p_glTexCoordPointer(format_gl->vtx_format, format_gl->vtx_type, e->stride,
                     get_vertex_attrib_pointer(e, state));
             gl_info->gl_ops.gl.p_glEnableClientState(GL_TEXTURE_COORD_ARRAY);
-            wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
+            if (bo)
+                wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
         }
         else
         {
@@ -5486,7 +5487,8 @@ static void wined3d_context_gl_load_vertex_data(struct wined3d_context_gl *conte
         checkGLcall("glVertexPointer(...)");
         gl_info->gl_ops.gl.p_glEnableClientState(GL_VERTEX_ARRAY);
         checkGLcall("glEnableClientState(GL_VERTEX_ARRAY)");
-        wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
+        if (bo)
+            wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
     }
 
     /* Normals */
@@ -5509,7 +5511,8 @@ static void wined3d_context_gl_load_vertex_data(struct wined3d_context_gl *conte
         checkGLcall("glNormalPointer(...)");
         gl_info->gl_ops.gl.p_glEnableClientState(GL_NORMAL_ARRAY);
         checkGLcall("glEnableClientState(GL_NORMAL_ARRAY)");
-        wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
+        if (bo)
+            wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
     }
     else
     {
@@ -5538,7 +5541,8 @@ static void wined3d_context_gl_load_vertex_data(struct wined3d_context_gl *conte
         checkGLcall("glColorPointer(4, GL_UNSIGNED_BYTE, ...)");
         gl_info->gl_ops.gl.p_glEnableClientState(GL_COLOR_ARRAY);
         checkGLcall("glEnableClientState(GL_COLOR_ARRAY)");
-        wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
+        if (bo)
+            wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
     }
     else
     {
@@ -5602,7 +5606,8 @@ static void wined3d_context_gl_load_vertex_data(struct wined3d_context_gl *conte
             }
             gl_info->gl_ops.gl.p_glEnableClientState(GL_SECONDARY_COLOR_ARRAY_EXT);
             checkGLcall("glEnableClientState(GL_SECONDARY_COLOR_ARRAY_EXT)");
-            wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
+            if (bo)
+                wined3d_buffer_validate_user(state->streams[e->stream_idx].buffer);
         }
         else
         {
@@ -5703,8 +5708,6 @@ static void wined3d_context_gl_load_numbered_arrays(struct wined3d_context_gl *c
 
         format_gl = wined3d_format_gl(element->format);
         stream = &state->streams[element->stream_idx];
-        wined3d_buffer_validate_user(stream->buffer);
-
 
         if (gl_info->supported[ARB_INSTANCED_ARRAYS])
         {
@@ -5738,6 +5741,9 @@ static void wined3d_context_gl_load_numbered_arrays(struct wined3d_context_gl *c
                 checkGLcall("glBindBuffer");
                 current_bo = bo;
             }
+            if (bo)
+                wined3d_buffer_validate_user(stream->buffer);
+
             /* Use the VBO to find out if a vertex buffer exists, not the vb
              * pointer. vb can point to a user pointer data blob. In that case
              * current_bo will be 0. If there is a vertex buffer but no vbo we