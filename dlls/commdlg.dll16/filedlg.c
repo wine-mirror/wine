wine-mirror/wine

commit 691c7775d11f9f62c363aa419d00eee1e8d7da55

commdlg: Pass the 16-bit structure to the WM_INITDIALOG hook.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=52356
Signed-off-by: Huw Davies <huw@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/commdlg.dll16/filedlg.c
@@ -482,6 +482,12 @@ static UINT_PTR CALLBACK call_hook_proc( WNDPROC16 hook, HWND hwnd, UINT msg, WP
     case WM_ACTIVATEAPP:
         ret = call_hook16( hook, hwnd, msg, wp, HTASK_16( lp ));
         break;
+    case WM_INITDIALOG:
+        {
+            OPENFILENAMEA *ofn = (OPENFILENAMEA *)lp;
+            ret = call_hook16( hook, hwnd, msg, wp, ofn->lCustData );
+            break;
+        }
     default:
         ret = call_hook16( hook, hwnd, msg, wp, lp );
         break;
@@ -591,7 +597,7 @@ BOOL16 WINAPI GetOpenFileName16( SEGPTR ofn ) /* [in/out] address of structure w
     ofn32.nFileOffset       = lpofn->nFileOffset;
     ofn32.nFileExtension    = lpofn->nFileExtension;
     ofn32.lpstrDefExt       = MapSL( lpofn->lpstrDefExt );
-    ofn32.lCustData         = lpofn->lCustData;
+    ofn32.lCustData         = ofn; /* See WM_INITDIALOG in the hook proc */
     ofn32.lpfnHook          = dummy_hook;  /* this is to force old 3.1 dialog style */
 
     if (lpofn->Flags & OFN_ENABLETEMPLATE)

commit fbbabca177dabb83c8bdc23b70063c1dbf3f2491

commdlg: Set lCustData the same in GetSaveFileName as GetOpenFileName.
Fixes: 691c7775d11f9f62c363aa419d00eee1e8d7da55
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=55715

dlls/commdlg.dll16/filedlg.c
@@ -664,7 +664,7 @@ BOOL16 WINAPI GetSaveFileName16( SEGPTR ofn ) /* [in/out] address of structure w
     ofn32.nFileOffset       = lpofn->nFileOffset;
     ofn32.nFileExtension    = lpofn->nFileExtension;
     ofn32.lpstrDefExt       = MapSL( lpofn->lpstrDefExt );
-    ofn32.lCustData         = lpofn->lCustData;
+    ofn32.lCustData         = ofn; /* See WM_INITDIALOG in the hook proc */
     ofn32.lpfnHook          = dummy_hook;  /* this is to force old 3.1 dialog style */
 
     if (lpofn->Flags & OFN_ENABLETEMPLATE)