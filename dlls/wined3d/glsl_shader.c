9921a2361bb367370bba334b2cd4e2faac9ee8e6
Zebediah Figura <zfigura@codeweavers.com>
2023-10-24 14:00:39 -0500

    wined3d/glsl: Clamp ftou lower bound to zero.
    Ported from 491146fa94c608c241c06114b17a3b4d8b9cea65 in vkd3d.

--- dlls/wined3d/glsl_shader.c
+++ dlls/wined3d/glsl_shader.c
@@ -4328,7 +4328,19 @@ static void shader_glsl_to_int(const struct wined3d_shader_instruction *ins)
 
 static void shader_glsl_to_uint(const struct wined3d_shader_instruction *ins)
 {
-    shader_glsl_cast(ins, "uvec", "uint");
+    struct wined3d_string_buffer *buffer = ins->ctx->buffer;
+    struct glsl_src_param src_param;
+    unsigned int mask_size;
+    DWORD write_mask;
+
+    write_mask = shader_glsl_append_dst(buffer, ins);
+    mask_size = shader_glsl_get_write_mask_size(write_mask);
+    shader_glsl_add_src_param(ins, &ins->src[0], write_mask, &src_param);
+
+    if (mask_size > 1)
+        shader_addline(buffer, "uvec%u(max(%s, vec%u(0.0))));\n", mask_size, src_param.param_str, mask_size);
+    else
+        shader_addline(buffer, "uint(max(%s, 0)));\n", src_param.param_str);
 }
 
 static void shader_glsl_to_float(const struct wined3d_shader_instruction *ins)
