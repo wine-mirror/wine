db50069167ba01917353870d5ac5a373b411b14f
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2023-11-06 09:55:12 +1100

    wmiutils: Remove DECLSPEC_HIDDEN usage.

--- dlls/wmiutils/wmiutils_private.h
+++ dlls/wmiutils/wmiutils_private.h
@@ -16,5 +16,5 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-HRESULT WbemPath_create(LPVOID *) DECLSPEC_HIDDEN;
-HRESULT WbemStatusCodeText_create(LPVOID *) DECLSPEC_HIDDEN;
+HRESULT WbemPath_create(LPVOID *);
+HRESULT WbemStatusCodeText_create(LPVOID *);
