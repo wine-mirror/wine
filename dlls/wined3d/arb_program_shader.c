wine-mirror/wine

commit 831a41ee34b6ed5974db7a0864e7aeac34c936a9

wined3d: Introduce a wined3d_state_get_ffp_texture() helper.

dlls/wined3d/arb_program_shader.c
@@ -490,7 +490,7 @@ static void shader_arb_load_np2fixup_constants(const struct arb_ps_np2fixup_info
     while (active)
     {
         i = wined3d_bit_scan(&active);
-        if (!(tex = state->textures[i]))
+        if (!(tex = wined3d_state_get_ffp_texture(state, i)))
         {
             ERR("Nonexistent texture is flagged for NP2 texcoord fixup.\n");
             continue;
@@ -6048,9 +6048,9 @@ static void alpha_test_arbfp(struct wined3d_context *context, const struct wined
 
 static void color_key_arbfp(struct wined3d_context *context, const struct wined3d_state *state, DWORD state_id)
 {
+    const struct wined3d_texture *texture = wined3d_state_get_ffp_texture(state, 0);
     struct wined3d_context_gl *context_gl = wined3d_context_gl(context);
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
-    const struct wined3d_texture *texture = state->textures[0];
     struct wined3d_device *device = context->device;
     struct wined3d_color float_key[2];
 

dlls/wined3d/glsl_shader.c
@@ -1299,7 +1299,7 @@ static void shader_glsl_load_np2fixup_constants(const struct glsl_ps_program *ps
 
     for (i = 0; fixup; fixup >>= 1, ++i)
     {
-        const struct wined3d_texture *tex = state->textures[i];
+        const struct wined3d_texture *tex = wined3d_state_get_ffp_texture(state, i);
         unsigned char idx = ps->np2_fixup_info->idx[i];
 
         if (!tex)
@@ -1522,7 +1522,7 @@ static void shader_glsl_load_color_key_constant(const struct glsl_ps_program *ps
         const struct wined3d_gl_info *gl_info, const struct wined3d_state *state)
 {
     struct wined3d_color float_key[2];
-    const struct wined3d_texture *texture = state->textures[0];
+    const struct wined3d_texture *texture = wined3d_state_get_ffp_texture(state, 0);
 
     wined3d_format_get_float_color_key(texture->resource.format, &texture->async.src_blt_color_key, float_key);
     GL_EXTCALL(glUniform4fv(ps->color_key_location, 2, &float_key[0].r));
@@ -11842,7 +11842,7 @@ static void glsl_vertex_pipe_texmatrix_np2(struct wined3d_context *context,
         const struct wined3d_state *state, DWORD state_id)
 {
     DWORD sampler = state_id - STATE_SAMPLER(0);
-    const struct wined3d_texture *texture = state->textures[sampler];
+    const struct wined3d_texture *texture = wined3d_state_get_ffp_texture(state, sampler);
     BOOL np2;
 
     if (!texture)

dlls/wined3d/shader.c
@@ -2858,7 +2858,7 @@ void find_ps_compile_args(const struct wined3d_state *state, const struct wined3
             /* Treat unbound textures as 2D. The dummy texture will provide
              * the proper sample value. The tex_types bitmap defaults to
              * 2D because of the memset. */
-            if (!(texture = state->textures[i]))
+            if (!(texture = wined3d_state_get_ffp_texture(state, i)))
                 continue;
 
             switch (wined3d_texture_gl(texture)->target)
@@ -2900,7 +2900,7 @@ void find_ps_compile_args(const struct wined3d_state *state, const struct wined3
                     break;
             }
 
-            if ((texture = state->textures[i]))
+            if ((texture = wined3d_state_get_ffp_texture(state, i)))
             {
                 /* Star Wars: The Old Republic uses mismatched samplers for rendering water. */
                 if (texture->resource.type == WINED3D_RTYPE_TEXTURE_2D
@@ -2930,8 +2930,7 @@ void find_ps_compile_args(const struct wined3d_state *state, const struct wined3
             if (!shader->reg_maps.resource_info[i].type)
                 continue;
 
-            texture = state->textures[i];
-            if (!texture)
+            if (!(texture = wined3d_state_get_ffp_texture(state, i)))
             {
                 args->color_fixup[i] = COLOR_FIXUP_IDENTITY;
                 continue;

dlls/wined3d/ati_fragment_shader.c
@@ -1057,7 +1057,7 @@ static void set_tex_op_atifs(struct wined3d_context *context, const struct wined
         if (mapped_stage != WINED3D_UNMAPPED_STAGE)
         {
             wined3d_context_gl_active_texture(context_gl, gl_info, mapped_stage);
-            texture_activate_dimensions(state->textures[i], gl_info);
+            texture_activate_dimensions(wined3d_state_get_ffp_texture(state, i), gl_info);
         }
     }
 

dlls/wined3d/context_gl.c
@@ -4906,7 +4906,7 @@ static void draw_primitive_immediate_mode(struct wined3d_context_gl *context_gl,
             continue;
         }
 
-        if (!ps && !state->textures[texture_idx])
+        if (!ps && !wined3d_state_get_ffp_texture(state, texture_idx))
             continue;
 
         texture_unit = context_gl->tex_unit_map[texture_idx];

dlls/wined3d/cs.c
@@ -1912,7 +1912,7 @@ static void wined3d_cs_exec_set_color_key(struct wined3d_cs *cs, const void *dat
                 break;
 
             case WINED3D_CKEY_SRC_BLT:
-                if (texture == cs->state.textures[0])
+                if (texture == wined3d_state_get_ffp_texture(&cs->state, 0))
                 {
                     device_invalidate_state(cs->c.device, STATE_COLOR_KEY);
                     if (!(texture->async.color_key_flags & WINED3D_CKEY_SRC_BLT))
@@ -1942,7 +1942,8 @@ static void wined3d_cs_exec_set_color_key(struct wined3d_cs *cs, const void *dat
                 break;
 
             case WINED3D_CKEY_SRC_BLT:
-                if (texture == cs->state.textures[0] && texture->async.color_key_flags & WINED3D_CKEY_SRC_BLT)
+                if (texture == wined3d_state_get_ffp_texture(&cs->state, 0)
+                        && texture->async.color_key_flags & WINED3D_CKEY_SRC_BLT)
                     device_invalidate_state(cs->c.device, STATE_RENDER(WINED3D_RS_COLORKEYENABLE));
 
                 texture->async.color_key_flags &= ~WINED3D_CKEY_SRC_BLT;

dlls/wined3d/nvidia_texture_shader.c
@@ -48,7 +48,7 @@ static void nvts_activate_dimensions(const struct wined3d_state *state,
         context_gl->c.texShaderBumpMap &= ~(1u << stage);
     }
 
-    if ((texture = state->textures[stage]))
+    if ((texture = wined3d_state_get_ffp_texture(state, stage)))
     {
         switch (wined3d_texture_gl(texture)->target)
         {
@@ -552,7 +552,7 @@ static void nvrc_colorop(struct wined3d_context *context, const struct wined3d_s
             if (gl_info->supported[NV_TEXTURE_SHADER2])
                 nvts_activate_dimensions(state, stage, context_gl);
             else
-                texture_activate_dimensions(state->textures[stage], gl_info);
+                texture_activate_dimensions(wined3d_state_get_ffp_texture(state, stage), gl_info);
         }
     }
 

dlls/wined3d/state.c
@@ -808,6 +808,7 @@ static void blend_dbb(struct wined3d_context *context, const struct wined3d_stat
 
 void state_alpha_test(struct wined3d_context *context, const struct wined3d_state *state, DWORD state_id)
 {
+    const struct wined3d_texture *texture = wined3d_state_get_ffp_texture(state, 0);
     const struct wined3d_gl_info *gl_info = wined3d_context_gl(context)->gl_info;
     int glParm = 0;
     float ref;
@@ -822,7 +823,7 @@ void state_alpha_test(struct wined3d_context *context, const struct wined3d_stat
      * WINED3D_RS_COLORKEYENABLE state(which is d3d <= 3 only). The texture
      * function will call alpha in case it finds some texture + colorkeyenable
      * combination which needs extra care. */
-    if (state->textures[0] && (state->textures[0]->async.color_key_flags & WINED3D_CKEY_SRC_BLT))
+    if (texture && (texture->async.color_key_flags & WINED3D_CKEY_SRC_BLT))
         enable_ckey = TRUE;
 
     if (enable_ckey || context->last_was_ckey)
@@ -2062,7 +2063,7 @@ static void set_tex_op(const struct wined3d_gl_info *gl_info, const struct wined
         op = WINED3D_TOP_SELECT_ARG1;
     }
 
-    if (isAlpha && !state->textures[Stage] && arg1 == WINED3DTA_TEXTURE)
+    if (isAlpha && !wined3d_state_get_ffp_texture(state, Stage) && arg1 == WINED3DTA_TEXTURE)
     {
         get_src_and_opr(WINED3DTA_DIFFUSE, isAlpha, &src1, &opr1);
     } else {
@@ -3109,7 +3110,7 @@ static void tex_colorop(struct wined3d_context *context, const struct wined3d_st
     /* The sampler will also activate the correct texture dimensions, so no
      * need to do it here if the sampler for this stage is dirty. */
     if (!isStateDirty(context, STATE_SAMPLER(stage)) && tex_used)
-        texture_activate_dimensions(state->textures[stage], gl_info);
+        texture_activate_dimensions(wined3d_state_get_ffp_texture(state, stage), gl_info);
 
     set_tex_op(gl_info, state, FALSE, stage,
             state->texture_states[stage][WINED3D_TSS_COLOR_OP],
@@ -3121,6 +3122,7 @@ static void tex_colorop(struct wined3d_context *context, const struct wined3d_st
 void tex_alphaop(struct wined3d_context *context, const struct wined3d_state *state, DWORD state_id)
 {
     unsigned int stage = (state_id - STATE_TEXTURESTAGE(0, 0)) / (WINED3D_HIGHEST_TEXTURE_STATE + 1);
+    struct wined3d_texture *texture = wined3d_state_get_ffp_texture(state, 0);
     struct wined3d_context_gl *context_gl = wined3d_context_gl(context);
     BOOL tex_used = context->fixed_function_usage_map & (1u << stage);
     const struct wined3d_gl_info *gl_info = context_gl->gl_info;
@@ -3144,9 +3146,9 @@ void tex_alphaop(struct wined3d_context *context, const struct wined3d_state *st
     arg2 = state->texture_states[stage][WINED3D_TSS_ALPHA_ARG2];
     arg0 = state->texture_states[stage][WINED3D_TSS_ALPHA_ARG0];
 
-    if (state->render_states[WINED3D_RS_COLORKEYENABLE] && !stage && state->textures[0])
+    if (state->render_states[WINED3D_RS_COLORKEYENABLE] && !stage && texture)
     {
-        struct wined3d_texture_gl *texture_gl = wined3d_texture_gl(state->textures[0]);
+        struct wined3d_texture_gl *texture_gl = wined3d_texture_gl(texture);
         GLenum texture_dimensions = texture_gl->target;
 
         if (texture_dimensions == GL_TEXTURE_2D || texture_dimensions == GL_TEXTURE_RECTANGLE_ARB)
@@ -3424,7 +3426,7 @@ static void tex_coordindex(struct wined3d_context *context, const struct wined3d
 static void sampler_texmatrix(struct wined3d_context *context, const struct wined3d_state *state, DWORD state_id)
 {
     const DWORD sampler = state_id - STATE_SAMPLER(0);
-    const struct wined3d_texture *texture = state->textures[sampler];
+    const struct wined3d_texture *texture = wined3d_state_get_ffp_texture(state, sampler);
 
     TRACE("context %p, state %p, state_id %#lx.\n", context, state, state_id);
 

commit 02b3a8cb7b7bcb3b1d375c66aa65a7a7b1e7cc99

wined3d: Revert uses of wined3d_state_get_ffp_texture() for non-FFP textures.
This fixes a crash when loading a world in Spore.

Fixes: 831a41ee34b6ed5974db7a0864e7aeac34c936a9

dlls/wined3d/arb_program_shader.c
@@ -490,7 +490,7 @@ static void shader_arb_load_np2fixup_constants(const struct arb_ps_np2fixup_info
     while (active)
     {
         i = wined3d_bit_scan(&active);
-        if (!(tex = wined3d_state_get_ffp_texture(state, i)))
+        if (!(tex = state->textures[i]))
         {
             ERR("Nonexistent texture is flagged for NP2 texcoord fixup.\n");
             continue;

dlls/wined3d/glsl_shader.c
@@ -1299,7 +1299,7 @@ static void shader_glsl_load_np2fixup_constants(const struct glsl_ps_program *ps
 
     for (i = 0; fixup; fixup >>= 1, ++i)
     {
-        const struct wined3d_texture *tex = wined3d_state_get_ffp_texture(state, i);
+        const struct wined3d_texture *tex = state->textures[i];
         unsigned char idx = ps->np2_fixup_info->idx[i];
 
         if (!tex)

dlls/wined3d/shader.c
@@ -2858,7 +2858,7 @@ void find_ps_compile_args(const struct wined3d_state *state, const struct wined3
             /* Treat unbound textures as 2D. The dummy texture will provide
              * the proper sample value. The tex_types bitmap defaults to
              * 2D because of the memset. */
-            if (!(texture = wined3d_state_get_ffp_texture(state, i)))
+            if (!(texture = state->textures[i]))
                 continue;
 
             switch (wined3d_texture_gl(texture)->target)
@@ -2900,7 +2900,7 @@ void find_ps_compile_args(const struct wined3d_state *state, const struct wined3
                     break;
             }
 
-            if ((texture = wined3d_state_get_ffp_texture(state, i)))
+            if ((texture = state->textures[i]))
             {
                 /* Star Wars: The Old Republic uses mismatched samplers for rendering water. */
                 if (texture->resource.type == WINED3D_RTYPE_TEXTURE_2D
@@ -2930,7 +2930,7 @@ void find_ps_compile_args(const struct wined3d_state *state, const struct wined3
             if (!shader->reg_maps.resource_info[i].type)
                 continue;
 
-            if (!(texture = wined3d_state_get_ffp_texture(state, i)))
+            if (!(texture = state->textures[i]))
             {
                 args->color_fixup[i] = COLOR_FIXUP_IDENTITY;
                 continue;