4608d8b46f15ec58f87d75b87fe733e0ea8d9265
Rémi Bernon <rbernon@codeweavers.com>
2023-12-01 23:40:17 +0100

    ntdll: Set RLIMIT_NICE to its hard limit.

--- dlls/ntdll/unix/loader.c
+++ dlls/ntdll/unix/loader.c
@@ -2178,6 +2178,9 @@ DECLSPEC_EXPORT void __wine_main( int argc, char *argv[] )
 #ifdef RLIMIT_AS
     set_max_limit( RLIMIT_AS );
 #endif
+#ifdef RLIMIT_NICE
+    set_max_limit( RLIMIT_NICE );
+#endif
 
     virtual_init();
     init_environment();
