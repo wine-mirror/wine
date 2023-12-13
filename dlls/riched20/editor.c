5cbe3a6cb186cffed20e3fea5f88abd0880d3914
Eric Pouech <epouech@codeweavers.com>
2023-12-13 15:12:38 +0100

    riched20: Don't write past end of buffer.
    Signed-off-by: Eric Pouech <epouech@codeweavers.com>

--- dlls/riched20/editor.c
+++ dlls/riched20/editor.c
@@ -4309,7 +4309,7 @@ int ME_GetTextW(ME_TextEditor *editor, WCHAR *buffer, int buflen,
     str = get_text( run, 0 );
   }
   /* append '\r' to the last paragraph. */
-  if (run == para_end_run( para_prev( editor_end_para( editor ) ) ) && bEOP)
+  if (run == para_end_run( para_prev( editor_end_para( editor ) ) ) && bEOP && buflen)
   {
     *buffer = '\r';
     buffer ++;
