253857383a93acf9f5c1afad5c29ddb3868ced59
Alex Henrie <alexhenrie24@gmail.com>
2024-01-06 12:53:30 -0700

    msvcp140_2: Implement __std_smf_hypot3.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=56078
    (cherry picked from commit fca71b6ef84e2f52afe369e1a1d1f5908f138881)

---
+++ dlls/msvcp140_2/math.c
@@ -0,0 +1,26 @@
+/*
+ * Special math functions
+ *
+ * Copyright 2024 Alex Henrie
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#include <math.h>
+
+double __stdcall __std_smf_hypot3(double x, double y, double z)
+{
+    return hypot(hypot(x, y), z);
+}
