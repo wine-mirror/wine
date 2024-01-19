46c5c2d41a014253f51d89c092d7e008f9f29575
Rémi Bernon <rbernon@codeweavers.com>
2024-01-19 15:46:44 +0100

    imm32: Complete the composition string when the IME is closed.

--- dlls/imm32/ime.c
+++ dlls/imm32/ime.c
@@ -653,11 +653,7 @@ BOOL WINAPI NotifyIME( HIMC himc, DWORD action, DWORD index, DWORD value )
             }
             break;
         case IMC_SETOPENSTATUS:
-            if (!ctx->fOpen)
-            {
-                input_context_set_comp_str( ctx, NULL, 0 );
-                if ((msg = ime_set_composition_status( himc, FALSE ))) ime_send_message( himc, msg, 0, 0 );
-            }
+            if (!ctx->fOpen) ImmNotifyIME( himc, NI_COMPOSITIONSTR, CPS_COMPLETE, 0 );
             NtUserNotifyIMEStatus( ctx->hWnd, ctx->fOpen );
             break;
         }
@@ -691,12 +687,12 @@ BOOL WINAPI NotifyIME( HIMC himc, DWORD action, DWORD index, DWORD value )
                 if (flags) ime_send_message( himc, WM_IME_COMPOSITION, wchr, flags );
             }
 
-            ImmSetOpenStatus( himc, FALSE );
-            break;
+            /* fallthrough */
         }
         case CPS_CANCEL:
             input_context_set_comp_str( ctx, NULL, 0 );
-            ImmSetOpenStatus( himc, FALSE );
+            if ((msg = ime_set_composition_status( himc, FALSE )))
+                ime_send_message( himc, msg, 0, 0 );
             break;
         default:
             FIXME( "himc %p, action %#lx, index %#lx, value %#lx stub!\n", himc, action, index, value );
