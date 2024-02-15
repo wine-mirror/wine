wine-mirror/wine

commit b5c57b9a62c396068d18237bd6e82b37c169fdc5

explorer: Set layered style on systray icons only when it's actually layered.
Fixes a regression introduced by 229b4561d9a8f10cbb49342dff0b6a3472d81c68,
which caused the icons to not be visible initially in the virtual desktop
systray.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

programs/explorer/systray.c
@@ -624,6 +624,7 @@ static BOOL show_icon(struct icon *icon)
     {
         icon->display = ICON_DISPLAY_DOCKED;
         icon->layered = TRUE;
+        SetWindowLongW( icon->window, GWL_EXSTYLE, GetWindowLongW( icon->window, GWL_EXSTYLE ) | WS_EX_LAYERED );
         SendMessageW( icon->window, WM_SIZE, SIZE_RESTORED, MAKELONG( icon_cx, icon_cy ) );
     }
     systray_add_icon( icon );
@@ -645,6 +646,7 @@ static BOOL hide_icon(struct icon *icon)
     {
         icon->display = ICON_DISPLAY_HIDDEN;
         icon->layered = FALSE;
+        SetWindowLongW( icon->window, GWL_EXSTYLE, GetWindowLongW( icon->window, GWL_EXSTYLE ) & ~WS_EX_LAYERED );
     }
     ShowWindow( icon->window, SW_HIDE );
     systray_remove_icon( icon );
@@ -732,7 +734,7 @@ static BOOL add_icon(NOTIFYICONDATAW *nid)
     icon->owner  = nid->hWnd;
     icon->display = ICON_DISPLAY_HIDDEN;
 
-    CreateWindowExW( WS_EX_LAYERED, tray_icon_class.lpszClassName, NULL, WS_CLIPSIBLINGS | WS_POPUP,
+    CreateWindowExW( 0, tray_icon_class.lpszClassName, NULL, WS_CLIPSIBLINGS | WS_POPUP,
                      0, 0, icon_cx, icon_cy, 0, NULL, NULL, icon );
     if (!icon->window) ERR( "Failed to create systray icon window\n" );
 

commit 8a71a4a30495635bdc905af41869d8ed9e43009f

explorer: Set layered style on systray icons before calling into the driver.
Fixes a regression introduced by b5c57b9a62c396068d18237bd6e82b37c169fdc5,
which broke the systray integration outside of virtual desktops on some
DEs like XFCE.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

programs/explorer/systray.c
@@ -619,13 +619,23 @@ static BOOL show_icon(struct icon *icon)
 
     if (icon->display != ICON_DISPLAY_HIDDEN) return TRUE;  /* already displayed */
 
-    if (!enable_taskbar && NtUserMessageCall( icon->window, WINE_SYSTRAY_DOCK_INSERT, icon_cx, icon_cy,
-                                              icon, NtUserSystemTrayCall, FALSE ))
+    if (!enable_taskbar)
     {
-        icon->display = ICON_DISPLAY_DOCKED;
-        icon->layered = TRUE;
-        SetWindowLongW( icon->window, GWL_EXSTYLE, GetWindowLongW( icon->window, GWL_EXSTYLE ) | WS_EX_LAYERED );
-        SendMessageW( icon->window, WM_SIZE, SIZE_RESTORED, MAKELONG( icon_cx, icon_cy ) );
+        DWORD old_exstyle = GetWindowLongW( icon->window, GWL_EXSTYLE );
+
+        /* make sure it is layered before calling into the driver */
+        SetWindowLongW( icon->window, GWL_EXSTYLE, old_exstyle | WS_EX_LAYERED );
+        paint_layered_icon( icon );
+
+        if (!NtUserMessageCall( icon->window, WINE_SYSTRAY_DOCK_INSERT, icon_cx, icon_cy,
+                                icon, NtUserSystemTrayCall, FALSE ))
+            SetWindowLongW( icon->window, GWL_EXSTYLE, old_exstyle );
+        else
+        {
+            icon->display = ICON_DISPLAY_DOCKED;
+            icon->layered = TRUE;
+            SendMessageW( icon->window, WM_SIZE, SIZE_RESTORED, MAKELONG( icon_cx, icon_cy ) );
+        }
     }
     systray_add_icon( icon );
 