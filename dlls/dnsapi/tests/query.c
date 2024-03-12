1482e2f960d9997a0e1fc23c2c72151d4bf64be7
Hans Leidekker <hans@codeweavers.com>
2024-03-12 13:34:08 +0100

    dnsapi/tests: Skip tests if no CNAME records are returned.

--- dlls/dnsapi/tests/query.c
+++ dlls/dnsapi/tests/query.c
@@ -167,6 +167,11 @@ static void test_DnsQuery(void)
         skip("query timed out\n");
         return;
     }
+    if (status == DNS_INFO_NO_RECORDS)
+    {
+        skip("no CNAME records\n");
+        return;
+    }
     ok(status == ERROR_SUCCESS, "got %ld\n", status);
     if (status == ERROR_SUCCESS && winetest_debug > 1)
     {
