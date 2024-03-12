0e7d38d1cecb2e8b9e896d0362f79034c7c300bc
Haoyang Chen <chenhaoyang@kylinos.cn>
2024-03-12 10:36:32 +0800

    comctl32/tests: Add EM_{SET,GET}PASSWORDCHAR tests for edit control.

--- dlls/comctl32/tests/edit.c
+++ dlls/comctl32/tests/edit.c
@@ -3893,6 +3893,55 @@ static void test_format_rect(void)
     }
 }
 
+static void test_PASSWORDCHAR(void)
+{
+    HWND hwEdit;
+    LONG r;
+    CHAR passwdChar = '#';
+
+    hwEdit = create_editcontrol(ES_AUTOHSCROLL | ES_AUTOVSCROLL, 0);
+
+    r = SendMessageA(hwEdit, EM_SETPASSWORDCHAR, passwdChar, 0);
+    todo_wine
+    ok(r == 1, "Expected: 1, got: %ld\n", r);
+
+    r = get_edit_style(hwEdit);
+    ok(r & ES_PASSWORD, "Wrong style expected ES_PASSWORD got: 0x%lx\n", r);
+
+    r = SendMessageA(hwEdit, EM_GETPASSWORDCHAR, 0, 0);
+    ok(r == passwdChar, "Expected: 0, got: %ld\n", r);
+
+    DestroyWindow (hwEdit);
+
+    hwEdit = create_editcontrol(ES_MULTILINE, 0);
+    r = get_edit_style(hwEdit);
+    ok(r == ES_MULTILINE, "Wrong style expected ES_PASSWORD got: 0x%lx\n", r);
+
+    r = SendMessageA(hwEdit, EM_SETPASSWORDCHAR, passwdChar, 0);
+    todo_wine
+    ok(r == 1, "Expected: 1, got: %ld\n", r);
+
+    r = get_edit_style(hwEdit);
+    todo_wine
+    ok(r == (ES_MULTILINE | ES_PASSWORD), "Wrong style expected ES_MULTILINE|ES_PASSWORD got: 0x%lx\n", r);
+
+    r = SendMessageA(hwEdit, EM_GETPASSWORDCHAR, 0, 0);
+    todo_wine
+    ok(r == passwdChar, "Expected: 0, got: %ld\n", r);
+
+    r = SendMessageA(hwEdit, EM_SETPASSWORDCHAR, 0, 0);
+    todo_wine
+    ok(r == 1, "Expected: 1, got: %ld\n", r);
+
+    r = get_edit_style(hwEdit);
+    ok(r == ES_MULTILINE, "Wrong style expected ES_PASSWORD got: 0x%lx\n", r);
+
+    r = SendMessageA(hwEdit, EM_GETPASSWORDCHAR, 0, 0);
+    ok(r == 0, "Expected: 0, got: %ld\n", r);
+
+    DestroyWindow (hwEdit);
+}
+
 START_TEST(edit)
 {
     ULONG_PTR ctx_cookie;
@@ -3943,6 +3992,7 @@ START_TEST(edit)
     test_cue_banner();
     test_ime();
     test_format_rect();
+    test_PASSWORDCHAR();
 
     UnregisterWindowClasses();
 
