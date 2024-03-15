5a97ad97dc58edcec0a3f21ca7359b26ed6a3ce0
Marc-Aurel Zent <mzent@codeweavers.com>
2024-03-15 08:56:18 +0100

    winemac.drv: Uppercase single keys in GetKeyNameText.

--- dlls/winemac.drv/keyboard.c
+++ dlls/winemac.drv/keyboard.c
@@ -1369,6 +1369,9 @@ INT macdrv_GetKeyNameText(LONG lparam, LPWSTR buffer, INT size)
             if (!len)
                 break;
 
+            if (status == noErr && len == 1 && buffer[0] >= 'a' && buffer[0] <= 'z')
+                buffer[0] += 'A' - 'a';
+
             TRACE("lparam 0x%08x -> %s\n", (unsigned int)lparam, debugstr_w(buffer));
             return len;
         }
