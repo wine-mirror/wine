wine-mirror/wine

commit 9d537999e315a74ef559fabb79d5b632b8a00208

widl: Use a struct list for the import stack.

tools/widl/parser.h
@@ -28,11 +28,9 @@ extern char *parser_text;
 extern int parser_debug;
 extern int yy_flex_debug;
 
-extern int import_stack_ptr;
+extern int parse_only;
 void push_import( char *import_name );
 void pop_import(void);
-
-#define parse_only import_stack_ptr
 
 int is_type(const char *name);
 

tools/widl/parser.l
@@ -81,13 +81,15 @@ static void switch_to_acf(void);
 
 static warning_list_t *disabled_warnings = NULL;
 
-#define MAX_IMPORT_DEPTH 20
-struct {
-  YY_BUFFER_STATE state;
-  char *input_name;
-  int   line_number;
-} import_stack[MAX_IMPORT_DEPTH];
-int import_stack_ptr = 0;
+struct import_state
+{
+    YY_BUFFER_STATE buffer;
+    char *input_name;
+    int line_number;
+    struct list entry;
+};
+static struct list import_stack = LIST_INIT( import_stack );
+int parse_only = 0;
 
 struct import
 {
@@ -159,7 +161,8 @@ struct uuid *parse_uuid(const char *u)
                         }
 <PP_PRAGMA>midl_echo[^\n]*  yyless(9); yy_pop_state(); return tCPPQUOTE;
 <PP_PRAGMA>winrt[^\n]*  {
-                            if(import_stack_ptr) {
+                            if (!list_empty( &import_stack ))
+                            {
                                 if(!winrt_mode)
                                     error_loc("winrt IDL file imported in non-winrt mode\n");
                             }else {
@@ -234,7 +237,7 @@ SAFEARRAY{ws}*/\(	return tSAFEARRAY;
 <INITIAL,ATTR>\.\.\.	return ELLIPSIS;
 <INITIAL,ATTR>.		return yytext[0];
 <<EOF>>			{
-                            if (import_stack_ptr)
+                            if (!list_empty( &import_stack ))
                                 return aEOF;
                             if (acf_name)
                             {
@@ -510,32 +513,38 @@ static char *get_buffered_cstring(void)
 
 void pop_import(void)
 {
-	int ptr = import_stack_ptr-1;
+    struct list *entry = list_head( &import_stack );
+    struct import_state *state;
+    assert( entry );
 
-	if (yyin) fclose( yyin );
-	yy_delete_buffer( YY_CURRENT_BUFFER );
-	yy_switch_to_buffer( import_stack[ptr].state );
-	input_name = import_stack[ptr].input_name;
-	line_number = import_stack[ptr].line_number;
-	import_stack_ptr--;
+    state = LIST_ENTRY( entry, struct import_state, entry );
+    list_remove( &state->entry );
+    parse_only = !list_empty( &import_stack );
+
+    if (yyin) fclose( yyin );
+    yy_delete_buffer( YY_CURRENT_BUFFER );
+    yy_switch_to_buffer( state->buffer );
+    input_name = state->input_name;
+    line_number = state->line_number;
+    free( state );
 }
 
 void push_import( char *import_name )
 {
+    struct import_state *state;
     FILE *f;
     char *path, *name;
     struct import *import;
-    int ptr = import_stack_ptr;
     int ret;
 
-    if (import_stack_ptr == MAX_IMPORT_DEPTH)
-        error_loc("Exceeded max import depth\n");
+    state = xmalloc( sizeof(struct import_state ));
+    list_add_head( &import_stack, &state->entry );
+    parse_only = !list_empty( &import_stack );
 
-    import_stack[ptr].state = YY_CURRENT_BUFFER;
-    import_stack[ptr].input_name = input_name;
-    import_stack[ptr].line_number = line_number;
-    import_stack_ptr++;
-    yyin = NULL;
+    state->buffer = YY_CURRENT_BUFFER;
+    state->input_name = input_name;
+    state->line_number = line_number;
+    input_name = NULL;
 
     /* reset buffer for <<EOF>>, in case import fails or already imported */
     yy_scan_string( "" );
@@ -573,12 +582,12 @@ void push_import( char *import_name )
 
 static void switch_to_acf(void)
 {
-    int ptr = import_stack_ptr;
-    int ret;
     char *name;
+    int ret;
     FILE *f;
 
-    assert(import_stack_ptr == 0);
+    if (yyin) fclose( yyin );
+    yy_delete_buffer( YY_CURRENT_BUFFER );
 
     input_name = acf_name;
     acf_name = NULL;
@@ -595,7 +604,6 @@ static void switch_to_acf(void)
     if((f = fopen(name, "r")) == NULL)
         error_loc("Unable to open %s\n", name);
 
-    import_stack[ptr].state = YY_CURRENT_BUFFER;
     yy_switch_to_buffer(yy_create_buffer(f, YY_BUF_SIZE));
 }
 

tools/widl/widl.c
@@ -871,8 +871,6 @@ int main(int argc,char *argv[])
   init_types();
   ret = parser_parse();
 
-  fclose(parser_in);
-
   if(ret) {
     exit(1);
   }

commit 3b12583db0cf68b356f20d7bbd091651af246eff

widl: Always close parsed input file.
Fixes a regression from 9d537999e315a7, which removed closing the file.
This causes Windows widl build to be to remove the temporary file on exit.

tools/widl/parser.h
@@ -42,5 +42,6 @@ int is_warning_enabled(int warning);
 
 extern char *find_input_file( const char *name, const char *parent );
 extern FILE *open_input_file( const char *path );
+extern void close_all_inputs(void);
 
 #endif

tools/widl/parser.l
@@ -481,10 +481,11 @@ static void print_imports(void)
     fprintf( stderr, "%s:%d:\n", state->input_name, state->where.first_line );
 }
 
-void pop_import( struct location *where )
+struct location pop_import(void)
 {
     struct list *entry = list_head( &import_stack );
     struct import_state *state;
+    struct location where;
     assert( entry );
 
     state = LIST_ENTRY( entry, struct import_state, entry );
@@ -496,8 +497,9 @@ void pop_import( struct location *where )
     yy_switch_to_buffer( state->buffer );
 
     input_name = state->input_name;
-    *where = state->where;
+    where = state->where;
     free( state );
+    return where;
 }
 
 void push_import( const char *import_name, struct location *where )
@@ -545,6 +547,12 @@ static void switch_to_acf(void)
     acf_name = NULL;
 
     yy_switch_to_buffer( yy_create_buffer( file, YY_BUF_SIZE ) );
+}
+
+void close_all_inputs(void)
+{
+    while (!list_empty( &import_stack )) pop_import();
+    if (yyin) fclose( yyin );
 }
 
 static void reset_location( struct location *where, const char *input_name )

tools/widl/parser.y
@@ -118,7 +118,7 @@ static typelib_t *current_typelib;
 
 int parser_lex( PARSER_STYPE *yylval, PARSER_LTYPE *yylloc );
 void push_import( const char *fname, PARSER_LTYPE *yylloc );
-void pop_import( PARSER_LTYPE *yylloc );
+PARSER_LTYPE pop_import(void);
 
 # define YYLLOC_DEFAULT( cur, rhs, n ) \
         do { if (n) init_location( &(cur), &YYRHSLOC( rhs, 1 ), &YYRHSLOC( rhs, n ) ); \
@@ -515,7 +515,7 @@ cppquote: tCPPQUOTE '(' aSTRING ')'		{ $$ = $3; }
 
 import_start: tIMPORT aSTRING ';'		{ $$ = $2; push_import( $2, &yylloc ); }
 	;
-import: import_start imp_statements aEOF	{ pop_import( &yylloc ); }
+import: import_start imp_statements aEOF	{ yyloc = pop_import(); }
 	;
 
 importlib: tIMPORTLIB '(' aSTRING ')'

tools/widl/widl.c
@@ -843,10 +843,8 @@ int main(int argc,char *argv[])
 
   init_types();
   ret = parser_parse();
-
-  if(ret) {
-    exit(1);
-  }
+  close_all_inputs();
+  if (ret) exit(1);
 
   /* Everything has been done successfully, don't delete any files.  */
   set_everything(FALSE);