08dae8afcc0ee8d94ed73b1e9fcc7b6fa1b77be9
Alex Henrie <alexhenrie24@gmail.com>
2024-05-13 21:09:25 -0600

    dinput: Don't include every version of DirectInputCreate in every DLL.
    DirectInputCreate(A|W|Ex) is only needed in dinput.dll, and
    DirectInput8Create is only needed in dinput8.dll. Excluding the
    unnecessary functions reduces the size of each DLL by about 4 KB.

--- dlls/dinput/dinput.c
+++ dlls/dinput/dinput.c
@@ -890,6 +890,8 @@ HRESULT WINAPI DllGetClassObject( REFCLSID clsid, REFIID iid, void **out )
     return CLASS_E_CLASSNOTAVAILABLE;
 }
 
+#if DIRECTINPUT_VERSION == 0x0700
+
 HRESULT WINAPI DirectInputCreateEx( HINSTANCE hinst, DWORD version, REFIID iid, void **out, IUnknown *outer )
 {
     IUnknown *unknown;
@@ -919,6 +921,18 @@ HRESULT WINAPI DirectInputCreateEx( HINSTANCE hinst, DWORD version, REFIID iid, 
 
     return DI_OK;
 }
+
+HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateA( HINSTANCE hinst, DWORD version, IDirectInputA **out, IUnknown *outer )
+{
+    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7A, (void **)out, outer );
+}
+
+HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateW( HINSTANCE hinst, DWORD version, IDirectInputW **out, IUnknown *outer )
+{
+    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7W, (void **)out, outer );
+}
+
+#else
 
 HRESULT WINAPI DECLSPEC_HOTPATCH DirectInput8Create( HINSTANCE hinst, DWORD version, REFIID iid, void **out, IUnknown *outer )
 {
@@ -952,12 +966,4 @@ HRESULT WINAPI DECLSPEC_HOTPATCH DirectInput8Create( HINSTANCE hinst, DWORD vers
     return S_OK;
 }
 
-HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateA( HINSTANCE hinst, DWORD version, IDirectInputA **out, IUnknown *outer )
-{
-    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7A, (void **)out, outer );
-}
-
-HRESULT WINAPI DECLSPEC_HOTPATCH DirectInputCreateW( HINSTANCE hinst, DWORD version, IDirectInputW **out, IUnknown *outer )
-{
-    return DirectInputCreateEx( hinst, version, &IID_IDirectInput7W, (void **)out, outer );
-}
+#endif
