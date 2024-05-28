0a42bd290de25d6ff4eb9bf895b74a9003833fa4
Elizabeth Figura <zfigura@codeweavers.com>
2024-05-28 18:50:44 -0500

    ntdll: Use file_complete_async() in serial_DeviceIoControl().
    Thereby also handling completion and APCs.

--- dlls/ntdll/unix/serial.c
+++ dlls/ntdll/unix/serial.c
@@ -1457,11 +1457,7 @@ NTSTATUS serial_DeviceIoControl( HANDLE device, HANDLE event, PIO_APC_ROUTINE ap
     if (needs_close) close( fd );
 
     if (!NT_ERROR(status))
-    {
-        io->Status = status;
-        io->Information = sz;
-        if (event) NtSetEvent(event, NULL);
-    }
+        file_complete_async( device, event, apc, apc_user, io, status, sz );
     return status;
 }
 
