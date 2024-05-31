6f927b9e93f164c973c46200452109abe2aa79b4
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2024-05-31 18:32:14 +1000

    include: Add missing TreeView_* defines.

--- include/commctrl.h
+++ include/commctrl.h
@@ -3022,6 +3022,8 @@ typedef struct tagTVKEYDOWN
 #define TreeView_GetCheckState(hwndTV, hti) \
    ((((UINT)(SNDMSG((hwndTV), TVM_GETITEMSTATE, (WPARAM)(hti),  \
                      TVIS_STATEIMAGEMASK))) >> 12) -1)
+#define TreeView_SetCheckState(hwndTV, hti, check) \
+    TreeView_SetItemState(hwndTV, hti, INDEXTOSTATEIMAGEMASK((check) ? 2 : 1), TVIS_STATEIMAGEMASK)
 
 #define TreeView_SetLineColor(hwnd, clr) \
     (COLORREF)SNDMSG((hwnd), TVM_SETLINECOLOR, 0, (LPARAM)(clr))
@@ -3039,6 +3041,34 @@ typedef struct tagTVKEYDOWN
     (BOOL)SNDMSG((hwnd), TVM_SETUNICODEFORMAT, (WPARAM)(fUnicode), 0)
 #define TreeView_GetUnicodeFormat(hwnd) \
     (BOOL)SNDMSG((hwnd), TVM_GETUNICODEFORMAT, 0, 0)
+
+#define TreeView_SetExtendedStyle(hwnd, style, mask) \
+    (DWORD)SNDMSG((hwnd), TVM_SETEXTENDEDSTYLE, mask, style)
+
+#define TreeView_GetExtendedStyle(hwnd) \
+    (DWORD)SNDMSG((hwnd), TVM_GETEXTENDEDSTYLE, 0, 0)
+
+#define TreeView_SetAutoScrollInfo(hwnd, pps, updatetime) \
+    SNDMSG((hwnd), TVM_SETAUTOSCROLLINFO, (WPARAM)(pps), (LPARAM)(updatetime))
+
+#define TreeView_SetHot(hwnd, hitem) \
+    SNDMSG((hwnd), TVM_SETHOT, 0, (LPARAM)(hitem))
+
+#define TreeView_GetSelectedCount(hwnd) \
+    (DWORD)SNDMSG((hwnd), TVM_GETSELECTEDCOUNT, 0, 0)
+
+#define TreeView_ShowInfoTip(hwnd, hitem) \
+    (DWORD)SNDMSG((hwnd), TVM_SHOWINFOTIP, 0, (LPARAM)(hitem))
+
+#define TreeView_GetItemPartRect(hwnd, hitem, rect, part) \
+{ \
+    TVGETITEMPARTRECTINFO info; \
+    info.hti = (hitem); \
+    info.prc = (rect); \
+    info.partID = (part); \
+    SNDMSG((hwnd), TVM_GETITEMPARTRECT, 0, (LPARAM)&info); \
+}
+
 
 /* Listview control */
 
