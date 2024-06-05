0da33d5493814b8cb8309c092bd7c6e1c60f1868
Nikolay Sivov <nsivov@codeweavers.com>
2024-06-05 11:52:49 +0200

    scrrun/dictionary: Implement putref_Item() method.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=56781
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

--- dlls/scrrun/dictionary.c
+++ dlls/scrrun/dictionary.c
@@ -559,11 +559,17 @@ static HRESULT WINAPI dictionary_Invoke(IDictionary *iface, DISPID dispIdMember,
     return hr;
 }
 
-static HRESULT WINAPI dictionary_putref_Item(IDictionary *iface, VARIANT *Key, VARIANT *pRetItem)
+static HRESULT WINAPI dictionary_putref_Item(IDictionary *iface, VARIANT *key, VARIANT *item)
 {
-    FIXME("%p, %p, %p stub\n", iface, Key, pRetItem);
+    struct dictionary *dictionary = impl_from_IDictionary(iface);
+    struct keyitem_pair *pair;
 
-    return E_NOTIMPL;
+    TRACE("%p, %s, %s.\n", iface, debugstr_variant(key), debugstr_variant(item));
+
+    if ((pair = get_keyitem_pair(dictionary, key)))
+        return VariantCopyInd(&pair->item, item);
+
+    return add_keyitem_pair(dictionary, key, item);
 }
 
 static HRESULT WINAPI dictionary_put_Item(IDictionary *iface, VARIANT *key, VARIANT *item)
@@ -576,7 +582,7 @@ static HRESULT WINAPI dictionary_put_Item(IDictionary *iface, VARIANT *key, VARI
     if ((pair = get_keyitem_pair(dictionary, key)))
         return VariantCopyInd(&pair->item, item);
 
-    return IDictionary_Add(iface, key, item);
+    return add_keyitem_pair(dictionary, key, item);
 }
 
 static HRESULT WINAPI dictionary_get_Item(IDictionary *iface, VARIANT *key, VARIANT *item)
