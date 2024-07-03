2186b8f84183da1f96da08be63036df38c9ddeb8
Marc-Aurel Zent <mzent@codeweavers.com>
2024-07-03 20:48:45 +0200

    server: Implement apply_thread_priority on macOS for application priorities.

--- server/thread.c
+++ server/thread.c
@@ -40,6 +40,11 @@
 #ifdef HAVE_SYS_RESOURCE_H
 #include <sys/resource.h>
 #endif
+#ifdef __APPLE__
+#include <mach/mach_init.h>
+#include <mach/mach_port.h>
+#include <mach/thread_act.h>
+#endif
 
 #include "ntstatus.h"
 #define WIN32_NO_STATUS
@@ -250,6 +255,79 @@ static void apply_thread_priority( struct thread *thread, int base_priority )
     /* map an NT application band [1,15] base priority to [-nice_limit, nice_limit] */
     niceness = (min + (base_priority - 1) * range / 14);
     setpriority( PRIO_PROCESS, thread->unix_tid, niceness );
+}
+
+#elif defined(__APPLE__)
+
+void init_threading(void)
+{
+}
+
+static int get_mach_importance( int base_priority )
+{
+    int min = -31, max = 32, range = max - min;
+    return min + (base_priority - 1) * range / 14;
+}
+
+static void apply_thread_priority( struct thread *thread, int base_priority )
+{
+    kern_return_t kr;
+    mach_msg_type_name_t type;
+    int throughput_qos, latency_qos;
+    struct thread_extended_policy thread_extended_policy;
+    struct thread_precedence_policy thread_precedence_policy;
+    mach_port_t thread_port, process_port = thread->process->trace_data;
+
+    if (!process_port) return;
+    kr = mach_port_extract_right( process_port, thread->unix_tid,
+                                  MACH_MSG_TYPE_COPY_SEND, &thread_port, &type );
+    if (kr != KERN_SUCCESS) return;
+    /* base priority 15 is for time-critical threads, so not compute-bound */
+    thread_extended_policy.timeshare = base_priority > 14 ? 0 : 1;
+    thread_precedence_policy.importance = get_mach_importance( base_priority );
+    /* adapted from the QoS table at xnu/osfmk/kern/thread_policy.c */
+    switch (thread->priority)
+    {
+    case THREAD_PRIORITY_IDLE: /* THREAD_QOS_MAINTENANCE */
+    case THREAD_PRIORITY_LOWEST: /* THREAD_QOS_BACKGROUND */
+        throughput_qos = THROUGHPUT_QOS_TIER_5;
+        latency_qos = LATENCY_QOS_TIER_3;
+        break;
+    case THREAD_PRIORITY_BELOW_NORMAL: /* THREAD_QOS_UTILITY */
+        throughput_qos = THROUGHPUT_QOS_TIER_2;
+        latency_qos = LATENCY_QOS_TIER_3;
+        break;
+    case THREAD_PRIORITY_NORMAL: /* THREAD_QOS_LEGACY */
+    case THREAD_PRIORITY_ABOVE_NORMAL: /* THREAD_QOS_USER_INITIATED */
+        throughput_qos = THROUGHPUT_QOS_TIER_1;
+        latency_qos = LATENCY_QOS_TIER_1;
+        break;
+    case THREAD_PRIORITY_HIGHEST: /* THREAD_QOS_USER_INTERACTIVE */
+        throughput_qos = THROUGHPUT_QOS_TIER_0;
+        latency_qos = LATENCY_QOS_TIER_0;
+        break;
+    case THREAD_PRIORITY_TIME_CRITICAL:
+    default: /* THREAD_QOS_UNSPECIFIED */
+        throughput_qos = THROUGHPUT_QOS_TIER_UNSPECIFIED;
+        latency_qos = LATENCY_QOS_TIER_UNSPECIFIED;
+        break;
+    }
+    /* QOS_UNSPECIFIED is assigned the highest tier available, so it does not provide a limit */
+    if (base_priority > THREAD_BASE_PRIORITY_LOWRT)
+    {
+        throughput_qos = THROUGHPUT_QOS_TIER_UNSPECIFIED;
+        latency_qos = LATENCY_QOS_TIER_UNSPECIFIED;
+    }
+
+    thread_policy_set( thread_port, THREAD_LATENCY_QOS_POLICY, (thread_policy_t)&latency_qos,
+                       THREAD_LATENCY_QOS_POLICY_COUNT );
+    thread_policy_set( thread_port, THREAD_THROUGHPUT_QOS_POLICY, (thread_policy_t)&throughput_qos,
+                       THREAD_THROUGHPUT_QOS_POLICY_COUNT );
+    thread_policy_set( thread_port, THREAD_EXTENDED_POLICY, (thread_policy_t)&thread_extended_policy,
+                       THREAD_EXTENDED_POLICY_COUNT );
+    thread_policy_set( thread_port, THREAD_PRECEDENCE_POLICY, (thread_policy_t)&thread_precedence_policy,
+                       THREAD_PRECEDENCE_POLICY_COUNT );
+    mach_port_deallocate( mach_task_self(), thread_port );
 }
 
 #else
