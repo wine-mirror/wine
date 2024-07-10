05e2f028e09c84cb7aa4cbaf54e753985a2b27fc
Paul Gofman <pgofman@codeweavers.com>
2024-07-10 16:41:20 -0600

    ntoskrnl.exe/tests: Open directory object with nonzero access in test_permanent().

--- dlls/ntoskrnl.exe/tests/driver.c
+++ dlls/ntoskrnl.exe/tests/driver.c
@@ -2289,7 +2289,7 @@ static void test_permanence(void)
     ok(!status, "got %#lx\n", status);
 
     attr.Attributes = 0;
-    status = ZwOpenDirectoryObject( &handle, 0, &attr );
+    status = ZwOpenDirectoryObject( &handle, DIRECTORY_ALL_ACCESS, &attr );
     ok(!status, "got %#lx\n", status);
     status = ZwMakeTemporaryObject( handle );
     todo_wine
@@ -2307,7 +2307,7 @@ static void test_permanence(void)
     todo_wine
     ok(!status, "got %#lx\n", status);
     attr.Attributes = OBJ_PERMANENT;
-    status = ZwOpenDirectoryObject( &handle2, 0, &attr );
+    status = ZwOpenDirectoryObject( &handle2, DIRECTORY_ALL_ACCESS, &attr );
     ok(status == STATUS_SUCCESS, "got %#lx\n", status);
     status = ZwClose( handle2 );
     ok(!status, "got %#lx\n", status);
