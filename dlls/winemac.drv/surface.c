3274763e23eeef4fe6b6d6bfb357a189ee628ea2
Rémi Bernon <rbernon@codeweavers.com>
2024-07-17 09:09:06 +0200

    winemac: Rely on win32u previous surface reuse.

--- dlls/winemac.drv/surface.c
+++ dlls/winemac.drv/surface.c
@@ -216,29 +216,17 @@ failed:
  */
 BOOL macdrv_CreateWindowSurface(HWND hwnd, const RECT *surface_rect, struct window_surface **surface)
 {
+    struct window_surface *previous;
     struct macdrv_win_data *data;
 
     TRACE("hwnd %p, surface_rect %s, surface %p\n", hwnd, wine_dbgstr_rect(surface_rect), surface);
 
+    if ((previous = *surface) && previous->funcs == &macdrv_surface_funcs) return TRUE;
     if (!(data = get_win_data(hwnd))) return TRUE; /* use default surface */
-
-    if (*surface) window_surface_release(*surface);
-    *surface = NULL;
-
-    if (data->surface)
-    {
-        if (EqualRect(&data->surface->rect, surface_rect))
-        {
-            /* existing surface is good enough */
-            window_surface_add_ref(data->surface);
-            *surface = data->surface;
-            goto done;
-        }
-    }
+    if (previous) window_surface_release(previous);
 
     *surface = create_surface(data->hwnd, data->cocoa_window, surface_rect);
 
-done:
     release_win_data(data);
     return TRUE;
 }
