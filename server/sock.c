wine-mirror/wine

commit 89f77a93a6c96896d90c943bc3e82139114cf58c

server: Mark the socket as cacheable when it is connected, marked listening, or created as connectionless.
Signed-off-by: Zebediah Figura <z.figura12@gmail.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

server/sock.c
@@ -416,7 +416,6 @@ static int sock_reselect( struct sock *sock )
         if (!(sock->state & ~FD_WINE_NONBLOCKING)) return 0;
         /* ok, it is, attach it to the wineserver's main poll loop */
         sock->polling = 1;
-        allow_fd_caching( sock->fd );
     }
     /* update condition mask */
     set_fd_events( sock->fd, ev );
@@ -1358,7 +1357,11 @@ static int init_socket( struct sock *sock, int family, int type, int protocol, u
     {
         return -1;
     }
-    sock_reselect( sock );
+
+    /* We can't immediately allow caching for a connection-mode socket, since it
+     * might be accepted into (changing the underlying fd object.) */
+    if (sock->type != WS_SOCK_STREAM) allow_fd_caching( sock->fd );
+
     return 0;
 }
 
@@ -1750,6 +1753,9 @@ static int sock_ioctl( struct fd *fd, ioctl_code_t code, struct async *async )
         sock->state |= FD_WINE_LISTENING;
         sock->state &= ~(FD_CONNECT | FD_WINE_CONNECTED);
 
+        /* a listening socket can no longer be accepted into */
+        allow_fd_caching( sock->fd );
+
         /* we may already be selecting for FD_ACCEPT */
         sock_reselect( sock );
         return 0;
@@ -1789,6 +1795,9 @@ static int sock_ioctl( struct fd *fd, ioctl_code_t code, struct async *async )
             set_error( sock_get_ntstatus( errno ) );
             return 0;
         }
+
+        /* a connected or connecting socket can no longer be accepted into */
+        allow_fd_caching( sock->fd );
 
         sock->pending_events &= ~(FD_CONNECT | FD_READ | FD_WRITE);
         sock->reported_events &= ~(FD_CONNECT | FD_READ | FD_WRITE);

commit 863f12c329fe3fe5285957e23446887d85e111c5

server: Mark the socket as cacheable when it is an accepted or accepted-into socket.
Fixes: 89f77a93a6c96896d90c943bc3e82139114cf58c
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=56977

server/sock.c
@@ -2045,6 +2045,7 @@ static struct sock *accept_socket( struct sock *sock )
             release_object( acceptsock );
             return NULL;
         }
+        allow_fd_caching( acceptsock->fd );
         unix_len = sizeof(unix_addr);
         if (!getsockname( acceptfd, &unix_addr.addr, &unix_len ))
         {
@@ -2093,6 +2094,7 @@ static int accept_into_socket( struct sock *sock, struct sock *acceptsock )
                                             get_fd_options( acceptsock->fd ) )))
             return FALSE;
     }
+    allow_fd_caching( newfd );
 
     acceptsock->state = SOCK_CONNECTED;
     acceptsock->bound = 1;