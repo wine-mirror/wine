b9879d5adc1cd98a6ceb7d087c4533fdd487bedb
Rémi Bernon <rbernon@codeweavers.com>
2024-08-09 13:36:26 +0200

    winewayland: Remove now unnecessary WindowPosChanging checks.

--- dlls/winewayland.drv/window.c
+++ dlls/winewayland.drv/window.c
@@ -431,23 +431,15 @@ BOOL WAYLAND_WindowPosChanging(HWND hwnd, UINT swp_flags, BOOL shaped, const REC
                                const RECT *client_rect, RECT *visible_rect)
 {
     struct wayland_win_data *data = wayland_win_data_get(hwnd);
-    HWND parent;
-    BOOL ret = FALSE;
 
     TRACE("hwnd %p, swp_flags %04x, shaped %u, window_rect %s, client_rect %s, visible_rect %s\n",
           hwnd, swp_flags, shaped, wine_dbgstr_rect(window_rect), wine_dbgstr_rect(client_rect),
           wine_dbgstr_rect(visible_rect));
 
     if (!data && !(data = wayland_win_data_create(hwnd, window_rect, client_rect))) return FALSE; /* use default surface */
-
-    parent = NtUserGetAncestor(hwnd, GA_PARENT);
-    if ((parent && parent != NtUserGetDesktopWindow())) goto done; /* use default surface */
-
-    ret = TRUE;
-
-done:
     wayland_win_data_release(data);
-    return ret;
+
+    return TRUE;
 }
 
 
