7f3bc13fbb2d6f56d1044d890fb8c8f4e6e56520
Esme Povirk <esme@codeweavers.com>
2024-08-13 20:11:15 +0000

    user32: Implement EVENT_SYSTEM_DIALOGEND.

--- dlls/user32/dialog.c
+++ dlls/user32/dialog.c
@@ -825,6 +825,7 @@ INT DIALOG_DoDialogBox( HWND hwnd, HWND owner )
         }
     }
     retval = dlgInfo->idResult;
+    NtUserNotifyWinEvent( EVENT_SYSTEM_DIALOGEND, hwnd, OBJID_WINDOW, 0 );
     NtUserDestroyWindow( hwnd );
     return retval;
 }
