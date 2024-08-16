e2f6aa3874e25d7b225ae00df322a44f2fbc2820
Louis Lenders <xerox.xerox2000x@gmail.com>
2024-08-16 22:49:19 +0200

    kernel32: Fake success in PowerSetRequest.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=48521

--- dlls/kernel32/powermgnt.c
+++ dlls/kernel32/powermgnt.c
@@ -156,8 +156,7 @@ BOOL WINAPI PowerSetRequest(HANDLE request, POWER_REQUEST_TYPE type)
 {
     FIXME("(%p, %u): stub\n", request, type);
 
-    SetLastError(ERROR_CALL_NOT_IMPLEMENTED);
-    return FALSE;
+    return TRUE;
 }
 
 /***********************************************************************
