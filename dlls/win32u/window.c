wine-mirror/wine

commit cb1c03b926ca01619be02f835f382c1dd7d4a478

win32u: Move visible rect computation out of the drivers.
Using a new GetWindowStyleMasks driver entry to get the style masks to
be used to mask out non-client decorations which are hidden by the host
decorations.

dlls/win32u/window.c
@@ -1818,6 +1818,29 @@ done:
 }
 
 
+static RECT get_visible_rect( HWND hwnd, BOOL shaped, UINT style, UINT ex_style, const struct window_rects *rects )
+{
+    UINT dpi = get_dpi_for_window( hwnd ), style_mask, ex_style_mask;
+    RECT visible_rect, rect = {0};
+
+    if (IsRectEmpty( &rects->window ) || EqualRect( &rects->window, &rects->client ) || shaped) return rects->window;
+    if (!user_driver->pGetWindowStyleMasks( hwnd, style, ex_style, &style_mask, &ex_style_mask )) return rects->window;
+    if (!NtUserAdjustWindowRect( &rect, style & style_mask, FALSE, ex_style & ex_style_mask, dpi )) return rects->window;
+
+    visible_rect = rects->window;
+    visible_rect.left   -= rect.left;
+    visible_rect.right  -= rect.right;
+    visible_rect.top    -= rect.top;
+    visible_rect.bottom -= rect.bottom;
+    if (visible_rect.top >= visible_rect.bottom) visible_rect.bottom = visible_rect.top + 1;
+    if (visible_rect.left >= visible_rect.right) visible_rect.right = visible_rect.left + 1;
+
+    TRACE( "hwnd %p, rects %s, style %#x, ex_style %#x -> visible_rect %s\n", hwnd,
+           debugstr_window_rects( rects ), style, ex_style, wine_dbgstr_rect( &visible_rect ) );
+    return visible_rect;
+}
+
+
 static BOOL get_surface_rect( const RECT *visible_rect, RECT *surface_rect )
 {
     RECT virtual_rect = NtUserGetVirtualScreenRect();
@@ -1874,8 +1897,12 @@ static struct window_surface *create_window_surface( HWND hwnd, UINT swp_flags, 
     BOOL shaped, needs_surface, create_opaque, is_layered;
     HWND parent = NtUserGetAncestor( hwnd, GA_PARENT );
     struct window_surface *new_surface;
+    UINT style, ex_style;
     RECT dummy;
     HRGN shape;
+
+    style = NtUserGetWindowLongW( hwnd, GWL_STYLE );
+    ex_style = NtUserGetWindowLongW( hwnd, GWL_EXSTYLE );
 
     if (get_window_region( hwnd, FALSE, &shape, &dummy )) shaped = FALSE;
     else if ((shaped = !!shape)) NtGdiDeleteObjectApp( shape );
@@ -1885,8 +1912,9 @@ static struct window_surface *create_window_surface( HWND hwnd, UINT swp_flags, 
     else if (parent && parent != NtUserGetDesktopWindow()) needs_surface = FALSE;
     else if (swp_flags & SWP_HIDEWINDOW) needs_surface = FALSE;
     else if (swp_flags & SWP_SHOWWINDOW) needs_surface = TRUE;
-    else needs_surface = !!(NtUserGetWindowLongW( hwnd, GWL_STYLE ) & WS_VISIBLE);
+    else needs_surface = !!(style & WS_VISIBLE);
 
+    rects->visible = get_visible_rect( hwnd, shaped, style, ex_style, rects );
     if (!get_surface_rect( &rects->visible, surface_rect )) needs_surface = FALSE;
     if (!get_default_window_surface( hwnd, surface_rect, &new_surface )) return NULL;
 

dlls/wineandroid.drv/window.c
@@ -1023,7 +1023,7 @@ static struct android_win_data *create_win_data( HWND hwnd, const struct window_
 /***********************************************************************
  *           ANDROID_WindowPosChanging
  */
-BOOL ANDROID_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, struct window_rects *rects )
+BOOL ANDROID_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, const struct window_rects *rects )
 {
     struct android_win_data *data = get_win_data( hwnd );
 

dlls/winemac.drv/window.c
@@ -74,24 +74,16 @@ static void set_internal_window_pos(HWND hwnd, UINT cmd, RECT *rect, POINT *pt)
 }
 
 
-/***********************************************************************
- *              get_cocoa_window_features
- */
-static struct macdrv_window_features get_cocoa_window_features(struct macdrv_win_data *data, DWORD style, DWORD ex_style,
-                                                               const RECT *window_rect, const RECT *client_rect)
+static struct macdrv_window_features get_window_features_for_style(DWORD style, DWORD ex_style, BOOL shaped)
 {
     struct macdrv_window_features wf = {0};
 
     if (ex_style & WS_EX_NOACTIVATE) wf.prevents_app_activation = TRUE;
 
-    if (disable_window_decorations) return wf;
-    if (IsRectEmpty(window_rect)) return wf;
-    if (EqualRect(window_rect, client_rect)) return wf;
-
     if ((style & WS_CAPTION) == WS_CAPTION && !(ex_style & WS_EX_LAYERED))
     {
         wf.shadow = TRUE;
-        if (!data->shaped)
+        if (!shaped)
         {
             wf.title_bar = TRUE;
             if (style & WS_SYSMENU) wf.close_button = TRUE;
@@ -103,12 +95,25 @@ static struct macdrv_window_features get_cocoa_window_features(struct macdrv_win
     if (style & WS_THICKFRAME)
     {
         wf.shadow = TRUE;
-        if (!data->shaped) wf.resizable = TRUE;
+        if (!shaped) wf.resizable = TRUE;
     }
     else if (ex_style & WS_EX_DLGMODALFRAME) wf.shadow = TRUE;
     else if ((style & (WS_DLGFRAME|WS_BORDER)) == WS_DLGFRAME) wf.shadow = TRUE;
 
     return wf;
+}
+
+/***********************************************************************
+ *              get_cocoa_window_features
+ */
+static struct macdrv_window_features get_cocoa_window_features(struct macdrv_win_data *data, DWORD style, DWORD ex_style)
+{
+    struct macdrv_window_features wf = {0};
+
+    if (ex_style & WS_EX_NOACTIVATE) wf.prevents_app_activation = TRUE;
+    if (EqualRect(&data->rects.window, &data->rects.visible)) return wf;
+
+    return get_window_features_for_style(style, ex_style, data->shaped);
 }
 
 
@@ -148,69 +153,6 @@ static void get_cocoa_window_state(struct macdrv_win_data *data,
     state->minimized = (style & WS_MINIMIZE) != 0;
     state->minimized_valid = state->minimized != data->minimized;
     state->maximized = (style & WS_MAXIMIZE) != 0;
-}
-
-
-/***********************************************************************
- *              get_mac_rect_offset
- *
- * Helper for macdrv_window_to_mac_rect.
- */
-static void get_mac_rect_offset(struct macdrv_win_data *data, unsigned int style, RECT *rect,
-                                const RECT *window_rect, const RECT *client_rect)
-{
-    unsigned int ex_style, style_mask = 0, ex_style_mask = 0;
-    UINT dpi = get_win_monitor_dpi(data->hwnd);
-
-    rect->top = rect->bottom = rect->left = rect->right = 0;
-
-    ex_style = NtUserGetWindowLongW(data->hwnd, GWL_EXSTYLE);
-
-    if (!data->shaped)
-    {
-        struct macdrv_window_features wf;
-        wf = get_cocoa_window_features(data, style, ex_style, window_rect, client_rect);
-
-        if (wf.title_bar)
-        {
-            style_mask |= WS_CAPTION;
-            ex_style_mask |= WS_EX_TOOLWINDOW;
-        }
-        if (wf.shadow)
-        {
-            style_mask |= WS_DLGFRAME | WS_THICKFRAME;
-            ex_style_mask |= WS_EX_DLGMODALFRAME;
-        }
-    }
-
-    NtUserAdjustWindowRect(rect, style & style_mask, FALSE, ex_style & ex_style_mask, dpi);
-
-    TRACE("%p/%p style %08x ex_style %08x shaped %d -> %s\n", data->hwnd, data->cocoa_window,
-          style, ex_style, data->shaped, wine_dbgstr_rect(rect));
-}
-
-
-/***********************************************************************
- *              macdrv_window_to_mac_rect
- *
- * Convert a rect from client to Mac window coordinates
- */
-static void macdrv_window_to_mac_rect(struct macdrv_win_data *data, DWORD style, RECT *rect,
-                                      const RECT *window_rect, const RECT *client_rect)
-{
-    RECT rc;
-
-    if ((style & (WS_POPUP|WS_CHILD)) == WS_CHILD) return;
-    if (IsRectEmpty(rect)) return;
-
-    get_mac_rect_offset(data, style, &rc, window_rect, client_rect);
-
-    rect->left   -= rc.left;
-    rect->right  -= rc.right;
-    rect->top    -= rc.top;
-    rect->bottom -= rc.bottom;
-    if (rect->top >= rect->bottom) rect->bottom = rect->top + 1;
-    if (rect->left >= rect->right) rect->right = rect->left + 1;
 }
 
 
@@ -361,7 +303,7 @@ static void set_cocoa_window_properties(struct macdrv_win_data *data)
     owner_win = macdrv_get_cocoa_window(owner, TRUE);
     macdrv_set_cocoa_parent_window(data->cocoa_window, owner_win);
 
-    wf = get_cocoa_window_features(data, style, ex_style, &data->rects.window, &data->rects.client);
+    wf = get_cocoa_window_features(data, style, ex_style);
     macdrv_set_cocoa_window_features(data->cocoa_window, &wf);
 
     get_cocoa_window_state(data, style, ex_style, &state);
@@ -600,7 +542,7 @@ static void create_cocoa_window(struct macdrv_win_data *data)
     style = NtUserGetWindowLongW(data->hwnd, GWL_STYLE);
     ex_style = NtUserGetWindowLongW(data->hwnd, GWL_EXSTYLE);
 
-    wf = get_cocoa_window_features(data, style, ex_style, &data->rects.window, &data->rects.client);
+    wf = get_cocoa_window_features(data, style, ex_style);
 
     frame = cgrect_from_rect(data->rects.visible);
     constrain_window_frame(&frame.origin, &frame.size);
@@ -1803,19 +1745,15 @@ LRESULT macdrv_WindowMessage(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp)
 /***********************************************************************
  *              WindowPosChanging   (MACDRV.@)
  */
-BOOL macdrv_WindowPosChanging(HWND hwnd, UINT swp_flags, BOOL shaped, struct window_rects *rects)
+BOOL macdrv_WindowPosChanging(HWND hwnd, UINT swp_flags, BOOL shaped, const struct window_rects *rects)
 {
     struct macdrv_win_data *data = get_win_data(hwnd);
-    DWORD style = NtUserGetWindowLongW(hwnd, GWL_STYLE);
     BOOL ret = FALSE;
 
     TRACE("hwnd %p, swp_flags %04x, shaped %u, rects %s\n", hwnd, swp_flags, shaped, debugstr_window_rects(rects));
 
     if (!data && !(data = macdrv_create_win_data(hwnd, rects))) return FALSE; /* use default surface */
     data->shaped = shaped;
-
-    macdrv_window_to_mac_rect(data, style, &rects->visible, &rects->window, &rects->client);
-    TRACE("-> %s\n", debugstr_window_rects(rects));
 
     ret = !!data->cocoa_window; /* use default surface if we don't have a window */
     release_win_data(data);
@@ -1852,6 +1790,31 @@ void macdrv_MoveWindowBits(HWND hwnd, const struct window_rects *new_rects, cons
                          &old_client_rect, &new_rects->client, &new_rects->window);
     }
 }
+
+/***********************************************************************
+ *      GetWindowStyleMasks   (X11DRV.@)
+ */
+BOOL macdrv_GetWindowStyleMasks(HWND hwnd, UINT style, UINT ex_style, UINT *style_mask, UINT *ex_style_mask)
+{
+    struct macdrv_window_features wf = get_window_features_for_style(style, ex_style, FALSE);
+
+    if (disable_window_decorations) return FALSE;
+
+    *style_mask = ex_style = 0;
+    if (wf.title_bar)
+    {
+        *style_mask |= WS_CAPTION;
+        *ex_style_mask |= WS_EX_TOOLWINDOW;
+    }
+    if (wf.shadow)
+    {
+        *style_mask |= WS_DLGFRAME | WS_THICKFRAME;
+        *ex_style_mask |= WS_EX_DLGMODALFRAME;
+    }
+
+    return TRUE;
+}
+
 
 /***********************************************************************
  *              WindowPosChanged   (MACDRV.@)

dlls/winewayland.drv/window.c
@@ -425,7 +425,7 @@ void WAYLAND_DestroyWindow(HWND hwnd)
 /***********************************************************************
  *           WAYLAND_WindowPosChanging
  */
-BOOL WAYLAND_WindowPosChanging(HWND hwnd, UINT swp_flags, BOOL shaped, struct window_rects *rects)
+BOOL WAYLAND_WindowPosChanging(HWND hwnd, UINT swp_flags, BOOL shaped, const struct window_rects *rects)
 {
     struct wayland_win_data *data = wayland_win_data_get(hwnd);
 

dlls/winex11.drv/window.c
@@ -303,18 +303,9 @@ static inline BOOL is_window_resizable( struct x11drv_win_data *data, DWORD styl
 /***********************************************************************
  *              get_mwm_decorations
  */
-static unsigned long get_mwm_decorations( struct x11drv_win_data *data,
-                                          DWORD style, DWORD ex_style,
-                                          const RECT *window_rect,
-                                          const RECT *client_rect )
+static unsigned long get_mwm_decorations_for_style( DWORD style, DWORD ex_style )
 {
     unsigned long ret = 0;
-
-    if (!decorated_mode) return 0;
-
-    if (EqualRect( window_rect, client_rect )) return 0;
-    if (IsRectEmpty( window_rect )) return 0;
-    if (data->shaped) return 0;
 
     if (ex_style & WS_EX_TOOLWINDOW) return 0;
     if (ex_style & WS_EX_LAYERED) return 0;
@@ -330,6 +321,16 @@ static unsigned long get_mwm_decorations( struct x11drv_win_data *data,
     else if (style & WS_THICKFRAME) ret |= MWM_DECOR_BORDER | MWM_DECOR_RESIZEH;
     else if ((style & (WS_DLGFRAME|WS_BORDER)) == WS_DLGFRAME) ret |= MWM_DECOR_BORDER;
     return ret;
+}
+
+
+/***********************************************************************
+ *              get_mwm_decorations
+ */
+static unsigned long get_mwm_decorations( struct x11drv_win_data *data, DWORD style, DWORD ex_style )
+{
+    if (EqualRect( &data->rects.window, &data->rects.visible )) return 0;
+    return get_mwm_decorations_for_style( style, ex_style );
 }
 
 
@@ -776,7 +777,7 @@ static void set_mwm_hints( struct x11drv_win_data *data, UINT style, UINT ex_sty
     }
     else
     {
-        mwm_hints.decorations = get_mwm_decorations( data, style, ex_style, &data->rects.window, &data->rects.client );
+        mwm_hints.decorations = get_mwm_decorations( data, style, ex_style );
         mwm_hints.functions = MWM_FUNC_MOVE;
         if (is_window_resizable( data, style )) mwm_hints.functions |= MWM_FUNC_RESIZE;
         if (!(style & WS_DISABLED))
@@ -1305,57 +1306,6 @@ void make_window_embedded( struct x11drv_win_data *data )
     data->managed = TRUE;
     sync_window_style( data );
     set_xembed_flags( data, (data->mapped || data->embedder) ? XEMBED_MAPPED : 0 );
-}
-
-
-/***********************************************************************
- *     get_decoration_rect
- */
-static void get_decoration_rect( struct x11drv_win_data *data, RECT *rect,
-                                 const RECT *window_rect, const RECT *client_rect )
-{
-    DWORD style, ex_style, style_mask = 0, ex_style_mask = 0;
-    unsigned long decor;
-    UINT dpi;
-
-    SetRectEmpty( rect );
-    if (!data->managed) return;
-
-    dpi = get_win_monitor_dpi( data->hwnd );
-    style = NtUserGetWindowLongW( data->hwnd, GWL_STYLE );
-    ex_style = NtUserGetWindowLongW( data->hwnd, GWL_EXSTYLE );
-    decor = get_mwm_decorations( data, style, ex_style, window_rect, client_rect );
-
-    if (decor & MWM_DECOR_TITLE) style_mask |= WS_CAPTION;
-    if (decor & MWM_DECOR_BORDER)
-    {
-        style_mask |= WS_DLGFRAME | WS_THICKFRAME;
-        ex_style_mask |= WS_EX_DLGMODALFRAME;
-    }
-
-    NtUserAdjustWindowRect( rect, style & style_mask, FALSE, ex_style & ex_style_mask, dpi );
-}
-
-
-/***********************************************************************
- *		X11DRV_window_to_X_rect
- *
- * Convert a rect from client to X window coordinates
- */
-static void X11DRV_window_to_X_rect( struct x11drv_win_data *data, RECT *rect,
-                                     const RECT *window_rect, const RECT *client_rect )
-{
-    RECT rc;
-
-    if (IsRectEmpty( rect )) return;
-
-    get_decoration_rect( data, &rc, window_rect, client_rect );
-    rect->left   -= rc.left;
-    rect->right  -= rc.right;
-    rect->top    -= rc.top;
-    rect->bottom -= rc.bottom;
-    if (rect->top >= rect->bottom) rect->bottom = rect->top + 1;
-    if (rect->left >= rect->right) rect->right = rect->left + 1;
 }
 
 
@@ -2543,7 +2493,7 @@ done:
 /***********************************************************************
  *		WindowPosChanging   (X11DRV.@)
  */
-BOOL X11DRV_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, struct window_rects *rects )
+BOOL X11DRV_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, const struct window_rects *rects )
 {
     struct x11drv_win_data *data = get_win_data( hwnd );
     BOOL ret = FALSE;
@@ -2562,9 +2512,6 @@ BOOL X11DRV_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, struct wi
         if (!(data = get_win_data( hwnd ))) return FALSE; /* use default surface */
         data->managed = TRUE;
     }
-
-    X11DRV_window_to_X_rect( data, &rects->visible, &rects->window, &rects->client );
-    TRACE( "-> %s\n", debugstr_window_rects(rects) );
 
     ret = !!data->whole_window; /* use default surface if we don't have a window */
     release_win_data( data );
@@ -2601,6 +2548,34 @@ void X11DRV_MoveWindowBits( HWND hwnd, const struct window_rects *new_rects, con
                           &old_client_rect, &new_rects->client, &new_rects->window );
     }
 }
+
+/***********************************************************************
+ *      GetWindowStyleMasks   (X11DRV.@)
+ */
+BOOL X11DRV_GetWindowStyleMasks( HWND hwnd, UINT style, UINT ex_style, UINT *style_mask, UINT *ex_style_mask )
+{
+    unsigned long decor = get_mwm_decorations_for_style( style, ex_style );
+    struct x11drv_win_data *data;
+
+    if (!decorated_mode) return FALSE;
+
+    if ((data = get_win_data( hwnd )))
+    {
+        if (!data->managed) decor = 0;
+        release_win_data( data );
+    }
+
+    *style_mask = ex_style = 0;
+    if (decor & MWM_DECOR_TITLE) *style_mask |= WS_CAPTION;
+    if (decor & MWM_DECOR_BORDER)
+    {
+        *style_mask |= WS_DLGFRAME | WS_THICKFRAME;
+        *ex_style_mask |= WS_EX_DLGMODALFRAME;
+    }
+
+    return TRUE;
+}
+
 
 /***********************************************************************
  *		WindowPosChanged   (X11DRV.@)

dlls/win32u/driver.c
@@ -881,9 +881,14 @@ static LRESULT nulldrv_WindowMessage( HWND hwnd, UINT msg, WPARAM wparam, LPARAM
     return 0;
 }
 
-static BOOL nulldrv_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, struct window_rects *rects )
+static BOOL nulldrv_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, const struct window_rects *rects )
 {
     return TRUE;
+}
+
+extern BOOL nulldrv_GetWindowStyleMasks( HWND hwnd, UINT style, UINT ex_style, UINT *style_mask, UINT *ex_style_mask )
+{
+    return FALSE;
 }
 
 static BOOL nulldrv_CreateWindowSurface( HWND hwnd, BOOL layered, const RECT *surface_rect, struct window_surface **surface )
@@ -1294,6 +1299,7 @@ static const struct user_driver_funcs lazy_load_driver =
     loaderdrv_UpdateLayeredWindow,
     nulldrv_WindowMessage,
     nulldrv_WindowPosChanging,
+    nulldrv_GetWindowStyleMasks,
     nulldrv_CreateWindowSurface,
     nulldrv_MoveWindowBits,
     nulldrv_WindowPosChanged,
@@ -1383,6 +1389,7 @@ void __wine_set_user_driver( const struct user_driver_funcs *funcs, UINT version
     SET_USER_FUNC(UpdateLayeredWindow);
     SET_USER_FUNC(WindowMessage);
     SET_USER_FUNC(WindowPosChanging);
+    SET_USER_FUNC(GetWindowStyleMasks);
     SET_USER_FUNC(CreateWindowSurface);
     SET_USER_FUNC(MoveWindowBits);
     SET_USER_FUNC(WindowPosChanged);

dlls/wineandroid.drv/android.h
@@ -95,7 +95,7 @@ extern void ANDROID_SetParent( HWND hwnd, HWND parent, HWND old_parent );
 extern void ANDROID_SetCapture( HWND hwnd, UINT flags );
 extern UINT ANDROID_ShowWindow( HWND hwnd, INT cmd, RECT *rect, UINT swp );
 extern LRESULT ANDROID_WindowMessage( HWND hwnd, UINT msg, WPARAM wp, LPARAM lp );
-extern BOOL ANDROID_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, struct window_rects *rects );
+extern BOOL ANDROID_WindowPosChanging( HWND hwnd, UINT swp_flags, BOOL shaped, const struct window_rects *rects );
 extern BOOL ANDROID_CreateWindowSurface( HWND hwnd, BOOL layered, const RECT *surface_rect, struct window_surface **surface );
 extern void ANDROID_WindowPosChanged( HWND hwnd, HWND insert_after, UINT swp_flags, const struct window_rects *new_rects,
                                       struct window_surface *surface );

dlls/winemac.drv/gdi.c
@@ -309,6 +309,7 @@ static const struct user_driver_funcs macdrv_funcs =
     .pMoveWindowBits = macdrv_MoveWindowBits,
     .pWindowPosChanged = macdrv_WindowPosChanged,
     .pWindowPosChanging = macdrv_WindowPosChanging,
+    .pGetWindowStyleMasks = macdrv_GetWindowStyleMasks,
     .pCreateWindowSurface = macdrv_CreateWindowSurface,
     .pVulkanInit = macdrv_VulkanInit,
     .pwine_get_wgl_driver = macdrv_wine_get_wgl_driver,

commit 531671cd20ee426dfc1c80e4c0cf64260eda4af1

win32u: Only allow a custom visible rect for toplevel windows.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57102
Fixes: cb1c03b926ca01619be02f835f382c1dd7d4a478

dlls/win32u/window.c
@@ -1894,7 +1894,7 @@ static BOOL get_default_window_surface( HWND hwnd, const RECT *surface_rect, str
 static struct window_surface *create_window_surface( HWND hwnd, UINT swp_flags, BOOL create_layered,
                                                      struct window_rects *rects, RECT *surface_rect )
 {
-    BOOL shaped, needs_surface, create_opaque, is_layered;
+    BOOL shaped, needs_surface, create_opaque, is_layered, is_child;
     HWND parent = NtUserGetAncestor( hwnd, GA_PARENT );
     struct window_surface *new_surface;
     UINT style, ex_style;
@@ -1903,18 +1903,19 @@ static struct window_surface *create_window_surface( HWND hwnd, UINT swp_flags, 
 
     style = NtUserGetWindowLongW( hwnd, GWL_STYLE );
     ex_style = NtUserGetWindowLongW( hwnd, GWL_EXSTYLE );
+    is_child = parent && parent != NtUserGetDesktopWindow();
 
     if (get_window_region( hwnd, FALSE, &shape, &dummy )) shaped = FALSE;
     else if ((shaped = !!shape)) NtGdiDeleteObjectApp( shape );
 
     rects->visible = rects->window;
     if (!user_driver->pWindowPosChanging( hwnd, swp_flags, shaped, rects )) needs_surface = FALSE;
-    else if (parent && parent != NtUserGetDesktopWindow()) needs_surface = FALSE;
+    else if (is_child) needs_surface = FALSE;
     else if (swp_flags & SWP_HIDEWINDOW) needs_surface = FALSE;
     else if (swp_flags & SWP_SHOWWINDOW) needs_surface = TRUE;
     else needs_surface = !!(style & WS_VISIBLE);
 
-    rects->visible = get_visible_rect( hwnd, shaped, style, ex_style, rects );
+    if (!is_child) rects->visible = get_visible_rect( hwnd, shaped, style, ex_style, rects );
     if (!get_surface_rect( &rects->visible, surface_rect )) needs_surface = FALSE;
     if (!get_default_window_surface( hwnd, surface_rect, &new_surface )) return NULL;
 