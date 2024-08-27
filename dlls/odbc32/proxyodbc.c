5f11bbdecc55cd6b0236e48ff3b6c6b78e091218
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2024-08-28 05:25:23 +1000

    odbc32: Correcly convert columns ID in SQLColAttribute/W for ODBC v2.0.
    The current conversion is converting ODBC 2 to 3 which is backwards.

--- dlls/odbc32/proxyodbc.c
+++ dlls/odbc32/proxyodbc.c
@@ -989,16 +989,32 @@ static SQLRETURN col_attribute_win32_a( struct statement *stmt, SQLUSMALLINT col
 
         switch (field_id)
         {
-        case SQL_COLUMN_COUNT:
-            field_id = SQL_DESC_COUNT;
+        case SQL_DESC_COUNT:
+            field_id = SQL_COLUMN_COUNT;
             break;
 
-        case SQL_COLUMN_NAME:
-            field_id = SQL_DESC_NAME;
+        case SQL_DESC_TYPE:
+            field_id = SQL_COLUMN_TYPE;
             break;
 
-        case SQL_COLUMN_NULLABLE:
-            field_id = SQL_DESC_NULLABLE;
+        case SQL_DESC_LENGTH:
+            field_id = SQL_COLUMN_LENGTH;
+            break;
+
+        case SQL_DESC_PRECISION:
+            field_id = SQL_COLUMN_PRECISION;
+            break;
+
+        case SQL_DESC_SCALE:
+            field_id = SQL_COLUMN_SCALE;
+            break;
+
+        case SQL_DESC_NULLABLE:
+            field_id = SQL_COLUMN_NULLABLE;
+            break;
+
+        case SQL_DESC_NAME:
+            field_id = SQL_COLUMN_NAME;
             break;
 
         case SQL_COLUMN_TYPE:
@@ -6175,16 +6191,32 @@ static SQLRETURN col_attribute_win32_w( struct statement *stmt, SQLUSMALLINT col
 
         switch (field_id)
         {
-        case SQL_COLUMN_COUNT:
-            field_id = SQL_DESC_COUNT;
+        case SQL_DESC_COUNT:
+            field_id = SQL_COLUMN_COUNT;
             break;
 
-        case SQL_COLUMN_NAME:
-            field_id = SQL_DESC_NAME;
+        case SQL_DESC_TYPE:
+            field_id = SQL_COLUMN_TYPE;
             break;
 
-        case SQL_COLUMN_NULLABLE:
-            field_id = SQL_DESC_NULLABLE;
+        case SQL_DESC_LENGTH:
+            field_id = SQL_COLUMN_LENGTH;
+            break;
+
+        case SQL_DESC_PRECISION:
+            field_id = SQL_COLUMN_PRECISION;
+            break;
+
+        case SQL_DESC_SCALE:
+            field_id = SQL_COLUMN_SCALE;
+            break;
+
+        case SQL_DESC_NULLABLE:
+            field_id = SQL_COLUMN_NULLABLE;
+            break;
+
+        case SQL_DESC_NAME:
+            field_id = SQL_COLUMN_NAME;
             break;
 
         case SQL_COLUMN_TYPE:
