fc8ed8b71131064b862e46894f9e5205acd90f6d
Hans Leidekker <hans@codeweavers.com>
2024-08-28 23:32:14 +0200

    msiexec: Remove an obsolete fixme.

--- programs/msiexec/msiexec.c
+++ programs/msiexec/msiexec.c
@@ -729,7 +729,6 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 			WINE_TRACE("argvW[%d] = %s\n", i, wine_dbgstr_w(argvW[i]));
 			PackageName = argvW[i];
 			StringListAppend(&property_list, L"ACTION=ADMIN");
-			WINE_FIXME("Administrative installs are not currently supported\n");
 		}
 		else if(msi_option_prefix(argvW[i], "f"))
 		{
