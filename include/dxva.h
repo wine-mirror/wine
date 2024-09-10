adafa62f6e9ad994709983646021c1126366ff96
Biswapriyo Nath <nathbappai@gmail.com>
2024-09-10 07:28:07 +0000

    include: Add DXVA_PicParams_HEVC_RangeExt declaration in dxva.h.
    Required for https://github.com/cgutman/FFmpeg/commit/9531ce6d655ff874634cc51e326cc85937b1f3f6

--- include/dxva.h
+++ include/dxva.h
@@ -360,6 +360,38 @@ typedef struct _DXVA_PicParams_HEVC
     UINT    StatusReportFeedbackNumber;
 } DXVA_PicParams_HEVC, *LPDXVA_PicParams_HEVC;
 
+typedef struct _DXVA_PicParams_HEVC_RangeExt
+{
+    DXVA_PicParams_HEVC params;
+    union
+    {
+        struct
+        {
+            USHORT transform_skip_rotation_enabled_flag    : 1;
+            USHORT transform_skip_context_enabled_flag     : 1;
+            USHORT implicit_rdpcm_enabled_flag             : 1;
+            USHORT explicit_rdpcm_enabled_flag             : 1;
+            USHORT extended_precision_processing_flag      : 1;
+            USHORT intra_smoothing_disabled_flag           : 1;
+            USHORT persistent_rice_adaptation_enabled_flag : 1;
+            USHORT high_precision_offsets_enabled_flag     : 1;
+            USHORT cabac_bypass_alignment_enabled_flag     : 1;
+            USHORT cross_component_prediction_enabled_flag : 1;
+            USHORT chroma_qp_offset_list_enabled_flag      : 1;
+            USHORT ReservedBits8                           : 5;
+        };
+        USHORT dwRangeExtensionFlags;
+    };
+    UCHAR diff_cu_chroma_qp_offset_depth;
+    UCHAR log2_sao_offset_scale_luma;
+    UCHAR log2_sao_offset_scale_chroma;
+    UCHAR log2_max_transform_skip_block_size_minus2;
+    CHAR cb_qp_offset_list[6];
+    CHAR cr_qp_offset_list[6];
+    UCHAR chroma_qp_offset_list_len_minus1;
+    USHORT ReservedBits9;
+} DXVA_PicParams_HEVC_RangeExt, *LPDXVA_PicParams_HEVC_RangeExt;
+
 typedef struct _DXVA_Qmatrix_HEVC
 {
     UCHAR ucScalingLists0[6][16];
