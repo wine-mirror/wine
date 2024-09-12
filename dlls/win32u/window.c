wine-mirror/wine

commit 05d727a935b9009f188cc74ca77e14afc44aef30

win32u: Update the layered surface attributes in apply_window_pos.
And after layered window creation in NtUserUpdateLayeredWindow.

dlls/win32u/window.c
@@ -1873,6 +1873,18 @@ static struct window_surface *create_window_surface( HWND hwnd, UINT swp_flags, 
         create_offscreen_window_surface( hwnd, surface_rect, &new_surface );
     }
 
+    if (new_surface && !is_layered)
+    {
+        DWORD lwa_flags = 0;
+        COLORREF key;
+        BYTE alpha;
+
+        if (!NtUserGetLayeredWindowAttributes( hwnd, &key, &alpha, &lwa_flags )) lwa_flags = 0;
+        if (!(lwa_flags & LWA_ALPHA)) alpha = 255;
+        if (!(lwa_flags & LWA_COLORKEY)) key = CLR_INVALID;
+        window_surface_set_layered( new_surface, key, alpha << 24, 0 );
+    }
+
     return new_surface;
 }
 
@@ -2248,6 +2260,8 @@ BOOL WINAPI NtUserUpdateLayeredWindow( HWND hwnd, HDC hdc_dst, const POINT *pts_
 
         NtGdiDeleteObjectApp( hdc );
         window_surface_unlock( surface );
+
+        window_surface_set_layered( surface, key, -1, 0xff000000 );
         window_surface_flush( surface );
 
         user_driver->pUpdateLayeredWindow( hwnd, &window_rect, key, alpha, flags );

dlls/win32u/dce.c
@@ -479,8 +479,6 @@ W32KAPI void window_surface_set_layered( struct window_surface *surface, COLORRE
         }
     }
     window_surface_unlock( surface );
-
-    window_surface_flush( surface );
 }
 
 W32KAPI void window_surface_set_clip( struct window_surface *surface, HRGN clip_region )

commit e7e58dd375c4bcbe4f8cb9dfbe3c4f6e6295ec27

win32u: Leave window surface alpha bits to -1 when unset.
Fixes: 05d727a935b9009f188cc74ca77e14afc44aef30
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57160

dlls/win32u/window.c
@@ -1949,14 +1949,14 @@ static struct window_surface *get_window_surface( HWND hwnd, UINT swp_flags, BOO
 
     if (new_surface && !is_layered)
     {
-        DWORD lwa_flags = 0;
+        DWORD lwa_flags = 0, alpha_bits = -1;
         COLORREF key;
         BYTE alpha;
 
         if (!NtUserGetLayeredWindowAttributes( hwnd, &key, &alpha, &lwa_flags )) lwa_flags = 0;
-        if (!(lwa_flags & LWA_ALPHA)) alpha = 255;
+        if (lwa_flags & LWA_ALPHA) alpha_bits = alpha << 24;
         if (!(lwa_flags & LWA_COLORKEY)) key = CLR_INVALID;
-        window_surface_set_layered( new_surface, key, alpha << 24, 0 );
+        window_surface_set_layered( new_surface, key, alpha_bits, 0 );
     }
 
     return new_surface;