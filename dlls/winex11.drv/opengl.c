wine-mirror/wine

commit 278ca7f68d727639b3389b78c363134fa2bd32a8

winex11: Only update the client window position in sync_client_position.

dlls/winex11.drv/window.c
@@ -1391,18 +1391,13 @@ static void sync_client_position( struct x11drv_win_data *data, const struct win
 
     changes.x      = data->rects.client.left - data->rects.visible.left;
     changes.y      = data->rects.client.top - data->rects.visible.top;
-    changes.width  = min( max( 1, data->rects.client.right - data->rects.client.left ), 65535 );
-    changes.height = min( max( 1, data->rects.client.bottom - data->rects.client.top ), 65535 );
-
     if (changes.x != old_rects->client.left - old_rects->visible.left) mask |= CWX;
     if (changes.y != old_rects->client.top  - old_rects->visible.top)  mask |= CWY;
-    if (changes.width  != old_rects->client.right - old_rects->client.left) mask |= CWWidth;
-    if (changes.height != old_rects->client.bottom - old_rects->client.top) mask |= CWHeight;
 
     if (mask)
     {
-        TRACE( "setting client win %lx pos %d,%d,%dx%d changes=%x\n",
-               data->client_window, changes.x, changes.y, changes.width, changes.height, mask );
+        TRACE( "setting client win %lx pos %d,%d changes=%x\n",
+               data->client_window, changes.x, changes.y, mask );
         XConfigureWindow( gdi_display, data->client_window, mask, &changes );
     }
 }

commit 03d985369dc9afe943ee3072478a2567b3fe4077

winex11: Resize GL drawable when necessary, if wglSwapBuffer isn't called.
Fixes: 278ca7f68d727639b3389b78c363134fa2bd32a8
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57189

dlls/winex11.drv/opengl.c
@@ -1311,7 +1311,12 @@ void sync_gl_drawable( HWND hwnd, BOOL known_child )
     switch (old->type)
     {
     case DC_GL_WINDOW:
-        if (!known_child) break; /* Still a childless top-level window */
+        if (!known_child)
+        {
+            /* Still a childless top-level window */
+            update_gl_drawable_size( old );
+            break;
+        }
         /* fall through */
     case DC_GL_PIXMAP_WIN:
         if (!(new = create_gl_drawable( hwnd, old->format, known_child, old->mutable_pf ))) break;

dlls/winex11.drv/window.c
@@ -2603,15 +2603,13 @@ void X11DRV_WindowPosChanged( HWND hwnd, HWND insert_after, UINT swp_flags, cons
 
     sync_client_position( data, &old_rects );
 
+    if (data->rects.client.right - data->rects.client.left != old_rects.client.right - old_rects.client.left ||
+        data->rects.client.bottom - data->rects.client.top != old_rects.client.bottom - old_rects.client.top)
+        sync_gl_drawable( hwnd, FALSE );
+
     if (!data->whole_window)
     {
-        BOOL needs_resize = (!data->client_window &&
-                             (data->rects.client.right - data->rects.client.left !=
-                              old_rects.client.right - old_rects.client.left ||
-                              data->rects.client.bottom - data->rects.client.top !=
-                              old_rects.client.bottom - old_rects.client.top));
         release_win_data( data );
-        if (needs_resize) sync_gl_drawable( hwnd, FALSE );
         return;
     }
 