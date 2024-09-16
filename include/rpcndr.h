e14f129ac7169a5feca310101867e288ed8cd356
Rémi Bernon <rbernon@codeweavers.com>
2024-09-16 11:53:43 +0200

    include: Use winnt.h DECLSPEC_NOVTABLE definition in rpcndr.h.

--- include/rpcndr.h
+++ include/rpcndr.h
@@ -120,6 +120,14 @@ typedef void (__RPC_USER *NDR_RUNDOWN)(void *context);
 typedef void (__RPC_USER *NDR_NOTIFY_ROUTINE)(void);
 typedef void (__RPC_USER *NDR_NOTIFY2_ROUTINE)(boolean flag);
 
+#ifndef DECLSPEC_NOVTABLE
+# if __has_declspec_attribute(novtable) && defined(__cplusplus)
+#  define DECLSPEC_NOVTABLE __declspec(novtable)
+# else
+#  define DECLSPEC_NOVTABLE
+# endif
+#endif
+
 #ifndef DECLSPEC_UUID
 # if __has_declspec_attribute(uuid) && defined (__cplusplus)
 #  define DECLSPEC_UUID(x) __declspec(uuid(x))
