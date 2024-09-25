wine-mirror/wine

commit c0bf67636ab3de46c67b8d02e44fb39b5853384a

uxtheme: Try to avoid TransparentBlt() when possible.

dlls/uxtheme/msstyles.c
@@ -1181,19 +1181,43 @@ PTHEME_PROPERTY MSSTYLES_FindProperty(PTHEME_CLASS tc, int iPartId, int iStateId
 }
 
 /* Prepare a bitmap to be used for alpha blending */
-static BOOL prepare_alpha (HBITMAP bmp, BOOL* hasAlpha)
+static BOOL prepare_alpha (HBITMAP bmp, BOOL* hasAlpha, BOOL *hasDefaultTransparentColour)
 {
     DIBSECTION dib;
-    int n;
+    int n, stride;
     BYTE* p;
 
     *hasAlpha = FALSE;
+    *hasDefaultTransparentColour = FALSE;
 
     if (!bmp || GetObjectW( bmp, sizeof(dib), &dib ) != sizeof(dib))
         return FALSE;
 
-    if (dib.dsBm.bmBitsPixel != 32 || dib.dsBmih.biCompression != BI_RGB)
-        /* nothing to do */
+    if (dib.dsBmih.biCompression != BI_RGB)
+        return TRUE;
+
+    if (dib.dsBm.bmBitsPixel == 24)
+    {
+        int y;
+
+        stride = (dib.dsBmih.biWidth * 3 + 3) & ~3;
+        p = dib.dsBm.bmBits;
+        for (y = 0; y < dib.dsBmih.biHeight; ++y)
+        {
+            p = (BYTE *)dib.dsBm.bmBits + stride * y;
+            for (n = 0; n < dib.dsBmih.biWidth; ++n, p += 3)
+            {
+                if (RGB(p[0], p[1], p[2]) == DEFAULT_TRANSPARENT_COLOR)
+                {
+                    *hasDefaultTransparentColour = TRUE;
+                    return TRUE;
+                }
+            }
+        }
+        return TRUE;
+    }
+
+    if (dib.dsBm.bmBitsPixel != 32)
         return TRUE;
 
     /* If all alpha values are 0xff, don't use alpha blending */
@@ -1219,11 +1243,13 @@ static BOOL prepare_alpha (HBITMAP bmp, BOOL* hasAlpha)
     return TRUE;
 }
 
-HBITMAP MSSTYLES_LoadBitmap (PTHEME_CLASS tc, LPCWSTR lpFilename, BOOL* hasAlpha)
+HBITMAP MSSTYLES_LoadBitmap (PTHEME_CLASS tc, LPCWSTR lpFilename, BOOL* hasAlpha, BOOL *hasDefaultTransparentColour)
 {
     WCHAR szFile[MAX_PATH];
     LPWSTR tmp;
     PTHEME_IMAGE img;
+    BOOL has_default;
+
     lstrcpynW(szFile, lpFilename, ARRAY_SIZE(szFile));
     tmp = szFile;
     do {
@@ -1240,6 +1266,8 @@ HBITMAP MSSTYLES_LoadBitmap (PTHEME_CLASS tc, LPCWSTR lpFilename, BOOL* hasAlpha
         {
             TRACE ("found %p %s: %p\n", img, debugstr_w (img->name), img->image);
             *hasAlpha = img->hasAlpha;
+            if (hasDefaultTransparentColour)
+                *hasDefaultTransparentColour = img->hasDefaultTransparentColour;
             return img->image;
         }
         img = img->next;
@@ -1247,8 +1275,11 @@ HBITMAP MSSTYLES_LoadBitmap (PTHEME_CLASS tc, LPCWSTR lpFilename, BOOL* hasAlpha
     /* Not found? Load from resources */
     img = malloc(sizeof(*img));
     img->image = LoadImageW(tc->hTheme, szFile, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
-    prepare_alpha (img->image, hasAlpha);
+    prepare_alpha (img->image, hasAlpha, &has_default);
     img->hasAlpha = *hasAlpha;
+    img->hasDefaultTransparentColour = has_default;
+    if (hasDefaultTransparentColour)
+        *hasDefaultTransparentColour = has_default;
     /* ...and stow away for later reuse. */
     lstrcpyW (img->name, szFile);
     img->next = tc->tf->images;

dlls/uxtheme/draw.c
@@ -250,7 +250,7 @@ static PTHEME_PROPERTY UXTHEME_SelectImage(HTHEME hTheme, int iPartId, int iStat
                 BOOL hasAlpha;
 
                 lstrcpynW(szPath, fileProp->lpValue, min(fileProp->dwValueLen+1, ARRAY_SIZE(szPath)));
-                hBmp = MSSTYLES_LoadBitmap(hTheme, szPath, &hasAlpha);
+                hBmp = MSSTYLES_LoadBitmap(hTheme, szPath, &hasAlpha, NULL);
                 if(!hBmp) continue;
 
                 GetThemeEnumValue(hTheme, iPartId, iStateId, TMT_IMAGELAYOUT, &imagelayout);
@@ -289,7 +289,7 @@ static PTHEME_PROPERTY UXTHEME_SelectImage(HTHEME hTheme, int iPartId, int iStat
  */
 static HRESULT UXTHEME_LoadImage(HTHEME hTheme, int iPartId, int iStateId, const RECT *pRect,
                                  BOOL glyph, HBITMAP *hBmp, RECT *bmpRect, BOOL *hasImageAlpha,
-                                 int *imageDpi)
+                                 BOOL *hasDefaultTransparentColour, int *imageDpi)
 {
     int imagelayout = IL_HORIZONTAL;
     int imagecount = 1;
@@ -304,7 +304,7 @@ static HRESULT UXTHEME_LoadImage(HTHEME hTheme, int iPartId, int iStateId, const
         return E_PROP_ID_UNSUPPORTED;
     }
     lstrcpynW(szPath, tp->lpValue, min(tp->dwValueLen+1, ARRAY_SIZE(szPath)));
-    *hBmp = MSSTYLES_LoadBitmap(hTheme, szPath, hasImageAlpha);
+    *hBmp = MSSTYLES_LoadBitmap(hTheme, szPath, hasImageAlpha, hasDefaultTransparentColour);
     if(!*hBmp) {
         TRACE("Failed to load bitmap %s\n", debugstr_w(szPath));
         return HRESULT_FROM_WIN32(GetLastError());
@@ -496,7 +496,7 @@ static inline BOOL UXTHEME_SizedBlt (HDC hdcDst, int nXOriginDst, int nYOriginDs
  * depend on whether the image has full alpha  or whether it is 
  * color-transparent or just opaque. */
 static inline void get_transparency (HTHEME hTheme, int iPartId, int iStateId, 
-                                     BOOL hasImageAlpha, INT* transparent,
+                                     BOOL hasImageAlpha, BOOL hasDefaultTransparentColour, INT* transparent,
                                      COLORREF* transparentcolor, BOOL glyph)
 {
     if (hasImageAlpha)
@@ -516,6 +516,8 @@ static inline void get_transparency (HTHEME hTheme, int iPartId, int iStateId,
                 transparentcolor))) {
                 *transparentcolor = DEFAULT_TRANSPARENT_COLOR;
             }
+            if (!hasDefaultTransparentColour && *transparentcolor == DEFAULT_TRANSPARENT_COLOR)
+                *transparent = ALPHABLEND_NONE;
         }
         else
             *transparent = ALPHABLEND_NONE;
@@ -541,7 +543,7 @@ static void reset_dc_alpha_values(HTHEME htheme, HDC hdc, int part_id, int state
         return;
 
     if (FAILED(UXTHEME_LoadImage(htheme, part_id, state_id, rect, FALSE, &hbmp, &image_rect,
-                                 &has_alpha, NULL)) || has_alpha)
+                                 &has_alpha, NULL, NULL)) || has_alpha)
         return;
 
     bitmap_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
@@ -576,10 +578,10 @@ static HRESULT UXTHEME_DrawImageGlyph(HTHEME hTheme, HDC hdc, int iPartId,
     POINT dstSize;
     POINT srcSize;
     POINT topleft;
-    BOOL hasAlpha;
+    BOOL hasAlpha, hasDefaultTransparentColour;
 
     hr = UXTHEME_LoadImage(hTheme, iPartId, iStateId, pRect, TRUE, &bmpSrc, &rcSrc, &hasAlpha,
-                           NULL);
+                           &hasDefaultTransparentColour, NULL);
     if(FAILED(hr)) return hr;
     hdcSrc = CreateCompatibleDC(hdc);
     if(!hdcSrc) {
@@ -593,8 +595,8 @@ static HRESULT UXTHEME_DrawImageGlyph(HTHEME hTheme, HDC hdc, int iPartId,
     srcSize.x = rcSrc.right-rcSrc.left;
     srcSize.y = rcSrc.bottom-rcSrc.top;
 
-    get_transparency (hTheme, iPartId, iStateId, hasAlpha, &transparent,
-        &transparentcolor, TRUE);
+    get_transparency (hTheme, iPartId, iStateId, hasAlpha, hasDefaultTransparentColour, &transparent,
+                      &transparentcolor, TRUE);
     GetThemeEnumValue(hTheme, iPartId, iStateId, TMT_VALIGN, &valign);
     GetThemeEnumValue(hTheme, iPartId, iStateId, TMT_HALIGN, &halign);
 
@@ -659,7 +661,7 @@ static HRESULT get_image_part_size(HTHEME hTheme, int iPartId, int iStateId, REC
     BOOL hasAlpha;
 
     hr = UXTHEME_LoadImage(hTheme, iPartId, iStateId, prc, FALSE, &bmpSrc, &rcSrc, &hasAlpha,
-                           &imageDpi);
+                           NULL, &imageDpi);
     if (FAILED(hr)) return hr;
 
     switch (eSize)
@@ -760,10 +762,10 @@ static HRESULT UXTHEME_DrawImageBackground(HTHEME hTheme, HDC hdc, int iPartId,
     int sizingtype = ST_STRETCH;
     INT transparent;
     COLORREF transparentcolor = 0;
-    BOOL hasAlpha;
+    BOOL hasAlpha, hasDefaultTransparentColour;
 
     hr = UXTHEME_LoadImage(hTheme, iPartId, iStateId, pRect, FALSE, &bmpSrc, &rcSrc, &hasAlpha,
-                           NULL);
+                           &hasDefaultTransparentColour, NULL);
     if(FAILED(hr)) return hr;
     hdcSrc = CreateCompatibleDC(hdc);
     if(!hdcSrc) {
@@ -774,7 +776,7 @@ static HRESULT UXTHEME_DrawImageBackground(HTHEME hTheme, HDC hdc, int iPartId,
 
     rcDst = *pRect;
     
-    get_transparency (hTheme, iPartId, iStateId, hasAlpha, &transparent,
+    get_transparency (hTheme, iPartId, iStateId, hasAlpha, hasDefaultTransparentColour, &transparent,
         &transparentcolor, FALSE);
 
     dstSize.x = rcDst.right-rcDst.left;
@@ -2224,10 +2226,10 @@ BOOL WINAPI IsThemeBackgroundPartiallyTransparent(HTHEME hTheme, int iPartId,
     if (bgtype != BT_IMAGEFILE) return FALSE;
 
     if (FAILED(UXTHEME_LoadImage(hTheme, iPartId, iStateId, &rect, FALSE, &bmpSrc, &rcSrc,
-                                 &hasAlpha, NULL)))
+                                 &hasAlpha, NULL, NULL)))
         return FALSE;
 
-    get_transparency (hTheme, iPartId, iStateId, hasAlpha, &transparent,
+    get_transparency (hTheme, iPartId, iStateId, hasAlpha, TRUE, &transparent,
         &transparentcolor, FALSE);
     return (transparent != ALPHABLEND_NONE);
 }

dlls/uxtheme/msstyles.h
@@ -67,7 +67,8 @@ typedef struct _THEME_CLASS {
 typedef struct _THEME_IMAGE {
     WCHAR name[MAX_PATH];
     HBITMAP image;
-    BOOL hasAlpha;
+    unsigned int hasAlpha : 1;
+    unsigned int hasDefaultTransparentColour : 1;
     
     struct _THEME_IMAGE *next;
 } THEME_IMAGE, *PTHEME_IMAGE;
@@ -103,7 +104,7 @@ PTHEME_PARTSTATE MSSTYLES_FindPart(PTHEME_CLASS tc, int iPartId);
 PTHEME_PARTSTATE MSSTYLES_FindPartState(PTHEME_CLASS tc, int iPartId, int iStateId, PTHEME_CLASS *tcNext);
 PTHEME_PROPERTY MSSTYLES_FindProperty(PTHEME_CLASS tc, int iPartId, int iStateId, int iPropertyPrimitive, int iPropertyId);
 PTHEME_PROPERTY MSSTYLES_FindMetric(int iPropertyPrimitive, int iPropertyId);
-HBITMAP MSSTYLES_LoadBitmap(PTHEME_CLASS tc, LPCWSTR lpFilename, BOOL* hasAlpha);
+HBITMAP MSSTYLES_LoadBitmap(PTHEME_CLASS tc, LPCWSTR lpFilename, BOOL* hasAlpha, BOOL *hasDefaultTransparentColour);
 
 HRESULT MSSTYLES_GetPropertyBool(PTHEME_PROPERTY tp, BOOL *pfVal);
 HRESULT MSSTYLES_GetPropertyColor(PTHEME_PROPERTY tp, COLORREF *pColor);

commit bf659caacc6b93ba25446d7cc417b339aa94650f

uxtheme: Better detect the presence of default transparent colour in prepare_alpha().
Fixes a regression introduced by
c0bf67636ab3de46c67b8d02e44fb39b5853384a.

dlls/uxtheme/msstyles.c
@@ -1193,8 +1193,67 @@ static BOOL prepare_alpha (HBITMAP bmp, BOOL* hasAlpha, BOOL *hasDefaultTranspar
     if (!bmp || GetObjectW( bmp, sizeof(dib), &dib ) != sizeof(dib))
         return FALSE;
 
-    if (dib.dsBmih.biCompression != BI_RGB)
+    if (dib.dsBm.bmBitsPixel <= 8)
+    {
+        RGBQUAD p[256];
+        HDC hdc = CreateCompatibleDC(NULL);
+        HBITMAP prev = SelectObject(hdc, bmp);
+        UINT count = GetDIBColorTable(hdc, 0, 256, p);
+
+        SelectObject(hdc, prev);
+        DeleteDC(hdc);
+
+        for (n = 0; n < count; ++n)
+        {
+            if (RGB(p[n].rgbRed, p[n].rgbGreen, p[n].rgbBlue) == DEFAULT_TRANSPARENT_COLOR)
+            {
+                *hasDefaultTransparentColour = TRUE;
+                return TRUE;
+            }
+        }
         return TRUE;
+    }
+
+    if (dib.dsBm.bmBitsPixel == 16)
+    {
+        unsigned short transparent_color;
+        int y;
+
+        if (dib.dsBmih.biCompression == BI_RGB)
+        {
+            transparent_color = 0x7c00 | 0x001f;
+        }
+        else if (dib.dsBmih.biCompression == BI_BITFIELDS && dib.dsBitfields[0])
+        {
+            transparent_color = dib.dsBitfields[0] | dib.dsBitfields[2];
+        }
+        else
+        {
+            WARN("biCompression %ld, bpp %d not checked for default transparent colour.\n", dib.dsBmih.biCompression, dib.dsBm.bmBitsPixel);
+            return TRUE;
+        }
+        stride = (dib.dsBmih.biWidth * 2 + 3) & ~3;
+        p = dib.dsBm.bmBits;
+        for (y = 0; y < dib.dsBmih.biHeight; ++y)
+        {
+            p = (BYTE *)dib.dsBm.bmBits + stride * y;
+            for (n = 0; n < dib.dsBmih.biWidth; ++n, p += 2)
+            {
+                if ((p[0] | (p[1] << 8)) == transparent_color)
+                {
+                    *hasDefaultTransparentColour = TRUE;
+                    return TRUE;
+                }
+            }
+        }
+        return TRUE;
+    }
+
+    if (dib.dsBmih.biCompression != BI_RGB)
+    {
+        WARN("biCompression %ld, bpp %d not checked for default transparent colour.\n", dib.dsBmih.biCompression, dib.dsBm.bmBitsPixel);
+        return TRUE;
+    }
 
     if (dib.dsBm.bmBitsPixel == 24)
     {
@@ -1222,8 +1281,12 @@ static BOOL prepare_alpha (HBITMAP bmp, BOOL* hasAlpha, BOOL *hasDefaultTranspar
 
     /* If all alpha values are 0xff, don't use alpha blending */
     for (n = 0, p = dib.dsBm.bmBits; n < dib.dsBmih.biWidth * dib.dsBmih.biHeight; n++, p += 4)
+    {
         if ((*hasAlpha = (p[3] != 0xff)))
             break;
+        if (RGB(p[0], p[1], p[2]) == DEFAULT_TRANSPARENT_COLOR)
+            *hasDefaultTransparentColour = TRUE;
+    }
 
     if (!*hasAlpha)
         return TRUE;