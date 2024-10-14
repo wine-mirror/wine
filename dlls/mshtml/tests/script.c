wine-mirror/wine

commit 88a06f18b467a31d96ab5b9c816cf3ee3b2bb2f2

mshtml: Use DispatchEx for document node InvokeEx implementation.

dlls/mshtml/dispex.c
@@ -1725,6 +1725,12 @@ static HRESULT WINAPI DispatchEx_InvokeEx(IDispatchEx *iface, DISPID id, LCID lc
     if(wFlags == (DISPATCH_PROPERTYPUT|DISPATCH_PROPERTYPUTREF))
         wFlags = DISPATCH_PROPERTYPUT;
 
+    if(This->info->desc->vtbl->disp_invoke) {
+        hres = This->info->desc->vtbl->disp_invoke(This, id, lcid, wFlags, pdp, pvarRes, pei, pspCaller);
+        if(hres != S_FALSE)
+            return hres;
+    }
+
     switch(get_dispid_type(id)) {
     case DISPEXPROP_CUSTOM:
         if(!This->info->desc->vtbl->invoke)

dlls/mshtml/htmldoc.c
@@ -5066,22 +5066,6 @@ static HRESULT WINAPI DocDispatchEx_InvokeEx(IDispatchEx *iface, DISPID id, LCID
 {
     HTMLDocumentNode *This = impl_from_IDispatchEx(iface);
 
-    if(This->window) {
-        switch(id) {
-        case DISPID_READYSTATE:
-            TRACE("DISPID_READYSTATE\n");
-
-            if(!(wFlags & DISPATCH_PROPERTYGET))
-                return E_INVALIDARG;
-
-            V_VT(pvarRes) = VT_I4;
-            V_I4(pvarRes) = This->window->base.outer_window->readystate;
-            return S_OK;
-        default:
-            break;
-        }
-    }
-
     return IDispatchEx_InvokeEx(&This->node.event_target.dispex.IDispatchEx_iface, id, lcid, wFlags, pdp, pvarRes, pei, pspCaller);
 }
 
@@ -5939,6 +5923,30 @@ static HRESULT HTMLDocumentNode_invoke(DispatchEx *dispex, DISPID id, LCID lcid,
     return S_OK;
 }
 
+static HRESULT HTMLDocumentNode_disp_invoke(DispatchEx *dispex, DISPID id, LCID lcid, WORD flags, DISPPARAMS *params,
+        VARIANT *res, EXCEPINFO *ei, IServiceProvider *caller)
+{
+    HTMLDocumentNode *This = impl_from_DispatchEx(dispex);
+
+    if(This->window) {
+        switch(id) {
+        case DISPID_READYSTATE:
+            TRACE("DISPID_READYSTATE\n");
+
+            if(!(flags & DISPATCH_PROPERTYGET))
+                return E_INVALIDARG;
+
+            V_VT(res) = VT_I4;
+            V_I4(res) = This->window->base.outer_window->readystate;
+            return S_OK;
+        default:
+            break;
+        }
+    }
+
+    return S_FALSE;
+}
+
 static HRESULT HTMLDocumentNode_next_dispid(DispatchEx *dispex, DISPID id, DISPID *pid)
 {
     DWORD idx = (id == DISPID_STARTENUM) ? 0 : id - MSHTML_DISPID_CUSTOM_MIN + 1;
@@ -6111,6 +6119,7 @@ static const event_target_vtbl_t HTMLDocumentNode_event_target_vtbl = {
         .get_dispid          = HTMLDocumentNode_get_dispid,
         .find_dispid         = HTMLDocumentNode_find_dispid,
         .invoke              = HTMLDocumentNode_invoke,
+        .disp_invoke         = HTMLDocumentNode_disp_invoke,
         .next_dispid         = HTMLDocumentNode_next_dispid,
         .get_compat_mode     = HTMLDocumentNode_get_compat_mode,
     },

dlls/mshtml/mshtml_private.h
@@ -383,6 +383,9 @@ typedef struct {
     HRESULT (*delete)(DispatchEx*,DISPID);
     HRESULT (*next_dispid)(DispatchEx*,DISPID,DISPID*);
 
+    /* Similar to invoke, but allows overriding all dispids */
+    HRESULT (*disp_invoke)(DispatchEx*,DISPID,LCID,WORD,DISPPARAMS*,VARIANT*,EXCEPINFO*,IServiceProvider*);
+
     /* Used by objects that want to delay their compat mode initialization until actually needed */
     compat_mode_t (*get_compat_mode)(DispatchEx*);
 

dlls/mshtml/tests/htmldoc.c
@@ -5681,6 +5681,29 @@ static void _test_readyState(unsigned line, IUnknown *unk)
     ok_(__FILE__,line) (V_VT(&out) == VT_I4, "V_VT(out)=%d\n", V_VT(&out));
     ok_(__FILE__,line) (V_I4(&out) == load_state%5, "VT_I4(out)=%ld, expected %d\n", V_I4(&out), load_state%5);
 
+    /* check on document node too */
+    if(load_state == LD_COMPLETE) {
+        IHTMLDocument2 *doc_node;
+        IHTMLWindow2 *window;
+
+        hres = IHTMLDocument2_get_parentWindow(htmldoc, &window);
+        ok(hres == S_OK, "get_parentWindow failed: %08lx\n", hres);
+
+        hres = IHTMLWindow2_get_document(window, &doc_node);
+        ok(hres == S_OK, "get_document failed: %08lx\n", hres);
+
+        VariantInit(&out);
+        hres = IHTMLDocument2_Invoke(doc_node, DISPID_READYSTATE, &IID_NULL, 0, DISPATCH_PROPERTYGET,
+                                     &dispparams, &out, NULL, NULL);
+        ok(hres == S_OK, "Invoke(DISPID_READYSTATE) failed: %08lx\n", hres);
+
+        ok_(__FILE__,line) (V_VT(&out) == VT_I4, "V_VT(out)=%d\n", V_VT(&out));
+        ok_(__FILE__,line) (V_I4(&out) == load_state%5, "VT_I4(out)=%ld, expected %d\n", V_I4(&out), load_state%5);
+
+        IHTMLDocument2_Release(doc_node);
+        IHTMLWindow2_Release(window);
+    }
+
     test_doscroll((IUnknown*)htmldoc);
 
     IHTMLDocument2_Release(htmldoc);

commit e62265b1130bc49efb51569528d4382c57949c96

mshtml: Make sure disp_invoke is called before locking the document mode.
ieframe's get_doc_ready_state retrieves the document's readystate, but
this happens way too early before meta elements are parsed (and thus
compat mode is not set yet). Prior to the regression, it was handling
DISPID_READYSTATE directly without going through DispatchEx's InvokeEx,
which calls ensure_real_info and locks the document mode.

Fixes a regression introduced by 88a06f18b467a31d96ab5b9c816cf3ee3b2bb2f2.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

dlls/mshtml/dispex.c
@@ -2259,9 +2259,6 @@ static HRESULT WINAPI DispatchEx_InvokeEx(IWineJSDispatchHost *iface, DISPID id,
 
     TRACE("%s (%p)->(%lx %lx %x %p %p %p %p)\n", This->info->name, This, id, lcid, wFlags, pdp, pvarRes, pei, pspCaller);
 
-    if(!ensure_real_info(This))
-        return E_OUTOFMEMORY;
-
     if(wFlags == (DISPATCH_PROPERTYPUT|DISPATCH_PROPERTYPUTREF))
         wFlags = DISPATCH_PROPERTYPUT;
 
@@ -2270,6 +2267,9 @@ static HRESULT WINAPI DispatchEx_InvokeEx(IWineJSDispatchHost *iface, DISPID id,
         if(hres != S_FALSE)
             return hres;
     }
+
+    if(!ensure_real_info(This))
+        return E_OUTOFMEMORY;
 
     if(This->jsdisp)
         return IWineJSDispatch_InvokeEx(This->jsdisp, id, lcid, wFlags, pdp, pvarRes, pei, pspCaller);

dlls/mshtml/tests/script.c
@@ -185,6 +185,7 @@ static const GUID CLSID_TestScript[] = {
 static const GUID CLSID_TestActiveX =
     {0x178fc163,0xf585,0x4e24,{0x9c,0x13,0x4b,0xb7,0xfa,0xf8,0x06,0x46}};
 
+static DWORD main_thread_id;
 static BOOL is_ie9plus, is_english;
 static IHTMLDocument2 *notif_doc;
 static IOleDocumentView *view;
@@ -4487,6 +4488,27 @@ static HRESULT WINAPI Protocol_Read(IInternetProtocolEx *iface, void *pv, ULONG 
     ULONG read;
     HRESULT hres;
 
+    if(GetCurrentThreadId() == main_thread_id) {
+        IHTMLDocument2 *doc_node;
+        DISPPARAMS dp = { 0 };
+        IHTMLWindow2 *window;
+        VARIANT v;
+
+        hres = IHTMLDocument2_get_parentWindow(notif_doc, &window);
+        ok(hres == S_OK, "get_parentWindow failed: %08lx\n", hres);
+
+        hres = IHTMLWindow2_get_document(window, &doc_node);
+        IHTMLWindow2_Release(window);
+        ok(hres == S_OK, "get_document failed: %08lx\n", hres);
+
+        V_VT(&v) = VT_EMPTY;
+        V_I4(&v) = 1234;
+        hres = IHTMLDocument2_Invoke(doc_node, DISPID_READYSTATE, &IID_NULL, 0, DISPATCH_PROPERTYGET, &dp, &v, NULL, NULL);
+        ok(hres == S_OK, "Invoke(DISPID_READYSTATE) failed: %08lx\n", hres);
+        ok(V_VT(&v) == VT_I4, "V_VT(v) = %d\n", V_VT(&v));
+        IHTMLDocument2_Release(doc_node);
+    }
+
     if(This->stream) {
         hres = IStream_Read(This->stream, pv, cb, &read);
         ok(SUCCEEDED(hres), "Read failed: %08lx\n", hres);
@@ -5247,6 +5269,7 @@ START_TEST(script)
 
     argc = winetest_get_mainargs(&argv);
     CoInitialize(NULL);
+    main_thread_id = GetCurrentThreadId();
     container_hwnd = create_container_window();
 
     detect_locale();