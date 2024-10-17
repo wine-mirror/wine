wine-mirror/wine

commit 5a7ff3b45c78784dca0a465d9e21483f72bce6e5

ddraw: Store surface handles in the global table.

dlls/ddraw/device.c
@@ -319,14 +319,6 @@ static ULONG WINAPI d3d_device_inner_Release(IUnknown *iface)
                     break;
                 }
 
-                case DDRAW_HANDLE_SURFACE:
-                {
-                    struct ddraw_surface *surf = entry->object;
-                    FIXME("Texture handle %#lx (%p) not unset properly.\n", i + 1, surf);
-                    surf->Handle = 0;
-                    break;
-                }
-
                 default:
                     FIXME("Handle %#lx (%p) has unknown type %#x.\n", i + 1, entry->object, entry->type);
                     break;
@@ -2760,7 +2752,7 @@ static HRESULT WINAPI d3d_device3_SetRenderState(IDirect3DDevice3 *iface,
                 break;
             }
 
-            surf = ddraw_get_object(&device->handle_table, value - 1, DDRAW_HANDLE_SURFACE);
+            surf = ddraw_get_object(NULL, value - 1, DDRAW_HANDLE_SURFACE);
             if (!surf)
             {
                 WARN("Invalid texture handle.\n");

dlls/ddraw/main.c
@@ -916,6 +916,10 @@ BOOL WINAPI DllMain(HINSTANCE inst, DWORD reason, void *reserved)
                         WARN("Material handle %#x (%p) not unset properly.\n", i + 1, entry->object);
                         break;
 
+                    case DDRAW_HANDLE_SURFACE:
+                        WARN("Texture handle %#x (%p) not unset properly.\n", i + 1, entry->object);
+                        break;
+
                     default:
                         WARN("Handle %#x (%p) has unknown type %#x.\n", i + 1, entry->object, entry->type);
                         break;

dlls/ddraw/executebuffer.c
@@ -347,14 +347,12 @@ HRESULT d3d_execute_buffer_execute(struct d3d_execute_buffer *buffer, struct d3d
 
                     instr += size;
 
-                    if (!(dst = ddraw_get_object(&device->handle_table,
-                            ci->hDestTexture - 1, DDRAW_HANDLE_SURFACE)))
+                    if (!(dst = ddraw_get_object(NULL, ci->hDestTexture - 1, DDRAW_HANDLE_SURFACE)))
                     {
                         WARN("Invalid destination texture handle %#lx.\n", ci->hDestTexture);
                         continue;
                     }
-                    if (!(src = ddraw_get_object(&device->handle_table,
-                            ci->hSrcTexture - 1, DDRAW_HANDLE_SURFACE)))
+                    if (!(src = ddraw_get_object(NULL, ci->hSrcTexture - 1, DDRAW_HANDLE_SURFACE)))
                     {
                         WARN("Invalid source texture handle %#lx.\n", ci->hSrcTexture);
                         continue;

dlls/ddraw/surface.c
@@ -5441,7 +5441,6 @@ static HRESULT WINAPI d3d_texture2_GetHandle(IDirect3DTexture2 *iface,
         IDirect3DDevice2 *device, D3DTEXTUREHANDLE *handle)
 {
     struct ddraw_surface *surface = impl_from_IDirect3DTexture2(iface);
-    struct d3d_device *device_impl = unsafe_impl_from_IDirect3DDevice2(device);
 
     TRACE("iface %p, device %p, handle %p.\n", iface, device, handle);
 
@@ -5449,7 +5448,7 @@ static HRESULT WINAPI d3d_texture2_GetHandle(IDirect3DTexture2 *iface,
 
     if (!surface->Handle)
     {
-        DWORD h = ddraw_allocate_handle(&device_impl->handle_table, surface, DDRAW_HANDLE_SURFACE);
+        DWORD h = ddraw_allocate_handle(NULL, surface, DDRAW_HANDLE_SURFACE);
         if (h == DDRAW_INVALID_HANDLE)
         {
             ERR("Failed to allocate a texture handle.\n");
@@ -6060,7 +6059,7 @@ static void STDMETHODCALLTYPE ddraw_surface_wined3d_object_destroyed(void *paren
 
     /* Having a texture handle set implies that the device still exists. */
     if (surface->Handle)
-        ddraw_free_handle(&surface->ddraw->d3ddevice->handle_table, surface->Handle - 1, DDRAW_HANDLE_SURFACE);
+        ddraw_free_handle(NULL, surface->Handle - 1, DDRAW_HANDLE_SURFACE);
 
     /* Reduce the ddraw surface count. */
     list_remove(&surface->surface_list_entry);

commit 6c157696e747a3d44aaa548b5d06f27854df9efd

ddraw: Use global handle table in d3d_device2_SwapTextureHandles().
Fixes a regression introduced by
5a7ff3b45c78784dca0a465d9e21483f72bce6e5.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57300

dlls/ddraw/ddraw_private.h
@@ -308,6 +308,7 @@ void ddraw_handle_table_destroy(struct ddraw_handle_table *t);
 DWORD ddraw_allocate_handle(struct ddraw_handle_table *t, void *object, enum ddraw_handle_type type);
 void *ddraw_free_handle(struct ddraw_handle_table *t, DWORD handle, enum ddraw_handle_type type);
 void *ddraw_get_object(struct ddraw_handle_table *t, DWORD handle, enum ddraw_handle_type type);
+extern struct ddraw_handle_table global_handle_table;
 
 struct d3d_device
 {

dlls/ddraw/device.c
@@ -562,7 +562,6 @@ static HRESULT WINAPI d3d_device1_GetCaps(IDirect3DDevice *iface,
 static HRESULT WINAPI d3d_device2_SwapTextureHandles(IDirect3DDevice2 *iface,
         IDirect3DTexture2 *tex1, IDirect3DTexture2 *tex2)
 {
-    struct d3d_device *device = impl_from_IDirect3DDevice2(iface);
     struct ddraw_surface *surf1 = unsafe_impl_from_IDirect3DTexture2(tex1);
     struct ddraw_surface *surf2 = unsafe_impl_from_IDirect3DTexture2(tex2);
     DWORD h1, h2;
@@ -573,8 +572,8 @@ static HRESULT WINAPI d3d_device2_SwapTextureHandles(IDirect3DDevice2 *iface,
 
     h1 = surf1->Handle - 1;
     h2 = surf2->Handle - 1;
-    device->handle_table.entries[h1].object = surf2;
-    device->handle_table.entries[h2].object = surf1;
+    global_handle_table.entries[h1].object = surf2;
+    global_handle_table.entries[h2].object = surf1;
     surf2->Handle = h1 + 1;
     surf1->Handle = h2 + 1;
 

dlls/ddraw/main.c
@@ -37,7 +37,7 @@ static HINSTANCE instance;
 /* value of ForceRefreshRate */
 DWORD force_refresh_rate = 0;
 
-static struct ddraw_handle_table global_handle_table;
+struct ddraw_handle_table global_handle_table;
 
 /* Structure for converting DirectDrawEnumerateA to DirectDrawEnumerateExA */
 struct callback_info

dlls/ddraw/tests/ddraw1.c
@@ -15587,16 +15587,16 @@ static void test_multiple_devices(void)
         0.0f, 0.0f, 0.0f, 4.0f,
     };
 
-    D3DTEXTUREHANDLE texture_handle, texture_handle2;
+    D3DTEXTUREHANDLE texture_handle, texture_handle2, texture_handle3;
+    IDirectDrawSurface *texture_surf, *texture_surf2;
     D3DMATERIALHANDLE mat_handle, mat_handle2;
     IDirect3DViewport *viewport, *viewport2;
+    IDirect3DTexture *texture, *texture2;
     IDirect3DDevice *device, *device2;
-    IDirectDrawSurface *texture_surf;
     D3DMATRIXHANDLE matrix_handle;
     IDirectDraw *ddraw, *ddraw2;
     IDirect3DMaterial *material;
     DDSURFACEDESC surface_desc;
-    IDirect3DTexture *texture;
     D3DMATRIX matrix;
     ULONG refcount;
     HWND window;
@@ -15648,11 +15648,26 @@ static void test_multiple_devices(void)
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     hr = IDirectDrawSurface_QueryInterface(texture_surf, &IID_IDirect3DTexture, (void **)&texture);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirectDraw_CreateSurface(ddraw, &surface_desc, &texture_surf2, NULL);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirectDrawSurface_QueryInterface(texture_surf2, &IID_IDirect3DTexture, (void **)&texture2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+
     hr = IDirect3DTexture_GetHandle(texture, device, &texture_handle);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     hr = IDirect3DTexture_GetHandle(texture, device2, &texture_handle2);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     ok(texture_handle == texture_handle2, "got same handles.\n");
+    hr = IDirect3DTexture_GetHandle(texture2, device, &texture_handle2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirect3DDevice_SwapTextureHandles(device, texture, texture2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirect3DTexture_GetHandle(texture, device, &texture_handle3);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    ok(texture_handle3 == texture_handle2, "got different handles.\n");
+    hr = IDirect3DTexture_GetHandle(texture2, device2, &texture_handle3);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    ok(texture_handle3 == texture_handle, "got different handles.\n");
 
     hr = IDirect3DDevice_CreateMatrix(device, &matrix_handle);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
@@ -15664,6 +15679,8 @@ static void test_multiple_devices(void)
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     ok(!memcmp(&matrix, &test_matrix, sizeof(matrix)), "matrix does not match.\n");
 
+    IDirect3DTexture_Release(texture2);
+    IDirectDrawSurface_Release(texture_surf2);
     IDirect3DTexture_Release(texture);
     IDirectDrawSurface_Release(texture_surf);
     IDirect3DMaterial_Release(material);

dlls/ddraw/tests/ddraw2.c
@@ -16504,15 +16504,15 @@ static void run_for_each_device_type(void (*test_func)(const GUID *))
 
 static void test_multiple_devices(void)
 {
-    D3DTEXTUREHANDLE texture_handle, texture_handle2;
+    D3DTEXTUREHANDLE texture_handle, texture_handle2, texture_handle3;
+    IDirectDrawSurface *surface, *texture_surf, *texture_surf2;
     IDirect3DDevice2 *device, *device2, *device3;
-    IDirectDrawSurface *surface, *texture_surf;
     D3DMATERIALHANDLE mat_handle, mat_handle2;
     IDirect3DViewport2 *viewport, *viewport2;
+    IDirect3DTexture2 *texture, *texture2;
     IDirectDraw2 *ddraw, *ddraw2;
     IDirect3DMaterial2 *material;
     DDSURFACEDESC surface_desc;
-    IDirect3DTexture2 *texture;
     IDirect3D2 *d3d;
     ULONG refcount;
     DWORD value;
@@ -16604,6 +16604,11 @@ static void test_multiple_devices(void)
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     hr = IDirectDrawSurface_QueryInterface(texture_surf, &IID_IDirect3DTexture2, (void **)&texture);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirectDraw2_CreateSurface(ddraw, &surface_desc, &texture_surf2, NULL);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirectDrawSurface_QueryInterface(texture_surf2, &IID_IDirect3DTexture2, (void **)&texture2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+
     hr = IDirect3DTexture2_GetHandle(texture, device, &texture_handle);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     hr = IDirect3DTexture2_GetHandle(texture, device2, &texture_handle2);
@@ -16612,14 +16617,33 @@ static void test_multiple_devices(void)
     hr = IDirect3DTexture2_GetHandle(texture, device3, &texture_handle2);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     ok(texture_handle == texture_handle2, "got different handles.\n");
+    hr = IDirect3DTexture2_GetHandle(texture2, device, &texture_handle2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    ok(texture_handle != texture_handle2, "got same handles.\n");
+    hr = IDirect3DDevice2_SwapTextureHandles(device, texture, texture2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirect3DTexture2_GetHandle(texture, device, &texture_handle3);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    ok(texture_handle3 == texture_handle2, "got different handles.\n");
+    hr = IDirect3DTexture2_GetHandle(texture2, device2, &texture_handle3);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    ok(texture_handle3 == texture_handle, "got different handles.\n");
+
     hr = IDirect3DDevice2_SetRenderState(device, D3DRENDERSTATE_TEXTUREHANDLE, texture_handle);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     hr = IDirect3DDevice2_SetRenderState(device2, D3DRENDERSTATE_TEXTUREHANDLE, texture_handle);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
     hr = IDirect3DDevice2_SetRenderState(device3, D3DRENDERSTATE_TEXTUREHANDLE, texture_handle);
     ok(hr == D3D_OK, "got %#lx.\n", hr);
+    hr = IDirect3DDevice2_SwapTextureHandles(device, texture, texture2);
+    ok(hr == S_OK, "got %#lx.\n", hr);
+    hr = IDirect3DDevice2_GetRenderState(device, D3DRENDERSTATE_TEXTUREHANDLE, &texture_handle2);
+    ok(hr == D3D_OK, "got %#lx.\n", hr);
+    todo_wine ok(texture_handle2 == texture_handle, "got different handles.\n");
 
+    IDirect3DTexture2_Release(texture2);
     IDirect3DTexture2_Release(texture);
+    IDirectDrawSurface_Release(texture_surf2);
     IDirectDrawSurface_Release(texture_surf);
     IDirect3DMaterial2_Release(material);
     IDirect3DViewport2_Release(viewport);