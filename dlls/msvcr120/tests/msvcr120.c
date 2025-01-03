a62ecc913bfb201c2443077e6b0f91bc192ad8ff
Piotr Caban <piotr@codeweavers.com>
2025-01-03 15:59:00 +0100

    msvcr120/tests: Skip _fsopen tests if file can't be created.

--- dlls/msvcr120/tests/msvcr120.c
+++ dlls/msvcr120/tests/msvcr120.c
@@ -1840,6 +1840,7 @@ static void test__fsopen(void)
     int i, ret;
     FILE *f;
     wchar_t wpath[MAX_PATH];
+    HANDLE h;
     static const struct {
         const char *loc;
         const char *path;
@@ -1862,8 +1863,17 @@ static void test__fsopen(void)
         ok(ret, "MultiByteToWideChar failed on %s with locale %s: %lx\n",
             tests[i].path, tests[i].loc, GetLastError());
 
-        f = p__fsopen(tests[i].path, "w", SH_DENYNO);
-        ok(!!f, "failed to create %s with locale %s\n", tests[i].path, tests[i].loc);
+        h = CreateFileW(wpath, GENERIC_READ | GENERIC_WRITE, 0, NULL,
+                CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+        if (h == INVALID_HANDLE_VALUE)
+        {
+            skip("can't create test file (%s)\n", wine_dbgstr_w(wpath));
+            continue;
+        }
+        CloseHandle(h);
+
+        f = p__fsopen(tests[i].path, "r", SH_DENYNO);
+        ok(!!f, "failed to create %s with locale %s\n", wine_dbgstr_a(tests[i].path), tests[i].loc);
         p_fclose(f);
 
         f = p__wfsopen(wpath, L"r", SH_DENYNO);
