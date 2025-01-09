wine-mirror/wine

commit 500470029b67a2a07b68e4ce8a134c6819b9b838

mf: Handle media source EndGetEvent() failure due to shutdown.
Windows robustly handles asynchronous shutdown of a media source used in
a session.

dlls/mf/session.c
@@ -1001,6 +1001,34 @@ static void session_flush_nodes(struct media_session *session)
     }
 }
 
+static void session_purge_pending_commands(struct media_session *session)
+{
+    struct session_op *op, *op2;
+
+    /* Purge all commands which are no longer valid after a forced source shutdown.
+     * Calling Stop() in this case is not required in native Windows. */
+    LIST_FOR_EACH_ENTRY_SAFE(op, op2, &session->commands, struct session_op, entry)
+    {
+        if (op->command == SESSION_CMD_SET_TOPOLOGY)
+            break;
+        if (op->command == SESSION_CMD_CLEAR_TOPOLOGIES || op->command == SESSION_CMD_CLOSE
+                || op->command == SESSION_CMD_SHUTDOWN)
+            continue;
+        list_remove(&op->entry);
+        IUnknown_Release(&op->IUnknown_iface);
+    }
+}
+
+static void session_reset(struct media_session *session)
+{
+    /* Media sessions in native Windows are not consistently usable after a
+     * forced source shutdown, but we try to clean up as well as possible. */
+    session->state = SESSION_STATE_STOPPED;
+    session_clear_presentation(session);
+    session_purge_pending_commands(session);
+    session_command_complete(session);
+}
+
 static void session_start(struct media_session *session, const GUID *time_format, const PROPVARIANT *start_position)
 {
     struct media_source *source;
@@ -2829,6 +2857,46 @@ static const IMFAsyncCallbackVtbl session_sa_ready_callback_vtbl =
     session_sa_ready_callback_Invoke,
 };
 
+static void session_handle_source_shutdown(struct media_session *session)
+{
+    BOOL finalize_sinks;
+
+    EnterCriticalSection(&session->cs);
+
+    finalize_sinks = session->presentation.flags & SESSION_FLAG_FINALIZE_SINKS;
+
+    /* When stopping the session, MESessionStopped is sent without waiting
+     * for MESourceStopped, so we need do nothing in that case. */
+    switch (session->state)
+    {
+        case SESSION_STATE_STARTING_SOURCES:
+        case SESSION_STATE_RESTARTING_SOURCES:
+        case SESSION_STATE_PREROLLING_SINKS:
+        case SESSION_STATE_STARTING_SINKS:
+            IMFMediaEventQueue_QueueEventParamVar(session->event_queue, MESessionStarted, &GUID_NULL,
+                    MF_E_INVALIDREQUEST, NULL);
+            break;
+        case SESSION_STATE_STOPPING_SINKS:
+        case SESSION_STATE_STOPPING_SOURCES:
+            if (!finalize_sinks)
+                IMFMediaEventQueue_QueueEventParamVar(session->event_queue, MESessionStopped, &GUID_NULL,
+                        MF_E_INVALIDREQUEST, NULL);
+            break;
+        default:
+            break;
+    }
+
+    if (session->state != SESSION_STATE_CLOSED)
+    {
+        if (finalize_sinks)
+            session_finalize_sinks(session);
+        else
+            session_reset(session);
+    }
+
+    LeaveCriticalSection(&session->cs);
+}
+
 static HRESULT WINAPI session_events_callback_QueryInterface(IMFAsyncCallback *iface, REFIID riid, void **obj)
 {
     if (IsEqualIID(riid, &IID_IMFAsyncCallback) ||
@@ -4050,8 +4118,15 @@ static HRESULT WINAPI session_events_callback_Invoke(IMFAsyncCallback *iface, IM
 
     if (FAILED(hr = IMFMediaEventGenerator_EndGetEvent(event_source, result, &event)))
     {
-        WARN("Failed to get event from %p, hr %#lx.\n", event_source, hr);
-        IMFMediaEventQueue_QueueEventParamVar(session->event_queue, MEError, &GUID_NULL, hr, NULL);
+        if (hr == MF_E_SHUTDOWN)
+        {
+            session_handle_source_shutdown(session);
+        }
+        else
+        {
+            WARN("Failed to get event from %p, hr %#lx.\n", event_source, hr);
+            IMFMediaEventQueue_QueueEventParamVar(session->event_queue, MEError, &GUID_NULL, hr, NULL);
+        }
         IMFMediaEventGenerator_Release(event_source);
         return hr;
     }

commit 7af71662366406a899e1bca35f638a0fa245dec4

mf: Do not clean up a session op if it was submitted to a work queue.
Fixes: 500470029b67a2a07b68e4ce8a134c6819b9b838

dlls/mf/session.c
@@ -56,6 +56,7 @@ struct session_op
     IUnknown IUnknown_iface;
     LONG refcount;
     enum session_command command;
+    BOOL submitted;
     union
     {
         struct
@@ -473,7 +474,10 @@ static HRESULT session_submit_command(struct media_session *session, struct sess
     if (SUCCEEDED(hr = session_is_shut_down(session)))
     {
         if (list_empty(&session->commands) && !(session->presentation.flags & SESSION_FLAG_PENDING_COMMAND))
+        {
             hr = MFPutWorkItem(MFASYNC_CALLBACK_QUEUE_STANDARD, &session->commands_callback, &op->IUnknown_iface);
+            op->submitted = SUCCEEDED(hr);
+        }
         if (op->command == SESSION_CMD_SHUTDOWN)
             list_add_head(&session->commands, &op->entry);
         else
@@ -859,6 +863,11 @@ static void session_clear_command_list(struct media_session *session)
 
     LIST_FOR_EACH_ENTRY_SAFE(op, op2, &session->commands, struct session_op, entry)
     {
+        /* Checking this flag is unnecessary if this function is only called
+         * from the callback or upon release, but do it for consistency and
+         * in case a call from elsewhere is added. */
+        if (op->submitted)
+            continue;
         list_remove(&op->entry);
         IUnknown_Release(&op->IUnknown_iface);
     }
@@ -955,6 +964,7 @@ static void session_command_complete(struct media_session *session)
 {
     struct session_op *op;
     struct list *e;
+    HRESULT hr;
 
     session->presentation.flags &= ~SESSION_FLAG_PENDING_COMMAND;
 
@@ -962,7 +972,8 @@ static void session_command_complete(struct media_session *session)
     if ((e = list_head(&session->commands)))
     {
         op = LIST_ENTRY(e, struct session_op, entry);
-        MFPutWorkItem(MFASYNC_CALLBACK_QUEUE_STANDARD, &session->commands_callback, &op->IUnknown_iface);
+        hr = MFPutWorkItem(MFASYNC_CALLBACK_QUEUE_STANDARD, &session->commands_callback, &op->IUnknown_iface);
+        op->submitted = SUCCEEDED(hr);
     }
 }
 
@@ -1020,6 +1031,10 @@ static void session_purge_pending_commands(struct media_session *session)
             break;
         if (op->command == SESSION_CMD_CLEAR_TOPOLOGIES || op->command == SESSION_CMD_CLOSE
                 || op->command == SESSION_CMD_SHUTDOWN)
+            continue;
+        /* Once a command is submitted, the callback becomes responsible
+         * for removal from the list and release of the ref. */
+        if (op->submitted)
             continue;
         list_remove(&op->entry);
         IUnknown_Release(&op->IUnknown_iface);