wine-mirror/wine

commit 895ca2eda64f506b936999cdd8f14e224eef6a7f

winex11: Don't update Win32 window position for offscreen windows.
The window manager might decide to move window outside of what we
believe is the virtual screen, for instance to implement virtual
desktops, or for embedded windows.

We don't need to tell Win32 about it, as it will otherwise end up
triggering the offscreen window logic in WindowPosChanged. We want to
keep that offscreen window logic for windows which are moved offscreen
by applications to hide them.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57472
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=57474

dlls/winex11.drv/window.c
@@ -1593,6 +1593,9 @@ static UINT window_update_client_config( struct x11drv_win_data *data )
     if (rect.right == old_rect.right && rect.bottom == old_rect.bottom) flags |= SWP_NOSIZE;
     else if (IsRectEmpty( &rect )) flags |= SWP_NOSIZE;
 
+    /* don't sync win32 position for offscreen windows */
+    if (!is_window_rect_mapped( &new_rect )) flags |= SWP_NOMOVE;
+
     if ((flags & (SWP_NOSIZE | SWP_NOMOVE)) == (SWP_NOSIZE | SWP_NOMOVE)) return 0;
 
     /* avoid event feedback loops from window rect adjustments of maximized / fullscreen windows */

commit b936f7426e33508538f48f7d20e7d7cb47cc1f32

winex11: Respect swp_flags when syncing window position.
Since 895ca2eda64f506b936999cdd8f14e224eef6a7f, we're not telling Win32
side of the fact the window moved offscreen. This means any SetWindowPos
sent afterwards has potential to resync the Window and "put it on screen
again", even if such operation has SWP_NOMOVE or SWP_NOSIZE. This causes
some fullscreen apps to follow the workspace/desktop instead of remaining
offscreen.

Fixes a regression introduced by 895ca2eda64f506b936999cdd8f14e224eef6a7f.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

dlls/winex11.drv/window.c
@@ -1778,6 +1778,7 @@ static void sync_window_position( struct x11drv_win_data *data, UINT swp_flags )
     DWORD style = NtUserGetWindowLongW( data->hwnd, GWL_STYLE );
     DWORD ex_style = NtUserGetWindowLongW( data->hwnd, GWL_EXSTYLE );
     BOOL above = FALSE;
+    RECT new_rect;
 
     if (data->managed && data->desired_state.wm_state == IconicState) return;
 
@@ -1795,7 +1796,19 @@ static void sync_window_position( struct x11drv_win_data *data, UINT swp_flags )
     set_size_hints( data, style );
     set_mwm_hints( data, style, ex_style );
     update_net_wm_states( data );
-    window_set_config( data, &data->rects.visible, above );
+
+    new_rect = data->rects.visible;
+    if (swp_flags & SWP_NOSIZE)
+    {
+        new_rect.right = new_rect.left + data->desired_state.rect.right - data->desired_state.rect.left;
+        new_rect.bottom = new_rect.top + data->desired_state.rect.bottom - data->desired_state.rect.top;
+    }
+    if (swp_flags & SWP_NOMOVE)
+    {
+        OffsetRect( &new_rect, data->desired_state.rect.left - new_rect.left,
+                    data->desired_state.rect.top - new_rect.top );
+    }
+    window_set_config( data, &new_rect, above );
 }
 
 