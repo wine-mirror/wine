d54f5d4c591c80d050223f46cf4f4a0ecab3405a
Daniel Lehman <dlehman25@gmail.com>
2025-02-21 19:05:04 -0800

    dwrite: Fix spelling for Sundanese.

--- dlls/dwrite/analyzer.c
+++ dlls/dwrite/analyzer.c
@@ -287,7 +287,7 @@ system_fallback_config[] =
     { "1A00-1A1F",              L"Noto Sans Buginese" },
     { "1A20-1AAF",              L"Noto Sans Tai Tham" },
     { "1B00-1B7F",              L"Noto Sans Balinese" },
-    { "1B80-1BBF, 1CC0-1CCF",   L"Noto Sans Sundanes" },
+    { "1B80-1BBF, 1CC0-1CCF",   L"Noto Sans Sundanese" },
     { "1BC0-1BFF",              L"Noto Sans Batak" },
     { "1C00-1C4F",              L"Noto Sans Lepcha" },
     { "1C50-1C7F",              L"Noto Sans Ol Chiki" },
