206dd8f6d26aeee04a53d8d752e4dbb52cef35d9
Bernhard Übelacker <bernhardu@mailbox.org>
2025-03-03 16:34:17 +0100

    d2d1/tests: Adjust tolerance of a few float comparisons in windows 7.

--- dlls/d2d1/tests/d2d1.c
+++ dlls/d2d1/tests/d2d1.c
@@ -15274,13 +15274,13 @@ static void test_compute_geometry_area(BOOL d3d11)
     todo_wine
     ok(hr == S_OK, "Got unexpected hr %#lx.\n", hr);
     if (hr == S_OK)
-        ok(compare_float(area, 156.9767f, 0), "Unexpected value %.8e.\n", area);
+        ok(compare_float(area, 156.9767f, 3), "Unexpected value %.8e.\n", area); /* Only win7 needs increased ulps. */
 
     hr = ID2D1EllipseGeometry_ComputeArea(ellipse_geometry, NULL, 200.0f, &area);
     todo_wine
     ok(hr == S_OK, "Got unexpected hr %#lx.\n", hr);
     if (hr == S_OK)
-        ok(compare_float(area, 100.0f, 0), "Unexpected value %.8e.\n", area);
+        ok(compare_float(area, 100.0f, 2), "Unexpected value %.8e.\n", area); /* Only win7 needs increased ulps. */
 
     set_matrix_identity(&matrix);
     scale_matrix(&matrix, 1.0f, 2.0f);
@@ -15288,13 +15288,13 @@ static void test_compute_geometry_area(BOOL d3d11)
     todo_wine
     ok(hr == S_OK, "Got unexpected hr %#lx.\n", hr);
     if (hr == S_OK)
-        ok(compare_float(area, 314.12088f, 0), "Unexpected value %.8e.\n", area);
+        ok(compare_float(area, 314.12088f, 2), "Unexpected value %.8e.\n", area); /* Only win7 needs increased ulps. */
 
     hr = ID2D1EllipseGeometry_ComputeArea(ellipse_geometry, &matrix, 200.0f, &area);
     todo_wine
     ok(hr == S_OK, "Got unexpected hr %#lx.\n", hr);
     if (hr == S_OK)
-        ok(compare_float(area, 200.0f, 0), "Unexpected value %.8e.\n", area);
+        ok(compare_float(area, 200.0f, 1), "Unexpected value %.8e.\n", area); /* Only win7 needs increased ulps. */
 
     ID2D1EllipseGeometry_Release(ellipse_geometry);
 
