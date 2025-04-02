bad9425328b994bb19dd233d2d690d2eea60876b
André Zwing <nerv@dawncrow.de>
2025-04-02 21:20:27 +0200

    kernel32/tests: Don't test function directly when reporting GetLastError().

--- dlls/kernel32/tests/file.c
+++ dlls/kernel32/tests/file.c
@@ -3343,6 +3343,8 @@ static void test_async_file_errors(void)
     HANDLE hFile;
     LPVOID lpBuffer = HeapAlloc(GetProcessHeap(), 0, 4096);
     OVERLAPPED ovl;
+    BOOL res;
+
     ovl.Offset = 0;
     ovl.OffsetHigh = 0;
     ovl.hEvent = hSem;
@@ -3358,7 +3360,6 @@ static void test_async_file_errors(void)
     ok(hFile != INVALID_HANDLE_VALUE, "CreateFileA(%s ...) failed\n", szFile);
     while (TRUE)
     {
-        BOOL res;
         DWORD count;
         while (WaitForSingleObjectEx(hSem, INFINITE, TRUE) == WAIT_IO_COMPLETION)
             ;
@@ -3378,8 +3379,10 @@ static void test_async_file_errors(void)
     /*printf("Error = %ld\n", GetLastError());*/
 
     SleepEx(0, TRUE); /* Flush pending APCs */
-    ok(CloseHandle(hFile), "CloseHandle: error %ld\n", GetLastError());
-    ok(CloseHandle(hSem), "CloseHandle: error %ld\n", GetLastError());
+    res = CloseHandle(hFile);
+    ok(res, "CloseHandle: error %ld\n", GetLastError());
+    res = CloseHandle(hSem);
+    ok(res, "CloseHandle: error %ld\n", GetLastError());
     HeapFree(GetProcessHeap(), 0, lpBuffer);
 }
 
