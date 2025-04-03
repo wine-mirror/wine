wine-mirror/wine

commit 7c11ce8d44f1758a855c4d3c976825f8afe5fbb2

mshtml: Keep the link from the inner window to the outer window even when detached.
Until the outer window is actually unlinked.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

dlls/mshtml/htmlwindow.c
@@ -138,7 +138,6 @@ static void detach_inner_window(HTMLInnerWindow *window)
     abort_window_bindings(window);
     release_script_hosts(window);
     unlink_ref(&window->jscript);
-    window->base.outer_window = NULL;
 
     if(outer_window && outer_window->base.inner_window == window) {
         outer_window->base.inner_window = NULL;
@@ -3779,6 +3778,8 @@ static void HTMLWindow_destructor(DispatchEx *dispex)
     HTMLInnerWindow *This = impl_from_DispatchEx(dispex);
     unsigned i;
 
+    if(This->base.outer_window)
+        list_remove(&This->outer_window_entry);
     VariantClear(&This->performance);
 
     for(i = 0; i < This->global_prop_cnt; i++)
@@ -4295,7 +4296,6 @@ static nsresult NSAPI outer_window_unlink(void *p)
     if(window->pending_window) {
         HTMLInnerWindow *pending_window = window->pending_window;
         abort_window_bindings(pending_window);
-        pending_window->base.outer_window = NULL;
         window->pending_window = NULL;
         IHTMLWindow2_Release(&pending_window->base.IHTMLWindow2_iface);
     }
@@ -4317,6 +4317,11 @@ static nsresult NSAPI outer_window_unlink(void *p)
     if(window->window_proxy) {
         unlink_ref(&window->window_proxy);
         wine_rb_remove(&window_map, &window->entry);
+    }
+    while(!list_empty(&window->inner_windows)) {
+        HTMLInnerWindow *inner_window = LIST_ENTRY(list_head(&window->inner_windows), HTMLInnerWindow, outer_window_entry);
+        list_remove(&inner_window->outer_window_entry);
+        inner_window->base.outer_window = NULL;
     }
     return NS_OK;
 }
@@ -4399,6 +4404,7 @@ static HRESULT create_inner_window(HTMLOuterWindow *outer_window, IMoniker *mon,
 
     window->base.outer_window = outer_window;
     window->base.inner_window = window;
+    list_add_tail(&outer_window->inner_windows, &window->outer_window_entry);
 
     init_event_target(&window->event_target, &Window_dispex, NULL);
 
@@ -4431,6 +4437,7 @@ HRESULT create_outer_window(GeckoBrowser *browser, mozIDOMWindowProxy *mozwindow
     window->base.inner_window = NULL;
     window->browser = browser;
     list_add_head(&browser->outer_windows, &window->browser_entry);
+    list_init(&window->inner_windows);
     ccref_init(&window->ccref, 1);
 
     mozIDOMWindowProxy_AddRef(mozwindow);
@@ -4483,7 +4490,6 @@ HRESULT create_pending_window(HTMLOuterWindow *outer_window, nsChannelBSC *chann
 
     if(outer_window->pending_window) {
         abort_window_bindings(outer_window->pending_window);
-        outer_window->pending_window->base.outer_window = NULL;
         IHTMLWindow2_Release(&outer_window->pending_window->base.IHTMLWindow2_iface);
     }
 

dlls/mshtml/htmlanchor.c
@@ -49,7 +49,7 @@ static HRESULT navigate_href_new_window(HTMLElement *element, nsAString *href_st
     IUri *uri;
     HRESULT hres;
 
-    if(!element->node.doc->window->base.outer_window)
+    if(is_detached_window(element->node.doc->window))
         return S_OK;
 
     nsAString_GetData(href_str, &href);
@@ -114,7 +114,7 @@ static HRESULT navigate_href(HTMLElement *element, nsAString *href_str, nsAStrin
     const PRUnichar *href;
     HRESULT hres;
 
-    if(!element->node.doc->window->base.outer_window)
+    if(is_detached_window(element->node.doc->window))
         return S_OK;
 
     window = get_target_window(element->node.doc->window->base.outer_window, target_str, &use_new_window);

dlls/mshtml/htmldoc.c
@@ -816,7 +816,7 @@ static HRESULT WINAPI HTMLDocument_get_frames(IHTMLDocument2 *iface, IHTMLFrames
         /* Not implemented by IE */
         return E_NOTIMPL;
     }
-    if(!This->window->base.outer_window)
+    if(is_detached_window(This->window))
         return E_FAIL;
     return IHTMLWindow2_get_frames(&This->window->base.outer_window->base.IHTMLWindow2_iface, p);
 }
@@ -1000,7 +1000,7 @@ static HRESULT WINAPI HTMLDocument_put_URL(IHTMLDocument2 *iface, BSTR v)
 
     TRACE("(%p)->(%s)\n", This, debugstr_w(v));
 
-    if(!This->window || !This->window->base.outer_window) {
+    if(!This->window || is_detached_window(This->window)) {
         FIXME("No window available\n");
         return E_FAIL;
     }
@@ -1015,7 +1015,7 @@ static HRESULT WINAPI HTMLDocument_get_URL(IHTMLDocument2 *iface, BSTR *p)
 
     TRACE("(%p)->(%p)\n", iface, p);
 
-    if(This->window && !This->window->base.outer_window) {
+    if(This->window && is_detached_window(This->window)) {
         WARN("detached document\n");
         return E_FAIL;
     }
@@ -1064,7 +1064,7 @@ static HRESULT WINAPI HTMLDocument_get_domain(IHTMLDocument2 *iface, BSTR *p)
         return E_NOTIMPL;
     }
 
-    if(This->window && (!This->window->base.outer_window || !This->window->base.outer_window->uri))
+    if(This->window && (is_detached_window(This->window) || !This->window->base.outer_window->uri))
         return E_FAIL;
 
     nsAString_Init(&nsstr, NULL);
@@ -1094,7 +1094,7 @@ static HRESULT WINAPI HTMLDocument_put_cookie(IHTMLDocument2 *iface, BSTR v)
 
     if(!This->window)
         return S_OK;
-    if(!This->window->base.outer_window)
+    if(is_detached_window(This->window))
         return E_FAIL;
 
     bret = InternetSetCookieExW(This->window->base.outer_window->url, NULL, v, 0, 0);
@@ -1118,7 +1118,7 @@ static HRESULT WINAPI HTMLDocument_get_cookie(IHTMLDocument2 *iface, BSTR *p)
         *p = NULL;
         return S_OK;
     }
-    if(!This->window->base.outer_window)
+    if(is_detached_window(This->window))
         return E_FAIL;
 
     size = 0;
@@ -1367,7 +1367,7 @@ static HRESULT WINAPI HTMLDocument_open(IHTMLDocument2 *iface, BSTR url, VARIANT
 
     *pomWindowResult = NULL;
 
-    if(!This->window || !This->window->base.outer_window)
+    if(!This->window || is_detached_window(This->window))
         return E_FAIL;
 
     if(!This->dom_document) {
@@ -2224,7 +2224,7 @@ static HRESULT WINAPI HTMLDocument3_get_documentElement(IHTMLDocument3 *iface, I
     TRACE("(%p)->(%p)\n", This, p);
 
     if(This->window) {
-        if(!This->window->base.outer_window)
+        if(is_detached_window(This->window))
             return E_FAIL;
         if(This->window->base.outer_window->readystate == READYSTATE_UNINITIALIZED) {
             *p = NULL;
@@ -4493,7 +4493,7 @@ static void HTMLDocumentNode_on_advise(IUnknown *iface, cp_static_data_t *cp)
 {
     HTMLDocumentNode *This = CONTAINING_RECORD((IHTMLDocument2*)iface, HTMLDocumentNode, IHTMLDocument2_iface);
 
-    if(This->window && This->window->base.outer_window)
+    if(This->window && !is_detached_window(This->window))
         update_doc_cp_events(This, cp);
 }
 
@@ -5589,7 +5589,7 @@ static HRESULT HTMLDocumentNode_location_hook(DispatchEx *dispex, WORD flags, DI
 
     if(!(flags & DISPATCH_PROPERTYPUT) || !This->window)
         return S_FALSE;
-    if(!This->window->base.outer_window)
+    if(is_detached_window(This->window))
         return E_FAIL;
 
     return IWineJSDispatchHost_InvokeEx(&This->window->event_target.dispex.IWineJSDispatchHost_iface,

dlls/mshtml/htmlform.c
@@ -519,7 +519,7 @@ static HRESULT WINAPI HTMLFormElement_submit(IHTMLFormElement *iface)
 
     if(This->element.node.doc) {
         HTMLDocumentNode *doc = This->element.node.doc;
-        if(doc->window && doc->window->base.outer_window)
+        if(doc->window && !is_detached_window(doc->window))
             this_window = doc->window->base.outer_window;
     }
     if(!this_window) {

dlls/mshtml/htmlframe.c
@@ -79,7 +79,7 @@ static HRESULT WINAPI HTMLFrameBase_put_src(IHTMLFrameBase *iface, BSTR v)
 
     TRACE("(%p)->(%s)\n", This, debugstr_w(v));
 
-    if(!This->content_window || !This->element.node.doc || !This->element.node.doc->window || !This->element.node.doc->window->base.outer_window) {
+    if(!This->content_window || !This->element.node.doc || !This->element.node.doc->window || is_detached_window(This->element.node.doc->window)) {
         nsAString nsstr;
         nsresult nsres;
 

dlls/mshtml/htmlstorage.c
@@ -332,7 +332,6 @@ static HRESULT send_storage_event(HTMLStorage *storage, BSTR key, BSTR old_value
 
     ctx.url = NULL;
 
-    /* FIXME: Events are actually sent to the current window on native, even if we're detached. */
     if(!window->base.outer_window)
         goto done;
 

dlls/mshtml/mshtml_private.h
@@ -752,6 +752,7 @@ struct HTMLOuterWindow {
     BSTR url;
     DWORD load_flags;
 
+    struct list inner_windows;
     struct list sibling_entry;
     struct wine_rb_entry entry;
 };
@@ -818,6 +819,8 @@ struct HTMLInnerWindow {
     ULONGLONG load_event_start_time;
     ULONGLONG load_event_end_time;
     ULONGLONG first_paint_time;
+
+    struct list outer_window_entry;
 };
 
 typedef enum {
@@ -1310,6 +1313,11 @@ HRESULT nscolor_to_str(LPCWSTR color, BSTR *ret);
 static inline BOOL is_main_content_window(HTMLOuterWindow *window)
 {
     return window->browser && window == window->browser->content_window;
+}
+
+static inline BOOL is_detached_window(HTMLInnerWindow *window)
+{
+    return !window->base.outer_window || (window->base.outer_window->base.inner_window != window && window->base.outer_window->pending_window != window);
 }
 
 struct HTMLAttributeCollection {

dlls/mshtml/mutation.c
@@ -446,7 +446,7 @@ static void set_document_mode(HTMLDocumentNode *doc, compat_mode_t document_mode
 
     TRACE("%p: %d\n", doc, document_mode);
 
-    max_compat_mode = doc->window && doc->window->base.outer_window
+    max_compat_mode = doc->window && !is_detached_window(doc->window)
         ? get_max_compat_mode(doc->window->base.outer_window->uri)
         : COMPAT_MODE_IE11;
     if(max_compat_mode < document_mode) {

commit 96837afc0bfc93ebbe435989da91f6f114671864

mshtml: Don't mess with the outer window if we're already detached.
Since now we always keep outer_window pointer valid (if it exists), we
could e.g. unlink location props from an already detached inner window. This
restores previous behavior on detach.

Fixes a regression introduced by 7c11ce8d44f1758a855c4d3c976825f8afe5fbb2.

Signed-off-by: Gabriel Ivăncescu <gabrielopcode@gmail.com>

dlls/mshtml/htmlwindow.c
@@ -108,7 +108,7 @@ static inline HRESULT get_window_event(HTMLWindow *window, eventid_t eid, VARIAN
 
 static void detach_inner_window(HTMLInnerWindow *window)
 {
-    HTMLOuterWindow *outer_window = window->base.outer_window;
+    HTMLOuterWindow *outer_window = is_detached_window(window) ? NULL : window->base.outer_window;
     HTMLDocumentNode *doc = window->doc, *doc_iter;
 
     while(!list_empty(&window->children)) {