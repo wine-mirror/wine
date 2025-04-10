wine-mirror/wine

commit 86f3e21d659b3c94eccc42353467cbdbbbf80eeb

cmd: Don't return syntax error code on empty lines.
Signed-off-by: Eric Pouech <epouech@codeweavers.com>

programs/cmd/wcmdmain.c
@@ -2536,6 +2536,11 @@ static BOOL node_builder_generate(struct node_builder *builder, CMD_NODE **node)
     }
     else
     {
+        if (!builder->num) /* line without tokens */
+        {
+            *node = NULL;
+            return TRUE;
+        }
         if (node_builder_parse(builder, 0, node) &&
             builder->pos + 1 >= builder->num) /* consumed all tokens? */
             return TRUE;

programs/cmd/batch.c
@@ -38,8 +38,11 @@ static RETURN_CODE WCMD_batch_main_loop(void)
             context->skip_rest = TRUE;
             break;
         case RPL_SUCCESS:
-            return_code = node_execute(node);
-            node_dispose_tree(node);
+            if (node)
+            {
+                return_code = node_execute(node);
+                node_dispose_tree(node);
+            }
             break;
         case RPL_SYNTAXERROR:
             return_code = RETURN_CODE_SYNTAX_ERROR;

programs/cmd/tests/test_builtins.bat.exp
@@ -32,7 +32,7 @@ FAILURE 1
 SUCCESS 0
 FAILURE 1024
 SUCCESS 0
-@todo_wine@SUCCESS 0
+SUCCESS 0
 FAILURE 666
 FAILURE 2
 FAILURE 666

programs/cmd/tests/test_builtins.cmd.exp
@@ -483,7 +483,7 @@ FAILURE 1
 SUCCESS 0
 FAILURE 1024
 SUCCESS 0
-@todo_wine@SUCCESS 0
+SUCCESS 0
 FAILURE 666
 FAILURE 2
 FAILURE 666

commit 277bafaaed94a67248e868decb73fc3ec29b6df0

cmd: Fix infinite loop in FOR /L.
Likely regression introduced by 86f3e21d659b3c94eccc42353467cbdbbbf80eeb.
FOR /L %v in () DO ...
should execute as an infinite loop.

Signed-off-by: Eric Pouech <epouech@codeweavers.com>

programs/cmd/wcmdmain.c
@@ -3614,8 +3614,13 @@ static RETURN_CODE for_control_execute_numbers(CMD_FOR_CONTROL *for_ctrl, CMD_NO
     int numbers[3] = {0, 0, 0}, var;
     int i;
 
-    wcscpy(set, for_ctrl->set);
-    handleExpansion(set, TRUE);
+    if (for_ctrl->set)
+    {
+        wcscpy(set, for_ctrl->set);
+        handleExpansion(set, TRUE);
+    }
+    else
+        set[0] = L'\0';
 
     /* Note: native doesn't check the actual number of parameters, and set
      * them by default to 0.
@@ -3648,7 +3653,7 @@ static RETURN_CODE for_control_execute(CMD_FOR_CONTROL *for_ctrl, CMD_NODE *node
 {
     RETURN_CODE return_code;
 
-    if (!for_ctrl->set) return NO_ERROR;
+    if (!for_ctrl->set && for_ctrl->operator != CMD_FOR_NUMBERS) return NO_ERROR;
 
     WCMD_save_for_loop_context(FALSE);
 