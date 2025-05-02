wine-mirror/wine

commit 1d3bfdc3abdaaa45075f0ec04228b911ab5ab8cf

gdi32: Limit source string length in logfont_AtoW (ASan).
test_logfont in dlls/gdi32/tests/font.c calls CreateFontIndirectA with a
non-null-terminated font name and expects it to not crash.

dlls/gdi32/text.c
@@ -781,8 +781,8 @@ static void text_metric_ex_WtoA(const NEWTEXTMETRICEXW *tmW, NEWTEXTMETRICEXA *t
 static void logfont_AtoW( const LOGFONTA *fontA, LPLOGFONTW fontW )
 {
     memcpy( fontW, fontA, sizeof(LOGFONTA) - LF_FACESIZE );
-    MultiByteToWideChar( CP_ACP, 0, fontA->lfFaceName, -1, fontW->lfFaceName,
-                         LF_FACESIZE );
+    MultiByteToWideChar( CP_ACP, 0, fontA->lfFaceName, strnlen( fontA->lfFaceName, LF_FACESIZE ),
+                         fontW->lfFaceName, LF_FACESIZE );
     fontW->lfFaceName[LF_FACESIZE - 1] = 0;
 }
 

commit 01cd2557e688c8c0fa50ed697affa58a8e68e128

gdi32: Ensure null termination in logfont_AtoW.
Fixes: 1d3bfdc3abdaaa45075f0ec04228b911ab5ab8cf

dlls/gdi32/text.c
@@ -780,10 +780,11 @@ static void text_metric_ex_WtoA(const NEWTEXTMETRICEXW *tmW, NEWTEXTMETRICEXA *t
 
 static void logfont_AtoW( const LOGFONTA *fontA, LPLOGFONTW fontW )
 {
+    int len = MultiByteToWideChar( CP_ACP, 0, fontA->lfFaceName,
+                                   strnlen( fontA->lfFaceName, LF_FACESIZE ),
+                                   fontW->lfFaceName, LF_FACESIZE );
+    fontW->lfFaceName[min( len, LF_FACESIZE - 1 )] = 0;
     memcpy( fontW, fontA, sizeof(LOGFONTA) - LF_FACESIZE );
-    MultiByteToWideChar( CP_ACP, 0, fontA->lfFaceName, strnlen( fontA->lfFaceName, LF_FACESIZE ),
-                         fontW->lfFaceName, LF_FACESIZE );
-    fontW->lfFaceName[LF_FACESIZE - 1] = 0;
 }
 
 static void logfont_WtoA( const LOGFONTW *fontW, LPLOGFONTA fontA )