e392386f175d76c9a54e436019ec53f2f767c986
Nikolay Sivov <nsivov@codeweavers.com>
2025-05-12 12:53:22 +0200

    comctl32/edit: Remove change notifications on Ctrl+A selections.
    It's not notified on Windows, and it makes sense not to because data hasn't changed.
    
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

--- dlls/comctl32/edit.c
+++ dlls/comctl32/edit.c
@@ -3406,11 +3406,7 @@ static LRESULT EDIT_WM_KeyDown(EDITSTATE *es, INT key)
             if (control)
             {
                 if (EDIT_EM_SetSel(es, 0, get_text_length(es), FALSE))
-                {
-                    if (!notify_parent(es, EN_UPDATE)) break;
-                    notify_parent(es, EN_CHANGE);
                     EDIT_EM_ScrollCaret(es);
-                }
             }
             break;
 	}
