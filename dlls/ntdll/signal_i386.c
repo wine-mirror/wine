ea6ae259b7178abb86ceac203922628ef26388fe
Alexandre Julliard <julliard@winehq.org>
2025-05-12 13:26:56 +0200

    ntdll: Define NtQueryInformationProcess as a custom syscall.

--- dlls/ntdll/signal_i386.c
+++ dlls/ntdll/signal_i386.c
@@ -72,29 +72,12 @@ extern DWORD EXC_CallHandler( EXCEPTION_RECORD *record, EXCEPTION_REGISTRATION_R
                               PEXCEPTION_HANDLER handler, PEXCEPTION_HANDLER nested_handler );
 
 
-#ifdef __WINE_PE_BUILD
-
-enum syscall_ids
-{
-#define SYSCALL_ENTRY(id,name,args) __id_##name = id,
-ALL_SYSCALLS
-#undef SYSCALL_ENTRY
-};
-
-/*******************************************************************
- *         NtQueryInformationProcess
- */
-void NtQueryInformationProcess_wrapper(void)
-{
-    asm( ".globl " __ASM_STDCALL("NtQueryInformationProcess", 20) "\n"
-         __ASM_STDCALL("NtQueryInformationProcess", 20) ":\n\t"
-         "movl %0,%%eax\n\t"
-         "call *%%fs:0xc0\n\t"
-         "ret $20"  :: "i" (__id_NtQueryInformationProcess) );
-}
-#define NtQueryInformationProcess syscall_NtQueryInformationProcess
-
-#endif /* __WINE_PE_BUILD */
+#undef SYSCALL_ENTRY_NtQueryInformationProcess
+#define SYSCALL_ENTRY_NtQueryInformationProcess(id,name,args) \
+    __ASM_STDCALL_FUNC( name, args, \
+                        "movl $(" #id "),%eax\n\t" \
+                        "call *%fs:0xc0\n\t" \
+                        "ret $" #args )
 
 /*******************************************************************
  *         syscalls
