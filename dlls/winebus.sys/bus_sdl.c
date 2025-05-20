8247a7a01052e2fea145aae6eed9483716d678b6
Makarenko Oleg <oleg@makarenk.ooo>
2025-05-20 03:54:36 +0300

    winebus: Fix detection of devices with no axis.

--- dlls/winebus.sys/bus_sdl.c
+++ dlls/winebus.sys/bus_sdl.c
@@ -999,7 +999,8 @@ static void sdl_add_device(unsigned int index)
         desc.is_gamepad = (axis_count == 6  && button_count >= 14);
     }
 
-    for (axis_offset = 0; axis_offset < axis_count; axis_offset += (options.split_controllers ? 6 : axis_count))
+    axis_offset = 0;
+    do
     {
         NTSTATUS status;
 
@@ -1026,7 +1027,9 @@ static void sdl_add_device(unsigned int index)
         }
 
         bus_event_queue_device_created(&event_queue, &impl->unix_device, &desc);
+        axis_offset += (options.split_controllers ? 6 : axis_count);
     }
+    while (axis_offset < axis_count);
 }
 
 static void process_device_event(SDL_Event *event)
