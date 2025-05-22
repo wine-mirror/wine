dd3a1e0ff834008a47a88f5204969630561cb08d
Zhiyi Zhang <zzhang@codeweavers.com>
2025-05-22 10:34:06 +0800

    windows.ui: Use helpers to implement IWeakReference.
    This also fix a double free in uisettings_Release().

--- dlls/windows.ui/uisettings.c
+++ dlls/windows.ui/uisettings.c
@@ -19,7 +19,7 @@
 
 #include "private.h"
 #include "initguid.h"
-#include "weakreference.h"
+#include "weakref.h"
 
 #include "wine/debug.h"
 
@@ -34,10 +34,7 @@ struct uisettings
     IUISettings3 IUISettings3_iface;
     IUISettings4 IUISettings4_iface;
     IUISettings5 IUISettings5_iface;
-    IWeakReferenceSource IWeakReferenceSource_iface;
-    IWeakReference IWeakReference_iface;
-    LONG ref_strong;
-    LONG ref_weak;
+    struct weak_reference_source weak_reference_source;
 };
 
 static inline struct uisettings *impl_from_IUISettings( IUISettings *iface )
@@ -78,7 +75,7 @@ static HRESULT WINAPI uisettings_QueryInterface( IUISettings *iface, REFIID iid,
     }
     else if (IsEqualGUID( iid, &IID_IWeakReferenceSource ))
     {
-        *out = &impl->IWeakReferenceSource_iface;
+        *out = &impl->weak_reference_source.IWeakReferenceSource_iface;
     }
 
     if (!*out)
@@ -94,20 +91,18 @@ static HRESULT WINAPI uisettings_QueryInterface( IUISettings *iface, REFIID iid,
 static ULONG WINAPI uisettings_AddRef( IUISettings *iface )
 {
     struct uisettings *impl = impl_from_IUISettings( iface );
-    ULONG ref = InterlockedIncrement( &impl->ref_strong );
+    ULONG ref = weak_reference_strong_add_ref( &impl->weak_reference_source );
     TRACE( "iface %p, ref %lu.\n", iface, ref );
-    IWeakReference_AddRef( &impl->IWeakReference_iface );
     return ref;
 }
 
 static ULONG WINAPI uisettings_Release( IUISettings *iface )
 {
     struct uisettings *impl = impl_from_IUISettings( iface );
-    ULONG ref = InterlockedDecrement( &impl->ref_strong );
+    ULONG ref = weak_reference_strong_release( &impl->weak_reference_source );
 
     TRACE( "iface %p, ref %lu.\n", iface, ref );
 
-    IWeakReference_Release( &impl->IWeakReference_iface );
     if (!ref) free( impl );
     return ref;
 }
@@ -480,120 +475,6 @@ static const struct IUISettings5Vtbl uisettings5_vtbl =
     uisettings5_remove_AutoHideScrollBarsChanged,
 };
 
-static inline struct uisettings *impl_from_IWeakReference( IWeakReference *iface )
-{
-    return CONTAINING_RECORD( iface, struct uisettings, IWeakReference_iface );
-}
-
-static HRESULT WINAPI weak_reference_QueryInterface( IWeakReference *iface, REFIID iid, void **out )
-{
-    struct uisettings *impl = impl_from_IWeakReference( iface );
-
-    TRACE( "iface %p, iid %s, out %p.\n", iface, debugstr_guid(iid), out );
-
-    if (IsEqualGUID( iid, &IID_IUnknown ) ||
-        IsEqualGUID( iid, &IID_IWeakReference ))
-    {
-        *out = &impl->IWeakReference_iface;
-        IInspectable_AddRef( *out );
-        return S_OK;
-    }
-
-    FIXME( "%s not implemented, returning E_NOINTERFACE.\n", debugstr_guid(iid) );
-    *out = NULL;
-    return E_NOINTERFACE;
-}
-
-static ULONG WINAPI weak_reference_AddRef( IWeakReference *iface )
-{
-    struct uisettings *impl = impl_from_IWeakReference( iface );
-    ULONG ref = InterlockedIncrement( &impl->ref_weak );
-    TRACE("iface %p increasing refcount to %lu.\n", iface, ref);
-    return ref;
-}
-
-static ULONG WINAPI weak_reference_Release( IWeakReference *iface )
-{
-    struct uisettings *impl = impl_from_IWeakReference( iface );
-    ULONG ref = InterlockedDecrement( &impl->ref_weak );
-    if (!ref)
-        free( impl );
-    return ref;
-}
-
-static HRESULT WINAPI weak_reference_Resolve( IWeakReference *iface, REFIID iid, IInspectable **out )
-{
-    struct uisettings *impl = impl_from_IWeakReference( iface );
-    HRESULT hr;
-    LONG ref;
-
-    TRACE( "iface %p, iid %s, out %p stub.\n", iface, debugstr_guid(iid), out );
-
-    *out = NULL;
-
-    do
-    {
-        if (!(ref = ReadNoFence( &impl->ref_strong )))
-            return S_OK;
-    } while (ref != InterlockedCompareExchange( &impl->ref_strong, ref + 1, ref ));
-
-    hr = IUISettings_QueryInterface( &impl->IUISettings_iface, iid, (void **)out );
-    InterlockedDecrement( &impl->ref_strong );
-    return hr;
-}
-
-static const struct IWeakReferenceVtbl weak_reference_vtbl =
-{
-    weak_reference_QueryInterface,
-    weak_reference_AddRef,
-    weak_reference_Release,
-    /* IWeakReference methods */
-    weak_reference_Resolve,
-};
-
-static inline struct uisettings *impl_from_IWeakReferenceSource( IWeakReferenceSource *iface )
-{
-    return CONTAINING_RECORD( iface, struct uisettings, IWeakReferenceSource_iface );
-}
-
-static HRESULT WINAPI weak_reference_source_QueryInterface( IWeakReferenceSource *iface, REFIID iid, void **out )
-{
-    struct uisettings *impl = impl_from_IWeakReferenceSource( iface );
-    return uisettings_QueryInterface( &impl->IUISettings_iface, iid, out );
-}
-
-static ULONG WINAPI weak_reference_source_AddRef( IWeakReferenceSource *iface )
-{
-    struct uisettings *impl = impl_from_IWeakReferenceSource( iface );
-    return uisettings_AddRef( &impl->IUISettings_iface );
-}
-
-static ULONG WINAPI weak_reference_source_Release( IWeakReferenceSource *iface )
-{
-    struct uisettings *impl = impl_from_IWeakReferenceSource( iface );
-    return uisettings_Release( &impl->IUISettings_iface );
-}
-
-static HRESULT WINAPI weak_reference_source_GetWeakReference( IWeakReferenceSource *iface, IWeakReference **ref )
-{
-    struct uisettings *impl = impl_from_IWeakReferenceSource(iface);
-
-    TRACE("iface %p, ref %p stub.\n", iface, ref);
-
-    *ref = &impl->IWeakReference_iface;
-    IWeakReference_AddRef( *ref );
-    return S_OK;
-}
-
-static const struct IWeakReferenceSourceVtbl weak_reference_source_vtbl =
-{
-    weak_reference_source_QueryInterface,
-    weak_reference_source_AddRef,
-    weak_reference_source_Release,
-    /* IWeakReferenceSource methods */
-    weak_reference_source_GetWeakReference,
-};
-
 struct uisettings_statics
 {
     IActivationFactory IActivationFactory_iface;
@@ -662,6 +543,7 @@ static HRESULT WINAPI factory_GetTrustLevel( IActivationFactory *iface, TrustLev
 static HRESULT WINAPI factory_ActivateInstance( IActivationFactory *iface, IInspectable **instance )
 {
     struct uisettings *impl;
+    HRESULT hr;
 
     TRACE( "iface %p, instance %p.\n", iface, instance );
 
@@ -676,10 +558,14 @@ static HRESULT WINAPI factory_ActivateInstance( IActivationFactory *iface, IInsp
     impl->IUISettings3_iface.lpVtbl = &uisettings3_vtbl;
     impl->IUISettings4_iface.lpVtbl = &uisettings4_vtbl;
     impl->IUISettings5_iface.lpVtbl = &uisettings5_vtbl;
-    impl->IWeakReferenceSource_iface.lpVtbl = &weak_reference_source_vtbl;
-    impl->IWeakReference_iface.lpVtbl = &weak_reference_vtbl;
-    impl->ref_strong = 1;
-    impl->ref_weak = 1;
+
+    if (FAILED(hr = weak_reference_source_init( &impl->weak_reference_source,
+                                                (IUnknown *)&impl->IUISettings_iface )))
+    {
+        *instance = NULL;
+        free( impl );
+        return hr;
+    }
 
     *instance = (IInspectable *)&impl->IUISettings5_iface;
     return S_OK;
