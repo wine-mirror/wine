ad580c7f991bbffbe35074352e559a18ed1ca4b5
Paul Gofman <pgofman@codeweavers.com>
2025-05-26 15:27:34 -0600

    avifil32: Assume OF_WRITE for OF_CREATE in AVIFileOpenW().

--- dlls/avifil32/tests/api.c
+++ dlls/avifil32/tests/api.c
@@ -810,9 +810,8 @@ static void test_avifile_write(void)
     hr = AVIFileOpenW(&avifile, fn, OF_CREATE, NULL);
     ok(hr == S_OK, "got %#lx.\n", hr);
     hr = AVIFileCreateStreamW(avifile, &stm, &si);
-    todo_wine ok(hr == S_OK, "got %#lx.\n", hr);
-    if (hr == S_OK)
-        IAVIStream_Release(stm);
+    ok(hr == S_OK, "got %#lx.\n", hr);
+    IAVIStream_Release(stm);
     IAVIFile_Release(avifile);
     ret = DeleteFileW(fn);
     ok(ret, "got error %lu.\n", GetLastError());
