wine-mirror/wine

commit 4069a4f58a6c268abf5a4f62c84fb6e90125d0ca

msv1_0: Support SECBUFFER_STREAM in ntlm_SpUnsealMessage().

dlls/msv1_0/main.c
@@ -1454,7 +1454,7 @@ static NTSTATUS verify_signature( struct ntlm_ctx *ctx, unsigned int flags, SecB
 
     for (i = 0; i < msg->cBuffers; i++)
     {
-        if (msg->pBuffers[i].BufferType == SECBUFFER_TOKEN)
+        if (msg->pBuffers[i].BufferType == SECBUFFER_TOKEN || msg->pBuffers[i].BufferType == SECBUFFER_STREAM)
         {
             buf[i].BufferType = SECBUFFER_TOKEN;
             buf[i].cbBuffer   = 16;
@@ -1539,7 +1539,10 @@ static NTSTATUS NTAPI ntlm_SpSealMessage( LSA_SEC_HANDLE handle, ULONG qop, SecB
 
 static NTSTATUS NTAPI ntlm_SpUnsealMessage( LSA_SEC_HANDLE handle, SecBufferDesc *msg, ULONG msg_seq_no, ULONG *qop )
 {
-    int token_idx, data_idx;
+    int data_idx = get_buffer_index( msg, SECBUFFER_DATA );
+    int stream_idx = get_buffer_index( msg, SECBUFFER_STREAM );
+    int token_idx = get_buffer_index( msg, SECBUFFER_TOKEN );
+    SecBuffer token_buf;
     struct ntlm_ctx *ctx;
 
     TRACE( "%#Ix, %p, %lu, %p\n", handle, msg, msg_seq_no, qop );
@@ -1547,11 +1550,25 @@ static NTSTATUS NTAPI ntlm_SpUnsealMessage( LSA_SEC_HANDLE handle, SecBufferDesc
 
     if (!handle) return SEC_E_INVALID_HANDLE;
 
-    if (!msg || !msg->pBuffers || msg->cBuffers < 2 ||
-        (token_idx = get_buffer_index( msg, SECBUFFER_TOKEN )) == -1 ||
-        (data_idx = get_buffer_index( msg, SECBUFFER_DATA )) == -1) return SEC_E_INVALID_TOKEN;
+    if (!msg || !msg->pBuffers || msg->cBuffers < 2 || (token_idx == -1 && stream_idx == -1) ||
+        (stream_idx != -1 && token_idx != -1) || data_idx == -1) return SEC_E_INVALID_TOKEN;
 
-    if (msg->pBuffers[token_idx].cbBuffer < 16) return SEC_E_BUFFER_TOO_SMALL;
+    if (stream_idx != -1)
+    {
+        if (msg->pBuffers[stream_idx].cbBuffer < 16) return SEC_E_BUFFER_TOO_SMALL;
+        token_buf.BufferType = SECBUFFER_TOKEN;
+        token_buf.cbBuffer = 16;
+        token_buf.pvBuffer = msg->pBuffers[stream_idx].pvBuffer;
+
+        /* native decrypts in-place even when an appropriately sized data buffer is supplied */
+        msg->pBuffers[data_idx].pvBuffer = (char *)msg->pBuffers[stream_idx].pvBuffer + 16;
+        msg->pBuffers[data_idx].cbBuffer = msg->pBuffers[stream_idx].cbBuffer - 16;
+    }
+    else
+    {
+        if (msg->pBuffers[token_idx].cbBuffer < 16) return SEC_E_BUFFER_TOO_SMALL;
+        token_buf = msg->pBuffers[token_idx];
+    }
 
     ctx = (struct ntlm_ctx *)handle;
     if (ctx->flags & FLAG_NEGOTIATE_NTLM2 && ctx->flags & FLAG_NEGOTIATE_SEAL)
@@ -1563,7 +1580,7 @@ static NTSTATUS NTAPI ntlm_SpUnsealMessage( LSA_SEC_HANDLE handle, SecBufferDesc
 
     /* make sure we use a session key for the signature check, SealMessage always does that,
        even in the dummy case */
-    return verify_signature( ctx, ctx->flags | FLAG_NEGOTIATE_SIGN, msg, &msg->pBuffers[token_idx] );
+    return verify_signature( ctx, ctx->flags | FLAG_NEGOTIATE_SIGN, msg, &token_buf );
 }
 
 static SECPKG_USER_FUNCTION_TABLE ntlm_user_table =

commit c001db58af3402e7cc81c4d3e6a0e3f59b14bde1

msv1_0: Perform NULL check before looking for a buffer of particular type.
This is a regression introduced by 4069a4f58a6c268abf5a4f62c84fb6e90125d0ca.

Signed-off-by: Dmitry Timoshkov <dmitry@baikal.ru>

dlls/msv1_0/main.c
@@ -1539,9 +1539,7 @@ static NTSTATUS NTAPI ntlm_SpSealMessage( LSA_SEC_HANDLE handle, ULONG qop, SecB
 
 static NTSTATUS NTAPI ntlm_SpUnsealMessage( LSA_SEC_HANDLE handle, SecBufferDesc *msg, ULONG msg_seq_no, ULONG *qop )
 {
-    int data_idx = get_buffer_index( msg, SECBUFFER_DATA );
-    int stream_idx = get_buffer_index( msg, SECBUFFER_STREAM );
-    int token_idx = get_buffer_index( msg, SECBUFFER_TOKEN );
+    int data_idx, stream_idx, token_idx;
     SecBuffer token_buf;
     struct ntlm_ctx *ctx;
 
@@ -1550,8 +1548,14 @@ static NTSTATUS NTAPI ntlm_SpUnsealMessage( LSA_SEC_HANDLE handle, SecBufferDesc
 
     if (!handle) return SEC_E_INVALID_HANDLE;
 
-    if (!msg || !msg->pBuffers || msg->cBuffers < 2 || (token_idx == -1 && stream_idx == -1) ||
-        (stream_idx != -1 && token_idx != -1) || data_idx == -1) return SEC_E_INVALID_TOKEN;
+    if (!msg || !msg->pBuffers || msg->cBuffers < 2) return SEC_E_INVALID_TOKEN;
+
+    data_idx = get_buffer_index( msg, SECBUFFER_DATA );
+    stream_idx = get_buffer_index( msg, SECBUFFER_STREAM );
+    token_idx = get_buffer_index( msg, SECBUFFER_TOKEN );
+
+    if ((token_idx == -1 && stream_idx == -1) || (stream_idx != -1 && token_idx != -1) || data_idx == -1)
+        return SEC_E_INVALID_TOKEN;
 
     if (stream_idx != -1)
     {