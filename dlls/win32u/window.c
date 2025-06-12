wine-mirror/wine

commit 5e4d40dddc3492bd48c6f0b042c581022d4ca9fc

win32u: Use a dedicated struct for window destroy entries.
So wIDmenu and userdata can be removed from the WND struct and read from
shared memory, before the window handle gets destroyed.

dlls/win32u/window.c
@@ -5153,16 +5153,43 @@ BOOL WINAPI NtUserDestroyWindow( HWND hwnd )
  */
 void destroy_thread_windows(void)
 {
-    WND *win, *entry, *free_list = NULL;
+    struct destroy_entry
+    {
+        HWND handle;
+        HMENU menu;
+        HMENU sys_menu;
+        struct list vulkan_surfaces;
+        struct window_surface *surface;
+        struct destroy_entry *next;
+    } *entry, *free_list = NULL;
     HANDLE handle = 0;
+    WND *win;
+
+    /* recycle WND structs as destroy_entry structs */
+    C_ASSERT( sizeof(struct destroy_entry) <= sizeof(WND) );
 
     user_lock();
     while ((win = next_thread_user_object( GetCurrentThreadId(), &handle, NTUSER_OBJ_WINDOW )))
     {
+        BOOL is_child = (win->dwStyle & (WS_CHILD | WS_POPUP)) == WS_CHILD;
+        struct destroy_entry tmp = {0};
+
         free_dce( win->dce, win->handle );
         set_user_handle_ptr( handle, NULL );
-        win->userdata = (UINT_PTR)free_list;
-        free_list = win;
+        free( win->pScroll );
+        free( win->text );
+
+        /* recycle the WND struct as a destroy_entry struct */
+        entry = (struct destroy_entry *)win;
+        tmp.handle = win->handle;
+        tmp.vulkan_surfaces = win->vulkan_surfaces;
+        if (!is_child) tmp.menu = (HMENU)win->wIDmenu;
+        tmp.sys_menu = win->hSysMenu;
+        tmp.surface = win->surface;
+        *entry = tmp;
+
+        entry->next = free_list;
+        free_list = entry;
     }
     if (free_list)
     {
@@ -5177,22 +5204,19 @@ void destroy_thread_windows(void)
 
     while ((entry = free_list))
     {
-        free_list = (WND *)entry->userdata;
+        free_list = entry->next;
         TRACE( "destroying %p\n", entry );
 
         user_driver->pDestroyWindow( entry->handle );
         vulkan_detach_surfaces( &entry->vulkan_surfaces );
 
-        if ((entry->dwStyle & (WS_CHILD | WS_POPUP)) != WS_CHILD && entry->wIDmenu)
-            NtUserDestroyMenu( UlongToHandle(entry->wIDmenu) );
-        if (entry->hSysMenu) NtUserDestroyMenu( entry->hSysMenu );
+        NtUserDestroyMenu( entry->menu );
+        NtUserDestroyMenu( entry->sys_menu );
         if (entry->surface)
         {
             register_window_surface( entry->surface, NULL );
             window_surface_release( entry->surface );
         }
-        free( entry->pScroll );
-        free( entry->text );
         free( entry );
     }
 }

commit 3a738f74ea7d1382e79662ecf231b2f5ef6076bf

win32u: Fix copy of vulkan surfaces list on window destruction.
Fixes: 5e4d40dddc3492bd48c6f0b042c581022d4ca9fc

dlls/win32u/window.c
@@ -5153,12 +5153,12 @@ BOOL WINAPI NtUserDestroyWindow( HWND hwnd )
  */
 void destroy_thread_windows(void)
 {
+    struct list vulkan_surfaces = LIST_INIT(vulkan_surfaces);
     struct destroy_entry
     {
         HWND handle;
         HMENU menu;
         HMENU sys_menu;
-        struct list vulkan_surfaces;
         struct window_surface *surface;
         struct destroy_entry *next;
     } *entry, *free_list = NULL;
@@ -5182,7 +5182,7 @@ void destroy_thread_windows(void)
         /* recycle the WND struct as a destroy_entry struct */
         entry = (struct destroy_entry *)win;
         tmp.handle = win->handle;
-        tmp.vulkan_surfaces = win->vulkan_surfaces;
+        list_move_tail( &vulkan_surfaces, &win->vulkan_surfaces );
         if (!is_child) tmp.menu = (HMENU)win->wIDmenu;
         tmp.sys_menu = win->hSysMenu;
         tmp.surface = win->surface;
@@ -5208,7 +5208,6 @@ void destroy_thread_windows(void)
         TRACE( "destroying %p\n", entry );
 
         user_driver->pDestroyWindow( entry->handle );
-        vulkan_detach_surfaces( &entry->vulkan_surfaces );
 
         NtUserDestroyMenu( entry->menu );
         NtUserDestroyMenu( entry->sys_menu );
@@ -5219,6 +5218,8 @@ void destroy_thread_windows(void)
         }
         free( entry );
     }
+
+    vulkan_detach_surfaces( &vulkan_surfaces );
 }
 
 /***********************************************************************