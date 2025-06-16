wine-mirror/wine

commit 73f4aeb2dcc33abd2f3ee02ee8fc1042d1998399

win32u: Implement NtUserRegisterWindowMessage.

dlls/wow64win/user.c
@@ -4079,6 +4079,13 @@ NTSTATUS WINAPI wow64_NtUserRemoveClipboardFormatListener( UINT *args )
     return NtUserRemoveClipboardFormatListener( hwnd );
 }
 
+NTSTATUS WINAPI wow64_NtUserRegisterWindowMessage( UINT *args )
+{
+    UNICODE_STRING *name = get_ptr( &args );
+
+    return NtUserRegisterWindowMessage( name );
+}
+
 NTSTATUS WINAPI wow64_NtUserRemoveMenu( UINT *args )
 {
     HMENU handle = get_handle( &args );

dlls/win32u/class.c
@@ -596,6 +596,24 @@ ULONG WINAPI NtUserGetAtomName( ATOM atom, UNICODE_STRING *name )
 }
 
 /***********************************************************************
+ *       NtUserRegisterWindowMessage   (win32u.@)
+ */
+ATOM WINAPI NtUserRegisterWindowMessage( UNICODE_STRING *name )
+{
+    RTL_ATOM atom;
+
+    TRACE( "%s\n", debugstr_us(name) );
+
+    if (!name)
+    {
+        RtlSetLastWin32Error( ERROR_INVALID_PARAMETER );
+        return 0;
+    }
+    if (!set_ntstatus( NtAddAtom( name->Buffer, name->Length, &atom ) )) return 0;
+    return atom;
+}
+
+/***********************************************************************
  *	     NtUserGetClassName   (win32u.@)
  */
 INT WINAPI NtUserGetClassName( HWND hwnd, BOOL real, UNICODE_STRING *name )

dlls/win32u/main.c
@@ -2022,6 +2022,11 @@ BOOL SYSCALL_API NtUserRegisterTouchPadCapable( BOOL capable )
     SYSCALL_FUNC( NtUserRegisterTouchPadCapable );
 }
 
+ATOM SYSCALL_API NtUserRegisterWindowMessage( UNICODE_STRING *name )
+{
+    SYSCALL_FUNC( NtUserRegisterWindowMessage );
+}
+
 BOOL SYSCALL_API NtUserReleaseCapture(void)
 {
     SYSCALL_FUNC( NtUserReleaseCapture );

dlls/win32u/win32u.spec
@@ -1286,7 +1286,7 @@
 @ stub NtUserRegisterUserApiHook
 @ stub NtUserRegisterUserHungAppHandlers
 @ stub NtUserRegisterWindowArrangementCallout
-@ stub NtUserRegisterWindowMessage
+@ stdcall -syscall NtUserRegisterWindowMessage(ptr)
 @ stdcall -syscall NtUserReleaseCapture()
 @ stdcall -syscall NtUserReleaseDC(long long)
 @ stub NtUserReleaseDwmHitTestWaiters

include/ntuser.h
@@ -944,6 +944,7 @@ W32KAPI ATOM    WINAPI NtUserRegisterClassExWOW( const WNDCLASSEXW *wc, UNICODE_
 W32KAPI BOOL    WINAPI NtUserRegisterHotKey( HWND hwnd, INT id, UINT modifiers, UINT vk );
 W32KAPI BOOL    WINAPI NtUserRegisterRawInputDevices( const RAWINPUTDEVICE *devices, UINT device_count, UINT size );
 W32KAPI BOOL    WINAPI NtUserRegisterTouchPadCapable( BOOL capable );
+W32KAPI ATOM    WINAPI NtUserRegisterWindowMessage( UNICODE_STRING *name );
 W32KAPI BOOL    WINAPI NtUserReleaseCapture(void);
 W32KAPI INT     WINAPI NtUserReleaseDC( HWND hwnd, HDC hdc );
 W32KAPI BOOL    WINAPI NtUserRemoveClipboardFormatListener( HWND hwnd );

commit b64c94d9d13ca2a8245010e8a3bd04aadb7591bd

wow64win: Fix UNICODE_STRING thunking in wow64_NtUserRegisterWindowMessage.
The function was passing a 32-bit UNICODE_STRING pointer directly to the 64-bit
function, causing string corruption. Use the unicode_str_32to64() helper like other
similar functions.

This fixes 32-bit .NET Framework applications failing to create windows, such as
dnSpyEx v6.5.1 (x86), when using a new-style WoW64 build.

Fixes: 73f4aeb2dcc33abd2f3ee02ee8fc1042d1998399

dlls/wow64win/user.c
@@ -4081,9 +4081,10 @@ NTSTATUS WINAPI wow64_NtUserRemoveClipboardFormatListener( UINT *args )
 
 NTSTATUS WINAPI wow64_NtUserRegisterWindowMessage( UINT *args )
 {
-    UNICODE_STRING *name = get_ptr( &args );
+    UNICODE_STRING32 *name32 = get_ptr( &args );
+    UNICODE_STRING name;
 
-    return NtUserRegisterWindowMessage( name );
+    return NtUserRegisterWindowMessage( unicode_str_32to64( &name, name32 ));
 }
 
 NTSTATUS WINAPI wow64_NtUserRemoveMenu( UINT *args )