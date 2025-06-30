wine-mirror/wine

commit 96cd811903e3d3f227c39e12235725baf793f4b9

kernel32: Reimplement conversion to DOS name using GetFinalPathNameByHandleW.

dlls/kernel32/path.c
@@ -424,29 +424,49 @@ char * CDECL wine_get_unix_file_name( LPCWSTR dosW )
 WCHAR * CDECL wine_get_dos_file_name( LPCSTR str )
 {
     UNICODE_STRING nt_name;
+    OBJECT_ATTRIBUTES attr;
+    IO_STATUS_BLOCK io;
     NTSTATUS status;
-    WCHAR *buffer;
-    ULONG len = strlen(str) + 1;
+    HANDLE handle = 0;
+    WCHAR *buffer = NULL, *nt_str = NULL;
+    ULONG res, len = strlen(str) + 1;
 
     if (str[0] != '/')  /* relative path name */
     {
         if (!(buffer = RtlAllocateHeap( GetProcessHeap(), 0, len * sizeof(WCHAR) ))) return NULL;
         MultiByteToWideChar( CP_UNIXCP, 0, str, len, buffer, len );
         status = RtlDosPathNameToNtPathName_U_WithStatus( buffer, &nt_name, NULL, NULL );
-        RtlFreeHeap( GetProcessHeap(), 0, buffer );
-        if (!set_ntstatus( status )) return NULL;
+        if (!set_ntstatus( status )) goto failed;
         buffer = nt_name.Buffer;
         len = nt_name.Length / sizeof(WCHAR) + 1;
     }
     else
     {
-        len += 8;  /* \??\unix prefix */
-        if (!(buffer = HeapAlloc( GetProcessHeap(), 0, len * sizeof(WCHAR) ))) return NULL;
-        if (!set_ntstatus( wine_unix_to_nt_file_name( str, buffer, &len )))
+        if (!(nt_str = RtlAllocateHeap( GetProcessHeap(), 0, (len + 8) * sizeof(WCHAR) ))) return NULL;
+        wcscpy( nt_str, L"\\??\\unix" );
+        MultiByteToWideChar( CP_UNIXCP, 0, str, len, nt_str + 8, len );
+        for (WCHAR *p = nt_str; *p; p++) if (*p == '/') *p = '\\';
+        RtlInitUnicodeString( &nt_name, nt_str );
+        InitializeObjectAttributes( &attr, &nt_name, 0, 0, NULL );
+        status = NtOpenFile( &handle, GENERIC_READ, &attr, &io, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                             FILE_SYNCHRONOUS_IO_NONALERT );
+        while (status)
         {
-            HeapFree( GetProcessHeap(), 0, buffer );
-            return NULL;
+            ULONG i = nt_name.Length / sizeof(WCHAR);
+            while (i && nt_name.Buffer[i - 1] != '\\') i--;
+            while (i && nt_name.Buffer[i - 1] == '\\') i--;
+            if (i <= 9) break;
+            nt_name.Length = i * sizeof(WCHAR);
+            status = NtOpenFile( &handle, GENERIC_READ, &attr, &io, FILE_SHARE_READ | FILE_SHARE_WRITE,
+                                 FILE_DIRECTORY_FILE | FILE_SYNCHRONOUS_IO_NONALERT );
         }
+        if (!set_ntstatus( status )) goto failed;
+        if (!(buffer = RtlAllocateHeap( GetProcessHeap(), 0, (len + 8) * sizeof(WCHAR) ))) goto failed;
+        res = GetFinalPathNameByHandleW( handle, buffer, len + 8, VOLUME_NAME_DOS );
+        if (!res || res > len + 8) goto failed;
+        wcscat( buffer, nt_name.Buffer + nt_name.Length / sizeof(WCHAR) );
+        NtClose( handle );
+        RtlFreeHeap( GetProcessHeap(), 0, nt_str );
     }
     if (buffer[5] == ':')
     {
@@ -456,6 +476,12 @@ WCHAR * CDECL wine_get_dos_file_name( LPCSTR str )
     }
     else buffer[1] = '\\';
     return buffer;
+
+ failed:
+    if (handle) NtClose( handle );
+    RtlFreeHeap( GetProcessHeap(), 0, buffer );
+    RtlFreeHeap( GetProcessHeap(), 0, nt_str );
+    return NULL;
 }
 
 /*************************************************************************

commit aae0f624d48d1e4f79e6947798e61b4d1006bf76

kernel32: Use the correct buffer length.
Fixes: 96cd811903e3d3f227c39e12235725baf793f4b9

dlls/kernel32/path.c
@@ -438,7 +438,6 @@ WCHAR * CDECL wine_get_dos_file_name( LPCSTR str )
         status = RtlDosPathNameToNtPathName_U_WithStatus( buffer, &nt_name, NULL, NULL );
         if (!set_ntstatus( status )) goto failed;
         buffer = nt_name.Buffer;
-        len = nt_name.Length / sizeof(WCHAR) + 1;
     }
     else
     {
@@ -472,7 +471,7 @@ WCHAR * CDECL wine_get_dos_file_name( LPCSTR str )
     {
         /* get rid of the \??\ prefix */
         /* FIXME: should implement RtlNtPathNameToDosPathName and use that instead */
-        memmove( buffer, buffer + 4, (len - 4) * sizeof(WCHAR) );
+        memmove( buffer, buffer + 4, (wcslen(buffer + 4) + 1) * sizeof(WCHAR) );
     }
     else buffer[1] = '\\';
     return buffer;