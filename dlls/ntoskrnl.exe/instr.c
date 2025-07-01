6e2cc7338ab8e2a9d066ecec9053b507dae68685
Jacek Caban <jacek@codeweavers.com>
2025-07-01 16:27:37 +0200

    ntoskrnl: Remove __GNUC__ check.

--- dlls/ntoskrnl.exe/instr.c
+++ dlls/ntoskrnl.exe/instr.c
@@ -64,12 +64,7 @@ static LDT_ENTRY idt[256];
 static inline struct idtr get_idtr(void)
 {
     struct idtr ret;
-#ifdef __GNUC__
     __asm__( "sidtl %0" : "=m" (ret) );
-#else
-    ret.base = (BYTE *)idt;
-    ret.limit = sizeof(idt) - 1;
-#endif
     return ret;
 }
 
