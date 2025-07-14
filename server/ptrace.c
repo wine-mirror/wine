cb6e050e4097e05effce37320f9b4b671e75167e
Thibault Payet <contact@thibaultpayet.fr>
2025-07-14 14:31:24 +0200

    server: Always use the thread Unix id in ptrace for FreeBSD.

--- server/ptrace.c
+++ server/ptrace.c
@@ -687,7 +687,7 @@ void init_thread_context( struct thread *thread )
         struct dbreg dbregs;
 
         memset( &dbregs, 0, sizeof(dbregs) );
-        ptrace( PTRACE_SETDBREGS, get_ptrace_tid( thread ), (caddr_t)&dbregs, 0 );
+        ptrace( PTRACE_SETDBREGS, thread->unix_tid, (caddr_t)&dbregs, 0 );
         resume_after_ptrace( thread );
     }
     thread->system_regs = 0;
