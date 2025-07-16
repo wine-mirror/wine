wine-mirror/wine

commit e0690d51c9ffaee562797ae73fd867cd88929791

winemac.drv: Use the ntdll_get_unix_file_name() helper.

dlls/winemac.drv/clipboard.c
@@ -588,78 +588,6 @@ static CPTABLEINFO *get_ansi_cp(void)
 }
 
 
-/***********************************************************************
- *           get_nt_pathname
- *
- * Simplified version of RtlDosPathNameToNtPathName_U.
- */
-static BOOL get_nt_pathname(const WCHAR *name, UNICODE_STRING *nt_name)
-{
-    static const WCHAR ntprefixW[] = {'\\','?','?','\\'};
-    static const WCHAR uncprefixW[] = {'U','N','C','\\'};
-    size_t len = lstrlenW(name);
-    WCHAR *ptr;
-
-    nt_name->MaximumLength = (len + 8) * sizeof(WCHAR);
-    if (!(ptr = malloc(nt_name->MaximumLength))) return FALSE;
-    nt_name->Buffer = ptr;
-
-    memcpy(ptr, ntprefixW, sizeof(ntprefixW));
-    ptr += ARRAYSIZE(ntprefixW);
-    if (name[0] == '\\' && name[1] == '\\')
-    {
-        if ((name[2] == '.' || name[2] == '?') && name[3] == '\\')
-        {
-            name += 4;
-            len -= 4;
-        }
-        else
-        {
-            memcpy(ptr, uncprefixW, sizeof(uncprefixW));
-            ptr += ARRAYSIZE(uncprefixW);
-            name += 2;
-            len -= 2;
-        }
-    }
-    memcpy(ptr, name, (len + 1) * sizeof(WCHAR));
-    ptr += len;
-    nt_name->Length = (ptr - nt_name->Buffer) * sizeof(WCHAR);
-    return TRUE;
-}
-
-
-/* based on wine_get_unix_file_name */
-static char *get_unix_file_name(const WCHAR *dosW)
-{
-    UNICODE_STRING nt_name;
-    OBJECT_ATTRIBUTES attr;
-    NTSTATUS status;
-    ULONG size = 256;
-    char *buffer;
-
-    if (!get_nt_pathname(dosW, &nt_name)) return NULL;
-    InitializeObjectAttributes(&attr, &nt_name, 0, 0, NULL);
-    for (;;)
-    {
-        if (!(buffer = malloc(size)))
-        {
-            free(nt_name.Buffer);
-            return NULL;
-        }
-        status = wine_nt_to_unix_file_name(&attr, buffer, &size, FILE_OPEN_IF);
-        if (status != STATUS_BUFFER_TOO_SMALL) break;
-        free(buffer);
-    }
-    free(nt_name.Buffer);
-    if (status)
-    {
-        free(buffer);
-        return NULL;
-    }
-    return buffer;
-}
-
-
 /**************************************************************************
  *              import_nsfilenames_to_hdrop
  *
@@ -936,7 +864,7 @@ static CFDataRef export_hdrop_to_filenames(void *data, size_t size)
         TRACE("    %s\n", dropfiles->fWide ? debugstr_w(p) : debugstr_a(p));
 
         if (dropfiles->fWide)
-            unixname = get_unix_file_name(p);
+            status = ntdll_get_unix_file_name( p, &unixname, FILE_OPEN );
         else
         {
             CPTABLEINFO *cp = get_ansi_cp();
@@ -954,9 +882,9 @@ static CFDataRef export_hdrop_to_filenames(void *data, size_t size)
             else
                 RtlCustomCPToUnicodeN(cp, buffer, buffer_len * sizeof(WCHAR), &len, p, len);
 
-            unixname = get_unix_file_name(buffer);
+            status = ntdll_get_unix_file_name( buffer, &unixname, FILE_OPEN );
         }
-        if (!unixname)
+        if (status)
         {
             WARN("failed to convert DOS path to Unix: %s\n",
                  dropfiles->fWide ? debugstr_w(p) : debugstr_a(p));

commit 9fcf36d76efc57219b1846f387ea58322c01cb9a

winemac: Define missing status variable.
Fixes: e0690d51c9ffaee562797ae73fd867cd88929791

dlls/winemac.drv/clipboard.c
@@ -860,6 +860,7 @@ static CFDataRef export_hdrop_to_filenames(void *data, size_t size)
     {
         char *unixname;
         CFStringRef filename;
+        NTSTATUS status;
 
         TRACE("    %s\n", dropfiles->fWide ? debugstr_w(p) : debugstr_a(p));
 