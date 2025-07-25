02ba4adcb5075e8e76e11a681a12a6b81573f89d
Nikolay Sivov <nsivov@codeweavers.com>
2025-07-25 19:35:37 +0200

    windowscodecs/converter: Propagate source failure in 48bppRGB -> 128bppRGBFloat conversion.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

--- dlls/windowscodecs/converter.c
+++ dlls/windowscodecs/converter.c
@@ -2047,7 +2047,7 @@ static HRESULT copypixels_to_128bppRGBFloat(struct FormatConverter *This, const 
         }
 
         free(srcdata);
-        return S_OK;
+        return hr;
     }
     case format_96bppRGBFloat:
     {
