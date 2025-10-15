metux/wine-hacks

gdi32: Add stub for GetEnhMetaFilePixelFormat.
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=58747

v2: Improve FIXME and return value.

dlls/gdi32/enhmetafile.c
@@ -3001,3 +3001,12 @@ end:
     DeleteMetaFile(hmf);
     return ret;
 }
+
+/******************************************************************
+ *         GetEnhMetaFilePixelFormat   (GDI32.@)
+ */
+UINT WINAPI GetEnhMetaFilePixelFormat( HENHMETAFILE hemf, UINT size, PIXELFORMATDESCRIPTOR *ppfd )
+{
+    FIXME( "(%p,%u,%p) stub\n", hemf, size, ppfd );
+    return GDI_ERROR;
+}

wine-mirror/wine

dlls/gdi32/enhmetafile.c
@@ -3001,3 +3001,12 @@ end:
     DeleteMetaFile(hmf);
     return ret;
 }
+
+/******************************************************************
+ *         GetEnhMetaFilePixelFormat   (GDI32.@)
+ */
+UINT WINAPI GetEnhMetaFilePixelFormat( HENHMETAFILE hemf, UINT size, PIXELFORMATDESCRIPTOR *fmt )
+{
+    FIXME( "(%p,%u,%p) stub\n", hemf, size, fmt );
+    return GDI_ERROR;
+}