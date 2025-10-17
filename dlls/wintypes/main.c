wine-mirror/wine

commit 4b1e53ceb96fc47a895a0fb917a5736d2db42935

wintypes: Make a copy of the passed string in IPropertyValueStatics::CreateString.
Fixes a use-after-free if the string is deleted after the call.

dlls/wintypes/main.c
@@ -1455,9 +1455,19 @@ static HRESULT STDMETHODCALLTYPE property_value_statics_CreateBoolean(IPropertyV
 }
 
 static HRESULT STDMETHODCALLTYPE property_value_statics_CreateString(IPropertyValueStatics *iface,
-        HSTRING value, IInspectable **property_value)
+        HSTRING value_str, IInspectable **property_value)
 {
-    TRACE("iface %p, value %s, property_value %p.\n", iface, debugstr_hstring(value), property_value);
+    HSTRING value;
+    HRESULT hr;
+
+    TRACE("iface %p, value %s, property_value %p.\n", iface, debugstr_hstring(value_str), property_value);
+
+    if (FAILED(hr = WindowsDuplicateString(value_str, &value)))
+    {
+        *property_value = NULL;
+        return hr;
+    }
+
     create_primitive_property_value_iref(PropertyType_String, irefs.hstring_iface.lpVtbl, iref_hstring_vtbl);
 }
 

commit 2715c6fb8c61e0c12224ce50161f2ea7add1805f

wintypes: Restore a pointer check in property_value_statics_CreateString().
Fix a regression from 4b1e53ce.

dlls/wintypes/main.c
@@ -1466,6 +1466,9 @@ static HRESULT STDMETHODCALLTYPE property_value_statics_CreateString(IPropertyVa
 
     TRACE("iface %p, value %s, property_value %p.\n", iface, debugstr_hstring(value_str), property_value);
 
+    if (!property_value)
+        return E_POINTER;
+
     if (FAILED(hr = WindowsDuplicateString(value_str, &value)))
     {
         *property_value = NULL;

dlls/wintypes/tests/wintypes.c
@@ -834,6 +834,9 @@ static void test_IPropertyValueStatics(void)
     hr = IPropertyValue_GetBoolean(value, &ret);
     ok(hr == TYPE_E_TYPEMISMATCH, "Got unexpected hr %#lx.\n", hr);
 
+    hr = IPropertyValueStatics_CreateString(statics, (HSTRING)-1, NULL);
+    ok(hr == E_POINTER, "Got unexpected hr %#lx.\n", hr);
+
     IPropertyValue_Release(value);
 
     /* Parameter checks for array types */