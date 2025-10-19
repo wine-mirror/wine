1ca1bde2a20ad7af638fce96144ba5fe49b03fe0
Alistair Leslie-Hughes <leslie_alistair@hotmail.com>
2025-10-20 07:10:31 +1100

    bcrypt: Add missing breaks.

--- dlls/bcrypt/gnutls.c
+++ dlls/bcrypt/gnutls.c
@@ -844,6 +844,7 @@ static NTSTATUS key_export_ecc_public( struct key *key, UCHAR *buf, ULONG len, U
             FIXME( "unsupported curve %u\n", key->u.a.curve_id );
             return STATUS_NOT_IMPLEMENTED;
         }
+        break;
 
     case ALG_ID_ECDH_P256:
         magic = BCRYPT_ECDH_PUBLIC_P256_MAGIC;
@@ -879,6 +880,7 @@ static NTSTATUS key_export_ecc_public( struct key *key, UCHAR *buf, ULONG len, U
             FIXME( "unsupported curve %u\n", key->u.a.curve_id );
             return STATUS_NOT_IMPLEMENTED;
         }
+        break;
 
     case ALG_ID_ECDSA_P256:
         magic = BCRYPT_ECDSA_PUBLIC_P256_MAGIC;
@@ -1271,6 +1273,7 @@ static NTSTATUS key_export_ecc( struct key *key, UCHAR *buf, ULONG len, ULONG *r
             FIXME( "unsupported curve %u\n", key->u.a.curve_id );
             return STATUS_NOT_IMPLEMENTED;
         }
+        break;
 
     case ALG_ID_ECDH_P256:
         magic = BCRYPT_ECDH_PRIVATE_P256_MAGIC;
@@ -1306,6 +1309,7 @@ static NTSTATUS key_export_ecc( struct key *key, UCHAR *buf, ULONG len, ULONG *r
             FIXME( "unsupported curve %u\n", key->u.a.curve_id );
             return STATUS_NOT_IMPLEMENTED;
         }
+        break;
 
     case ALG_ID_ECDSA_P256:
         magic = BCRYPT_ECDSA_PRIVATE_P256_MAGIC;
