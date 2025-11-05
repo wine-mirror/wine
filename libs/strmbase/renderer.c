f9f0ceaa8582945f78d770e4d4d7690eb62ca374
Alexandre Julliard <julliard@winehq.org>
2025-11-05 11:36:31 +0100

    strmbase: Use CRT allocation functions.
    And get rid of the strmbase_private.h header.

--- libs/strmbase/renderer.c
+++ libs/strmbase/renderer.c
@@ -18,7 +18,9 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
  */
 
-#include "strmbase_private.h"
+#define COBJMACROS
+#include "wine/strmbase.h"
+#include "wine/debug.h"
 
 WINE_DEFAULT_DEBUG_CHANNEL(quartz);
 
