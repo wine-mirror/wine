wine-mirror/wine

commit 052722667c9dbf96a5dc04d45af55599718d9f92

ntdll: Fix handling jmp in epilogue unwind on x64.

dlls/ntdll/unwind.c
@@ -1905,14 +1905,10 @@ static BOOL is_inside_epilog( BYTE *pc, ULONG64 base, const RUNTIME_FUNCTION *fu
             return TRUE;
         case 0xe9: /* jmp nnnn */
             pc += 5 + *(LONG *)(pc + 1);
-            if (pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress)
-                continue;
-            break;
+            return !(pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress);
         case 0xeb: /* jmp n */
             pc += 2 + (signed char)pc[1];
-            if (pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress)
-                continue;
-            break;
+            return !(pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress);
         case 0xf3: /* rep; ret (for amd64 prediction bug) */
             return pc[1] == 0xc3;
         }
@@ -1967,17 +1963,13 @@ static void interpret_epilog( BYTE *pc, CONTEXT *context, KNONVOLATILE_CONTEXT_P
             context->Rip = *(ULONG64 *)context->Rsp;
             context->Rsp += sizeof(ULONG64) + *(WORD *)(pc + 1);
             return;
+        case 0xe9: /* jmp nnnn */
+        case 0xeb: /* jmp n */
         case 0xc3: /* ret */
         case 0xf3: /* rep; ret */
             context->Rip = *(ULONG64 *)context->Rsp;
             context->Rsp += sizeof(ULONG64);
             return;
-        case 0xe9: /* jmp nnnn */
-            pc += 5 + *(LONG *)(pc + 1);
-            continue;
-        case 0xeb: /* jmp n */
-            pc += 2 + (signed char)pc[1];
-            continue;
         }
         return;
     }

dlls/ntdll/tests/unwind.c
@@ -3092,6 +3092,64 @@ static void test_virtual_unwind_x86(void)
     };
 #endif
 
+    static const BYTE function_6_1[] =
+    {
+        0x55,                     /* 00: push %rbp */
+        0x90,                     /* 01: nop */
+        0x5d,                     /* 02: pop %rbp */
+        0xeb, 0x02,               /* 03: jmp 06 */
+        0x90,                     /* 04: nop */
+        0xc3,                     /* 05: ret */
+     };
+
+    static const BYTE function_6_2[] =
+    {
+        0x55,                     /* 00: push %rbp */
+        0x90,                     /* 01: nop */
+        0x5d,                     /* 02: pop %rbp */
+        0xeb, 0x01,               /* 03: jmp 05 */
+        0x90,                     /* 04: nop */
+        0xc3,                     /* 05: ret */
+     };
+
+    static const BYTE function_6_3[] =
+    {
+        0x55,                     /* 00: push %rbp */
+        0x90,                     /* 01: nop */
+        0x5d,                     /* 02: pop %rbp */
+        0xe9, 0x55, 0x55, 0x55, 0x55, /* 03: jmp away */
+        0x90,                     /* 07: nop */
+        0xc3,                     /* 08: ret */
+     };
+
+    static const BYTE unwind_info_6[] =
+    {
+        1,                             /* version + flags */
+        0x1,                           /* prolog size */
+        1,                             /* opcode count */
+        0,                             /* frame reg */
+
+        0x01, UWOP(PUSH_NONVOL, rbp),  /* 02: push %rbp */
+    };
+
+    static const struct results_x86 results_6_epilogue[] =
+    {
+      /* offset  rbp   handler  rip   frame   registers */
+        { 0x00,  0x00,  FALSE, 0x000, 0x000, { {rsp,0x008}, {-1,-1} }},
+        { 0x01,  0x00,  FALSE, 0x008, 0x000, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x02,  0x00,  FALSE, 0x008, 0x000, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x03,  0x00,  FALSE, 0x000, 0x000, { {rsp,0x008}, {-1,-1} }},
+    };
+
+    static const struct results_x86 results_6_body[] =
+    {
+      /* offset  rbp   handler  rip   frame   registers */
+        { 0x00,  0x00,  FALSE, 0x000, 0x000, { {rsp,0x008}, {-1,-1} }},
+        { 0x01,  0x00,  FALSE, 0x008, 0x000, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x02,  0x00,  FALSE, 0x008, 0x000, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x03,  0x00,  FALSE, 0x008, 0x000, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+    };
+
     static const struct unwind_test_x86 tests[] =
     {
         { function_0, sizeof(function_0), unwind_info_0, results_0, ARRAY_SIZE(results_0), broken_results_0 },
@@ -3104,6 +3162,9 @@ static void test_virtual_unwind_x86(void)
 #if 0  /* crashes before Win10 21H2 */
         { function_5, sizeof(function_5), NULL,          results_5, ARRAY_SIZE(results_5) },
 #endif
+        { function_6_1, sizeof(function_6_1), unwind_info_6, results_6_epilogue, ARRAY_SIZE(results_6_epilogue) },
+        { function_6_2, sizeof(function_6_2), unwind_info_6, results_6_body, ARRAY_SIZE(results_6_body) },
+        { function_6_3, sizeof(function_6_3), unwind_info_6, results_6_epilogue, ARRAY_SIZE(results_6_epilogue) },
     };
     unsigned int i;
 

commit 16768a917050a2df35a13f327127512a102ee347

ntdll: Check for related functions in is_inside_epilog() when detecting tail call.
Fixes a regression introduced by 052722667c9dbf96a5dc04d45af55599718d9f92.

dlls/ntdll/unwind.c
@@ -1843,6 +1843,32 @@ static int get_opcode_size( struct opcode op )
     }
 }
 
+static void *get_main_function_start( const RUNTIME_FUNCTION *f, ULONG_PTR base )
+{
+    const struct UNWIND_INFO *info;
+    const union handler_data *data;
+
+    while (f->UnwindData)
+    {
+        info = (const struct UNWIND_INFO *)((char *)base + f->UnwindData);
+        if (!(info->flags & UNW_FLAG_CHAININFO)) break;
+
+        data = (union handler_data *)&info->opcodes[(info->count + 1) & ~1];
+        f = &data->chain;
+    }
+    return (char *)base + f->BeginAddress;
+}
+
+static BOOL is_address_in_function( BYTE *pc, ULONG64 base, const RUNTIME_FUNCTION *function )
+{
+    RUNTIME_FUNCTION *f;
+    ULONG_PTR pc_base;
+
+    if (pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress) return TRUE;
+    if (!(f = RtlLookupFunctionEntry( (ULONG_PTR)pc, &pc_base, NULL ))) return FALSE;
+    return get_main_function_start( f, pc_base ) == get_main_function_start( function, base );
+}
+
 static BOOL is_inside_epilog( BYTE *pc, ULONG64 base, const RUNTIME_FUNCTION *function )
 {
     /* add or lea must be the first instruction, and it must have a rex.W prefix */
@@ -1907,10 +1933,10 @@ static BOOL is_inside_epilog( BYTE *pc, ULONG64 base, const RUNTIME_FUNCTION *fu
             return TRUE;
         case 0xe9: /* jmp nnnn */
             pc += 5 + *(LONG *)(pc + 1);
-            return !(pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress);
+            return !is_address_in_function( pc, base, function );
         case 0xeb: /* jmp n */
             pc += 2 + (signed char)pc[1];
-            return !(pc - (BYTE *)base >= function->BeginAddress && pc - (BYTE *)base < function->EndAddress);
+            return !is_address_in_function( pc, base, function );
         case 0xf3: /* rep; ret (for amd64 prediction bug) */
             return pc[1] == 0xc3;
         case 0xff: /* jmp */

dlls/ntdll/tests/unwind.c
@@ -2695,9 +2695,10 @@ static const char * const reg_names_x86[16] =
 
 #define UWOP(code,info) (UWOP_##code | ((info) << 4))
 
+static const int code_offset = 1024;
+
 static void call_virtual_unwind_x86( int testnum, const struct unwind_test_x86 *test )
 {
-    static const int code_offset = 1024;
     static const int unwind_offset = 2048;
     void *data;
     NTSTATUS status;
@@ -3278,6 +3279,43 @@ static void test_virtual_unwind_x86(void)
         { 0x3c,  0x30,  FALSE, 0x000, 0x000, { {rsp,0x008}, {-1,-1} }},
     };
 
+    static const struct results_x86 results_7_chain_jmp_detected[] =
+    {
+      /* offset  rbp   handler  rip   frame   registers */
+        { 0x1a,  0x30,  FALSE, 0x030, 0x000, { {rsp,0x038}, {rbx,0x010}, {-1,-1} }},
+        { 0x1b,  0x30,  FALSE, 0x038, 0x000, { {rsp,0x040}, {rbx,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x1f,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x24,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+
+        /* jump to chained function, no special handling */
+        { 0x2d,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        /* jump inside inner function, no special handling */
+        { 0x2d,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+
+        { 0x32,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x33,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x37,  0x30,  FALSE, 0x058, 0x058, { {rsp,0x060}, {rbp,0x050}, {-1,-1} }},
+        { 0x3b,  0x30,  FALSE, 0x008, 0x008, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x3c,  0x30,  FALSE, 0x000, 0x000, { {rsp,0x008}, {-1,-1} }},
+    };
+
+    static const struct results_x86 broken_results_7_chain_jmp_detected[] =
+    {
+      /* offset  rbp   handler  rip   frame   registers */
+        { 0x1a,  0x30,  FALSE, 0x030, 0x000, { {rsp,0x038}, {rbx,0x010}, {-1,-1} }},
+        { 0x1b,  0x30,  FALSE, 0x038, 0x000, { {rsp,0x040}, {rbx,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x1f,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x24,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x2d,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x2d,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x32,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        { 0x33,  0x30,  FALSE, 0x068, 0x000, { {rsp,0x070}, {rbx,0x010}, {rbp,0x030}, {-1,-1} }},
+        /* Before Win11 output frame in epilogue is set with fpreg even after it is popped. */
+        { 0x37,  0x30,  FALSE, 0x058, 0x000, { {rsp,0x060}, {rbp,0x050}, {-1,-1} }},
+        { 0x3b,  0x30,  FALSE, 0x008, 0x000, { {rsp,0x010}, {rbp,0x000}, {-1,-1} }},
+        { 0x3c,  0x30,  FALSE, 0x000, 0x000, { {rsp,0x008}, {-1,-1} }},
+    };
+
     static const struct unwind_test_x86 tests[] =
     {
         { function_0, sizeof(function_0), unwind_info_0, results_0, ARRAY_SIZE(results_0), broken_results_0 },
@@ -3296,10 +3334,12 @@ static void test_virtual_unwind_x86(void)
         { function_7, sizeof(function_7), unwind_info_7, results_7, ARRAY_SIZE(results_7), broken_results_7 },
     };
 
-    struct unwind_test_x86 jump_test = { function_tail_jump_ff, sizeof(function_tail_jump_ff), unwind_info_6,
+    struct unwind_test_x86 test_data = { function_tail_jump_ff, sizeof(function_tail_jump_ff), unwind_info_6,
                                          results_6_epilogue, ARRAY_SIZE(results_6_epilogue) };
 
     unsigned int i, rex_prefix, ind;
+    RUNTIME_FUNCTION rtf;
+    BOOL bret;
 
     for (rex_prefix = 0; rex_prefix <= 8; ++rex_prefix)
     {
@@ -3317,24 +3357,74 @@ static void test_virtual_unwind_x86(void)
             if (((rex_prefix == 8 || !rex_prefix) && i == 0x25)
                 || (rex_prefix == 8 && ext == 4))
             {
-                jump_test.results = results_6_epilogue;
-                jump_test.nb_results = ARRAY_SIZE(results_6_epilogue);
+                test_data.results = results_6_epilogue;
+                test_data.nb_results = ARRAY_SIZE(results_6_epilogue);
                 expected = 1;
             }
             else
             {
-                jump_test.results = results_6_body;
-                jump_test.nb_results = ARRAY_SIZE(results_6_body);
+                test_data.results = results_6_body;
+                test_data.nb_results = ARRAY_SIZE(results_6_body);
                 expected = 0;
             }
             winetest_push_context( "rex %#x, byte %#x, expected %d", rex_prefix, i, expected );
-            call_virtual_unwind_x86( 0, &jump_test );
+            call_virtual_unwind_x86( 0, &test_data );
             winetest_pop_context();
         }
     }
 
     for (i = 0; i < ARRAY_SIZE(tests); i++)
         call_virtual_unwind_x86( i, &tests[i] );
+
+    /* jmp is out of the current function but jump destination function has the same start address */
+    rtf.BeginAddress = code_offset;
+    rtf.EndAddress = code_offset + sizeof(function_6_1) + 1;
+    rtf.UnwindData = 0;
+    bret = RtlAddFunctionTable( &rtf, 1, (ULONG_PTR)code_mem );
+    ok( bret, "RtlAddFunctionTable failed.\n"  );
+    test_data.function = function_6_1;
+    test_data.function_size = sizeof(function_6_1);
+    test_data.unwind_info = unwind_info_6;
+    test_data.results = results_6_body;
+    test_data.nb_results = ARRAY_SIZE(results_6_body);
+    test_data.broken_results = results_6_epilogue; /* before Win10 2009. */
+    winetest_push_context( "line %d", __LINE__ );
+    call_virtual_unwind_x86( 0, &test_data );
+    winetest_pop_context();
+    bret = RtlDeleteFunctionTable( &rtf );
+    ok( bret, "RtlDeleteFunctionTable failed.\n"  );
+
+    /* jump destination is in a function which range covers our function but the start address is different */
+    rtf.BeginAddress = code_offset - 1;
+    rtf.EndAddress = code_offset + sizeof(function_6_1) + 1;
+    rtf.UnwindData = 0;
+    bret = RtlAddFunctionTable( &rtf, 1, (ULONG_PTR)code_mem );
+    ok( bret, "RtlAddFunctionTable failed.\n"  );
+    test_data.results = results_6_epilogue;
+    test_data.nb_results = ARRAY_SIZE(results_6_epilogue);
+    winetest_push_context( "line %d", __LINE__ );
+    call_virtual_unwind_x86( 0, &test_data );
+    winetest_pop_context();
+    bret = RtlDeleteFunctionTable( &rtf );
+    ok( bret, "RtlDeleteFunctionTable failed.\n"  );
+
+    /* jump destination is in a chained function */
+    rtf.BeginAddress = code_offset;
+    rtf.EndAddress = code_offset + 0x1a;
+    rtf.UnwindData = 0;
+    bret = RtlAddFunctionTable( &rtf, 1, (ULONG_PTR)code_mem );
+    ok( bret, "RtlAddFunctionTable failed.\n"  );
+    test_data.function = function_7;
+    test_data.function_size = sizeof(function_7);
+    test_data.unwind_info = unwind_info_7;
+    test_data.results = results_7_chain_jmp_detected;
+    test_data.nb_results = ARRAY_SIZE(results_7_chain_jmp_detected);
+    test_data.broken_results = broken_results_7_chain_jmp_detected;
+    winetest_push_context( "line %d", __LINE__ );
+    call_virtual_unwind_x86( 0, &test_data );
+    winetest_pop_context();
+    bret = RtlDeleteFunctionTable( &rtf );
+    ok( bret, "RtlDeleteFunctionTable failed.\n"  );
 }
 
 #endif  /* __x86_64__ */