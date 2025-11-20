wine-mirror/wine

commit b9318e3805f9d000474411cb889719003805fa32

winex11: Move managed window check to window_set_net_wm_state.

dlls/winex11.drv/window.c
@@ -1223,7 +1223,7 @@ static void window_set_net_wm_state( struct x11drv_win_data *data, UINT new_stat
 
     new_state &= x11drv_thread_data()->net_wm_state_mask;
     data->desired_state.net_wm_state = new_state;
-    if (!data->whole_window) return; /* no window, nothing to update */
+    if (!data->whole_window || !data->managed) return; /* no window or not managed, nothing to update */
     if (data->wm_state_serial) return; /* another WM_STATE update is pending, wait for it to complete */
     /* we ignore and override previous _NET_WM_STATE update requests */
     if (old_state == new_state) return; /* states are the same, nothing to update */
@@ -1351,7 +1351,7 @@ static void update_net_wm_states( struct x11drv_win_data *data )
     static const UINT fullscreen_mask = (1 << NET_WM_STATE_MAXIMIZED) | (1 << NET_WM_STATE_FULLSCREEN);
     UINT style, ex_style, new_state = 0;
 
-    if (!data->managed || data->embedded) return;
+    if (data->embedded) return;
     if (data->whole_window == root_window)
     {
         if (is_virtual_desktop()) window_set_net_wm_state( data, is_desktop_fullscreen() ? (1 << NET_WM_STATE_FULLSCREEN) : 0 );

commit c4b1dba391f1d0436fb90878f2ec0ccd8846fadb

winex11: Initialize thread data when checking _NET_WM_STATE mask.
This is a workaround fixing some regression introduced with
b9318e3805f9d000474411cb889719003805fa32, when UpdateLayeredWindow is
being called from a non-owner thread which also doesn't have its winex11
thread data initialized.

It would be better to avoid updating windows from any non-owner thread,
and most of user32 functions make sure of it by sending messages to the
window owner thread when appropriate, but that doesn't work with ULW as
the owner thread might not be processing messages, and yet ULW windows
are still supposed to be updated and moved around.

The proper fix for that would be to have a dedicated thread managing
window surfaces, which might introduced in the future for compositing
purposes, but that's a much bigger change and we the workaround should
be acceptable in the meantime.

Based on work by Jacob Czekalla.

dlls/winex11.drv/window.c
@@ -1330,7 +1330,7 @@ static void window_set_net_wm_state( struct x11drv_win_data *data, UINT new_stat
 {
     UINT i, count, old_state = data->pending_state.net_wm_state;
 
-    new_state &= x11drv_thread_data()->net_wm_state_mask;
+    new_state &= x11drv_init_thread_data()->net_wm_state_mask;
     data->desired_state.net_wm_state = new_state;
     if (!data->whole_window || !data->managed || data->embedded) return; /* no window or not managed, nothing to update */
     if (data->wm_state_serial) return; /* another WM_STATE update is pending, wait for it to complete */