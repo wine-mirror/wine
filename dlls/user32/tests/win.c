wine-mirror/wine

commit 8d7de32cd646f3d1f118836e643e6146c9837278

win32u: Respect STARTF_USESHOWWINDOW in show_window().

dlls/win32u/window.c
@@ -4515,6 +4515,7 @@ void update_window_state( HWND hwnd )
  */
 static BOOL show_window( HWND hwnd, INT cmd )
 {
+    static volatile LONG first_window = 1;
     WND *win;
     HWND parent;
     DWORD style = get_window_long( hwnd, GWL_STYLE ), new_style;
@@ -4526,6 +4527,19 @@ static BOOL show_window( HWND hwnd, INT cmd )
     TRACE( "hwnd=%p, cmd=%d, was_visible %d\n", hwnd, cmd, was_visible );
 
     context = set_thread_dpi_awareness_context( get_window_dpi_awareness_context( hwnd ));
+
+    if ((!(style & (WS_POPUP | WS_CHILD))
+         || ((style & (WS_POPUP | WS_CHILD | WS_CAPTION)) == (WS_POPUP | WS_CAPTION)))
+        && InterlockedExchange( &first_window, 0 ))
+    {
+        RTL_USER_PROCESS_PARAMETERS *params = NtCurrentTeb()->Peb->ProcessParameters;
+
+        if (params->dwFlags & STARTF_USESHOWWINDOW && (cmd == SW_SHOW || cmd == SW_SHOWNORMAL || cmd == SW_SHOWDEFAULT))
+        {
+            cmd = params->wShowWindow;
+            TRACE( "hwnd=%p, using cmd %d from startup info.\n", hwnd, cmd );
+        }
+    }
 
     switch(cmd)
     {

dlls/user32/tests/win.c
@@ -13704,14 +13704,14 @@ static void test_startupinfo_showwindow_proc( int test_id )
     expected = test->cmd_show != SW_HIDE && test->cmd_show != SW_FORCEMINIMIZE && !test->show_affected;
     bval = !!IsWindowVisible( hwnd );
 
-    todo_wine_if((test->show_affected && sa.wShowWindow == SW_HIDE) || test->cmd_show == SW_FORCEMINIMIZE)
+    todo_wine_if(test->cmd_show == SW_FORCEMINIMIZE)
     ok( bval == expected, "got %d, expected %d.\n", bval, expected );
 
     /* After default args were used once SW_SHOWDEFAULT doesn't use startupinfo. */
     ShowWindow( hwnd, SW_SHOWDEFAULT );
     bval = !!IsWindowVisible( hwnd );
     expected = test->counted_as_first;
-    todo_wine_if(!test->counted_as_first && sa.wShowWindow == SW_HIDE) ok( bval == expected, "got %d, expected %d.\n", bval, expected );
+    ok( bval == expected, "got %d, expected %d.\n", bval, expected );
     DestroyWindow( hwnd );
     pump_messages();
 

commit f163d1dd0c960c2a3a0b18a4ed33f36c75f03c3c

win32u: Ignore startup cmd show mode for owned windows.
Fixes a regression introduced by 8d7de32cd646f3d1f118836e643e6146c9837278.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59034

dlls/win32u/window.c
@@ -4739,6 +4739,7 @@ static BOOL show_window( HWND hwnd, INT cmd )
 
     if ((!(style & (WS_POPUP | WS_CHILD))
          || ((style & (WS_POPUP | WS_CHILD | WS_CAPTION)) == (WS_POPUP | WS_CAPTION)))
+        && !get_window_relative( hwnd, GW_OWNER )
         && InterlockedExchange( &first_window, 0 ))
     {
         RTL_USER_PROCESS_PARAMETERS *params = NtCurrentTeb()->Peb->ProcessParameters;

dlls/user32/tests/win.c
@@ -13677,20 +13677,27 @@ static const struct test_startupinfo_showwindow_test test_startupinfo_showwindow
 static void test_startupinfo_showwindow_proc( int test_id )
 {
     const struct test_startupinfo_showwindow_test *test = &test_startupinfo_showwindow_tests[test_id];
-    static const DWORD ignored_window_styles[] =
+    static const struct
     {
-        WS_CHILD,
-        WS_POPUP, /* WS_POPUP windows are not ignored when used with WS_CAPTION (which is WS_BORDER | WS_DLGFRAME) */
-        WS_CHILD | WS_POPUP,
-        WS_POPUP | WS_BORDER,
-        WS_POPUP | WS_DLGFRAME,
-        WS_POPUP | WS_SYSMENU | WS_THICKFRAME| WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+        DWORD style;
+        BOOL parent;
+    }
+    ignored_window_styles[] =
+    {
+        { WS_CHILD, TRUE },
+        { WS_POPUP }, /* Unowned WS_POPUP windows are not ignored when used with WS_CAPTION (which is WS_BORDER | WS_DLGFRAME) */
+        { WS_CHILD | WS_POPUP, TRUE },
+        { WS_POPUP | WS_BORDER },
+        { WS_POPUP | WS_DLGFRAME },
+        { WS_POPUP | WS_SYSMENU | WS_THICKFRAME| WS_MINIMIZEBOX | WS_MAXIMIZEBOX },
+        { WS_OVERLAPPED, TRUE }, /* owned window */
+        { WS_POPUP | WS_CAPTION, TRUE }, /* owned window */
     };
     BOOL bval, expected;
     STARTUPINFOW sa;
     unsigned int i;
     DWORD style;
-    HWND hwnd;
+    HWND parent, hwnd;
 
     GetStartupInfoW( &sa );
 
@@ -13705,26 +13712,27 @@ static void test_startupinfo_showwindow_proc( int test_id )
      * SW_ variants for ShowWindow() which are not altered by startup info still consume startup info usage so can
      * only be tested once per process. */
 
-    hwnd = CreateWindowA( "static", "parent", WS_OVERLAPPED, 0, 0, 0, 0, NULL, NULL,
+    parent = CreateWindowA( "static", "parent", WS_OVERLAPPED, 0, 0, 0, 0, NULL, NULL,
                            GetModuleHandleW( NULL ), NULL );
     pump_messages();
     for (i = 0; i < ARRAY_SIZE(ignored_window_styles); ++i)
     {
         winetest_push_context( "%u", i );
-        hwnd = CreateWindowA( "static", "overlapped", ignored_window_styles[i], 0, 0, 0, 0,
-                               ignored_window_styles[i] & WS_CHILD ? hwnd : NULL, NULL,
+        hwnd = CreateWindowA( "static", "overlapped", ignored_window_styles[i].style, 0, 0, 0, 0,
+                               ignored_window_styles[i].parent ? parent : NULL, NULL,
                                GetModuleHandleW( NULL ), NULL );
         ok( !!hwnd, "got NULL.\n" );
         ShowWindow( hwnd, SW_SHOW );
         bval = IsWindowVisible( hwnd );
-        if ((ignored_window_styles[i] & (WS_CHILD | WS_POPUP)) == WS_CHILD)
+        if ((ignored_window_styles[i].style & (WS_CHILD | WS_POPUP)) == WS_CHILD)
             ok( !bval, "unexpectedly visible.\n" );
         else
             ok( bval, "unexpectedly invisible.\n" );
         pump_messages();
+        DestroyWindow( hwnd );
         winetest_pop_context();
     }
-    DestroyWindow( hwnd );
+    DestroyWindow( parent );
     pump_messages();
 
     style = test->style;