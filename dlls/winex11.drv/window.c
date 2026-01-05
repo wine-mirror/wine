wine-mirror/wine

commit 3bea06b80dd21d6ceb6afd44d25dc5d3fde4ff5c

winex11.drv: Fix xinerama_get_fullscreen_monitors() not working correctly with non-fullscreen rects.
xinerama_get_fullscreen_monitors() was supposed to be called with fullscreen rects. Thus, we could
assume the monitor indices are all zero when there is only one monitor as an optimization. However,
after baf65867, xinerama_get_fullscreen_monitors() can be called with non-fullscreen rects. So we
need to remove the optimization so that it works correctly for non-fullscreen rects as well.

Fix the Guild Wars 2 game window having an offset in fullscreen mode.

dlls/winex11.drv/window.c
@@ -1685,9 +1685,10 @@ static UINT window_update_client_config( struct x11drv_win_data *data )
      * window rect will be repeatedly changed by the WM and the application, causing a flickering effect */
     if (data->is_fullscreen)
     {
-        xinerama_get_fullscreen_monitors( &data->rects.visible, &old_generation, old_monitors );
-        xinerama_get_fullscreen_monitors( &data->current_state.rect, &generation, monitors );
-        if (!memcmp( old_monitors, monitors, sizeof(monitors) )) return 0;
+        if (xinerama_get_fullscreen_monitors( &data->rects.visible, &old_generation, old_monitors )
+            && xinerama_get_fullscreen_monitors( &data->current_state.rect, &generation, monitors )
+            && !memcmp( old_monitors, monitors, sizeof(monitors) ))
+            return 0;
     }
 
     flags = SWP_NOACTIVATE | SWP_NOZORDER;

dlls/winex11.drv/x11drv.h
@@ -746,7 +746,7 @@ extern POINT virtual_screen_to_root( INT x, INT y );
 extern POINT root_to_virtual_screen( INT x, INT y );
 extern RECT get_host_primary_monitor_rect(void);
 extern RECT get_work_area( const RECT *monitor_rect );
-extern void xinerama_get_fullscreen_monitors( const RECT *rect, unsigned int *generation, long *indices );
+extern BOOL xinerama_get_fullscreen_monitors( const RECT *rect, unsigned int *generation, long *indices );
 extern void xinerama_init( unsigned int width, unsigned int height );
 extern void init_recursive_mutex( pthread_mutex_t *mutex );
 extern void init_icm_profile(void);

dlls/winex11.drv/xinerama.c
@@ -124,20 +124,16 @@ static inline int query_screens(void)
 
 #endif  /* SONAME_LIBXINERAMA */
 
-/* Get xinerama monitor indices required for _NET_WM_FULLSCREEN_MONITORS */
-void xinerama_get_fullscreen_monitors( const RECT *rect, unsigned int *generation, long *indices )
+/* Get xinerama monitor indices required for _NET_WM_FULLSCREEN_MONITORS. Return FALSE if rect is
+ * not fullscreen */
+BOOL xinerama_get_fullscreen_monitors( const RECT *rect, unsigned int *generation, long *indices )
 {
     RECT window_rect, intersected_rect, monitor_rect;
+    BOOL ret = FALSE;
     POINT offset;
     INT i;
 
     pthread_mutex_lock( &xinerama_mutex );
-    if (nb_monitors == 1)
-    {
-        memset( indices, 0, sizeof(*indices) * 4 );
-        *generation = xinerama_generation;
-        goto done;
-    }
 
     /* Convert window rectangle to root coordinates */
     offset = virtual_screen_to_root( rect->left, rect->top );
@@ -173,10 +169,11 @@ void xinerama_get_fullscreen_monitors( const RECT *rect, unsigned int *generatio
         }
     }
 
-    if (indices[0] == -1) WARN("Failed to get xinerama fullscreen monitor indices.\n");
+    if (indices[0] != -1)
+        ret = TRUE;
 
-done:
     pthread_mutex_unlock( &xinerama_mutex );
+    return ret;
 }
 
 static BOOL xinerama_get_gpus( struct x11drv_gpu **new_gpus, int *count, BOOL get_properties )

commit 97033f3faa3eda73a34f77689610a20243c747b6

winex11.drv: Don't send -1 fullscreen monitor indices to window managers.
Fix a regression from 3bea06b8. This partly reverts 6a2e414d, which introduced this bug initially.
With 3bea06b8, it becomes much more likely that indices (-1, -1, -1, -1) got sent to window managers.
This can happen, for example, a game with a 1920x1080 window changes the display resolution to
3840x2160, then changes the window size to 3840x2160. After the resolution change and before the
window size change, xinerama_get_fullscreen_monitors() will return monitor indices (-1, -1, -1, -1)
because 1920x1080 is not fullscreen.

While sending a _NET_WM_FULLSCREEN_MONITORS client message with fullscreen monitor indices
(-1, -1, -1, -1) clears the _NET_WM_FULLSCREEN_MONITORS property for a window on KWin, it can also
make the window to disappear and lose focus. See KWin X11Window::fullscreenMonitorsArea()[1] and
X11Window::updateFullscreenMonitors()[2]. Note that QRect is constructed with an empty rect at the
beginning of X11Window::fullscreenMonitorsArea(). So fullscreen monitor indices (-1, -1, -1, -1)
will produce an empty rect for moveResize() in X11Window::updateFullscreenMonitors() and cause the
window to disappear. While indices (-1, 0, 0, 0) might work, I think it's better not to change
_NET_WM_FULLSCREEN_MONITORS when the window is not fullscreen because removing the property is not
part of any standard.

[1]: https://invent.kde.org/plasma/kwin-x11/-/blob/v6.5.4/src/x11window.cpp?ref_type=tags#L4817
[2]: https://invent.kde.org/plasma/kwin-x11/-/blob/v6.5.4/src/x11window.cpp?ref_type=tags#L4794

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59180

dlls/winex11.drv/window.c
@@ -1289,7 +1289,8 @@ static void update_net_wm_fullscreen_monitors( struct x11drv_win_data *data )
     if (!X11DRV_DisplayDevices_SupportEventHandlers())
         return;
 
-    xinerama_get_fullscreen_monitors( &data->rects.visible, &monitors.generation, monitors.indices );
+    if (!xinerama_get_fullscreen_monitors( &data->rects.visible, &monitors.generation, monitors.indices ))
+        return;
     data->desired_state.monitors = monitors;
 
     if (!memcmp( old_monitors, &monitors, sizeof(monitors) )) return; /* states are the same, nothing to update */
@@ -1299,9 +1300,8 @@ static void update_net_wm_fullscreen_monitors( struct x11drv_win_data *data )
         memcpy( &data->pending_state.monitors, &monitors, sizeof(monitors) );
         TRACE( "window %p/%lx, requesting _NET_WM_FULLSCREEN_MONITORS %s serial %lu\n", data->hwnd, data->whole_window,
                debugstr_monitor_indices( &monitors ), NextRequest( data->display ) );
-        if (monitors.indices[0] == -1) XDeleteProperty( data->display, data->whole_window, x11drv_atom(_NET_WM_FULLSCREEN_MONITORS) );
-        else XChangeProperty( data->display, data->whole_window, x11drv_atom(_NET_WM_FULLSCREEN_MONITORS),
-                              XA_CARDINAL, 32, PropModeReplace, (unsigned char *)monitors.indices, 4 );
+        XChangeProperty( data->display, data->whole_window, x11drv_atom(_NET_WM_FULLSCREEN_MONITORS),
+                         XA_CARDINAL, 32, PropModeReplace, (unsigned char *)monitors.indices, 4 );
     }
     else
     {