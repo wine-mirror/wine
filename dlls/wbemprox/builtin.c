xodiosx/Wine-arm64ce

wbemprox: Add Win32_SystemEnclosure.SerialNumber.
CW-Bug-Id: #26354

dlls/wbemprox/builtin.c
@@ -490,6 +490,7 @@ static const struct column col_systemenclosure[] =
     { L"Manufacturer", CIM_STRING|COL_FLAG_DYNAMIC },
     { L"Name",         CIM_STRING },
     { L"Tag",          CIM_STRING },
+    { L"SerialNumber", CIM_STRING|COL_FLAG_DYNAMIC },
 };
 static const struct column col_systemsecurity[] =
 {
@@ -1050,6 +1051,7 @@ struct record_systemenclosure
     const WCHAR        *manufacturer;
     const WCHAR        *name;
     const WCHAR        *tag;
+    const WCHAR        *serial_number;
 };
 struct record_videocontroller
 {
@@ -4347,6 +4349,13 @@ done:
     return ret;
 }
 
+static WCHAR *get_systemenclosure_serialnumber( const char *buf, UINT len )
+{
+    WCHAR *ret = get_smbios_string( SMBIOS_TYPE_SYSTEM, 0, offsetof(struct smbios_system, serial), buf, len );
+    if (!ret) return wcsdup( L"0" );
+    return ret;
+}
+
 static enum fill_status fill_systemenclosure( struct table *table, const struct expr *cond )
 {
     struct record_systemenclosure *rec;
@@ -4368,6 +4377,7 @@ static enum fill_status fill_systemenclosure( struct table *table, const struct 
     rec->manufacturer = get_systemenclosure_manufacturer( buf, len );
     rec->name         = L"System Enclosure";
     rec->tag          = L"System Enclosure 0";
+    rec->serial_number = get_systemenclosure_serialnumber( buf, len );
     if (!match_row( table, row, cond, &status )) free_row_values( table, row );
     else row++;
 

wine-mirror/wine

dlls/wbemprox/builtin.c
@@ -526,6 +526,7 @@ static const struct column col_systemenclosure[] =
     { L"LockPresent",  CIM_BOOLEAN },
     { L"Manufacturer", CIM_STRING|COL_FLAG_DYNAMIC },
     { L"Name",         CIM_STRING },
+    { L"SerialNumber", CIM_STRING|COL_FLAG_DYNAMIC },
     { L"Tag",          CIM_STRING },
 };
 static const struct column col_systemsecurity[] =
@@ -1123,6 +1124,7 @@ struct record_systemenclosure
     int                 lockpresent;
     const WCHAR        *manufacturer;
     const WCHAR        *name;
+    const WCHAR        *serial_number;
     const WCHAR        *tag;
 };
 struct record_videocontroller
@@ -4572,6 +4574,13 @@ done:
     return ret;
 }
 
+static WCHAR *get_systemenclosure_serialnumber( const char *buf, UINT len )
+{
+    WCHAR *ret = get_smbios_string( SMBIOS_TYPE_CHASSIS, 0, offsetof(struct smbios_chassis, serial), buf, len );
+    if (!ret) return wcsdup( L"0" );
+    return ret;
+}
+
 static enum fill_status fill_systemenclosure( struct table *table, const struct expr *cond )
 {
     struct record_systemenclosure *rec;
@@ -4592,6 +4601,7 @@ static enum fill_status fill_systemenclosure( struct table *table, const struct 
     rec->lockpresent  = get_systemenclosure_lockpresent( buf, len );
     rec->manufacturer = get_systemenclosure_manufacturer( buf, len );
     rec->name         = L"System Enclosure";
+    rec->serial_number = get_systemenclosure_serialnumber( buf, len );
     rec->tag          = L"System Enclosure 0";
     if (!match_row( table, row, cond, &status )) free_row_values( table, row );
     else row++;