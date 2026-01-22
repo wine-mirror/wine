wine-mirror/wine

commit 88854b52599d76b9fdd51819396aacf009712655

wineboot: Fix a memory leak in create_computer_name_keys.
If the canonical hostname was "localhost", the address info was not
freed.

Don't bother checking whether ai_canonname is NULL because if there is
no canonical name, getaddrinfo must set ai_canonname to the input name.

Fixes: ca5a6d07dc92ba631b178ec175e6b3fd5295e3d6

programs/wineboot/wineboot.c
@@ -833,15 +833,16 @@ static void create_computer_name_keys(void)
 
     if (gethostname( buffer, sizeof(buffer) )) return;
     hints.ai_flags = AI_CANONNAME;
-    if (!getaddrinfo( buffer, NULL, &hints, &res ) &&
-        res->ai_canonname && strcasecmp(res->ai_canonname, "localhost") != 0)
+    if (getaddrinfo( buffer, NULL, &hints, &res ) != 0)
+        res = NULL;
+    else if (strcasecmp( res->ai_canonname, "localhost" ) != 0)
         name = res->ai_canonname;
     dot = strchr( name, '.' );
     if (dot) *dot++ = 0;
     else dot = name + strlen(name);
     SetComputerNameExA( ComputerNamePhysicalDnsDomain, dot );
     SetComputerNameExA( ComputerNamePhysicalDnsHostname, name );
-    if (name != buffer) freeaddrinfo( res );
+    if (res) freeaddrinfo( res );
 
     if (RegOpenKeyW( HKEY_LOCAL_MACHINE, L"System\\CurrentControlSet\\Control\\ComputerName", &key ))
         return;

commit 00f88cd59bb1b867d1b8b0064eb442583c1eefbf

wineboot: Check if ai_canonname is NULL.
Linux may not ever return a null cannonname when AI_CANONNAME is set,
but macOS does.

Fixes: 88854b52599d76b9fdd51819396aacf009712655

programs/wineboot/wineboot.c
@@ -836,7 +836,7 @@ static void create_computer_name_keys(void)
     hints.ai_flags = AI_CANONNAME;
     if (getaddrinfo( buffer, NULL, &hints, &res ) != 0)
         res = NULL;
-    else if (strcasecmp( res->ai_canonname, "localhost" ) != 0)
+    else if (res->ai_canonname && strcasecmp( res->ai_canonname, "localhost" ) != 0)
         name = res->ai_canonname;
     dot = strchr( name, '.' );
     if (dot) *dot++ = 0;