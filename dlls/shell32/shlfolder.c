1b2aa602d60d53cc17f21fa908d477595ececd14
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2026-02-28 18:33:54 +0900

    shell32: Only compute HASSUBFOLDER if requested.
    SFGAO_HASSUBFOLDER enumerates the whole subdirectory and the slowness is
    immediately noticeable in the system file dialog when opening /.
    
    Most shell32 users don't require HASSUBFOLDER so skip the computation when
    not requested.

--- dlls/shell32/shlfolder.c
+++ dlls/shell32/shlfolder.c
@@ -443,15 +443,18 @@ HRESULT SHELL32_GetItemAttributes (IShellFolder2 *psf, LPCITEMIDLIST pidl, LPDWO
 
             *pdwAttributes |= (SFGAO_FOLDER | SFGAO_FILESYSANCESTOR | SFGAO_STORAGEANCESTOR | SFGAO_STORAGE);
 
-            if (SUCCEEDED(IShellFolder2_BindToObject(psf, pidl, NULL, &IID_IShellFolder, (void **)&child)))
+            if (dwAttributes & SFGAO_HASSUBFOLDER)
             {
-                if (IShellFolder_EnumObjects(child, NULL, SHCONTF_FOLDERS|SHCONTF_INCLUDEHIDDEN, &enum_ids) == S_OK)
+                if (SUCCEEDED(IShellFolder2_BindToObject(psf, pidl, NULL, &IID_IShellFolder, (void **)&child)))
                 {
-                    if (IEnumIDList_Skip(enum_ids, 1) != S_OK)
-                        *pdwAttributes &= ~SFGAO_HASSUBFOLDER;
-                    IEnumIDList_Release(enum_ids);
+                    if (IShellFolder_EnumObjects(child, NULL, SHCONTF_FOLDERS|SHCONTF_INCLUDEHIDDEN, &enum_ids) == S_OK)
+                    {
+                        if (IEnumIDList_Skip(enum_ids, 1) != S_OK)
+                            *pdwAttributes &= ~SFGAO_HASSUBFOLDER;
+                        IEnumIDList_Release(enum_ids);
+                    }
+                    IShellFolder_Release(child);
                 }
-                IShellFolder_Release(child);
             }
         }
 	else
