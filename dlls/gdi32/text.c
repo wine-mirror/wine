2d7046c3458382c5cea6c373d1903e00d983cb7e
Elizabeth Figura <zfigura@codeweavers.com>
2026-03-15 23:25:30 -0500

    gdi32: Redirect system32 in AddFontResourceEx().
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=58963

--- dlls/gdi32/text.c
+++ dlls/gdi32/text.c
@@ -2557,6 +2557,12 @@ static int add_font_resource( const WCHAR *str, DWORD flags, void *dv )
             free( system_dir );
             return 0;
         }
+
+        /* Windows does not redirect the path here, which is presumably a bug.
+         * Stratego (1997) tries to create a font resource in system32
+         * and fails on 64-bit Windows. */
+        redirect_path( &nt_name );
+
         ret = NtGdiAddFontResourceW( nt_name.Buffer, nt_name.Length / sizeof(WCHAR) + 1,
                                      1, flags, 0, dv );
         RtlFreeUnicodeString( &nt_name );
@@ -2572,6 +2578,8 @@ INT WINAPI AddFontResourceExW( const WCHAR *str, DWORD flags, void *dv )
     WCHAR *filename = NULL;
     BOOL hidden;
     INT ret;
+
+    TRACE( "%s flags %#lx res %p\n", debugstr_w(str), flags, dv );
 
     if ((ret = add_font_resource( str, flags, dv ))) return ret;
 
