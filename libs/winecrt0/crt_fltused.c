51b5ea979ab0833a6237fd6f0a0bfb97e983a090
Alexandre Julliard <julliard@winehq.org>
2026-04-02 13:46:30 +0200

    winecrt0: Add LGPL license exception.
    Using the GCC Runtime Library exception, similar to what glibc is
    doing for files that get linked directly into the application.

--- libs/winecrt0/crt_fltused.c
+++ libs/winecrt0/crt_fltused.c
@@ -6,6 +6,14 @@
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
  *
+ * In addition to the permissions in the GNU Lesser General Public License,
+ * the authors give you unlimited permission to link the compiled version
+ * of this file with other programs, and to distribute those programs
+ * without any restriction coming from the use of this file.  (The GNU
+ * Lesser General Public License restrictions do apply in other respects;
+ * for example, they cover modification of the file, and distribution when
+ * not linked into another program.)
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
