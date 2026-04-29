wine-mirror/wine

commit be60f77909fc9ce2c9b6f1abd005e3b4aabaafec

win32u: Update the window config before its state, as internal config.
When restoring a window, so that the window gets restored to the size
it currently has from the host point of view. Either the host already
has restored it to its original size, or it will shortly, but we should
avoid requesting config changes concurrently if possible.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=58804

dlls/win32u/message.c
@@ -2229,18 +2229,27 @@ static LRESULT handle_internal_message( HWND hwnd, UINT msg, WPARAM wparam, LPAR
         HWND foreground;
 
         if (!user_driver->pGetWindowStateUpdates( hwnd, &state_cmd, &swp_flags, &window_rect, &foreground )) return 0;
-        if (foreground) NtUserSetForegroundWindow( foreground );
-        if (state_cmd)
-        {
-            if (LOWORD(state_cmd) == SC_RESTORE && HIWORD(state_cmd)) NtUserSetActiveWindow( hwnd );
-            send_message( hwnd, WM_SYSCOMMAND, LOWORD(state_cmd), 0 );
+        window_rect = map_rect_raw_to_virt( window_rect, get_thread_dpi() );
 
-            /* state change might have changed the window config already, check again */
-            user_driver->pGetWindowStateUpdates( hwnd, &state_cmd, &swp_flags, &window_rect, &foreground );
-            if (foreground) NtUserSetForegroundWindow( foreground );
-            if (state_cmd) WARN( "window %p state needs another update, ignoring\n", hwnd );
+        if (foreground) NtUserSetForegroundWindow( foreground );
+        switch (LOWORD(state_cmd))
+        {
+        case SC_MAXIMIZE:
+        case SC_MINIMIZE:
+            send_message( hwnd, WM_SYSCOMMAND, LOWORD(state_cmd), 0 );
+            break;
+        case SC_RESTORE:
+            if (HIWORD(state_cmd)) NtUserSetActiveWindow( hwnd );
+            NtUserSetInternalWindowPos( hwnd, SW_SHOW, &window_rect, NULL );
+            send_message( hwnd, WM_SYSCOMMAND, LOWORD(state_cmd), 0 );
+            break;
+        default:
+            if (!swp_flags) break;
+            NtUserSetWindowPos( hwnd, 0, window_rect.left, window_rect.top, window_rect.right - window_rect.left,
+                                window_rect.bottom - window_rect.top, swp_flags );
+            break;
         }
-        if (swp_flags) NtUserSetRawWindowPos( hwnd, window_rect, swp_flags, FALSE );
+
         return 0;
     }
     case WM_WINE_UPDATEWINDOWSTATE:

commit ec61f02ccf5aaca4904872001d957f425c1de0ea

win32u: Set the normal window position without going through NtUserSetInternalWindowPos().
Fix a regression from be60f77909fc9ce2c9b6f1abd005e3b4aabaafec, the intent of which was to restore
the win32 window position to the current host window position. However, NtUserSetInternalWindowPos()
eventually calls NtUserSetWindowPos() and NtUserShowWindow(). We don't want that because they might
generate window position change messages that applications might not expect.

Fix Homeworld Remastered Collection (244160) Homeworld 2 Classic black screen after restoring from
the minimized state in fullscreen mode due to an unexpected window size.

dlls/win32u/message.c
@@ -2235,7 +2235,10 @@ static LRESULT handle_internal_message( HWND hwnd, UINT msg, WPARAM wparam, LPAR
         {
         case SC_RESTORE:
             if (HIWORD(state_cmd)) NtUserSetActiveWindow( hwnd );
-            NtUserSetInternalWindowPos( hwnd, SW_SHOW, &window_rect, NULL );
+
+            /* make the win32 window restore to the current host window config */
+            set_window_normal_placement( hwnd, window_rect );
+
             /* fallthrough */
         default:
             send_message( hwnd, WM_SYSCOMMAND, LOWORD(state_cmd), 0 );

dlls/win32u/win32u_private.h
@@ -305,6 +305,7 @@ extern void map_window_region( HWND from, HWND to, HRGN hrgn );
 extern BOOL screen_to_client( HWND hwnd, POINT *pt );
 extern LONG_PTR set_window_long( HWND hwnd, INT offset, UINT size, LONG_PTR newval,
                                  BOOL ansi );
+extern void set_window_normal_placement( HWND hwnd, RECT rect );
 extern BOOL set_window_pos( WINDOWPOS *winpos, int parent_x, int parent_y );
 extern UINT set_window_style_bits( HWND hwnd, UINT set_bits, UINT clear_bits );
 extern void update_window_state( HWND hwnd );

dlls/win32u/window.c
@@ -3075,6 +3075,18 @@ static void make_point_onscreen( POINT *pt )
     pt->y = rect.top;
 }
 
+void set_window_normal_placement( HWND hwnd, RECT rect )
+{
+    WND *win = get_win_ptr( hwnd );
+
+    if (win && win != WND_OTHER_PROCESS && win != WND_DESKTOP)
+    {
+        make_rect_onscreen( &rect );
+        win->normal_rect = rect_thread_to_win_dpi( hwnd, rect );
+        release_win_ptr( win );
+    }
+}
+
 static BOOL set_window_placement( HWND hwnd, const WINDOWPLACEMENT *wndpl, UINT flags )
 {
     WND *win = get_win_ptr( hwnd );