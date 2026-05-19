c2752bbb3a4cbef7b1eda950a1fb361b84a9c503
Stefan Dösinger <stefan@codeweavers.com>
2026-05-19 17:13:40 +0300

    shell32: Add an Info icon.
    The icon file is taken from dlls/user32/resources/oic_note.svg but
    renamed info.svg to match "SIID_INFO".

--- dlls/shell32/shresdef.h
+++ dlls/shell32/shresdef.h
@@ -375,6 +375,8 @@
 #define IDI_SHELL_RESTRICTED2      338
 /* 339 - 511 not used on Windows */
 #define IDI_SHELL_WEB_BROWSER      512
+/* 512 - 1000 not used on Windows */
+#define IDI_SHELL_INFO             1001
 
 /* 
 AVI resources, windows shell32 has 14 of them: 150-152 and 160-170
