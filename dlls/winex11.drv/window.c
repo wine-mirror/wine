wine-mirror/wine

commit 4c07e5568e5c70a7899839ab85dee68d705935e3

winex11.drv: Go though WithdrawnState when transitioning from IconicState to NormalState.
When transitioning a window from IconicState to NormalState and the window is managed, go through
WithdrawnState. This is needed because Mutter doesn't unmap windows when making windows iconic/minimized
as Mutter needs to support live preview for minimized windows. So on Mutter, a window can be both
iconic and mapped. If the window is mapped, then XMapWindow() will have no effect according to the
XMapWindow() documentation. Thus we have to transition to WithdrawnState first, then to NormalState.

Fix Mass Effect Legendary Edition (1328670) starts minimized.

dlls/winex11.drv/window.c
@@ -1448,6 +1448,20 @@ static void window_set_wm_state( struct x11drv_win_data *data, UINT new_state )
         return; /* another map/unmap WM_STATE update is pending, wait for it to complete */
     if (old_state == new_state) return; /* states are the same, nothing to update */
 
+    /* When transitioning a window from IconicState to NormalState and the window is managed, go
+     * through WithdrawnState. This is needed because Mutter doesn't unmap windows when making
+     * windows iconic/minimized as Mutter needs to support live preview for minimized windows. So on
+     * Mutter, a window can be both iconic and mapped. If the window is mapped, then XMapWindow()
+     * will have no effect according to the  XMapWindow() documentation. Thus we have to transition
+     * to WithdrawnState first, then to NormalState */
+    if (data->managed && MAKELONG(old_state, new_state) == MAKELONG(IconicState, NormalState))
+    {
+        WARN( "window %p/%lx is iconic, remapping to workaround Mutter issues.\n", data->hwnd, data->whole_window );
+        window_set_wm_state( data, WithdrawnState );
+        window_set_wm_state( data, NormalState );
+        return;
+    }
+
     switch (MAKELONG(old_state, new_state))
     {
     case MAKELONG(WithdrawnState, IconicState):

commit 16c5eccc119aaf14242376d6b787a9c90658e1d0

winex11.drv: Wait for the previous request when transitioning from IconicState to NormalState.
Fix a regression from 4c07e556. ea44fc34 is also related. Due to ea44fc34, when calling
window_set_wm_state(WithdrawnState), Wine doesn't wait for the previous WM_STATE update to be
completed. So when calling window_set_wm_state(WithdrawnState) and then window_set_wm_state(NormalState),
the queued NormalState can be overwritten by an unexpected IconicState property notify event from
a previous request, causing a window to be stuck in IconicState. For example,

```
40551.339:012c:fixme:x11drv:window_set_wm_state calling XIconifyWindow for hwnd 0x200ae.
...
40551.340:012c:warn:x11drv:window_set_wm_state window 0x200ae/aa00003 is iconic, remapping to workaround Mutter issues. data->current_state.wm_state 1 data->pending_state.wm_state 3.
40551.340:012c:trace:x11drv:window_set_wm_state window 0x200ae/aa00003, requesting WM_STATE 0x3 -> 0 serial 5227, foreground 0x10020, activate 0
...
40551.342:012c:warn:x11drv:handle_state_change window 0x200ae/aa00003 mismatch WM_STATE 0x3/5232, expected 0/5227
```

Fix RiME(493200) sometimes fails to restore from Alt+Tab.

dlls/winex11.drv/window.c
@@ -1588,6 +1588,10 @@ static void window_set_wm_state( struct x11drv_win_data *data, UINT new_state, B
      * to WithdrawnState first, then to NormalState */
     if (data->managed && MAKELONG(old_state, new_state) == MAKELONG(IconicState, NormalState))
     {
+        /* Previous IconicState request is still pending, wait for it to complete so that there is no
+         * unexpected IconicState WM_STATE notify that will override the NormalState soon to be queued */
+        if (data->wm_state_serial) return;
+
         WARN( "window %p/%lx is iconic, remapping to workaround Mutter issues.\n", data->hwnd, data->whole_window );
         window_set_wm_state( data, WithdrawnState, FALSE );
         window_set_wm_state( data, NormalState, activate );