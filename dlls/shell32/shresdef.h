wine-mirror/wine

commit def41b05f639ab0f07fd57323a1d29e914e335ca

shell32: Add shield icon.
Co-authored-by: Joel Holdsworth <joel@airwebreathe.org.uk>

dlls/shell32/shell32.rc
@@ -590,6 +590,9 @@ IDI_SHELL_BLANK4 ICON resources/blank.ico
 /* @makedep: resources/drive.ico */
 IDI_SHELL_UNKNOWN_DRIVE ICON resources/drive.ico
 
+/* @makedep: resources/shield.ico */
+IDI_SHELL_SHIELD ICON resources/shield.ico
+
 /* FIXME: Following resource is not yet added */
 /* @makedep: resources/document.ico */
 IDI_SHELL_FILES ICON resources/document.ico

dlls/shell32/shresdef.h
@@ -227,6 +227,7 @@
 #define IDI_SHELL_BLANK3            52
 #define IDI_SHELL_BLANK4            53
 #define IDI_SHELL_UNKNOWN_DRIVE     54
+#define IDI_SHELL_SHIELD            78
 /* 55 - 132 not used on Windows */
 #define IDI_SHELL_FILES            133
 #define IDI_SHELL_FILE_SEARCH      134

dlls/shell32/Makefile.in
@@ -61,6 +61,7 @@ SOURCES = \
 	resources/restricted.svg \
 	resources/search.svg \
 	resources/security.svg \
+	resources/shield.svg \
 	resources/shortcut.svg \
 	resources/shut_down.svg \
 	resources/sleep.svg \

commit 7327ba0899f6253a742566e9c40b76e12cdbaad7

shell32: Correct shield icon ID.
Fixes: def41b05f639ab0f07fd57323a1d29e914e335ca

dlls/shell32/shell32.rc
@@ -590,9 +590,6 @@ IDI_SHELL_BLANK4 ICON resources/blank.ico
 /* @makedep: resources/drive.ico */
 IDI_SHELL_UNKNOWN_DRIVE ICON resources/drive.ico
 
-/* @makedep: resources/shield.ico */
-IDI_SHELL_SHIELD ICON resources/shield.ico
-
 /* FIXME: Following resource is not yet added */
 /* @makedep: resources/document.ico */
 IDI_SHELL_FILES ICON resources/document.ico
@@ -1075,6 +1072,9 @@ IDI_SHELL_INFO ICON resources/info.ico
 /* FIXME: Following resource is not yet added */
 /* @makedep: resources/mycomputer.ico */
 IDI_SHELL_COMPUTER_OFF ICON resources/mycomputer.ico
+
+/* @makedep: resources/shield.ico */
+IDI_SHELL_SHIELD ICON resources/shield.ico
 
 /* @makedep: resources/idb_sort_incr.bmp */
 IDB_SORT_INCR BITMAP resources/idb_sort_incr.bmp

dlls/shell32/shresdef.h
@@ -227,7 +227,6 @@
 #define IDI_SHELL_BLANK3            52
 #define IDI_SHELL_BLANK4            53
 #define IDI_SHELL_UNKNOWN_DRIVE     54
-#define IDI_SHELL_SHIELD            78
 /* 55 - 132 not used on Windows */
 #define IDI_SHELL_FILES            133
 #define IDI_SHELL_FILE_SEARCH      134
@@ -379,6 +378,8 @@
 #define IDI_SHELL_INFO             1001
 /* 1012 - 8239 not used on Windows */
 #define IDI_SHELL_COMPUTER_OFF     8240
+/* 8241 - 16709 not used on Windows */
+#define IDI_SHELL_SHIELD           16747
 
 /* 
 AVI resources, windows shell32 has 14 of them: 150-152 and 160-170