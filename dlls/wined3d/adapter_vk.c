wine-mirror/wine

commit 035dc483881225fb8181f5e4ad85d05ba5c3b989

wined3d: Request VK_KHR_external_memory on Vulkan 1.0.
For VkPhysicalDeviceIDProperties. Found by the validation layers.

Signed-off-by: Henri Verbeet <hverbeet@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/wined3d/adapter_vk.c
@@ -1821,6 +1821,7 @@ static const struct
 vulkan_instance_extensions[] =
 {
     {VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME, VK_API_VERSION_1_1, FALSE},
+    {VK_KHR_EXTERNAL_MEMORY_EXTENSION_NAME,                  VK_API_VERSION_1_1, FALSE},
     {VK_KHR_SURFACE_EXTENSION_NAME,                          ~0u,                TRUE},
     {VK_KHR_WIN32_SURFACE_EXTENSION_NAME,                    ~0u,                TRUE},
 };

commit ccdaa2b58121381fdf5507c9c6210ae6306cb722

wined3d: Enable VK_KHR_external_fence_capabilities when initializing vulkan.
Currently, the vulkan validation layers emit an error for using
VkPhysicalDeviceIDProperties when using VK_API_VERSION_1_0 without
importing any of the instance extensions that implement it, which are
VK_KHR_external_fence_capabilities, VK_KHR_external_memory_capabilities,
and VK_KHR_external_semaphore_capabilities.

VK_KHR_external_memory_capabilities (instance extension) should not be
confused with VK_KHR_external_memory (device extension), this is a
mistake in the current code.

Regardless of this, replacing with VK_KHR_external_memory_capabilities
doesn't fix the error since it seems that there is a bug in the vulkan
validation layer as well, so VK_KHR_external_fence_capabilities is
initialized instead.

This is only needed for VK_API_VERSION_1_0, since it the missing struct
is part of the core functionalities in VK_API_VERSION_1_1.

Fixes: 035dc483881225fb8181f5e4ad85d05ba5c3b989

dlls/wined3d/adapter_vk.c
@@ -1933,7 +1933,7 @@ static const struct
 vulkan_instance_extensions[] =
 {
     {VK_KHR_GET_PHYSICAL_DEVICE_PROPERTIES_2_EXTENSION_NAME, VK_API_VERSION_1_1, FALSE},
-    {VK_KHR_EXTERNAL_MEMORY_EXTENSION_NAME,                  VK_API_VERSION_1_1, FALSE},
+    {VK_KHR_EXTERNAL_FENCE_CAPABILITIES_EXTENSION_NAME,      VK_API_VERSION_1_1, FALSE},
     {VK_KHR_SURFACE_EXTENSION_NAME,                          ~0u,                TRUE},
     {VK_KHR_WIN32_SURFACE_EXTENSION_NAME,                    ~0u,                TRUE},
 };