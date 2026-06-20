30a06ed7cc11b0743aed04aaeb056cec5c361543
Eric Pouech <epouech@codeweavers.com>
2026-06-20 10:45:54 +0200

    winedbg: Support watch operation larger than 4 bytes.
    (eg. x86_64 supports 8-byte watchpoints).
    
    Signed-off-by: Eric Pouech <epouech@codeweavers.com>

--- programs/winedbg/debugger.h
+++ programs/winedbg/debugger.h
@@ -187,7 +187,7 @@ struct dbg_breakpoint
     unsigned int        info;
     struct              /* only used for watchpoints */
     {
-        BYTE		len : 2;
+        BYTE		len;
         DWORD64		oldval;
     } w;
     struct expr*        condition;
