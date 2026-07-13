searchingin/wine-9.0

ntdll: Work around AMD SYSRET SS descriptor behavior on FreeBSD.
On AMD CPUs the cached SS descriptor attributes could be invalid after
return to the userspace with SYSRET, which doesn't matter in the
long mode, but would easily crash programs in the compatibility mode.
Reloading %ss avoids that.

Note that Linux already has X86_BUG_SYSRET_SS_ATTRS as the workaround
for this issue.

dlls/ntdll/unix/signal_x86_64.c
@@ -2971,6 +2971,11 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
                    "testl $4,%r14d\n\t"            /* SYSCALL_HAVE_PTHREAD_TEB */
                    "jz 1f\n\t"
                    "movw 0x338(%r13),%fs\n"        /* amd64_thread_data()->fs */
+# ifdef __FreeBSD__
+                   /* reset %ss (after sysret) for AMD */
+                   "movq $0x3b,%rsi\n\t"           /* GSEL(GUDATA_SEL, SEL_UPL) */
+                   "movq %rsi,%ss\n\t"
+# endif
                    "1:\n\t"
 #elif defined __APPLE__
                    "movq %rax,%r8\n\t"
@@ -3258,6 +3263,11 @@ __ASM_GLOBAL_FUNC( __wine_unix_call_dispatcher,
                    "testl $4,%r14d\n\t"            /* SYSCALL_HAVE_PTHREAD_TEB */
                    "jz 1f\n\t"
                    "movw 0x338(%r13),%fs\n"        /* amd64_thread_data()->fs */
+# ifdef __FreeBSD__
+                   /* reset %ss (after sysret) for AMD */
+                   "movq $0x3b,%rsi\n\t"           /* GSEL(GUDATA_SEL, SEL_UPL) */
+                   "movq %rsi,%ss\n\t"
+# endif
                    "1:\n\t"
 #elif defined __APPLE__
                    "movq %rax,%rdx\n\t"

wine-mirror/wine

dlls/ntdll/unix/signal_x86_64.c
@@ -3172,6 +3172,11 @@ __ASM_GLOBAL_FUNC( __wine_syscall_dispatcher,
                    "testw %dx,%dx\n\t"
                    "jz 1f\n\t"
                    "movw %dx,%fs\n"
+# ifdef __FreeBSD__
+                   /* reset %ss (after sysret) for AMD */
+                   "movw $0x3b,%dx\n\t"           /* GSEL(GUDATA_SEL, SEL_UPL) */
+                   "movw %dx,%ss\n\t"
+# endif
                    "1:\n\t"
 #elif defined __APPLE__
                    "movq %rax,%r8\n\t"
@@ -3463,6 +3468,11 @@ __ASM_GLOBAL_FUNC( __wine_unix_call_dispatcher,
                    "testw %dx,%dx\n\t"
                    "jz 1f\n\t"
                    "movw %dx,%fs\n"
+# ifdef __FreeBSD__
+                   /* reset %ss (after sysret) for AMD */
+                   "movw $0x3b,%dx\n\t"           /* GSEL(GUDATA_SEL, SEL_UPL) */
+                   "movw %dx,%ss\n\t"
+# endif
                    "1:\n\t"
 #elif defined __APPLE__
                    "movq %rax,%rdx\n\t"