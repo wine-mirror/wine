e61823fa1d90908f9d7bda1569620f2b17ff301e
Paul Gofman <pgofman@codeweavers.com>
2026-08-14 14:06:35 -0600

    ntdll: Add WAYLAND_DISPLAY to ignored variables list.

--- dlls/ntdll/unix/env.c
+++ dlls/ntdll/unix/env.c
@@ -355,6 +355,7 @@ static BOOL is_ignored_env_var( const char *var )
             STARTS_WITH( var, "SDL_AUDIO_DRIVER=" ) ||
             STARTS_WITH( var, "SDL_VIDEODRIVER=" ) ||
             STARTS_WITH( var, "SDL_VIDEO_DRIVER=" ) ||
+            STARTS_WITH( var, "WAYLAND_DISPLAY=" ) ||
             STARTS_WITH( var, "VK_" ));
 }
 
