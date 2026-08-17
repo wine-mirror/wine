3e48bc67a2bf922b7d1e156d31a1aa16c37a455f
Bernhard Übelacker <bernhardu@mailbox.org>
2026-08-17 20:01:56 +0200

    comctl32_v6: Avoid uninitialized values in szDim in SYSLINK_Render.
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59944

--- dlls/comctl32_v6/syslink.c
+++ dlls/comctl32_v6/syslink.c
@@ -1235,6 +1235,7 @@ static VOID SYSLINK_Render (const SYSLINK_INFO *infoPtr, HDC hdc, PRECT pRect)
                 }
             }
 
+            szDim.cx = szDim.cy = 0;
             if((n == 0 && SkipChars != 0) ||
                GetTextExtentExPointW(hdc, tx, n, rc.right - x, &nFit, NULL, &szDim))
             {
