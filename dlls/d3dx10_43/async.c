2c838cdb310a8d821f9faf4ef04021f1a5a444eb
Matteo Bruni <mbruni@codeweavers.com>
2026-09-04 13:30:36 +0200

    d3dx10: Add missing wide string allocation error check in D3DX10CreateAsyncFileLoaderA().
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=60241

--- dlls/d3dx10_43/async.c
+++ dlls/d3dx10_43/async.c
@@ -385,6 +385,9 @@ HRESULT WINAPI D3DX10CreateAsyncFileLoaderA(const char *filename, ID3DX10DataLoa
 
     len = MultiByteToWideChar(CP_ACP, 0, filename, -1, NULL, 0);
     filename_w = malloc(len * sizeof(*filename_w));
+    if (!filename_w)
+        return E_OUTOFMEMORY;
+
     MultiByteToWideChar(CP_ACP, 0, filename, -1, filename_w, len);
 
     hr = D3DX10CreateAsyncFileLoaderW(filename_w, loader);
