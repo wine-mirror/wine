wine-mirror/wine

commit 07562f1e3927b2aa1521811f0c16a7f05c175cba

d3dx9: Factor out free_parameter_object_data() function.
Signed-off-by: Paul Gofman <gofmanp@gmail.com>
Signed-off-by: Matteo Bruni <mbruni@codeweavers.com>
Signed-off-by: Alexandre Julliard <julliard@winehq.org>

dlls/d3dx9_36/effect.c
@@ -542,16 +542,21 @@ static void free_sampler(struct d3dx_sampler *sampler)
 
 static void d3dx_pool_release_shared_parameter(struct d3dx_top_level_parameter *param);
 
-static void free_parameter_data(struct d3dx_parameter *param, BOOL child)
+static void free_parameter_object_data(struct d3dx_parameter *param, const void *data, unsigned int bytes)
 {
-    if (!param->data)
+    unsigned int i, count;
+
+    if (param->class != D3DXPC_OBJECT)
         return;
-    if (param->class == D3DXPC_OBJECT && !param->element_count)
+
+    count = min(param->element_count ? param->element_count : 1, bytes / sizeof(void *));
+
+    for (i = 0; i < count; ++i)
     {
         switch (param->type)
         {
             case D3DXPT_STRING:
-                heap_free(*(char **)param->data);
+                heap_free(((char **)data)[i]);
                 break;
 
             case D3DXPT_TEXTURE:
@@ -561,7 +566,8 @@ static void free_parameter_data(struct d3dx_parameter *param, BOOL child)
             case D3DXPT_TEXTURECUBE:
             case D3DXPT_PIXELSHADER:
             case D3DXPT_VERTEXSHADER:
-                if (*(IUnknown **)param->data) IUnknown_Release(*(IUnknown **)param->data);
+                if (*(IUnknown **)data)
+                    IUnknown_Release(((IUnknown **)data)[i]);
                 break;
 
             case D3DXPT_SAMPLER:
@@ -569,7 +575,8 @@ static void free_parameter_data(struct d3dx_parameter *param, BOOL child)
             case D3DXPT_SAMPLER2D:
             case D3DXPT_SAMPLER3D:
             case D3DXPT_SAMPLERCUBE:
-                free_sampler((struct d3dx_sampler *)param->data);
+                assert(count == 1);
+                free_sampler((struct d3dx_sampler *)data);
                 return;
 
             default:
@@ -577,6 +584,16 @@ static void free_parameter_data(struct d3dx_parameter *param, BOOL child)
                 break;
         }
     }
+}
+
+static void free_parameter_data(struct d3dx_parameter *param, BOOL child)
+{
+    if (!param->data)
+        return;
+
+    if (!param->element_count)
+        free_parameter_object_data(param, param->data, param->bytes);
+
     if (!child || is_param_type_sampler(param->type))
         heap_free(param->data);
 }

commit f4d8c9ac9465d4e6bde37def5fab3e0005fc677d

d3dx9: Fix sampler parameters release.
Fixes: 07562f1e3927b2aa1521811f0c16a7f05c175cba
Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=60241

dlls/d3dx9_36/effect.c
@@ -551,7 +551,10 @@ static void free_parameter_object_data(struct d3dx_parameter *param, const void 
     if (param->class != D3DXPC_OBJECT)
         return;
 
-    count = min(param->element_count ? param->element_count : 1, bytes / sizeof(void *));
+    if (is_param_type_sampler(param->type))
+        count = 1;
+    else
+        count = min(param->element_count ? param->element_count : 1, bytes / sizeof(void *));
 
     for (i = 0; i < count; ++i)
     {