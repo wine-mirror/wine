ab9ab01153f6c59c4dedb24e3f84dd90fae76474
Connor McAdams <cmcadams@codeweavers.com>
2026-09-10 10:33:53 -0400

    winebus: Get USB interface information on MacOS if possible.
    Signed-off-by: Connor McAdams <cmcadams@codeweavers.com>

--- dlls/winebus.sys/bus_iohid.c
+++ dlls/winebus.sys/bus_iohid.c
@@ -55,6 +55,7 @@
 #define PAGE_SHIFT __carbon_PAGE_SHIFT
 #include <IOKit/IOKitLib.h>
 #include <IOKit/hid/IOHIDLib.h>
+#include <IOKit/usb/IOUSBLib.h>
 #undef ULONG
 #undef E_INVALIDARG
 #undef E_OUTOFMEMORY
@@ -98,6 +99,29 @@
 
 WINE_DEFAULT_DEBUG_CHANNEL(hid);
 #ifdef __APPLE__
+
+const char *debugstr_cf(CFTypeRef t)
+{
+    CFStringRef s;
+    const char* ret;
+
+    if (!t) return "(null)";
+
+    if (CFGetTypeID(t) == CFStringGetTypeID())
+        s = CFStringCreateWithFormat(NULL, NULL, CFSTR("\"%@\""), t);
+    else
+        s = CFCopyDescription(t);
+    ret = CFStringGetCStringPtr(s, kCFStringEncodingUTF8);
+    if (ret) ret = __wine_dbg_strdup(ret);
+    if (!ret)
+    {
+        char buf[300];
+        CFStringGetCString(s, buf, sizeof(buf), kCFStringEncodingUTF8);
+        ret = __wine_dbg_strdup(buf);
+    }
+    CFRelease(s);
+    return ret;
+}
 
 static pthread_mutex_t iohid_cs = PTHREAD_MUTEX_INITIALIZER;
 
@@ -266,6 +290,62 @@ static const struct raw_device_vtbl iohid_device_vtbl =
     iohid_device_set_feature_report,
 };
 
+static io_service_t get_parent_class_object(io_service_t dev, const io_name_t class_name)
+{
+    io_service_t tmp = dev, tmp2 = 0;
+
+    while (!IORegistryEntryGetParentEntry(tmp, kIOServicePlane, &tmp2))
+    {
+        if (tmp != dev) IOObjectRelease(tmp);
+        tmp = tmp2;
+        if (IOObjectConformsTo(tmp, class_name)) return tmp;
+    }
+    if (tmp && tmp != dev) IOObjectRelease(tmp);
+    return 0;
+}
+
+static BOOL get_ioregistry_number_value(io_service_t dev, CFStringRef property, UINT *val)
+{
+    CFDataRef data;
+    BOOL ret;
+
+    *val = 0;
+    if (!(data = IORegistryEntryCreateCFProperty(dev, property, kCFAllocatorDefault, 0)))
+    {
+        WARN("Failed to get property %s.\n", debugstr_cf(property));
+        return FALSE;
+    }
+
+    if ((ret = (CFGetTypeID(data) == CFNumberGetTypeID())))
+        *val = CFNumberToDWORD((CFNumberRef)data);
+    if (!ret) ERR("Got wrong data type %lu for property %s.\n", CFGetTypeID(data), debugstr_cf(property));
+    CFRelease(data);
+    return ret;
+}
+
+static void get_usb_device_info(io_service_t usb_hid_dev, struct device_desc *desc)
+{
+    UINT class = 0, subclass = 0, protocol = 0;
+    io_service_t usb_host_iface = 0;
+    BOOL ret = FALSE;
+
+    if (!(usb_host_iface = get_parent_class_object(usb_hid_dev, kIOUSBHostInterfaceClassName))
+            && !(usb_host_iface = get_parent_class_object(usb_hid_dev, kIOUSBInterfaceClassName)))
+    {
+        ERR("Failed to get IOUSBHostInterface class object.\n");
+        goto exit;
+    }
+
+    if (!(ret = get_ioregistry_number_value(usb_host_iface, CFSTR(kUSBHostMatchingPropertyInterfaceClass), &class))) goto exit;
+    if (!(ret = get_ioregistry_number_value(usb_host_iface, CFSTR(kUSBHostMatchingPropertyInterfaceSubClass), &subclass))) goto exit;
+    if (!(ret = get_ioregistry_number_value(usb_host_iface, CFSTR(kUSBHostMatchingPropertyInterfaceProtocol), &protocol))) goto exit;
+
+    desc->bus_id = ((class & 0xff) << 16) | ((subclass & 0xff) << 8) | (protocol & 0xff);
+exit:
+    if (usb_host_iface) IOObjectRelease(usb_host_iface);
+    if (!ret) desc->bus_type = BUS_TYPE_UNKNOWN;
+}
+
 static void handle_DeviceMatchingCallback(void *context, IOReturn result, void *sender, IOHIDDeviceRef IOHIDDevice)
 {
     struct device_desc desc =
@@ -304,6 +384,8 @@ static void handle_DeviceMatchingCallback(void *context, IOReturn result, void *
         WARN("Ignoring HID device %p (vid %04x, pid %04x): not a joystick or gamepad\n", IOHIDDevice, desc.vid, desc.pid);
         return;
     }
+
+    if (desc.bus_type == BUS_TYPE_USB) get_usb_device_info(IOHIDDeviceGetService(IOHIDDevice), &desc);
 
     if (IOHIDDeviceOpen(IOHIDDevice, 0) != kIOReturnSuccess)
     {
