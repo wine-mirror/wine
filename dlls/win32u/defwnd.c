wine-mirror/wine

commit 29ef555d2749e95eca67fe2fbf4d06d2f83b91c2

win32u: Support PRF_CHILDREN when handling WM_PRINT for DefWindowProc().

dlls/win32u/defwnd.c
@@ -2831,11 +2831,39 @@ LRESULT default_window_proc( HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam, 
         if ((lparam & PRF_CHECKVISIBLE) && !is_window_visible ( hwnd )) break;
         result = 1;
 
-        if (lparam & (PRF_CHILDREN | PRF_OWNED | PRF_NONCLIENT))
+        if (lparam & (PRF_OWNED | PRF_NONCLIENT))
             WARN( "WM_PRINT message with unsupported lparam %lx\n", lparam );
 
         if (lparam & PRF_ERASEBKGND) send_message( hwnd, WM_ERASEBKGND, wparam, 0 );
-        if (lparam & PRF_CLIENT) send_message(hwnd, WM_PRINTCLIENT, wparam, lparam );
+        if (lparam & PRF_CLIENT)
+        {
+            send_message( hwnd, WM_PRINTCLIENT, wparam, lparam );
+
+            if (lparam & PRF_CHILDREN)
+            {
+                HWND *list, *child;
+                POINT org;
+                RECT rect;
+                UINT dpi;
+
+                if ((list = list_window_children( hwnd )))
+                {
+                    for (child = list; *child; child++)
+                    {
+                        if (!is_window_visible( *child ))
+                            continue;
+
+                        dpi = NtUserGetDpiForWindow( *child );
+                        NtUserGetClientRect( *child, &rect, dpi );
+                        NtUserMapWindowPoints( *child, hwnd, (POINT *)&rect, 2, dpi );
+                        offset_viewport_org( (HDC)wparam, rect.left, rect.top, &org );
+                        send_message( *child, WM_PRINT, wparam, PRF_NONCLIENT | PRF_CLIENT | PRF_ERASEBKGND | PRF_CHILDREN );
+                        set_viewport_org( (HDC)wparam, org.x, org.y, NULL );
+                    }
+                    free( list );
+                }
+            }
+        }
         break;
 
     case WM_APPCOMMAND:

dlls/user32/tests/msg.c
@@ -21201,9 +21201,8 @@ static void test_defwinproc_wm_print(void)
     lr = DefWindowProcA(hwnd, WM_PRINT, (WPARAM)hdc, PRF_CHILDREN | PRF_CLIENT);
     ok(lr == 1, "Got unexpected lr %Id.\n", lr);
     color = GetPixel(hdc, 50, 50);
-    todo_wine
     ok(color == RGB(0xff, 0, 0), "Got unexpected color %#lx.\n", color);
-    ok_sequence(wm_print_prf_children, "DefWindowProc WM_PRINT with PRF_CHILDREN | PRF_CLIENT", TRUE);
+    ok_sequence(wm_print_prf_children, "DefWindowProc WM_PRINT with PRF_CHILDREN | PRF_CLIENT", FALSE);
     flush_sequence();
 
     ReleaseDC(hwnd, hdc);

dlls/win32u/dc.c
@@ -1095,6 +1095,19 @@ BOOL WINAPI NtGdiSetBrushOrg( HDC hdc, INT x, INT y, POINT *oldorg )
 }
 
 
+BOOL offset_viewport_org( HDC hdc, INT x, INT y, POINT *point )
+{
+    DC *dc;
+
+    if (!(dc = get_dc_ptr( hdc ))) return FALSE;
+    if (point) *point = dc->attr->vport_org;
+    dc->attr->vport_org.x += x;
+    dc->attr->vport_org.y += y;
+    release_dc_ptr( dc );
+    return NtGdiComputeXformCoefficients( hdc );
+}
+
+
 BOOL set_viewport_org( HDC hdc, INT x, INT y, POINT *point )
 {
     DC *dc;

dlls/win32u/ntgdi_private.h
@@ -185,6 +185,7 @@ extern struct dce *get_dc_dce( HDC hdc );
 extern void set_dc_dce( HDC hdc, struct dce *dce );
 extern WORD set_dce_flags( HDC hdc, WORD flags );
 extern DWORD set_stretch_blt_mode( HDC hdc, DWORD mode );
+extern BOOL offset_viewport_org( HDC hdc, INT x, INT y, POINT *point );
 extern BOOL set_viewport_org( HDC hdc, INT x, INT y, POINT *point );
 extern void DC_InitDC( DC * dc );
 extern void DC_UpdateXforms( DC * dc );

commit 79e718d7bfe61c8c61cff87f4f3119e1285a96f9

win32u: Set a clip region when handling WM_PRINT PRF_CHILDREN.
Fix a regression from "29ef555d win32u: Support PRF_CHILDREN when handling WM_PRINT for DefWindowProc().".

SlingPlayer 1.5 draws its child window out of the window boundary, thus corrupting the parent window
content. Tests show that a clip region is set to protect the child window from doing so.

Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=59934

dlls/win32u/defwnd.c
@@ -2856,13 +2856,18 @@ LRESULT default_window_proc( HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam, 
 
             if (lparam & PRF_CHILDREN)
             {
+                HDC hdc = (HDC)wparam;
                 HWND *list, *child;
+                HRGN clip_rgn;
+                int old_clip;
                 POINT org;
                 RECT rect;
                 UINT dpi;
 
                 if ((list = list_window_children( hwnd )))
                 {
+                    clip_rgn = NtGdiCreateRectRgn( 0, 0, 0, 0 );
+
                     for (child = list; *child; child++)
                     {
                         if (!(get_window_long( *child, GWL_STYLE ) & WS_VISIBLE))
@@ -2871,10 +2876,19 @@ LRESULT default_window_proc( HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam, 
                         dpi = NtUserGetDpiForWindow( *child );
                         NtUserGetClientRect( *child, &rect, dpi );
                         NtUserMapWindowPoints( *child, hwnd, (POINT *)&rect, 2, dpi );
-                        offset_viewport_org( (HDC)wparam, rect.left, rect.top, &org );
+
+                        old_clip = NtGdiGetRandomRgn( hdc, clip_rgn, NTGDI_RGN_MIRROR_RTL | 1 );
+                        NtGdiIntersectClipRect( hdc, rect.left, rect.top, rect.right, rect.bottom );
+                        offset_viewport_org( hdc, rect.left, rect.top, &org );
                         send_message( *child, WM_PRINT, wparam, PRF_NONCLIENT | PRF_CLIENT | PRF_ERASEBKGND | PRF_CHILDREN );
-                        set_viewport_org( (HDC)wparam, org.x, org.y, NULL );
+                        set_viewport_org( hdc, org.x, org.y, NULL );
+                        if (old_clip == 0)
+                            NtGdiExtSelectClipRgn( hdc, NULL, RGN_COPY );
+                        else if (old_clip == 1)
+                            NtGdiExtSelectClipRgn( hdc, clip_rgn, RGN_COPY );
                     }
+
+                    NtGdiDeleteObjectApp( clip_rgn );
                     free( list );
                 }
             }

dlls/user32/tests/msg.c
@@ -11440,12 +11440,10 @@ static LRESULT WINAPI WmPrintProcA(HWND hwnd, UINT message, WPARAM wp, LPARAM lp
 
         clip_rgn = CreateRectRgn(0, 0, 1, 1);
         has_clip_rgn = GetClipRgn(hdc, clip_rgn);
-        todo_wine
         ok(has_clip_rgn == 1, "Expected a clip region.\n");
         ret = GetRgnBox(clip_rgn, &rect);
         ok(ret == SIMPLEREGION, "Got unexpected ret %d.\n", ret);
         SetRect(&expected_rect, 50, 50, 100, 100);
-        todo_wine
         ok(EqualRect(&rect, &expected_rect), "Got unexpected rect %s.\n", wine_dbgstr_rect(&rect));
         DeleteObject(clip_rgn);
 