ae807355d62d5fee2574a5eb3bbdb6072a7c611b
Jacek Caban <jacek@codeweavers.com>
2026-09-18 17:52:23 +0200

    ieframe: Handle memory allocation failure in create_callback.

--- dlls/ieframe/navigate.c
+++ dlls/ieframe/navigate.c
@@ -585,7 +585,10 @@ static const IHttpSecurityVtbl HttpSecurityVtbl = {
 static BindStatusCallback *create_callback(DocHost *doc_host, LPCWSTR url, PBYTE post_data,
         ULONG post_data_len, LPCWSTR headers)
 {
-    BindStatusCallback *ret = malloc(sizeof(BindStatusCallback));
+    BindStatusCallback *ret;
+
+    if(!(ret = malloc(sizeof(BindStatusCallback))))
+        return NULL;
 
     ret->IBindStatusCallback_iface.lpVtbl = &BindStatusCallbackVtbl;
     ret->IHttpNegotiate_iface.lpVtbl      = &HttpNegotiateVtbl;
@@ -1032,8 +1035,10 @@ HRESULT navigate_url(DocHost *This, LPCWSTR url, const VARIANT *Flags,
         task_navigate_bsc_t *task;
 
         task = malloc(sizeof(*task));
-        task->bsc = create_callback(This, url, post_data, post_data_len, headers);
-        push_dochost_task(This, &task->header, navigate_bsc_proc, navigate_bsc_task_destr, This->url == NULL);
+        if((task->bsc = create_callback(This, url, post_data, post_data_len, headers)))
+            push_dochost_task(This, &task->header, navigate_bsc_proc, navigate_bsc_task_destr, This->url == NULL);
+        else
+            hres = E_OUTOFMEMORY;
     }
 
     if(post_data)
@@ -1084,8 +1089,10 @@ static HRESULT navigate_hlink(DocHost *This, IMoniker *mon, IBindCtx *bindctx,
     if(This->doc_navigate) {
         hres = async_doc_navigate(This, url, headers, post_data, post_data_len, FALSE);
     }else {
-        bsc = create_callback(This, url, post_data, post_data_len, headers);
-        hres = navigate_bsc(This, bsc, mon);
+        if((bsc = create_callback(This, url, post_data, post_data_len, headers)))
+            hres = navigate_bsc(This, bsc, mon);
+        else
+            hres = E_OUTOFMEMORY;
         IBindStatusCallback_Release(&bsc->IBindStatusCallback_iface);
     }
 
