4c18d96e9bc5e3c4e9a2a9aa19b909f5c93afd4e
Alexandre Julliard <julliard@winehq.org>
2026-09-23 19:34:02 +0200

    ntdll: Always reserve some top-down space.
    The preloader may skip it if it overlaps with the stack.

--- dlls/ntdll/unix/virtual.c
+++ dlls/ntdll/unix/virtual.c
@@ -751,13 +751,10 @@ static void mmap_init( const struct preload_info *preload_info )
         reserve_area( user_space_limit, 0 );
 
 #else
-
+    reserve_area( (void *)0x7ffff0000000, (void *)0x7ffffe000000 ); /* top-down area */
     if (preload_info) return;
-    /* if we don't have a preloader, try to reserve the space now */
     reserve_area( (void *)0x000000010000, (void *)0x000068000000 );
     reserve_area( (void *)0x00007f000000, (void *)0x00007fff0000 );
-    reserve_area( (void *)0x7ffffe000000, (void *)0x7fffffff0000 );
-
 #endif
 }
 
