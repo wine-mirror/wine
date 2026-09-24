7666b8eafd0eb716ff07a319af6541f8b68ab5c7
Nikolay Sivov <nsivov@codeweavers.com>
2026-09-24 12:28:56 +0200

    unicode: Update to Unicode 18.0.0.
    Signed-off-by: Nikolay Sivov <nsivov@codeweavers.com>

--- dlls/wineps.drv/vertical.c
+++ dlls/wineps.drv/vertical.c
@@ -1,5 +1,5 @@
 /* Unicode Vertical Orientation */
-/* generated from https://www.unicode.org/Public/17.0.0/ucd/UCD.zip:VerticalOrientation.txt */
+/* generated from https://www.unicode.org/Public/18.0.0/ucd/UCD.zip:VerticalOrientation.txt */
 /* DO NOT EDIT!! */
 
 #include "windef.h"
