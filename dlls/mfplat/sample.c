3d3cceaff348eee45ca807e9003ad7646646a3d9
Conor McCarthy <cmccarthy@codeweavers.com>
2026-09-25 17:57:04 +1000

    mfplat: Check for null media type in sample_allocator_InitializeSampleAllocator().
    Wine-Bug: https://bugs.winehq.org/show_bug.cgi?id=60241

--- dlls/mfplat/sample.c
+++ dlls/mfplat/sample.c
@@ -1518,6 +1518,8 @@ static HRESULT WINAPI sample_allocator_InitializeSampleAllocator(IMFVideoSampleA
 
     if (!sample_count)
         return E_INVALIDARG;
+    if (!media_type)
+        return E_POINTER;
 
     EnterCriticalSection(&allocator->cs);
 
